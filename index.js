b5c6ebd2fad18eb3311f32de01666355a9641b3d
Axhon <axhonruiz@gmail.com>
2018-04-02 16:56:55 -1000

    refactoring

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
 const cardProps = {
   title: "Lambda School",
-  handle: "@lambdaschool",
+  handle: "lambdaschool",
   date: "26 jan",
+  image: {
+    src: "https://ibin.co/3whrpKSBbZ81.png",
+    alt: "lambda logo"
+  },
+
   message:
     "Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!",
   hero: {
     title: "Get started with React",
     message:
       "React makes it painless to create interactive UIs. Design simple view for eacth state in your application.",
-    url: "reactjs.org"
+    url: "reactjs.org",
+    img: {
+      src: "https://ibin.co/3wnC6SgIOJud.png",
+      alt: "reactjs logo"
+    }
   }
 };
 
-const { hero } = cardProps;
+const Card = ({ ...props }) => (
+  <div className="Card--container">
+    <img className="Card--logo" src={props.image.src} alt={props.image.alt} />
+    <div className="Card">
+      <h3 className="Card--title">{props.title}</h3>
+      <a href={`https://twitter.com/${props.handle}`} className="Card--handle">
+        @{props.handle}
+      </a>
+      <p className="Card--date">{props.date}</p>
+      <p className="Card--message">{props.message}</p>
+      <div className="Hero">
+        <img
+          className="Hero--image"
+          src={props.hero.img.src}
+          alt={props.hero.img.alt}
+        />
+        <h3>{props.hero.title}</h3>
+        <p className="Hero--message">{props.hero.message}</p>
+        <a href="reactjs.org" className="Hero--url">
+          {props.hero.url}
+        </a>
+      </div>
+    </div>
+  </div>
+);
 
 const App = () => {
   return (
-    <div className="container">
-      <img
-        className="logo"
-        src="https://ibin.co/3whrpKSBbZ81.png"
-        alt="lambda logo"
-      />
-      <div className="Card">
-        <h3 className="Card--title">{cardProps.title}</h3>
-        <a
-          href="https://twitter.com/{cardProps.handle}"
-          className="Card--handle"
-        >
-          {cardProps.handle}
-        </a>
-        <p className="Card--date">{cardProps.date}</p>
-        <p className="Card--message">{cardProps.message}</p>
-        <div className="Hero">
-          <img
-            className="Hero--image"
-            src="https://ibin.co/3wnC6SgIOJud.png"
-            alt="react logo"
-          />
-          <h3>{hero.title}</h3>
-          <p className="Hero--message">{hero.message}</p>
-          <a href="reactjs.org" className="Hero--url">
-            {hero.url}
-          </a>
-        </div>
-      </div>
+    <div>
+      <Card {...cardProps} />
+      <div className="Calc--container" />
     </div>
   );
 };
