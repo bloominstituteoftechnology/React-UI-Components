d01389a75070367e2c32713cb20fc283a89cf5ed
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-04-30 19:24:24 -0400

    Moved css and data object to external files

---
+++ js/social-card-data.js
@@ -0,0 +1,20 @@
+const TWEETS = [
+  {
+    id: 1,
+    timestamp: "26 jan",
+    user: {
+      fullName: "Lambda School",
+      username: "LambdaSchool",
+      avatar: "https://ibin.co/3whrpKSBbZ81.png"
+    },
+    message:
+      "Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!",
+    media: {
+      img: "https://ibin.co/3wnC6SgIOJud.png",
+      url: "reactjs.org",
+      title: "Get started with React",
+      description:
+        "React makes it painless to create interactive UIs. Design simple views for each state in your application."
+    }
+  }
+];
