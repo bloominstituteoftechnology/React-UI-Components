a3bb9fe7f1d2ef237c0de38597e3d98b173f48d4
Zach Campbell <beazlebubbles@gmail.com>
2018-04-30 22:59:14 -0600

    Added flexibility to button components specified individual button component's props

--- js/index_calc.jsx
+++ js/index_calc.jsx
@@ -1,42 +1,42 @@
-const Display = () => {
+const Display = (props) => {
   return (
-    <div className="display">0</div>
+    <div className="display">{props.calcDisplay}</div>
   );
 }
 
-const NormalButton = () => {
+const NormalButton = (props) => {
   return (
-    <div className="normalButton">1</div>
+    <div className={'normalButton ' + props.custClass}>{props.text}</div>
   );
 }
 
-const BigButton = () => {
+const BigButton = (props) => {
   return (
-    <div className="bigButton">1</div>
+    <div className={'bigButton ' + props.custClass}>{props.text}</div>
   );
 }
 
 const App = () => {
   return (
     <div className="calculator">
-      <Display />
+      <Display calcDisplay="80085"/>
 
-      <BigButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <NormalButton />
-      <BigButton />
-      <NormalButton />
+      <BigButton text="clear" />
+      <NormalButton text="/" custClass="opButton" />
+      <NormalButton text="7" />
+      <NormalButton text="8" />
+      <NormalButton text="9" />
+      <NormalButton text="*" custClass="opButton" />
+      <NormalButton text="4" />
+      <NormalButton text="5" />
+      <NormalButton text="6" />
+      <NormalButton text="-" custClass="opButton" />
+      <NormalButton text="1" />
+      <NormalButton text="2" />
+      <NormalButton text="3" />
+      <NormalButton text="+" custClass="opButton" />
+      <BigButton text="0" />
+      <NormalButton text="=" custClass="opButton" />
     </div>
   );
 }
