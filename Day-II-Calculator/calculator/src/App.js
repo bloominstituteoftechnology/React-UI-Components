2af2098f996ce2a9432c9539c825e6e1224c4376
Ryan Hamblin <ryanhca@gmail.com>
2018-06-04 17:25:37 -0600

    adds repo for calc

---
+++ Day-II-Calculator/calculator/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h3>Welcome to React Calculator</h3>
+      <p>
+        We have given you a starter project. You'll want to build out your
+        components in their respective files, remove this code and replace it
+        with the proper components
+      </p>
+    </div>
+  );
+};
+
+export default App;
