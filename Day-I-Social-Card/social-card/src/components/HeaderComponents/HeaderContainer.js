fe472ab7737c7a4ebe46d88a3bdb3f0868f7b523
DirupT <iamcooled@gmail.com>
2018-06-11 16:20:23 -0500

    Day-I finished everything up to footer

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle.js';
+import HeaderContent from './HeaderContent.js';
+import ImageThumbnail from './ImageThumbnail.js';
+
+const HeaderContainer = () => {
+    return (
+        <div className='header-wrapper'>
+        <ImageThumbnail />
+        <HeaderContent />
+        </div>
+    );
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className='header-title'>
+        <h1>Lambda School</h1>
+        <h3>@LambdaSchool • 26 jan</h3>
+        </div>
+    );
+}
+
+export default HeaderTitle;
