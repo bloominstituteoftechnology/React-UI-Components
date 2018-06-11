20b9e12ae3dbfb3a9dd4c043ac5fd3574c07d9e7
Erin Meredith <ekmeredith@att.net>
2018-06-11 17:36:31 -0400

    first commit

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div>
+        <h3 className='card-title'> Get started with React</h3>
+        <p className='card-content'>React makes it painless to create interactiv UIs. Design simple views for each state in your application.</p>
+        <p className='link'>reactjs.org</p>
+        </div>
+)};
+
+export default CardContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+import Logo from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return (
+        <div className='container'>
+        <div className='header'>
+        <Logo />
+        <HeaderTitle />
+         </div>
+        <HeaderContent />
+       
+        </div>
+    )
+
+};
+
+export default HeaderContainer;
