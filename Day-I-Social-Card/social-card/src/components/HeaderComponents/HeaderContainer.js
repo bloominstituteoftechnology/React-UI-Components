21d0d6e773495147f949f4b3acf9480bf57602dd
Joe Stanfield <jlstan06@hotmail.com>
2018-06-11 15:49:12 -0400

    created header components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from "./HeaderContent";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+
+
+const HeaderContainer = () => {
+    return (
+    <div className="header-container">
+    <ImageThumbnail />
+    <HeaderTitle />
+    <HeaderContent />
+    </div>
+    );
+};
+
+export default HeaderContainer
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => <div className="headerTitle"></div>
+
+
+export default HeaderTitle
