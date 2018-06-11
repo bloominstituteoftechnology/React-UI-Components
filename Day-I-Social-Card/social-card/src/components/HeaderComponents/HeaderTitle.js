d087a7b8db24f30fd3e8bc5345686ea078ecf3b8
meganfontenot <meganfontenot517@yahoo.com>
2018-06-11 18:07:59 -0500

    didnt get far

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+const HeaderTitle = () => {
+    return (
+        <div className="title">
+        <h1>Lambda School 
+            <small>@LambdaSchool &middot; 
+                <time>June 11</time>
+                </small>
+                </h1>
+        </div>
+    );
+};
+
+export default HeaderTitle;
