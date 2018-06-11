89dcaa7928f1221a38f09e20d6df71d014e2f052
Munsu <dark_specineff2002@yahoo.com>
2018-06-11 16:01:51 -0700

    Khoa Nguyen React-UI-Components Day-I-Social-Card

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return <div className="card-content">
+    <h1>Get started with React</h1>
+    <p>React makes it painless to creat interactive UIs. Design simple views for each state in your application.</p>
+    <p>reactjs.org</p>
+    </div>
+};
+
+export default CardContent;
