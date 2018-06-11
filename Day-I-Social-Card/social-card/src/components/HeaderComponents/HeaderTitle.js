03ddfa594fcc957faaefe1edcd6e82ab055a7918
snowcoding <vimalshah77@gmail.com>
2018-06-11 13:03:33 -0700

    Basic Layout in place

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <p>
+      <span className='title'>Lambda School</span>
+      <span className='twitter'>@Lambda School</span>
+      <span>&middot;</span> 
+      <span className='date'>26 jan</span>
+    </p>
+  )
+}
+
+export default HeaderTitle
