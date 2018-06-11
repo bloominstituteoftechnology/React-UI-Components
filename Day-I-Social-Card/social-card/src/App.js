4e7ad814b947f716ec264ae7a713c5afc671c264
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-11 18:48:45 -0400

    completed card banner - MVP done. Starting on stretch

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import HeaderContent from './components/HeaderComponents/HeaderContent';
-import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
+import CardContainer from './components/CardComponents/CardContainer';
+
 
 const App = () => {
   return (
     <div className="container">
       <HeaderContainer />
+      <CardContainer />
     </div>
   );
 };
