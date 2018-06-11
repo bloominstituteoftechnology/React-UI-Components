85016c7de5bd8b705939008c04b0d1b60bf7f369
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-11 13:48:36 -0600

    Basic react header and card structure in place

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import "./Header.css";
+import HeaderTitle from "./HeaderTitle.js";
+import HeaderContent from "./HeaderContent.js";
+
+const HeaderContainer = () => {
+  return (
+    <div className="header-container">
+      <div className="header-logo">
+        <img src="https://ibin.co/3whrpKSBbZ81.png" alt="Lambda Logo" />
+      </div>
+      <div className="header-text">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title">
+            <h1>
+                Lambda School <span className="gray-note">@LambdaSchool · 26 jan </span>
+            </h1>
+        </div>
+    
+    );
+};
+
+export default HeaderTitle;
