8c676dd8176463fbd0802798421f9c00f4660d66
Luis <luis@Luiss-MacBook-Air.local>
2018-06-11 14:28:12 -0700

    made cardBanner img a background instead inside an image tag

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import './Card.css'
 
-const CardBanner = () => (
-  <div className='CardBanner'>
-    <img
-      src={'https://ibin.co/3wnC6SgIOJud.png'}
-      alt='banner logo'
-      className='img-fluid'
-    />
-  </div>
-)
+const CardBanner = () => <div className='CardBanner' />
 
 export default CardBanner
