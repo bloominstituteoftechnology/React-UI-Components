e05891d4bd4edb17e2fb98fb8ab47ca070c2ecb5
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-06-11 18:00:19 -0400

    Third commit - Added footer

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return <div className="footer">
+        <div>
+            <img className="icon-gray" src="https://ibin.co/44xlP0ABev28.png" />
+        </div>
+            
+        <div>
+            <img src="https://ibin.co/44xlmPyeKO8e.png" />
+        </div>
+
+        <div>
+            <p className="icon-gray">6</p>
+        </div>
+
+        <div>
+            <img src="https://ibin.co/44xl8SDNdsyU.png" />
+        </div>
+
+        <div>
+            <p className="icon-gray">4</p>
+        </div>
+            
+        <div>
+            <img src="https://ibin.co/44xmBhw6oGrm.png" />
+        </div>
+
+
+    </div>;
+};
+
+export default Footer;
