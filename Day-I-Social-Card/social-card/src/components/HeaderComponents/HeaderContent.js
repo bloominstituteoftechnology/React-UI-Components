d16e325a3f19dbfcba505780b3027baf4794e16c
Wdifulvio523 <w.difulvio523@gmail.com>
2018-06-11 19:05:01 -0400

    minor changes

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -7,7 +7,7 @@ const HeaderContent = () => {
 return (
 <div className="header-content">
  <HeaderTitle />
- <p>
+ <p  className="header-text">
      Let's learn React by building simple interfaces with components.
     Don't try to overthink it, just keep it simple and have fun.
     Once you feel comfortable using components you are well on your way to mastering React!
