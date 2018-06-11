f1e2eb71da521c98decdb7178f1ce1448e691e40
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-11 17:39:28 -0500

    added footer

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './components/HeaderComponents/HeaderContainer';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
     <div className='container'>
       <HeaderContainer />
       <CardContainer />
+      <Footer />
     </div>
   );
 };
