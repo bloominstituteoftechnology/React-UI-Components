1952cf7f2602ce3bc57c365f8a36426d4afee9bf
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-11 17:53:59 -0600

    Got moment working

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import moment from 'moment';
 import './Header.css';
 
+let m = moment().format("D MMM");
 
 const HeaderTitle = () => {
     return (
         <div className="header-title">
             <h1>
-                Lambda School <span className="gray-note">@LambdaSchool · 26 jan </span>
+                Lambda School <span className="gray-note">@LambdaSchool · {m} </span>
             </h1>
         </div>
     
