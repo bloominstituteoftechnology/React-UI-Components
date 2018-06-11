7b35676ca8b63ba816835ff2e60f8e56597d5188
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-11 16:33:33 -0700

    cleans up spacing

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import './Footer.css';
 
-let commentCount = 0;
+let commentCount = 1;
 let syncCount = 0;
 let likeCount = 0;
 let emailCount = 0;
 
 const Footer = () => (
   <div className="footer">
-    <div>
-      <i class="far fa-comment" data-fa-transform="flip-h" />{' '}
+    <div
+      onClick={() => {
+        commentCount++;
+        console.log(commentCount);
+      }}
+    >
+      <i className="far fa-comment" data-fa-transform="flip-h" />{' '}
       {commentCount ? commentCount : ''}
     </div>
     <div>
-      <i class="fas fa-sync" />
+      <i className="fas fa-sync" />
       {syncCount ? syncCount : ''}
     </div>
     <div>
-      <i class="far fa-heart" />
+      <i className="far fa-heart" />
       {likeCount ? likeCount : ''}
     </div>
     <div>
-      <i class="far fa-envelope" />
+      <i className="far fa-envelope" />
       {emailCount ? emailCount : ''}
     </div>
   </div>
