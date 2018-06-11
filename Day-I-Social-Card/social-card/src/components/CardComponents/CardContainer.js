d4d6abe05b5bddb924ec3660ee4280ef64607f25
Simon <Simon_Freelance@hotmail.com>
2018-06-11 17:11:07 -0400

    The whole card container now has the reactjs link MVP completed

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner.js';
 import CardContent from './CardContent.js';
+
+function activateLink(link) {
+    return window.location.href = `${link}`;
+}
 const CardContainer = () => {
     return (
-        <div class = "card-container">
+        <div class = "card-container" onClick={() => 
+                activateLink('https://www.reactjs.org')}>
+
         <CardBanner />
         <CardContent />
         </div>
