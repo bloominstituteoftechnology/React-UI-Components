0fa68f92cd57c6833447bfc950a59bc2e71a4087
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-11 15:21:07 -0500

    completed component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -4,7 +4,10 @@ import './Header.css';
 const HeaderTitle = () => {
     return (
         <div className="HeaderTitle">
-        
+            <h1>Lambda School</h1>
+            <p className='lambda-handle'> @LambdaSchool • 26 jan</p>
         </div>
     );
 };
+
+export default HeaderTitle;
