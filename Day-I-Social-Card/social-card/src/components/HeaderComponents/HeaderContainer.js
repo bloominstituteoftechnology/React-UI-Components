bfb513ad40047d74aae31bd947572749aaa6f3a4
Tai <bowlofryce@gmail.com>
2018-06-11 17:51:11 -0500

    Ongoing processing, React is awesome.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = () => {
+    return <div className="header-wrapper">Header Container</div>;
+};
+
+export default HeaderContainer;
