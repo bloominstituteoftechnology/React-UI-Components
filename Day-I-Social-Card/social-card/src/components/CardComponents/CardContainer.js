05aae987c6dde88551553fd7471f1ed6d10077f4
chainchompa <chainchompa@gmail.com>
2018-06-11 17:54:46 -0400

    card component redirects to react website

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -3,9 +3,14 @@ import './Card.css';
 import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
+function redirect() {
+  return window.location.href = `https://www.reactjs.org`;
+}
+
 const CardContainer = () => {
   return (
-  <div className = 'card-wrapper'>
+  <div className = 'card-wrapper' onClick={() =>
+    redirect()}>
     <div className = 'card-image'>
       <CardBanner />
     </div>
