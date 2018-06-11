9d0047abd077a0441cd2c426e15039c4e0c71ece
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-11 14:48:34 -0500

    created HeaderContainer

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import './ImageThumbnail';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return (
+        <div className="HeaderContainer">
+            <ImageThumbnail />
+        </div>
+    );
+};
+
+
+export default HeaderContainer;
