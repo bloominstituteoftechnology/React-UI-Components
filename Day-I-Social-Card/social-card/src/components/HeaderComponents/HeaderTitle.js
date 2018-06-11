0042cb1b064078b91ddc865b539ea1ad30953f1e
Simon <Simon_Freelance@hotmail.com>
2018-06-11 17:45:44 -0400

    Moment.js has been implemented in the HeaderTitle

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css';
-
+const moment = require('moment');
 const HeaderTitle = () => {
     return (
-        <div className ="header-title-container">
-        <h2 className = 'lambda-title'>Lambda School</h2>
-        <p className = 'lambda-handle'>@LambdaSchool</p>
-        <p className = 'timestamp'>26 jan</p>
+        <div className="header-title-container">
+            <h2 className='lambda-title'>Lambda School</h2>
+            <p className='lambda-handle'>@LambdaSchool</p>
+            <p className='timestamp'>{moment().format('DD MMM').toLowerCase()}</p>
         </div>
     );
 }
