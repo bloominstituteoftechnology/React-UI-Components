66793742148ff17fc97ed61feefb7ca0253b2d85
Kevin Tena <kevten09@gmail.com>
2018-06-11 17:55:26 -0500

    Added a primitive version of Footer to component

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div>
       <HeaderContainer />
+      <a href='https://www.reactjs.org'>
       <CardContainer />
+      </a>
+      <Footer />
     </div>
   );
 };
---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+     <div className="footer-wrapper">
+        <img src="https://ibin.co/44xlP0ABev28.png"></img>
+        <img src="https://ibin.co/44xlmPyeKO8e.png"></img>
+        <p>6</p>
+        <img src="https://ibin.co/44xl8SDNdsyU.png"></img>
+        <p>4</p>
+        <img src="https://ibin.co/44xmBhw6oGrm.png"></img>
+    </div>
+    );
+}
+
+export default Footer;
