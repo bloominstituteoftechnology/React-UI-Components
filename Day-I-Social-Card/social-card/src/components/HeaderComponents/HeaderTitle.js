a4af52ac4f1aa8cf49e46cdb86014cad963c1d8c
Nicky Chen <nicky.chen0@gmail.com>
2018-06-11 14:13:39 -0700

    initial commit

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <p><strong>Lambda School</strong> <span class="grey">@LambdaSchool • 26 jan</span></p>
+
+
+
+    )
+    
+}
+
+
+export default HeaderTitle;
