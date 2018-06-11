05f95230132dfc0d221ab53a4073f5b1ba0d5ab3
Allen Hai <allenhai03@gmail.com>
2018-06-11 17:45:07 -0500

    got header done

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
+import ImageThumbnail from './ImageThumbnail'
+import HeaderContent from './HeaderContent'
+import HeaderTitle from './HeaderTitle'
 import './Header.css';
+
+function HeaderContainer () {
+  return (
+    <div className="header-container">
+      <div className="img-container">
+        <ImageThumbnail />
+      </div>
+      <div className="header-content-container">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  )
+}
+
+export default HeaderContainer
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle () {
+  return (
+    <div className="header-title-container">
+      <h1>Lambda School</h1>
+      <span> @LambdaSchool - 26 jan </span>
+    </div>
+  )
+}
+
+export default HeaderTitle
