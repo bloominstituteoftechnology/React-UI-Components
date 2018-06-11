cfadb7c96a91d3e144e1454241f805570f2d5517
jeffyjkang <jeffyjkang@gmail.com>
2018-06-11 12:49:21 -0700

    Jeff Kang social card update

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div>
+            <h1>
+                @LambdaSchool
+            </h1>
+            <h1>
+                timestamp
+            </h1>
+        </div>
+    )
+}
+
+export default HeaderTitle;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return (
+    <img class = "image" src="https://ibin.co/3whrpKSBbZ81.png" alt="lambda logo"/>
+    );
+};
+
+export default ImageThumbnail;
