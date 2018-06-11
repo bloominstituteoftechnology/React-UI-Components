389324631dfd6b20bec1485b5952cf481036de58
Vu Cao <cdhvu@yahoo.com>
2018-06-11 12:58:20 -0700

    Initial Commit
    Working on HeaderContainer.js.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+function HeaderContainer() {
+    return (
+        <div className='header-container-wrapper'>
+            <HeaderContent />
+        </div>
+    )
+}
+
+export default HeaderContainer;
