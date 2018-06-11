44efa1638492aca8df55870d627d4247947c9d8c
Simon <Simon_Freelance@hotmail.com>
2018-06-11 16:13:12 -0400

    Completed rough draft of styling of the header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -5,7 +5,7 @@ const HeaderTitle = () => {
     return (
         <div className ="header-title-container">
         <h2 className = 'lambda-title'>Lambda School</h2>
-        <p className = 'lambda-handle'>LambdaSchool</p>
+        <p className = 'lambda-handle'>@LambdaSchool</p>
         <p className = 'timestamp'>26 jan</p>
         </div>
     );
