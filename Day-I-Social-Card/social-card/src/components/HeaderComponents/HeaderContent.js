ae1546dc0ad6688c47eeafcaaf15d9fb745d30d2
emaquino44 <prexy44@yahoo.com>
2018-06-11 14:12:03 -0700

    rough layout for header section but getting used to component setup

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderContent = () => {
+return (
+	<p>Let's Learn React from Simple Interfaces with Components. Don't try to overthink, just keep it simple and have fun. Once you feel comfortable using components you are well on your way mastering React.</p>
+	)
+}
+
+export default HeaderContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+	return (
+		<div>
+        <h4>Lambda School <span className="Banner__greyText">@LambdaSchool ♦ 26 Jan</span></h4>
+        <p>Let's learn React by building simple interfaces with components.  Don't try to overthink it, just keep it simple and have fun.  Once you geel comfortable using components you are well on your way to mastering React!
+        </p>
+      </div>
+
+
+	)
+}
+
+export default HeaderTitle;
