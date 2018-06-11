76ab65708e4cbfa0abbbac59cebaf41e09a2014f
Sania Parekh <sparekh@Sanias-MacBook.home>
2018-06-11 17:49:06 -0500

    Header almost complete

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+      return (
+        <div className = 'header-wrapper'>
+        <ImageThumbnail />
+       Header Container
+        <HeaderContent />
+     </div>
+    );
+};
+
+export default HeaderContainer;
