a72aea215f332d8b0aae709e5ba64ce61d0561a4
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-11 17:48:22 -0400

    finished react Header, working on CSS

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = () => {
+    return (
+        <div className="header-container">
+            <ImageThumbnail />
+            <div className="header-content">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
+    );
+};
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className = "header-title">
+            <p>
+                <span className="header-title-text">Lambda School</span>
+                <a className="header-title-handle" href="https://twitter.com/lambdaschool">@lambdaschool</a>
+                &middot;
+                <span className="header-title-date">26 Jan</span>
+            </p>
+        </div>
+    );
+};
+
+export default HeaderTitle;
