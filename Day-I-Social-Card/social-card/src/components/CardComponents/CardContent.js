0fdde5eea6bf89b56fdb8f0c6a72bb5798778ee4
Jameson <jamesonbrown@neolivity.com>
2018-06-11 15:50:02 -0600

    Minor CSS adjustments to CSS Header and Card.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const CardContent = () => {
     return (
-        <div class="card-content">
+        <div className="card-content">
             <h3>Get started with React</h3>
             <p>React makes it painless to create interactive 
                 UIs. Design simple views for each state in 
