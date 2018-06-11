3c15e9c02c6271239b861983677f97bf7de2fbbe
cmok4290 <cmok4290@gmail.com>
2018-06-11 17:03:18 -0400

    finished header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 
 const ImageThumbnail = () => {
-    return <img src="https://ibin.co/3whrpKSBbZ81.png" alt="logo"/>;
+    return <img src="https://ibin.co/3whrpKSBbZ81.png" class="image-thumbnail" alt="logo"/>;
 };
 
 export default ImageThumbnail;
