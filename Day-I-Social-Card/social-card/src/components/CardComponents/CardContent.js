fb496e0f1ab5d08010663ab196a958c133532a1a
Wdifulvio523 <w.difulvio523@gmail.com>
2018-06-11 17:15:02 -0400

    completed card components

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+
+
+const CardContent = () => {
+    return (
+      <div className="card-content">
+        <p>Get started with React</p>
+        <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+        <p>reactjs.org</p>
+      </div>
+    );
+  };
+  
+  export default CardContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -3,7 +3,7 @@ import "./Header.css";
 
 const ImageThumbnail = () => {
   return (
-    <img src="https://ibin.co/3whrpKSBbZ81.png" className="image-thumbnail" />
+    <img src="https://ibin.co/3whrpKSBbZ81.png" className="image-thumbnail" alt="thumbnail" />
   );
 };
 
