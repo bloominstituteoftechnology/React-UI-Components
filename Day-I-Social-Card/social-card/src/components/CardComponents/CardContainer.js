e1ef09b44ba323f8b4979cc66324b321bc291343
Simon <Simon_Freelance@hotmail.com>
2018-06-11 16:41:18 -0400

    Finished CardContent component and implemented in CardContainer.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner.js';
-
+import CardContent from './CardContent.js';
 const CardContainer = () => {
     return (
         <div class = "card-container">
         <CardBanner />
+        <CardContent />
         </div>
     );
 };
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div class="card-content-container">
+            <div class = "title">Get started with React</div>
+            <p class="content">React makes it paineless to create interactive Uls.
+                Design simple views for each state in your application.
+            </p>
+            <p class = "link">reactjs.org</p>
+        </div>
+    )
+}
+
+export default CardContent;
