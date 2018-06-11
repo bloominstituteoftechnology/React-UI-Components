b9e093e2067eb4555fef9f2a2340c47434ac809c
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-11 16:09:51 -0500

    completed base requirement

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Header.css';
 
-const HeaderContent = () => <p className = 'HeaderContent'>Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>;
+const HeaderContent = () => {
+    return (
+        <div className='HeaderContent'>
+            <p>Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
+        </div>
+    );
+};
 
 export default HeaderContent;
