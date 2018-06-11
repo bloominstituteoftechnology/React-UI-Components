f63056af30d53cc43208e4e72d75c23b52bf1b72
Luis <luis@Luiss-MacBook-Air.local>
2018-06-11 12:32:12 -0700

    created CardContent stateless functional component & imported into CardContainer

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import './Card.css';
+import React from 'react'
+import './Card.css'
+
+const CardContent = () => {
+  return (
+    <div>
+      <span>
+        {' '}
+        <strong>Get started with React</strong>
+      </span>
+      <p>
+        React makes it painless to create interactive Uls. Design simple views
+        for each state in your application.
+      </p>
+      <a href='reactjs.org'>reactjs.org</a>
+    </div>
+  )
+}
+
+export default CardContent
