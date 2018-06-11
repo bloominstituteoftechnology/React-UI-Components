34f1e5574070e10694759b529126ea1119c6de8f
Tyson Gushiken <tysonism@gmail.com>
2018-06-11 10:47:28 -0700

    Initial commit

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
         Begin by exploring the `components` directory. You'll notice we have a
         few files that we've already included in there to get you started right
         away building components. You'll need to make sure you include your
-        components that you build in this file to watch your app come to life
+        components that you build in this file to watch your app come to life 
       </p>
       <p>
         <strong>
