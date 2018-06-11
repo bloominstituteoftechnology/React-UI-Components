68589466c7bd08b51c2ca27002b385ffabda9448
Alec Jordan <alec.jordan.cs@gmail.com>
2018-06-11 17:46:14 -0400

    Update ImageThumbnail.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+        return <img className="header-img" src="https://ibin.co/3whrpKSBbZ81.png" alt="lambda school logo"/>
+        };
+
+export default ImageThumbnail;
