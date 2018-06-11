06f194e74c16e564e39d6fa2fffccec69b8593c1
Jameson <jamesonbrown@neolivity.com>
2018-06-11 14:48:17 -0600

    Created and styled HeaderContainer and added to app.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title">
+            <h3>Lambda School</h3>
+            <p className="sub-head">@LambdaSchool * 26 Jan</p>
+        </div>
+    )
+    };
+    
+    export default HeaderTitle;
