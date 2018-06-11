690ef6ca07e2de54debb160555ec9bb52f9c5573
Kevin Tena <kevten09@gmail.com>
2018-06-11 17:00:07 -0500

    Completed Header of component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return (
+     <div className="header-wrapper">
+        <ImageThumbnail />
+        <div className="header-text">
+        <HeaderTitle />
+        <HeaderContent />
+        </div>
+    </div>
+    );
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,5 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => <img className='lambda-thumb' src='https://ibin.co/3whrpKSBbZ81.png'></img>;
+export default ImageThumbnail;
