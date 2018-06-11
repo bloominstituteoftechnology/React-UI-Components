f34c05474e7d5d82d6f7ab56a04fbcdb9e46cd65
Alec <afroenygam@gmail.com>
2018-06-11 19:01:57 -0400

    CardContainer.js updated

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner';
+import CardContent from './cardContent';
 
 const CardContainer = () => {
 	return (
 		<div className="card-container">
 		Card Container
 		<CardBanner />
+		<CardContent />
 		</div>
 		)
 	}
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+			return (
+				<div class="card-content">
+					<h2>Get Started With React</h2>
+					<p>React makes it painless to create interactive UIs. Design simple views for each state in your application</p>
+					<small>reactjs.org</small>
+				</div>
+				)
+			}
+
+export default CardContent;
