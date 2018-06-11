b603e847dce8f20f3680449bcd371821048cc10f
Simon <Simon_Freelance@hotmail.com>
2018-06-11 16:30:17 -0400

    Completed CardBanner component

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => <img className = 'react-image' 
+                            src="https://ibin.co/3wnC6SgIOJud.png" alt=""/>;
+
+export default CardBanner;
