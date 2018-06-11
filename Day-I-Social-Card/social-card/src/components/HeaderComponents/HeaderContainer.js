ae1df668baefff9232e9cf7fbacd43ce89d41791
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-11 12:05:41 -0700

    Initial Pull Request

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from '/HeaderTitle,js';
+import HeaderContent from 'HeaderContent.js';
+
+const  HeaderContainer = () => {
+  return (
+    <div>
+      <HeaderTitle />
+      <HeaderContent />
+    </div>
+  );
+};
