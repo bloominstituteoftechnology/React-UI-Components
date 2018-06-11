89fad2218854027bfde29fb7e56c2e775c2b0103
Ciara Burkett <ciaraburkett@gmail.com>
2018-06-11 16:59:16 -0400

    Added skeleton Header Container and Image Thumbnail

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = () => {
+  return (
+    <div className="header">
+      <ImageThumbnail />
+      <HeaderTitle />
+      <HeaderContent />
+    </div>
+  );
+};
+
+export default HeaderContainer;
