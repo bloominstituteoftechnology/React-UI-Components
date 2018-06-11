b41648e38e88babadd9676f30fc4969086b0f68a
Gabriel Duquette <gabrielduquette@gmail.com>
2018-06-11 17:44:14 -0400

    add alt text

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -4,7 +4,7 @@ import './Card.css';
 const CardBanner = () => {
   return (
     <div>
-      <img src={'https://ibin.co/3wnC6SgIOJud.png'} className="banner-img"/>
+      <img src={'https://ibin.co/3wnC6SgIOJud.png'} className="banner-img" alt="React.js logo."/>
     </div>
   );
 };
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -4,7 +4,7 @@ import './Header.css';
 const ImageThumbnail = () => {
   return (
     <div>
-      <img src={'https://ibin.co/3whrpKSBbZ81.png'} className="header-logo"/>
+      <img src={'https://ibin.co/3whrpKSBbZ81.png'} className="header-logo" alt="Lambda logo."/>
     </div>
   );
 };
