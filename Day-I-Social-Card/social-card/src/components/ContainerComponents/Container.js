b82028d2d9aab226d93fae98974984f89c8e04bb
Erin Meredith <ekmeredith@att.net>
2018-06-11 18:53:34 -0400

    added footer layout

--- Day-I-Social-Card/social-card/src/components/ContainerComponents/Container.js
+++ Day-I-Social-Card/social-card/src/components/ContainerComponents/Container.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Container.css';
 import HeaderContainer from '../HeaderComponents/HeaderContainer';
 import CardContainer from '../CardComponents/CardContainer';
+import Footer from '../FooterComponents/Footer';
 
 const Container = () => {
     return (
         <div className='container'>
         <HeaderContainer />
         <CardContainer />
+        <Footer />
         </div>
     )
 };
---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+        <img className='message' src='https://ibin.co/44xlP0ABev28.png'/>
+        <img className='refresh' src='https://ibin.co/44xlmPyeKO8e.png'/>
+        <img className='heart' src='https://ibin.co/44xl8SDNdsyU.png'/>
+        <img className='email' src='https://ibin.co/44xmBhw6oGrm.png'/>
+        </div>
+    )
+};
+
+export default Footer;
