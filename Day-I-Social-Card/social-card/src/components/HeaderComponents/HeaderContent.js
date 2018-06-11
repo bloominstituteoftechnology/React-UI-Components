c87a1d8e22bee8e19b44dc15c2f4b4f810588d51
Wdifulvio523 <w.difulvio523@gmail.com>
2018-06-11 15:50:46 -0400

    William DiFulvio [React-UI-Components]
    Initial PR. Began creating components, starting with Header Contaner.JS, App.js, and Headercontent.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from "./HeaderTitle";
+import ImageThumbnail from "./ImageThumbnail";
+
+const HeaderContent = () => {
+return (
+<div className="header-content">
+<HeaderTitle />
+<ImageThumbnail />
+</div>);
+}
+
+export default HeaderContent;
+
+
