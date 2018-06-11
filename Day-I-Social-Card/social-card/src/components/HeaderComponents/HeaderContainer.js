a3be81ed78380cff5a9510c54e24339fcd230630
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-11 15:21:26 -0500

    completed component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Header.css';
 import './ImageThumbnail';
 import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 
 const HeaderContainer = () => {
     return (
         <div className="HeaderContainer">
             <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />
         </div>
     );
 };
