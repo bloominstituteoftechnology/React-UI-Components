f5d9c80d577a2fc94900a36d2a1366bb63ba7fae
Dil Sim <dilyasom@gmail.com>
2018-06-11 12:40:06 -0700

    header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = () => {
+    return (
+        <div className='header-wrapper'>
+            <div className='row'>
+                <div className="col-2">
+                    <img src='https://pbs.twimg.com/profile_images/973277209644249089/0Te2jtBH_400x400.jp' className='lambdaImg' />
+                </div>
+                <div className='col-10'>
+
+                    <HeaderTitle />
+                    <HeaderContent />
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return (
+ 
+            <div className='col-10'>
+                <h5 className='header_title'>Lambda School</h5>
+            </div>   
+     
+    )
+}
+
+export default HeaderTitle;
