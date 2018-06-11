3da9e5fd658ce868e7bd80c1fa511042e8ca0f4c
chainchompa <chainchompa@gmail.com>
2018-06-11 17:00:23 -0400

    created cardcontent and cardbanner components

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+  return (
+    <div className = "card-content">
+      <div className = "card-content-title">
+        Get Started with React
+      </div>
+      <div className = "card-content-text">
+        React makes it painless to create Interactive UIs. Design simple view for each state in your application.
+      </div>
+      <div className = "card-content-link">
+        reactjs.org
+      </div>
+    </div>
+  )
+}
+
+export default CardContent;
