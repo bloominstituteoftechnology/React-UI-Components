a096b681e3cc396d5b2f3c6ed28bb77e4c7aad82
Sukhada Gholba <sukhadagholba@gmail.com>
2018-06-11 14:11:12 -0700

    styled the header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css';
-import './HeaderTitle.js';
-import './ImageThumbnail.js';
+import HeaderTitle from './HeaderTitle.js';
+import ImageThumbnail from './ImageThumbnail.js';
 
 const HeaderContainer = () => {
 	return (
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Header.css';
 
+const HeaderLargeText = () =>  <h1 className="header-title">Lambda School</h1> 
+const HeaderSmallText =() => <p className="header-small-text">@LambdaSchool.26jan</p>
+const HeaderParagraph =() => <p className="header-paragraph">Lets learn React by building simple interface components. Try not to overthink it, just keep it simple and have fun. Once you feel comfortable using componentsyou are well on your way to mastering React!</p>
+
+
 const HeaderTitle = () => {
 	return(
-		<h1 className="header-title">Lambda School</h1>	
-
+		<div className="header-title-main">
+		<HeaderLargeText />
+		<HeaderSmallText />
+		<HeaderParagraph />
+		</div>
 	);
 };
 
