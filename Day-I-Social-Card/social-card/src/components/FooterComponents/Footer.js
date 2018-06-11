84572f0f92bb97573c3f732007857dec3f3b9bcc
chainchompa <chainchompa@gmail.com>
2018-06-11 18:19:27 -0400

    added footer and finished styling

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className = 'footer-wrapper'>
+      <div className = 'footer1'>
+        <img src = "https://ibin.co/44xlP0ABev28.png"/>
+      </div>
+      <div className = 'footer2'>
+        <img src = "https://ibin.co/44xlmPyeKO8e.png"/>
+        6
+      </div>
+      <div className = 'footer3'>
+        <img src = "https://ibin.co/44xl8SDNdsyU.png"/>
+        4
+      </div>
+      <div className = 'footer4'>
+        <img src = "https://ibin.co/44xmBhw6oGrm.png"/>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
