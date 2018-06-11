090441c26604efa383e0de03a4dca43cd52dea72
cmok4290 <cmok4290@gmail.com>
2018-06-11 17:41:11 -0400

    card container linked to reactjs.org

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -5,10 +5,12 @@ import CardContent from './CardContent';
 
 const CardContainer = () => {
     return (
-    <div className="card-container">
-        <CardBanner />
-        <CardContent />
-    </div>
+    <a href="https://www.reactjs.org">
+        <div className="card-container">
+            <CardBanner />
+            <CardContent />
+        </div>
+    </a>
     );
 };
 
