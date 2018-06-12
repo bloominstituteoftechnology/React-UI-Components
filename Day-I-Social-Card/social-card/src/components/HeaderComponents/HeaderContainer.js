60d3fa396a4e8c070923e8190aa2cb4820857ebb
cubsfan46350 <cubsfan.46350@msn.com>
2018-06-12 03:56:21 -0500

    added image in header container

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+const HeaderContainer = () => {
+    return (
+        <div className="Header-Wrapper">Header Container
+            <div className="image-logo">
+                <ImageThumbnail />
+            </div>
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+    );
+};
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => 
+    <h1 className="header-title">Lambda School</h1>;
+
+
+export default HeaderTitle;
