dd720b5fad7b7d861c6b57e7b8b06b207854cea2
Alec <afroenygam@gmail.com>
2018-06-12 15:56:45 -0400

    Day-II Start Calculator Display.js, index.html and  App.js updated

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
     <div>
       <h3>Welcome to React Calculator</h3>
       <p>
-        We have given you a starter project. You'll want to build out your
+        We hav given you a starter project. You'll want to build out your
         components in their respective files, remove this code and replace it
         with the proper components.
       </p>
+
+	<CalculatorDisplay value="0" />	
       <p>
         <strong>
           Don't forget to `default export` your components and import them here
