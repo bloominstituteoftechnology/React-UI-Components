8f7c917073a3192954218d491c47fcec7ff3a0e2
Sukhada Gholba <sukhadagholba@gmail.com>
2018-06-12 13:03:56 -0700

    created a number button and styled it

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import './App.css';
+import NumberButton from '././components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+
+ 	<NumberButton customClass="number-btn" number="7" />
+        <NumberButton customClass="number-btn" number="8" />
+        <NumberButton customClass="number-btn" number="9" />
+	<br /> 
+	<NumberButton customClass="number-btn" number="4" />
+        <NumberButton customClass="number-btn" number="5" />
+        <NumberButton customClass="number-btn" number="6" /> 
+	<br />  
+	<NumberButton customClass="number-btn" number="1" />
+	<NumberButton customClass="number-btn" number="2" />
+	<NumberButton customClass="number-btn" number="3" /> 
+
+
+
+
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props =>{
+
+return <button className={props.customClass}>{props.number}</button>
+};
+
+
+
+
+
+
+
+
+
+export default NumberButton;
