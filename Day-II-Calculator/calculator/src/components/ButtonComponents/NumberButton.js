dd096ac24193cb73ffba6d43f6688d4d2e1be6b1
snowcoding <vimalshah77@gmail.com>
2018-06-12 13:53:21 -0700

    Basic Layout in place

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+  return (
+    // <button className="button">Number</button>
+    <button className={`button ${props.buttonStyle}`}>{props.text}</button>
+  )
+}
+
+NumberButton.defaultProps = {
+  buttonStyle: "number",
+  text: "3"
+}
+export default NumberButton
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = (props) => {
+  return(
+    <div className="display">{props.value}</div>
+  )
+}
+
+export default CalculatorDisplay
