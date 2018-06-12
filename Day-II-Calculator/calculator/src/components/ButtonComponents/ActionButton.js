6704c35634cd54bc4d44e4828ff788331ead2243
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-12 18:14:20 -0400

    finished App.js, calcdisplay, action. button.js - working on css

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+const Funcs = () => {
+    return (
+      <div className='action'>
+        <Act className='act' text='&divide;' />
+        <Act className='act' text='&times;' />
+        <Act className='act' text='&minus;' />
+        <Act className='act' text='&#43;' />
+        <ActE className='act' text='&#61;' />
+      </div>
+    );
+  };
+
+export default Funcs;
