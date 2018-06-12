a90474cc5f25834cfc8479b825a9c594525cb1d2
Tyson Gushiken <tysonism@gmail.com>
2018-06-12 13:52:36 -0700

    Create basic app structure

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator">
+      <CalculatorDisplay />
+      <NumberButton buttonStyle="button--number button--0" text="0"  />
+      <NumberButton buttonStyle="button--number button--1" text="1"  />
+      <NumberButton buttonStyle="button--number button--2" text="2"  />
+      <NumberButton buttonStyle="button--number button--3" text="3"  />
+      <NumberButton buttonStyle="button--number button--4" text="4"  />
+      <NumberButton buttonStyle="button--number button--5" text="5"  />
+      <NumberButton buttonStyle="button--number button--6" text="6"  />
+      <NumberButton buttonStyle="button--number button--7" text="7"  />
+      <NumberButton buttonStyle="button--number button--8" text="8"  />
+      <NumberButton buttonStyle="button--number button--9" text="9"  />
+      <ActionButton buttonStyle="button--clear" text="clear"  />
+      <ActionButton buttonStyle="button--operator button--divide" text={'\u00f7'}  />
+      <ActionButton buttonStyle="button--operator button--multiply" text={'\u00d7'}  />
+      <ActionButton buttonStyle="button--operator button--minus" text={'\u2212'}  />
+      <ActionButton buttonStyle="button--operator button--plus" text={'\u002b'}  />
+      <ActionButton buttonStyle="button--operator button--equals" text={'\u003d'}  />
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+  return(
+    <button className="button">
+      <div>{props.text}</div>
+    </button>
+  );
+};
+
+export default NumberButton;
