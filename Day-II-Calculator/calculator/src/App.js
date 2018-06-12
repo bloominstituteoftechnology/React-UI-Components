66e80bffce60f8229853cb107aa2743ce81ee8f1
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-12 14:53:34 -0700

    finishes styling

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div className="calc-body">
       <CalculatorDisplay value="0" />
+      <ActionButton name="clear" />
+      <ActionButton name="0" />
+      <div className="operations">
+        <NumberButton kind="operations" item="÷" />
+        <NumberButton kind="operations" item="×" />
+        <NumberButton kind="operations" item="−" />
+        <NumberButton kind="operations" item="+" />
+        <NumberButton kind="operations" item="=" />
+      </div>
+      <div className="grid">
+        <NumberButton kind="number" item="7" />
+        <NumberButton kind="number" item="8" />
+        <NumberButton kind="number" item="9" />
+        <NumberButton kind="number" item="4" />
+        <NumberButton kind="number" item="5" />
+        <NumberButton kind="number" item="6" />
+        <NumberButton kind="number" item="1" />
+        <NumberButton kind="number" item="2" />
+        <NumberButton kind="number" item="3" />
+      </div>
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => (
+  <div className={`button-${props.kind}`}>
+    <p>{props.item}</p>
+  </div>
+);
+
+export default NumberButton;
