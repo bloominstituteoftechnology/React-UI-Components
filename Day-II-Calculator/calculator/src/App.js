2fe290f15b3e529075c5b44e880b0874b85cfcbe
Sukhada Gholba <sukhadagholba@gmail.com>
2018-06-12 13:31:55 -0700

    created and styled action button

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import NumberButton from '././components/ButtonComponents/NumberButton';
-
+import ActionButton from '././components/ButtonComponents/ActionButton';
 const App = () => {
   return (
     <div>
@@ -9,15 +9,17 @@ const App = () => {
  	<NumberButton customClass="number-btn" number="7" />
         <NumberButton customClass="number-btn" number="8" />
         <NumberButton customClass="number-btn" number="9" />
+	<ActionButton customClass="action-btn" action="&#xd7;" />
 	<br /> 
 	<NumberButton customClass="number-btn" number="4" />
         <NumberButton customClass="number-btn" number="5" />
         <NumberButton customClass="number-btn" number="6" /> 
+	<ActionButton customClass="action-btn" action="&#x208b;" />
 	<br />  
 	<NumberButton customClass="number-btn" number="1" />
 	<NumberButton customClass="number-btn" number="2" />
 	<NumberButton customClass="number-btn" number="3" /> 
-
+	<ActionButton customClass="action-btn" action="&#x2b;" />
 
 
 
