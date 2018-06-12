df95d221b54f1bfc7c8341f302ba4c755bf8c2da
Joe Stanfield <jlstan06@hotmail.com>
2018-06-12 16:39:18 -0400

    Joe Stanfield 2nd day of REACT

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'; 
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="main-container">
+    <CalculatorDisplay />
+    <div className="firstcontainer">
+    <ActionButton actionstyle="longerbutton" text="clear" />
+    <NumberButton buttonstyle="buttonstyle" text="&divide;"/>
+    </div>
+    <div className="secondcontainer">
+    <NumberButton buttonstyle="buttonstylewhite" text="7"/>
+    <NumberButton buttonstyle="buttonstylewhite" text="8"/>
+    <NumberButton buttonstyle="buttonstylewhite" text="9"/>
+    <NumberButton buttonstyle="buttonstyle" text="&times;"/>
+    </div>
+    <div className="thirdcontainer">
+    <NumberButton buttonstyle="buttonstylewhite" text="4"/>
+    <NumberButton buttonstyle="buttonstylewhite" text="5"/>
+    <NumberButton buttonstyle="buttonstylewhite" text="6"/>
+    <NumberButton buttonstyle="buttonstyle" text="&minus;"/>
+    </div>
+    <div className="fourcontainer">
+    <NumberButton buttonstyle="buttonstylewhite" text="1"/>
+    <NumberButton buttonstyle="buttonstylewhite" text="2"/>
+    <NumberButton buttonstyle="buttonstylewhite" text="3"/>
+    <NumberButton buttonstyle="buttonstyle" text="+"/>
+    </div>
+    <div className="fivecontainer">
+    <ActionButton actionstyle="longerbutton" text="0" />
+    <NumberButton buttonstyle="buttonstyle" text="="/>
+    </div>
+
+
+
+
+    
+  
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => 
+    <div className="display">
+        <div className="num-display">
+        0
+        </div>
+
+    </div>;
+
+
+export default CalculatorDisplay
+
+// className={props.customeStyleProp}>{props.name}
