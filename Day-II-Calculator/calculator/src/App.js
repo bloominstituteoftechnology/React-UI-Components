e9c69261d250f3a27c31ae8fcfa55c1be63c2b9c
Libby Hart <libbjo@gmail.com>
2018-06-12 16:23:38 -0600

    ip

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="app">
+      <CalculatorDisplay />
+      <container>
+      <ActionButton backgroundColor="action-button" text="clear" />
+      <NumberButton backgroundColor="red-button" text="/" />
+      </container>
+      <container>
+      <NumberButton backgroundColor="number-button" text="7" />
+      <NumberButton backgroundColor="number-button" text="8" />
+      <NumberButton backgroundColor="number-button" text="9" />
+      <NumberButton backgroundColor="red-button" text="X" />
+      </container>
+      <container>
+      <NumberButton backgroundColor="number-button" text="4" />
+      <NumberButton backgroundColor="number-button" text="5" />
+      <NumberButton backgroundColor="number-button" text="6" />
+      <NumberButton backgroundColor="red-button" text="-" />
+      </container>
+      <container>
+      <NumberButton backgroundColor="number-button" text="1" />
+      <NumberButton backgroundColor="number-button" text="2" />
+      <NumberButton backgroundColor="number-button" text="3" />
+      <NumberButton backgroundColor="red-button" text="+" />
+      </container>
+      <container>
+      <ActionButton backgroundColor="action-button" text="0" />
+      <NumberButton backgroundColor="red-button" text="=" />
+      </container>
     </div>
   );
 };
