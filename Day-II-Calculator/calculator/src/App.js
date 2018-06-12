514b09d49f5c6a2be0b6eaddb64a9af23143d42a
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-12 15:47:54 -0700

    Converted App to a class and began working on App function to update output status

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -4,46 +4,55 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.
 import ActionButton from './components/ButtonComponents/ActionButton.js';
 import NumberButton from './components/ButtonComponents/NumberButton.js';
 
-const App = () => {
-  return (
-    <div className="app">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {total : 0};
+  }
+  clickHandler = button => {
+    console.log("")
+  }
+  render() {
+    return (
+      <div className="app" onClick={this.clickHandler}>
+        <div className="row calculator">
+          <CalculatorDisplay className="calc-display" number={this.state.total}/>
 
-      <div className="row calculator">
-        <CalculatorDisplay className="calc-display" number="0"/>
+          <div className="row">
+            <ActionButton className="bigBtn" action="clear"/>
+            <ActionButton className="mathbtn" action="÷"/>
+          </div>
 
-        <div className="row">
-          <ActionButton className="bigBtn" action="clear"/>
-          <ActionButton className="mathbtn" action="÷"/>
-        </div>
+          <div className="row">
+            <NumberButton number="7"/>
+            <NumberButton number="8"/>
+            <NumberButton number="9"/>
+            <ActionButton className="mathbtn" action="X"/>
+          </div>
 
-        <div className="row">
-          <NumberButton number="7"/>
-          <NumberButton number="8"/>
-          <NumberButton number="9"/>
-          <ActionButton className="mathbtn" action="X"/>
-        </div>
+          <div className="row">
+            <NumberButton number="4"/>
+            <NumberButton number="5"/>
+            <NumberButton number="6"/>
+            <ActionButton className="mathbtn" action="−"/>
+          </div>
 
-        <div className="row">
-          <NumberButton number="4"/>
-          <NumberButton number="5"/>
-          <NumberButton number="6"/>
-          <ActionButton className="mathbtn" action="−"/>
-        </div>
+          <div className="row">
+            <NumberButton number="1"/>
+            <NumberButton number="2"/>
+            <NumberButton number="3"/>
+            <ActionButton className="mathbtn" action="+"/>
+          </div>
 
-        <div className="row">
-          <NumberButton number="1"/>
-          <NumberButton number="2"/>
-          <NumberButton number="3"/>
-          <ActionButton className="mathbtn" action="+"/>
-        </div>
-
-        <div className="row">
-          <NumberButton className="bigBtn" number="0"/>
-          <ActionButton  className="mathbtn" action="="/>
+          <div className="row">
+            <NumberButton className="bigBtn" number="0"/>
+            <ActionButton  className="mathbtn" action="="/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 };
 
 export default App;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Button.css';
 
-
 const NumberButton = props => {
   return (
-    <div className={`button numberButton ${props.className}`}>{props.number}</div>
+    <div className={`button numberButton ${props.className}`} onClick={clickHandler}>{props.number}</div>
   );
+
 };
 
 export default NumberButton;
