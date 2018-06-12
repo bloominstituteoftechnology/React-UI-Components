7438b0de48d67fa18f8b231c18325ba12593b315
Kevin Tena <kevten09@gmail.com>
2018-06-12 16:26:50 -0500

    Completed actionButtons and numberButtons functionality and design

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div>
       <CalculatorDisplay />
+      <ActionButton customStyleProp="clear-button" text="clear"/>
+      <ActionButton text="÷"/>
+      <NumberButton text="7"/>
+      <NumberButton text="8"/>
+      <NumberButton text="9"/>
+      <ActionButton text="×"/>
+      <NumberButton text="4"/>
+      <NumberButton text="5"/>
+      <NumberButton text="6"/>
+      <ActionButton text="−"/>
+      <NumberButton text="1"/>
+      <NumberButton text="2"/>
+      <NumberButton text="3"/>
+      <ActionButton text="+"/>
+      <NumberButton customStyleProp="zero-button" text="0"/>
+      <ActionButton text="="/>
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Button.css';
 
-const ActionButton = () => {
+const ActionButton = props => {
     return <button  className={`button-styles ${props.customStyleProp}`}>
-    {props.name}
+    {props.text}
     </button>
 }
 
 ActionButton.defaultProps = {
     // defaultProps is how we safeguard our components to make them truly reusable
     // if a user doesn't supply the component with the proper attrs, they will default to these props.
-    name: 'Default text',
-    customStyleProp: 'default-button-styles'
+    text: 'Default text',
+    customStyleProp: 'default-action-button-styles'
   };
 export default ActionButton;
