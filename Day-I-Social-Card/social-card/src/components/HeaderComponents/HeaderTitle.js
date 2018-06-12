ed0d1c533f85efb785a1773e594a33fbef13094d
Jameson <jamesonbrown@neolivity.com>
2018-06-12 10:23:04 -0600

    added middot and live date to HeaderTitle.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
 const HeaderTitle = () => {
     return (
         <div className="header-title">
             <h3>Lambda School</h3>
-            <p className="sub-head">@LambdaSchool * 26 Jan</p>
+            <p className="sub-head">@LambdaSchool &middot; {moment().format('D MMM')}</p>
         </div>
     )
     };
