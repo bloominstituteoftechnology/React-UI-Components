eb47717cfe0048664fd55dea8e5ee7fc03f4ac0a
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-11 17:19:22 -0700

    Added date with moment

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
-const LocalDate = () => {
+// const LocalDate = () => {
+//   let months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+//   let monthOut = months[new Date().getMonth()];
+//   let day = new Date().getDay();
+//
+//
+//   return <div>{day} {monthOut}</div>
+// }
+const ImageTitle = () => {
   let months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
   let monthOut = months[new Date().getMonth()];
   let day = new Date().getDay();
 
-  return <div>{day} {monthOut}</div>
-}
-const ImageTitle = () => {
   return (
     <div className="header-title">
       <h1 className="title-text">Lambda School </h1>
       <div className="header-handle">@LambdaSchool </div>
-      <div className="date-time"><LocalDate /></div>
+      <div className="date-time">&middot; {moment().format('D MMM')}</div>
     </div>
   );
 };
+
 
 export default ImageTitle;
