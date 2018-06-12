c435f9b08e562eca59f4a742bab9ad92f89ec554
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-06-12 15:56:40 -0400

    Blake Fletcher - Day-II-Calculator
    First commit
    Created and styled CalculatorDisplay, needs content
    Styled number buttons and symbol buttons
    Filled in middle three row with numbers 1-9 and 3 symbols using props
    Styled zero and clear buttons, need content
    Need to add division symbol and equals sign
    On to the next, on, on to the next one

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return <div className="calculator-display">
+         
+    </div>;
+ };
+ 
+ export default CalculatorDisplay;
