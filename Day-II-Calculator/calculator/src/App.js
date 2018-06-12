50d7d6322a0a9807132a578309d6849dc4497437
Tyson Gushiken <tysonism@gmail.com>
2018-06-12 15:04:30 -0700

    Refactor App to be a classical component with state (Begin stretch)

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -4,28 +4,66 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
 
-const App = () => {
-  return (
-    <div className="calculator">
-      <CalculatorDisplay />
-      <NumberButton buttonStyle="button--0" text="0"  />
-      <NumberButton buttonStyle="button--1" text="1"  />
-      <NumberButton buttonStyle="button--2" text="2"  />
-      <NumberButton buttonStyle="button--3" text="3"  />
-      <NumberButton buttonStyle="button--4" text="4"  />
-      <NumberButton buttonStyle="button--5" text="5"  />
-      <NumberButton buttonStyle="button--6" text="6"  />
-      <NumberButton buttonStyle="button--7" text="7"  />
-      <NumberButton buttonStyle="button--8" text="8"  />
-      <NumberButton buttonStyle="button--9" text="9"  />
-      <ActionButton buttonStyle="button--clear" text="clear"  />
-      <ActionButton buttonStyle="button--divide" text={'\u00f7'}  />
-      <ActionButton buttonStyle="button--multiply" text={'\u00d7'}  />
-      <ActionButton buttonStyle="button--minus" text={'\u2212'}  />
-      <ActionButton buttonStyle="button--plus" text={'\u002b'}  />
-      <ActionButton buttonStyle="button--equals" text={'\u003d'}  />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor() { 
+    super();
+    this.state = {
+      result: 0
+    };
+  }
+  render() {
+    return (
+      <div className="calculator">
+        <CalculatorDisplay total={this.state.result} />
+        <NumberButton buttonStyle="button--0" text="0"  />
+        <NumberButton buttonStyle="button--1" text="1"  />
+        <NumberButton buttonStyle="button--2" text="2"  />
+        <NumberButton buttonStyle="button--3" text="3"  />
+        <NumberButton buttonStyle="button--4" text="4"  />
+        <NumberButton buttonStyle="button--5" text="5"  />
+        <NumberButton buttonStyle="button--6" text="6"  />
+        <NumberButton buttonStyle="button--7" text="7"  />
+        <NumberButton buttonStyle="button--8" text="8"  />
+        <NumberButton buttonStyle="button--9" text="9"  />
+        <ActionButton buttonStyle="button--clear" text="clear"  />
+        <ActionButton buttonStyle="button--divide" text={'\u00f7'}  />
+        <ActionButton buttonStyle="button--multiply" text={'\u00d7'}  />
+        <ActionButton buttonStyle="button--minus" text={'\u2212'}  />
+        <ActionButton buttonStyle="button--plus" text={'\u002b'}  />
+        <ActionButton buttonStyle="button--equals" text={'\u003d'}  />
+      </div>
+    );
+  }
+} 
 
 export default App;
+
+// const App = () => {
+//   this.state = {
+//     result: 0
+//   };
+
+//   return (
+//     <div className="calculator">
+//       <CalculatorDisplay />
+//       <NumberButton buttonStyle="button--0" text="0"  />
+//       <NumberButton buttonStyle="button--1" text="1"  />
+//       <NumberButton buttonStyle="button--2" text="2"  />
+//       <NumberButton buttonStyle="button--3" text="3"  />
+//       <NumberButton buttonStyle="button--4" text="4"  />
+//       <NumberButton buttonStyle="button--5" text="5"  />
+//       <NumberButton buttonStyle="button--6" text="6"  />
+//       <NumberButton buttonStyle="button--7" text="7"  />
+//       <NumberButton buttonStyle="button--8" text="8"  />
+//       <NumberButton buttonStyle="button--9" text="9"  />
+//       <ActionButton buttonStyle="button--clear" text="clear"  />
+//       <ActionButton buttonStyle="button--divide" text={'\u00f7'}  />
+//       <ActionButton buttonStyle="button--multiply" text={'\u00d7'}  />
+//       <ActionButton buttonStyle="button--minus" text={'\u2212'}  />
+//       <ActionButton buttonStyle="button--plus" text={'\u002b'}  />
+//       <ActionButton buttonStyle="button--equals" text={'\u003d'}  />
+//     </div>
+//   );
+// };
+
+// export default App;
