8ade8ef7d3f54fa7367b0233b75bc44f9a63df21
emaquino44 <prexy44@yahoo.com>
2018-06-12 14:01:05 -0700

    set up yarn and getting calcuatordisplay and app.jss to work

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = (props) => {
+	return (
+	  <button className={`buttonStyles ${props.buttonStyles}`}>
+	  {props.text}
+	  </button>
+	)
+}
+
+export default NumberButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = (props) => {
+	return (
+	   <div className="display">
+	   	<p>{props.value}</p>
+	   </div>
+	)
+}
+
+
+
+export default CalculatorDisplay;
