812aa4aae6060118e665d0e470bb438f3a4c01eb
Joe Stanfield <jlstan06@hotmail.com>
2018-06-11 20:02:20 -0400

    finished with moment timestamp

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './Header.css';
+import moment from "moment";
 
 
-const HeaderTitle = () => <div className="headerTitle"><h4>Lambda School</h4> <h4 className="lambdagray">@LambdaSchool</h4><ul>*Jan 26</ul></div>
+const HeaderTitle = () => 
+<div className="headerTitle">
+    <h4>Lambda School</h4> 
+    <h4 className="lambdagray">
+    @LambdaSchool
+    </h4>
+        <p className="datethis">
+        {moment().format('D MMM')}
+        </p>
+</div>
 
 
 export default HeaderTitle
