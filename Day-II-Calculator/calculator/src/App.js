29cfd89a344a15cc527176042b27604c3285c0e0
Vu Cao <cdhvu@yahoo.com>
2018-06-12 12:04:20 -0700

    Completed layout for Calculator

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import './App.css';
 
+function SmallButton(props) {
+  return <button className={props.className}>{props.name}</button>
+}
+
+function LargeButton(props) {
+  return <button className={props.className}>{props.name}</button>
+}
+
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calc-wrapper">
+      <div className="display">0</div>
+      <div className="buttons-wrapper">
+        <LargeButton className="large-button" name="Clear"/>
+        <SmallButton className="calculation-button" name='&#247;'/>
+        <SmallButton className="small-button" name='7'/>
+        <SmallButton className="small-button" name='8'/>
+        <SmallButton className="small-button" name='9'/>
+        <SmallButton className="calculation-button" name='&#215;'/>
+        <SmallButton className="small-button" name='4'/>
+        <SmallButton className="small-button" name='5'/>
+        <SmallButton className="small-button" name='6'/>
+        <SmallButton className="calculation-button" name='&#8722;'/>
+        <SmallButton className="small-button" name='1'/>
+        <SmallButton className="small-button" name='2'/>
+        <SmallButton className="small-button" name='3'/>
+        <SmallButton className="calculation-button" name='&#43;'/>
+        <LargeButton className="large-button" name="0"/>
+        <SmallButton className="calculation-button" name='&#61;'/>
+      </div>
     </div>
   );
 };
