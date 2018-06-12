8d1de1354b6bf3bf4c6f9212058df5ae7e1d865f
Luis <luis@Luiss-MacBook-Air.local>
2018-06-12 13:43:26 -0700

    re-factored calculatorDisplay component to be class component

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,51 +1,9 @@
 import React from 'react'
 import './App.css'
-import NumberButton from './components/ButtonComponents/NumberButton'
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
-import ActionButton from './components/ButtonComponents/ActionButton'
 
-const App = () => {
-  return (
-    <div className='container'>
-      <CalculatorDisplay />
-      <ActionButton text='clear' buttonStyle='clear' />
-      <NumberButton
-        text='&#247;'
-        buttonStyle='red-style'
-        customStyle='operations'
-      />
-      <NumberButton text='7' buttonStyle='number-btn' />
-      <NumberButton text='8' buttonStyle='number-btn' />
-      <NumberButton text='9' buttonStyle='number-btn' />
-      <NumberButton
-        text='&#215;'
-        buttonStyle='red-style'
-        customStyle='operations'
-      />
-      <NumberButton text='4' buttonStyle='number-btn' />
-      <NumberButton text='5' buttonStyle='number-btn' />
-      <NumberButton text='6' buttonStyle='number-btn' />
-      <NumberButton
-        text='&#8722;'
-        buttonStyle='red-style'
-        customStyle='operations'
-      />
-      <NumberButton text='1' buttonStyle='number-btn' />
-      <NumberButton text='2' buttonStyle='number-btn' />
-      <NumberButton text='3' buttonStyle='number-btn' />
-      <NumberButton
-        text='&#43;'
-        buttonStyle='red-style'
-        customStyle='operations'
-      />
-      <ActionButton text='0' buttonStyle='clear' />
-      <NumberButton
-        text='&#61;'
-        buttonStyle='red-style'
-        customStyle='operations'
-      />
-    </div>
-  )
+const App = (props) => {
+  return <CalculatorDisplay />
 }
 
 export default App
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react'
 import './Display.css'
-
+import NumberButton from '../ButtonComponents/NumberButton'
+import ActionButton from '../ButtonComponents/ActionButton'
 class CalculatorDisplay extends Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
     this.state = {
       display: 0
     }
   }
+  handleSubmit = (props) => {
+    this.setState({ display: this.state.display + props.text })
+  }
+
   render () {
     return (
-      <div className='CalculatorDisplay'>
-        <div className='display'> {this.state.display}</div>
+      <div className='container'>
+        <div className='CalculatorDisplay'>
+          <div className='display'> {this.state.display}</div>
+        </div>
+        <ActionButton text='clear' buttonStyle='clear' />
+        <NumberButton
+          text='&#247;'
+          buttonStyle='red-style'
+          customStyle='operations'
+        />
+        <NumberButton text='7' buttonStyle='number-btn' />
+        <NumberButton text='8' buttonStyle='number-btn' />
+        <NumberButton text='9' buttonStyle='number-btn' />
+        <NumberButton
+          text='&#215;'
+          buttonStyle='red-style'
+          customStyle='operations'
+        />
+        <NumberButton text='4' buttonStyle='number-btn' />
+        <NumberButton text='5' buttonStyle='number-btn' />
+        <NumberButton text='6' buttonStyle='number-btn' />
+        <NumberButton
+          text='&#8722;'
+          buttonStyle='red-style'
+          customStyle='operations'
+        />
+        <NumberButton text='1' buttonStyle='number-btn' />
+        <NumberButton text='2' buttonStyle='number-btn' />
+        <NumberButton text='3' buttonStyle='number-btn' />
+        <NumberButton
+          text='&#43;'
+          buttonStyle='red-style'
+          customStyle='operations'
+        />
+        <ActionButton text='0' buttonStyle='clear' />
+        <NumberButton
+          text='&#61;'
+          buttonStyle='red-style'
+          customStyle='operations'
+        />
       </div>
     )
   }
