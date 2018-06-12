1455e97ec2763223a3c4be0ae5d2463043ae4039
Ciara Burkett <ciaraburkett@gmail.com>
2018-06-12 18:47:42 -0400

    Finished MVP

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButtons.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButtons.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import './Buttons.css';
 
-const NumberButtons = () => {
+const NumberButtons = (props) => {
   return (
     <div className="numbers">
+      <div className={props.defaultRowClass}>
+        <button className={`${props.defaultNumberClass} clear`}>clear</button>
+      </div>
 
+      <div className={props.defaultRowClass}>
+        <button className={props.defaultNumberClass}>{props.text[0][0]}</button>
+        <button className={props.defaultNumberClass}>{props.text[0][1]}</button>
+        <button className={props.defaultNumberClass}>{props.text[0][2]}</button>
+      </div>
+
+      <div className={props.defaultRowClass}>
+        <button className={props.defaultNumberClass}>{props.text[1][0]}</button>
+        <button className={props.defaultNumberClass}>{props.text[1][1]}</button>
+        <button className={props.defaultNumberClass}>{props.text[1][2]}</button>
+      </div>
+
+      <div className={props.defaultRowClass}>
+        <button className={props.defaultNumberClass}>{props.text[2][0]}</button>
+        <button className={props.defaultNumberClass}>{props.text[2][1]}</button>
+        <button className={props.defaultNumberClass}>{props.text[2][2]}</button>
+      </div>
+
+      <div className={props.defaultRowClass}>
+        <button className={`${props.defaultNumberClass} zero`}>0</button>
+      </div>
     </div>
   );
+};
+
+NumberButtons.defaultProps = {
+  defaultNumberClass: 'number',
+  defaultRowClass: 'number-row',
+  text: [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3']
+  ]
 };
 
 export default NumberButtons;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/OperatorButtons.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/OperatorButtons.js
@@ -4,7 +4,11 @@ import './Buttons.css';
 const OperatorButtons = () => {
   return (
     <div className="operators">
-
+      <button className="operator">&divide;</button>
+      <button className="operator">&times;</button>
+      <button className="operator">&minus;</button>
+      <button className="operator">&#x2b;</button>
+      <button className="operator">&#x3d;</button>
     </div>
   );
 };
