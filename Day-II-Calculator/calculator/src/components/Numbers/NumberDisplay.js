172b378f3c4b8584658e8719c588479dfb4a17cb
Kairi <kairijb@gmail.com>
2018-06-12 17:42:26 -0500

    finished mvp

--- Day-II-Calculator/calculator/src/components/Numbers/NumberDisplay.js
+++ Day-II-Calculator/calculator/src/components/Numbers/NumberDisplay.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Number.css';
 
+class Calc extends React.Component {
+    constructor(props) {
+        super(props);
+        let change = document.querySelectorAll('input');
+        let changed = document.querySelectorAll('input');
+        change.addEventListener('click', () => {
+            return changed.props.name = change.props.name;
+        });
+    }
+}
+
 const MyNumber = props => {
     return <p className={props.number}>{props.name}</p>
 }
 
 const NumberDisplay = () => {
     return <div className='row number'>
-    <MyNumber number='number' name='0' />
+        <MyNumber number='number' name='0' />
     </div>
 }
 
