82c378b897b3e2cfc18a4c1da4961920a0060f5f
JacobLayton <jlayjlay3@gmail.com>
2018-06-12 14:29:54 -0700

    calculator basic layout complete

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div className="app">
     <CalculatorDisplay />
+    <ActionButton customStyleProp="clear-style" name="clear" />
+    <NumberButton customStyleProp="operator" name="d" />
+    <NumberButton customStyleProp="number" name="7" />
+    <NumberButton customStyleProp="number" name="8" />
+    <NumberButton customStyleProp="number" name="9" />
+    <NumberButton customStyleProp="operator" name="x" />
+    <NumberButton customStyleProp="number" name="4" />
+    <NumberButton customStyleProp="number" name="5" />
+    <NumberButton customStyleProp="number" name="6" />
+    <NumberButton customStyleProp="operator" name="-" />
+    <NumberButton customStyleProp="number" name="1" />
+    <NumberButton customStyleProp="number" name="2" />
+    <NumberButton customStyleProp="number" name="3" />
+    <NumberButton customStyleProp="operator" name="+" />
+    <ActionButton customStyleProp="zero-style" name="zero" />
+    <NumberButton customStyleProp="operator" name="=" />
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return <button className="action-button">{props.name}</button>
+};
+
+export default ActionButton
