e2be631b399fa7b6ba40e38ea2cdc6ca725babba
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-12 16:33:13 -0500

    completed component

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = (props) => { return <p className={`${props.style}`}>{props.text}</p> };
+
+NumberButton.defaultProps = {
+    'style': 'default-num-bttn-style',
+}
+
+export default NumberButton;
