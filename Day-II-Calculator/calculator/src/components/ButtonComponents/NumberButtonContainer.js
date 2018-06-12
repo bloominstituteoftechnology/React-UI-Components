7252f4a566c5e625b9bd6233e30b8cd0e4f6342a
Adrian Adames <adrianadames@gmail.com>
2018-06-12 15:26:16 -0400

    first commit for day II of React-UI-Components (i.e. the calculator assignemnt

---
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButtonContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Button.css';
+
+
+
+
+import React from 'react';
+import './Header.css';
+import HeaderContent from './HeaderContent';
+
+
+
+const HeaderContainer = () => {
+    return (
+        <div className = "header-container">
+            <HeaderContent /> 
+        </div>
+    );
+};
+
+export default HeaderContainer;
