a285fe504b941a84c08577fb00828d726e55c604
Alec <afroenygam@gmail.com>
2018-06-12 17:20:29 -0400

    Sytles updated to match closer to design file

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -9,20 +9,20 @@ const App = () => {
     <div>
 	<CalculatorDisplay value="0" />	
 	<ActionButton value="clear" customClass="default-button clear-button" />
-	<ActionButton value="%" customClass="default-button operator-button" />
+	<ActionButton value="&divide;" customClass="default-button operator-button" />
 	<NumberButton value="7" customClass="default-button number-button" />
 	<NumberButton value="8" customClass="default-button number-button" />
 	<NumberButton value="9" customClass="default-button number-button" />
-	<ActionButton value="x" customClass="default-button operator-button" />
+	<ActionButton value="&times;" customClass="default-button operator-button" />
 	<NumberButton value="4" customClass="default-button number-button" />
 	<NumberButton value="5" customClass="default-button number-button" />
 	<NumberButton value="6" customClass="default-button number-button" />
-	<ActionButton value="-" customClass="default-button operator-button" />
+	<ActionButton value="&minus;" customClass="default-button operator-button" />
 	<NumberButton value="1" customClass="default-button number-button" />
 	<NumberButton value="2" customClass="default-button number-button" />
 	<NumberButton value="3" customClass="default-button number-button" />
 	<ActionButton value="+" customClass="default-button operator-button" />
-	<NumberButton value="0" customClass="default-button clear-button" />
+	<NumberButton value="0" customClass="default-button zero-button" />
 	<ActionButton value="=" customClass="default-button operator-button" />
 	
     </div>
