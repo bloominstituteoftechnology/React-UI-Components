84a06e515cdf99fd8558acd71e7f6e0028832fcb
Wdifulvio523 <w.difulvio523@gmail.com>
2018-06-12 15:50:57 -0400

    Day 2 Initial Commit
    completed display, began creating buttons

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
+    <div className="calculator-container">
+  <CalculatorDisplay />
+  <NumberButton customStyleProp="wide-button" text="clear" />
+  <NumberButton customStyleProp="normal-button math-button" text="X" />
+
       <p>
         <strong>
           Don't forget to `default export` your components and import them here
