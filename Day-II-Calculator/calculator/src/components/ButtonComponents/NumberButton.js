c2b62c0f39b301dae1e7cc5ec212760be7b37163
Allen Hai <allenhai03@gmail.com>
2018-06-12 17:03:16 -0500

    Create: NumberButton component with preliminary styles

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton ({number}) {
+    return (
+        <div className="number-button">{number}</div>       
+    )
+}
+
+export default NumberButton
