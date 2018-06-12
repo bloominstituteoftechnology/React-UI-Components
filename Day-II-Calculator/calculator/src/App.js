a9d33467917e81a1a4d9e16d3441fc7c1d61595b
Vu Cao <cdhvu@yahoo.com>
2018-06-12 16:22:02 -0700

    Finished debugging. Calculator is functioning as aspected.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -9,26 +9,34 @@ class App extends React.Component {
     super();
     this.state = {
       total:'0',
-      savedValue: '0',
+      savedValue: 0,
       operation: ''
     };
   }
 
   handleClickButton = (value) => {
     this.state.total += value;
-    this.state.total = Number(this.state.total);
+    this.state.total = this.state.total;
     this.setState({total: this.state.total});
   }
 
   handleClearButton = (value) => {
-    this.state.savedValue = '0';
+    this.state.savedValue = 0;
     this.state.total = '0';
     this.state.operation = '';
     this.setState(this.state);
   }
 
+  handleOperation = (value) => {
+    this.state.savedValue = Number(this.state.total);
+    this.state.total = '0';
+    this.state.operation = value;
+    this.setState(this.state);
+    console.log(this.state);
+  }
+
   Multiply = (value) => {
-    this.state.savedValue = this.state.total;
+    this.state.savedValue = Number(this.state.total);
     this.state.total = '0';
     this.state.operation = 'multiply';
     this.setState(this.state);
@@ -36,7 +44,7 @@ class App extends React.Component {
   }
 
   Divide = (value) => {
-    this.state.savedValue = this.state.total;
+    this.state.savedValue = Number(this.state.total);
     this.state.total = '0';
     this.state.operation = 'divide';
     this.setState(this.state);
@@ -44,7 +52,7 @@ class App extends React.Component {
   }
 
   Subtract = (value) => {
-    this.state.savedValue = this.state.total;
+    this.state.savedValue = Number(this.state.total);
     this.state.total = '0';
     this.state.operation = 'subtract';
     this.setState(this.state);
@@ -52,7 +60,7 @@ class App extends React.Component {
   }
 
   Add = (value) => {
-    this.state.savedValue = this.state.total;
+    this.state.savedValue = Number(this.state.total);
     this.state.total = '0';
     this.state.operation = 'add';
     this.setState(this.state);
@@ -61,13 +69,13 @@ class App extends React.Component {
 
   Result = (value) => {
     if (this.state.operation === 'multiply'){
-      this.state.total *= this.state.savedValue;
+      this.state.total = Number(this.state.total) * this.state.savedValue;
     } else if (this.state.operation === 'divide'){
-      this.state.total /= this.state.savedValue;
+      this.state.total = this.state.savedValue / Number(this.state.total);
     } else if (this.state.operation === 'subtract'){
-      this.state.total -= this.state.savedValue;
+      this.state.total = this.state.savedValue - Number(this.state.total);
     } else if (this.state.operation === 'add'){
-      this.state.total += this.state.savedValue;
+      this.state.total = this.state.savedValue + Number(this.state.total);
     } 
     this.state.savedValue = this.state.total;
     this.setState(this.state);
@@ -76,22 +84,22 @@ class App extends React.Component {
   render() {
     return (
       <div className="calc-wrapper">
-        <CalculatorDisplay totalValue={this.state.total} />
+        <CalculatorDisplay totalValue={Number(this.state.total)} />
         <div className="buttons-wrapper">
           <ActionButton buttonStyle="action-button" text="Clear" value="0" onClick={this.handleClearButton}/>
-          <NumberButton buttonStyle="calculation-button" text='&#247;' value='' onClick={this.Divide}/>
+          <NumberButton buttonStyle="calculation-button" text='&#247;' value='divide' onClick={this.handleOperation}/>
           <NumberButton buttonStyle="number-button" text='7' value="7" onClick={this.handleClickButton}/>
           <NumberButton buttonStyle="number-button" text='8' value="8" onClick={this.handleClickButton}/>
           <NumberButton buttonStyle="number-button" text='9' value="9" onClick={this.handleClickButton}/>
-          <NumberButton buttonStyle="calculation-button" text='&#215;' value='' onClick={this.Multiply}/>
+          <NumberButton buttonStyle="calculation-button" text='&#215;' value='multiply' onClick={this.handleOperation}/>
           <NumberButton buttonStyle="number-button" text='4' value="4" onClick={this.handleClickButton}/>
           <NumberButton buttonStyle="number-button" text='5' value="5" onClick={this.handleClickButton}/>
           <NumberButton buttonStyle="number-button" text='6' value="6" onClick={this.handleClickButton}/>
-          <NumberButton buttonStyle="calculation-button" text='&#8722;' value='' onClick={this.Subtract}/>
+          <NumberButton buttonStyle="calculation-button" text='&#8722;' value='subtract' onClick={this.handleOperation}/>
           <NumberButton buttonStyle="number-button" text='1' value="1" onClick={this.handleClickButton}/>
           <NumberButton buttonStyle="number-button" text='2' value="2" onClick={this.handleClickButton}/>
           <NumberButton buttonStyle="number-button" text='3' value="3" onClick={this.handleClickButton}/>
-          <NumberButton buttonStyle="calculation-button" text='&#43;' value='' onClick={this.Add}/>
+          <NumberButton buttonStyle="calculation-button" text='&#43;' value='add' onClick={this.handleOperation}/>
           <ActionButton buttonStyle="action-button" text='0' value="0" onClick={this.handleClickButton}/>
           <NumberButton buttonStyle="calculation-button" text='&#61;' value='' onClick={this.Result}/>
         </div>
