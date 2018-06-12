f19b15858e2ea15303bae1f70d35da22f8d92552
Simon <Simon_Freelance@hotmail.com>
2018-06-12 16:28:31 -0400

    Made the rough draft of the Calculator setup and layout

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import InsertButton from './components/ButtonComponents/InsertButton';
+import ClearButton from './components/ButtonComponents/ClearButton';
+import EqualsButton from './components/ButtonComponents/EqualsButton';
 const App = () => {
   return (
+
+    // Calculator Container uses CSS grids
     <div className = "calculator-container">
+      {/* Row 1 */}
       <CalculatorDisplay />
+
+      {/* Row 2 */}
+      <ClearButton />
+      <InsertButton symbol = "÷" class = "btn--red"/>
+
+      {/* Row 3 */}
+      <InsertButton symbol = "7"/>
+      <InsertButton symbol = "8"/>
+      <InsertButton symbol = "9"/>
+      <InsertButton symbol = "X" class = "btn--red"/>
+
+      {/* Row 4 */}
+      <InsertButton symbol = "4"/>
+      <InsertButton symbol = "5"/>
+      <InsertButton symbol = "6"/>
+      <InsertButton symbol = "-" class = "btn--red"/>
+
+      {/* Row 5 */}
+      <InsertButton symbol = "1"/>
+      <InsertButton symbol = "2"/>
+      <InsertButton symbol = "3"/>
+      <InsertButton symbol = "+" class = "btn--red"/>
+
+      {/* Row 6 */}
+      <InsertButton symbol = "0" class = "btn--white btn__zero" />
+      <EqualsButton class = "btn--red"/>
     </div>
   );
 };
