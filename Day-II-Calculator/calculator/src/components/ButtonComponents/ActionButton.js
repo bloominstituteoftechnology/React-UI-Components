57dc8e032936c752fc239b24300df7c50650cbdc
Erin Meredith <ekmeredith@att.net>
2018-06-12 16:45:21 -0400

    started calculator layout

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (props) => {
+    console.log('PROPS', props);
+    return <button className='operator'>{props.name}</button>;
+
+}
+
+export default ActionButton;
