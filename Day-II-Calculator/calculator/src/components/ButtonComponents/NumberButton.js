9459d5e7568694472bb91240e583664dd637858b
Sam Khaled <samkhaled@outlook.com>
2018-06-12 15:27:49 -0400

    Day2-calculator
    got display styling completed

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+
+
+const NumberButton = () => {
+  return (
+    <div className = 'number'>
+    </div>
+  );
+};
+
+export default NumberButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+  return (
+    <div className="result">
+      <div className="answer">0</div>
+    </div>
+)};
+
+
+export default CalculatorDisplay;
