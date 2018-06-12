230fb6f3f59d7b95013ce1be2437424fd5e55b96
Vu Cao <cdhvu@yahoo.com>
2018-06-12 13:00:11 -0700

    Converted App to a classical component

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -4,30 +4,37 @@ import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
-const App = () => {
-  return (
-    <div className="calc-wrapper">
-      <CalculatorDisplay totalValue={0} />
-      <div className="buttons-wrapper">
-        <ActionButton buttonStyle="action-button" text="Clear"/>
-        <NumberButton buttonStyle="calculation-button" text='&#247;'/>
-        <NumberButton buttonStyle="number-button" text='7'/>
-        <NumberButton buttonStyle="number-button" text='8'/>
-        <NumberButton buttonStyle="number-button" text='9'/>
-        <NumberButton buttonStyle="calculation-button" text='&#215;'/>
-        <NumberButton buttonStyle="number-button" text='4'/>
-        <NumberButton buttonStyle="number-button" text='5'/>
-        <NumberButton buttonStyle="number-button" text='6'/>
-        <NumberButton buttonStyle="calculation-button" text='&#8722;'/>
-        <NumberButton buttonStyle="number-button" text='1'/>
-        <NumberButton buttonStyle="number-button" text='2'/>
-        <NumberButton buttonStyle="number-button" text='3'/>
-        <NumberButton buttonStyle="calculation-button" text='&#43;'/>
-        <ActionButton buttonStyle="action-button" text="0"/>
-        <NumberButton buttonStyle="calculation-button" text='&#61;'/>
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state ={total:0};
+  }
+
+  render() {
+    return (
+      <div className="calc-wrapper">
+        <CalculatorDisplay totalValue={this.state.total} />
+        <div className="buttons-wrapper">
+          <ActionButton buttonStyle="action-button" text="Clear"/>
+          <NumberButton buttonStyle="calculation-button" text='&#247;'/>
+          <NumberButton buttonStyle="number-button" text='7'/>
+          <NumberButton buttonStyle="number-button" text='8'/>
+          <NumberButton buttonStyle="number-button" text='9'/>
+          <NumberButton buttonStyle="calculation-button" text='&#215;'/>
+          <NumberButton buttonStyle="number-button" text='4'/>
+          <NumberButton buttonStyle="number-button" text='5'/>
+          <NumberButton buttonStyle="number-button" text='6'/>
+          <NumberButton buttonStyle="calculation-button" text='&#8722;'/>
+          <NumberButton buttonStyle="number-button" text='1'/>
+          <NumberButton buttonStyle="number-button" text='2'/>
+          <NumberButton buttonStyle="number-button" text='3'/>
+          <NumberButton buttonStyle="calculation-button" text='&#43;'/>
+          <ActionButton buttonStyle="action-button" text="0"/>
+          <NumberButton buttonStyle="calculation-button" text='&#61;'/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default App;
