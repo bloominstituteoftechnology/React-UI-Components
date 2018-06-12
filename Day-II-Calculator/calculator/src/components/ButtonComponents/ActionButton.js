f2a5aac63b5447b3df686f7ca6c11159bdc65312
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-12 16:32:53 -0500

    completed component

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (props) => { return <p className={`${props.style}`}>{props.text}</p>};
+
+ActionButton.defaultProps = {
+    'style': 'default-act-bttn-style',
+}
+
+export default ActionButton;
