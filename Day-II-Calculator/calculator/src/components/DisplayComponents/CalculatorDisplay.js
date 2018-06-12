4dd045800a884f55cf9c365b053891da0513ef43
meganfontenot <meganfontenot517@yahoo.com>
2018-06-12 15:30:21 -0500

    setting up import-exports

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return(
+        <div className="watermelon">
+        <p>
+        0
+        </p>
+        </div>
+    );
+};
+export default CalculatorDisplay;
