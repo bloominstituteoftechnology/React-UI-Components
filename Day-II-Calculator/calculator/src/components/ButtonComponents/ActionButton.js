84fd078474fa5702a1ef1db07c645626d19b2928
Kevin Tena <kevten09@gmail.com>
2018-06-12 15:43:53 -0500

    Added Display for Calculator

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = () => {
+    return <button  className={`button-styles ${props.customStyleProp}`}>
+    {props.name}
+    </button>
+}
+
+ActionButton.defaultProps = {
+    // defaultProps is how we safeguard our components to make them truly reusable
+    // if a user doesn't supply the component with the proper attrs, they will default to these props.
+    name: 'Default text',
+    customStyleProp: 'default-button-styles'
+  };
+export default ActionButton;
