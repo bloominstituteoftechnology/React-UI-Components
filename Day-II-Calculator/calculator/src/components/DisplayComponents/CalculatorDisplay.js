2e9a36ccce8cda3e2b9235443834d6b3007c84f0
Daniel Chamorro <dan@dcitllc.com>
2018-06-12 17:57:17 -0400

    Calulator

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalculatorDisplay = props => {
+  return <div className={props.btnClass}>{props.num}</div>;
+};
+
+export default CalculatorDisplay;
