b00bdd660efb6b052730dc24dcc150600239d716
Randy Calderon <randycweb@gmail.com>
2018-06-12 14:37:37 -0700

    Added CalculatorDisplay to App.js

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
+
 
 const App = () => {
   return (
     <div className="main-container">
+      <div>
+      <CalculatorDisplay display="display" displayText="0"/>
+      </div>
       <ActionButton actionStyle="button-clear" text="clear"/>
       <NumberButton buttonStyle="number-button" text="7" />
       <NumberButton buttonStyle="number-button" text="8" />
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return <div className={props.display}>{props.displayText}</div>
+}
+
+export default CalculatorDisplay;
