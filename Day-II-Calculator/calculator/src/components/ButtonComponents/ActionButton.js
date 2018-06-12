b468b264215bd9437acccfe0c90952dcd3e81f28
Dil Sim <dilyasom@gmail.com>
2018-06-12 11:22:51 -0700

    initial commit

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = () => {
+    return (
+        <div>ActionButton</div>
+    )
+}
+ 
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Button.css';
+
+const MyButton = props => {
+    return (
+        // `${console.log('props: ', props)}`
+        <button className={`displayFlex ${props.customeStyle}`}>{props.text}</button>
+    )
+        
+    
+}
+
+const NumberButton = () => {
+    return (
+        <div>
+            <MyButton customeStyle='whiteBack' text='7' />
+
+        </div>
+    )
+}
+ 
+export default NumberButton;
