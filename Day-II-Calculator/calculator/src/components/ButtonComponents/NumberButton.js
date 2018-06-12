5f7b6486dca11654a1bf02616941481b245f6c07
JJ Ashcraft <jj@jjashcraft.com>
2018-06-12 18:40:43 -0400

    functionality in progress

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Button.css';
 
 const NumberButton = props => {
+    // const clickHandler = ()=> {
+    // Display.text = this.text;
+    // };
   return (
       <button className = {`numbers ${props.customStyleProp}`}>{props.text} 
       </button> 
