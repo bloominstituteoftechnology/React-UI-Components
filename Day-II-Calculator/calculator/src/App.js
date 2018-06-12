b8e0be93f44e6b308297cabc6c7d5f240cf231f7
chainchompa <chainchompa@gmail.com>
2018-06-12 15:34:46 -0400

    added action buttons, finished mvp and styling

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -35,7 +35,7 @@ const App = () => {
       <div className = 'action-buttons'>
         <ActionButton name = '&#247;'/>
         <ActionButton name = 'x'/>
-        <ActionButton name = '-'/>
+        <ActionButton name = '&#8722;'/>
         <ActionButton name = '+'/>
         <ActionButton name = '='/>
       </div>
