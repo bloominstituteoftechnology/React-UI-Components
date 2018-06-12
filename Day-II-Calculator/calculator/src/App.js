98a5276f0d2e87ad704d25627b7585ca31b49a58
snowcoding <vimalshah77@gmail.com>
2018-06-12 14:10:05 -0700

    Modified font sizes and weight

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -9,26 +9,26 @@ const App = () => {
     <div className='container-app'>
       <CalculatorDisplay value="0"/>
       <div class="buttons">
-        <ActionButton buttonStyle="wide-action"/>
-        <ActionButton />
+        <ActionButton text="clear" buttonStyle="wide-action"/>
+        <ActionButton text="/"/>
 
-        <NumberButton />
-        <NumberButton />
-        <NumberButton />
-        <ActionButton />
+        <NumberButton text="7"/>
+        <NumberButton text="8"/>
+        <NumberButton text="9"/>
+        <ActionButton text="X"/>
 
-        <NumberButton />
-        <NumberButton />
-        <NumberButton />
-        <ActionButton />
+        <NumberButton text="4"/>
+        <NumberButton text="5"/>
+        <NumberButton text="6"/>
+        <ActionButton text="-"/>
 
-        <NumberButton />
-        <NumberButton />
-        <NumberButton />
-        <ActionButton />
+        <NumberButton text="1"/>
+        <NumberButton text="2"/>
+        <NumberButton text="3"/>
+        <ActionButton text="+"/>
 
-        <NumberButton buttonStyle="wide-number"/>
-        <ActionButton />
+        <NumberButton text="0" buttonStyle="wide-number"/>
+        <ActionButton text="="/>
 
       </div>
     </div>
