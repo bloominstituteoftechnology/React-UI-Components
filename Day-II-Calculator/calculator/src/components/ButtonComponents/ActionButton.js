f69fb701bee3d00ac631744cb20ef9b47b058abf
-tristan- <tristan.linehan@gmail.com>
2018-06-12 16:10:57 -0700

    daily standup

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,51 @@
 import React from 'react';
 import './Button.css';
+
+// const ActionButton = () => {
+//     return (
+//         <div className="action-button">
+//         A
+//         </div>
+        
+//     )
+// } ;
+// export default ActionButton ;
+
+// const ActionButton = props => {
+//     return (
+//         <div className={`action-button ${props.customStyleProp}`}>
+//             {props.name}            
+//         </div>
+//     ) ;
+// } ;
+// //DEFAULT PROPS
+// ActionButton.defaultProps = {
+//     name: '!!!',
+//     customStyleProp: 'action-button'
+// } ;
+// export default ActionButton ;
+
+const ActionButton = props => {
+
+    const clickHandler = () => {
+        alert(props.name);
+    };
+
+    return (
+        <div 
+        
+        onClick={clickHandler}
+        
+        className={`action-button ${props.customStyleProp}`}>
+            {props.name}            
+        </div>
+    ) ;
+} ;
+//DEFAULT PROPS
+ActionButton.defaultProps = {
+    name: '!!!',
+    customStyleProp: 'action-button'
+} ;
+export default ActionButton ;
+
+
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,43 @@
 import React from 'react';
 import './Button.css';
+
+// const NumberButton = () => {
+//     return (
+//         <div className="number-button">            
+//         </div>
+//     )
+// }
+
+// const NumberButton = props => {
+//     return (
+//         <div className={`number-button ${props.customStyleProp}`}>
+//             {props.name}            
+//         </div>
+//     ) ;
+// } ;
+// //DEFAULT PROPS
+// NumberButton.defaultProps = {
+//     name: '#',
+//     customStyleProp: 'number-button'
+// } ;
+// export default NumberButton ;
+
+const NumberButton = props => {
+    const clickHandler = () => {
+        alert(props.name);
+    };
+
+    return (
+        <div 
+        onClick={clickHandler}
+        className={`number-button ${props.customStyleProp}`}>
+            {props.name}            
+        </div>
+    ) ;
+} ;
+//DEFAULT PROPS
+NumberButton.defaultProps = {
+    name: '#',
+    customStyleProp: 'number-button'
+} ;
+export default NumberButton ;
