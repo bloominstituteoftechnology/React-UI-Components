0653c968eb63cbb86d489641876b159e5c1e93a7
Simon <Simon_Freelance@hotmail.com>
2018-06-17 16:42:55 -0400

    Added clearHandler

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 const ClearButton = (props) => {
-    return <button className = "btn__clear btn--white">{props.name}</button>
+    return <button className = "btn__clear btn--white" onClick = {props.clearHandler}>{props.name}</button>
 }
 
 ClearButton.defaultProps = {
