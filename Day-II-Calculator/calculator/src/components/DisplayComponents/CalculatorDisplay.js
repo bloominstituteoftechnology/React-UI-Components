f0abde6d483aea70560b7e907fb206b7561e3096
Simon <Simon_Freelance@hotmail.com>
2018-06-17 16:45:14 -0400

    Added props to display

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Display.css';
 
-const CalculatorDisplay = () => {
-    return <div className = 'display'>0</div>
+const CalculatorDisplay = (props) => {
+    return <div className = 'display'>{props.displayValue}</div>
 }
 
 export default CalculatorDisplay;
