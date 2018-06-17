22994e420bdbb8346655bc728c7369043230a55d
Simon <Simon_Freelance@hotmail.com>
2018-06-17 16:45:05 -0400

    Added handler to InsertButton

--- Day-II-Calculator/calculator/src/components/ButtonComponents/InsertButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/InsertButton.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Button.css';
-
+import './../DisplayComponents/CalculatorDisplay';
 const InsertButton = (props) => {
-    return <button className = {props.class}>{props.symbol}</button>
+    return <button className = {props.class} onClick = {() => props.symbolHandler(props.symbol)}>{props.symbol}</button>
 }
 InsertButton.defaultProps = {
     class: 'btn--white',
