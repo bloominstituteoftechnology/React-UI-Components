1f224e27b3a9938c8a0d10bb6bf3c58a73727c4a
Terrance <tbarnett2020@gmail.com>
2018-06-18 16:29:33 -0400

    Terrance Danckert CS11 (React-UI-Components)

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return <div className ="small-text" >Lambda School <small>@LambdaSchool • 26 Jan</small> </div>;
+};
+
+
+
+export default HeaderTitle;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return <img src="https://ibin.co/3whrpKSBbZ81.png" height="42" width="42" />;
+};
+
+
+
+export default ImageThumbnail;
