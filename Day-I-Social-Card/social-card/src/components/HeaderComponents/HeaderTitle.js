76f6fc592269e8c9664cb24b371709394da53f9a
Kelley Henican <kelleyhenican2@gmail.com>
2018-06-18 16:53:23 -0500

    set up text for header, no styles

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return (
+    <div>
+    <h1 className="title">LambdaSchool</h1>
+    <h2 className="handle">@LambdaSchool</h2>
+    <h2 className="date"> . 18 June</h2>
+    </div>
+    )
+}
+   
+
+export default HeaderTitle;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+
+const ImageThumbnail = () => <img src="https://ibin.co/3whrpKSBbZ81.png" className="image-thumbnail"/>
+
+
+export default ImageThumbnail;
