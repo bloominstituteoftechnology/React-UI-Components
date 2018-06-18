27183f28aaba95493196e7dd552f6aefbe43a033
Eric B <timberwolf.eric@gmail.com>
2018-06-18 14:54:47 +0900

    Initial commit w/ demo setup

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return (
+        <div className="header-wrapper">Header Container 
+            <HeaderContent />
+        </div>
+    )
+};
+
+export default HeaderContainer;
