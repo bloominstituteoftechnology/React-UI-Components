081114b0197e9ddd70288e44cd7525ae3c642569
M.Real <mreal126@protonmail.com>
2018-06-18 17:19:32 -0400

    Container linked up, about to look for footer images.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -4,7 +4,9 @@ import './Card.css';
 const CardBanner = () => {
     return (
     <div className="card-banner">
+    <a href="https://www.reactjs.org">
     <img className="banner" src="https://ibin.co/3wnC6SgIOJud.png" alt="banner"/>
+    </a>
     </div>
     );
 };
