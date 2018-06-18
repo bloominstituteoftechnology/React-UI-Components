af55e6216e7a8d8eaa91b0631b34a13f78443371
Katie Gorbell <katiegorbell@gmail.com>
2018-06-18 16:38:37 -0500

    structured card-container and added card content

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return <img class='banner-image' src='https://ibin.co/3wnC6SgIOJud.png' alt='React Logo'/>
+}
+ 
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 const CardContainer = () => {
-    return <div className='card-container'>Card Container</div>
+    return (
+        <div className='card-container'>
+            <CardBanner />
+            <CardContent />
+        </div>
+    );
 };
 
 export default CardContainer;
