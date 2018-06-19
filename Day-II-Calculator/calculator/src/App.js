f076eafec271842f7a0192b1c6d760f60cf95ce5
sean chavez <seanchavez@seanchavezsMBP.home>
2018-06-19 16:01:45 -0700

    Finish calculator; layout is off; need more styling

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -9,20 +9,21 @@ const App = () => {
     <div className = "container">
       <CalculatorDisplay text="0"/>
       <ActionButton text="clear"/>
-      <NumberButton text="&divide;"/>
+      <NumberButton custom="operations" text="&divide;"/>
       <NumberButton text="7"/>
       <NumberButton text="8"/>
       <NumberButton text="9"/>
-      <NumberButton text="&multiply;"/>
+      <NumberButton custom="operations" text="x"/>
       <NumberButton text="4"/>
       <NumberButton text="5"/>
       <NumberButton text="6"/>
-      <NumberButton text="-"/>
+      <NumberButton custom="operations" text="-"/>
       <NumberButton text="1"/>
       <NumberButton text="2"/>
       <NumberButton text="3"/>
-      <NumberButton text="+"/>
+      <NumberButton custom="operations" text="+"/>
       <ActionButton text="0"/>
+      <NumberButton custom="operations" text="="/>
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -3,7 +3,7 @@ import './Button.css';
 import ActionButton from './ActionButton';
 
 const NumberButton = props => {
-    return <button className={`number ${props.color}`}>{props.text}</button>
+    return <button className={`number ${props.custom}`}>{props.text}</button>
 }
 
 export default NumberButton
