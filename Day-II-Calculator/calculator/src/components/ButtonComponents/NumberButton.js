c54ef0093cbbb7438b143df2db7748f09cbc12b5
unknown <may.alex43@gmail.com>
2018-06-19 18:03:29 -0400

    Finished number and display layout

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import './Button.css';
+
+
+
+const NumberButton= () => {
+    return (
+        <div className="number-button">
+            <span className="clear-button">clear</span>
+            <h2>7</h2>
+            <h2>8</h2>
+            <h2>9</h2>
+            <h2>4</h2>
+            <h2>5</h2>
+            <h2>6</h2>
+            <h2>1</h2>
+            <h2>2</h2>
+            <h2>3</h2>
+            <h2 className="zero">0</h2>  
+        </div>
+    );
+};
+
+export default NumberButton;
