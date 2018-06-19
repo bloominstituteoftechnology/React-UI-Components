ba69e55dd387294dbe71d07071fa36439f32e08d
tommycam <tommycam67@gmail.com>
2018-06-19 14:18:39 -0500

    MVP

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 import OperationGrid from './components/ButtonComponents/OperationGrid';
 import InputGrid from './components/ButtonComponents/InputGrid';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 const App = () => {
   return (
-    <div>
+    <div className='app-wrapper'>
+      <CalculatorDisplay />
       <div className="buttons">
         <InputGrid />
         <OperationGrid />
