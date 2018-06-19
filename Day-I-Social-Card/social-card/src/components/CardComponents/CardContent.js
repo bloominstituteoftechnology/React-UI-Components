2e36c49966d0b72bb1163fe5f0149c52cb2b7241
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-06-19 10:40:45 -0600

    added CardContent link

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,8 +4,18 @@ import './Card.css';
 const CardContent = () => {
    return (
        <div className="card-content">
+       <p className="bottom-text">Get started with React</p>
+        <p className="smaller-bottom-text">
+        React makes it painless to make interactive UI's. Design simple views
+        for each state in your application.</p>
+
+        <a
+        className="link">
+        href="http://reactjs.org"
+        reactjs.org
+        </a>
        </div>   
-   ) 
-}
+   );
+};
 
 export default CardContent;
