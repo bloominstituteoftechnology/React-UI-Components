68b4bf58f9cf444be5db5b2906b56aa38c96517d
Kelley Henican <kelleyhenican2@gmail.com>
2018-06-18 23:14:40 -0500

    added banner and fixed heading and card styling

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardBanner = () => <div className="card-banner"></div>
+    
+
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+
+
+
+const CardContainer = () => {
+    return (
+    <div className="card-wrapper">
+    <CardBanner />
+    </div>
+    )
+}
+
+
+export default CardContainer;
