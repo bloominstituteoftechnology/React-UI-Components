332d7164eaf022c79868e3bb846f130613472748
unknown <may.alex43@gmail.com>
2018-06-18 22:01:55 -0400

    Nearly finished

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+
+
+const CardBanner = () => {
+    return (    
+        <img className="card-banner" src="https://ibin.co/3wnC6SgIOJud.png" alt ="Card Banner"/>
+    );
+};
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
+
+
+const CardContainer= () => {
+    return (
+        <div className="card-wrapper">
+            <CardContent />
+        </div>
+    );
+};
+
+export default CardContainer;
