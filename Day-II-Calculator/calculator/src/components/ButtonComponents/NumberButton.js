67d46dbb025f0608a68a0679cf0660b1d4ac6566
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-06-19 16:20:04 -0600

    working on class App and css

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return (
+        <button className={'number-button ${props.className}'}>
+          {props.text}</button>
+    );
+};
+
+export default NumberButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return (
+        <div className="total">0</div>
+    );
+};
+
+export default CalculatorDisplay;
