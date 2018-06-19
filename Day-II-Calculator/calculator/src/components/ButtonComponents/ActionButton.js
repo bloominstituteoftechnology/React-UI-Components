0cd237e8e2cfa08d1df328135f6d133a28cc3940
Noah-Amar <ne.amar@outlook.com>
2018-06-19 18:03:25 -0500

    action button, number button

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = () => {
+    return(
+<div className='action-button'>
+ActionButton
+</div>
+    );
+};
+
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = () => {
+    return(
+<div className='number-button'>
+NumberButton
+</div>
+    );
+};
+
+export default NumberButton;
