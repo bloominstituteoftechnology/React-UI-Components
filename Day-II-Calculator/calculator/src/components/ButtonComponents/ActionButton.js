175ccb20dd34c3948069c705b6a7c946b2754316
Kelley Henican <kelleyhenican2@gmail.com>
2018-06-19 17:46:43 -0500

    added components for calculator

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Button.css';
+
+
+const ActionButton = props => <button className={`button-styles ${props.CustomStyleProp}`}>{props.text}</button>
+
+export default ActionButton;
