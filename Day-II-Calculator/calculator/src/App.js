f607b83774be5051ce8663a472849a47d94b3df5
Kelley Henican <kelleyhenican2@gmail.com>
2018-06-19 20:28:14 -0500

    clear and divide buttons

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js'
+import ActionButton from './components/ButtonComponents/ActionButton.js'
+import NumberButton from './components/ButtonComponents/NumberButton.js'
 
 const App = () => {
   return (
     <div>
      <CalculatorDisplay buttonStyle="display-style" text="0" />
+     <div class="row-one">
+     <ActionButton buttonStyle="action-style" text="clear" />
+    <NumberButton buttonStyle="operator-style" text="&divide;" />
+    </div>
     </div>
   );
 };
