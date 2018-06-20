0997862882a5241834c300bd9264c5583bf40641
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-06-19 18:40:26 -0600

    Display and Button css

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './App.css';
-import CalculatorDisplay from './CalculatorDisplay';
-import ButtonContainer from './ButtonContainer';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ButtonContainer from './components/ButtonComponents/ButtonContainer';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 
 class App extends React.Component {
-  
+  render() {
+   return (
+    <div className="calculator-container">
+    <CalculatorDisplay />
+    <ButtonContainer className="clear-button">
+     <ActionButton className="text" text="clear" />
+     <NumberButton color="red-button" text="/" />
+     </ButtonContainer>
+     <ButtonContainer>
+       <NumberButton color="number-button" text="7" />
+       <NumberButton color="number-button" text="8" />
+       <NumberButton color="number-button" text="9" />
+       <NumberButton color="number-button" text="X" />
+       </ButtonContainer>
+       <ButtonContainer>
+       <NumberButton color="number-button" text="4" />
+       <NumberButton color="number-button" text="5" />
+       <NumberButton color="number-button" text="6" />
+       <NumberButton color="number-button" text="-" />
+       </ButtonContainer>
+       <ButtonContainer>
+       <NumberButton color="number-button" text="1" />
+       <NumberButton color="number-button" text="2" />
+       <NumberButton color="number-button" text="3" />
+       <NumberButton color="number-button" text="+" />
+       </ButtonContainer>
+       <ButtonContainer>
+       <ActionButton color="number-button" text="0" />
+       <NumberButton color="number-button" text="=" />
+       </ButtonContainer>
+
+    </div>
+    );
+  }
 }
 
 export default App;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const NumberButton = props => {
     return (
-        <button className={'number-button ${props.className}'}>
+        <button className={'number-button ${props.color}'}>
           {props.text}</button>
     );
 };
