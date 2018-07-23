66663d5f0d714244e403391803ea168352808c38
keegn <keegan.t.burkett@gmail.com>
2018-07-23 16:24:23 -0500

    initial commit
    completed header components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title-container">
+            <div className="header-title">
+                Lambda School
+                <span className="user-date">@LambdaSchool 26 Jan</span>
+            </div>
+            <HeaderContent />
+        </div>
+    );
+};
+
+export default HeaderTitle;
