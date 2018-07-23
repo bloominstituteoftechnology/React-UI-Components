c16c439d23c3d9b646857c1ba50e4e4716b5f0c7
Naaz Shaikh <naazhome@Naazs-MacBook-Pro-3.local>
2018-07-23 13:11:40 -0700

    Initial commit: working on importing;

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = () => {
+    return <div className = 'container-header'> Hello from Header </div>
+};
+
+export default HeaderContainer;
