e3285c9d2b07f55b30da1f8b6585f4c84d5893a0
Grant Reighard <grant.reighard@gmail.com>
2018-07-23 17:49:39 -0400

    Added footer.

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-    return <div>
-        <div><img src="../../../img/conversation.png" alt="Converation icon"/></div>
-        <div><img src="../img/retweet.png" alt="Retweet icon"/></div>
-        <div><img src="../img/heart.png" alt="Heart icon"/></div>
-        <div><img src="../img/mail.png" alt="Mail icon"/></div>
+    return <div className="footerBar">
+        <div><img src="/img/conversation.png" alt="Converation icon"/></div>
+        <div className="padLeft"><img src="/img/retweet.png" alt="Retweet icon"/>&nbsp;&nbsp;&nbsp;6</div>
+        <div className="padLeft"><img src="/img/heart.png" alt="Heart icon"/>&nbsp;&nbsp;&nbsp;4</div>
+        <div className="padLeft"><img src="/img/mail.png" alt="Mail icon"/></div>
     </div>
 }
 
