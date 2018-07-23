130b5533365473985c4d4ab5132c1532a98fac2c
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-07-23 16:45:20 -0600

    Card finished, Header started

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Card.css';
+import CardContent from "./CardContent";
+import CardBanner from './CardBanner';
 
 const CardContainer = () => {
-    return <div className="banner-wrapper">Hello from Card Container</div>;
+    return <div className="banner-wrapper">
+        <CardBanner />
+        <CardContent />
+    </div>;
 };
 
 export default CardContainer;
