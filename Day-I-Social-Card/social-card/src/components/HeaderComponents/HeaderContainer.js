e2c54daa4b3add4a5767985d703e85467fbd696f
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-07-23 13:19:17 -0600

    Header Content component complete.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,3 +2,4 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumnail.js';
 import HeaderTitle from './HeaderTitle.js'; 
+import HeaderContent from './HeaderContent.js'; 
