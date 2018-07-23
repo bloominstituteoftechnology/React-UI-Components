00b4f4aca0fb2c4b963214b12f54486734a48b0f
johnoro <jjcummins102@gmail.com>
2018-07-23 19:40:09 -0400

    Repost and favorite functionality added.

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => {
-    const onClickComment = () => alert('No commenting functionality added yet! Oops!');
-    const onClickRepost = () => alert('No resposting functionality added yet! Oops!');
-    const onClickFav = () => alert('No favorite functionality added yet! Oops!');
-    const onClickMail = () => alert('No mailing functionality added yet! Oops!');
-    return (
-        <footer>
-            <div className="footer">
-                <span className="icon" onClick={onClickComment}>
-                    <i className="far fa-comment"></i>
-                </span>
-                <span className="icon" onClick={onClickRepost}>
-                    <i className="fas fa-sync"></i> &nbsp; <span className="stats-num">6</span>
-                </span>
-                <span className="icon" onClick={onClickFav}>
-                    <i className="far fa-heart"></i> &nbsp; <span className="stats-num">4</span>
-                </span>
-                <span className="icon" onClick={onClickMail}>
-                    <i className="far fa-envelope"></i>
-                </span>
-            </div>
-            <hr />
-        </footer>
-    );
-};
+class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            repost: 6,
+            fav: 4,
+        };
+    }
+
+    onClickComment = () => alert('No commenting functionality added yet! Oops!');
+    onClickRepost = () => this.setState({repost: this.state.repost + 1});
+    onClickFav = () => this.setState({fav: this.state.fav + 1});
+    onClickMail = () => alert('No mailing functionality added yet! Oops!');
+
+    render() {
+        return (
+            <footer>
+                <div className="footer">
+                    <span className="icon" onClick={this.onClickComment}>
+                        <i className="far fa-comment"></i>
+                    </span>
+                    <span className="icon" onClick={this.onClickRepost}>
+                        <i className="fas fa-sync"></i> &nbsp; <span className="stats-num">{this.state.repost}</span>
+                    </span>
+                    <span className="icon" onClick={this.onClickFav}>
+                        <i className="far fa-heart"></i> &nbsp; <span className="stats-num">{this.state.fav}</span>
+                    </span>
+                    <span className="icon" onClick={this.onClickMail}>
+                        <i className="far fa-envelope"></i>
+                    </span>
+                </div>
+                <hr />
+            </footer>
+        );
+    }
+}
 
 export default Footer;
