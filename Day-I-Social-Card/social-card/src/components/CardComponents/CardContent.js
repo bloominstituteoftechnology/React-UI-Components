d8d15d4c0907a811ed7ae0f953f4f105e532d6df
Arthur Bates <aabatesj@gmail.com>
2018-07-23 18:30:10 -0400

    Arthur Bates
    Sorry!
    
    Card content, container and Cardbanner added to app.js

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className="content">
+            <p>
+                <strong>
+                    Get started with react
+                </strong>
+            </p>
+            <p>
+                React makes it painless to create interactive UIs. Design simple views for each state in your application.
+            </p>
+            <p>
+                reactjs.org
+            </p>
+        </div>
+    );
+};
+
+export default CardContent; 
