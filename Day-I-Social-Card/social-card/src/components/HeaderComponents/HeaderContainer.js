57410f0d752d3a0eeadb10848e331161287993bf
Kelley Henican <kelleyhenican2@gmail.com>
2018-07-23 17:06:14 -0500

    added header content

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 
 const HeaderContainer = () => { 
     return (
     <div>
    <ImageThumbnail />
    <HeaderTitle /> 
+   <HeaderContent />
    </div>
     )
 }
