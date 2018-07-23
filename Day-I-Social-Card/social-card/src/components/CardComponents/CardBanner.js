14a7ab3e37395e487d01f845f8f902647d6c2cf8
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-23 17:24:03 -0500

    all components added, styling now

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Card.css';
 
 const CardBanner = () => {
-    return <img className='custom-card-img' src="https://ibin.co/3wnC6SgIOJud.png" alt="React logo" />
+    return <div className='custom-card-banner'></div>
 }
 
 export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return <div className='custom-card-content'>
+    <h4>Get started with React</h4>
+    <p>React makes it painfless to create interactive UIs. Design simple views for each state in your application.</p>
+    <p>reactjs.org</p>
+    </div>
+}
+
+export default CardContent;
