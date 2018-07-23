37867b73b6d20fe232d83df57e9082f00b65a184
Carey <careycbaldwin@gmail.com>
2018-07-23 19:07:41 -0400

    worked on styling with pixel perfect

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const HeaderTitle = () => {
     return <div className = "header-title">
-    <div className = "title"><h1> Lambda School</h1></div> 
+    <div className = "title"><h4> Lambda School</h4></div> 
     <div className = "handle"><p>@lambdaschool</p></div>
     <div className = "time"><time datetime = "2018-07-23"> . July 23, 2018</time></div>
         {/* timestamp research */}
