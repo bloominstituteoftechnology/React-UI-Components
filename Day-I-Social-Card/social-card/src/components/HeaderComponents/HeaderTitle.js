944fbbc2f0a8eae5c82d250021d2c486db894054
Ashwin <aswinjeganath@gmail.com>
2018-07-24 02:26:36 +0530

    Set up the structure, add and style header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => (
+  <div className="header__title-container mb-2">
+    <span className="header__title">Lambda School</span>
+    <span className="header__handle">@LambdaSchool</span>
+    <span className="header__timestamp">26 Jan</span>
+  </div>
+);
+
+export default HeaderTitle;
