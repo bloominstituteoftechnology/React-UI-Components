f5318c8710b8d7adbc6944007830064573d210b9
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-23 14:31:37 -0700

    add minutes.js into date-stamp

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
 const HeaderTitle = () => {
 	return (
 		<div className="headerTitle">
-			<p><strong>Lambda School</strong> <small>@LambdaSchool 26 Jan</small></p>
+			<p><strong>Lambda School</strong> <small className="date">@LambdaSchool {moment().format('MMMM Do YYYY')}</small></p>
 		</div>
 	);
 }
