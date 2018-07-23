fcfd4c39a21552594ed9b99f8020ee41e6d8f1dd
jaymzsocool <james.mcraejr@gmail.com>
2018-07-23 18:38:46 -0400

    Changed astrick to bulletpoint

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const HeaderTitle = () => <div className="title-container">
 <h2 className="header-title">Lambda School</h2> 
-<h3 className="header-subtitle"> @LambdaSchool * 26 Jan</h3>
+<h3 className="header-subtitle"> @LambdaSchool • 26 Jan</h3>
 </div>;
 
 export default HeaderTitle;
