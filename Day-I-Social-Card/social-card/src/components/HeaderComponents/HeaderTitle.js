fb283211cd653c8debd0ae753f8109ed0c6895a9
rodacoram <rodacoram8@gmail.com>
2018-07-23 17:06:10 -0600

    added moment.js functionality

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
+
 
 const HeaderTitle = ()=> {
     return (
         <div class="title">
-            <h1>Lambda School</h1><small>@LambdaSchool - 26 Jan</small>
+            <h1>Lambda School</h1><small>{moment().format("MMM Do YY")}</small>
         </div>
     );
 };
