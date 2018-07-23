da879777fd2af29cdc34220063e6ba977fc20c6d
Lauren <llwor94@gmail.com>
2018-07-23 16:10:50 -0700

    retweet and fave icons update individually on click, and hold value upon refresh

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -6,13 +6,71 @@ import refresh from './assets/refresh.svg';
 import heart from './assets/heart.svg';
 import mail from './assets/mail.svg';
 
-const Footer = () => (
-  <div className="footer">
-    <FooterIcon iconImg={speechBubble}/>
-    <FooterIcon iconImg={refresh} count="6"/>
-    <FooterIcon iconImg={heart} count="4"/>
-    <FooterIcon iconImg={mail}/>
-  </div>
-);
+export default class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddRetweet = this.handleAddRetweet.bind(this);
+    this.handleAddFave = this.handleAddFave.bind(this);
+    this.state = {
+      retweetCount: 0,
+      faveCount: 0
+    };
+  }
+  componentDidMount() {
+    const stringRetweetCount = localStorage.getItem('retweetCount');
+    const stringFaveCount = localStorage.getItem('faveCount');
+    const retweetCount = parseInt(stringRetweetCount, 10);
+    const faveCount = parseInt(stringFaveCount, 10);
 
-export default Footer;
+    if (!isNaN(retweetCount) && !isNaN(faveCount)) {
+      this.setState(() => ({ 
+        retweetCount,
+        faveCount 
+      }));
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.retweetCount !== this.state.retweetCount) {
+      localStorage.setItem('retweetCount', this.state.retweetCount);
+    }
+    if (prevState.faveCount !== this.state.faveCount) {
+      localStorage.setItem('faveCount', this.state.faveCount);
+    }
+  }
+  handleAddRetweet() {
+    this.setState((prevState) => {
+      return {
+        retweetCount: prevState.retweetCount + 1
+      };
+    });
+  }
+  handleAddFave() {
+    this.setState((prevState) => {
+      return {
+        faveCount: prevState.faveCount + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="footer">
+        <FooterIcon iconImg={speechBubble}/>
+        <FooterIcon onClick={this.handleAddRetweet} iconImg={refresh} count={this.state.retweetCount} />
+        <FooterIcon onClick={this.handleAddFave} iconImg={heart} count={this.state.faveCount}/>
+        <FooterIcon iconImg={mail}/>
+      </div>
+    )
+  }
+}
+// const Footer = () => (
+//   <div className="footer">
+//     <FooterIcon iconImg={speechBubble}/>
+//     <FooterIcon iconImg={refresh} />
+//     <FooterIcon iconImg={heart} />
+//     <FooterIcon iconImg={mail}/>
+//   </div>
+// );
+
+// export default Footer;
--- Day-I-Social-Card/social-card/src/components/FooterComponents/FooterIcon.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/FooterIcon.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Footer.css';
 
 const FooterIcon = props => (
-  <div className="footer-icon">
-    <img  className="icon" src={props.iconImg} alt="icon"/>
-    {props.count && <div className="count">{props.count}</div>}
+  <div className="footer-icon" onClick={props.onClick}>
+    <img className="icon" src={props.iconImg} alt="icon"/>
+    {props.count > 0 && <div className="count">{props.count}</div>}
   </div>
 );
 
