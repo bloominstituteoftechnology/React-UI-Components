edb54206e51e5b2fe699f4762bb9d15d0a461672
AI2EJAY <JHammett1314@gmail.com>
2018-07-23 18:34:38 -0500

    Most recent

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from "./components/HeaderComponents/HeaderContainer.js";
-//import CardContainer from "./components/CardComponents/CardContainer.js";
+import CardContainer from "./components/CardComponents/CardContainer.js";
 const App = () => {
   return (
     <div className="card-container">
     <HeaderContainer />
-  
+    <CardContainer />
     </div>
 
     
@@ -16,4 +16,3 @@ const App = () => {
 export default App;
 
 
-//<CardContainer />
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <p className="card-content">
+            Et adipiscing lacus cursus cubilia, erat felis. Facilisis 
+            curae; risus pharetra egestas, quisque pharetra.
+             A magnis adipiscing tincidunt nascetur, tellus mi dolor etiam, 
+             suspendisse luctus cras mollis pharetra id. Tortor.
+             </p>
+        );
+    };
+    
+export default CardContent;
