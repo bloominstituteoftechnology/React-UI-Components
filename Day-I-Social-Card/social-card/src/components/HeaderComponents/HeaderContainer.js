cda2a044a81bfdd15b069a77deb1b3f00e7e8308
Sean <sean47900@gmail.com>
2018-07-23 15:54:40 -0500

    Header complete

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css';
-import HeaderTitle from './HeaderTitle.js';
-import ImageThumbnail from './ImageThumnail.js';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
 import HeaderContent from './HeaderContent';
 
 const HeaderContainer = () => {
