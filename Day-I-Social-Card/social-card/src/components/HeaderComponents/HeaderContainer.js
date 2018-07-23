01a104c2c0642824f89db8010c2f5aeb67ee6b41
Charlie Winslow <cwinslow22@yahoo.com>
2018-07-23 15:47:17 -0500

    Charlie Winslow

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = () => {
+    return <div className='header-container-wrapper'>Hello from header container</div>;
+};
+
+export default HeaderContainer;
