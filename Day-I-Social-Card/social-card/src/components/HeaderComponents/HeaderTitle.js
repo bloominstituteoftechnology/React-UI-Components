c05ae36eee6cefc9d99d3bdff9e5c0d1eb35552b
AI2EJAY <JHammett1314@gmail.com>
2018-07-23 17:43:50 -0500

    Some progress

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
+import HeaderContent from "./HeaderContent";
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return ( 
+        <div className="header-title-container">
+            <div className="header-title">
+                Lambda School
+                <span className="time-stamp"> 8am Lambda Time Zone</span>
+                <HeaderContent />
+        </div>
+    </div>
+    );
+};
+
+export default HeaderTitle;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Header.css';
-import "./HeaderContainer.js";
-//Create an <ImageThumbnail /> component using this image url https://ibin.co/3whrpKSBbZ81.png as it's img src.
 
-const ImageThumbnail = props => {
-    return <img src="https://ibin.co/3whrpKSBbZ81.png"
-    alt="Lambda"/>
-}
+
+const ImageThumbnail = () => {
+    return (
+    <div className="thumbnail-div">
+    <img className="thumbnail-img" src="https://ibin.co/3whrpKSBbZ81.png"
+        alt="Lambda" />
+    </div>
+    );
+};
+
+
+export default ImageThumbnail;
