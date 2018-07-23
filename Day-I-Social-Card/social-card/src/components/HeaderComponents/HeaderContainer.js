60961b91303fdc256baeee2ab90b764c3a5d3f18
Grant Reighard <grant.reighard@gmail.com>
2018-07-23 15:41:36 -0400

    Initial commit.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return <div className="headerBox">
+    <ImageThumbnail />
+    </div>
+}
+
+export default HeaderContainer;
