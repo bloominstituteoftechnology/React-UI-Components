f85facc591ef20b0ba0d7bc5e8431f856697e201
Sean <sean47900@gmail.com>
2018-07-23 17:52:26 -0500

    More CSS

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,10 +3,14 @@ import './Header.css';
 
 const HeaderTitle = () => {
         return (
-            <div>
+            
+            
+            <div className = "header">
+            <div className = "header-info">
                 <h1 className = "header-title" >Lambda School</h1>
                 <h2 className = "header-subheading">@LambdaSchool</h2>
                 <h2 className = "header-subheading-date">26 jan</h2>
+            </div>
             </div>
         )
     }
