33084394aa9a02ba5c055d88922ea363302f6bed
codejoncode <jonathanjholloway@gmail.com>
2018-07-23 18:06:39 -0500

    push before stand up everything is completed for MVP some stretch still no functionality on icon clicks

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -7,27 +7,28 @@ import './Footer.css';
 
 const Footer = () => { 
     let comment = 0;
-    let sync = 0; 
+    let sync = 0;
+    function handleCommentClick (comment) {
+        let answer = prompt('enter your comment');
+        if (answer){
+            return comment++;
+        }
+    }
+    
+    function handleSyncClick(sync) {
+        return sync+=1
+    }
     return (
     <div className="footer">
-        <i onClick ={handleCommentClick(comment)}className="far fa-comment">{comment}</i>
-        <i onClick={handleSyncClick(sync)} className="fas fa-sync">{sync}</i>
+        <i onClick ={prompt('enter comment')}className="far fa-comment">{comment}</i>
+        <i onClick={() => sync++} className="fas fa-sync">{sync}</i>
         <i className="far fa-heart"></i>
         <i className="far fa-envelope"></i>
     </div>
     );
 }
 
-function handleCommentClick (comment) {
-    let answer = prompt('enter your comment');
-    if (answer){
-        return comment++;
-    }
-}
 
-function handleSyncClick(sync) {
-    return sync+=1
-}
 
 export default Footer; 
 
