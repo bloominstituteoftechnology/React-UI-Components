8bfde37ba34cddfba6b3428de454f248f6f45b48
Grant Reighard <grant.reighard@gmail.com>
2018-07-23 17:34:47 -0400

    Added images to img folder.

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return <div>
+        <div><img src="../../../img/conversation.png" alt="Converation icon"/></div>
+        <div><img src="../img/retweet.png" alt="Retweet icon"/></div>
+        <div><img src="../img/heart.png" alt="Heart icon"/></div>
+        <div><img src="../img/mail.png" alt="Mail icon"/></div>
+    </div>
+}
+
+export default Footer;
