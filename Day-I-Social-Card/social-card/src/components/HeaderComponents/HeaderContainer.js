547bf15f433db1798b6a36021fba8d4ab555d2ed
sliw <slioweng@gmail.com>
2018-07-24 05:41:15 +0800

    Steven L

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <ImageThumbnail />
+      <HeaderContent />
+    </div>
+  );
+};
+
+export default Header;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div className='header-title'>
+      <h2>Lambda School</h2> 
+      <span>@LambdaSchool · 26 jan</span>  
+    </div>
+  )
+}
+
+export default HeaderTitle;
