21f866a9e3f431c70e01c02600c48e63b42fd209
Sean <sean47900@gmail.com>
2018-07-23 15:01:50 -0500

    Trying to set up Header files

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle.js';
+import ImageThumbnail from './ImageThumnail.js';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return(
+        <div className = "header-container">
+        <ImageThumbnail />
+        <div className = "header-text">
+        <HeaderTitle />
+        <HeaderContent />
+        </div>
+    </div>
+    )
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+        return (
+            <div>
+                <h1 className = "header-title" >Lambda School</h1>
+                <h2 className = "header-subheading">@LambdaSchool</h2>
+                <h2 className = "header-subheading-date">26 jan</h2>
+            </div>
+        )
+    }
+    
+    export default HeaderTitle; 
