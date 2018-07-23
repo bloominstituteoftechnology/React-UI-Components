1fd91283958b61f90083454807c33b0a6244e9ef
William <kwontaewoongah@gmail.com>
2018-07-23 16:47:45 -0700

    imported moment to get day and month of today

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
-const HeaderTitle=()=><p><strong>Lambda School</strong> @LambdaSchool · 23 July</p>
+const HeaderTitle=()=><p><strong>Lambda School</strong> <span class='gray'>@LambdaSchool · {moment().format('D MMMM')}</span></p>
 
 export default HeaderTitle;
