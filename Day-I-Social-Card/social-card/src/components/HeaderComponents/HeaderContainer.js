df6a0a4c5a1dd0f4161df78034c9f5f99a9fe4a8
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-07-23 16:19:21 -0500

    Header components
    Working on the header components, but ended up putting everything in on component so I have to change that.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import './Header.css';
+
+
+const heading = () => {
+    return(
+        <div class="container-header">
+        <div className="title">
+        <p>Lambda School</p>
+        <p className="faded">@LambdaSchool</p>
+        <p className="faded">July 23, 2018</p>
+        </div>
+        <div className="content-header">
+        <p>
+            Lets learn React by building simpleInterfaces with components. Dont try to
+            overthink it. Just keep it simple and have fun. Once you feel comfortable using
+            components you are well on you way to mastering React!
+        </p>
+        </div>
+        </div>
+
+    )
+}
+export default heading;
