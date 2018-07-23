1d7e7f1df7698ed400209c490174949d29be8e43
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-23 15:30:03 -0500

    ran yarn and started working on cardcomponents

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
-    <div>
+    <div className=''>
       <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
@@ -16,6 +17,7 @@ const App = () => {
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
+        <CardContainer />
       </p>
     </div>
   );
