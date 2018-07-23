012c2cdabbab705fee682db2e91b58972f5ecdcd
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-07-23 16:06:47 -0600

    Added alt tags to images

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -2,6 +2,6 @@ import React from 'react';
 import './Header.css';
 
 
-const ImageThumbnail  = () => <img src = "https://ibin.co/3whrpKSBbZ81.png" width = "40px" height = "40px"/>
+const ImageThumbnail  = () => <img src = "https://ibin.co/3whrpKSBbZ81.png" width = "40px" height = "40px" alt = "Lambda School Logo"/>
 
 export default ImageThumbnail; 
