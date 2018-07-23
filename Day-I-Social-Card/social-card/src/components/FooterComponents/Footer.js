fb2f587a89f549fa61613a2bd054da13b55a763e
Ashwin <aswinjeganath@gmail.com>
2018-07-24 04:12:06 +0530

    Add click functionality to footer icons

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,22 +1,67 @@
 import React from 'react';
 import './Footer.css';
 
-const FooterAction = ({ iconClass, number }) => (
-  <div className="Footer__action">
-    <span className="Footer__icon">
-      <i className={iconClass} />
-    </span>
-    {number ? <span className="Footer__number">{number}</span> : ''}
-  </div>
-);
+const FooterAction = ({ iconClass, number, liked, retweeted, onClick }) => {
+  const className =
+    'Footer__action' +
+    (liked
+      ? ' Footer__icon--liked'
+      : retweeted
+        ? ' Footer__icon--retweeted'
+        : '');
+  return (
+    <div className={className} onClick={onClick}>
+      <span className="Footer__icon">
+        <i className={iconClass} />
+      </span>
+      {number ? (
+        <span className="Footer__number">
+          {liked || retweeted ? number + 1 : number}
+        </span>
+      ) : (
+        ''
+      )}
+    </div>
+  );
+};
 
-const Footer = () => (
-  <div className="Footer py-1">
-    <FooterAction iconClass="far fa-comment" />
-    <FooterAction iconClass="fas fa-sync" number={6} />
-    <FooterAction iconClass="far fa-heart" number={4} />
-    <FooterAction iconClass="far fa-envelope" />
-  </div>
-);
+class Footer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      liked: false,
+      retweeted: false
+    };
+  }
+
+  handleClick(type) {
+    return () =>
+      this.setState({
+        [type]: !this.state[type]
+      });
+  }
+
+  render() {
+    return (
+      <div className="Footer py-1">
+        <FooterAction iconClass="far fa-comment" />
+        <FooterAction
+          iconClass="fas fa-sync"
+          number={6}
+          retweeted={this.state.retweeted}
+          onClick={this.handleClick('retweeted')}
+        />
+        <FooterAction
+          iconClass="far fa-heart"
+          number={4}
+          liked={this.state.liked}
+          onClick={this.handleClick('liked')}
+        />
+        <FooterAction iconClass="far fa-envelope" />
+      </div>
+    );
+  }
+}
 
 export default Footer;
