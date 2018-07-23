e2f0d12c94cfe4397fcfe681a11c96a7a59dcaea
mkerbleski <mkerbles@gmail.com>
2018-07-23 14:27:25 -0600

    Created 'CardContainer' and 'HeaderContainer', imported them into 'App', and deployed to local server.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+import HeaderContainer from '../HeaderComponents/HeaderContainer.js';
+
+const CardContainer = () => {
+  return <div className="card-container">
+            CardContainer
+            <HeaderContainer />
+          </div>;
+
+};
+
+export default CardContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+const  HeaderContainer = () => {
+  return <div className="header-container">HeaderContainer</div>;
+};
+
+
+export default HeaderContainer;
