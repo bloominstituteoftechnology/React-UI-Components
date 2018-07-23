6f8910ad2ac193c326743211d8726bb3093da5e1
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-07-23 13:48:34 -0600

    Added CardBanner component

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    <div className = "cardBanner">
+        <img src = "https://ibin.co/3wnC6SgIOJud.png" />
+    </div>
+}
+
+export default CardBanner; 
