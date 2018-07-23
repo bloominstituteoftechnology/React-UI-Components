a6c7e56a6ad17c431ef72807a8091a015600815f
Charlie Winslow <cwinslow22@yahoo.com>
2018-07-23 16:29:12 -0500

    added header container
    added header title and imported to header container
    
    added image thumbnail and imported to headercontainer

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
 
 const HeaderContainer = () => {
-    return <div className='header-container-wrapper'>Hello from header container</div>;
+    return <div className='header-container-wrapper'>
+    <ImageThumbnail />
+    <HeaderTitle />    
+    </div>;
 };
 
 export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return <h5>Lambda School @LambdaSchool 26 jan</h5>;
+};
+
+export default HeaderTitle;
