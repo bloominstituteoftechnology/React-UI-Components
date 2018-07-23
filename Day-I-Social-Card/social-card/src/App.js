e794a7adf52e91d2b3353da005606ec3c6b331d5
MichaelBoegner <cmikeb@gmail.com>
2018-07-23 17:45:10 -0400

    Charles Boegner
    MVPs completed. Attempting Stretch.

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 
 const App = () => {
 
- return (
-<HeaderContainer />
-);
+  return (
+    <div className="main-container">
+      <HeaderContainer />
+      <a href="http://www.reactjs.org"><CardContainer /></a>
+    </div>
+  );
 }
 
 
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Card.css';
 
-// const CardBanner = () => <div>tester</div>
+const CardBanner = () => {
 
-// export default CardBanner;
+    return (
+        <div >
+            <img className="card-banner" src="https://ibin.co/3wnC6SgIOJud.png" alt="react logo" />
+        </div>
+    );
+}
+
+
+
+export default CardBanner;
