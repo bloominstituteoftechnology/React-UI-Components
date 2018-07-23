6832f7cb51492942bb3fece53663842ff4dd3573
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-07-23 16:52:05 -0400

    Realized I haven't been pushing like I should
    -imported all the things
    -working on styling
    -kinda jumping around playing with everything

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+
+  return(
+    <div className="header-title">
+      <p>Lambda School @LambdaSchool 26 Jan</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
