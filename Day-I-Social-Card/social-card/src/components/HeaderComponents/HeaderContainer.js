daaf074e808ec39f5b3ab704bc475b018cddcbea
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-07-23 13:37:31 -0600

    Finished HeaderContainer component and rendered using App.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import './Header.css';
-import ImageThumbnail from './ImageThumnail.js';
-import HeaderTitle from './HeaderTitle.js'; 
-import HeaderContent from './HeaderContent.js'; 
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle'; 
+import HeaderContent from './HeaderContent'; 
+
+const HeaderContainer = () => {
+    return(
+        <div className = "header-container">
+        <ImageThumbnail />
+        <div className = "header-text">
+        <HeaderTitle />
+        <HeaderContent />
+        </div>
+    </div>
+    )
+}
+
+export default HeaderContainer;
