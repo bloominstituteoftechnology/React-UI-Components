6dc966ec5c8cc6fa54091ed3c1b76af745fe6939
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-07-23 14:07:59 -0600

    Finished CardContainer component and added to App.js

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './Card.css';
 
 const CardBanner = () => {
-    <div className = "cardBanner">
-        <img src = "https://ibin.co/3wnC6SgIOJud.png" />
-    </div>
+    return (
+        <div className = "cardBanner">
+            <img src = "https://ibin.co/3wnC6SgIOJud.png" />
+        </div>
+    )
+    
 }
 
 export default CardBanner; 
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return(
+        <div className = "cardContent">
+            <h1 className = "cardContent-header">Get Started With React</h1>
+            <p className ="cardContent-text">React makes it painless to create interactive UIs. Design simple views for each state in youor application</p>
+            <a className = "cardContent-link">reactjs.org</a>
+        </div>
+    )
+}
+
+export default CardContent; 
