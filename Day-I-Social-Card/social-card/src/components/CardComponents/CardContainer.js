63092d5c0392b5b04ac20d7aeca8c86ea70b8e5b
johnoro <jjcummins102@gmail.com>
2018-07-23 16:15:40 -0400

    Added CardContainer component and subcomponents.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardContainer = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default CardContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div>
+            <span>Lambda School</span>
+            <span>@LambdaSchool · 23 July</span>
+        </div>
+    );
+};
+
+export default HeaderTitle;
