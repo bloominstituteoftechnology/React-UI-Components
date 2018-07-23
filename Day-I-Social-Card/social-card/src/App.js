7246c10a3bb4e08c78cf94f2ad290ea11106e157
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-23 17:19:43 -0500

    Img banner in
    Working on the content below the image banner

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import   HeaderContainer from './components/HeaderComponents/HeaderContainer';
-// import HeaderContent from './components/HeaderComponents/HeaderContent';
-// import HeaderTitle from './components/HeaderComponents/HeaderTitle'; 
-// import ImageThumbnail from './components/HeaderComponents/ImageThumbnail'
+import CardContainer from './components/CardComponents/CardContainer'
 
 const App = () => {
   return (
     <div>
       <HeaderContainer />
+      <CardContainer />
     </div>
   );
 };
