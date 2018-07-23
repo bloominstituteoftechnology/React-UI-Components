cc2a0d845502e556f4a8a12fd486a981ccdb4d49
Alberto-Funk <laptopreneur@gmail.com>
2018-07-23 15:07:53 -0700

    WIP

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+    
+    </div>
+  );
+};
+
+export default Footer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+  return (
+    <div className="img-container">
+      <img src="https://ibin.co/3whrpKSBbZ81.png" alt="lambda logo" />
+    </div>
+  );
+};
+
+
+export default ImageThumbnail;
