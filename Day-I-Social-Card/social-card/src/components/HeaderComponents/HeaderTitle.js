8b7a228941418bb47a615f70d52105f1bdd8f3cc
Ashwin <aswinjeganath@gmail.com>
2018-07-24 04:24:35 +0530

    Add moment js to display current time in header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import moment from 'moment';
 import './Header.css';
 
-const HeaderTitle = () => (
-  <div className="header__title-container mb-2">
-    <span className="header__title">Lambda School</span>
-    <span className="header__handle">@LambdaSchool</span>
-    <span className="header__timestamp">26 Jan</span>
-  </div>
-);
+class HeaderTitle extends React.Component {
+  constructor() {
+    super();
+
+    this.formats = ['DD MMM', 'dddd', 'YYYY', 'hh:mm:ss', ''];
+
+    this.state = {
+      formatIndex: 0
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      formatIndex: (this.state.formatIndex + 1) % this.formats.length
+    });
+  }
+
+  render() {
+    return (
+      <div className="header__title-container mb-2">
+        <span className="header__title">Lambda School</span>
+        <span className="header__handle">@LambdaSchool</span>
+        <span
+          style={{ cursor: 'pointer' }}
+          className="header__timestamp"
+          onClick={() => this.handleClick()}
+        >
+          {moment().format(this.formats[this.state.formatIndex])}
+        </span>
+      </div>
+    );
+  }
+}
 
 export default HeaderTitle;
