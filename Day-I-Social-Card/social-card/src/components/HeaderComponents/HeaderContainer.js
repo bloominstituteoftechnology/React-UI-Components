78c7b60b4805245d4d763a985ef6b1c5ce16c3f7
Carey <careycbaldwin@gmail.com>
2018-07-22 21:26:44 -0400

    added content to Header .js files

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail.js';
+import HeaderTitle from './HeaderTitle.js';
+import HeaderContent from './HeaderContent.js';
+
+const HeaderContainer = () => {
+    return (
+        <div className = "header-container">
+        <ImageThumbnail />
+        <div className = "header-title-content">
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+        </div>
+    );
+};
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return <div className = "header-title">
+    <h1> Lambda School</h1> 
+    <p>@lambdaschool</p>
+    <time datetime = "2018-07-23">July 23, 2018</time>
+        {/* timestamp research */}
+    </div>
+
+};
+
+export default HeaderTitle;
