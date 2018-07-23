8dfc5d62621484af5c30c2b019e3c02de5ec7b70
Grant Reighard <grant.reighard@gmail.com>
2018-07-23 17:11:35 -0400

    Finished all objectives.

--- Day-I-Social-Card/social-card/src/index.js
+++ Day-I-Social-Card/social-card/src/index.js
@@ -4,3 +4,9 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+let cardContent = document.querySelector(".cardBox");
+
+cardContent.addEventListener('click', () => {
+    window.open('https://www.reactjs.org/')
+})
