441fa9adf091f4c688263fc9c14a7e2c446ddf3c
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-23 16:06:19 -0500

    added header image and sized it

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,7 +3,9 @@ import './Header.css';
 
 
 const HeaderContainer = () => {
-    return <header className='custom-header-container'></header>;
+    return <header className='custom-header-container'>
+        <img className='custom-header-img' src="https://ibin.co/3whrpKSBbZ81.png" alt="Lambda logo" />
+    </header>;
 }
 
 export default HeaderContainer;
