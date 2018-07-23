93f69ac36ee07f372e0ed2ae702b165cfb2c2c43
AI2EJAY <JHammett1314@gmail.com>
2018-07-23 15:48:53 -0500

    First commit

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+import "./HeaderContainer.js";
+//Create an <ImageThumbnail /> component using this image url https://ibin.co/3whrpKSBbZ81.png as it's img src.
+
+const ImageThumbnail = props => {
+    return <img src="https://ibin.co/3whrpKSBbZ81.png"
+    alt="Lambda"/>
+}
