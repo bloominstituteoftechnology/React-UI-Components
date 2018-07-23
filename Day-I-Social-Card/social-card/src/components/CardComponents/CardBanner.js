021e70c574389bf64aa869d500f194fc7a7c4329
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-07-23 17:19:43 -0500

    Img banner in
    Working on the content below the image banner

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const bannerImg=()=>{
+    return(
+        <div className="img-banner">
+        <img class="banner-img" src="https://ibin.co/3wnC6SgIOJud.png"></img>
+        </div> 
+    )  
+}
+
+export default bannerImg; 
