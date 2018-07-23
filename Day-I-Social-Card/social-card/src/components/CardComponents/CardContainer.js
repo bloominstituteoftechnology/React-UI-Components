7bbd29c0637de2e2691c273398ede2745e0a47e0
kamalnrf <kamalnrf@hotmail.com>
2018-07-24 03:25:34 +0530

    Added CSS to Card

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,11 +4,13 @@ import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
 const content = "Some content"
+const title = `Get Started with react`
+const url = `https://www.reactjs.org`
 function CardContainer (props){
     return (
         <div class="cardContainer">
             <CardBanner url="https://ibin.co/3wnC6SgIOJud.png" alt="banner"/>
-            <CardContent content={content} />
+            <CardContent content={content} title={title} url={url}/>
         </div>
     )
 }
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,7 +4,9 @@ import './Card.css';
 function CardContent(props) {
     return (
         <div class="cardContent">
+            <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <div class="url"><a href={props.url}>{props.url}</a></div>
         </div>
     )
 }
