1e070ae1f38d7ec11079610076d6f65459ad15d9
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-23 17:04:27 -0500

    Working on the card
    Just finished with the header container. Now I am working on the Car container and components.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const headerTitle = () =>{
+    return  (
+
+        <div className="title">
+        <p>Lambda School</p>
+        <p className="faded">@LambdaSchool</p>
+        <p className="faded">July 23, 2018</p>
+        </div>
+    )
+}
+
+export default headerTitle; 
