1fa6e8c6b1e42f8ce9a9ec0b75680c05cb58b8c9
kamalnrf <kamalnrf@hotmail.com>
2018-07-24 02:43:52 +0530

    Added cardContainer

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 // const App = () => {
 //   return (
@@ -26,6 +27,7 @@ const App = () => {
   return (
     <div class="card">
       <HeaderContainer />
+      <CardContainer />
     </div>
   )
 }
