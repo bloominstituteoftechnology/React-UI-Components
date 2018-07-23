3c7daef20e708cca5ee1d1fdbff2edf7f58869f6
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-07-23 15:54:53 -0600

    [CAdded moment.js module to node modules and updated date

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment'; 
+
+let date = Date.now(); 
+console.log(date); 
 
 const HeaderTitle = () => {
     return (
         <div className = "header-title">
             <h1 className = "header-heading" >Lambda School</h1>
             <h2 className = "header-subheading">@LambdaSchool</h2>
-            <h2 className = "header-subheading-date">26 jan</h2>
+            <h2 className = "header-subheading-date">{moment(date).format("MMM Do")}</h2>
         </div>
     )
 }
