acf71b7546ed7282c9db4169ac1b24588837b006
Noah-Amar <ne.amar@outlook.com>
2018-07-23 16:35:35 -0500

    img tag for CardBanner

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Card.css';
 
 const CardBanner=()=>{
-    return;
+    return <img src='https://ibin.co/3wnC6SgIOJud.png' alt='react-symbol' width='50px' height='50px';
 }
 
 export default CardBanner;
