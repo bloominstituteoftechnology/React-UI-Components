025438789be88d87ae9933590125eb120395c117
jaymzsocool <james.mcraejr@gmail.com>
2018-07-23 16:32:25 -0400

    Thumbnail image compotent set up

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail'
 
-const HeaderContainer = () => <div className="header-container">Hello from Header Container.</div>;
+const HeaderContainer = () => <div className="header-container">{ImageThumbnail()}Hello from Header Container.</div>;
 
 export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderImage = () => <img src="https://ibin.co/3whrpKSBbZ81.png" className="header-image" alt="Thumbnail"></img>;
+
+export default HeaderImage;
