baf528442f52712c4f5aa4c4c1fc77ebd261672b
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-07-23 15:23:47 -0700

    Major Changes - Header
    Finished setting up the header.
    
    Still missing the Lambda Logo.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 
 const HeaderContainer = () => {
     return (
         <div className="top-container">
             <HeaderTitle />
+            <HeaderContent />
         </div>
     );
 };
