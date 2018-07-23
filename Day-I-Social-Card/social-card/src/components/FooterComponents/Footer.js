0f851f035d641f8fc153e7576022b363118ba744
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-07-23 19:31:16 -0400

    Minor footer styling change

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,7 +5,7 @@ const FooterContainer = () => {
 
   return(
     <div className="footer-container">
-			<a href="#" className="footer-container-icon flip"><i className="far fa-comment " /></a>
+			<a href="#" className="footer-container-icon"><i className="far fa-comment flip" /></a>
             <a href="#" className="footer-container-icon"><i className="fas fa-exchange-alt "></i> 4</a>
             <a href="#" className="footer-container-icon"><i className="far fa-heart "></i> 6</a>
             <a href="#" className="footer-container-icon"><i className="far fa-envelope "></i></a>
