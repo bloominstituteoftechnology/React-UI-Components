0bbe02cf87c1b5026db86dcad52bdecfafe42206
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-23 18:18:52 -0500

    continued minor styling tweaks

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,9 +3,9 @@ import './Header.css';
 
 const HeaderTitle = () => {
     return <span className='custom-header-title'>
-    <span>Lambda School</span>
-    <span className='custom-span'>@LambdaSchool</span>
-    <span className='custom-span'>• 26 jan</span>
+    <h4 className='custom-h4'>Lambda School</h4>
+    <span className='custom-span'> @LambdaSchool </span>
+    <span className='custom-span'> • 26 jan</span>
     </span>
 }
 
