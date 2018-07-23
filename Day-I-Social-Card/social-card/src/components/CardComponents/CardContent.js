51bfaf7ec29a347d77e0b5a09192422b83d29dea
AI2EJAY <JHammett1314@gmail.com>
2018-07-23 18:47:51 -0500

    Most done

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,12 +3,17 @@ import './Card.css';
 
 const CardContent = () => {
     return (
+        <div className = "content-wrapper">
+        <h2 className ="content-title"> Here's a title </h2>
         <p className="card-content">
             Et adipiscing lacus cursus cubilia, erat felis. Facilisis 
             curae; risus pharetra egestas, quisque pharetra.
              A magnis adipiscing tincidunt nascetur, tellus mi dolor etiam, 
              suspendisse luctus cras mollis pharetra id. Tortor.
              </p>
+             <span> Link </span>
+             </div>
+
         );
     };
     
