3012508aa86e3d56a07dda04bc6b2e8587403426
Trevor <trevorfehrman@gmail.com>
2018-07-23 15:59:12 -0700

    Moment.js implemented

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Header.css";
+import moment from "moment";
 
 const HeaderTitle = () => {
 	return (
 		<div className='header__title'>
 			<span className='header__title--bold'> Lambda School </span>
-			<span className='header__title--grey'> @LambdaSchool * 23 jul</span>
+			<span className='header__title--grey'>{ moment().format("D MMM").toLowerCase() }</span>
 		</div>
 	);
 };
