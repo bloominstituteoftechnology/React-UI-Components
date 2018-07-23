94ce66ec9523b894616918fb2ee633ceda166cb3
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-23 12:50:40 -0700

    add header component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = () => {
+	return (
+		<div className="headerContent">
+			<p>Text here</p>
+		</div>
+	);
+}
+
+export default HeaderContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+	return (
+		<div className="headerTitle">
+			<p><strong>Lambda School</strong> <small>@LambdaSchool 26 Jan</small></p>
+		</div>
+	);
+}
+
+export default HeaderTitle;
