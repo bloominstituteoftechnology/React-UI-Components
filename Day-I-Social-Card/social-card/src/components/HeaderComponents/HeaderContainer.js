44979028ae9fe7c324605ac756ea81f221c00e0d
William <kwontaewoongah@gmail.com>
2018-07-23 14:00:20 -0700

    finished part I unstyled

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer=()=><div><div><ImageThumbnail/></div><div><HeaderContent/><HeaderTitle/></div></div>;
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle=()=><h2><strong>Lambda School</strong> @LambdaSchool · 23 July</h2>
+
+export default HeaderTitle;
