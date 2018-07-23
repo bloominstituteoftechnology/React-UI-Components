a9fc82f30930fea5876a4ead42c672524f7cfd1e
Grant Reighard <grant.reighard@gmail.com>
2018-07-23 16:09:01 -0400

    Finished with header components.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (<div className="headerTitle">Lambda School <span>@LambdaSchool &bull; 26 jan</span></div>);
+}
+
+export default HeaderTitle;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 
 const ImageThumbnail = () => {
-    return <img src="https://ibin.co/3whrpKSBbZ81.png" alt="Lambda School Logo"/>;
+    return <img src="https://ibin.co/3whrpKSBbZ81.png" alt="Lambda School Logo" width="30" className="thumbnail"/>;
 }
 
 export default ImageThumbnail;
