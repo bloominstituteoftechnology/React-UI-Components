499f21ac59f9078b32de356a54f17464fda035b7
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-23 17:52:42 -0500

    got link to work

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,10 +4,14 @@ import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
 const CardContainer = () => {
-    return <div className="card-container">
+    return <a href='https://reactjs.org/' target="_blank" className='custom-card-img'><div className="card-container">
     <CardBanner />
     <CardContent />
-    </div>;
+    </div></a>;
 }
+
+// const card = document.querySelector('.card-container');
+
+// card.addEventListener('click', )
 
 export default CardContainer;
