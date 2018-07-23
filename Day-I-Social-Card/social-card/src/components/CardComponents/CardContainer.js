5735b46e21b32f67f1472efc6b43686e32276007
kamalnrf <kamalnrf@hotmail.com>
2018-07-24 02:39:58 +0530

    Added CardContainer

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const content = "Some content"
+function CardContainer (props){
+    return (
+        <div class="cardContainer">
+            <CardBanner url="https://ibin.co/3wnC6SgIOJud.png" alt="banner"/>
+            <CardContent content={content} />
+        </div>
+    )
+}
+
+export default CardContainer
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent(props) {
+    return (
+        <div class="cardContent">
+            <p>{props.content}</p>
+        </div>
+    )
+}
+
+export default CardContent
