81d8b9b6c82adf4baa82315570a5a805c64c6b68
jaredrcooper <jaredrcoopet@gmail.com>
2018-07-23 15:49:24 -0400

    created components/subcomponents and imported together

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const cardContainer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default cardContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import headerTitle from './HeaderTitle';
+import headerContent from './HeaderContent';
+import imageThumbnail from './ImageThumbnail'
 import './Header.css';
+
+const headerContainer = () => {
+    return (
+        <div>
+            <ImageThumbnail />
+            <headerTitle />
+            <headerContent />
+        </div>
+    )
+}
+
+export default headerContainer;
