e0a64135d70648de7276e480ada335fffa5840f4
Ashwin <aswinjeganath@gmail.com>
2018-07-24 03:19:58 +0530

    Complete card component
     - Add image to banner,
     - Add text to content area
     - Style the image and content

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
     <div className="container mt-5">
       <HeaderContainer />
+      <div className="card-wrapper">
+        <div className="card-section">
+          <a href="https://reactjs.org" target="_blank" className="card-link">
+            <CardContainer />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => (
+  <img
+    src="https://ibin.co/3wnC6SgIOJud.png"
+    alt=""
+    className="Card__banner img-fluid"
+  />
+);
+
+export default CardBanner;
