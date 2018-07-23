396efbfbcb63a8b357e98356ba231d261b64493c
Trevor <trevorfehrman@gmail.com>
2018-07-23 15:48:01 -0700

    Footer stretch complete

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
 import CardContainer from "./components/CardComponents/CardContainer";
+import FooterContainer from "./components/FooterComponents/FooterContainer";
 
 const App = () => {
 	return (
 		<div className="appContainer">
 			<HeaderContainer />
 			<CardContainer />
+			<FooterContainer />
 		</div>
 	);
 };
