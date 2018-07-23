fb587bbe7f55e77b9b7feaee2e0531fc8b06e8b1
szincone <sawyerzincone@gmail.com>
2018-07-23 17:57:30 -0600

    added fa icons to footer and linked to app.js

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import CardContainer from "./components/CardComponents/CardContainer";
 import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
-
+import Footer from "./components/FooterComponents/Footer";
 const App = () => {
   return (
     <div className="app-container">
       <HeaderContainer />
       <CardContainer />
+      <Footer />
     </div>
   );
 };
--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,7 +4,12 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-wrapper">
-      
+      <i className="fa fa-comment-o" />
+      <i className="fa fa-refresh" />
+      <i className="fa fa-heart-o" />
+      <i className="fa fa-envelope-o" />
     </div>
-  )
-}
+  );
+};
+
+export default Footer;
