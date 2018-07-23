e725b66875a768b736b1feeeb246f3f5a07c9baf
kamalnrf <kamalnrf@hotmail.com>
2018-07-24 02:37:03 +0530

    Added html structure for header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle (props){
+    return <div class="headerTitle">
+        <h1>{props.title}</h1>
+        <div class="headerUserName">{props.userName} . {props.timeStamp}</div>
+    </div>
+}
+
+export default HeaderTitle
