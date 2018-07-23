d79c42662c3c7da652701709842187de53c83313
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-07-23 18:22:16 -0400

    finished MVP moving on to stretch

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
     <div className="card-container">
-   {/* <HeaderContainer /> */}
+   <HeaderContainer />
    <CardContainer />
   </div>
   );
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import HeaderContent from './HeaderContent';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+return(
+<div className="header-title">Lambda School
+<span>@LambdaSchool 
+    &bull; 26 jan
+    </span>
+    <HeaderContent />
+    </div>);
+   
+};
+
+export default HeaderTitle;
