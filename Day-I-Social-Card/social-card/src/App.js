ecdf52f3791605a6d00b2cae5256d7fbb2e8cc0c
LizBaker <lizbaker.pdx@gmail.com>
2018-07-23 16:37:00 -0700

    Added footer and font awesome for icons -can't get the retweet/circle arrow icon to show up properly, maybe a browser thing

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import CardContainer from './components/CardComponents/CardContainer';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import Footer from './components/FooterComponents/Footer'
 
 const App = () => {
   return (
     <div className='card-body'>
       <HeaderContainer/>
       <CardContainer/>
+      <Footer/>
     </div>
   );
 };
