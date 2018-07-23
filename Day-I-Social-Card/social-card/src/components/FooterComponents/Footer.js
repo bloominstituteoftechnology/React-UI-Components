eeba8d00ecb893451a5ed367518601d1f5980221
codejoncode <jonathanjholloway@gmail.com>
2018-07-23 18:34:52 -0500

    got the like button working

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -22,20 +22,34 @@ import './Footer.css';
 class Footer extends React.Component {
     constructor(props){
         super(props);
-        this.state = {sync: 0}
+        this.state = {sync: 0, like:0}
         this.handleSyncClick = this.handleSyncClick.bind(this); 
+        this.handleLickClick = this.handleLikeClick.bind(this);
     }
+    // handleSyncClick() {
+    //     let syncCount = this.state.sync +1;
+    //     console.log(syncCount);  
+    //     this.setState({sync : syncCount});  
+    // }
+
     handleSyncClick() {
-        let syncCount = this.state.sync +1;
-        console.log(syncCount);  
-        this.setState({sync : syncCount});  
+        this.state.sync +=1;
+        // console.log(syncCount);  
+        this.forceUpdate(); 
     }
+
+    handleLikeClick(){
+        this.state.like ++;
+        this.forceUpdate();
+
+    }
+
     render(){
         return (
             <div className="footer">
                 <i className="far fa-comment">{this.state.comment}</i>
-                <i onClick={this.handleSyncClick} className="fas fa-sync">{this.state.sync}</i>
-                <i className="far fa-heart"></i>
+                <i onClick={this.handleSyncClick} className="fas fa-sync">  {this.state.sync}</i>
+                <i onClick={this.handleLickClick}className="far fa-heart">  {this.state.like}</i>
                 <i className="far fa-envelope"></i>
             </div>
             );
