752fe5e3030ef8eb3843a98f11b4c2dbaf4587f3
Kelley Henican <kelleyhenican2@gmail.com>
2018-07-23 16:46:46 -0500

    added image thumbnail component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail'
+
+const HeaderContainer = () => { 
+    return (
+   <ImageThumbnail />
+    )
+}
+
+
+
+export default HeaderContainer; 
