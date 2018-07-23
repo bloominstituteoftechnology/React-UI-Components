c8fddff10fb62429ec3b7145f45b1e41967819c8
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-07-23 15:54:54 -0700

    Added the Lambda Logo to the top

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return  <div classname="logo-div">
+                <img classname="lambda-logo" src="https://ibin.co/3whrpKSBbZ81.png"></img>
+            </div>;
+};
+
+export default ImageThumbnail;
