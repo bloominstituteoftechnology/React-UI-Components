6e01e374188fe96cddafb0703383a72863f591d3
codejoncode <jonathanjholloway@gmail.com>
2018-07-23 18:18:55 -0500

    added a class instead of constructor to see if i can get icon to work

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,29 +5,41 @@ import './Footer.css';
 
  
 
-const Footer = () => { 
-    let comment = 0;
-    let sync = 0;
-    function handleCommentClick (comment) {
-        let answer = prompt('enter your comment');
-        if (answer){
-            return comment++;
-        }
-    }
-    
-    function handleSyncClick(sync) {
-        return sync+=1
-    }
-    return (
-    <div className="footer">
-        <i onClick ={prompt('enter comment')}className="far fa-comment">{comment}</i>
-        <i onClick={() => sync++} className="fas fa-sync">{sync}</i>
-        <i className="far fa-heart"></i>
-        <i className="far fa-envelope"></i>
-    </div>
-    );
-}
+// const Footer = () => { 
+//     let comment = 0;
+//     let sync = 0;
+//     return (
+//     <div className="footer">
+//         <i onClick ={prompt('enter comment')}className="far fa-comment">{comment}</i>
+//         <i onClick={() => sync++} className="fas fa-sync">{sync}</i>
+//         <i className="far fa-heart"></i>
+//         <i className="far fa-envelope"></i>
+//     </div>
+//     );
+// }
 
+
+class Footer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {comment:0, sync: 0}
+        this.handleSyncClick = this.handleSyncClick.bind(this); 
+    }
+    handleSyncClick() {
+        return this.setState.sync = this.state.sync + 1; 
+    }
+    render(){
+        return (
+            <div className="footer">
+                <i onClick ={prompt('enter comment')}className="far fa-comment">{this.state.comment}</i>
+                <i onClick={this.handleSyncClick()} className="fas fa-sync">{this.state.sync}</i>
+                <i className="far fa-heart"></i>
+                <i className="far fa-envelope"></i>
+            </div>
+            );
+    }
+
+}
 
 
 export default Footer; 
