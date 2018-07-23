7564d4a874536925756a39c0897925c43889d690
sca4cs <shannon.atkinson.ca@gmail.com>
2018-07-23 18:47:27 -0400

    card component incomplete

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return <img src="https://ibin.co/3wnC6SgIOJud.png" alt="React Image" className="banner"></img>
+}
+
+export default CardBanner;
