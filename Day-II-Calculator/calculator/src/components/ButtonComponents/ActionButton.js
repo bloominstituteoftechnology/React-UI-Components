23223e083c29171ac2ad61b5dd4e5be88abcab50
codejoncode <jonathanjholloway@gmail.com>
2018-07-24 16:07:58 -0500

    I believe enough is done to get the MVP I want to adjust the sizing and fonts some what though

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -12,15 +12,15 @@ class ActionButton extends React.Component {
     render() {
        return (
              <div className = "operators">
-                <div className="operation"><i class="fas fa-divide"></i></div>
+                <div className="operation"><i className="fas fa-divide"></i></div>
                  
-                <div className="operation"><i class="fas fa-times"></i></div>
+                <div className="operation"><i className="fas fa-times"></i></div>
                  
-                <div className="operation"><i class="fas fa-minus"></i></div>
+                <div className="operation"><i className="fas fa-minus"></i></div>
                  
-                <div className="operation"><i class="fas fa-plus"></i></div>
+                <div className="operation"><i className="fas fa-plus"></i></div>
                  
-                <div className="operation"><i class="fas fa-equals"></i></div>
+                <div className="operation"><i className="fas fa-equals"></i></div>
                  
              </div>
         ); 
