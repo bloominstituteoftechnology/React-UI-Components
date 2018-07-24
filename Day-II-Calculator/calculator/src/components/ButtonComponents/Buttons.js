fbc757054f64ced952da63f24c017e7ff4db3ac8
Trevor <trevorfehrman@gmail.com>
2018-07-24 11:41:03 -0700

    Initial Commit

---
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/Buttons.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Button.css";
+
+const Buttons = () => {
+    return (
+        <div className="buttonsContainer">
+        </div>
+    );
+}
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = () => {
+    return (
+        <div classname="display">0</div>
+    );
+};
+
+export default CalculatorDisplay;
