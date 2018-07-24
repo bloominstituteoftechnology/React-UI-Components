a42235ea1971ab108224147f3fa0603551dc9891
LaikaFusion <newrad4@gmail.com>
2018-07-24 17:22:59 -0400

    Completed look a like of calculator, moving to stretch

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './App.css';
 import NumberButton from "./components/ButtonComponents/NumberButton";
+import Display from "./components/DisplayComponents/CalculatorDisplay";
+
 
 const PadMaker = () =>{
   const padText = [{text:"clear", style:"action"},{text:"/", style:"functionButton"},{text:"7", style:"numberButton"},{text:"8", style:"numberButton"},{text:"9", style:"numberButton"},{text:"X", style:"functionButton"},{text:"6", style:"numberButton"},{text:"5", style:"numberButton"},{text:"4", style:"numberButton"},{text:"-", style:"functionButton"},{text:"1", style:"numberButton"},{text:"2", style:"numberButton"},{text:"3", style:"numberButton"},{text:"+", style:"functionButton"},{text:"0", style:"action"},{text:"=", style:"functionButton"}];
-  return padText.map(element => {
-      return (<NumberButton buttonContent={element.text} buttonStyle={element.style} />)
+  return padText.map((element, index) => {
+      return (<NumberButton key={index} uni={index} buttonContent={element.text} buttonStyle={element.style} />)
   });
 }
 
 const App = () => {
   return (
     <div>
+      
+      <Display text= "0" />
+
       <div className="calcPad">
+      
       <PadMaker />
     </div>
     </div>
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 export const NumberButton = (props) =>{
-    return (<div className={props.buttonStyle}><span className="buttonText">{props.buttonContent}</span></div>);
+    return (<div id={props.uni} className={props.buttonStyle}><span className="buttonText">{props.buttonContent}</span></div>);
 }
 
 export default NumberButton;
