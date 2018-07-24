90cb97d6041e6d54d601bb7c1e240de7f6645754
tulf <timzulf@gmail.com>
2018-07-24 18:28:12 -0400

    getting there just uggly

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -8,32 +8,32 @@ import Container from './components/ButtonComponents/Container';
 const App = () => {
   return (
     <div className = "container text-center" id = "calc">
-      <Display />
+      <Display className= "display" />
           <Container className="clear">
             <Action className="action" text="clear" />
-            <Number color="operator" text="/" />
+            <Number color="col operator" text="/" />
           </Container>
           <Container>
             <Number color="num" text="7" />
             <Number color="num" text="8" />
             <Number color="num" text="9" />
-            <Number color="operator" text="X" />
+            <Number color="col operator" text="X" />
           </Container>
           <Container>
             <Number color="num" text="4" />
             <Number color="num" text="5" />
             <Number color="num" text="6" />
-            <Number color="operator" text="-" />
+            <Number color="col operator" text="-" />
           </Container>
           <Container>
             <Number color="num" text="1" />
             <Number color="num" text="2" />
             <Number color="num" text="3" />
-            <Number color="operator" text="+" />
+            <Number color="col operator" text="+" />
           </Container>
           <Container>
             <Action className="action" text="0" />
-            <Number color="operator" text="=" />
+            <Number color="col operator" text="=" />
           </Container>
     </div>
   );
