40603d2af36d55c0468b341d3c2f5cbdb4b9492a
szincone <sawyerzincone@gmail.com>
2018-07-24 14:12:36 -0600

    rough version of number buttons styled, and imported

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
+
+
 const App = () => {
   return (
     <div className="app-wrapper">
-      <CalculatorDisplay total="0"/>
+      <div className="calc-wrapper">
+        <CalculatorDisplay total="0"/>
+        <div className="numbersButtonsDiv">
+          
+          <NumberButton numberLabel="9" />
+          <NumberButton numberLabel="8" />
+          <NumberButton numberLabel="7" />
+          <NumberButton numberLabel="6" />
+          <NumberButton numberLabel="5" />
+          <NumberButton numberLabel="4" />
+          <NumberButton numberLabel="3" />
+          <NumberButton numberLabel="2" />
+          <NumberButton numberLabel="1" />
+          <NumberButton numberLabel="0" extraStyles="zeroNumberStyles" />
+        </div>
+      </div>
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+  return (
+    <div className={`numberButton-div ${props.extraStyles}`}>
+      <p>{props.numberLabel}</p>
+    </div>
+  );
+};
+
+export default NumberButton;
