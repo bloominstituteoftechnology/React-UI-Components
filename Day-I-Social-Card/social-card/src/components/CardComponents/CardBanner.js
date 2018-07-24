039607a606633a20516ebd9b518ee92a4fff8aa2
Stephanie Banks <thisgirlcancode@gmail.com>
2018-07-24 17:38:25 -0500

    added card banner

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './Card.css';
-
+import React from "react";
+import "./Card.css";
 
 const CardBanner = () => {
-    return (<img className='image' src="https://ibin.co/3wnC6SgIOJud.png" alt="react logo" />)
-}
-
+    return (
+        <a className="banner-container-wrapper" href="https://reactjs.org">
+            <img
+                alt="react logo"
+                className="banner-img"
+                src="https://ibin.co/3wnC6SgIOJud.png"
+            />
+        </a>
+    );
+};
 
 export default CardBanner;
