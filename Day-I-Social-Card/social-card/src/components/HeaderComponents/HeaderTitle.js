90a190b5c2a0ec7e1f2bc9f6354f2d352527770c
Charles Cortinas <crcortinas@gmail.com>
2018-07-24 02:48:06 -0500

    Header
    Header built and styled

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div className='header-title'>
+      Lambda School
+      <div className='handle'>
+        @LambdaSchool
+      </div>
+      <div className='date'>
+        26 Jan
+      </div>
+    </div>
+  )
+}
+
+export default HeaderTitle;
