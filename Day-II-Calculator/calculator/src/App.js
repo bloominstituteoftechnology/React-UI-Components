1c452bd5c305edf0c68d424cecb11c6d7c248397
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-24 16:53:04 -0500

    finished with CSS
    working on finding the icons for the signs, and also the font that is in the calculator currently.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -9,21 +9,21 @@ const App = () => {
     <div className="calculator">
       <CalculatorDisplay text="0" />
       <ActionButton text="Clear"/>
-      <NumberButton text="/" />
+      <NumberButton extraClass ="signs" text="/" />
       <NumberButton text="1" />
       <NumberButton text="2" />
       <NumberButton text="3" />
-      <NumberButton text="X" />
+      <NumberButton extraClass ="signs" text="X" />
       <NumberButton text="4" />
       <NumberButton text="5" />
       <NumberButton text="6" />
-      <NumberButton text="-" />
+      <NumberButton extraClass ="signs" text="-" />
       <NumberButton text="7" />
       <NumberButton text="8" />
       <NumberButton text="9" />
-      <NumberButton text="+" />
+      <NumberButton extraClass ="signs" text="+" />
       <ActionButton text="0" />
-      <NumberButton text="=" />
+      <NumberButton extraClass ="signs" text="=" />
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Button.css';
 
-const NumberBtnSquare =props=> <div className={`square`}>{props.text}</div>; 
+const NumberBtnSquare =props=> <div className={`square ${props.extraClass}`}>{props.text}</div>; 
 const NumberBtnSquareContainer = props =>{
     return(
-        <NumberBtnSquare text ={props.text}/>
+        <NumberBtnSquare extraClass={props.extraClass} text ={props.text}/>
     )
 }
 export default NumberBtnSquareContainer; 
