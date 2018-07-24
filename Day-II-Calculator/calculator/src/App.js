461e49dae4f8e280b1932171f3377150d091fbfe
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-07-24 14:56:09 -0600

    Calculator progress. Calculator formatted.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
-const App = () => {
-  return (
+class App extends React.Component {
+  render() {
+    return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      <CalculatorDisplay />
+      <div className="button-container">
+      <ActionButton className="clear-text" text="clear" />
+      <NumberButton text="&divide;" />
+      </div>
+      <div className="button-container">
+      <NumberButton text="7" />
+      <NumberButton text="8" />
+      <NumberButton text="9" />
+      <NumberButton text="&times;" />
+      </div>
+      <div className="button-container">
+      <NumberButton text="7" />
+      <NumberButton text="8" />
+      <NumberButton text="9" />
+      <NumberButton text="&minus;" />
+      </div>
+      <div className="button-container">
+      <NumberButton text="4" />
+      <NumberButton text="5" />
+      <NumberButton text="6" />
+      <NumberButton text='+' />
+      </div>
+      <div className="button-container">
+      <ActionButton className="clear-text" text="0" />
+      <NumberButton text="=" />
+      </div>
     </div>
-  );
-};
+    );
+  }
+}   
 
 export default App;
