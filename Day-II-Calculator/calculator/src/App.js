d4c9608b08a93c3d8e18e3aa4388b5637f53c9de
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-07-24 18:24:48 -0400

    some updates, on stretch

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -5,33 +5,32 @@ import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
-  // render() {
     return (
   <div className="calculator-wrapper">
   <CalculatorDisplay />
   <ActionButton className="three-text" text="clear" />
-  <NumberButton bgColor="red-button" text="/"/>
+  <NumberButton bgColor="red-button" text="&#247;"/>
 
   <NumberButton bgColor="num-button" text="7" />
   <NumberButton bgColor="num-button" text="8" />
   <NumberButton bgColor="num-button" text="9" />
-  <NumberButton bgColor="num-button" text="X" />
+  <NumberButton bgColor="red-button" text="&#215;" />
 
   <NumberButton bgColor="num-button" text="4" />
   <NumberButton bgColor="num-button" text="5" />
   <NumberButton bgColor="num-button" text="6" />
-  <NumberButton bgColor="num-button" text="-" />
+  <NumberButton bgColor="red-button" text="-" />
 
   <NumberButton bgColor="num-button" text="1" />
   <NumberButton bgColor="num-button" text="2" />
   <NumberButton bgColor="num-button" text="3" />
-  <NumberButton bgColor="num-button" text="+" />
+  <NumberButton bgColor="red-button" text="+" />
 
   <ActionButton className="three-text" text="0" />
   <NumberButton bgColor="red-button" text="=" />
   </div>
       );
-    // }
+    
   }
 
 export default App;
