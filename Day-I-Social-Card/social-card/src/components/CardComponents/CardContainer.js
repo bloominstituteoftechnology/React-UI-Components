22d23225321570535776eebab48abf63d43bbc7e
Stephanie Banks <thisgirlcancode@gmail.com>
2018-07-24 17:47:19 -0500

    imported cardcontent and banner

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './Card.css';
-
+import React from "react";
+import "./Card.css";
+import CardContent from "./CardContent";
+import CardBanner from "./CardBanner";
 
 const CardContainer = () => {
     return (
-        <div className={'card-container'}><CardContent />
-            <div className={'image'}><CardBanner />
-            </div>
+        <div className="banner-wrapper">
+            <CardBanner />
+            <CardContent />
         </div>
     );
 };
