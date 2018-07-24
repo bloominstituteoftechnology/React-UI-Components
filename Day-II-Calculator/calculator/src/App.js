7b3c0b1d1c0d3f64c6a218c142ab31885812bc2a
William <kwontaewoongah@gmail.com>
2018-07-24 14:55:30 -0700

    finished calculatordisplay component sans styling

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -2,27 +2,30 @@ import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton.js';
 import ActionButton from './components/ButtonComponents/ActionButton.js';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
 import './components/ButtonComponents/Button.css';
 import './components/DisplayComponents/Display.css';
+
 const App = () => {
   return (
     <div>
-   <NumberButton text='1' buttonStyle='numberbutton' />
-   <NumberButton text='2' buttonStyle='numberbutton' />
-   <NumberButton text='3' buttonStyle='numberbutton' />
-   <NumberButton text='4' buttonStyle='numberbutton' />
-   <NumberButton text='5' buttonStyle='numberbutton' />
-   <NumberButton text='6' buttonStyle='numberbutton' />
-   <NumberButton text='7' buttonStyle='numberbutton' />
-   <NumberButton text='8' buttonStyle='numberbutton' />
-   <NumberButton text='9' buttonStyle='numberbutton' />
-   <NumberButton text='÷' buttonStyle='operatorbutton' />
-   <NumberButton text='×' buttonStyle='operatorbutton' />
-   <NumberButton text='+' buttonStyle='operatorbutton' />
-   <NumberButton text='-' buttonStyle='operatorbutton' />
-   <NumberButton text='=' buttonStyle='operatorbutton' />
-   <ActionButton text='0' />
-   <ActionButton text='clear' />
+      <CalculatorDisplay />
+      <NumberButton text='1' buttonStyle='numberbutton' />
+      <NumberButton text='2' buttonStyle='numberbutton' />
+      <NumberButton text='3' buttonStyle='numberbutton' />
+      <NumberButton text='4' buttonStyle='numberbutton' />
+      <NumberButton text='5' buttonStyle='numberbutton' />
+      <NumberButton text='6' buttonStyle='numberbutton' />
+      <NumberButton text='7' buttonStyle='numberbutton' />
+      <NumberButton text='8' buttonStyle='numberbutton' />
+      <NumberButton text='9' buttonStyle='numberbutton' />
+      <NumberButton text='÷' buttonStyle='operatorbutton' />
+      <NumberButton text='×' buttonStyle='operatorbutton' />
+      <NumberButton text='+' buttonStyle='operatorbutton' />
+      <NumberButton text='-' buttonStyle='operatorbutton' />
+      <NumberButton text='=' buttonStyle='operatorbutton' />
+      <ActionButton text='0' />
+      <ActionButton text='clear' />
    </div>
   );
 };
