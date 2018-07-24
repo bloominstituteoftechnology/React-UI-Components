5fae038f0c23e0d31c51606a39601e8e032afe20
szincone <sawyerzincone@gmail.com>
2018-07-24 16:46:10 -0600

    cleaned up and last airtable push

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import './App.css';
-import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
-import NumberButton from './components/ButtonComponents/NumberButton';
-import ActionButton from './components/ButtonComponents/ActionButton';
-
+import React from "react";
+import "./App.css";
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import NumberButton from "./components/ButtonComponents/NumberButton";
+import ActionButton from "./components/ButtonComponents/ActionButton";
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <div className="calc-wrapper">
-        <CalculatorDisplay total="0"/>
+        <CalculatorDisplay total="0" />
         <ActionButton actionButtonLabel="clear" extraStyles="clearWidth" />
         <ActionButton actionButtonLabel="&divide;" />
         <div className="numbersButtonsDiv">
-          
           <NumberButton numberLabel="9" />
           <NumberButton numberLabel="8" />
           <NumberButton numberLabel="7" />
@@ -27,12 +25,14 @@ const App = () => {
         </div>
 
         <div className="actionButtonRow">
-          <ActionButton actionButtonLabel="&times;" extraStyles="actionButtonWidth"/>
-          <ActionButton actionButtonLabel="-" extraStyles="actionButtonWidth"/>
-          <ActionButton actionButtonLabel="+" extraStyles="actionButtonWidth"/>
-          <ActionButton actionButtonLabel="=" extraStyles="actionButtonWidth"/>
+          <ActionButton
+            actionButtonLabel="&times;"
+            extraStyles="actionButtonWidth"
+          />
+          <ActionButton actionButtonLabel="-" extraStyles="actionButtonWidth" />
+          <ActionButton actionButtonLabel="+" extraStyles="actionButtonWidth" />
+          <ActionButton actionButtonLabel="=" extraStyles="actionButtonWidth" />
         </div>
-
       </div>
     </div>
   );
