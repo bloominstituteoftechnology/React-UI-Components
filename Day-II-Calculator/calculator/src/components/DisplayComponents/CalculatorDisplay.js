c8c401ae9103076d099d2a2b4dd373825862b77a
sliw <slioweng@gmail.com>
2018-07-25 05:18:35 +0800

    Steven L

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+  return (
+    <div className="display">result</div>
+  );
+}
+
+export default CalculatorDisplay;
