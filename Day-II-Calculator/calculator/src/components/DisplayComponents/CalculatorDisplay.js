06d64ffb39918f5e5dfd27a18bf083b0a77df2ba
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-24 17:41:21 -0500

    prettier working
    downloaded prettier and had to restart my system so that it would work.

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import './Display.css';
-
+import React from "react";
+import "./Display.css";
 
 const CalDisplay = props => <div className={`display`}>{props.text}</div>;
-
-const DisplaySpace = props =>{
-    return(
-        <CalDisplay text={props.text} />
-    )
-}
-export default DisplaySpace; 
+const DisplaySpace = props => {
+  return <CalDisplay text={props.text} />;
+};
+export default DisplaySpace;
