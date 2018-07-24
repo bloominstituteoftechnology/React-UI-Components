7268043edd6cbfc1e857ce21db5ced65a7c3aba7
Arthur Bates <aabatesj@gmail.com>
2018-07-24 16:29:44 -0400

    React II Action buttons
    Action button component

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = () => {
+    return (
+        <div className="action-buttons">
+            <button className="calulator-key key-divide">÷</button>
+            <button className="calulator-key key-multiply">×</button>
+            <button className="calulator-key key-subtract">-</button>
+            <button className="calulator-key key-add">+</button>
+            <button className="calulator-key key-equals">=</button>
+        </div>
+    )
+};
+
+export default ActionButton;
