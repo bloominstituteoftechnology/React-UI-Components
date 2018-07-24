58e7c9fe315b836e0eb9951d2d78da2d1f66f3e1
Sean <sean47900@gmail.com>
2018-07-24 17:02:56 -0500

    action button working, CSS complete

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -3,10 +3,10 @@ import './Button.css';
 
 const ActionButton = (props) => {
     return (
-    <div className = "actionButton">
-        <div>{props.action}</div>
-    </div>
-    )
+        <button className={props.buttonStyle}>
+            {props.text}
+        </button>
+    );
 }
 
 export default ActionButton;
