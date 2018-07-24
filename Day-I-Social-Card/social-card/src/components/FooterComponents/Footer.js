21165c985350c1ed3be42b6a6d8d4e3bf74170e8
Erik Kimsey <erik@erikkimsey.com>
2018-07-23 22:25:28 -0400

    Added icons to footer. Gave retweet icon click functionality

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './Footer.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComment, faRetweet, faHeart, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faComment, faRetweet, faHeart, faEnvelope)
+
+function retweet(){
+    console.log("retweet");
+    
+}
 
 const Footer = () => {
+
     return (
-        <div className="footer">footer</div>
+        <div className="footer">
+             <FontAwesomeIcon icon={faComment} />
+             <FontAwesomeIcon icon={faRetweet} onClick={retweet}/>
+             <FontAwesomeIcon icon={faHeart} />
+             <FontAwesomeIcon icon={faEnvelope} />
+        </div>
     )
 };
 
