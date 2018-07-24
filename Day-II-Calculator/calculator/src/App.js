8a5615a510eafe11751da233fd9861454f7a1ba6
sca4cs <shannon.atkinson.ca@gmail.com>
2018-07-24 16:30:13 -0400

    calculator component section

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calc-container">
+      <CalculatorDisplay />
+      
+      <div className="white-button">
+        <ActionButton extraStyles="rectangular-btn" text="clear" />
+      
+        <NumberButton text="1" extraStyles="square-button" />
+        <NumberButton text="2" extraStyles="square-button" />
+        <NumberButton text="3" extraStyles="square-button" />
+        <NumberButton text="4" extraStyles="square-button" />
+        <NumberButton text="5" extraStyles="square-button" />
+        <NumberButton text="6" extraStyles="square-button" />
+        <NumberButton text="7" extraStyles="square-button" />
+        <NumberButton text="8" extraStyles="square-button" />
+        <NumberButton text="9" extraStyles="square-button" />
+
+        <ActionButton extraStyles="rectangular-btn" text="0" />
+      </div>
+
+      <div className="red-button">
+        <NumberButton text="/" extraStyles="square-button" />
+        <NumberButton text="x" extraStyles="square-button" />
+        <NumberButton text="-" extraStyles="square-button" />
+        <NumberButton text="+" extraStyles="square-button" />
+        <NumberButton text="=" extraStyles="square-button" />
+      </div>
+
     </div>
   );
 };
 
 export default App;
-//test comment
+
