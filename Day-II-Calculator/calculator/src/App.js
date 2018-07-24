35a71c6b57795173c1784641b732f492f7f3ff7a
codejoncode <jonathanjholloway@gmail.com>
2018-07-24 16:14:56 -0500

    formated the app component into a class and i create a total property for the state object

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -6,13 +6,33 @@ import NumberButton from './components/ButtonComponents/NumberButton';
 
 
 
-const App = () => {
-  return (
-    <div className = "main-div">
-      <CalculatorDisplay />
-      <NumberButton one="1" two="2" three="3" four="4" five="5" six="6" seven = "7" eight = "8" nine = "9" zero = "0" clear="clear"/>
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div className = "main-div">
+//       <CalculatorDisplay />
+//       <NumberButton one="1" two="2" three="3" four="4" five="5" six="6" seven = "7" eight = "8" nine = "9" zero = "0" clear="clear"/>
+//     </div>
+//   );
+// };
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {total: 0}
+  }
+
+
+
+  render(){
+
+    return (
+      <div className = "main-div">
+        <CalculatorDisplay />
+        <NumberButton one="1" two="2" three="3" four="4" five="5" six="6" seven = "7" eight = "8" nine = "9" zero = "0" clear="clear"/>
+      </div>
+    );
+
+  }
+}
 
 export default App;
