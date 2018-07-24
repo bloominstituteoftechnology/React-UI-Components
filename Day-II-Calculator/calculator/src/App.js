a6f6db8c2a6eba76c086c7aa6523c63cd2964cfd
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-24 16:43:07 -0500

    completed all layout and js elements in basic form

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -2,19 +2,36 @@ import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
     <div className="calculator-container">
       <CalculatorDisplay />
       <div>
-        <NumberButton buttonStyle='operator' symbol='÷' />
+        <ActionButton content='clear' />
         <NumberButton buttonStyle='operator' symbol='÷' />
       </div>
       <div>
-        <NumberButton buttonStyle='digit' symbol='÷' />
-        <NumberButton buttonStyle='digit' symbol='÷' />
-        <NumberButton buttonStyle='digit' symbol='÷' />
+        <NumberButton buttonStyle='digit' symbol='7' />
+        <NumberButton buttonStyle='digit' symbol='8' />
+        <NumberButton buttonStyle='digit' symbol='9' />
+        <NumberButton buttonStyle='operator' symbol='x' />
+      </div>
+      <div>
+        <NumberButton buttonStyle='digit' symbol='4' />
+        <NumberButton buttonStyle='digit' symbol='5' />
+        <NumberButton buttonStyle='digit' symbol='6' />
+        <NumberButton buttonStyle='operator' symbol='–' />
+      </div>
+      <div>
+        <NumberButton buttonStyle='digit' symbol='1' />
+        <NumberButton buttonStyle='digit' symbol='2' />
+        <NumberButton buttonStyle='digit' symbol='3' />
+        <NumberButton buttonStyle='operator' symbol='+' />
+      </div>
+      <div>
+        <ActionButton content='0' />
         <NumberButton buttonStyle='operator' symbol='÷' />
       </div>
     </div>
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -3,6 +3,6 @@ import './Button.css';
 
 const NumberButton = props => {
     return <button className={props.buttonStyle}>{props.symbol}</button>
-}
+};
 
 export default NumberButton;
