653c98b5f7a4b9acc0e34bf0ab2fd8ee132548e6
Charles Cortinas <crcortinas@gmail.com>
2018-07-24 03:13:47 -0500

    Card component
    Card content built and styled. MVP goals met

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
     <div className='app-container'>
       <HeaderContainer />
-
+      <CardContainer />
     </div>
   );
 };
