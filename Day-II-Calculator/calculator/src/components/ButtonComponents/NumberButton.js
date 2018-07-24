632f01dc78320d89b00442102d4da6d21b64ad6b
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-07-24 15:30:53 -0500

    Started NumberBtn
    Making two types of buttons that can be accessed

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Button.css';
+
+
+const numberBtnSquare = props =>{
+    return(
+       <div className="square">
+        <numberBtnSquare />
+       </div>
+    )
+}
+
+const numberBtn = props =>{
+    return(
+       <div className="rectangle">
+        <numberBtn />
+       </div>
+    )
+}
