7c3e732e1279d3912f2ab143248eb344c54c41ee
Stephanie Banks <thisgirlcancode@gmail.com>
2018-07-24 17:43:05 -0500

    added header info and date js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from "./HeaderContent";
+
+ const HeaderTitle = () => {
+    const date = new Date(1313564400000)
+    const day = date.getDay();
+    const month = date.getMonth();
+    const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const timeStamp = `${monthArray[month]} ${day}`
+    return (
+        <div>
+        <strong>Lambda School < / strong>
+        <p>@LambdaSchool   - {timeStamp}</p>
+            </div> 
+    )
+
+}
+
+export default HeaderTitle
