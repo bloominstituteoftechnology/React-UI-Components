bc2aaf8dc43bac181c93f00e7601b23c96f55a84
LizBaker <lizbaker.pdx@gmail.com>
2018-07-24 12:28:45 -0700

    Finished MVP -didn't actually separate the operator buttons so I'll go back and do that -pixel close enough -going to replace the operators with font awesome if I can get it to look good

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return (
+        <div className={`display`}>
+        <p>{props.text}</p>
+        </div>
+    )
+}
+
+export default CalculatorDisplay;
+
+
