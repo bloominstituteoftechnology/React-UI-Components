928999474ead48fe17681d3a2f107407cb3e5281
AI2EJAY <JHammett1314@gmail.com>
2018-07-24 17:02:15 -0500

    Visual Done. Not yet functioning.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import './App.css';
+import ActionButton from "./components/ButtonComponents/ActionButton.js"
+import NumberButton from "./components/ButtonComponents/NumberButton.js"
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay.js"
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <CalculatorDisplay />
+    <div className="row first-row">
+    
+    <NumberButton text="clear" extraStyles="long-btn" />
+    <ActionButton text="÷" extraStyles="operator" />
     </div>
+    <div className="row second-row">
+    <NumberButton text="9" extraStyles="number" />
+    <NumberButton text="8" extraStyles="number" />
+    <NumberButton text="7" extraStyles="number" />
+    <ActionButton text="×" extraStyles="operator" />
+    </div>
+    <div className="row third-row">
+    <NumberButton text="6" extraStyles="number" />
+    <NumberButton text="5" extraStyles="number" />
+    <NumberButton text="4" extraStyles="number" />
+    <ActionButton text="-" extraStyles="operator" />
+  </div>
+    <div className="row fourth-row">
+    <NumberButton text="3" extraStyles="number" />
+    <NumberButton text="2" extraStyles="number" />
+    <NumberButton text="1" extraStyles="number" />
+    <ActionButton text="+" extraStyles="operator" />
+  </div>
+    <div className="row fifth-row">
+    <NumberButton text="0" extraStyles="number long-btn" />
+    <ActionButton text="=" extraStyles="operator" />
+
+</div>
+  </div>
   );
 };
 
