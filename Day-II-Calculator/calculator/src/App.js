b0129fd268d337eb7c2f2d5222490ea490d94952
dasgma <dasgrigoma@gmail.com>
2018-07-24 13:32:16 -0700

    Working on calculator

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      <div><ActionButton text="clear" /><NumberButton text="÷" /></div>
+      <div> 
+      <NumberButton text="7" />
+      <NumberButton text="8" />
+      <NumberButton text="9" />
+      <NumberButton text="x" />
+      </div>
+      <div>
+      <NumberButton text="4" />
+      <NumberButton text="5" />
+      <NumberButton text="6" />
+      <NumberButton text="-" />
+      </div>
+      <div>
+      <NumberButton text="1" />
+      <NumberButton text="2" />
+      <NumberButton text="3" />
+      <NumberButton text="+" />
+      </div>
+      <div>
+        <ActionButton text="0" /><NumberButton text="=" />
+      </div>
+     
+      
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Button.css';
+
+
+const ActionButton = props => {
+    return <button className="actionButton" >{props.text}</button>
+}
+
+export default ActionButton;
