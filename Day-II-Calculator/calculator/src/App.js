55487f88b3723ee8110bbabea0aa205a82a737f5
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-24 17:55:40 -0500

    added flexbox and other styles

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -8,31 +8,31 @@ const App = () => {
   return (
     <div className="calculator-container">
       <CalculatorDisplay />
-      <div>
+      <div className='enable-flex'>
         <ActionButton content='clear' />
         <NumberButton buttonStyle='operator' symbol='÷' />
       </div>
-      <div>
+      <div className='enable-flex'>
         <NumberButton buttonStyle='digit' symbol='7' />
         <NumberButton buttonStyle='digit' symbol='8' />
         <NumberButton buttonStyle='digit' symbol='9' />
         <NumberButton buttonStyle='operator' symbol='x' />
       </div>
-      <div>
+      <div className='enable-flex'>
         <NumberButton buttonStyle='digit' symbol='4' />
         <NumberButton buttonStyle='digit' symbol='5' />
         <NumberButton buttonStyle='digit' symbol='6' />
         <NumberButton buttonStyle='operator' symbol='–' />
       </div>
-      <div>
+      <div className='enable-flex'>
         <NumberButton buttonStyle='digit' symbol='1' />
         <NumberButton buttonStyle='digit' symbol='2' />
         <NumberButton buttonStyle='digit' symbol='3' />
         <NumberButton buttonStyle='operator' symbol='+' />
       </div>
-      <div>
+      <div className='enable-flex'>
         <ActionButton content='0' />
-        <NumberButton buttonStyle='operator' symbol='÷' />
+        <NumberButton buttonStyle='operator' symbol='=' />
       </div>
     </div>
   );
