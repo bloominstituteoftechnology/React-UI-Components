18fe8a7e8e0da0745cf0d5bf4838a4c339a18eae
cubsfan46350 <cubsfan.46350@msn.com>
2018-07-24 01:37:57 -0500

    added card component banner image

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardBanner = () => {
+    return (
+        <img className="cardBanner" src="/Day-I-Social-Card/design-files/react_thumbnail.jpeg" />
+    )
+}
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
-import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 const HeaderContainer = () => {
     return (
         <div>
-            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />
         </div>
     )
 }
