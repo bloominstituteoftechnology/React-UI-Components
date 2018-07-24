25002fa06698ebb018ed9f47c5b371f7bb86e28b
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-24 16:42:32 -0500

    done with structure
    Working on styling the calculator

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+
+const NumberBtn = props => <div className={`rectangle`}>{props.text}</div>
+
+const NumberBtnContainer = props =>{
+    return(
+     <NumberBtn text={props.text} />
+    )
+}
+
+export default NumberBtnContainer; 
