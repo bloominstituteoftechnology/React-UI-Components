1300c260bb356c15c66081c5364880012a72f6e4
Alberto-Funk <laptopreneur@gmail.com>
2018-07-24 15:38:47 -0700

    MVP Complete

---
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Button.css';
+import NumberButton from './NumberButton';
+import ActionButton from './ActionButton';
+
+const ButtonContainer = () => {
+  return (
+    <div className="button-container">
+      <div className="button__numbers-container">
+        <div className="button__numbers--clear">
+          <NumberButton custom="btn-lg"  weight="btn-weight" content="clear" />
+        </div>
+        <div className="button__numbers--numerals">
+          <div className="button__numbers--numerals-container">
+            <NumberButton content="7" />
+            <NumberButton content="8" />
+            <NumberButton content="9" />
+          </div>
+          <div className="button__numbers--numerals-container">
+            <NumberButton content="4" />
+            <NumberButton content="5" />
+            <NumberButton content="6" />
+          </div>
+          <div className="button__numbers--numerals-container">
+            <NumberButton content="1" />
+            <NumberButton content="2" />
+            <NumberButton content="3" />
+          </div>
+        </div>
+        <div className="button__numbers--zero">
+          <NumberButton custom="btn-lg" content="0" />
+        </div>
+      </div>
+      <div className="button__actions-container">
+        <ActionButton custom="btn-colored" weight="btn-weight" content="÷" />
+        <ActionButton custom="btn-colored" weight="btn-weight" content="x" />
+        <ActionButton custom="btn-colored" weight="btn-weight" content="-" />
+        <ActionButton custom="btn-colored" weight="btn-weight" content="+" />
+        <ActionButton custom="btn-colored" weight="btn-weight" content="=" />
+      </div>
+    </div>
+  );
+};
+
+
+export default ButtonContainer;
