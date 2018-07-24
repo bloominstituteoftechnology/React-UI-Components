9e2f16b79cbbf21190adbfca62b04ad640f8aea6
Charlie Winslow <cwinslow22@yahoo.com>
2018-07-24 15:34:13 -0500

    built out calculator display and imported into app.js

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return (
+        <div className="calc-display" >{props.text}</div>
+    )
+};
+
+CalculatorDisplay.defaultProps = {
+    text: 'Default Text'
+  };
+
+export default CalculatorDisplay;
