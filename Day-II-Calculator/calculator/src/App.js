3a026446785ea0e2b9ea1c67d48297556fd51d2a
dasgma <dasgrigoma@gmail.com>
2018-07-24 14:03:03 -0700

    Still working on Calculator

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -2,28 +2,30 @@ import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
-    <div>
-      <div><ActionButton text="clear" /><NumberButton text="÷" /></div>
+    <div className="wrap">
+    <CalculatorDisplay />
+      <div><ActionButton text="clear" /><NumberButton text="&#247;" /></div>
       <div> 
       <NumberButton text="7" />
       <NumberButton text="8" />
       <NumberButton text="9" />
-      <NumberButton text="x" />
+      <NumberButton text="&#215;" />
       </div>
       <div>
       <NumberButton text="4" />
       <NumberButton text="5" />
       <NumberButton text="6" />
-      <NumberButton text="-" />
+      <NumberButton text="&#8722;" />
       </div>
       <div>
       <NumberButton text="1" />
       <NumberButton text="2" />
       <NumberButton text="3" />
-      <NumberButton text="+" />
+      <NumberButton text="&#43;" />
       </div>
       <div>
         <ActionButton text="0" /><NumberButton text="=" />
