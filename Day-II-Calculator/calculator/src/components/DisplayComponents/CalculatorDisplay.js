4f26443e5aea6ac86e4e40f1119e65cc70df1873
Arthur Bates <aabatesj@gmail.com>
2018-07-24 17:16:26 -0400

    React II display
    imported components into calculator display

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import './Display.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const CalculatorDisplay = () => {
     return (
         <div className="calculator">
-            <button className="calculator-key key-divide">÷</button>
-            <button className="calculator-key key-multiply">×</button>
-            <button className="calculator-key key-subtract">-</button>
-            <button className="calculator-key key-add">+</button>
-            <button className="calculator-key key-equals">=</button>
+            <div className="calculator-display">0</div>
+            <div className="calculator-keys">
+                <div className="row">
+                    <ActionButton className="key-clear" />
+                    <ActionButton className="key-divide" />
+                </div>
+                <div className="row">
+                    <NumberButton className="key-7" />
+                    <NumbernButton className="key-8" />
+                    <NumberButton className="key-9" />
+                    <ActionButton className="key-multiply" />
+                </div>
+                <div className="row">
+                    <NumberButton className="key-4" />
+                    <NumbernButton className="key-5" />
+                    <NumberButton className="key-6" />
+                    <ActionButton className="key-subtract" />
+                </div>
+                <div className="row">
+                    <NumberButton className="key-1" />
+                    <NumbernButton className="key-2" />
+                    <NumberButton className="key-3" />
+                    <ActionButton className="key-add" />
+                </div>
+                <div className="row">
+                    <NumberButton className="key-0" />
+                    <ActionButton className="key-equals" />
+                </div>
+            </div>
         </div>
     )
 };
