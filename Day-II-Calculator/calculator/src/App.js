4de316fe35ed30e4e8be7b04965efb11ef8dc49b
johnoro <jjcummins102@gmail.com>
2018-07-24 18:34:04 -0400

    Added calculation functionality.
    and I did so stubbornly. Congrats me for caveman-ing it.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
-const App = () => {
-  return (
-    <div>
-      <CalculatorDisplay />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: '',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <CalculatorDisplay total={this.state.total} />
+      </div>
+    );
+  };
 };
 
 export default App;
