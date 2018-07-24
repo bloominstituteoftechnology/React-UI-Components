87ff1cb85d8079ea92456e3067a64696ef7dc6f0
Grant Reighard <grant.reighard@gmail.com>
2018-07-24 16:54:29 -0400

    Refactored App.js to be a classical component.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -5,17 +5,39 @@ import NumberButtonSmall from './components/ButtonComponents/NumberButton';
 import NumberButtonLarge from './components/ButtonComponents/NumberButtonLarge';
 import ActionButton from './components/ButtonComponents/ActionButton';
 
-const App = () => {
-  return (
-    <div className="calculator-layout">
-      <CalculatorDisplay text="0"/>
-      <NumberButtonLarge text="clear" /><ActionButton text="&divide;" />
-      <NumberButtonSmall text="7" /><NumberButtonSmall text="8" /><NumberButtonSmall text="9" /><ActionButton text="x" />
-      <NumberButtonSmall text="4" /><NumberButtonSmall text="5" /><NumberButtonSmall text="6" /><ActionButton text="-" />
-      <NumberButtonSmall text="1" /><NumberButtonSmall text="2" /><NumberButtonSmall text="3" /><ActionButton text="+" />
-      <NumberButtonLarge text="0" /><ActionButton text="=" />
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div className="calculator-layout">
+//       <CalculatorDisplay text="0"/>
+//       <NumberButtonLarge text="clear" /><ActionButton text="&divide;" />
+//       <NumberButtonSmall text="7" /><NumberButtonSmall text="8" /><NumberButtonSmall text="9" /><ActionButton text="x" />
+//       <NumberButtonSmall text="4" /><NumberButtonSmall text="5" /><NumberButtonSmall text="6" /><ActionButton text="-" />
+//       <NumberButtonSmall text="1" /><NumberButtonSmall text="2" /><NumberButtonSmall text="3" /><ActionButton text="+" />
+//       <NumberButtonLarge text="0" /><ActionButton text="=" />
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor(attributes) {
+    super(attributes)
+
+    this.state = attributes.state;
+  }
+
+  render() {
+    return (<div className="calculator-layout">
+    <CalculatorDisplay text="0"/>
+    <NumberButtonLarge text="clear" /><ActionButton text="&divide;" />
+    <NumberButtonSmall text="7" /><NumberButtonSmall text="8" /><NumberButtonSmall text="9" /><ActionButton text="x" />
+    <NumberButtonSmall text="4" /><NumberButtonSmall text="5" /><NumberButtonSmall text="6" /><ActionButton text="-" />
+    <NumberButtonSmall text="1" /><NumberButtonSmall text="2" /><NumberButtonSmall text="3" /><ActionButton text="+" />
+    <NumberButtonLarge text="0" /><ActionButton text="=" />
+    </div>)
+  }
+}
 
 export default App;
+// return displayCalculator();
+
+// export default App;
