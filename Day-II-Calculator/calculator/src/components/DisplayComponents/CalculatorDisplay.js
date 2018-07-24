c655cf05633a60d31b0a854fff066a2a96a51803
rodacoram <rodacoram8@gmail.com>
2018-07-24 15:07:13 -0600

    Rodrigo Acosta-Ramos
    What is a flex-box?

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+// import Calculate from './'
+
+const CalculatorDisplay = ()=> {
+    return (
+        <div className="display">
+            000
+        </div>
+    );
+};
+
+export default CalculatorDisplay;
