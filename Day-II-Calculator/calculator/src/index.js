532e394be5b893f61732788fa4df7789f6630cfb
johnoro <jjcummins102@gmail.com>
2018-07-24 11:53:16 -0400

    Fixed my file strucuture so that the CSS is properly connected.

--- Day-II-Calculator/calculator/src/index.js
+++ Day-II-Calculator/calculator/src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './less/index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
