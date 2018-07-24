25849c4b5e904bc8f88a93582c3e33fe880ca160
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-07-24 17:37:44 -0600

    changed how the app outputs back to normal and from html entities to hex code for math operators.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -4,40 +4,38 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton';
 
-class App extends React.Component {
-  render() {
+const App = () => {
     return (
     <div className="container-wrapper">
       <CalculatorDisplay />
       <div className="button-container">
       <ActionButton className="clear-text" text="clear" />
-      <NumberButton className="red-button" text="&divide;" />
+      <NumberButton className="red-button" text="&#xf7;" />
       </div>
       <div className="button-container">
       <NumberButton text="7" />
       <NumberButton text="8" />
       <NumberButton text="9" />
-      <NumberButton className="red-button" text="&times;" />
+      <NumberButton className="red-button" text="&#xd7;" />
       </div>
       <div className="button-container">
       <NumberButton text="4" />
       <NumberButton text="5" />
       <NumberButton text="6" />
-      <NumberButton className="red-button" text="&minus;" />
+      <NumberButton className="red-button" text="&#x2212;" />
       </div>
       <div className="button-container">
       <NumberButton text="1" />
       <NumberButton text="2" />
       <NumberButton text="3" />
-      <NumberButton className="red-button" text='+' />
+      <NumberButton className="red-button" text='&#x2b;' />
       </div>
       <div className="button-container">
       <ActionButton className="number-text" text="0" />
-      <NumberButton className="red-button" text="=" />
+      <NumberButton className="red-button" text="&#x3d;" />
       </div>
     </div>
     );
-  }
-}   
+  }; 
 
 export default App;
