290b683d960650f716c6e47f0e334cc56073aee8
Grant Reighard <grant.reighard@gmail.com>
2018-07-24 15:51:07 -0400

    Working on calculator display.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButtonSmall = props => {
+    return (<div className="small-number">
+        {props.text}
+    </div>)
+}
+
+export default NumberButtonSmall;
