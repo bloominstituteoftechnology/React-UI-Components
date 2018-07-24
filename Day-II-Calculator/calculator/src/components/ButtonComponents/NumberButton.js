36986f4ac08a458c1394a77b6df053fd187adb19
LaikaFusion <newrad4@gmail.com>
2018-07-24 16:45:05 -0400

    Got Numpad displayed

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+export const NumberButton = (props) =>{
+    return (<div className={props.buttonStyle}><span className="buttonText">{props.buttonContent}</span></div>);
+}
+
+export default NumberButton;
