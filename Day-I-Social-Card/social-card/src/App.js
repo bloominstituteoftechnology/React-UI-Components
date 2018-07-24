1a43563ff937ba7a0d6167a470a19823b6042b7e
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-07-24 10:54:03 -0400

    MVP completed

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
 import CardContainer from './components/CardComponents/CardContainer';
-import CardBanner from './components/CardComponents/CardBanner';
+// import CardBanner from './components/CardComponents/CardBanner';
 import HeadContainer from './components/HeaderComponents/HeaderContainer';
 
 const App = () => {
   return (
     <div className="card-container">
         <HeadContainer/>
-
-        <CardBanner/>
-
+        <CardContainer/>
     </div>
   );
 };
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -5,6 +5,7 @@ import './Card.css';
 const CardBanner = () => (
     <img src="https://ibin.co/3wnC6SgIOJud.png"
     className="card-banner-image"
+    alt="banner"
     />
 );
 
