7815f4a39d1c84101c6dc4ab6124851cd8611fff
sca4cs <shannon.atkinson.ca@gmail.com>
2018-07-24 17:59:08 -0400

    calculator styling

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -9,28 +9,30 @@ const App = () => {
     <div className="calc-container">
       <CalculatorDisplay />
       
-      <div className="white-button">
-        <ActionButton extraStyles="rectangular-btn" text="clear" />
-      
-        <NumberButton text="1" extraStyles="square-button" />
-        <NumberButton text="2" extraStyles="square-button" />
-        <NumberButton text="3" extraStyles="square-button" />
-        <NumberButton text="4" extraStyles="square-button" />
-        <NumberButton text="5" extraStyles="square-button" />
-        <NumberButton text="6" extraStyles="square-button" />
-        <NumberButton text="7" extraStyles="square-button" />
-        <NumberButton text="8" extraStyles="square-button" />
-        <NumberButton text="9" extraStyles="square-button" />
+      <div className="inner-container">
+        <div className="white-button">
+          <ActionButton extraStyles="rectangular-btn" text="clear" />
+        
+          <NumberButton text="1" extraStyles="square-button" />
+          <NumberButton text="2" extraStyles="square-button" />
+          <NumberButton text="3" extraStyles="square-button" />
+          <NumberButton text="4" extraStyles="square-button" />
+          <NumberButton text="5" extraStyles="square-button" />
+          <NumberButton text="6" extraStyles="square-button" />
+          <NumberButton text="7" extraStyles="square-button" />
+          <NumberButton text="8" extraStyles="square-button" />
+          <NumberButton text="9" extraStyles="square-button" />
 
-        <ActionButton extraStyles="rectangular-btn" text="0" />
-      </div>
+          <ActionButton extraStyles="rectangular-btn" text="0" />
+        </div>
 
-      <div className="red-button">
-        <NumberButton text="/" extraStyles="square-button" />
-        <NumberButton text="x" extraStyles="square-button" />
-        <NumberButton text="-" extraStyles="square-button" />
-        <NumberButton text="+" extraStyles="square-button" />
-        <NumberButton text="=" extraStyles="square-button" />
+        <div className="red-button">
+          <NumberButton text="&divide;" extraStyles="square-button-red" />
+          <NumberButton text="&times;" extraStyles="square-button-red" />
+          <NumberButton text="&ndash;" extraStyles="square-button-red" />
+          <NumberButton text="+" extraStyles="square-button-red" />
+          <NumberButton text="=" extraStyles="square-button-red" />
+        </div>
       </div>
 
     </div>
