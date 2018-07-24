7fa580b8dbe10fef1e7e59be840606843d8a7186
LizBaker <lizbaker.pdx@gmail.com>
2018-07-24 16:18:06 -0700

    Added font awesome icons for operators

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -8,21 +8,21 @@ const App = () => {
     <div>
         <CalculatorDisplay text="0"/>
         <NumberButton text="clear" btncolor="white" btnsize="widebtn"/>
-        <NumberButton text="÷" btncolor="red"/>
+        <NumberButton text= {<i class='fas fa-divide'></i>} btncolor="red"/>
         <NumberButton text="7" btncolor="white"/>
         <NumberButton text="8" btncolor="white"/>
         <NumberButton text="9" btncolor="white"/>
-        <NumberButton text="x" btncolor="red"/>
+        <NumberButton text={<i class="fas fa-times"></i>} btncolor="red"/>
         <NumberButton text="4" btncolor="white"/>
         <NumberButton text="5" btncolor="white"/>
         <NumberButton text="6" btncolor="white"/>
-        <NumberButton text="-" btncolor="red"/>
+        <NumberButton text={<i class="fas fa-minus"></i>} btncolor="red"/>
         <NumberButton text="1" btncolor="white"/>
         <NumberButton text="2" btncolor="white"/>
         <NumberButton text="3" btncolor="white"/>
-        <NumberButton text="+" btncolor="red"/>
+        <NumberButton text={<i class="fas fa-plus"></i>} btncolor="red"/>
         <NumberButton text="0" btncolor="white" btnsize="widebtn"/>
-        <NumberButton text="=" btncolor="red"/>
+        <NumberButton text={<i class="fas fa-equals"></i>} btncolor="red"/>
       
     </div>
   );
