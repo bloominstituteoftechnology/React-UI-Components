e85bdf9f0ddaabf81a2f2d1f313b66d15331fd7a
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-07-24 13:46:41 -0600

    Added calculator components and CSS

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import './App.css';
+import ActionButton from  "./components/ButtonComponents/ActionButton"; 
+import NumberButton from './components/ButtonComponents/NumberButton'; 
+import  Display from './components/DisplayComponents/CalculatorDisplay'; 
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className = "container">
+      
+      <div className = "row-1">
+        <Display />
+      </div>
+      <div className = "row-2">
+        <ActionButton action = "clear" />
+        <NumberButton text = "/" buttonStyle = "operator" />
+      </div>
+      <div className = "row-3">
+        <NumberButton text = "7" buttonStyle = "number" />
+        <NumberButton text = "8" buttonStyle = "number" />
+        <NumberButton text = "9" buttonStyle = "number" />
+        <NumberButton text = "*" buttonStyle = "operator" />
+      </div>
+      <div className = "row-4">
+        <NumberButton text = "4" buttonStyle = "number" />
+        <NumberButton text = "5" buttonStyle = "number" />
+        <NumberButton text = "6" buttonStyle = "number" />
+        <NumberButton text = "-" buttonStyle = "operator" />
+      </div>
+      <div className = "row-5">
+        <NumberButton text = "1" buttonStyle = "number" />
+        <NumberButton text = "2" buttonStyle = "number"/>
+        <NumberButton text = "3" buttonStyle = "number"/>
+        <NumberButton text = "+" buttonStyle = "operator"/>
+      </div>
+      <div className = "row-6">
+        <ActionButton action = "0" />
+        <NumberButton text = "=" buttonStyle = "operator" />
+      </div>
+      
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+
+const Display = () => {
+    return (
+    <div className = "display">
+        <div className = "displayNum" >0</div>
+    </div>)
+}
+
+export default Display; 
