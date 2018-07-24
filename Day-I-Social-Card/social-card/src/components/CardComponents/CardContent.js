feead0170a016261cdadffbe339140fcb6a36692
dasgma <dasgrigoma@gmail.com>
2018-07-23 21:30:54 -0700

    Added footer. Left the borders on.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,6 +4,9 @@ import './Card.css';
 const CardContent = () => {
     return <p>
         React makes it painless to create interactive UIs. Design simple views for each state in application.
+        <br />
+        <br />
+        <a className="link" href="htps://www.reactjs.org">reactjs.org</a>
     </p>
     
 }
