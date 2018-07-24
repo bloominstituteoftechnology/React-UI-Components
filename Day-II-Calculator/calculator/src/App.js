df003de6f93bba0754b618c3183d4a0a4ea4e873
sca4cs <shannon.atkinson.ca@gmail.com>
2018-07-24 18:16:55 -0400

    fixed number order

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -12,18 +12,18 @@ const App = () => {
       <div className="inner-container">
         <div className="white-button">
           <ActionButton extraStyles="rectangular-btn" text="clear" />
-        
-          <NumberButton text="1" extraStyles="square-button" />
-          <NumberButton text="2" extraStyles="square-button" />
-          <NumberButton text="3" extraStyles="square-button" />
-          <NumberButton text="4" extraStyles="square-button" />
-          <NumberButton text="5" extraStyles="square-button" />
-          <NumberButton text="6" extraStyles="square-button" />
-          <NumberButton text="7" extraStyles="square-button" />
-          <NumberButton text="8" extraStyles="square-button" />
-          <NumberButton text="9" extraStyles="square-button" />
+          
+          <NumberButton text="7" extraStyles="square-button number-button" />
+          <NumberButton text="8" extraStyles="square-button number-button" />
+          <NumberButton text="9" extraStyles="square-button number-button" />
+          <NumberButton text="4" extraStyles="square-button number-button" />
+          <NumberButton text="5" extraStyles="square-button number-button" />
+          <NumberButton text="6" extraStyles="square-button number-button" />
+          <NumberButton text="1" extraStyles="square-button number-button" />
+          <NumberButton text="2" extraStyles="square-button number-button" />
+          <NumberButton text="3" extraStyles="square-button number-button" />
 
-          <ActionButton extraStyles="rectangular-btn" text="0" />
+          <ActionButton extraStyles="rectangular-btn number-button" text="0" />
         </div>
 
         <div className="red-button">
