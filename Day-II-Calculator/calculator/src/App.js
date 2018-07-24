77aa009e619264453e7d1b7aa8386c227bd71d84
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-07-24 17:02:57 -0400

    React Calculator MVP completed

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-// import ActionButton from "./components/ButtonComponents/ActionButton";
+import ActionButton from "./components/ButtonComponents/ActionButton";
 import NumberButton from "./components/ButtonComponents/NumberButton";
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
 
@@ -8,13 +8,29 @@ import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay"
 const App = () => {
   return (
     <div className="calculator-app">
-
       <CalculatorDisplay/> 
 
+      <ActionButton buttonStyle="clear-button" text="clear"/>
+      <ActionButton buttonStyle="action-button" text="/"/>
 
+      <NumberButton buttonStyle="number-button" text="7"/>
+      <NumberButton buttonStyle="number-button" text="8"/>
+      <NumberButton buttonStyle="number-button" text="9"/>
+      <ActionButton buttonStyle="action-button" text="X"/>
+
+      <NumberButton buttonStyle="number-button" text="4"/>
+      <NumberButton buttonStyle="number-button" text="5"/>
+      <NumberButton buttonStyle="number-button" text="6"/>
+      <ActionButton buttonStyle="action-button" text="-"/>
+      
+      
+      <NumberButton buttonStyle="number-button" text="3"/>
       <NumberButton buttonStyle="number-button" text="2"/>
       <NumberButton buttonStyle="number-button" text="1"/>
+      <ActionButton buttonStyle="action-button" text="+"/>
 
+      <ActionButton buttonStyle="zero-button" text="0"/>
+      <ActionButton buttonStyle="action-button" text="="/>
     </div>
   );
 };
