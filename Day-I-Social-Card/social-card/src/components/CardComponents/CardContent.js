2a8b1ee6a78ad1fe8edef1c48657255778834654
William <kwontaewoongah@gmail.com>
2018-07-23 18:31:54 -0700

    styled some css

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Card.css';
 
-const CardContent=()=><div id='card-content'><a href='https://reactjs.org'><h2>Get started with React</h2><p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p><p class='gray'>reactjs.org</p></a></div>;
+const CardContent=()=><div id='card-content'><a href='https://reactjs.org'><p><strong>Get started with React</strong></p><p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p><p class='gray'>reactjs.org</p></a></div>;
 
 export default CardContent;
