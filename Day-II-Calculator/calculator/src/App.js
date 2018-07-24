d2982f3820f8ecdfc19c3603aea6fe52aba0860b
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-07-24 16:40:08 -0400

    NumberButton, CalculatorDisplay completed

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './App.css';
+// import ActionButton from "./components/ButtonComponents/ActionButton";
+import NumberButton from "./components/ButtonComponents/NumberButton";
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator-app">
+
+      <CalculatorDisplay/> 
+
+
+      <NumberButton buttonStyle="number-button" text="2"/>
+      <NumberButton buttonStyle="number-button" text="1"/>
+
     </div>
   );
 };
