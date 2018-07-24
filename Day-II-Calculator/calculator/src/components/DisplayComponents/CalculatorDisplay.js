d7a98c68317f290173556b1694bb7e03233b3048
szincone <sawyerzincone@gmail.com>
2018-07-24 13:35:06 -0600

    total display imported and rough version working

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalculatorDisplay = props => {
+  return (
+    <div className="total-div">
+      <p>{props.total}</p>
+    </div>
+  );
+};
+
+export default CalculatorDisplay;
