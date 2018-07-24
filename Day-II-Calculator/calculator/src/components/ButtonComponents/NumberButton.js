78a7f5edce6f710d2edbfd2f737861dacbd7ef82
MichaelBoegner <cmikeb@gmail.com>
2018-07-24 14:48:54 -0400

    Charles Boegner
    Calculator pull initialization

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+
+
+const NumberButton = props => <button className={props.buttonStyle}>props.text</button>
+
+
+
+
+export default NumberButton;
