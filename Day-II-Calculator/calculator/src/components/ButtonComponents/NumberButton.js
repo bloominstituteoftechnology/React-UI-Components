d009ae3678616666935d9081e7c2f924a6d511de
Arthur Bates <aabatesj@gmail.com>
2018-07-24 16:23:21 -0400

    React II
    number button components

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = () => {
+    return (
+        <div className="number-buttons">
+            <button className="calulator-key key-0">0</button>
+            <button className="calulator-key key-1">1</button>
+            <button className="calulator-key key-2">2</button>
+            <button className="calulator-key key-3">3</button>
+            <button className="calulator-key key-4">4</button>
+            <button className="calulator-key key-5">5</button>
+            <button className="calulator-key key-6">6</button>
+            <button className="calulator-key key-7">7</button>
+            <button className="calulator-key key-8">8</button>
+            <button className="calulator-key key-9">9</button>
+        </div>
+    )
+};
+
+export default NumberButton;
