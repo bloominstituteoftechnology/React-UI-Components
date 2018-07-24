6e452740d8a74f3879eda70cb7848b109114f640
keegn <keegan.t.burkett@gmail.com>
2018-07-24 15:46:55 -0500

    completed calculator display

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return (
+        <div className="flex-container">
+            <div className="total">0</div>
+        </div>
+    );
+};
+
+export default CalculatorDisplay;
