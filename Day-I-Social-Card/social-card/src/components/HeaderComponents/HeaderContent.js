6ad923dd23e00fe84a2a5e610548f3dba8c7661d
Stephanie Banks <thisgirlcancode@gmail.com>
2018-07-24 17:41:35 -0500

    added heaeder content

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
+const HeaderContent = () => (
+    <p className="header-content">
+        Let's learn React by building simple user interfaces with components. Don't
+        try to overthink it, just keep it simple and have fun. Once you feel
+        comfortable building components, you are well on your way to mastering
+        React!
+  </p>
+);
 
-const card-container = () => {
-    return <div class="card-wrapper">Hello</div>
-};
-
-export default cardContainer;
+export default HeaderContent;
