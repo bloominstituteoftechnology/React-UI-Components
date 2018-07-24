b921fa1e1ebcccb236e260bdfd14def374052315
Erica Thompson <EricaThompson@iMac.lan>
2018-07-24 12:21:04 -0700

    Stretch Almost

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../FooterComponents/Footer.css';
+
+
+
+const Footer = () => 
+<footer>
+    <i class="far fa-comment"></i>
+    <i class="fas fa-sync"><span>6</span></i>
+    <i class="far fa-heart"><span>4</span></i>
+    <i class="far fa-envelope"></i>
+</footer>;
+
+
+
+export default Footer;
