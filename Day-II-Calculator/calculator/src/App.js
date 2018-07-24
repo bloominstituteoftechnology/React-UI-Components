e40aca33f6ba539a96e713503d7416b77d7c139a
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-24 17:09:01 -0500

    Done with MVP
    finished the MVP, I added the signs for dividsion and multiplicatio, then also added a new prop called extraClass where you can add a new class to a Button

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -8,16 +8,16 @@ const App = () => {
   return (
     <div className="calculator">
       <CalculatorDisplay text="0" />
-      <ActionButton text="Clear"/>
-      <NumberButton extraClass ="signs" text="/" />
+      <ActionButton text="clear"/>
+      <NumberButton extraClass ="signs" text="÷" />
       <NumberButton text="1" />
       <NumberButton text="2" />
       <NumberButton text="3" />
-      <NumberButton extraClass ="signs" text="X" />
+      <NumberButton extraClass ="signs" text="x" />
       <NumberButton text="4" />
       <NumberButton text="5" />
       <NumberButton text="6" />
-      <NumberButton extraClass ="signs" text="-" />
+      <NumberButton extraClass ="signs" text="–" />
       <NumberButton text="7" />
       <NumberButton text="8" />
       <NumberButton text="9" />
