d620ff088ff6660a8f8845a1693c39809214b29a
jordan-massingill <jordan_massingill@hotmail.com>
2018-07-24 15:46:13 -0500

    Completed Static App
    Completed static calculator app
    Near pixel perfection
    Structured app components slightly differently than directed, action buttons were all math operators and number buttons were all white buttons.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton.js';
+import ActionButton from './components/ButtonComponents/ActionButton.js';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="app bordered">
+      <CalculatorDisplay number="0" />
+      <NumberButton text="clear" buttonStyle="btn-lrg" />
+      <ActionButton action="&#xf7;" buttonStyle="btn-sml"/>
+      <NumberButton text="7" buttonStyle="btn-sml" />
+      <NumberButton text="8" buttonStyle="btn-sml" />
+      <NumberButton text="9" buttonStyle="btn-sml" />
+      <ActionButton action="&#xd7;" buttonStyle="btn-sml" />
+      <NumberButton text="4" buttonStyle="btn-sml" />
+      <NumberButton text="5" buttonStyle="btn-sml" />
+      <NumberButton text="6" buttonStyle="btn-sml" />
+      <ActionButton action="&#x2212;" buttonStyle="btn-sml" />
+      <NumberButton text="1" buttonStyle="btn-sml" />
+      <NumberButton text="2" buttonStyle="btn-sml" />
+      <NumberButton text="3" buttonStyle="btn-sml" />
+      <ActionButton action="&#x2b;" buttonStyle="btn-sml" />
+      <NumberButton text="0" buttonStyle="btn-lrg" />
+      <ActionButton action="&#x3d;" buttonStyle="btn-sml" />
     </div>
   );
 };
