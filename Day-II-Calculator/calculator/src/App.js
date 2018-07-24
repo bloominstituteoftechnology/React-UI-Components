dce59331b15210ad65b45ae34424cae172636b04
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-24 12:29:27 -0700

    add all components and styling to calculator components

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
+  return <div className="container">
+      <CalculatorDisplay />
+
+      <ActionButton text="clear" buttonStyle="large white" />
+      <NumberButton text="%" buttonStyle="small red" />
+
+      <NumberButton text="7" buttonStyle="small white" />
+      <NumberButton text="8" buttonStyle="small white" />
+      <NumberButton text="9" buttonStyle="small white" />
+      <NumberButton text="X" buttonStyle="small red" />
+
+      <NumberButton text="4" buttonStyle="small white" />
+      <NumberButton text="5" buttonStyle="small white" />
+      <NumberButton text="6" buttonStyle="small white" />
+      <NumberButton text="-" buttonStyle="small red" />
+
+      <NumberButton text="1" buttonStyle="small white" />
+      <NumberButton text="2" buttonStyle="small white" />
+      <NumberButton text="3" buttonStyle="small white" />
+      <NumberButton text="+" buttonStyle="small red" />
+
+      <ActionButton text="0" buttonStyle="large white" />
+      <NumberButton text="=" buttonStyle="small red" />
+    </div>;
 };
 
 export default App;
