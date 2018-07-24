b17c9dae9bf9d206bc229ba5d9a03219acf3ca68
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-24 15:50:04 -0500

    completed number button functionality and began styling

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div className="calculator-container">
-    <CalculatorDisplay />
+      <CalculatorDisplay />
+      <div>
+        <NumberButton buttonStyle='digit' symbol='÷'/>
+      </div>
+
     </div>
   );
 };
