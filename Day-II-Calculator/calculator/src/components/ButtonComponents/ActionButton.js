7c9b63fbf946841743f7792bc0cf5aa5a0459323
Stephanie Banks <thisgirlcancode@gmail.com>
2018-07-24 17:56:23 -0500

    finished action button

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+
+import React from 'react';
+import './Button.css';
+
+const ActionButton = props => {
+    return (
+        <button className={`large-button ${props.className}`}>
+            {props.text}
+        </button>
+    );
+};
+
+export default ActionButton;
