2fa8d2f74e08d27153516ee36049a19bb3ea7b48
codejoncode <jonathanjholloway@gmail.com>
2018-07-24 18:13:48 -0500

    figured out how to use paramaters by using arrow functions

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -25,9 +25,9 @@ class App extends React.Component{
 
   addToDisplay(item){
     //on click of one of the buttons we will add its text/prop to the display. it will also add to the list property in the state object.
-    // console.log(item, "item");
-    this.state.total = this.state.total + 1 
-    console.log(this.state.total)
+    console.log(item, "item");
+    // this.state.total = this.state.total + 1 
+    // console.log(this.state.total)
     // console.log(this.state.total)
     console.log(item)
     const screen = document.querySelector('#onScreen');
@@ -44,7 +44,8 @@ class App extends React.Component{
     return (
       <div className = "main-div">
         <CalculatorDisplay />
-        <NumberButton one= "1" two="2" three="3" four="4" five="5" six="6" seven = "7" eight = "8" nine = "9" zero = "0" clear="clear" handleButtonClick = {this.addToDisplay} />
+        <NumberButton one= "1" two="2" three="3" four="4" five="5" six="6" seven = "7" eight = "8" nine = "9" zero = "0" clear="clear"
+         handleButtonClick = {item => this.addToDisplay(item) } />
       </div>
     );
 
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -18,17 +18,17 @@ class NumberButton extends React.Component {
                     <div className="numberInputs">
                         <div className="clear">{this.props.clear}</div>
 
-                        <div onClick={this.props.handleButtonClick} className="nonzero sm">{this.props.seven}</div>
-                        <div onClick={this.props.handleButtonClick(this.props.eight)}className="nonzero">{this.props.eight}</div>
-                        <div onClick={this.props.handleButtonClick(this.props.nine)}className="nonzero">{this.props.nine}</div>
-                        <div onClick={this.props.handleButtonClick(this.props.four)}className="nonzero sm">{this.props.four}</div>
-                        <div onClick={this.props.handleButtonClick(this.props.five)}className="nonzero">{this.props.five}</div>
-                        <div onClick={this.props.handleButtonClick(this.props.six)}className="nonzero">{this.props.six}</div>
-                        <div onClick={this.props.handleButtonClick(this.props.one)}className="nonzero sm">{this.props.one}</div>
-                        <div onClick={this.props.handleButtonClick(this.props.two)}className="nonzero">{this.props.two}</div>
-                        <div onClick={this.props.handleButtonClick(this.props.three)}className="nonzero">{this.props.three}</div>
+                        <div onClick={ () => this.props.handleButtonClick(this.props.seven)  } className="nonzero sm">{this.props.seven}</div>
+                        <div onClick={() => this.props.handleButtonClick(this.props.eight)}className="nonzero">{this.props.eight}</div>
+                        <div onClick={() => this.props.handleButtonClick(this.props.nine)}className="nonzero">{this.props.nine}</div>
+                        <div onClick={() => this.props.handleButtonClick(this.props.four)}className="nonzero sm">{this.props.four}</div>
+                        <div onClick={() => this.props.handleButtonClick(this.props.five)}className="nonzero">{this.props.five}</div>
+                        <div onClick={() => this.props.handleButtonClick(this.props.six)}className="nonzero">{this.props.six}</div>
+                        <div onClick={() => this.props.handleButtonClick(this.props.one)}className="nonzero sm">{this.props.one}</div>
+                        <div onClick={() => this.props.handleButtonClick(this.props.two)}className="nonzero">{this.props.two}</div>
+                        <div onClick={() => this.props.handleButtonClick(this.props.three)}className="nonzero">{this.props.three}</div>
 
-                        <div onClick={this.props.handleButtonClick(this.props.zero)}className="clear zero">{this.props.zero}</div>
+                        <div onClick={() => this.props.handleButtonClick(this.props.zero)}className="clear zero">{this.props.zero}</div>
                     </div>
                     <ActionButton />
                 </div>
