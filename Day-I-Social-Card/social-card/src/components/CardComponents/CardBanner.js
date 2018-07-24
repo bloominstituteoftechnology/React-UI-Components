c05d4283c6cbbe544127cfcacd56f0d65467688f
Charles Cortinas <crcortinas@gmail.com>
2018-07-24 10:39:25 -0500

    Minor tweaks
    Fixed thumbnail height issue, added alt text to react logo

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -5,7 +5,7 @@ const CardBanner = () => {
   return (
     <div className='card-banner'>
       <a href="https://www.reactjs.org">
-        <img src='https://ibin.co/3wnC6SgIOJud.png'></img>
+        <img src='https://ibin.co/3wnC6SgIOJud.png' alt='React logo'></img>
       </a>
     </div>
   )
