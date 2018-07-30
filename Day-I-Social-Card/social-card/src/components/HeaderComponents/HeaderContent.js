19840e909c63ceb18399a94d6d712544227c74bc
JoCatherine Griggs <jocafrin@gmail.com>
2018-07-30 18:06:11 -0400

    Built HeaderContent

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './Header.css';
 
 
-const HeaderContent = () => {
-    return (
-        <div className="content-wrapper">
-        <h3>Lambda School</h3>
-        <p>
+const HeaderContent = () => (
+    <p className="content-wrapper">
+      Let's learn React by building simple user interfaces with components. Don't
+    try to overthink it, just keep it simple and have fun. Once you feel
+    comfortable building components, you are well on your way to mastering
+    React!
+    </p>
+);
 
-        </p>
-        </div>
-    )
-}
+export default HeaderContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,12 +2,13 @@ import React from 'react';
 import HeaderContent from   "./HeaderContent";
 import './Header.css';
 
+
 const HeaderTitle = () => {
     return (
         <div className="header-wrapper">
             <div className="header-title">
         <h1>Lambda School</h1>
-        <span className="time-stamp">{time.format("MMM Do ")}</span>
+        
         <HeaderContent />
             </div>
         </div>
