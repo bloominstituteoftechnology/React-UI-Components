1aa58094c8d78575de1581619cf73d9e6fd25cb2
JoCatherine Griggs <jocafrin@gmail.com>
2018-07-30 17:07:03 -0400

    built ImageThumbnail.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,3 +2,14 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from "./ImageThumbnail";
 import HeaderTitle from "./HeaderTitle";
+
+const HeaderContainer = () => {
+    return (
+       <div className="container-header">
+       <ImageThumbnail />
+       <HeaderTitle />
+       </div>
+    );
+};
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return (
+        <div className="logo-wrapper">
+        <img alt="lambda logo"
+        className="thumbnail-img"
+        src="https://ibin.co/3whrpKSBbZ81.png" />
+        </div>
+    );
+}; 
+
+export default ImageThumbnail
