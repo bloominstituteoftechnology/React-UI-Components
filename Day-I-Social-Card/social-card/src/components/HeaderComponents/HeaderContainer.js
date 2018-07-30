26da06c5289f8baee7dab310adc93c2f5eb8a81d
bri79319 <bri79319@gmail.com>
2018-07-30 15:44:05 -0400

    attempting first MVP

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+import './HeaderTitle';
+import './ImageThumbnail';
+
+const HeaderContainer = () => {
+
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return <h4 className="lambda-header">Lambda School</h4>
+};
+
+export default HeaderTitle;
