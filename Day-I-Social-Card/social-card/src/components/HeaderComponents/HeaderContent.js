6cc86478fa0f34f98b4fb95c4abc2ba22f42f28a
JoCatherine Griggs <jocafrin@gmail.com>
2018-07-30 17:46:14 -0400

    built HeaderTitle

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderContent = () => {
+    return (
+        <div className="content-wrapper">
+        <h3>Lambda School</h3>
+        <p>
+
+        </p>
+        </div>
+    )
+}
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import HeaderContent from   "./HeaderContent";
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="header-title">
+        <h1>Lambda School</h1>
+        <span className="time-stamp">{time.format("MMM Do ")}</span>
+        <HeaderContent />
+            </div>
+        </div>
+    );
+};
+
+export default HeaderTitle;
