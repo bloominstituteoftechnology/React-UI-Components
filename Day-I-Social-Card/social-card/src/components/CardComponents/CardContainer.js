c6d986e1f00ca96bd29ef80fabb511d41dc5b1b0
AlexanderCaves <ahcaves@gmail.com>
2018-07-30 16:22:20 -0700

    all mvp components complete

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Card.css';
 
 const CardContainer = () => {
-    <div className='card-container'>{CardBanner}, {CardContent}</div>;
+    return <div className='card-container'>{CardBanner}, {CardContent}</div>;
 };
 
 export default CardContainer;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+
+const Header = () => <h3>Get started with React</h3>;
+const Content = () => <p>React makes it painless to create interactive UIs.  Design simple views for each state in your application.</p>;
+const SiteAddress = () => <h5>reactjs.org</h5>;
+const CardContent = () => {
+    return <div className='card-content'>{Header}, {Content}, {SiteAddress}</div>;
+};
+
+export default CardContent;
