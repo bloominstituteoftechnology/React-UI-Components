7977f2ea7a702f64be72387be3b621c8e0b8079e
bri79319 <bri79319@gmail.com>
2018-07-30 13:50:17 -0400

    starting project with react.js

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+const CardContainer = () => {
+    return <div className="container">Hello from Card Container</div>
+};
+
+export default CardContainer;
