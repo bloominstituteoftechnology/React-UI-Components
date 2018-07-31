c72f6e3772730d5381aba2dfa1cede88df4987fc
Cwright0428 <cwright0428@yahoo.com>
2018-07-31 09:25:59 -0500

    final push for social card

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './Card.css';
 
 const CardBanner = () => {
-    return <div></div>;
+  return (
+    <a className="banner-container-wrapper" href="https://reactjs.org">
+      <img
+        alt="react logo"
+        className="banner-img"
+        src="https://ibin.co/3wnC6SgIOJud.png"
+      />
+    </a>
+  );
 };
 
 export default CardBanner;
