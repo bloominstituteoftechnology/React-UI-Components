bafd24e98d77a01ccd33c223b90d29bc31078acf
farste <sfargali@gmail.com>
2018-07-31 18:13:34 -0400

    Stephen Fargali
    MBP almost complete, needs timestamp and click event

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle.js';
+import ImageThumbnail from './ImageThumbnail.js'
+import HeaderContent from './HeaderContent.js'
+const HeaderContainer = () => {
+  return (
+    <div className = 'header-container'>
+      <ImageThumbnail />
+    <div className = 'header-text'>
+      <HeaderTitle />
+      <HeaderContent />
+    </div>
+    </div>
+  );
+
+  
+};
+
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className='header-title'>
+            <span className = 'name'>Lambda School</span>
+            <span className = 'handle'>@LambdaSchool</span>
+        </div>
+        );
+};
+
+export default HeaderTitle;
