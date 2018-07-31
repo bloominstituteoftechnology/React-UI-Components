3429b83dd4f355e1413591e1f6cfce462da5d07b
rverdi642 <rverdi642@gmail.com>
2018-07-30 21:51:51 -0500

    components built have all the rendering....need CSS styling
    Richard Verdier

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle'
+import ImageThumbnail from './ImageThumbnail'
+
+const HeaderContainer= () => {
+    return (
+
+        <div className="headerContainer">
+        
+       <ImageThumbnail />
+        <HeaderTitle />
+        <HeaderContent />
+        
+          
+        </div>
+      );
+   
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return (
+      <div className="headerContainer">
+        <div className="headerTitle">
+          Lambda School
+         
+          <HeaderContent />
+        </div>
+      </div>
+    );
+  };
+  
+  export default HeaderTitle;
