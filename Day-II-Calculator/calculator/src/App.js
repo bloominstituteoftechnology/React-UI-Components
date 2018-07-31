52f4c9fbdbd83eac2932cb00f4cb889e2de9ebb3
bri79319 <bri79319@gmail.com>
2018-07-31 18:28:00 -0400

    finished the set up for mvp

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ButtonContainer from './components/ButtonComponents/ButtonContainer';
 import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <NumberButton />
-      <ActionButton />
+    <div className = "calc-wrap">
+      <CalculatorDisplay />
+
+      <ButtonContainer className = "clear-button">
+        <ActionButton className = "wide-btn" text = "clear" />
+        <NumberButton bgColor = "red-btn" text = "/" />
+      </ButtonContainer>
+
+      <ButtonContainer>
+        <NumberButton bgColor = "num-button" text = "7" />
+        <NumberButton bgColor = "num-button" text = "8" />
+        <NumberButton bgColor = "num-button" text = "9" />
+        <NumberButton bgColor = "red-btn" text = "X" />
+      </ButtonContainer>
+
+      <ButtonContainer>
+        <NumberButton bgColor = "num-button" text = "4" />
+        <NumberButton bgColor = "num-button" text = "5" />
+        <NumberButton bgColor = "num-button" text = "6" />
+        <NumberButton bgColor = "red-btn" text = "-" />
+      </ButtonContainer>
+
+      <ButtonContainer>
+        <NumberButton bgColor = "num-button" text = "1" />
+        <NumberButton bgColor = "num-button" text = "2" />
+        <NumberButton bgColor = "num-button" text = '3' />
+        <NumberButton bgColor = "red-btn" text = "+" />
+      </ButtonContainer>
+
+      <ButtonContainer>
+        <ActionButton className = "wide-btn" text = "0" />
+        <NumberButton bgColor = "red-btn" text = "=" />
+      </ButtonContainer>
       
     </div>
   );
