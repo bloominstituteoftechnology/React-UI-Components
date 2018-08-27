6306c58f67740e146506a946b99fe95c1a96bb15
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-08-27 15:17:47 -0500

    Imported all Components to Container file

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+// Component Imports
+
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
