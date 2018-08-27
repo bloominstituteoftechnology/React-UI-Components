f9acbbbae5b47d73f5a902d0252bb5edc9d9143e
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-08-27 16:57:07 -0400

    image thumbnail layout done, header components complete

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const imageThumbnail = props => {
+  return (
+    <div>
+      <img className = 'header-img' src = {'https://ibin.co/3whrpKSBbZ81.png'} alt = 'Lambda School Logo'/>
+    </div>
+  );
+};
+
+export default ImageThumbnail;
