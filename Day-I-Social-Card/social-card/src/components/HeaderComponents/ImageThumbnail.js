04e168a91fdadcaee3ebdf826f643be24cb48d5a
James Cassidy <cassidyjamesw@gmail.com>
2018-08-27 16:26:10 -0400

    initial commit

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const ImageThumbnail = () => {
+  return (
+    <div className="thumb-nail">
+      <img
+        src="https://pbs.twimg.com/profile_images/973277209644249089/0Te2jtBH_400x400.jpg"
+        alt="lambda thumbnail img"
+        class="thumb-img"
+      />
+    </div>
+  );
+};
