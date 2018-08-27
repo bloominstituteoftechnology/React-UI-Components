cda50c973e2beb877cef4c2d7b06b049ce8bf98f
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-08-27 17:31:52 -0400

    Added HTML portion of the header to social card component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import './ImageThumbnail';
+import HeaderImage from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => (
+  <div className="header-container">
+    <HeaderImage />
+    <HeaderTitle />
+    <HeaderContent />
+  </div>
+);
+
+export default HeaderContainer;
