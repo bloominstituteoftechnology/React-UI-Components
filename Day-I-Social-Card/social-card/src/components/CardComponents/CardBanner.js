e0f63148b8f3fd51c6c8fdd318479aea46bf9f73
lyang9 <lyang9@buffalo.edu>
2018-08-27 17:41:31 -0400

    Completed CardBanner

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+        <img classNane='card-banner' src='https://ibin.co/3wnC6SgIOJud.png' alt='Background image'/>
+    );
+};
+
+export default CardBanner;
