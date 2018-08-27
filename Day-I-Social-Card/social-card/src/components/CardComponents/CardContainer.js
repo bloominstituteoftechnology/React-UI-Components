01d782a15b2cd779faece55435d2d5621602f49c
Andrew <ghobrial.andrew@gmail.com>
2018-08-27 18:47:28 -0400

    Card container complete

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+const CardContainer = () => {
+    return (
+      <div className = "cardContainer">
+        <img src = "https://ibin.co/3wnC6SgIOJud.png" alt = "" className = "cardContainerImg"/>
+        <p>Get started with React</p>
+        <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+        <p>reactjs.org</p>
+      </div>
+    );
+  };
+
+export default CardContainer;
