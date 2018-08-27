007e76bc4c997b3af8b586cca327638fc32b7cee
decagondev <decagondevelopment@gmail.com>
2018-08-27 20:31:08 +0100

    (REACT-DAY-! SOCIAL CARD) : (CARD) : added structure for card content components

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = props => {
+  return (
+    <div className="card-content-wrapper">
+      <div className="card-content-title">Get started with React</div>
+      <div className="card-content-txt">React makes it painless to create interactive UIs. Design simple views for each state in your application.</div>
+      <div className="card-content-link-txt">reactjs.org</div>
+    </div>
+  );
+};
+
+export default CardContent;
