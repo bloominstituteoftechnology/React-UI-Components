e150e2d948c54c4c457b89fedb0c3c5adc389469
David Benavidez <benavidez_david13@yahoo.com>
2018-08-27 16:00:19 -0700

    almost done styling

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => (
+    <div class = " cardContent ">
+    <h1>
+        Get Started with react
+    </h1>
+    <p>
+        React Makes it painless to create interactive UIs. Design Simple views 
+        for each state in your application.
+    </p>
+    <h2>
+        reactjs.org
+    </h2>
+    </div>
+)
+
+export default CardContent;
