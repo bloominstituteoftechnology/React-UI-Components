33548b1948eddf5ab0ee150cb60012f0a9128aef
Benny Oseguera <oseguerabenny27@gmail.com>
2018-08-27 15:57:55 -0700

    Finished Benny Oseguera

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardContent = () => (
+  <div class="card-content">
+    <p>
+      <div class="title">Get started with React</div>
+      <br />
+      React makes it painless to create interactive UIs. Design simple views for
+      each state in your application.
+      <br />
+      <div class="link">reactjs.org</div>
+    </p>
+  </div>
+);
+
+export default CardContent;
