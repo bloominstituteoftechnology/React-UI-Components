452880867b7002900666aec977f7a30d2b39e91d
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-08-27 15:46:06 -0500

    ImageThumbnail structure

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = props => {
+  return (
+    <div>
+      <img
+        className="header-img"
+        src={'https://ibin.co/3whrpKSBbZ81.png'}
+        alt="Lambda Logo"
+      />
+    </div>
+  );
+};
+
+export default ImageThumbnail;
