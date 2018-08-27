c1fce8c2c404a74c8ff59b2d9c7e1c6dd3befa4c
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-08-27 17:53:52 -0500

    changed link tag

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -5,7 +5,7 @@ const CardContent = () => (
     <div className="card-content">
         <h2>Get started with React</h2>
         <p className="card-content-text">React makes it painless to create interavtive UIs. Design simple views for each state in your application.</p>
-        <p className="card-content-link">reactjs.org</p>
+        <a href="https://www.reactjs.org" className="card-content-link">reactjs.org</a>
     </div>
 );
 
