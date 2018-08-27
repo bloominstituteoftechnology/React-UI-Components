2c1c3185135d3fe2fb59b8dc9a854b09684f8edb
unknown <cornielleandres@gmail.com>
2018-08-27 18:58:38 -0400

    Add link to reactjs.org.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,10 +4,10 @@ import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
 const CardContainer = () => (
-    <div>
+    <a className="card-container-a" href="https://www.reactjs.org">
         <CardBanner />
         <CardContent />
-    </div>
+    </a>
 );
 
 export default CardContainer;
