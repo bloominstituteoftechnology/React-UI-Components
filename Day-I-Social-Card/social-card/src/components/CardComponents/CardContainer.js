0232f63a4ce9acc1c3ac16cf4a25c7d44acf0511
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-08-27 15:45:34 -0700

    Finished basic tasks today, working on responsive design

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+// Create a <CardContainer/ > container component that will hold your card components.
+// The entire <CardContainer /> should take a user to https://www.reactjs.org when clicked.
+
+const CardContainer = () => (
+  <a href="https://www.reactjs.org">
+    <div className="card">
+      <CardBanner />
+      <CardContent />
+    </div>
+  </a>
+);
+
+export default CardContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+// Create an <ImageThumbnail /> component using this image url https://ibin.co/3whrpKSBbZ81.png as it's img src.
+const ImageThumbnail = () => (
+  <img src = "https://ibin.co/3whrpKSBbZ81.png" />
+);
+
+export default ImageThumbnail;
