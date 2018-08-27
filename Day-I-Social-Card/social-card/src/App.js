644203812f164a9f798affab2347a43b531a28ef
Kevin Nguyen <kevinn858@gmail.com>
2018-08-27 15:58:41 -0700

    did a sample of how to import a component

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Thumbnail from './components/HeaderComponents/ImageThumbnail';
+
 const App = () => {
   return (
     <div>
+      <Thumbnail />
       <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const Thumbnail = () =>{
+    return <img src='https://ibin.co/3whrpKSBbZ81.png' alt='Lambda Logo'></img>;
+};
+
+export default Thumbnail;
