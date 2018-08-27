b528282f645ebcc9370a924af327665eb18cda98
alejandrok93 <alejandrok93@gmail.com>
2018-08-27 14:36:28 -0700

    added footer

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -3,12 +3,15 @@ import "./Card.css";
 import CardBanner from "./CardBanner.js";
 import HeaderContainer from "../HeaderComponents/HeaderContainer";
 import CardContent from "./CardContent.js";
+import Footer from "../FooterComponents/Footer.js";
+
 const cardContainer = () => (
   <div className="card-container">
     <a href="https://www.reactjs.org">
       <HeaderContainer />
       <CardBanner />
       <CardContent />
+      <Footer />
     </a>
   </div>
 );
---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const footer = () => (
+  <div className="footer">
+    <i className="far fa-comment" />
+    <i className="fas fa-retweet" />
+    <i className="far fa-heart" />
+    <i className="far fa-envelope" />
+  </div>
+);
+
+export default footer;
