656036f93fe796b8c5eb5798505e4261d154d43c
decagondev <decagondevelopment@gmail.com>
2018-08-28 00:42:23 +0100

    (REACT-DAY-! SOCIAL CARD) : (STRETCH) : added onClick events to the social icons

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,12 +2,11 @@ import React from 'react';
 import icons from 'glyphicons'
 import './Footer.css';
 
-const Footer = () => {
+const Footer = props => {
+  
     return (
     <div className="footer-wrapper">
-    <i className="fa tom-tag fa-comment fa-lg comment left-fa"></i><i className="fa tom-tag fa-retweet fa-lg comment"></i><span>6</span><i className="fa tom-tag fa-heart fa-lg comment"> </i><span>4</span><i className="fa tom-tag fa-envelope fa-lg comment"> </i>
-
-
+      <i className="fa tom-tag fa-comment fa-lg comment left-fa" onClick={() => { alert("You can not comment on this thread!") }}></i><i className="fa tom-tag fa-retweet fa-lg comment" onClick={(props) => { alert(`This post has 6 retweets!`) }}></i><span>6</span><i className="fa tom-tag fa-heart fa-lg comment" onClick={() => { alert("Do you really like it, is it is it wicked??") }}> </i><span>4</span><i className="fa tom-tag fa-envelope fa-lg comment" onClick={() => { alert("you do not have permission to send emails - maybe your computer is broken!?!") }}> </i>
     </div>
     );
 };
