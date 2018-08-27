cd63084ccdc04af8265360498baf40e2caae53eb
BrianARuff <thelonewolf198927@gmail.com>
2018-08-27 16:21:34 -0400

    header component done

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+export default function HeaderContent() {
+    return (
+        <p>
+            Let's learn React by building simple interfaces with components. Don't try to overthink it, just keepti simple an have fun. Once you feel comfortable using components you are well on your way to mastering React!
+        </p>
+    )
+}
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+export default function HeaderTitle() {
+    return (
+        <div className="header-title">
+            <span className="school-name" href="#"> Lambda School </span>
+            <span className="school-social-media" href="#"> @LambdaSchool </span>
+            <span className="date-posted" href="#" > &middot; 26 Jan </span>
+        </div>
+    )
+}
