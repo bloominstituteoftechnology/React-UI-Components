5597ff27af08c60a88e7a73a6820347205da68bf
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-08-27 15:45:38 -0500

    HeaderTitle structure

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+  return (
+    <div className="header-title-wrap">
+      <div className="header-name">Lambda School</div>
+      <div className="header-tag">@LambdaSchool</div>
+      <div className="header-space">&bull;</div>
+      <div className="header-date">26 jan</div>
+    </div>
+  );
+};
+
+export default HeaderTitle;
