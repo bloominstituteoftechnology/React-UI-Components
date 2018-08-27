b60b54d78cab6d1256127742b4ac4023896b067d
Clint Kunz <clintkunz@gmail.com>
2018-08-27 17:58:55 -0600

    styling

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 
 const ImageThumbnail = () => {
-    return <img src="https://ibin.co/3whrpKSBbZ81.png" alt="logo" className="logo" />;
+    return <img src="https://pbs.twimg.com/profile_images/973277209644249089/0Te2jtBH_400x400.jpg" alt="logo" className="logo" />;
 };
 
 export default ImageThumbnail;
