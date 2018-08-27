03a9cef544c474e07bda4168b28d73c4479947f6
lyang9 <lyang9@buffalo.edu>
2018-08-27 16:30:31 -0400

    Completed HeaderTitle

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className='container-header'>
+            <div className='username'>Lambda School</div>
+            <div className='tagname'>@LambdaSchool</div>
+            <div className='bullet'>&bull;</div>
+            <div className='date'>27 aug</div>
+        </div>
+    );
+};
+
+export default HeaderTitle;
