19a5a802e516e3ce91f545a13a651a254802078d
Austinlp4 <apendergrast44@gmail.com>
2018-08-27 16:15:49 -0500

    Project minus stretch

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => (
+    <div className='header-container'>
+        <ImageThumbnail />
+        <HeaderTitle />
+        
+    </div>
+);
+
+export default HeaderContainer;
