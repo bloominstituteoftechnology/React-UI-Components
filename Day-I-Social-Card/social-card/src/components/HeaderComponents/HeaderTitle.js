96556bada0a226b507ae5ad4225e18280023c52f
Timothy Hoang <timh1203@gmail.com>
2018-08-27 15:57:58 -0500

    Add header date

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './Header.css';
 
 const HeaderTitle = () => {
+  const date = new Date();
+  const options = { month: "short" };
+  const short = new Intl.DateTimeFormat("en-US", options).format(date);
+  const today = `${date.getDate()} ${short}`;
+
+
   return (
     <div className="HeaderTitle">
       <p className="displayName">Lambda School</p>
       <p className="accountName">@LambdaSchool</p>
       <p className="bullet"> • </p>
-      <p className="date">Date</p>
+      <p className="date">{today}</p>
     </div>
   )
 }
