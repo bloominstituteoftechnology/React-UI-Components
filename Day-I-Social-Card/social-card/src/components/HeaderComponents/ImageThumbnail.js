1a1f305abfe1c64d9a5dd4e8c712f6de948764e0
Anthony Greb <agreb255@gmail.com>
2018-08-27 17:18:13 -0400

    first commit

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return (
+        <div className="thumb-wrapper">
+            <img 
+                src="https://pbs.twimg.com/profile_images/973277209644249089/0Te2jtBH_400x400.jpg"
+                alt="lambda thumbnail image"
+                className="thum-img"
+            />
+        </div>
+    );
+};
+
+export default ImageThumbnail;
