92963461050aeb589c81c2cd281829ddd436490a
MikeRGarrity <mikergarrity@gmail.com>
2018-08-27 15:59:47 -0500

    finished with components, working on styling

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+  return (
+    <div className="thumbnail">
+      <img src="https://ibin.co/3whrpKSBbZ81.png" className="LambdaLogo" alt="Lambda Logo"/>
+    </div>
+  );
+};
+
+export default ImageThumbnail;
