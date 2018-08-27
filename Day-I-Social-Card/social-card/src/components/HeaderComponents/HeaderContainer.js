9b2d9ff03748dc67147e382da7c9f9f042be7bc3
IlanL75 <anatoly.leytman@gmail.com>
2018-08-27 16:09:21 -0700

    initial pull request

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = () => {
+	return (<div><h1> Why React is so hard???</h1></div>);
+};
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Header.css';
+
+
+export default ImageThumbnail = () => (
+	<h1>Lambda School</h1>;
+);
