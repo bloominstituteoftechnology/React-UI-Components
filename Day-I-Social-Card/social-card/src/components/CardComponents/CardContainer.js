c6deb80de3096ea0e8f4a6d5ad70daffa9cc338f
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-08-27 19:06:33 -0400

    Completed MVP

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,9 +4,11 @@ import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
 const CardContainer = () => (
-  <a href="https://www.reactjs.org" className="card-container">
-    <CardBanner />
-    <CardContent />
+  <a href="https://www.reactjs.org">
+    <div className="card-container">
+      <CardBanner />
+      <CardContent />
+    </div>
   </a>
 );
 
