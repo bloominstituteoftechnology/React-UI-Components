25556354ad46b1675642a9b06374131c3151afc6
Andrew <ghobrial.andrew@gmail.com>
2018-08-27 18:30:23 -0400

    Header title complete

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,8 +3,17 @@ import './Header.css';
 
 const HeaderContainer = () => {
     return (
-      <div>
-         <h1>Hello, world!</h1>
+      <div className = "container">
+        <div>
+          <img src="https://ibin.co/3whrpKSBbZ81.png" alt = "" className = "headerImg"/>
+        </div>
+        <div className = "headerText">
+          <div className = "headerTitle">
+            <p>Lambda School</p>
+            <p className = "date">@lambdaschool - 26 Jan</p>
+          </div>
+          <p>Let's learn react by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components, you are well on your way to mastering React!</p>
+        </div>
       </div>
     );
   };
