319bfdaa8d7df8dcdf0b4fe95e54bd9431c842ed
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-08-27 17:22:52 -0500

    Footer Structure

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <i class="icon-comment-alt" />
+      <i class="icon-refresh">6</i>
+      <i class="icon-heart-empty">4</i>
+      <i class="icon-envelope-alt" />
+    </div>
+  );
+};
+export default Footer;
