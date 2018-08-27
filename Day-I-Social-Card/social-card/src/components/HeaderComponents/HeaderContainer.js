6aba4a07f12191b3a18af245b25a8c4c3240bcfc
lyang9 <lyang9@buffalo.edu>
2018-08-27 17:14:14 -0400

    Completed HeaderContainer

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+
+import ImageThumbnail from './ImageThumbnail.js';
+import HeaderTitle from './HeaderTitle.js';
+import HeaderContent from './HeaderContent.js';
+
+const HeaderContent = () => {
+    return (
+        <div className='header-container'>
+            <ImageThumbnail />
+            <div className='header-text'>
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
+    );
+};
+
+export default HeaderContainer;
