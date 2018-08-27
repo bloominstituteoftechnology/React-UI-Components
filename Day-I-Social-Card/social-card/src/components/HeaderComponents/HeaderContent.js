8e23e9042e2ef8706648db1a74542bf982302ffb
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-08-27 16:15:48 -0400

    header imports and components complete

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Header.css';
+
+// importing other Components
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
