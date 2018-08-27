84d51b24493424a9e8a47b065329af9881473e10
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-08-27 16:05:11 -0500

    header components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => (
+    <div className="header-container">
+        <ImageThumbnail />
+        <div className="header-content">
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+    </div>
+);
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => (
+    <div className="header-title">
+        <div className="org-name">Lambda School</div>
+        <div className="org-social-media">@LambdaSchool</div>
+        <div className="post-date">26 jan</div>
+    </div>
+);
+
+export default HeaderTitle;
