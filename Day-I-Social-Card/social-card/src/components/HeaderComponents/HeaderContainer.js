fd2d59ec3355b72533f3dedc6dd9af8a6917b5bb
Michael Trevino <preince560m@gmail.com>
2018-08-27 17:46:47 -0500

    finish the react file

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return (
+        <div className='container-header'>
+        <ImageThumbnail />
+        <HeaderTitle />
+        </div>
+    );
+};
+
+export default HeaderContainer;
