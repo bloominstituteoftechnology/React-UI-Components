896d63663b3249f4032ba83fb477b86e60a441f1
unknown <cornielleandres@gmail.com>
2018-08-27 17:39:11 -0400

    Complete CardContent component.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 const CardContainer = () => (
     <div>
         <CardBanner />
+        <CardContent />
     </div>
 );
 
