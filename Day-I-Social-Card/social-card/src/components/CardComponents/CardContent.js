14b5487decaa78dbf96a15a52315fa7affe5d15b
marshall lanners <marshalllanners@marshalls-MacBook-Air.local>
2018-08-27 15:39:13 -0700

    finished hw mvp

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () => (
+	<div className="card-text">
+		<h2 className="title">Get started with React</h2>
+		<p>React makes it painless to create interactive UIs. Design simple views for each state in your application</p>
+		<p className="grey">reactjs.org</p>
+	</div>
+);
+
+
+export default CardContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThubnail = () => (
+	<img src='https://ibin.co/3whrpKSBbZ81.png' className="logo" />
+);
+
+export default ImageThubnail;
