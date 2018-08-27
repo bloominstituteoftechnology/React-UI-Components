63b6e3426c420079f10002f6d880458a4a5419fb
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-08-27 16:26:42 -0400

    Added Lambda School header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = content => {
+    return (
+      <div className="header-container-title">
+        <HeaderTitle />
+        
+      </div>
+    );
+  };
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+    return (
+        <div className="header-container-title">
+        
+            Lambda School
+            <span className="user-date">@LambdaSchool 26 Jan</span>
+       
+    </div>
+    );
+  };
+   export default HeaderTitle;
+       
+  
