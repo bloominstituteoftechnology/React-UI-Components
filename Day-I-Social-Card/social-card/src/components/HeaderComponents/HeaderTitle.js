55588c2c41fb859317a2186b54f49e4601659dcd
JeanetteFernandez <jeanfern555@gmail.com>
2018-08-27 17:50:15 -0500

    I have the  header set up by making changes in the HeaderComponents folder and the App.js file.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => (
+    <div className = "header-title">
+        <h1>Lambda School</h1>
+        <h1 className="handle">@LambdaSchool</h1>
+    </div>
+);
+
+export default HeaderTitle;
