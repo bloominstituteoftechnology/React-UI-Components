c3cfe4aac80a1952185576d4019721517adad0b9
unknown <cornielleandres@gmail.com>
2018-08-27 18:14:26 -0400

    Complete Footer component.

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+    <div className="footer-div">
+        <i className="far fa-comment"></i>
+        <i className="fas fa-sync-alt"></i><span>6</span>
+        <i className="far fa-heart"></i><span>4</span>
+        <i className="far fa-envelope"></i>
+    </div>
+);
+
+export default Footer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -5,7 +5,7 @@ const HeaderTitle = () => {
     return (
         <div className="header-title-div">
             <span className="title">Lambda School</span> <span className="handle">@LambdaSchool &#8901; 26 jan</span>
-            <p>Let's learn React by building simple interfaces with components.  Don't try to overthink it, just keep it simple and have fun.  Once you feel comfortable using components you are well on your way to mastering React!</p>
+            <p>Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
         </div>
     )
 };
