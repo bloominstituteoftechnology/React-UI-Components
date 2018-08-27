cc207f697e67a7ae96dc31f374113d6077629838
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-08-27 18:55:54 -0500

    footer react js and css

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+    <div className="footer-container">
+        <div className="footer-links">
+        <i class="far fa-comment"></i>
+        <i class="fas fa-sync"> 6</i>
+        <i class="far fa-heart"> 4</i>
+        <i class="far fa-envelope"></i>
+    </div>
+    </div>
+    
+);
+
+export default Footer;
