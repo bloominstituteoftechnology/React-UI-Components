86e221ba6d13bad263df6d30b21b5c31dba35220
decagondev <decagondevelopment@gmail.com>
2018-08-27 19:36:59 +0100

    (DAY 1 SOCIAL CARD) : started working on the header component : added structure to the HeaderContainer

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -7,5 +7,15 @@ import HeaderTitle from './HeaderTitle';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderContent from './HeaderContent';
 
-
+const HeaderContainer = props => {
+  return (
+    <div className="header-wrapper">
+      <ImageThumbnail />
+      <div className="header-title-and-content-wrapper">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
 
