2138d7886d1d4638f9ae5a3f55c178d71b576370
Max <maxkajiwara@gmail.com>
2018-08-27 16:11:26 -0700

    Whoops first commit

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner.js'
+import CardContent from './CardContent.js'
+
+const CardContainer = (props) => {
+  return (
+    <div className='card-container'>
+        <CardBanner banner={props.banner} />
+        <CardContent title={props.title} link={props.link} />
+    </div>
+  );
+}
+
+export default CardContainer;
