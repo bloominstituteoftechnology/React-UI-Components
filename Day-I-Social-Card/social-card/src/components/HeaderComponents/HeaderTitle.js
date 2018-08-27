6ee8dd17e60e828f374b0d81ef3a0a615b76962d
decagondev <decagondevelopment@gmail.com>
2018-08-27 22:27:05 +0100

    (REACT-DAY-! SOCIAL CARD) : (STRETCH) : added the moment nodejs module and formatted todays date in place of creation date

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
 const HeaderTitle = props => {
   return (
@@ -7,7 +8,7 @@ const HeaderTitle = props => {
       <div className="header-user-name">Lambda School</div>
       <div className="header-user-tag">@LambdaSchool</div>
       <div className="header-seperator">&bull;</div>
-      <div className="header-creation-date">26 jan</div>
+      <div className="header-creation-date">{moment().format("MMM D")}</div>
     </div>
   );
 };
