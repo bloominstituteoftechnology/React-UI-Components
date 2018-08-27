3f66ea0d5f6eb0fcc82da326dea1a55931b3fc07
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-08-27 17:22:26 -0500

    Imported Footer

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer.js';
 import CardContainer from './components/CardComponents/CardContainer.js';
+import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className="content-wrap">
         <HeaderContainer />
         <CardContainer />
+        <Footer />
       </div>
     </div>
   );
--- Day-I-Social-Card/social-card/src/index.js
+++ Day-I-Social-Card/social-card/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
 
