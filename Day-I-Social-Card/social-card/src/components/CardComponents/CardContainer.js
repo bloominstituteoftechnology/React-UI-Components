602dce7a55e6869d963b7ab434f4c973a9f114d1
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-08-27 17:34:24 -0600

    Fixed a few things.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,7 +4,7 @@ import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
 const CardContainer = () => (
-    <a className = "card-container">
+    <a href="https://www.reactjs.org" className="card-container">
         <CardBanner />
         <CardContent />
     </a>
