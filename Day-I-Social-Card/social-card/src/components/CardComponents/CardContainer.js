6b4f81a58773f8e0a431762e132dfed56c052a25
Andrew <ghobrial.andrew@gmail.com>
2018-08-27 18:55:06 -0400

    card container border adjusted and url linked

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,10 +4,12 @@ import './Card.css';
 const CardContainer = () => {
     return (
       <div className = "cardContainer">
-        <img src = "https://ibin.co/3wnC6SgIOJud.png" alt = "" className = "cardContainerImg"/>
-        <p>Get started with React</p>
-        <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-        <p>reactjs.org</p>
+        <a href = "reactjs.org">
+            <img src = "https://ibin.co/3wnC6SgIOJud.png" alt = "" className = "cardContainerImg"/>
+            <p>Get started with React</p>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <p>reactjs.org</p>
+        </a>
       </div>
     );
   };
