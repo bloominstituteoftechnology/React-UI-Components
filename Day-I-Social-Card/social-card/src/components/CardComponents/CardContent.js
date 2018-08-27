42bb2d7d902e43ba490a041320a52b51d9a8bc3a
lyang9 <lyang9@buffalo.edu>
2018-08-27 17:42:05 -0400

    Completed CardContent

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className='card-content'>
+            <div className='card-title'>Get Started with React</div>
+            <div className='card-status'>React makes it painless to create interactive UIs.
+            Design simple views for each state in your application.</div>
+            <div className='card-link'>reactjs.org</div>
+        </div>
+    );
+};
+
+export default CardContent;
