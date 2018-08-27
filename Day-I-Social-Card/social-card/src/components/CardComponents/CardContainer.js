01285a4a91b24bc625ce0da59262321393210473
BrianARuff <thelonewolf198927@gmail.com>
2018-08-27 16:40:06 -0400

    card container

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+export default function CardContainer() {
+    return (
+        <div className="card-container">
+            <CardBanner />
+            <CardContent />
+        </div>
+    )
+}
