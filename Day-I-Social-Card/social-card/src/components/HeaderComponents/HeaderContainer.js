2d44b0ef02c6c22c790f8d6e057c71dfbf45b6a7
unknown <cornielleandres@gmail.com>
2018-08-27 15:47:57 -0400

    Complete ImageThumbnail Component.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = () => {
+    return (
+        <div className="header-container">
+            {/* <ImageThumbnail src="https://ibin.co/3whrpKSBbZ81.png" /> */}
+            <ImageThumbnail src="https://pbs.twimg.com/profile_images/973277209644249089/0Te2jtBH_400x400.jpg" />
+            <HeaderTitle />
+        </div>
+    )
+};
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = (props) => <img alt="Lambda logo" class="image-thumbnail" src={props.src} />;
+
+export default ImageThumbnail;
