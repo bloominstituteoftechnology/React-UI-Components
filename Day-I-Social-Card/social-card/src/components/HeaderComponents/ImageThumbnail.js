b13d6fbd18e0a842be3159dc0553e7b03ef634e0
lyang9 <lyang9@buffalo.edu>
2018-08-27 16:39:47 -0400

    Completed ImageThumbnail

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return (
+        <img className='header-img' src='https://ibin.co/3whrpKSBbZ81.png' alt='Lambda logo'/>
+    );
+};
+
+export default ImageThumbnail;
