6718d649be7bed833a74b62e5af71356d194a21b
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-08-27 17:52:56 -0400

    Card Structured added image to card added text content to card

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer.js';
+import CardContainer from './components/CardComponents/CardContainer.js';
 
 const App = () => {
   return (
     <div className="main">
       <div className="within">
         <HeaderContainer />
+        <CardContainer />
         
       </div>
     </div>
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const CardContainer = props => {
+    return (
+        <div className="card-container">
+        <div className="card-container-content">
+        <CardBanner />
+        <CardContent />
+
+        </div>
+        </div>
+    );
+};
+
+export default CardContainer;
