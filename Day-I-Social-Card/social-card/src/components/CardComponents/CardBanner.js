fd1a5572f4ac3b594f5dc27643467676ba77b1c6
mparedes003 <michelle.g.paredes@gmail.com>
2018-08-27 14:33:35 -0700

    wrote code for CardBanner.js

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => (
+  <img className="card-banner" src="https://ibin.co/3wnC6SgIOJud.png" alt="React Logo"></img>
+  )
+  
+  export default CardBanner;
