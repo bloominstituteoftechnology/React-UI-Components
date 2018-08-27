c1fbdd198b4a49e8209e211b3ab5e1f5c45c6713
unknown <cornielleandres@gmail.com>
2018-08-27 18:40:56 -0400

    Include moment.js to format time-stamp.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
 const HeaderTitle = () => {
+    const date = moment().format("DD MMM").toLocaleLowerCase();
     return (
         <div className="header-title-div">
-            <span className="title">Lambda School</span> <span className="handle">@LambdaSchool &#8901; 26 jan</span>
+            <span className="title">Lambda School</span> <span className="handle">@LambdaSchool &#8901; {date}</span>
             <p>Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
         </div>
     )
