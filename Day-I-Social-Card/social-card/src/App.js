eadd5cb5416544a12478f2c583b3bd963ec20bca
marshall lanners <marshalllanners@marshalls-MacBook-Air.local>
2018-08-27 19:42:34 -0700

    added footer

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+
 import './App.css';
 import './components/HeaderComponents/Header.css';
 import './components/CardComponents/Card.css';
+import './components/FooterComponents/Footer.css';
+
 import HeaderContainer from './components/HeaderComponents/HeaderContainer'; 
 import CardContainer from './components/CardComponents/CardContainer'; 
+import FooterContainer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div className="main-container">
+     <a href="https://www.reactjs.org">
       <HeaderContainer />
       <CardContainer />
+      <FooterContainer />
+     </a>
     </div>
   );
 };
---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+
+const FooterContainer = () => (
+	<div className="footer-margin">
+		<div className="footer-flex">
+			<div className="image-spacing"><img src="https://www.freeiconspng.com/minicovers/speech-bubble-png-3.png" className="icons"/></div>
+			<div className="image-spacing"><img src="https://static1.squarespace.com/static/59b5c3638419c20a25da9b25/t/59c2c22f197aea0c2f81a334/1505935937515/arrows+circle+icon.png" className="icons" />6</div>
+			<div className="image-spacing"><img src="https://png.icons8.com/ios/2x/hearts.png" className="icons" />4</div>
+			<div className="image-spacing"><img src="https://png.icons8.com/ios/2x/new-post.png" className="icons" /></div>
+		</div>
+	</div>
+);
+
+export default FooterContainer;
