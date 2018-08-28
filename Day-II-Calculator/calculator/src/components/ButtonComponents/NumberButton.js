2109093b2f77fc736ded946a001e45d886332cf9
marshall lanners <marshalllanners@marshalls-MacBook-Air.local>
2018-08-28 13:22:25 -0700

    finished hw calculator

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Button.css';
+
+const NumberBrn = (props) => (
+	<div className="num-container">
+		<div className="clear">{props.nums.clear}</div>
+		<div className="flexNum">
+			<div><div className="btn">{props.nums.seven}</div><div className="btn">{props.nums.eight}</div><div className="btn">{props.nums.nine}</div></div>
+			<div><div className="btn">{props.nums.four}</div><div className="btn">{props.nums.five}</div><div className="btn">{props.nums.six}</div></div>
+			<div><div className="btn">{props.nums.one}</div><div className="btn">{props.nums.two}</div><div className="btn">{props.nums.three}</div></div>
+		</div>
+		<div className="zero">{props.nums.zero}</div>
+	</div>
+);
+
+
+
+
+export default NumberBrn;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalDisplay = (props) => (
+
+	<div className="display-cal">
+		<div>{props.nums.zero}</div>
+	</div>
+
+);
+
+export default CalDisplay;
