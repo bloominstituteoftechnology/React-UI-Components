f250c78d7d443aa80e114cf417be3543ee1530d7
Brian Ruff <31252964+BrianARuff@users.noreply.github.com>
2018-08-28 17:46:09 -0400

    Update ActionButton.js

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+export default function ActionButton(props) {
+    return (
+        <div className={`${props.cssClass} button`}>
+            <span className="button-span">{props.entity ? props.entity : props.action}</span>
+        </div>
+    )
+}
