6ff40ce1f7f46800cba8b2153174cc4a81990182
decagondev <decagondevelopment@gmail.com>
2018-08-28 19:40:40 +0100

    (REACT UI DAY 2 - CALC) (STRUCTURE) : done structure of the ActionButton Component

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,1 +1,9 @@
 import React from 'react';
+
+function  ActionButton(props) {
+  return (
+    <button className={`large-button ${props.type}`}>{props.value}</button>
+  );
+};
+
+export default ActionButton;
