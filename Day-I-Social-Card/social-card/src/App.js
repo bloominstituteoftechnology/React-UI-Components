f2bd61b51858ef90ab8bbd3ab403d2928aa1e389
lyang9 <lyang9@buffalo.edu>
2018-08-28 11:37:56 -0400

    Added Footer

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer.js';
 import CardContainer from './components/CardComponents/CardContainer.js';
+import Footer from './components/FooterComponents/Footer.js';
 
 const App = () => {
   return (
     <div className='app-container'>
       <HeaderContainer />
       <CardContainer />
+      <Footer />
     </div>
   );
 };
