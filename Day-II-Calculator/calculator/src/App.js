cd3dd0507dc3ce44901ad38a4acb8bf5760ad42c
lyang9 <lyang9@buffalo.edu>
2018-08-28 18:31:20 -0400

    Imported components files and completed app

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import './App.css';
 
+import NumberButton from './components/ButtonComponents/NumberButton.js';
+import ActionButton from './components/ButtonComponents/ActionButton.js';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
+
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div class='app-container'>
+      <CalculatorDisplay />
+      <div className='num-container'>
+        <ActionButton type='wide-action' text='Clear' />
+        <ActionButton type='operator' text='÷' />
+      </div>
+
+      <div className='num-container'>
+        <NumberButton type='number' text='7' />
+        <NumberButton type='number' text='8' />
+        <NumberButton type='number' text='9' />
+        <ActionButton type='operator' text='x' />
+      </div>
+
+      <div className='num-container'>
+        <NumberButton type='number' text='4' />
+        <NumberButton type='number' text='5' />
+        <NumberButton type='number' text='6' />
+        <ActionButton type='operator' text='-' />
+      </div>
+
+      <div class='num-container'>
+        <NumberButton type='number' text='1' />
+        <NumberButton type='number' text='2' />
+        <NumberButton type='number' text='3' />
+        <ActionButton type='operator' text='+' />
+      </div>
+
+      <div className='num-container'>
+        <NumberButton type='wide-number' text='0' />
+        <ActionButton type='operator' text='=' />
+      </div>
     </div>
   );
 };
