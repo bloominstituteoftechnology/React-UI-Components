13cfe7356a09211ee755e67a1057abc89caea674
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-08-28 13:32:50 -0700

    button layout

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,29 @@
 import React from 'react';
 import './Button.css';
+
+
+const NumberButton = () => {
+    return (
+      <div class= 'entrypad'>
+        <h1 class='bigbutton'>Clear</h1>
+        <h2 class='symbol'>÷</h2>
+        <h2>7</h2>
+        <h2>8</h2>
+        <h2>9</h2>
+        <h2 class='symbol'>•</h2>
+        <h2>4</h2>
+        <h2>5</h2>
+        <h2>6</h2>
+        <h2 class='symbol'>-</h2>
+        <h2>1</h2>
+        <h2>2</h2>
+        <h2>3</h2>
+        <h2 class='symbol'>+</h2>
+        <h1 class='bigbutton'>0</h1>
+        <h2 class='symbol'>=</h2>
+       
+      </div>
+    );
+  };
+  
+  export default NumberButton;
