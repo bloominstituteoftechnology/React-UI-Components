fe15146050daa736060993c176c8b9990d4db361
Brian Ruff <31252964+BrianARuff@users.noreply.github.com>
2018-08-28 17:46:37 -0400

    Update NumberButton.js

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+export default function NumberButton(props) {
+    console.log(props)
+    return (
+        <div className="number-button">
+            <span className="button-span">{props.number}</span>
+        </div>
+    )
+}
