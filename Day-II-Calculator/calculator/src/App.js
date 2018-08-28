ac0bd0b1d8e546672ecb625036e67959f8aaf743
Nicocchi <nicocchilover@gmail.com>
2018-08-28 17:26:18 -0400

    design updated

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -6,25 +6,40 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <div class="button-container">
+    <div class="calculator-container">
+      <div class="display">
         <CalculatorDisplay buttonStyle="calculator-display" text="0"/>
-        <ActionButton buttonStyle="action-button" text="Clear"/>
-        <NumberButton buttonStyle="operator-button" text="÷"/>
-        <NumberButton buttonStyle="number-button" text="7"/>
-        <NumberButton buttonStyle="number-button" text="8"/>
-        <NumberButton buttonStyle="number-button" text="9"/>
-        <NumberButton buttonStyle="operator-button" text="x"/>
-        <NumberButton buttonStyle="number-button" text="4"/>
-        <NumberButton buttonStyle="number-button" text="5"/>
-        <NumberButton buttonStyle="number-button" text="6"/>
-        <NumberButton buttonStyle="operator-button" text="-"/>
-        <NumberButton buttonStyle="number-button" text="1"/>
-        <NumberButton buttonStyle="number-button" text="2"/>
-        <NumberButton buttonStyle="number-button" text="3"/>
-        <NumberButton buttonStyle="operator-button" text="+"/>
-        <ActionButton buttonStyle="action-button" text="0"/>
-        <NumberButton buttonStyle="operator-button" text="="/>
+      </div>
+      <div class="button-container">
+        <div class="column-left">
+          <div class="inner-full-row">
+            <ActionButton buttonStyle="clear-button" text="Clear"/>
+          </div>
+
+          <NumberButton buttonStyle="number-button" text="7"/>
+          <NumberButton buttonStyle="number-button" text="8"/>
+          <NumberButton buttonStyle="number-button" text="9"/>
+          <NumberButton buttonStyle="number-button" text="4"/>
+          <NumberButton buttonStyle="number-button" text="5"/>
+          <NumberButton buttonStyle="number-button" text="6"/>
+          <NumberButton buttonStyle="number-button" text="1"/>
+          <NumberButton buttonStyle="number-button" text="2"/>
+          <NumberButton buttonStyle="number-button" text="3"/>
+          
+          <div class="inner-full-row">
+            <ActionButton buttonStyle="action-button" text="0"/>
+          </div>
+          
+        </div>
+        <div class="column-right">
+          <NumberButton buttonStyle="operator-button" text="÷"/>
+          <NumberButton buttonStyle="operator-button" text="x"/>
+          <NumberButton buttonStyle="operator-button" text="-"/>
+          <NumberButton buttonStyle="operator-button" text="+"/>
+          <NumberButton buttonStyle="operator-button" text="="/>
+        </div>
+        
+        
       </div>
     </div>
   );
