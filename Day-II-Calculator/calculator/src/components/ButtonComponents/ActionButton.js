d1d9a6d21d2488aee33adcf38c8fe6aac48bafaa
Austinlp4 <apendergrast44@gmail.com>
2018-08-28 16:00:12 -0500

    main project complete

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (props) => {
+    return (
+        <div className = {props.className}>
+            <h1 id = {props.id}>{props.text}</h1>
+        </div>
+    );
+}
+
+export default ActionButton;
