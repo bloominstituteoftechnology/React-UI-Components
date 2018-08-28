6cba039b697aa2278705072398fb93fc018284e0
Clint Kunz <clintkunz@gmail.com>
2018-08-28 17:12:44 -0600

    moved numbers on calc

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -6,15 +6,15 @@ const ActionButton = () => {
         <div className="action-buttons">
             <div className="clear">clear</div>
             <div className="numbers">
-                <div className="number">1</div>
-                <div className="number">2</div>
-                <div className="number">3</div>
-                <div className="number">4</div>
-                <div className="number">5</div>
-                <div className="number">6</div>
                 <div className="number">7</div>
                 <div className="number">8</div>
                 <div className="number">9</div>
+                <div className="number">4</div>
+                <div className="number">5</div>
+                <div className="number">6</div>
+                <div className="number">1</div>
+                <div className="number">2</div>
+                <div className="number">3</div>
             </div>
             <div className="zero">0</div>
         </div>
