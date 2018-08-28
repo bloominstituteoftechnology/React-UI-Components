73e473b251166152c991c38254478a37d3793fee
Alex <alexandraswartz@AlexandrasAir2.fios-router.home>
2018-08-28 18:55:31 -0400

    more margin changes

--- Day-II-Calculator/calculator/src/components/ButtonComponents/MathButtons.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/MathButtons.js
@@ -5,7 +5,7 @@ const MathButton = (props)=> {
     return (
         <div className='mathbtns'>
         <div className='mathbtn'>
-           <h2 className='mathbtntext'>/</h2>
+           <h2 className='mathbtntext'>÷</h2>
             <div className='bottombtns'>
                 <h2 className='mathbtntext'>x</h2>
                 <h2 className='mathbtntext'>-</h2>
