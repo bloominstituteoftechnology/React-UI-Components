6218b70905deb015543bc9274fe82bca5ff8fb9c
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-08-27 20:45:03 -0500

    completed content for Header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+
+const ImageThumbnail = () => {
+    return <img src='https://ibin.co/3whrpKSBbZ81.png' alt='LamdaLogo'/>
+}
+
+export default ImageThumbnail;
