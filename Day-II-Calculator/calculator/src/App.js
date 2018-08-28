2db45ac1d80b8ad938633fd684cb595adbd91c18
Kevin Nguyen <kevinn858@gmail.com>
2018-08-28 16:54:19 -0700

    got calculator looking close to design

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
@@ -21,28 +22,42 @@ const App = () => {
       </p>
 
       {/* Calculator */}
-      <div class='numbers'>
-        <div class='numbers 9to1'>
-          <NumberButton number='9' />
-          <NumberButton number='8' />
-          <NumberButton number='7' />
-          <NumberButton number='6' />
-          <NumberButton number='5' />
-          <NumberButton number='4' />
-          <NumberButton number='3' />
-          <NumberButton number='2' />
-          <NumberButton number='1' />
-        </div>
-        <div class='numbers 0'>
-          <NumberButton number='0' />
-        </div>
+      <div class='display'>
+        <CalculatorDisplay />
       </div>
-      <div class='actions'>
-        <ActionButton action='/' />
-        <ActionButton action='*' />
-        <ActionButton action='-' />
-        <ActionButton action='+' />
-        <ActionButton action='=' />
+      <div class='lower-section'>
+        <div class='left'>
+          <div class='actions clear'>
+              <ActionButton action='clear' />
+          </div>
+          <div class='numbers 9to1'>
+            <div class='row one'>
+              <NumberButton number='7' />
+              <NumberButton number='8' />
+              <NumberButton number='9' />
+            </div>
+            <div class='row two'>
+              <NumberButton number='4' />
+              <NumberButton number='5' />
+              <NumberButton number='6' />
+            </div>
+            <div class='row three'>
+              <NumberButton number='1' />
+              <NumberButton number='2' />
+              <NumberButton number='3' />
+            </div>
+          </div>
+          <div class='numbers 0'>
+            <NumberButton number='0' />
+          </div>
+        </div>
+        <div class='right'>
+          <ActionButton action='/' />
+          <ActionButton action='*' />
+          <ActionButton action='-' />
+          <ActionButton action='+' />
+          <ActionButton action='=' />
+        </div>
       </div>
     </div>
   );
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './Button.css';
 
 const ActionButton = (props) =>{
+    if(props.action==='clear'){
+        return(
+            <div class='action button clear'>{props.action}</div>
+        );
+    }
     return(
         <div class='action button'>{props.action}</div>
     );
