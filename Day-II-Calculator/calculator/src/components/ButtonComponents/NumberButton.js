3c7508809809d18d46f9405982661c9dd2d2fa2d
BrianARuff <thelonewolf198927@gmail.com>
2018-08-28 18:27:13 -0400

    calc functional

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './Button.css';
 
 export default function NumberButton(props) {
-    console.log(props)
+    function handleNumberClick() {
+        props.handleNumberClick(props.number);
+    }
     return (
-        <div className="number-button">
+        <div className="number-button" onClick={handleNumberClick}>
             <span className="button-span">{props.number}</span>
         </div>
     )
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -3,55 +3,93 @@ import './Display.css';
 import ActionButton from '../ButtonComponents/ActionButton';
 import NumberButton from '../ButtonComponents/NumberButton';
 
-export default function CalculatorDisplay() {
-    const action = {
-        divide: '/',
-        multiply: '*',
-        subtract: '-',
-        add: '+',
-        evaluate: '=', 
-        clear: 'Clear'
+export default class CalculatorDisplay extends React.Component {
+    
+    constructor(){
+        super();
+        this.state = {
+            expression: '',
+            evaluation: 0,
+        }
+        this.handleNumberClick = this.handleNumberClick.bind(this);
+        this.handleOperatorClick = this.handleOperatorClick.bind(this);
     }
 
-    function cssClass(className){
+    handleNumberClick = (num) => {
+        console.log(num);
+        let currentState = this.state.expression;
+        this.setState({
+            expression: currentState += num
+        })
+    }
+
+    handleOperatorClick = (action) => {
+        console.log(action);
+        let currentState = this.state.expression;
+        if (action === '='){
+            console.log(action);
+            let result = eval(this.state.expression);
+            this.setState({expression: '', evaluation: result});
+        } else if(action === 'clear'){
+            this.setState({expression: '', evaluation: ''})
+        } else {
+            this.setState({
+                expression: currentState += action
+            })
+        }
+    }
+
+    render(){
+
+        const action = {
+            divide: '/',
+            multiply: '*',
+            subtract: '-',
+            add: '+',
+            evaluate: '=', 
+            clear: 'clear'
+        }
+    
+        function cssClass(className){
+            return (
+                Object.keys(action).filter(c => c === className)
+            )
+        }
+
         return (
-            Object.keys(action).filter(c => c === className)
+            <div className="container">
+                <div className="evaluation">
+                    <h1 className="eval-number">
+                        {this.state.evaluation ? this.state.evaluation : this.state.expression}
+                    </h1>
+                </div>
+                <div className="row">
+                    <ActionButton action={action.clear} cssClass={cssClass('clear') + ' white'} handleOperatorClick={this.handleOperatorClick} />
+                    <ActionButton action={action.divide} entity="&divide;" cssClass={cssClass('divide')} handleOperatorClick={this.handleOperatorClick}  />
+                </div>
+                <div className="row">
+                    <NumberButton number={7} handleNumberClick={this.handleNumberClick} />
+                    <NumberButton number={8} handleNumberClick={this.handleNumberClick} />
+                    <NumberButton number={9} handleNumberClick={this.handleNumberClick} />
+                    <ActionButton action={action.multiply} entity="x" cssClass={cssClass('multiply')} handleOperatorClick={this.handleOperatorClick}/>
+                </div>
+                <div className="row">
+                    <NumberButton number={4} handleNumberClick={this.handleNumberClick} />
+                    <NumberButton number={5} handleNumberClick={this.handleNumberClick} />
+                    <NumberButton number={6} handleNumberClick={this.handleNumberClick} />
+                    <ActionButton action={action.subtract} cssClass={cssClass('subtract')} handleOperatorClick={this.handleOperatorClick}/>
+                </div>
+                <div className="row">
+                    <NumberButton number={1} handleNumberClick={this.handleNumberClick} />
+                    <NumberButton number={2} handleNumberClick={this.handleNumberClick} />
+                    <NumberButton number={3} handleNumberClick={this.handleNumberClick} />
+                    <ActionButton action={action.add} cssClass={cssClass('add')} handleOperatorClick={this.handleOperatorClick}/>
+                </div>
+                <div className="row">
+                    <NumberButton number={0} handleNumberClick={this.handleNumberClick} />
+                    <ActionButton action={action.evaluate} cssClass={cssClass('evaluate')} handleOperatorClick={this.handleOperatorClick}/>
+                </div>
+            </div>
         )
     }
-
-    return (
-        <div className="container">
-            <div className="evaluation">
-                <h1 className="eval-number">
-                    0
-                </h1>
-            </div>
-            <div className="row">
-                <ActionButton action={action.clear} cssClass={cssClass('clear') + ' white'} />
-                <ActionButton action={action.divide} entity="&divide;" cssClass={cssClass('divide')}  />
-            </div>
-            <div className="row">
-                <NumberButton number={7} />
-                <NumberButton number={8} />
-                <NumberButton number={9} />
-                <ActionButton action={action.multiply} entity="x" cssClass={cssClass('multiply')} />
-            </div>
-            <div className="row">
-                <NumberButton number={4} />
-                <NumberButton number={5} />
-                <NumberButton number={6} />
-                <ActionButton action={action.subtract} cssClass={cssClass('subtract')} />
-            </div>
-            <div className="row">
-                <NumberButton number={1} />
-                <NumberButton number={2} />
-                <NumberButton number={3} />
-                <ActionButton action={action.add} cssClass={cssClass('add')} />
-            </div>
-            <div className="row">
-                <NumberButton number={0} />
-                <ActionButton action={action.evaluate} cssClass={cssClass('evaluate')} />
-            </div>
-        </div>
-    )
 }
