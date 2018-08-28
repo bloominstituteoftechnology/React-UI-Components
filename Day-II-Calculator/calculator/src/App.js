542fe3446a660f114ec5971e55223fab40c90b79
Max <maxkajiwara@gmail.com>
2018-08-28 16:11:55 -0700

    State.total is passed to display

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -4,28 +4,37 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.
 import NumberButton from './components/ButtonComponents/NumberButton.js';
 import ActionButton from './components/ButtonComponents/ActionButton.js';
 
-const App = () => {
-  return (
-    <div className='calculator-container'>
-      <CalculatorDisplay />
-      <ActionButton buttonStyle='fat-action' buttonText='clear' />
-      <ActionButton buttonText='÷' />
-      <NumberButton buttonStyle='pixel-perfect-width' buttonText='7' />
-      <NumberButton buttonText='8' />
-      <NumberButton buttonText='9' />
-      <ActionButton buttonText='×' />
-      <NumberButton buttonStyle='pixel-perfect-width' buttonText='4' />
-      <NumberButton buttonText='5' />
-      <NumberButton buttonText='6' />
-      <ActionButton buttonText='−' />
-      <NumberButton buttonStyle='pixel-perfect-width' buttonText='1' />
-      <NumberButton buttonText='2' />
-      <NumberButton buttonText='3' />
-      <ActionButton buttonText='+' />
-      <NumberButton buttonStyle='fat-number' buttonText='0' />
-      <ActionButton buttonText='=' />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: 0
+    };
+  }
+
+  render () {
+    return (
+      <div className='calculator-container'>
+        <CalculatorDisplay total={this.state.total} />
+        <ActionButton buttonStyle='fat-action' buttonText='clear' />
+        <ActionButton buttonText='÷' />
+        <NumberButton buttonStyle='pixel-perfect-width' buttonText='7' />
+        <NumberButton buttonText='8' />
+        <NumberButton buttonText='9' />
+        <ActionButton buttonText='×' />
+        <NumberButton buttonStyle='pixel-perfect-width' buttonText='4' />
+        <NumberButton buttonText='5' />
+        <NumberButton buttonText='6' />
+        <ActionButton buttonText='−' />
+        <NumberButton buttonStyle='pixel-perfect-width' buttonText='1' />
+        <NumberButton buttonText='2' />
+        <NumberButton buttonText='3' />
+        <ActionButton buttonText='+' />
+        <NumberButton buttonStyle='fat-number' buttonText='0' />
+        <ActionButton buttonText='=' />
+      </div>
+    );
+  }
 };
 
 export default App;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Display.css';
 
-const CalculatorDisplay = () => {
+const CalculatorDisplay = (props) => {
   return (
-    <input type='text' pattern='[0-9]+' maxlength='10' placeholder='0' className='calculator-display' />
+    <input type='text' pattern='[0-9]+' maxlength='10' value={props.total} className='calculator-display' />
   );
 }
 
