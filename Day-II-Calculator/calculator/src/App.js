e31bc3420f6960488d75bd1ab246290643d0634f
MikeRGarrity <mikergarrity@gmail.com>
2018-08-28 16:37:48 -0500

    finished with MVP

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import NumberButton from './components/ButtonComponents/NumberButton';
 
-const App = () => {
+
+function App() {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator-container">
+      <CalculatorDisplay text="0"/> 
+      <ActionButton buttonStyle="clear-button" text="clear"/>
+      <ActionButton buttonStyle="action-button" text="÷"/>
+      <NumberButton buttonStyle="number-button" text="7"/>
+      <NumberButton buttonStyle="number-button" text="8"/>
+      <NumberButton buttonStyle="number-button" text="9"/>
+      <ActionButton buttonStyle="action-button" text="×"/>
+      <NumberButton buttonStyle="number-button" text="4"/>
+      <NumberButton buttonStyle="number-button" text="5"/>
+      <NumberButton buttonStyle="number-button" text="6"/>
+      <ActionButton buttonStyle="action-button" text="-"/>
+      
+      <NumberButton buttonStyle="number-button" text="1"/>
+      <NumberButton buttonStyle="number-button" text="2"/>
+      <NumberButton buttonStyle="number-button" text="3"/>
+      <ActionButton buttonStyle="action-button" text="+"/>
+      <NumberButton buttonStyle="zero-button" text="0"/>
+      <ActionButton buttonStyle="action-button" text="="/>
     </div>
   );
-};
+}
 
 export default App;
