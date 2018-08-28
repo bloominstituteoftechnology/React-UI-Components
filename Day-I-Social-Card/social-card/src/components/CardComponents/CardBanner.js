798a10ea774a33502ba273cef3ec3eaa17856c7e
lyang9 <lyang9@buffalo.edu>
2018-08-28 01:05:41 -0400

    Fixed typo on className

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const CardBanner = () => {
     return (
-        <img classNane='card-banner' src='https://ibin.co/3wnC6SgIOJud.png' alt='Background image'/>
+        <img className='card-banner' src='https://ibin.co/3wnC6SgIOJud.png' alt='Background image'/>
     );
 };
 
