43e78f5207aab66bc8799fcf44be37249fc4b5d8
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-08-28 17:27:39 -0400

    basic calculator layout complete, no styling

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import NumberButton from "./components/ButtonComponents/NumberButton";
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
+    <div className = 'wrapper-main'>
+      <div className = 'wrapper-sub'>
+        <CalculatorDisplay/>
+
+        <div className = 'button-wrap'>
+          <ActionButton type = 'act-button'/>
+          <NumberButton type = 'slash-button' text = '/'/>
+        </div>
+
+        <div className = 'button-wrap'>
+          <NumberButton type = 'num-button' value = '7'/>
+          <NumberButton type = 'num-button' value = '8'/>
+          <NumberButton type = 'num-button' value = '9'/>
+          <ActionButton type = 'act-button' value = 'X'/>
+        </div>
+
+        <div className = 'button-wrap'>
+          <NumberButton type = 'num-button' value = '4'/>
+          <NumberButton type = 'num-button' value = '5'/>
+          <NumberButton type = 'num-button' value = '6'/>
+          <ActionButton type = 'act-button' value = '-'/>
+        </div>
+
+        <div className = 'button-wrap'>
+          <NumberButton type = 'num-button' value = '1'/>
+          <NumberButton type = 'num-button' value = '2'/>
+          <NumberButton type = 'num-button' value = '3'/>
+          <ActionButton type = 'act-button' value = '+'/>
+        </div>
+
+        <div className = 'button-wrap'>
+          <NumberButton type = 'wide-button num-button' value = '0' />
+          <ActionButton type = 'act-button' value = '=' />
+        </div>
+
+      </div> // wrapper-sub
+    </div> // wrapper-main
   );
 };
 
