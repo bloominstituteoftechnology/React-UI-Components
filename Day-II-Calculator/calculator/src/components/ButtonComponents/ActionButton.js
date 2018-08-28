c963318a24e6a43a15f3d4777c59b694c5e58ef4
Austinlp4 <apendergrast44@gmail.com>
2018-08-28 16:05:16 -0500

    calc

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -3,8 +3,8 @@ import './Button.css';
 
 const ActionButton = (props) => {
     return (
-        <div className = {props.className}>
-            <h1 id = {props.id}>{props.text}</h1>
+        <div className ={props.className}>
+            <h1 id ={props.id}>{props.text}</h1>
         </div>
     );
 }
