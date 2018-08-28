f48b53905ab7e2b72f96ad8409af2d81f6649f62
Clint Kunz <clintkunz@gmail.com>
2018-08-28 14:39:57 -0600

    calculator with action buttons, operator buttons, and prop

---
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/OperatorButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Button.css';
+
+const OperatorButton = () => {
+    return (
+        <div className="operators">
+            <div className="operator division">%</div>
+            <div className="operator multiplication">x</div>
+            <div className="operator subtraction">-</div>
+            <div className="operator addition">+</div>
+            <div className="operator equal">=</div>
+        </div>
+    )
+}
+
+export default OperatorButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = (props) => {
+    return (
+        <div className="output-display">
+            <div className="number-display">{props.number.state}</div>
+        </div>
+    );
+};
+
+export default CalculatorDisplay;
