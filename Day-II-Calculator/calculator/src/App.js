f8772cdaf4ad6588bcfe56a4165236e4bd380924
Colin Preston <prestonresult@gmail.com>
2018-08-28 16:10:40 -0500

    calculator display/ some functionality

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,23 +1,57 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+class App extends React.Component {
+  // let number
+  constructor(props){
+    super(props);
+    this.state ={
+      total: 0
+    };
+
+  }
+  handleClickButton = (e) => {
+    let number = this.state.total;
+    if(e.target.outerText ==='clear'){
+      this.setState({
+        total: 0
+      })
+    }else{
+      this.setState({
+        total: Number(e.target.outerText) + number
+      })
+    }
+
+  };
+
+  render() {
+    return (
+      <div className='app'>
+        <CalculatorDisplay number={this.state.total} background='btn__xlong'/>
+        <ActionButton number='clear' onClick={this.handleClickButton} background='btn__long'/>
+        <NumberButton number='%' onClick={this.handleClickButton} background='btn__red'/>
+        <NumberButton number={9} onClick={this.handleClickButton}/>
+        <NumberButton number={8} onClick={this.handleClickButton}/>
+        <NumberButton number={7} onClick={this.handleClickButton}/>
+        <NumberButton number='X' onClick={this.handleClickButton} background='btn__red'/>
+        <NumberButton number='6' onClick={this.handleClickButton}/>
+        <NumberButton number='5' onClick={this.handleClickButton}/>
+        <NumberButton number='4' onClick={this.handleClickButton}/>
+        <NumberButton number='-' onClick={this.handleClickButton} background='btn__red'/>        
+        <NumberButton number='3' onClick={this.handleClickButton}/>
+        <NumberButton number='2' onClick={this.handleClickButton}/>
+        <NumberButton number='1' onClick={this.handleClickButton}/>
+        <NumberButton number='+' onClick={this.handleClickButton} background='btn__red'/>
+        <ActionButton number='0' onClick={this.handleClickButton} background='btn__long'/>
+        <NumberButton number='=' onClick={this.handleClickButton} background='btn__red'/>
     </div>
-  );
+    );
+    
+  }
+    
 };
 
 export default App;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (props) => {
+
+    return (
+        <button className={props.background} onClick={props.onClick}> {props.number} </button>
+    )
+}
+
+export default ActionButton;
