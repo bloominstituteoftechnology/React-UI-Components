cebe52306dc99bd39cc36f621b469bb84eca2b5c
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-08-28 15:50:48 -0600

    Finished MVP. No stretch goals. Used different items for action button because I didn't like the organization in the instructions.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (props) => (<button className={`action-${props.type}`}>{props.type}</button>);
+
+export default ActionButton;
