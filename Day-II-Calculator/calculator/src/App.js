1622ea5e9c298bdef857f727caedfa98112e9009
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-08-28 16:21:20 -0500

    Main Wrap CSS

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -10,34 +10,34 @@ const App = () => {
       <div className="inner-wrap">
         <CalculatorDisplay />
         <div className="button-wrap">
-          <ActionButton type="wide-button" value="clear" />
-          <NumberButton type="danger-button" value="/" />
+          <NumberButton type="wide-button" value="clear" />
+          <ActionButton type="danger-button" value="&#247;" />
         </div>
 
         <div className="button-wrap">
           <NumberButton type="number-button" value="7" />
           <NumberButton type="number-button" value="8" />
           <NumberButton type="number-button" value="9" />
-          <ActionButton type="danger-button" value="X" />
+          <ActionButton type="danger-button" value="&#215;" />
         </div>
 
         <div className="button-wrap">
           <NumberButton type="number-button" value="4" />
           <NumberButton type="number-button" value="5" />
           <NumberButton type="number-button" value="6" />
-          <ActionButton type="danger-button" value="-" />
+          <ActionButton type="danger-button" value="&#8722;" />
         </div>
 
         <div className="button-wrap">
           <NumberButton type="number-button" value="1" />
           <NumberButton type="number-button" value="2" />
           <NumberButton type="number-button" value="3" />
-          <ActionButton type="danger-button" value="+" />
+          <ActionButton type="danger-button" value="&#x2b;" />
         </div>
 
         <div className="button-wrap">
           <NumberButton type="wide-button number-button" value="0" />
-          <ActionButton type="danger-button" value="=" />
+          <ActionButton type="danger-button" value="&#x3d;" />
         </div>
       </div>
     </div>
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Button.css';
 
 const ActionButton = props => {
-  return <button className={`big-button ${props.type}`}>{props.value}</button>;
+  return (
+    <button className={`action-button ${props.type}`}>{props.value}</button>
+  );
 };
 
 export default ActionButton;
