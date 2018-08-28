169099bbd22e71f3878633f84dc34cfc5ec80c0d
Clint Kunz <clintkunz@gmail.com>
2018-08-28 17:07:41 -0600

    division sign instead of modulo sign

--- Day-II-Calculator/calculator/src/components/ButtonComponents/OperatorButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/OperatorButton.js
@@ -4,7 +4,7 @@ import './Button.css';
 const OperatorButton = () => {
     return (
         <div className="operators">
-            <div className="operator division">%</div>
+            <div className="operator division">/</div>
             <div className="operator multiplication">x</div>
             <div className="operator subtraction">-</div>
             <div className="operator addition">+</div>
