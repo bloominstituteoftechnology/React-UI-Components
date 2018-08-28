2e53ddcb2ac05a0e614678266fd3af2e86fc9205
decagondev <decagondevelopment@gmail.com>
2018-08-28 23:51:46 +0100

    (REACT UI DAY 2 - CALC) (STRETCH - EXTRA) : decided to test the apps reusability and made 2 calculators

--- Day-II-Calculator/calculator/src/index.js
+++ Day-II-Calculator/calculator/src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App clickfn="this.handleClick()"/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root2'));
