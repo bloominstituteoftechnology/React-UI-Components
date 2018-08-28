3ef32ec750a99a88323620b040bd23c0aba90d16
unknown <cornielleandres@gmail.com>
2018-08-28 15:22:37 -0400

    Complete calculator display.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
+    <div className="app">
+      <CalculatorDisplay />
+
+      <ActionButton buttonStyle="clear-btn" text="clear" />
+      
+      <ActionButton buttonStyle="operator-btn" text="÷" />
+      
+      <NumberButton buttonStyle="number-btn" text="7" />
+      <NumberButton buttonStyle="number-btn" text="8" />
+      <NumberButton buttonStyle="number-btn" text="9" />
+
+      <ActionButton buttonStyle="operator-btn" text="x" />
+
+      <NumberButton buttonStyle="number-btn" text="4" />
+      <NumberButton buttonStyle="number-btn" text="5" />
+      <NumberButton buttonStyle="number-btn" text="6" />
+
+      <ActionButton buttonStyle="operator-btn" text="-" />
+
+      <NumberButton buttonStyle="number-btn" text="1" />
+      <NumberButton buttonStyle="number-btn" text="2" />
+      <NumberButton buttonStyle="number-btn" text="3" />
+
+      <ActionButton buttonStyle="operator-btn" text="+" />
+      {/* <h3>Welcome to React Calculator</h3>
       <p>
         We have given you a starter project. You'll want to build out your
         components in their respective files, remove this code and replace it
@@ -15,7 +41,7 @@ const App = () => {
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
-      </p>
+      </p> */}
     </div>
   );
 };
