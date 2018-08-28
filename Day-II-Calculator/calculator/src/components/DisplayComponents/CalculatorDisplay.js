6ede5d3dcfc36fef06d4879479ae7dc89b4d5d38
Andrew <ghobrial.andrew@gmail.com>
2018-08-28 17:06:11 -0400

    Calculator display complete

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplayContainer = () => {
+    return (
+      <div className = "calcDisplay">
+        <p>0</p>
+      </div>
+    );
+  };  
+
+export default CalculatorDisplayContainer;
