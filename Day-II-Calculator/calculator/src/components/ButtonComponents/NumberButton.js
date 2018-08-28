c618e19abca1ba1b19df2f4546ff9efedb9d4be6
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-08-28 14:38:36 -0700

    restyling

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -5,12 +5,12 @@ import './Button.css';
 const NumberButton = () => {
     return (
       <div class= 'entrypad'>
-        <h1 class='bigbutton'>Clear</h1>
+        <h1 class='bigbutton'>clear</h1>
         <h2 class='symbol'>÷</h2>
         <h2>7</h2>
         <h2>8</h2>
         <h2>9</h2>
-        <h2 class='symbol'>X</h2>
+        <h2 class='symbol'>x</h2>
         <h2>4</h2>
         <h2>5</h2>
         <h2>6</h2>
@@ -19,7 +19,7 @@ const NumberButton = () => {
         <h2>2</h2>
         <h2>3</h2>
         <h2 class='symbol'>+</h2>
-        <h1 class='bigbutton'>0</h1>
+        <h1 class='bigbutton bold'>0</h1>
         <h2 class='symbol'>=</h2>
       </div>
     );
