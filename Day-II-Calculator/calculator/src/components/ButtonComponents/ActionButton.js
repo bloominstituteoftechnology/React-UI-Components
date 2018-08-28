24d88afa5504c4ace03ee7d47b55b5c4b682607f
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-08-28 16:16:40 -0400

    initial day 2 commit with yarn and ActionButton setup

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+function ActionButton(props) {
+  return (
+    <button className = {`button-large ${props.type}`}>{props.value}</button>
+  );
+};
+
+export default ActionButton;
