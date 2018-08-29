cd43570d4693f431ea731014ff71332857e40f68
Lucas Beemer <lucasbeemer@Lukes-MBP.hsd1.ca.comcast.net>
2018-08-29 12:54:46 -0700

    solution code

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,16 +1,13 @@
+
 import React from 'react';
 import './Button.css';
 
-const ActionButton = () => {
-    return (
-        <div className="actionButtons">
-            <h2>/</h2>
-            <h2>X</h2>
-            <h2>-</h2>
-            <h2>+</h2>
-            <h2>=</h2>
-        </div>
-    );
+const ActionButton = props => {
+  return (
+    <button className={`three-quarter-button ${props.className}`}>
+      {props.text}
+    </button>
+  );
 };
 
 export default ActionButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import './Display.css';
-import ActionButton from '../ButtonComponents/ActionButton';
-import NumberButton from '../ButtonComponents/NumberButton';
-
 
 const CalculatorDisplay = () => {
-    return (
-        <div className="calcDisplay">
-        <NumberButton />
-        <ActionButton />
-        </div>
-    );
+  return <div className="calc-total">0</div>;
 };
-
 
 export default CalculatorDisplay;
