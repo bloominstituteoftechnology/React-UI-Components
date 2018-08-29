d6a8649234d21987ea67e250a4761880a11059de
Tristen Neu <tristenneu@Tristens-MacBook-Pro.local>
2018-08-29 08:18:51 -0500

    Functional Components-II

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return (
+      <div className={`btn ${props.custom} ${props.weight}`}>{props.content}</div>
+    );
+  };
+   export default ActionButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return (
+      <div className="display-container">
+        <p className="display-content">{props.number}</p>
+      </div>
+    );
+  };
+   export default CalculatorDisplay;
