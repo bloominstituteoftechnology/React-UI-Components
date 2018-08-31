dc269ea2ae0a9baec6dd2a4dfe3078b7b2870aae
mich-shrak <micah.shrak@gmail.com>
2018-08-31 15:19:17 -0700

    Rendered components next step is CSS

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+	return (
+		<div className="cardcontent">
+			<div className="cardtitle">Get Started with React</div>
+			<div className="cardstatus">
+				React makes it painless to create interactive UIs. Design simple views
+				for each state in your application.
+			</div>
+			<a className="cardlink" href="https://www.reactjs.org" target="#">
+				reactjs.org
+			</a>
+		</div>
+	);
+};
+
+export default CardContent;
---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+	return (
+		<div className="cardfooter">
+			<i class="far fa-comment" />
+			<i class="fas fa-retweet" />
+			<i class="far fa-heart" />
+			<i class="far fa-envelope" />
+		</div>
+	);
+};
+
+export default Footer;
