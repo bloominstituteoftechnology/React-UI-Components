c04575dbed126160c243b849db02faf22bacfe28
Ray Lomeli <raylom96@gmail.com>
2018-08-31 15:15:08 -0500

    imported Containers and ran hello world in app.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+// component imports 
+import HeaderTitle from './HeaderTitle'
+import ImageThumbnial from "./ImageThumbnail";
+import HeaderContent from './HeaderContent'
+
+const HeaderContainer = props => {
+    return true;
+}
+
+export default HeaderContainer;
