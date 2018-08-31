0a1be13ed5fa39254a36cf182d32aaebd9f1b405
Ray Lomeli <raylom96@gmail.com>
2018-08-31 17:39:28 -0500

    imported moment.js for header timestamp

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Header.css";
+import moment from "moment";
 
 const HeaderTitle = proprs => {
   return (
     <div className="header-title-wrapper">
       {/* <div className="header-user-name">{props.name}</div>
       <div className="header-user-tag">{props.tagname}</div>
-      <div className="header-seperator">&bull;</div>
-      <div className="header-creation-date">{moment().format("MMM D")}</div> */}
+      <div className="header-seperator">&bull;</div> */}
+      <div className="header-creation-date">{moment().format("MMM D")}</div>
     </div>
   );
 };
