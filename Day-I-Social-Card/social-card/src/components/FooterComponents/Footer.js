b6ca1da1e148df7c977ff656f2a52e7d98e1f5cd
crypto-jones <integral.being@gmail.com>
2018-08-31 16:08:30 -0700

    implement like counter in footer

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import tweet from './images/twitter.jpg';
 import './Footer.css';
 
-const Footer = () => {
-  return <div />;
-};
+class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      likeCount: 0
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ likeCount: this.state.likeCount + 1 });
+  };
+
+  render() {
+    return (
+      <div className="footer-container">
+        <img
+          className="twitter-icon"
+          src={tweet}
+          alt="Twitter Logo"
+          onClick={this.handleClick}
+        />
+        <div>{this.state.likeCount}</div>
+      </div>
+    );
+  }
+}
 
 export default Footer;
