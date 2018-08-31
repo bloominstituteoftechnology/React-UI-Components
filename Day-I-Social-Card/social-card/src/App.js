92292d9175412b69bcd21ea6987258d96e55bf37
Ray Lomeli <raylom96@gmail.com>
2018-08-31 15:46:46 -0500

    updated app.css to include container for text

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -6,21 +6,15 @@ import Footer from './components/FooterComponents/Footer'
 
 
 const App = () => {
-  return <div > Hello World < /div>
+  return ( <
+    div className = "main-container" >
+    <
+    div > Hello World!! < /div>
+
+    <
+    /div>
+
+  )
 };
 
 export default App;
-
-/* <h3>Welcome to React Social Card!</h3>
-      <p>
-        Begin by exploring the `components` directory. You'll notice we have a
-        few files that we've already included in there to get you started right
-        away building components. You'll need to make sure you include your
-        components that you build in this file to watch your app come to life
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p> */
