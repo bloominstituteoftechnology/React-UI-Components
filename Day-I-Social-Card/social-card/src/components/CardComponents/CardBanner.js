1a44a8bc4eeae87ecc1a404cd717113614880e67
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2018-09-04 18:16:31 -0500

    Social Card Component

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+  return (
+    <a href="https://reactjs.org" className="banner-container-wrapper">
+      <img
+        src="https://ibin.co/3wnC6SgIOJud.png"
+        alt="react logo"
+        className="banner-img"
+      />
+    </a>
+  );
+};
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+  return (
+    <div className="thumb-wrapper">
+      <img
+        src="https://ibin.co/3whrpKSBbZ81.png"
+        alt="lambda thumbnail image"
+        className="thumb-img"
+      />
+    </div>
+  );
+};
+
+export default ImageThumbnail;
