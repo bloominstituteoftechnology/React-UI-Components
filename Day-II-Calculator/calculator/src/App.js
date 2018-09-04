68bc875948d1ac47a537285d17ec9b420d7ae22e
Ray Lomeli <raylom96@gmail.com>
2018-09-04 18:09:59 -0500

    added calculator buttons

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,12 +1,40 @@
 import React from "react";
 import "./App.css";
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import NumberButton from "./components/ButtonComponents/NumberButton";
 
 const App = () => {
   return (
-    <div className="main-container">
+    <div className="main-wrapper">
       <div className="inner-wrapper">
         <CalculatorDisplay />
+        <div className="btn-wrapper">
+          <ActionButton clickfn={this.handleClick} type="wide-btn" />
+          <ActionButton clickfn={this.handleClick} type="danger-btn" />
+        </div>
+        <div className="btn-wrapper">
+          <NumberButton clickfn={this.handleClick} type="num-btn" value="7" />
+          <NumberButton clickfn={this.handleClick} type="num-btn" />
+          <NumberButton clickfn={this.handleClick} type="num-btn" />
+          <ActionButton clickfn={this.handleClick} type="danger-btn" />
+        </div>
+        <div className="btn-wrapper">
+          <NumberButton clickfn={this.handleClick} type="num-btn" value="7" />
+          <NumberButton clickfn={this.handleClick} type="num-btn" />
+          <NumberButton clickfn={this.handleClick} type="num-btn" />
+          <ActionButton clickfn={this.handleClick} type="danger-btn" />
+        </div>
+        <div className="btn-wrapper">
+          <NumberButton clickfn={this.handleClick} type="num-btn" value="7" />
+          <NumberButton clickfn={this.handleClick} type="num-btn" />
+          <NumberButton clickfn={this.handleClick} type="num-btn" />
+          <ActionButton clickfn={this.handleClick} type="danger-btn" />
+        </div>
+        <div className="btn-wrapper">
+          <ActionButton clickfn={this.handleClick} type="wide-btn" />
+          <ActionButton clickfn={this.handleClick} type="danger-btn" />
+        </div>
       </div>
     </div>
   );
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+function NumberButton(props) {
+  return (
+    <button onClick={props.clickfn} className={`num-btn ${props.type}`}>
+      {" "}
+    </button>
+  );
+}
+
+export default NumberButton;
