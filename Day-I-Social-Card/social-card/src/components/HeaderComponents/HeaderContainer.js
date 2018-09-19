779cfc1799344c4d1803a0e0cbe741f233e0b787
Julie Jonak <juliejonak10@gmail.com>
2018-09-19 00:53:58 -0500

    Added main header components and some CSS styling. Will continue tomorrow building out the remainder of the social card

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Header.css';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
 
 
 const HeaderContainer = () => {
     return (
-        <HeaderTitle />
+        <div class="Header-Container">
+            <ImageThumbnail />
+            <div class="Text-Content">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
     )
 }
 
