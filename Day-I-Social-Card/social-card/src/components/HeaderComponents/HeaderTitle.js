009524d5a15c50b0d9e6c34483e632adbb166fb2
Rick Fleming <elfeming111@yahoo.com>
2018-09-18 21:00:50 -0700

    Created Header and Card components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = (props) => {
+    return (
+        <div>
+            <h1>Lambda School</h1>
+            <p>@LambdaSchool </p>
+            <ul><li>26 Jan</li></ul>
+        </div>
+        
+    )
+}
+
+export default HeaderTitle;
