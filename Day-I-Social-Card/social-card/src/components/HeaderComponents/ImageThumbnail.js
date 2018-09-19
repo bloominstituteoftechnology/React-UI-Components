ab1de3b29eb90fcbabbb2b065a20615dd389d226
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-09-18 21:32:34 -0600

    Bryce Evans

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return (
+        <div className="thumb-wrapper">
+            <img 
+            src="https://ibin.co/3whrpKSBbZ81.png" 
+            alt="thumbnail"
+            classname="thumb-img"
+            />
+        </div>
+    );
+};
+
+export default ImageThumbnail;
