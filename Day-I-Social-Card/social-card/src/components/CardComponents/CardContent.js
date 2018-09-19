26cb9d036f8f1a4cb2cd3f3621c6a579b30b7b37
paulapivat <paul.apivat@gmail.com>
2018-09-20 06:57:16 +0700

    add content to CardContainer, need styling

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div>
+            <h1>Get started with React</h1>
+            <p>React makes it painless to create interactive UIs. 
+                Design simple views for each state in your application.  </p>
+            <h4>reactjs.org</h4>
+        </div>
+    )
+}
+
+export default CardContent;
