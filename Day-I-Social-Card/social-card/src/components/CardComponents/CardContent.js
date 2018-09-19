ebd395d5be3e4a11e8e72eee8a64a040bf34d93d
Marguel Gutierrez <marguel.gtz@gmail.com>
2018-09-19 16:41:53 -0500

    card content component done

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,29 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+import CardBanner from "./CardBanner";
+
+const CardContainer = () => {
+  return (
+    <a href="https://www.reactjs.org">
+      <div className="card-banner">
+        <CardBanner />
+      </div>
+      <div className="text-container">
+        <div className="card-tittle">
+          <h1>Get Started with React</h1>
+        </div>
+        <div className="card-text">
+          <p>
+            React makes it painless to create interactive UIs. Design simple
+            views for each state in your application.
+          </p>
+        </div>
+        <div className="card-subtext">
+          <p>reactjs.org</p>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default CardContainer;
