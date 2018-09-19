fd9f0acf209802e6a7b392474137055e471e218a
Julie Jonak <juliejonak10@gmail.com>
2018-09-19 01:43:18 -0500

    Built Card contents and CSS styling

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
-    <HeaderContainer />
+    <div class="App-Container">
+      <HeaderContainer />
+      <CardContainer />
+    </div>
   );
 };
 export default App;
