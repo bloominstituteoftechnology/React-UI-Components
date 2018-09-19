4b7efd34f32774510075128f09eb5d646e835ec7
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-09-19 01:20:40 -0500

    finished implenting React for the HeaderComponents section added some css to the header section and body to begin developing layout

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+
+
+
+
+let HeaderTitle = (props) => {
+    return(
+      <div class="header-title">
+        <p>Lambda School</p>
+        <p>@Lambda School</p>
+        <ul><li>July 26</li></ul> 
+      </div>
+    );
+}
+
+
+export default HeaderTitle; 
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+
+let ImageThumbnail = (props) => {
+    return(
+        <img class="lambda-logo" src="https://ibin.co/3whrpKSBbZ81.png"/>
+    )
+}
+
+export default ImageThumbnail; 
