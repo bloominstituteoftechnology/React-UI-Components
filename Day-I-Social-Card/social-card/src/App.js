c693944b57808c891605a4301a5825d990442fbc
Kendra Williams <kmonique.williams@gmail.com>
2018-09-19 19:43:21 -0400

    container added to app

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
+    <div className="app-container">
     <HeaderContainer />
+    <CardContainer />
+    </div>
     /*<div>
       <h3>Welcome to React Social Card!</h3>
       <p>
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+import Banner from './CardBanner';
+import Content from './CardContent';
+
+const CardContainer = () =>{
+    return(
+        <div className="card-container">
+            <Banner />
+            <Content />
+        </div>
+    )
+};
+
+export default CardContainer;
