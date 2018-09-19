174b267f2864dfd91e5e503e64e0078f654754b1
Obed Lorisson <Obed.lorisson@gmail.com>
2018-09-19 01:57:20 -0400

    CardContainer Completed

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () =>{
+  return(
+    <div>
+      <h3>Get started with React</h3>
+      <p>React make it painless to create interactive UI.Design
+        simple views for each state in your application.
+      </p>
+      <a href="#">reactjs.org</a>
+    </div>
+  )
+}
+
+export default CardContent;
