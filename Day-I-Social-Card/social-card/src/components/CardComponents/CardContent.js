b1ef4cb0fa86fe4b4be03eb015825d9044062390
Eric Fleming <efleming111@yahoo.com>
2018-09-19 08:13:56 -0700

    Added styling to the card component

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,10 +3,10 @@ import './Card.css';
 
 const CardContent = (props) => {
     return (
-        <div>
+        <div className="card-content">
             <h2>Get started with React</h2>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-            <p>reactjs.org</p>
+            <p className="url-link">reactjs.org</p>
         </div>
     )
 }
