bceef6a76324215e19e35fefdce052593882444b
Andres Rivera <andres.rivera.toro@gmail.com>
2018-09-19 17:18:35 -0400

    imported HeaderTitle in HeaderContainer

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
+import HeaderTitle from './HeaderTitle';
 import './Header.css';
+
+const HeaderContainer = () => {
+  return (
+    <HeaderTitle />
+  )
+};
+
+export default HeaderContainer;
