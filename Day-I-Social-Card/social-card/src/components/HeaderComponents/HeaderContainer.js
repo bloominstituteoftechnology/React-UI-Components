9741028135f5d6982357d3aefab651f337cf511d
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-09-18 23:09:09 -0600

    HeaderTitle is displaying

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+
+const HeaderContainer = () => {
+    return (
+        <header>
+            <HeaderTitle />
+        </header>
+    );
+};
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = (props) => {
+    return (
+        <section>
+            <h1>Lambda School</h1>
+            <p>@LambdaSchool - 26 jan</p>
+        </section>
+    );
+};
+
+export default HeaderTitle;
