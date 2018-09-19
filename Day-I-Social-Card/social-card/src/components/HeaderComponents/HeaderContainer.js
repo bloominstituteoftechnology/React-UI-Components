ac2a5dedc5238d39050b2235d9b47aee29bcd51f
Mitchell Robles <mitchellr92@live.com>
2018-09-19 16:34:07 -0700

    created header components but having trouble getting yarn to work

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = (props) => {
+    return (
+        <div>
+            <HeaderTitle />
+            <HeaderContent />
+            <ImageThumbnail />
+        </div>
+    )
+};
+
+export default HeaderContainer;
