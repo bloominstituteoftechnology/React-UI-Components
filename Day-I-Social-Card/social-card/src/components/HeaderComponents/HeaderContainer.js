ad02f38b9d8a2d7c0669bc4348a4b671500594cf
Marguel Gutierrez <marguel.gtz@gmail.com>
2018-09-19 16:12:22 -0500

    header container and header content files set up

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,3 +1,13 @@
 import React from "react";
 import "./Header.css";
 import HeaderContent from "./HeaderContent";
+
+const HeaderContainer = () => {
+  return (
+    <div className="header-container">
+      <HeaderContent />
+    </div>
+  );
+};
+
+export default HeaderContainer;
