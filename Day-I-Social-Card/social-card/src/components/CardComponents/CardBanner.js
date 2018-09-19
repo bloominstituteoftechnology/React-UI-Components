b946dbdffd50a794950d1eb48ce530ac1681849f
Joseph Thompson <joe.thompson838@gmail.com>
2018-09-19 00:11:51 -0400

    fixed some minor issues with pixel perfect and finished the card component section

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+  return (
+    <section className='card-banner'>
+      
+    </section>
+  );
+}
+
+export default CardBanner;
