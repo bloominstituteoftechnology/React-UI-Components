8d4595baaf9aec1865e68cb0784907e2ef468dba
Emmanium <pradoe.ep@gmail.com>
2018-09-18 23:02:17 -0600

    created components for Header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './HeaderThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+
+HeaderContainer = () => {
+    return (
+        <div>
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+    )
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div>
+            <h1>Lambda School</h1>
+            <h3>@LambdaSchool</h3>
+            <h3>- 26 jan</h3>
+        </div>
+    )
+};
+
+export default HeaderTitle;
