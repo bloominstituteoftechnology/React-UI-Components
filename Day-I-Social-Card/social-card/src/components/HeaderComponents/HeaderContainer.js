260e4a49e15acc78b409150d7916e8b74858b6d8
BrandonMoll <brandonmoll85@gmail.com>
2018-09-18 23:00:08 -0500

    added single line to page through react and styled it

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = (props) => {
+    return (
+        <div className="header">This is a Test</div>
+    );
+}
+
+export default HeaderContainer;
