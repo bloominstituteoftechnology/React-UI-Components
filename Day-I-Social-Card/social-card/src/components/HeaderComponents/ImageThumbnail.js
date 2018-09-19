95e8167d423a0bcd6a51d25c6058bb580f653f60
Marguel Gutierrez <marguel.gtz@gmail.com>
2018-09-19 16:23:30 -0500

    Image Thumbnail component done

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,6 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const ImageThumbnail = () => {
+  return <img src="https://ibin.co/3whrpKSBbZ81.png" alt="" />;
+};
