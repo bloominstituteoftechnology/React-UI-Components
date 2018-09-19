ee09b5cd48e1ea0034a30226c334440f6e3ed663
paulapivat <paul.apivat@gmail.com>
2018-09-19 12:08:14 +0700

    just compiled something successfully from HeaderTitle -> HeaderContainer -> App.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = () => {
+    return (
+        <HeaderTitle />
+    )
+}
+
+export default HeaderContainer;
+
+//container lambda thumbnail
+//containers lambda tweet content
