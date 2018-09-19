a538bbaef12021a9f7043363541be6d8b2b2d095
Tommy Collison <tommy@collison.ie>
2018-09-19 14:27:03 -0700

    working on social card.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle => () { 
+    return (
+        <div>
+            <h1>Lambda School</h1>
+            <p>@LambdaSchool</p>
+            <p>-19 Sept</p>    
+        </div>
+    )
+};
+
+export default HeaderTitle;
