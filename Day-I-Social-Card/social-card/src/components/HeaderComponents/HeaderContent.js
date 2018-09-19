7fdfd326e2f58917fa920ebee66a7d901b678a34
Obed Lorisson <Obed.lorisson@gmail.com>
2018-09-19 01:37:56 -0400

    initial commit Created HeaderContainer Create ImageTHumbnail Created HeaderTitle Created HeaderContent

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = () =>{
+  return(
+    <div>
+      <p>let's learn react by building simple interfaces with componenents.Don't try to overthink it 
+        just keep simple and have fun.Once you feel comfortable using component you are well
+        on your way to mastering react.
+      </p>
+    </div>
+  )
+}
+
+export default HeaderContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () =>{
+  return(
+    <div>
+      <h3>Lambda School<span>@LambdaSchool . 26 jan </span></h3>
+    </div>
+  )
+}
+
+export default HeaderTitle;
