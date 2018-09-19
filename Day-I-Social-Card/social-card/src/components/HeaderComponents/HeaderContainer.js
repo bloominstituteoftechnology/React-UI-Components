88aefd213217b63b40367ef0b4fe15b50675b8a0
Pascale Pierre <pascaleee@gmail.com>
2018-09-19 18:47:59 -0500

    got logo to show up in header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ImageThumbnail from './ImageThumbnail'
 import './Header.css';
+
+
+class HeaderContainer extends Component {
+  render() {
+    return (
+      <div className="Navbar">
+
+          <ImageThumbnail />
+        </div>
+
+    );
+  }
+}
+
+
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.css';
+import logo from './img/lambda.png'; // Tell Webpack this JS file uses this image
+
+
+
+class ImageThumbnail extends Component {
+  render() {
+    return (
+      <div className="Image">
+
+      <img src={logo} alt="Logo" />
+
+        </div>
+
+    );
+  }
+}
+
+
+
+export default ImageThumbnail;
