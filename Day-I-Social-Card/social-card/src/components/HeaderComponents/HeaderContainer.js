4e17b9ffe2fa4eb832e9762b79c608d4647a50e1
Andres Rivera <andres.rivera.toro@gmail.com>
2018-09-19 17:59:52 -0400

    completed ImageThumbnail.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 import './Header.css';
 
 const HeaderContainer = () => {
   return (
-    <HeaderTitle />
+    <div className="header-container">
+      <ImageThumbnail />
+      <HeaderTitle />
+      <HeaderContent />
+    </div>
   );
 };
 
