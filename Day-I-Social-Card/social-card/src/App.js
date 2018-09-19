e2cfa19a04f5bd7fc796c04acf184c27be5c04f0
Kendra Williams <kmonique.williams@gmail.com>
2018-09-19 18:29:26 -0400

    header completed

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+
 const App = () => {
   return (
-    <div>
+    <HeaderContainer />
+    /*<div>
       <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
@@ -17,7 +20,7 @@ const App = () => {
           inside of this file in order to make them work.
         </strong>
       </p>
-    </div>
+    </div>*/
   );
 };
 
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderContent from "./HeaderContent";
+
+const HeaderTitle = (props) => {
+    return(
+        <div className="header-title">
+            <h1>Lambda School</h1>
+            <p className="grey">@LambdaSchool</p>
+            <p className="grey">- 26 jan</p>
+            <HeaderContent />
+        </div>
+    )
+};
+
+export default HeaderTitle;
