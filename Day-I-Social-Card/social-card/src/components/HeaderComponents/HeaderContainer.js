b36d7e3b8446f1706a4e853c30d7d4ca1d7525cc
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-19 22:21:40 -0400

    added import statements for ImageThumbnail and HeaderTitle

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Header.css';
 
+import ImageThumbnail from 'ImageThumbnail.js';
+import HeaderTitle from 'HeaderTitle';
+
 export const HeaderContainer = () => (
   <div>
     <ImageThumbnail />
