382613a9a56913dee88e60d15f2a5ab4ae7e3863
ShardeDeLane <rhcprocker@yahoo.com>
2018-09-19 20:14:18 -0400

    added reactjs.org in box

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -5,6 +5,7 @@ const CardContent = props => (
   <div className="card-container__card-content">
     <h2 className="header-container__h2 title-margin-small">Get started with React</h2>
     <p className="p-margin-small header-container__text-content">React makes it painless to create interactive UIS. Design simple views for each state in your application.</p>
+    <span className="header-container__title header-container__text-content">reactjs.org</span>
   </div>
 )
 
