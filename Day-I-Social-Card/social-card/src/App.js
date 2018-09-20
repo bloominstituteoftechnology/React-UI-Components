ba50be4b068e9511350ed9c9ddb757c7ce5430fb
kurtve <kurt@vanetten.us>
2018-09-20 00:53:05 -0400

    Added first components based on notes from help session given by Terrie

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
+        <HeaderContainer />
+    </div>
+  );
+};
+
+export default App;
+
+/*
       <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
@@ -13,12 +22,8 @@ const App = () => {
       </p>
       <p>
         <strong>
-          Don't forget to `default export` your components and import them here
+          Do not forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
       </p>
-    </div>
-  );
-};
-
-export default App;
+*/
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = () => {
+    return (
+        <HeaderTitle />
+    )
+};
+
+export default HeaderContainer;
