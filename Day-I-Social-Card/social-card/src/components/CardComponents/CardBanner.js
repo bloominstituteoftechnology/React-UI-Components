6ebf105a66ab3adb1c97621a78107f9626c7b418
Matthew Meitl <darksamus6@gmail.com>
2018-09-19 19:29:37 -0500

    initial card components

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardBanner = () => {
+    return (
+        <div><img src="../../../favicon.ico" alt="favicon"></img></div>
+    );
+}
+
+export default CardBanner;
