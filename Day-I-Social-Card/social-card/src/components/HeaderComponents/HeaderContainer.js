36966280009bf94a072b68b336aade2777659d06
Brandon G. Elzy <brandon.g.elzy@gmail.com>
2018-09-19 20:55:41 -0500

    Brandon G. Elzy - Initialize Project

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
+
+class HeaderContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+
 import './Header.css';
