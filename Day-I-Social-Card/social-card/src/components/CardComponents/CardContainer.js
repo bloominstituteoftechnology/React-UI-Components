0593851b3fbfb2794d38884fbb5b0a6dcd5f5a8b
Alex Ostrander <alexanderostrander23@gmail.com>
2018-09-20 19:22:13 -0400

    Completed MVP for day 1 social. Working on footer now

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 const CardContainer = () => {
 	return (
 		<div className="card-container">
+			<a href = "https://www.reactjs.org" target = "_blank">
 			<CardBanner />
+			<CardContent />
+			</a>
 		</div>
 	)
 }
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () => {
+	return (
+		<div className="card-content-container">
+			<h1 className="content-heading">Get started with React</h1>
+			<p className="content-paragraph">React makes in painless to create interactive UIs. Design simple views for each state in your application.</p>
+			<h3 className="content-h3">reactjs.org</h3>
+		</div>
+	)
+};
+
+export default CardContent;
