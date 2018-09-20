39014b29ecc56179a941871d67e2589b5a454676
Rick Fleming <elfeming111@yahoo.com>
2018-09-19 19:50:59 -0700

    Added footer components and styled footer

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = (props) => {
+    return (
+        <div className="footer">
+            <i className="fa fa-comment-o"></i>
+            <i className="fa fa-refresh"><span>6</span></i>
+            <i className="fa fa-heart-o"><span>4</span></i>
+            <i className="fa fa-envelope-o"></i>
+        </div>
+    )
+}
+
+export default Footer;
--- Day-I-Social-Card/social-card/src/index.js
+++ Day-I-Social-Card/social-card/src/index.js
@@ -4,3 +4,9 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const cardContainer = document.querySelector('.card-container');
+cardContainer.addEventListener('click', function(){
+    window.location.href = 'http://www.reactjs.org/';
+})
