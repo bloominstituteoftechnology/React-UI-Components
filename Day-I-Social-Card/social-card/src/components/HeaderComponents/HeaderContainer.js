365123d97bfd57976c69a6aff8fb6a078ab4e9e9
jbrock1981 <jbrock1981@gmail.com>
2018-09-19 19:37:04 -0500

    Jonathan Brock

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+	return (
+        <div>
+        <ImageThumbnail />   
+		<HeaderTitle />
+        <HeaderContent />
+        </div>
+	)
+};
+
+export default HeaderContainer;
