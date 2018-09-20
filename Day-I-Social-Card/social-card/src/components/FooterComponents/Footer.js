5cbfdbf314cdcbffbddf1d65c8f8eb16c563b7e2
BrandonMoll <brandonmoll85@gmail.com>
2018-09-19 22:52:26 -0500

    changed some icons

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,10 +4,10 @@ import './Footer.css';
 const Footer = (props) => {
     return (
         <div className="icons">
-            <i class="fas fa-comment"></i>
-            <i class="fas fa-sync-alt">6</i>
-            <i class="fas fa-heart">4</i>
-            <i class="fas fa-envelope"></i>
+            <i class="far fa-comment"></i>
+            <i class="fas fa-sync-alt"> 6</i>
+            <i class="far fa-heart"> 4</i>
+            <i class="far fa-envelope"></i>
         </div>
     );
 };
