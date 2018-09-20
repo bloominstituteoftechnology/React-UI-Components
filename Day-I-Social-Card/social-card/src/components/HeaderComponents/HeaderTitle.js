5b4cc14b81eff6ac228f6dc9ff4d19343b831bcf
ShardeDeLane <rhcprocker@yahoo.com>
2018-09-19 20:51:34 -0400

    added date

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.css';
 
+const getCurrentDate = () => {
+  const date = new Date()
+  return date.toDateString()
+}
 const HeaderTitle = props => (
   <h2 className="header-container__title title-margin-small">
     <span className="header-container__h2">LambdaSchool </span>
-    <span className="header-container__span">@LambdaSchool &middot; 26 jan</span>
+    <span className="header-container__span">@LambdaSchool &middot; {getCurrentDate()}</span>
   </h2>
 )
 
