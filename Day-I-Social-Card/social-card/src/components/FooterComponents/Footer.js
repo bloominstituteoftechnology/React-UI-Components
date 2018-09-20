f8cec768e55b3bbd86c332016f5d3cfa06bc6f65
Kendra Williams <kmonique.williams@gmail.com>
2018-09-19 21:23:25 -0400

    footer added

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <img src="https://cdn4.iconfinder.com/data/icons/large-black-icons/512/Message_chat_text_bubble_phone.png" alt="comment" />
+            <img src="https://cdn.iconscout.com/public/images/icon/premium/png-512/sync-refresh-clockwise-arrows-rotate-reload-synchronization-3be2bb3ee12944c6-512x512.png" alt="refresh" />
+            <img src="https://cdn4.iconfinder.com/data/icons/pyconic-icons-1-2/512/heart-outline-512.png" alt="heart" />
+            <img src="https://cdn4.iconfinder.com/data/icons/business-and-finance-vol-2/48/57-512.png" alt="message" />
+        </div>
+    )
+};
+
+export default Footer;
