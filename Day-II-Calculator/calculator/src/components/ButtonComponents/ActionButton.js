3e371ba30dafeba2b3322e151102c7a6b4638585
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-09-20 18:10:02 -0400

    Fleshing out the design/structure. Display, Clear button and Division sign currently rendered.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Button.css';
+
+const Clear = () => {
+    return(
+        <div className='rowZero'>
+            <div className='clearBtn'>
+            Clear
+             </div> 
+             <div className='divSignFlex' >÷</div>
+            
+       
+        
+        </div>
+       
+    )
+}
+
+export default Clear ;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Display.css';
+
+const Display = () => {
+    return (
+        <div className="calcBorder"> <div className="calcDisplay">0</div></div>
+       
+    )
+}
+
+export default Display ;
