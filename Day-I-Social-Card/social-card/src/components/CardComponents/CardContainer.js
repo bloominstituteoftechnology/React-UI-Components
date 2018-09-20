c21b5f304ada9fcebe233de973a7e5e68325e04a
Wesley Meeks <wesmeeks88@gmail.com>
2018-09-19 21:03:09 -0600

    Created card content component and cardBanner component and placed it in cardcontainer. need to link to app.js and style.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from '..CardComponents/CardBanner';
+import CardContent from '../CardComponents/CardContent';
+
+const CardContainer =() => {
+  return (
+    <div class = "card-link">
+      <a href = "https://reactjs.org/">
+       <div class = "card-container">
+          <CardBanner />
+          <CardContent />
+       </div>
+      </a>
+    </div>
+  )
+};
+
+export default CardContainer;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+ const CardContent = () => {
+   return (
+     <div class ='card-content'>
+       <h3> Get Started with React</h3>
+       <p>React makes it painless to create interactive UIs. Design simple views for each state of your application.</p>
+       <p>reactjs.org</p>
+      </div>
+   )
+ };
+
+ export default CardContent;
