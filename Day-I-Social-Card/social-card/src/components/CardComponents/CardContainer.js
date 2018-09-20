8ca0606bc4ede3381c29eeba8f605e50ff85e349
TheGameFreak720 <luis.alvarez@commithub.com>
2018-09-19 22:07:23 -0400

    when card container is clicked the browser will send the user to the react website

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -3,10 +3,14 @@ import './Card.css';
 import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
+function goToLink(url) {
+    window.location.href = 'https://www.reactjs.org'
+}
+
 
 const CardContainer = (props) => {
     return (
-        <section className='card-container'>
+        <section className='card-container' onClick={goToLink}>
             <CardBanner />
             <CardContent />
         </section>
