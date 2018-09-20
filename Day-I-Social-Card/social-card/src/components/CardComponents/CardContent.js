7b735cf042efec1b5db7981258ad81782f95b081
mglittleton <michaelglittleton@gmail.com>
2018-09-19 21:41:20 -0400

    Initial pull request
    Container skeletons are complete and ready for data.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = (props) => {
+    return (
+        <div>
+            CardContent
+        </div>
+    );
+};
+
+export default CardContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = (props) => {
+    return (
+        <div>
+            HeaderTitle
+        </div>
+    );
+};
+
+export default HeaderTitle;
