21b57781152cf6da748adcb304ed12ac12c24379
ShardeDeLane <rhcprocker@yahoo.com>
2018-09-20 16:28:53 -0400

    updated clear button style

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Button.css';
 
+// make the clear button font-weight light
+const fontWeight = val => {
+  return !isNaN(val) || val === '='
+}
 export const ActionButton = props => (
-  <div className="clear-button">{props.children}</div>
+  <div className={`clear-button ${
+    fontWeight(props.children) ? null: 'no-bold'
+  }`}>{props.children}</div>
 )
