9c70fa83ecc10a37c5b8e9c0911e6ee245a3bb75
FreeKeyBoy <jjhutton81@gmail.com>
2018-09-19 19:42:35 -0700

    Finished styling card components. Added clickable link to card.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,9 +3,11 @@ import './Card.css';
 
 const CardContent = () => {
 	return ( 
-		<div>
+		<div className="cardP">
 			<h1>Get started with React</h1>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <p className="org">reactjs.org</p>
+            <a href="https://reactjs.org"></a>
 		</div>
 	)
 };
