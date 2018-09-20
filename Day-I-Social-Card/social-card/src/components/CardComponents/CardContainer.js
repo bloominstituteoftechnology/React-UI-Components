bf9b220cc7ec2a21bfef4eaa6208b49c80fe7925
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-19 23:11:35 -0400

    added import for CardBanner added css class name added reactjs.org url

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner.js';
+
+const CardContainer = () => (
+  <a className='card-container' href='https://www.reactjs.org'>
+    <CardBanner />
+  </a>
+);
