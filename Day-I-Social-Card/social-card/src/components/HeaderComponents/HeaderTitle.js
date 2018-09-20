5f704308421a13a9729ac34a67c9a54abc64e602
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2018-09-19 23:44:51 -0500

    Finished project with main objectives done still need to work on stretch goals

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className= "tittle">
+            <h1>Lambda School</h1>
+            <span>@LambdaSchool • 26 jan</span>            
+        </div>    
+    )
+}
+
+export default HeaderTitle;
