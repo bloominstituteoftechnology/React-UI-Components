ca039fc2772a4a10385ed06960d5d5ad2a5b1bf9
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-09-19 23:56:34 -0400

    Working on properly inserting link.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,13 +4,16 @@ import './Card.css';
 const CardContent = () => {
     return (
         <div className="lowerContent" >
-        <h5 className='ReactHeader'>Get Started With React</h5>
+        <a href="https://reactjs.org/"> <h5 className='ReactHeader'>Get Started With React</h5>
             <p className="lowerPara">
                 
                 React makes it painless to create interactive UIs. Design simple views for each state in your application.
-            </p>
+            </p></a>
+       
         </div>
     )
 }
+
+
 
 export default CardContent ;
