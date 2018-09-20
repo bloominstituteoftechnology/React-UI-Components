7bbdb08d90557250c996f89cd90faa0120072af7
hraya <hraya@lssu.edu>
2018-09-19 22:42:18 -0500

    COmpleted the Card Content
    Input all components into the Card Container
    
    Completed styling the Middle card section

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardBanner = () => {
+    return (
+        <div>
+          <img className='cardBannerImg' src='https://ibin.co/3wnC6SgIOJud.png'/>
+        </div>
+    );
+};
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () => {
+    return (
+        <div className='cardContentContainer'>
+            <h1 className='cardContentHeader'>Get started with React</h1>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <p className='cardWebSite'>reactjs.org</p>
+        </div>
+    );
+};
+
+export default CardContent;
