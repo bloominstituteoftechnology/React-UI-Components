748e3464cc283bd7f009c23f2cd91141a54fd0f7
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-19 23:50:53 -0400

    added the link that takes us to the react site

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Card.css';
 
+const click = () => {
+    window.open('https://reactjs.org/', '_blank');
+}
 const CardContent = () => {
     return (
-        <div className="Card-Content">
+        <div className="Card-Content" onClick={click}>
             <p className="bold">Get started with React</p>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
             <p className="gray">reactjs.org</p>
