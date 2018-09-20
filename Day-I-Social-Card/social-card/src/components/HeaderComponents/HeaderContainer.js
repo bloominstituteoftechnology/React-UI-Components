c87bd25ea76072fb45d3508ca4b5d04858cae1a2
Phinehas90 <kohler.nai@gmail.com>
2018-09-19 20:25:22 -0600

    [x] Created HeaderContainer component [] Create ImageThumbnail [] Create HeaderContent [] Create CardContainer [] Create CardContent

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return (
+        <div>
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+    );
+};
+export default HeaderContainer;
