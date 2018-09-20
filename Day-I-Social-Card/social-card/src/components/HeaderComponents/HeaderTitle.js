40d0d5369f138f88eede68410ee88574a5360f04
Rob Salzberg <robsalzberg@gmail.com>
2018-09-19 21:34:01 -0400

    Header complete except for styling and correct timestamp

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = (props) => {
+  return (
+    <div className = 'headertitle'>
+      <h1>Lambda School</h1>
+      <p className = 'th'>@LambdaSchool</p>
+      <p className = 'date'>•Jan 26</p>
+     
+    </div>
+  );
+}
+
+export default HeaderTitle;
