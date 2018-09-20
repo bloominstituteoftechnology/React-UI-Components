34cfa81036f88dcce6edb14240325461654e9288
Ken Barlow <kenbrlow@gmail.com>
2018-09-19 22:26:07 -0700

    Finished the top main content sizing.

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -28,7 +28,7 @@ const HeaderTitle = () => {
 };
 
 const HeaderContent = () => {
-  return (<div>
+  return (<div className="hc-main-content">
     <p>Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
   </div>);
 };
