474c03ea960242f42cea471c125accf293453c36
Jennifer King <jenbellpgh@gmail.com>
2018-09-19 21:59:27 -0400

    Got header text put in, again using props so that it can be changed later.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 
 const HeaderContainer = (props) => {
     return (
         <div>
             <ImageThumbnail />
             <HeaderTitle name="Lambda School" handle="LambdaSchool" date="26 jan"/>
+            <HeaderContent text="Let's learn React by building simple interfaces with components. 
+                Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable
+                using components you are well on your way to mastering React!" />
         </div>
     );
 }
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = (props) => {
+    return (
+        <div className="headerText">{props.text}</div>
+    );
+};
+
+export default HeaderContent;
