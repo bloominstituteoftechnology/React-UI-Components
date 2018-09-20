eb0711d61581eabbb54d5104bdc6af7e724bbb65
Chris Juelfs <CRJuelfs24@comcast.net>
2018-09-20 15:44:06 -0500

    Header components completed

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(){
+    return (
+        <div className = 'header-title-info'>
+            <h1>Lambda School</h1>
+            <span>@LambdaSchool</span>
+            <span>26Jan</span>
+        </div>
+    );
+};
+
+export default HeaderTitle;
