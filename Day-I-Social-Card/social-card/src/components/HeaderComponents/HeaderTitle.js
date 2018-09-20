1f7df445c71ae8be13b0fba9e06f93efb498ed0c
Jess <jessdev83@gmail.com>
2018-09-19 21:29:19 -0400

    Heading component
    Heading components built and styled.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return ( 
+    <div className='header-title'>
+        <h1>Lambda School</h1>
+        <p>@LambdaSchool</p> 
+        <p>26 Jan</p> 
+    </div>
+    )
+};
+
+export default HeaderTitle;
