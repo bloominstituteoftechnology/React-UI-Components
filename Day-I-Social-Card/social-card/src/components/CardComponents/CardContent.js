068f4770beef8974e4893a792b16036b0146dfa2
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-09-19 20:28:45 -0600

    Card-banner css done

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,7 +4,7 @@ import './Card.css';
 const CardContent = () => {
     return (
         <section className="card-content">
-            <h1>Get Started With React</h1>
+            <h2>Get started with React</h2>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
             <a href="http://reactjs.org">reactjs.org</a>
         </section>
