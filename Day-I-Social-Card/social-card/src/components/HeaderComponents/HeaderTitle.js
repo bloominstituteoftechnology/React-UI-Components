9906a957ce6ae9dc7c8cc1d48a2c3f20713114a3
jryel1970 <jryel1970@gmail.com>
2018-09-20 12:16:05 -0400

    Having issues importing from other files. Only works if I am within the same files.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+let HeaderTitle = (props) => {
+	return (
+			<div class = "header-title">
+				<p>Lambda School</p>
+				<p>@Lambda School</p>
+				<ul><li>September 20</li></ul>
+			</div>
+		);
+}
+
+export default HeaderTitle;
