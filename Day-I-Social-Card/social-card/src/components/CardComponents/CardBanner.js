6de546e86c4eae3c930fcebe028cfc114f41c706
Glenn-David Daniel <d2rd@outlook.com>
2018-09-19 21:55:32 -0700

    WIP changed <ImageThumbnail> tag to <img>

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Card.css';
+import ImageThumbnail from '../HeaderComponents/ImageThumbnail';
 class CardBanner extends React.Component {
   render() {
-    return <h1>CardBanner stuff</h1>;
+    return (<h1>CardBanner stuff</h1>,
+    <ImageThumbnail src="https://ibin.co/3wnC6SgIOJud.png"></ImageThumbnail>)
+    }
   }
-}
 
 export default CardBanner;
