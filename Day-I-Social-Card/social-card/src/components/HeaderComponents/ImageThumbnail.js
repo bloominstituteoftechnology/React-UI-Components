de1a30c84c5a8ca42ade11d84d5c811be9a29595
Abigayle Franklin <abifanklin@gmail.com>
2018-09-19 21:27:13 -0500

    Header content displaying, not styled yet

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+import logo from "./img/lambda.png";
+
+const ImageThumbnail = () => {
+	return (
+		<img src={logo} />
+	)
+};
+
+export default ImageThumbnail;
