39b93358f29940a74881fc72ac0cb7fd40191e31
Wesley Meeks <wesmeeks88@gmail.com>
2018-09-19 20:11:24 -0600

    Initial commit- read through readme, and created my header title component, imported it to header container and imported that to apps.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = () => {
+  return (
+    <HeaderTitle/>
+  )
+};
+
+export default HeaderContainer;
