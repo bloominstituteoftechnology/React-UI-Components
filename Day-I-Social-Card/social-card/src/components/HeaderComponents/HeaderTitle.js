fee5ea4c7039b04f12cb5d7328d215a00eac1900
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-19 23:05:18 -0400

    made a couple styling changes to the header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -4,9 +4,9 @@ import './Header.css';
 const HeaderTitle = () => {
     return (
         <div className= "Header-Title">
-            <p className="name-of-school">Lambda School</p>
-            <p className="gray-color">@LambdaSchool</p>
-            <p className="gray-color">&#5867; 26 Jan</p>
+            <p className="bold">Lambda School</p>
+            <p className="gray-margin">@LambdaSchool</p>
+            <p className="gray-margin">&#5867; 26 Jan</p>
         </div>
     )
 };
