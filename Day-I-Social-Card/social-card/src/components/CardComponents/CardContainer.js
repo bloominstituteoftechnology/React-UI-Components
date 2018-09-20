75f3e7df7ac710bd5a7f57a602cb19212f335b56
Kendra Williams <kmonique.williams@gmail.com>
2018-09-19 20:07:30 -0400

    standard goals completed

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -6,7 +6,7 @@ import Content from './CardContent';
 
 const CardContainer = () =>{
     return(
-        <div className="card-container">
+        <div className="card-container" onClick={() => window.open("https://reactjs.org/")}>
             <Banner />
             <Content />
         </div>
@@ -14,3 +14,24 @@ const CardContainer = () =>{
 };
 
 export default CardContainer;
+
+// This way does not work in react:
+// Instead use onClick: onClick must take in
+// a function to avoid running when the page is redered
+
+// class PageLink{
+//     constructor(el){
+//         this.el = el;
+//         this.el.addEventListener("click", () =>{
+//             this.redirect();
+//         });
+//     }
+//     redirect(){
+//         window.open("www.reactjs.org");
+//     }
+// }
+
+// let linkArea = document.querySelector(".card-container");
+// console.log(linkArea);
+// linkArea = new PageLink(linkArea);
+// console.log(linkArea);
