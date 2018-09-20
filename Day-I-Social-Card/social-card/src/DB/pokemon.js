84049fc00ca28c667e04a92085fd12b8347cb3c3
TheGameFreak720 <luis.alvarez@commithub.com>
2018-09-19 23:54:25 -0400

    fixed css so it looks nice and clean on both cards

--- Day-I-Social-Card/social-card/src/DB/pokemon.js
+++ Day-I-Social-Card/social-card/src/DB/pokemon.js
@@ -1,9 +1,9 @@
 const pokemon = {
-    userName: 'Ash',
+    userName: 'Ash Ketchum',
     profileIcon: 'https://orig00.deviantart.net/1d83/f/2013/104/9/a/profile_picture_by_askrp_ash_ketchum-d61rak5.jpg',
-    userLink: '@Ash',
+    userLink: '@AshKetchum',
     postDate: '. May 1',
-    postHeaderText: 'Let\'s catch them all',
+    postHeaderText: 'Pokem ipsum dolor sit amet Scolipede Venonat in voluptate velit esse cillum dolore eu fugiat nulla pariatur Hoothoot Steel Mew. Steel Claydol Lotad Stunky Slaking Kangaskhan Jesse.',
     url: 'https://www.pokemongo.com/en-us/',
     banner: 'https://images6.alphacoders.com/638/thumb-1920-638835.jpg',
     postTitle: 'Pokemon Go events',
