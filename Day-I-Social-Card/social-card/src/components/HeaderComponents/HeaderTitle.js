e2a2dffd706063ffdf060be701fc8873b1954ca8
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-19 22:38:16 -0400

    added HeaderTitle content

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => (
+  <div>
+    <h1>Lambda School</h1>
+    <p>@LambdaSchool</p>
+    <p>26 jan</p>
+  </div>
+);
+
+export default HeaderTitle;
