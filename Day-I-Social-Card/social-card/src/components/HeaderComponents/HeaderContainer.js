9a172a093b9b58fd8399a6684d5944c42327827c
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-19 22:45:05 -0400

    Corrected import file paths for HeaderTitle and ImageThumbnail

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Header.css';
 
-import ImageThumbnail from 'ImageThumbnail.js';
-import HeaderTitle from 'HeaderTitle';
+import ImageThumbnail from './ImageThumbnail.js';
+import HeaderTitle from './HeaderTitle';
 
 const HeaderContainer = () => (
   <div>
