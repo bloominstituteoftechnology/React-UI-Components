fd4e454353b208be81e11add0d20e3b54d6b0696
Julie Jonak <juliejonak10@gmail.com>
2018-09-19 22:41:50 -0500

    Made card banner clickable to react website

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -4,7 +4,9 @@ import './Card.css';
 
 const CardBanner = () => {
     return (
+        <a href="https://www.reactjs.org">
         <img className="Card-Banner" src="https://ibin.co/3wnC6SgIOJud.png" alt="Background Banner"/>
+        </a>
     )
 };
 
