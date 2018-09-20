3f02153b526c708b03a962aef23a2c2c761a1c6a
Rob Salzberg <robsalzberg@gmail.com>
2018-09-19 22:12:32 -0400

    Card Components Added

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer.js'
+import CardContainer from './components/CardComponents/CardContainer.js'
 
 const App = () => {
   return (
     <div>
+    <div>
       <HeaderContainer /> 
     </div>
+    <div>
+      <CardContainer />
+    </div>
+    </div>
   );
-};
+}
 
 
 export default App;
