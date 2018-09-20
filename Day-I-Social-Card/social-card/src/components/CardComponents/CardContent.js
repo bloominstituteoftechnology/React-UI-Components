056fc496072e7e6b2afde55d349307176d317eeb
Griffin <griffinshuth@gmail.com>
2018-09-19 21:41:17 -0600

    finished all the JS

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return(
+        <div>
+        <h2>Get started with React</h2>
+
+        <p>Reach makes it painless to create interactive UIs.  Design simple views for each state in your application.</p>
+
+        <h4>reactjs.org</h4>
+        </div>
+    )
+
+};
+
+export default CardContent;
