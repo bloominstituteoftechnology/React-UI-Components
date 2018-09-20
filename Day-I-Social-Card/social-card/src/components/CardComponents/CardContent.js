c5839102716c74f59d4bf76837b2fa9c3b596c98
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-19 23:06:00 -0400

    added the structure and styling for the card component

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className="Card-Content">
+            <p className="bold">Get started with React</p>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <p className="gray">reactjs.org</p>
+        </div>
+    );
+};
+
+export default CardContent;
