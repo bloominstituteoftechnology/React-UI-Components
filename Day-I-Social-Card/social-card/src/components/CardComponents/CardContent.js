d00acb7d43248bf3da966ae735bc2c8d82944edf
stevekutz <stkutz@gmail.com>
2018-09-20 00:31:23 -0500

    completed MVP requirements

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+  return (
+    <div className = 'cardInfo'>
+      <h1> Get Started with React</h1>
+      <p className = 'cardText'> React makes ot painless to create interactive UIs. Design
+      simple views for each state in your application</p>
+      <a className = 'reactLink' href = 'https://www.reactjs.org'> reactjs.org</a>
+
+    </div>
+
+
+  )
+
+};
+
+export default CardContent;
