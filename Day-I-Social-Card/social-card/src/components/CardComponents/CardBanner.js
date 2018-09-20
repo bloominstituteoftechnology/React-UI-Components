2aa594111e41bc4fe5c2b252217750820b343c87
Denise Escobar <descobar1490@gmail.com>
2018-09-19 22:40:53 -0500

    Created and added my CardContainer component
    -I created the CardBanner Component that is just a div that will have a background image
    -I created a CardContent component with an h1 and two p tags
    -I put all of those components within an a tag in the container

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () =>{
+    return(
+        <div>
+        This will be an image;    
+        </div>
+    )
+};
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './Header.css';
 import HeaderTitle from './HeaderTitle';
 import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+
 
 const HeaderContainer = () => {
     return (
         <div>
-            <ImageThumbnail />
-            <HeaderTitle />
+            <div>
+                <ImageThumbnail />
+                <HeaderTitle />
+            </div>
+            <div>
+                <HeaderContent />
+            </div>
         </div>
     )
 };
