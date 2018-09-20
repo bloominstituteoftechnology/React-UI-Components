ef168694794820423b376d602aadab756093ccf2
Robin U Familara <robin.familara@cgius.com>
2018-09-19 21:11:18 -0500

    Styled the html and headed portion

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
 import HeaderTitle from './HeaderTitle';
- const HeaderContainer =  () => {
-    return (
-
-     <HeaderTitle />
-    );
-  };
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from "./ImageThumbnail";
+const HeaderContainer =  () => {	const HeaderContainer =  () => {
+    return (	    return <div className="header">
+         <ImageThumbnail />
+    <HeaderTitle />	        <HeaderTitle />
+    );	        <HeaderContent />
+      </div>;
    export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+
 const HeaderTitle = () => {
-	return (
-		<div>
-			<h1>Lambda School</h1>
-			<p>@LambdaSchool</p>
-			<p>-26 Jan</p>
-		</div>
-	)
-};
+    return (
+        <div>
+        <div className="lambdaHeader">
+          <h1> Lambda School</h1> <h3>@LambdaSchool</h3> <h3>&bull; 19 sept</h3>
+        </div>
+      </div>
+    );
+    }
 
 export default HeaderTitle;
