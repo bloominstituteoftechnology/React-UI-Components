dd4da844a32e87643532cab8fcf480a221163839
TheGameFreak720 <luis.alvarez@commithub.com>
2018-09-19 21:25:02 -0400

    did main card container components

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+
+const CardContainer = (props) => {
+    return (
+        <section className='card-container'>
+            <CardBanner />
+            <CardContent />
+        </section>
+    );
+};
+
+export default CardContainer;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = (props) => {
+    return (
+        <section>
+            <p className='title'>Get started with React</p>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application</p>
+            <p className='tag'>reactjs.org</p>
+        </section>
+    );
+};
+
+export default CardContent;
