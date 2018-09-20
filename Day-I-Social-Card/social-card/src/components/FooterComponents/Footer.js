4f9831d68375ffb09e40d9944b031b6bc7f570d1
Joseph Thompson <joe.thompson838@gmail.com>
2018-09-20 00:14:21 -0400

    added functionality to the icons at bottom and excluded mail to be unclickable

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,10 +5,10 @@ import Icon from './Icon';
 const Footer = (props) => {
   return (
     <section className='footer'>
-      <Icon icon='icon far fa-comment fa-2x' upvotes={2}/>
-    <Icon icon='icon fas fa-sync fa-2x' upvotes={6}/>
-  <Icon icon='icon far fa-heart fa-2x' upvotes={4}/>
-<Icon icon='icon far fa-envelope fa-2x' upvotes=''/>
+      <Icon icon='icon far fa-comment fa-2x' upvotes={2} id='comment'/>
+      <Icon icon='icon fas fa-sync fa-2x' upvotes={6} id='share'/>
+      <Icon icon='icon far fa-heart fa-2x' upvotes={4} id='like'/>
+      <Icon icon='icon far fa-envelope fa-2x' id='mail'/>
     </section>
   );
 }
--- Day-I-Social-Card/social-card/src/components/FooterComponents/Icon.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Icon.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './Footer.css';
 
-const addToUpvote = () => {
-  console.log('it works on click');
+const addToUpvote = (element) => {
+  this.id = element.target.id;
+  const icons = document.querySelectorAll('.icon');
+
+  icons.forEach((icon) => {
+    if(icon.id === this.id) {
+      // do not allow clicking for mail
+      if(this.id === 'mail') {
+        return null;
+      }
+      else {
+      let currentNumber = icon.nextSibling.innerHTML;
+      let result = Number(currentNumber) + 1;
+      icon.nextSibling.innerHTML = result;
+      }
+    }
+  })
+
 }
 
 const Icon = (props) => {
   return (
     <section className='icon-area'>
-      <i className={props.icon} onClick={addToUpvote}></i>
+      <i className={props.icon} onClick={addToUpvote} id={props.id}></i>
     <p className='upvotes'>{props.upvotes}</p>
     </section>
   );
