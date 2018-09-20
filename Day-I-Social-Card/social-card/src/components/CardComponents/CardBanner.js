b8e2366c03fe513fba379e9a3cf2febe7cc45e67
FreeKeyBoy <jjhutton81@gmail.com>
2018-09-19 18:32:35 -0700

    Forgot to commit and push several hours ago. My bad yo. Finished the header components. Finished the js for card components. Started styling card components.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+	return ( 
+		<div>
+			<img className="backgroundImg" src="https://ibin.co/3wnC6SgIOJud.png"></img>
+		</div>
+	)
+};
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+	return (
+        
+		<div className="title">
+			<h1>Lambda School</h1>
+			<p>@LambdaSchool</p>
+			<p>-26 Jan</p>
+		</div>
+	)
+};
+
+export default HeaderTitle;
