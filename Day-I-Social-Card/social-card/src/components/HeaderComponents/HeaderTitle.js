ba6c6ce43295db886b22ef43faea23cbc8683937
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-09-19 21:59:09 -0500

    Added js to get current date showing up

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './Header.css';
 
+let day = new Date().getDate();
+let month = new Date().getMonth();
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+];
+
 const HeaderTitle = props => {
   return (
     <h1>
-      Lambda School <span>@LambdaSchool - 26 Jan</span>
+      Lambda School{' '}
+      <span>
+        @LambdaSchool • {day} {months[month]}
+      </span>
     </h1>
   );
 };
