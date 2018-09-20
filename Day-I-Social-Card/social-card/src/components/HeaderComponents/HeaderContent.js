68c61d4eb2c672e98074e79bbc90bc7d7c9300f9
hraya <hraya@lssu.edu>
2018-09-19 21:40:50 -0500

    Completed Styling The Header Content
    Corrected and moved some components around to follow the outline

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import  HeaderTitle from './HeaderTitle';
 
 
 
 const HeaderContent = () => {
     return (
         <div className='headerContent'>
+        <HeaderTitle/>
             <p>
                 Let's learn React by building simple interfaces with components.
                 Don't try to overthink it, just keep it simple and have fun.
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './Header.css';
-import './ImageThumbnail';
-import ImageThumbnail from './ImageThumbnail';
+
 
 
 const HeaderTitle = () => {
     return (
         <div className='headerTitleContainer'>
-            <ImageThumbnail/>
             <h1 className='headerMainTitle'>Lambda School</h1>
-            <p className='socialName'>@Lambda School</p>
-            <p className='date'>•26 Jan</p>
+            <p className='socialName'>@LambdaSchool</p>
+            <p className='date'>• 26 Jan</p>
         </div>
     );
 };
