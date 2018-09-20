d6329702206e6d6bd911a51a88f93175ba831cba
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-09-19 20:02:54 -0600

    Card-banner css done

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const CardBanner = () => {
     return (
-        <img src="https://ibin.co/3wnC6SgIOJud.png" />
+        <img className="card-banner" src="https://ibin.co/3wnC6SgIOJud.png" />
     );
 };
 
