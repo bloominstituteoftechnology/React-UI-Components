780a12cfbe5a3adf3ed3dd2501f79d8343178f3c
Julie Jonak <juliejonak10@gmail.com>
2018-09-21 00:19:04 -0500

    Fixed white background and border styling on buttons

--- Day-II-Calculator/calculator/src/components/ButtonComponents/Keypad.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/Keypad.js
@@ -8,15 +8,15 @@ const Keypad = () => {
         <div className="keypad">
             <ActionButton buttonStyle="action clear" text="clear" />
             <div className="number-box">
-                <NumberButton buttonStyle="number" text="7"/>
-                <NumberButton buttonStyle="number" text="8"/>
-                <NumberButton buttonStyle="number" text="9"/>
-                <NumberButton buttonStyle="number" text="4"/>
-                <NumberButton buttonStyle="number" text="5"/>
-                <NumberButton buttonStyle="number" text="6"/>
-                <NumberButton buttonStyle="number" text="1"/>
-                <NumberButton buttonStyle="number" text="2"/>
-                <NumberButton buttonStyle="number" text="3"/>
+                <NumberButton buttonStyle="number top left" text="7"/>
+                <NumberButton buttonStyle="number top center" text="8"/>
+                <NumberButton buttonStyle="number top right" text="9"/>
+                <NumberButton buttonStyle="number middle left" text="4"/>
+                <NumberButton buttonStyle="number middle center" text="5"/>
+                <NumberButton buttonStyle="number middle right" text="6"/>
+                <NumberButton buttonStyle="number bottom left" text="1"/>
+                <NumberButton buttonStyle="number bottom center" text="2"/>
+                <NumberButton buttonStyle="number bottom right" text="3"/>
             </div>
             <ActionButton buttonStyle="action zero" text="0" />
         </div>
