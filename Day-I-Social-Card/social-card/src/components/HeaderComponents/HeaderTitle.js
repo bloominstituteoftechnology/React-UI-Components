d8bce7b5331f56c81658f73c2f2c17edfc57231e
vy3191 <venkyafhs@gmail.com>
2018-09-20 21:00:02 -0400

    Added header image styles

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,10 +3,12 @@ import './Header.css';
 
 const HeaderTitle = () => {
 	return (
-		<div>
+		<div className='header-title'>
 			<h1>Lambda School</h1>
 			<p>@LambdaSchool</p>
 			<p>-26 Jan</p>
 		</div>
 	)
 };
+
+export default HeaderTitle;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => { 
+      return (<div className='logo-image'>
+       <img className="logo" alt="logo-img" src = 'https://ibin.co/3whrpKSBbZ81.png' />;
+       </div>);
+
+    }
+export default ImageThumbnail;
