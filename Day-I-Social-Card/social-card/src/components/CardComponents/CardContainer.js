48d3c750b28f7b7abeb288a4a2b73b72b66d4efc
Ken Barlow <kenbrlow@gmail.com>
2018-09-21 15:31:00 -0700

    Split out card container components.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner.js';
+import CardContent from './CardContent.js';
 
 const CardContainer = () => {
     return (<div className="cc-main">
@@ -10,19 +12,4 @@ const CardContainer = () => {
     </div>);
   };
   
-  const CardBanner = () => {
-    return (<div>
-      <img src="https://ibin.co/3wnC6SgIOJud.png" alt ="React Logo"></img>
-    </div>);
-  };
-  
-  const CardContent = () => {
-    return (<div className="cc-main-card-content">
-      <h4>Get started with React</h4>
-      <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-      <p className="cc-main-link">reactjs.org</p>
-    </div>
-    );
-  };
-
   export default CardContainer;
