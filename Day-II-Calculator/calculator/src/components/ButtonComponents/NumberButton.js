333f44e73e9af9c4a88dcd0a298bdea28bbc5029
Jennifer King <jenbellpgh@gmail.com>
2018-09-20 23:42:52 -0400

    Added Numberbutton,which uses 2 props: type: digit (default) or symbol - css class for displaystyle label: visible label - 1-9 or +-=*/ Added test css for both digit and symbol.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = (props) => {
+    // props.type: css to display white/number or red/symbol.
+    //             digit (default) or symbol
+    // props.label: 1-9 or +-=x/
+    return (
+        <div className={props.type}>{props.label}</div>
+    );
+};
+NumberButton.defaultProps = { type: "digit" };
+export default NumberButton;
