425fde8f2bcf7c4e0562e451092831abf36fa181
Jennifer King <jenbellpgh@gmail.com>
2018-09-20 23:55:10 -0400

    basic code in for NumberButton: Uses 2 props: label (display on button) and type (act-digit or act-clear) Test styling in for both.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (props) => {
+    // Uses two props 
+    // - label: display on button
+    // - type: change style based on type
+    // -      act-digit (default) or act-clear
+    return(
+        <div className="action">{props.label}</div>
+    );
+};
+ActionButton.defaultProps = { type: "act-digit" };
+
+export default ActionButton;
