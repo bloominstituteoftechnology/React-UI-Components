5e3890ce88be3b28605dba8e6f3b9e871209bb6c
TheGameFreak720 <luis.alvarez@commithub.com>
2018-09-20 23:49:51 -0400

    did general idea of calculatordisplay component. Not sure if it works but it's a good way to visualize it

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,32 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return (
+        <section>
+            <section className='display'></section>
+            <section className='buttons-container'>
+                <section className='numeric-buttons-container'>
+                    <ActionButton data={'clear'} />
+                    <NumberButton data={7} />
+                    <NumberButton data={6} />
+                    <NumberButton data={5} />
+                    <NumberButton data={4} />
+                    <NumberButton data={3} />
+                    <NumberButton data={2} />
+                    <NumberButton data={1} />
+                    <ActionButton data={0} />
+                </section>
+                <section className='expression-buttons-container'>
+                    <NumberButton data={'/'} />
+                    <NumberButton data={'X'} />
+                    <NumberButton data={'-'} />
+                    <NumberButton data={'+'} />
+                    <NumberButton data={'='} />
+                </section>
+            </section>
+        </section>
+    );
+};
+
+export default CalculatorDisplay;
