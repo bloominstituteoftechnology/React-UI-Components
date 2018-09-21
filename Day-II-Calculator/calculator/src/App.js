b6fd91280f06a65e8cfeee2a0671987012cbec38
paulapivat <paul.apivat@gmail.com>
2018-09-21 11:09:21 +0700

    set up Calculator project

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
+      <div>
+        <NumberButton buttonStyle={'number'} text={'7'} />
+        <NumberButton buttonStyle={'number'} text={'8'} />
+        <NumberButton buttonStyle={'number'} text={'9'} />
+      </div>
+      <div>
+        <NumberButton buttonStyle={'number'} text={'4'} />
+        <NumberButton buttonStyle={'number'} text={'5'} />
+        <NumberButton buttonStyle={'number'} text={'6'} />
+      </div>
+      <div>
+        <NumberButton buttonStyle={'number'} text={'1'} />
+        <NumberButton buttonStyle={'number'} text={'2'} />
+        <NumberButton buttonStyle={'number'} text={'3'} />
+      </div>
+  </div>
+
+    // <div>
+    //   <h3>Welcome to React Calculator</h3>
+    //   <p>
+    //     We have given you a starter project. You'll want to build out your
+    //     components in their respective files, remove this code and replace it
+    //     with the proper components.
+    //   </p>
+    //   <p>
+    //     <strong>
+    //       Don't forget to `default export` your components and import them here
+    //       inside of this file in order to make them work.
+    //     </strong>
+    //   </p>
+    // </div>
   );
 };
 
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+//this is a re-usable component to be passed into App.js
+const NumberButton = (props) => {
+    return (
+        //className = props.buttonStyle which is named 'number', so in Button.css will be '.number'
+        <button className={props.buttonStyle}> {props.text} </button>   
+    )
+}
+
+export default NumberButton;
