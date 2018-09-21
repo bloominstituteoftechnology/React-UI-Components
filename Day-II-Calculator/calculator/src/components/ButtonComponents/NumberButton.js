83301ab7a14df2fdfed5fa1be341e0c1321eace5
Denise Escobar <descobar1490@gmail.com>
2018-09-20 22:23:42 -0500

    Started my project and followed along with Russel's example to create the NumberButton component and import/export it

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = (props) =>{
+   //prop.displayNumber===1
+    return(
+        <div>
+            I am a number Button!
+        </div>
+   )
+}
+
+export default NumberButton;
