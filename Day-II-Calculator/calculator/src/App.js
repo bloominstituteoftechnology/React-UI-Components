8b889181cc58df91df7e35df780f313feef9a1bf
iepoch <staclem@live.com>
2018-09-20 22:30:45 -0700

    Finished Button Layout - Will add more to Math buttons

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import Display from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
     <div>
        <div className ="app-content">
+       <div>
+         <Display />
+       </div>
+        <div> 
+          <ActionButton buttonStyle={'action'} text={'clear'} />
+          <NumberButton  buttonStyle={'math'} text={'*/*'} />
+          
+        </div>
+      
         <div>
          <NumberButton  buttonStyle={'number'} text={'7'} />
          <NumberButton  buttonStyle={'number'} text={'8'} />
          <NumberButton  buttonStyle={'number'} text={'9'} />
+         <NumberButton  buttonStyle={'math'}   text={'x'} />
          </div>
          <div>
          <NumberButton  buttonStyle={'number'} text={'4'} />
          <NumberButton  buttonStyle={'number'} text={'5'} />
          <NumberButton  buttonStyle={'number'} text={'6'} />
+         <NumberButton  buttonStyle={'math'}   text={'-'} />
          </div>
          <div>
          <NumberButton  buttonStyle={'number'} text={'1'} />
          <NumberButton  buttonStyle={'number'} text={'2'} />
          <NumberButton  buttonStyle={'number'} text={'3'} />
+         <NumberButton  buttonStyle={'math'} text={'+'} />
          </div>
-
+         <div> 
+          <ActionButton buttonStyle={'action'} text={'0'} />
+          <NumberButton  buttonStyle={'math'} text={'='} />
+        </div>
        </div>
     </div>
   );
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Display.css';
+
+
+
+const Display = (props) => {
+
+    return (
+    
+      <div className="display">0</div>
+
+
+ 
+    )
+}
+
+export default Display
