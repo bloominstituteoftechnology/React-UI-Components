7eed277883905df2fd36d2945b359d0d3e26d16c
iepoch <staclem@live.com>
2018-09-20 21:17:26 -0700

    Started- Calculator - Added NumberButton props, Added Styling for App.css

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+//Number may look like props.displayNumber === 1
+const NumberButton = (props) => {
+
+    return (
+    
+         <button className= {props.buttonStyle}>{props.text}</button>
+
+
+ 
+    )
+}
+
+export default NumberButton
