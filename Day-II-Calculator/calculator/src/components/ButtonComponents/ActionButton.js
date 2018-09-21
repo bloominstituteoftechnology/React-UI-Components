8c18bb8526e10f7b232c6fc740ec7c0c1081a53d
Robert Billington <chillingtonbillington@gmail.com>
2018-09-21 15:07:51 -0500

    partially completed, mostly built components for calculator. Still nedd styleing.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,40 @@
 import React from 'react';
 import './Button.css';
+
+const OperatorButton = (props) =>{
+    return(
+        <button className = {props.buttonStyle}>
+            {props.text}
+        </button>    
+    );
+};
+
+const Operators = () =>{
+    return(
+        <OperatorButton buttonStyle = {'operator'} text ={'/'} /> //Where do I get division symbol?button 
+        <OperatorButton buttonStyle = {'operator'} text ={'*'} />
+        <OperatorButton buttonStyle = {'operator'} text ={'-'} />
+        <OperatorButton buttonStyle = {'operator'} text ={'+'} />
+        <OperatorButton buttonStyle = {'operator'} text ={'='} />
+    );
+};
+
+const ActionButton = (props) =>{
+    return(
+        <button className = {props.buttonStyle}>
+            {props.text}
+        </button>    
+    );
+};
+
+const Actions = () =>{
+    return(
+        <ActionButton buttonStyle = {'action'} text = {'0'} />
+        <ActionButton buttonStyle = {'action'} text = {'clear'} />
+        
+        
+    )
+}
+
+export default OperatorButton;
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () =>{
+    return(
+        <button className = "display">
+            0
+        </button>    
+    )
+}
