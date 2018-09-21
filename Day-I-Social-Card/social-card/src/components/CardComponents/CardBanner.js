764ab1c04684848bf679c8c416b2022d7f33204c
vy3191 <venkyafhs@gmail.com>
2018-09-20 23:17:09 -0400

    Working on banner image

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+	<div className="card-banner">
+       <img className="image-banner" src="https://ibin.co/3wnC6SgIOJud.png" alt="banner image" />
+	</div>
+}
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner.js';
+import CardContent from './CardContent.js';
+
+const CardContainer = () => {
+
+
+	
+}
