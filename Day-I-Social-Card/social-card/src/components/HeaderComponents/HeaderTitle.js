c10fb42b3af18ff6b47e2a5e401fca1cc81144b7
Alex Ostrander <alexanderostrander23@gmail.com>
2018-09-20 20:08:19 -0400

    Added moment.js and used it to show the current date on my social cards header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
 const HeaderTitle = () => {
 	return (
 		<div className="lambda-heading-container">
 			<h1 className="lambda-heading">Lambda School</h1>
 			<p className="lambda-paragraph">@LambdaSchool</p>
-			<p className="lambda-paragraph">· 19 Sept</p> 
+			<p className="lambda-paragraph">{moment().format("Do MMM")}</p> 
 		</div>
 	)
 };
