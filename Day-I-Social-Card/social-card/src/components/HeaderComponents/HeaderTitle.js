c06f99de538b76b07f8545234b1dae49eeb81cac
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-20 20:43:19 -0400

    refactored jsx layout

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,11 +3,9 @@ import './Header.css';
 
 const HeaderTitle = () => (
   <div className='header-title'>
-    <span>
-      <h1>Lambda School</h1>
-      <p>@LambdaSchool</p>
-      <p>26 jan</p>
-    </span>
+    <h1>Lambda School</h1>
+    <p>@LambdaSchool</p>
+    <p>26 jan</p>
   </div>
 );
 
