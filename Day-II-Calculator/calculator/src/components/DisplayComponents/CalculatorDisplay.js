845955d6bb562c9f37d944f66d759cde02dac7e6
hraya <hraya@lssu.edu>
2018-09-20 22:36:02 -0500

    Initial Commit for Day 2
    Made a temp Calculator display and imported to App.js

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = (props) => {
+    return (
+        <div>
+            I am the Calculator display.
+        </div>
+    );
+};
+
+export default CalculatorDisplay;
