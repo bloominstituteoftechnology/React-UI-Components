1195aa2def0fe0d9994021e4582f8cd2c22a0113
Ben <bennybooftw@gmail.com>
2018-09-21 14:23:54 -0400

    Finished Card Component

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
     <div className='container'>
       <HeaderContainer />
+      <CardContainer />
     </div>
   );
 };
