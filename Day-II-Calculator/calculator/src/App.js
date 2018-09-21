a50ea1162232b8291dbc02cc36abb6f6f3db6cda
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-09-21 11:08:14 -0500

    everything functions on the calculator except for the clear button.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -27,28 +27,28 @@ const App = () => {
 
         <div className="row">
           <ActionButton action="clear" buttonStyle="action-button" />
-          <NumberButton number="÷" buttonStyle="symbol" />
+          <NumberButton number="÷" buttonStyle="symbol" dataOperator="/" />
         </div>
 
         <div className="row">
           <NumberButton number="7" buttonStyle="number" />
           <NumberButton number="8" buttonStyle="number" />
           <NumberButton number="9" buttonStyle="number" />
-          <NumberButton number="x" buttonStyle="symbol" />
+          <NumberButton number="x" buttonStyle="symbol" dataOperator="*" />
         </div>
 
         <div className="row">
           <NumberButton number="4" buttonStyle="number" />
           <NumberButton number="5" buttonStyle="number" />
           <NumberButton number="6" buttonStyle="number" />
-          <NumberButton number="–" buttonStyle="symbol" />
+          <NumberButton number="–" buttonStyle="symbol" dataOperator="-" />
         </div>
 
         <div className="row">
           <NumberButton number="1" buttonStyle="number" />
           <NumberButton number="2" buttonStyle="number" />
           <NumberButton number="3" buttonStyle="number" />
-          <NumberButton number="+" buttonStyle="symbol" />
+          <NumberButton number="+" buttonStyle="symbol" dataOperator="+" />
         </div>
 
         <div className="row">
--- Day-II-Calculator/calculator/src/index.js
+++ Day-II-Calculator/calculator/src/index.js
@@ -5,14 +5,64 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-class Button {
+let displayText = '';
+let displayMath = '';
+let display = document.querySelector('.display-screen');
+display.innerText = displayText;
+
+class NumberButton {
   constructor(element) {
     this.element = element;
-    this.number = this.element.innerText;
-    console.log(this.number);
+    this.text = this.element.innerText;
+    this.click = this.element.addEventListener('click', () => {
+      displayText += this.text;
+      displayMath += this.text;
+      display.innerText = displayText;
+      console.log(displayText);
+    });
+  }
+}
+
+class SymbolButton {
+  constructor(element) {
+    this.element = element;
+    this.text = this.element.innerText;
+    this.operator = this.element.dataset.operator;
+    this.click = this.element.addEventListener('click', () => {
+      if (this.text === '=') {
+        const doMath = () => {
+          return eval(displayMath);
+        };
+        display.innerText = doMath();
+        displayText = display.innerText;
+        displayMath = display.innerText;
+      } else {
+        displayText += this.text;
+        displayMath += this.operator;
+        display.innerText = displayText;
+        console.log(this.operator);
+      }
+    });
+  }
+}
+
+class ClearButton {
+  constructor(element) {
+    this.element = element;
   }
 }
 
 let numberButtons = document.querySelectorAll('.number');
 // console.log(numberButtons);
-numberButtons = Array.from(numberButtons).map(btn => new Button(btn));
+numberButtons = Array.from(numberButtons).map(btn => new NumberButton(btn));
+
+let symbolButtons = document.querySelectorAll('.symbol');
+symbolButtons = Array.from(symbolButtons).map(btn => new SymbolButton(btn));
+// console.log(symbolButtons);
+
+let clearButton = document.querySelectorAll('.action-button');
+clearButton = Array.from(clearButton).map(btn => {
+  if (btn.innerText === 'clear') {
+    new ClearButton(btn);
+  }
+});
