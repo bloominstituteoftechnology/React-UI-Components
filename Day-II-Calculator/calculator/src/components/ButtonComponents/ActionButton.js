d6fa3071740ea89f252773b617984cdf170edfea
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-09-20 20:14:08 -0600

    Button Built, but not sized differently yet.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return (
+        <button className={`action-button ${props.className}`}>
+            {props.text}
+        </button>
+    );
+};
+
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return (
+        <div className={`display-button ${props.className}`}>
+            {props.text}
+        </div>
+    );
+};
+
+export default CalculatorDisplay;
