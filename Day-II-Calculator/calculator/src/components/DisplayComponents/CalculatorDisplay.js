53993bee95d61bfd5c5cf96d5534b6d6d32b286a
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-09-22 12:40:39 -0400

    Initialize project and create Display

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = (props) =>  {
+    return(
+        <div className="display">
+            {props.value}
+        </div>
+    )
+}
+
+export default CalculatorDisplay;
