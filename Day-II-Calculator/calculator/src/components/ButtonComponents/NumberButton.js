a5bfb6bbb3138c0dead8dd0246dea35f568e61d6
Mitchell Robles <mitchellr92@live.com>
2018-09-22 11:14:04 -0700

    changed the order of the numbers

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -8,15 +8,15 @@ const NumberButton = (props) => {
                 <p>clear</p>
             </div>    
             <div class='main-numbers'>
-                <p class='number'>1</p>
-                <p class='number'>2</p>
-                <p class='number'>3</p>
-                <p class='number'>4</p>
-                <p class='number'>5</p>
-                <p class='number'>6</p>
                 <p class='number'>7</p>
                 <p class='number'>8</p>
                 <p class='number'>9</p>
+                <p class='number'>4</p>
+                <p class='number'>5</p>
+                <p class='number'>6</p>
+                <p class='number'>1</p>
+                <p class='number'>2</p>
+                <p class='number'>3</p>
             </div>
             <div class='zero'>
                 <p>0</p>
