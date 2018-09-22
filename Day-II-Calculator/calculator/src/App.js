f145b2cbad7a7e7aa26463b4ea31badca0e9a17b
hraya <hraya@lssu.edu>
2018-09-22 14:04:37 -0500

    Refactored code
    Made a KeypadContainer to include all the buttons
    
    Need to refactor code to attempt functionality

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,39 +1,13 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
-import NumberButton from './components/ButtonComponents/NumberButton';
-import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorKeypad from './components/ButtonComponents/CalculatorKeypad';
 
 const App = () => {
   return (
     <div className='CalculatorContainer'>
       <CalculatorDisplay displayStyle={'display'} text={'0'}/>
-      <div>
-        <ActionButton buttonStyle={'action'} text={'clear'}/>
-        <NumberButton buttonStyle={'symbol'} text={'÷'} />
-      </div>
-      <div>
-      <NumberButton buttonStyle={'num'} text={'7'}/>
-      <NumberButton buttonStyle={'num'} text={'8'}/>
-      <NumberButton buttonStyle={'num'} text={'9'}/>
-      <NumberButton buttonStyle={'symbol'} text={'x'}/>
-      </div>
-      <div>
-      <NumberButton buttonStyle={'num'} text={'4'}/>
-      <NumberButton buttonStyle={'num'} text={'5'}/>
-      <NumberButton buttonStyle={'num'} text={'6'}/>
-      <NumberButton buttonStyle={'symbol'} text={'-'}/>
-      </div>
-      <div>
-      <NumberButton buttonStyle={'num'} text={'1'}/>
-      <NumberButton buttonStyle={'num'} text={'2'}/>
-      <NumberButton buttonStyle={'num'} text={'3'}/>
-      <NumberButton buttonStyle={'symbol'} text={'+'}/>
-      </div>
-      <div>
-        <ActionButton buttonStyle={'action num'} text={'0'}/>
-        <NumberButton buttonStyle={'symbol'} text={'='} />
-      </div>
+      <CalculatorKeypad/>
     </div>
   );
 };
