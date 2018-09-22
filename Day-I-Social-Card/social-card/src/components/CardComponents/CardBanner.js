891defb92d1c8ad9d9277883baeff09b04289443
Glenn-David Daniel <d2rd@outlook.com>
2018-09-22 10:22:07 -0700

    WIP solved issue with CardBanner component.  Needs formatting and image import.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './Card.css';
-import ImageThumbnail from './HeaderComponents/ImageThumbnail';
 class CardBanner extends React.Component {
   render() {
     return (
     <div>
       <h1>CardBanner stuff</h1>
-      </div>
-      <div>
-        <img src="https://ibin.co/3wnC6SgIOJud.png"  alt="LambdaShield" height="20" width="20">)
-      </div>
-    }
+      <img src="https://ibin.co/3wnC6SgIOJud.png"  alt="LambdaShield" height="20" width="20" />
+    </div>
+    )
+  }
   }
 
-export default CardBanner;
+export default CardBanner
