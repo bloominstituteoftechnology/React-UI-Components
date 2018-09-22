22d778f001e63612e3ad8877baee4ce4bfc758a4
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-09-22 15:00:42 -0600

    Buttons are close to pixel perfect

--- Day-II-Calculator/calculator/src/components/ButtonComponents/CalculatorButtons.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/CalculatorButtons.js
@@ -8,24 +8,24 @@ const CalculatorButtons = () => {
     return (
         <div className="buttons">
             <div className="number-pad">
-                <ActionButton text="0" buttonStyle="button-action" />
-                <NumberButton text="1" buttonStyle="button-number" />
-                <NumberButton text="2" buttonStyle="button-number" />
-                <NumberButton text="3" buttonStyle="button-number" />
-                <NumberButton text="4" buttonStyle="button-number" />
-                <NumberButton text="5" buttonStyle="button-number" />
-                <NumberButton text="6" buttonStyle="button-number" />
-                <NumberButton text="7" buttonStyle="button-number" />
-                <NumberButton text="8" buttonStyle="button-number" />
-                <ActionButton text="9" buttonStyle="button-number" />
-                <ActionButton text="clear" buttonStyle="button-action" />
+                <ActionButton text="clear" buttonStyle="button button-action" />
+                <NumberButton text="7" buttonStyle="button button-number" />
+                <NumberButton text="8" buttonStyle="button button-number" />
+                <NumberButton text="9" buttonStyle="button button-number" />
+                <NumberButton text="4" buttonStyle="button button-number" />
+                <NumberButton text="5" buttonStyle="button button-number" />
+                <NumberButton text="6" buttonStyle="button button-number" />
+                <NumberButton text="1" buttonStyle="button button-number" />
+                <NumberButton text="2" buttonStyle="button button-number" />
+                <ActionButton text="3" buttonStyle="button button-number" />
+                <ActionButton text="0" buttonStyle="button button-action" />
             </div>
             <div class="operators">
-                <NumberButton text="&divide;" buttonStyle="button-operator" />
-                <NumberButton text="&times;" buttonStyle="button-operator" />
-                <NumberButton text="&minus;" buttonStyle="button-operator" />
-                <NumberButton text="&#43;" buttonStyle="button-operator" />
-                <NumberButton text="&#61;" buttonStyle="button-operator" />
+                <NumberButton text="&divide;" buttonStyle="button button-operator" />
+                <NumberButton text="&times;" buttonStyle="button button-operator" />
+                <NumberButton text="&minus;" buttonStyle="button button-operator" />
+                <NumberButton text="&#43;" buttonStyle="button button-operator" />
+                <NumberButton text="&#61;" buttonStyle="button button-operator" />
             </div>
         </div>
     );
