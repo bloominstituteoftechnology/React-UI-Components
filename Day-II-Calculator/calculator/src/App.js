d279cfb650471bef6fbce79cd287112d1dd6ac4e
Alex Ostrander <alexanderostrander23@gmail.com>
2018-09-22 13:55:45 -0400

    Created the display and added the components to the App.js, now working on the styling of the calculator

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -2,11 +2,40 @@ import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton.js'
 import ActionButton from './components/ButtonComponents/ActionButton.js'
-
+import Display from './components/DisplayComponents/CalculatorDisplay.js'
 const App = () => {
   return (
-	  <NumberButton buttonStyle={'number'} text={'7'} />
-	    
+	  <div className = "container">
+      	<div className = "row-1">
+        	<Display />
+      	</div>
+      <div className = "row-2">
+        <ActionButton displayStyle="action" text="clear" />
+        <NumberButton displayStyle="operator"  text="&#247;"/>
+      </div>
+      <div className = "row-3">
+        <NumberButton displayStyle="number" text="7" />
+        <NumberButton displayStyle="number" text="8" />
+		<NumberButton displayStyle="number" text="9" />
+        <NumberButton displayStyle="operator" text="&#215;" />
+      </div>
+      <div className = "row-4">
+        <NumberButton displayStyle="number" text="4" />
+		<NumberButton displayStyle="number" text="5" />
+		<NumberButton displayStyle="number" text="6" />
+		<NumberButton displayStyle="operator" text="-" />
+      </div>
+      <div className = "row-5">
+        <NumberButton displayStyle="number" text="1" />
+		<NumberButton displayStyle="number" text="2" />
+		<NumberButton displayStyle="number" text="3" />
+        <NumberButton displayStyle="operator" text="+" />
+      </div>
+      <div className = "row-6">
+        <ActionButton displayStyle="action" text="0" />
+        <NumberButton displayStyle="operator" text="=" />
+      </div>
+    </div>
   );
 };
 
