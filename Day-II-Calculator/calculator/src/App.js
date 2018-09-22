e7e613eb2e677643f944378a9c86dbba0194f0fd
BrandonMoll <brandonmoll85@gmail.com>
2018-09-22 08:55:45 -0500

    created numbers

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -7,12 +7,20 @@ import NumberButton from './components/ButtonComponents/NumberButton';
 import ZeroButton from './components/ButtonComponents/ZeroButton';
 
 const App = () => {
+  const numbers = {
+    numArray: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  };
+
+  const actions = {
+    actionArray: ['divide', 'X', '-', '+', '=']
+  };
+
   return (
     <div className="CalculatorContainer">
       <Display />
       <ClearButton />
       <ActionButton />
-      <NumberButton />
+      <NumberButton number={numbers} />
       <ZeroButton />
     </div>
   );
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -4,7 +4,15 @@ import './Button.css';
 const NumberButton = (props) => {
     return (
         <div>
-            number
+            <div className="numbers">{props.number.numArray[1]}</div>
+            <div className="numbers">{props.number.numArray[2]}</div>
+            <div className="numbers">{props.number.numArray[3]}</div>
+            <div className="numbers">{props.number.numArray[4]}</div>
+            <div className="numbers">{props.number.numArray[5]}</div>
+            <div className="numbers">{props.number.numArray[6]}</div>
+            <div className="numbers">{props.number.numArray[7]}</div>
+            <div className="numbers">{props.number.numArray[8]}</div>
+            <div className="numbers">{props.number.numArray[9]}</div>
         </div>
     );
 };
