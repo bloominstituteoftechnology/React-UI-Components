660b8147ed512ce4f0b5080c6d03db7f03b2aa49
Robin U Familara <robin.familara@cgius.com>
2018-09-22 10:59:35 -0500

    Created number buttons

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+const NumberButton = (props) => {
+	return (
+	)
+}
+export default NumberButton;
+
+
+
+Import this into the Apps.js file.
+
+
+
+import NumberButton from ‘./components/ButtonComponents/NumberButton.js’
