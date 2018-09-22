4e0517d99af1ed908203574ea5e30e1899ea8518
Lloyd Napier <lloyd.a.napier@gmail.com>
2018-09-22 08:49:50 -0700

    added footer

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
     return (
         <section className='social-card'>
             <HeaderContainer />
             <CardContainer />
+            <Footer />
         </section>
     );
 };
