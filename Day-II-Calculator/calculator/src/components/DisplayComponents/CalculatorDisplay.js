73f7552fe16830c64e2c54775ad3bca5e84ff438
TheGameFreak720 <luis.alvarez@commithub.com>
2018-09-22 01:08:11 -0400

    struggling a bit making buttons take the whole width and height

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -5,18 +5,28 @@ import NumberButton from '../ButtonComponents/NumberButton';
 
 const CalculatorDisplay = () => {
     return (
-        <section>
+        <section className='calculator'>
             <section className='display'></section>
             <section className='buttons-container'>
                 <section className='numeric-buttons-container'>
                     <ActionButton data={'clear'} displayStyle={'btn-action'}/>
-                    <NumberButton data={7} displayStyle={'btn'} />
-                    <NumberButton data={6} displayStyle={'btn'}/>
-                    <NumberButton data={5} displayStyle={'btn'}/>
-                    <NumberButton data={4} displayStyle={'btn'}/>
-                    <NumberButton data={3} displayStyle={'btn'}/>
-                    <NumberButton data={2} displayStyle={'btn'}/>
-                    <NumberButton data={1} displayStyle={'btn'}/>
+                    <section className='numeric-buttons-well'>
+                        <section className='number-row'>
+                            <NumberButton data={7} displayStyle={'btn'} />
+                            <NumberButton data={8} displayStyle={'btn'} />
+                            <NumberButton data={9} displayStyle={'btn'} />
+                        </section>
+                        <section className='number-row'>
+                            <NumberButton data={4} displayStyle={'btn'}/>
+                            <NumberButton data={5} displayStyle={'btn'}/>
+                            <NumberButton data={6} displayStyle={'btn'}/>
+                        </section>
+                        <section className='number-row'>
+                            <NumberButton data={1} displayStyle={'btn'}/>
+                            <NumberButton data={2} displayStyle={'btn'}/>
+                            <NumberButton data={3} displayStyle={'btn'}/>
+                        </section>
+                    </section>
                     <ActionButton data={0} displayStyle={'btn-action'}/>
                 </section>
                 <section className='expression-buttons-container'>
