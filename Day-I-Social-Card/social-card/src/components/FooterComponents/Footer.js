1744d344153495994425a6f5970e624ecfd189d3
vy3191 <venkyafhs@gmail.com>
2018-09-21 23:21:31 -0400

    Working on stretch goals

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+    return (<div className='footer-container'>
+    	       <i className="fa fa-comment-o fa-lg" ariaHidden="true"></i>
+    	       <i className="fa fa-refresh fa-lg" ariaHidden="true"></i> 
+    	       <i className="fa fa-heart-o fa-lg" ariaHidden="true"></i>
+    	       <i className="fa fa-envelope-o fa-lg" ariaHidden="true"></i>           
+    	    </div>);
+}
+
+export default Footer;
