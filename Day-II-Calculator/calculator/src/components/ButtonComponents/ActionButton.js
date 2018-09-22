9e6af5bf25439f1da4af127e204e070d080e6225
Jess <jessdev83@gmail.com>
2018-09-22 13:25:35 -0400

    Refactoring
    Refactored ActionButton.js, NumberButton.js, and NumPad.js to make button components accept props and improve reusability.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './Button.css';
 
-const ActionButton = () => {
-    return <div className="action">
-        <div className="action-button">&divide;</div>
-        <div className="action-button">&times;</div>
-        <div className="action-button">&minus;</div>
-        <div className="action-button">&#43;</div>
-        <div className="action-button">&#61;</div>
-      </div>;
-}
+const ActionButton = props => {
+  return (
+      <div className={props.buttonStyle}>{props.text}</div>
+  )
+};
 
 export default ActionButton;
+
