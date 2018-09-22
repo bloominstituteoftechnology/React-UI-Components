8e251cf1e2190db6302ca9966e768434c18b9cf4
mglittleton <michaelglittleton@gmail.com>
2018-09-22 14:18:41 -0400

    Calculator image complete
    Calculator components are functioning and the CSS is done such that the image I've created is similar to the design file.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NumButton from "./components/ButtonComponents/NumberButton";
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import CalcDisplay from "./components/DisplayComponents/CalculatorDisplay";
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="container">
+      <div className="display">
+        <CalcDisplay label={0} />
+      </div>
+      <div className="numpad">
+        <div className="numpad-row1 row">
+          <ActionButton label="clear" buttonStyle="long" />
+          <NumButton label="÷" buttonStyle="red"/>
+        </div>
+        <div className="numpad-row2 row">
+          <NumButton label="7" buttonStyle="gray"/>
+          <NumButton label="8" buttonStyle="gray"/>
+          <NumButton label="9" buttonStyle="gray"/>
+          <NumButton label="x" buttonStyle="red"/>
+        </div>
+        <div className="numpad-row3 row">
+          <NumButton label="4" buttonStyle="gray"/>
+          <NumButton label="5" buttonStyle="gray"/>
+          <NumButton label="6" buttonStyle="gray"/>
+          <NumButton label="-" buttonStyle="red"/>
+        </div>
+        <div className="numpad-row4 row">
+          <NumButton label="1" buttonStyle="gray"/>
+          <NumButton label="3" buttonStyle="gray"/>
+          <NumButton label="2" buttonStyle="gray"/>
+          <NumButton label="+" buttonStyle="red"/>
+        </div>
+        <div className="numpad-row5 row">
+          <ActionButton label={0} buttonStyle="long" />
+          <NumButton label="=" buttonStyle="red"/>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default App;
+
+// #### Day 2 MVP requirements
+
+// - Create a `<NumberButton />` component that can accept `props` and display any number/symbol passed down as `text`.
+//   - Example your component should be able to render a dynamic prop called text:
+//   - Your button button should also be able to accept dynamic props `buttonStyle` for styling
+
+// ```js
+// <button className={props.buttonStyle}>{props.text}</button>
+// ```
+
+// - Create an `<ActionButton />` component that will be used for the `zero` character and the `clear` button.
+// - Create a `<CalculatorDisplay />` component that will be used as the `calculator display`
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalcDisplay = props => {
+  console.log(props.label);
+  return (
+    <div className='display'>
+      {props.label}
+    </div>
+  );
+};
+
+export default CalcDisplay;
