defef366f737e233b3184b615ce4cbcdd64584f5
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-21 23:11:52 -0400

    added the action bar to the main container

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
-
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ContainerButtons from './components/ButtonComponents/ContainerButtons';
 const App = () => {
   return (
     <div>
     <CalculatorDisplay currentNumber={0} />
+    <ContainerButtons />
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Button.css';
+import SquareButton from './SquareButton';
+
+const ActionButton = () => {
+    return (
+        <div className='actionBar'>
+            <SquareButton type = 'square action' action = '	&#247;' />
+            <SquareButton type = 'square action' action = '	&#215;' />
+            <SquareButton type = 'square action' action = '	&#8722;' />
+            <SquareButton type = 'square action' action = '	&#43;' />
+            <SquareButton type = 'square action' action = '	&#61;' />
+        </div>
+    );
+};
+
+export default ActionButton;
