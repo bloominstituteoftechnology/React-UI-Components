8d4dce4e119262fe40bbc86401afd4ef850b3ba9
Lloyd Napier <lloyd.a.napier@gmail.com>
2018-09-22 09:44:10 -0700

    Added moment()

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
 const HeaderTitle = () => {
     return (
         <div className ='header-info'>
             <h1>Lambda School</h1>
             <p className='atlambda'>@LambdaSchool</p>
-            <p>&nbsp;&#8226; 26 jan</p>
+            <p>&nbsp;&#8226; {moment().format("Do MMM")}</p>
         </div>
     );
 }
