38558f3ce8616ef54b5f7d2f75ab3d06385fb40d
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-21 23:12:34 -0400

    started on the numbers buttons

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+import WideButtons from './WideButton';
+import SquareButtons from './SquareButton';
+
+const NumberButtons = () => {
+    return (
+        <div className='numberContainer'>
+            <WideButtons />
+            <div className= 'numbers'></div>
+            <div className= 'numbers'></div>
+            <div className= 'numbers'></div>
+            <WideButtons />
+        </div>
+    )
+}
