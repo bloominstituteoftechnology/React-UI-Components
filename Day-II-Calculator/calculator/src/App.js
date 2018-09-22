3dd6251025bd44c34432fc5e8b3cd42003bebdeb
Kyle Teeter <kyleteeter12@gmail.com>
2018-09-22 14:05:49 -0500

    Still working on state.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from "react";
+
 import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton.js'
 import ActionButton from './components/ButtonComponents/ActionButton.js'
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js'
+import { Input } from './components/Input';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: ''
+    };
+  }
   render() {
     return (
       <div>
