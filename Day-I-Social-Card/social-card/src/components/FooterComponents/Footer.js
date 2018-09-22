03dc4d2c799edebb9a4b515f28970a3dcc2cd3ed
Willie Yturralde <willieino@hotmail.com>
2018-09-22 08:44:06 -0700

    added a few files

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,9 +4,16 @@ import './Footer.css';
 
 const Footer = () => {
 	return (
-		<div className='Footer'><p>and this is the footer and all the icons go here...</p></div>
+		<div className='Footer'><div className="icon1"></div>
+		<div className="icon2"></div><div className="icon3"></div><div className="icon4"></div></div>
         
 	)
 };
+let icon_1 = document.querySelector(".icon1");
+/* icon1.addEventListener("click" , icon1Handler); */
+console.log("valueof icon_1 is:" + icon_1);
+function icon1Handler() {
 
+
+}
 export default Footer;
