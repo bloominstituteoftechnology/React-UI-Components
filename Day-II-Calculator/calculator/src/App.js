a726c1e189d1c0059309c3a2c3b9b460f9d51f10
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-09-22 12:52:02 -0400

    Add all buttons to App.js

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -8,8 +8,22 @@ const App = () => {
   return (
     <div>
         <CalculatorDisplay value="0" />
-        <ActionButton value="Clear" />
+        <ActionButton value="Clear"/>
+        <ActionButton value="÷" />
         <NumberButton value="7" />
+        <NumberButton value="8" />
+        <NumberButton value="9" />
+        <ActionButton value="X" />
+        <NumberButton value="4" />
+        <NumberButton value="5" />
+        <NumberButton value="6" />
+        <ActionButton value="-" />
+        <NumberButton value="1" />
+        <NumberButton value="2" />
+        <NumberButton value="3" />
+        <ActionButton value="+" />
+        <NumberButton value="0" />
+        <ActionButton value="=" />
     </div>
   );
 };
