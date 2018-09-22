0513ad67292633adf5071e6d1d404152cb85693c
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-21 21:51:50 -0400

    completed the display component

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 
 const App = () => {
   return (
     <div>
-      
+    <CalculatorDisplay currentNumber={0} />
     </div>
   );
 };
