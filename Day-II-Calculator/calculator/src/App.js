f57f1d7e821d3d59781dcdac76e652101316f00f
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-09-22 08:46:50 -0700

    Added styling for number and operator buttons

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -7,32 +7,32 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.
 const App = () => {
   return (
     <div>
-      <CalculatorDisplay buttonStyle={'number'} text={'0'} />
+      <CalculatorDisplay buttonStyle={'all'} text={'0'} />
       <div className='row'>
-        <ActionButton buttonStyle={'number'} text={'clear'} />
-        <NumberButton buttonStyle={'number'} text={'/'} />
+        <ActionButton buttonStyle={'all'} text={'clear'} />
+        <NumberButton buttonStyle={'all operator'} text={'/'} />
       </div>
       <div className='row'>
-        <NumberButton buttonStyle={'number'} text={'7'} />
-        <NumberButton buttonStyle={'number'} text={'8'} />
-        <NumberButton buttonStyle={'number'} text={'9'} />
-        <NumberButton buttonStyle={'number'} text={'X'} />
+        <NumberButton buttonStyle={'all number'} text={'7'} />
+        <NumberButton buttonStyle={'all number'} text={'8'} />
+        <NumberButton buttonStyle={'all number'} text={'9'} />
+        <NumberButton buttonStyle={'all operator'} text={'X'} />
       </div >
       <div className='row'>
-        <NumberButton buttonStyle={'number'} text={'4'} />
-        <NumberButton buttonStyle={'number'} text={'5'} />
-        <NumberButton buttonStyle={'number'} text={'6'} />
-        <NumberButton buttonStyle={'number'} text={'-'} />
+        <NumberButton buttonStyle={'all number'} text={'4'} />
+        <NumberButton buttonStyle={'all number'} text={'5'} />
+        <NumberButton buttonStyle={'all number'} text={'6'} />
+        <NumberButton buttonStyle={'all operator'} text={'-'} />
       </div>
       <div className='row'>
-        <NumberButton buttonStyle={'number'} text={'1'} />
-        <NumberButton buttonStyle={'number'} text={'2'} />
-        <NumberButton buttonStyle={'number'} text={'3'} />
-        <NumberButton buttonStyle={'number'} text={'+'} />
+        <NumberButton buttonStyle={'all number'} text={'1'} />
+        <NumberButton buttonStyle={'all number'} text={'2'} />
+        <NumberButton buttonStyle={'all number'} text={'3'} />
+        <NumberButton buttonStyle={'all operator'} text={'+'} />
       </div>
       <div className='row'>
-        <ActionButton buttonStyle={'number'} text={'0'} />
-        <NumberButton buttonStyle={'number'} text={'='} />
+        <ActionButton buttonStyle={'all'} text={'0'} />
+        <NumberButton buttonStyle={'all operator'} text={'='} />
       </div>
     </div>
   );
