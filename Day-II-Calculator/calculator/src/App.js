eeb5dd94bde456c52530e64cbf672b3819d283e0
Kyle Teeter <kyleteeter12@gmail.com>
2018-09-22 13:06:27 -0500

    Attempting the stretch goal.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,43 +1,46 @@
+import React, { Component } from "react";
 import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton.js'
 import ActionButton from './components/ButtonComponents/ActionButton.js'
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js'
 
-const App = () => {
-  return (
-    <div>
+class App extends Component {
+  render() {
+    return (
       <div>
-        <CalculatorDisplay displayStyle={'display'} text={'0'} />
+        <div>
+          <CalculatorDisplay displayStyle={'display'} text={'0'} />
+        </div>
+        <div>
+          <ActionButton buttonStyle={'action'} text={'clear'} />
+          <NumberButton buttonStyle={'numberRed'} text={"\u00f7"} />
+        </div>
+        <div>
+          <NumberButton buttonStyle={'number'} text={'7'} />
+          <NumberButton buttonStyle={'number'} text={'8'} />
+          <NumberButton buttonStyle={'number'} text={'9'} />
+          <NumberButton buttonStyle={'numberRed'} text={'x'} />
+        </div>
+        <div>
+          <NumberButton buttonStyle={'number'} text={'4'} />
+          <NumberButton buttonStyle={'number'} text={'5'} />
+          <NumberButton buttonStyle={'number'} text={'6'} />
+          <NumberButton buttonStyle={'numberRed'} text={'-'} />
+        </div>
+        <div>
+          <NumberButton buttonStyle={'number'} text={'1'} />
+          <NumberButton buttonStyle={'number'} text={'2'} />
+          <NumberButton buttonStyle={'number'} text={'3'} />
+          <NumberButton buttonStyle={'numberRed'} text={'+'} />
+        </div>
+        <div>
+          <ActionButton buttonStyle={'action'} text={'0'} />
+          <NumberButton buttonStyle={'numberRed'} text={'='} />
+        </div>
       </div>
-      <div>
-        <ActionButton buttonStyle={'action'} text={'clear'} />
-        <NumberButton buttonStyle={'numberRed'} text={"\u00f7"} />
-      </div>
-      <div>
-        <NumberButton buttonStyle={'number'} text={'7'} />
-        <NumberButton buttonStyle={'number'} text={'8'} />
-        <NumberButton buttonStyle={'number'} text={'9'} />
-        <NumberButton buttonStyle={'numberRed'} text={'x'} />
-      </div>
-      <div>
-        <NumberButton buttonStyle={'number'} text={'4'} />
-        <NumberButton buttonStyle={'number'} text={'5'} />
-        <NumberButton buttonStyle={'number'} text={'6'} />
-        <NumberButton buttonStyle={'numberRed'} text={'-'} />
-      </div>
-      <div>
-        <NumberButton buttonStyle={'number'} text={'1'} />
-        <NumberButton buttonStyle={'number'} text={'2'} />
-        <NumberButton buttonStyle={'number'} text={'3'} />
-        <NumberButton buttonStyle={'numberRed'} text={'+'} />
-      </div>
-      <div>
-        <ActionButton buttonStyle={'action'} text={'0'} />
-        <NumberButton buttonStyle={'numberRed'} text={'='} />
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default App;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Button.css';
 
+const isOperator = val => {
+    return !isNaN(val) || val === "." || === "=";
+}
 const ActionButton = (props) => {
     return (
-        <button className={props.buttonStyle}>{props.text}</button>
+        <button className={`props.buttonStyle ${isOperator(props.children) ? null : "operator"}`}>{props.text}{props.children}</button>
     )
 }
 
