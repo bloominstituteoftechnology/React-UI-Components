c6925d0c84d40abd61d861dbefcb6351f7345bfe
Robin U Familara <robin.familara@cgius.com>
2018-09-22 12:08:15 -0500

    Updated NumberButtons and Calculator Display JS

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay(props){
+    return (
+        <div className={`calculator-display ${props.type}`} > {props.text}</div>
+    )
+}
+ export default CalculatorDisplay;
