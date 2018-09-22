c6d9fb552603cdeb7a29fcb88ef307813dc592c6
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-09-21 21:03:58 -0700

    Refactored buttons into divs in order to address styling issues

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -5,17 +5,17 @@ import NumberButton from './components/ButtonComponents/NumberButton.js'
 const App = () => {
   return (
     <div>
-      <div>
+      <div className='row'>
         <NumberButton buttonStyle={'number'} text={'7'} />
         <NumberButton buttonStyle={'number'} text={'8'} />
         <NumberButton buttonStyle={'number'} text={'9'} />
-      </div>
-      <div>
+      </div >
+      <div className='row'>
         <NumberButton buttonStyle={'number'} text={'4'} />
         <NumberButton buttonStyle={'number'} text={'5'} />
         <NumberButton buttonStyle={'number'} text={'6'} />
       </div>
-      <div>
+      <div className='row'>
         <NumberButton buttonStyle={'number'} text={'1'} />
         <NumberButton buttonStyle={'number'} text={'2'} />
         <NumberButton buttonStyle={'number'} text={'3'} />
