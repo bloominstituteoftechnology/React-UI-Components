0d7519ec18357ac152ec7d05818565cb593fa590
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-09-22 11:45:54 -0500

    added the red buttons to the calculator

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -5,20 +5,24 @@ import NumberButton from './components/ButtonComponents/NumberButton.js';
 const App = () => {
   return (
         <div className="container">
+
           <div>
             <NumberButton buttonStyle={'number'} text={'7'} />
             <NumberButton buttonStyle={'number'} text={'8'} />
             <NumberButton buttonStyle={'number'} text={'9'} />
+            <NumberButton buttonStyle={'redNumber'} text={'X'} />
           </div>
           <div>
             <NumberButton buttonStyle={'number'} text={'4'} />
             <NumberButton buttonStyle={'number'} text={'5'} />
             <NumberButton buttonStyle={'number'} text={'6'} />
+            <NumberButton buttonStyle={'redNumber'} text={'-'} />
           </div>
           <div>
             <NumberButton buttonStyle={'number'} text={'1'} />
             <NumberButton buttonStyle={'number'} text={'2'} />
             <NumberButton buttonStyle={'number'} text={'3'} />
+            <NumberButton buttonStyle={'redNumber'} text={'+'} />
           </div>
         </div>
       );
