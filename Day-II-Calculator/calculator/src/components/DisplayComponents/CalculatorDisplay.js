70416ab4492b1f584638a2d50da5466616540305
Colin Dismuke <colin.dismuke@gmail.com>
2018-09-21 22:37:24 -0500

    Finished initial MVP of calculator.

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Display.css';
+
+
+function CalculatorDisplay() {
+    return (
+        <div className='total'>0</div>
+    );
+};
+
+export default CalculatorDisplay;
