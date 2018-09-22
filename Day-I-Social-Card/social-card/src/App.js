42220056ea91ac7bac964f38a5772a9c44f203c4
kurtve <kurt@vanetten.us>
2018-09-22 01:07:46 -0400

    Complete calculator MVP goals

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -11,19 +11,3 @@ const App = () => {
 };
 
 export default App;
-
-/*
-      <h3>Welcome to React Social Card!</h3>
-      <p>
-        Begin by exploring the `components` directory. You'll notice we have a
-        few files that we've already included in there to get you started right
-        away building components. You'll need to make sure you include your
-        components that you build in this file to watch your app come to life
-      </p>
-      <p>
-        <strong>
-          Do not forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-*/
--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
+import Button from './components/ButtonComponents/Button'
 
 const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
+    return (
+        <div className="container">
+            <CalculatorDisplay initialValue="0" />
+            <div className="buttonBox">
+                <div className="keyPad">
+                    <Button type={'clear'} text={'clear'} />
+                    <div className="keyRow">
+                        <Button type={'digit'} text={'7'} />
+                        <Button type={'digit'} text={'8'} />
+                        <Button type={'digit'} text={'9'} />
+                    </div>
+                    <div className="keyRow">
+                        <Button type={'digit'} text={'4'} />
+                        <Button type={'digit'} text={'5'} />
+                        <Button type={'digit'} text={'6'} />
+                    </div>
+                    <div className="keyRow">
+                        <Button type={'digit'} text={'1'} />
+                        <Button type={'digit'} text={'2'} />
+                        <Button type={'digit'} text={'3'} />
+                    </div>
+                    <Button type={'zero'} text={'0'} />
+                </div>
+                <div className="opPad">
+                    <Button type={'oper'} text={'÷'} />
+                    <Button type={'oper'} text={'x'} />
+                    <Button type={'oper'} text={'-'} />
+                    <Button type={'oper'} text={'+'} />
+                    <Button type={'oper'} text={'='} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
