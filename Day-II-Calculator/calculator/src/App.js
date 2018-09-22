d0d10e07fe876235ff6f41ef698fc97b52fc460b
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-22 14:19:24 -0400

    added flex and width styles to App.css

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -7,6 +7,14 @@ const App = () => {
   return (
     <div>
       <NumberButton buttonStyle={'number'} text={'7'} />
+      <NumberButton buttonStyle={'number'} text={'8'} />
+      <NumberButton buttonStyle={'number'} text={'9'} />
+      <NumberButton buttonStyle={'number'} text={'4'} />
+      <NumberButton buttonStyle={'number'} text={'5'} />
+      <NumberButton buttonStyle={'number'} text={'6'} />
+      <NumberButton buttonStyle={'number'} text={'1'} />
+      <NumberButton buttonStyle={'number'} text={'2'} />
+      <NumberButton buttonStyle={'number'} text={'3'} />
     </div>
   );
 };
