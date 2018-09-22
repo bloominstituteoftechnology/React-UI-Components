62bbbdd53c8ef52560229efc670193915dbcb759
Alex Ostrander <alexanderostrander23@gmail.com>
2018-09-22 13:38:30 -0400

    Created action button for 0 and clear buttons. Creating the display now:

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton.js'
+import ActionButton from './components/ButtonComponents/ActionButton.js'
 
 const App = () => {
   return (
