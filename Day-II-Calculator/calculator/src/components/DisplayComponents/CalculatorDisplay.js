8d3661d7f7279498d769b2d4ff9775bdc8787c17
FreeKeyBoy <jjhutton81@gmail.com>
2018-09-22 10:20:45 -0700

    Finished the button components.js files, both number and action buttons. Added a js file for clear and 0 button. I'm hoping that by having the two buttons in seperate div's it will make it easier to lay out with flexbox.Looking up advice for the display component, since it should not be a button.

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = (props) => {
+	return (
+		<button className={props.buttonStyle}>
+			{props.text}
+		</button>
+	)
+}
+
+export default CalculatorDisplay;
