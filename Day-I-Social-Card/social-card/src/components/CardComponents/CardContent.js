37cd42dbe4f09b788a09d9a927bf1804c200810e
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-09-22 11:16:28 -0400

    added border-radius and border to CardContainer refactored CardBanner css added height to CardBanner img added styles to CardContent

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -5,7 +5,7 @@ const CardContent = () => (
   <div className='card-content'>
     <h3>Get Started with React</h3>
     <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-    <p>reactjs.org</p>
+    <p className='web-address'>reactjs.org</p>
   </div>
 );
 
