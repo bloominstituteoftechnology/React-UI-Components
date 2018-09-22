7422e8975281f0cb8e897055e4c38bbfc7509643
jbierly1 <james.bierly@gmail.com>
2018-09-21 20:53:15 -0400

    top header text works, but image is crashing everything, no idea what's wrong

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderTitle from './HeaderTitle.js';
+
+import HeaderContent from './HeaderContent.js';
+import ImageThumbnail from './ImageThumbnail.js';
+
+const HeaderContainer = () => {
+	return (           
+        <div className="container">
+         <ImageThumbnail/>
+		<HeaderTitle />
+        
+        <HeaderContent />
+        </div>
+	)
+};
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle=()=>{
+    return(
+        <div className="HeaderTitle">
+            <h1>Lambda School </h1><p>@Lambda School</p>
+            <p>·</p><p>26 Jan</p>
+
+        </div>
+    )
+}
+
+export default HeaderTitle;
