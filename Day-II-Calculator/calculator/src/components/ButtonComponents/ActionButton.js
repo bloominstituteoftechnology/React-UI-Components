8186753f186dd2cc2965621b6fd280b285963dcf
Chris Juelfs <CRJuelfs24@comcast.net>
2018-09-22 16:06:15 -0500

    calculator MVP's complete!

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+
+function ActionButton(props){
+    return (
+        <button className = {props.displayStyle}>{props.action}</button>
+    );
+};
+
+export default ActionButton;
