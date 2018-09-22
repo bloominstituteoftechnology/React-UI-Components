5602e1370694a51bff9bd275f739ace76b4a273b
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2018-09-21 23:56:35 -0400

    completed Day 1 Social Card

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+
+const ImageThumbnail = () => {
+    return (
+        <div className='imageThumnail'><img className='imgIcon' src="https://ibin.co/3whrpKSBbZ81.png" alt='iconIMG'></img> </div>
+    );
+};
+
+export default ImageThumbnail
