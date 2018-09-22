9e18bd71caf255a88d8f3eb7595ebb075e52cdcc
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-09-22 12:45:17 -0400

    Create Action Button Component

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js'
+import ActionButton from './components/ButtonComponents/ActionButton.js'
 
 const App = () => {
   return (
     <div>
         <CalculatorDisplay value="0" />
+        <ActionButton value="Clear"/>
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (props)    =>  {
+    return(
+        <div>
+            {props.value}
+        </div>
+    )
+}
+
+export default ActionButton;
