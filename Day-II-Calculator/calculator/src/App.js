905d4e7d636f91790e0c18c56751cca7fd284521
Wesley Meeks <wesmeeks88@gmail.com>
2018-09-22 10:49:33 -0600

    Created layout of calculator, got everything divided into rows. Imported CalcDisplay, Action and NumberButton to my Apps.js.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
+import ActionButton from './components/ButtonComponents/ActionButton.js';
+import NumberButton from './components/ButtonComponents/NumberButton.js';
+
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div class ='container'>
+    <div class = "calculator">
+
+    <div class ='display-screen'>
+    <CalculatorDisplay text = '100' type = 'screen'/>
     </div>
+
+    <div class = 'row'>
+    <ActionButton text = 'ClEAR' type = 'clear'/>
+    <ActionButton text = '/' type = 'divide'/>
+    </div>
+
+    <div class = 'row'>
+    <NumberButton text = '7'/>
+    <NumberButton text = '8'/>
+    <NumberButton text = '9'/>
+    <ActionButton text = 'X' type = 'multiply'/>
+    </div>
+
+    <div class = 'row'>
+    <NumberButton text = '4'/>
+    <NumberButton text = '5'/>
+    <NumberButton text = '6'/>
+    <ActionButton text = '-' type = 'subtract'/>
+    </div>
+
+    <div class = 'row'>
+    <NumberButton text = '1'/>
+    <NumberButton text = '2'/>
+    <NumberButton text = '3'/>
+    <ActionButton text = '+' type = 'add'/>
+    </div>
+
+    <div class = 'row'>
+    <NumberButton text = '0'/>
+    <ActionButton text = '=' type = 'equals'/>
+    </div>
+    </div>
+    </div>
+
+
   );
 };
 
