6d1e006caa323b497c61f44190f3203ee151ed2c
alasalle <angela.lasalle91@gmail.com>
2018-09-22 11:45:53 -0400

    added space between icon and text in footer

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     return (
         <div className="Footer">
             <a href="#" target="_blank"><i className="far fa-comment fa-flip-horizontal fa-2x"></i></a>
-            <a href="#" target="_blank"><i className="fas fa-sync fa-2x">6</i></a>
-            <a href="#" target="_blank"><i className="far fa-heart fa-2x">4</i></a>
+            <a href="#" target="_blank"><i className="fas fa-sync fa-2x">{` ${6}`}</i></a>
+            <a href="#" target="_blank"><i className="far fa-heart fa-2x">{` ${4}`}</i></a>
             <a href="#" target="_blank"><i className="far fa-envelope fa-2x"></i></a>
         </div>
     )
