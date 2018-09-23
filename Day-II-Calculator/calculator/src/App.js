49c9579cb7e7fe277bff971d421e61b664f5ac82
Henry Moland <henrymoland@gmail.com>
2018-09-23 13:12:14 -0700

    Completed Day 2 Calculator Project

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+
+      <div>
+          <CalculatorDisplay displayStyle={'display'} text={'0'} />
+      </div>
+
+      <div>
+          <ActionButton buttonStyle={'action'} text={'clear'} />
+          <NumberButton buttonStyle={'number-red'} text={"\u00f7"} />
+      </div>
+
+      <div className ="top-row">
+        <NumberButton buttonStyle = {'number'} text= {'7'} />
+        <NumberButton buttonStyle = {'number'} text= {'8'} />
+        <NumberButton buttonStyle = {'number'} text= {'9'} />
+        <NumberButton buttonStyle = {'number-red'} text= {'X'} />
+      </div>
+
+      <div className ="middle-row">
+        <NumberButton buttonStyle = {'number'} text= {'4'} />
+        <NumberButton buttonStyle = {'number'} text= {'5'} />
+        <NumberButton buttonStyle = {'number'} text= {'6'} />
+        <NumberButton buttonStyle = {'number-red'} text= {'-'} />
+      </div>
+
+       <div className ="middle-row">
+        <NumberButton buttonStyle = {'number'} text= {'1'} />
+        <NumberButton buttonStyle = {'number'} text= {'2'} />
+        <NumberButton buttonStyle = {'number'} text= {'3'} />
+        <NumberButton buttonStyle = {'number-red'} text= {'+'} />
+      </div>
+
+      <div>
+          <ActionButton buttonStyle={'action'} text={'0'} />
+          <NumberButton buttonStyle={'number-red'} text={'='} />
+      </div>
+
     </div>
+
   );
 };
 
