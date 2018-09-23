806f44f77b1c3cf799b7b695e071d3ecb8267968
Glenn-David Daniel <d2rd@outlook.com>
2018-09-22 17:51:23 -0700

    Refactored caclulator base.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import NumberButton from "./components/ButtonComponents/NumberButton";
+import ActionButton from "./components/ButtonComponents/ActionButton";
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="container">
+      <CalculatorDisplay />
+      <div className="row">
+        <NumberButton btnWidth="btn-wide">clear</NumberButton>
+        <ActionButton>÷</ActionButton>
+      </div>
+      <div className="row">
+        <NumberButton btnWidth="btn-regular">7</NumberButton>
+        <NumberButton btnWidth="btn-regular">8</NumberButton>
+        <NumberButton btnWidth="btn-regular">9</NumberButton>
+        <ActionButton>X</ActionButton>
+      </div>
+      <div className="row">
+        <NumberButton btnWidth="btn-regular">7</NumberButton>
+        <NumberButton btnWidth="btn-regular">8</NumberButton>
+        <NumberButton btnWidth="btn-regular">9</NumberButton>
+        <ActionButton>X</ActionButton>
+      </div>
+      <div className="row">
+        <NumberButton btnWidth="btn-regular">7</NumberButton>
+        <NumberButton btnWidth="btn-regular">8</NumberButton>
+        <NumberButton btnWidth="btn-regular">9</NumberButton>
+        <ActionButton>X</ActionButton>
+      </div>
+      <div className="row">
+        <NumberButton btnWidth="btn-wide">0</NumberButton>
+        <ActionButton>=</ActionButton>
+      </div>
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Button.css';
+
+
+
+export default (props) => <div className={`btn-borders NumberButton ${props.btnWidth}`}>{props.children}</div> // stateless functional component ( no this.state object)
