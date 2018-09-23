3dd26cee2b518e1f545f75a3ef9da350f677ffe7
vy3191 <venkyafhs@gmail.com>
2018-09-22 23:16:21 -0400

    calculator styles changed

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -11,29 +11,29 @@ const App = () => {
       <CalculatorDisplay padding="padding-right"/>
       <div className='button-container'>
          <ActionButton customStyle="clear" text="clear" />
-         <NumberButton color="red" text="/" />
+         <NumberButton color="red divide" text="&divide;" />
       </div>
       <div className='button-container'>
          <NumberButton color="white" text="7" />
          <NumberButton color="white" text="8" />
          <NumberButton color="white" text="9" />
-         <NumberButton color="red" text="X" />
+         <NumberButton color="red" text="&times;" />
       </div>
       <div className='button-container'>
          <NumberButton color="white" text="4" />
          <NumberButton color="white" text="5" />
          <NumberButton color="white" text="6" />
-         <NumberButton color="red" text="-" />
+         <NumberButton color="red minus" text="&minus;" />
       </div>
       <div className='button-container'>
          <NumberButton color="white" text="1" />
          <NumberButton color="white" text="2" />
          <NumberButton color="white" text="3" />
-         <NumberButton color="red" text="+" />
+         <NumberButton color="red" text="&#43;" />
       </div>
       <div className='button-container'>
          <ActionButton customStyle="zero" text="0" />
-         <NumberButton color="red" text="=" />
+         <NumberButton color="red" text="&#61;" />
       </div>
     </div>
   );
