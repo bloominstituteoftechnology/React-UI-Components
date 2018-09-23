aa4a6fc97fdb51cffc0c3ed2163ce6f9992f591f
Glenn-David Daniel <d2rd@outlook.com>
2018-09-22 18:54:07 -0700

    WIP button style adjustments.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -7,28 +7,30 @@ import ActionButton from "./components/ButtonComponents/ActionButton";
 const App = () => {
   return (
     <div className="container">
-      <CalculatorDisplay />
+    <div className="CalculatorDisplay">
+      <CalculatorDisplay>0</CalculatorDisplay>
+    </div>
       <div className="row">
-        <NumberButton btnWidth="btn-wide">clear</NumberButton>
+        <NumberButton id="btn-clear" btnWidth="btn-wide">clear</NumberButton>
         <ActionButton>÷</ActionButton>
       </div>
       <div className="row">
         <NumberButton btnWidth="btn-regular">7</NumberButton>
         <NumberButton btnWidth="btn-regular">8</NumberButton>
         <NumberButton btnWidth="btn-regular">9</NumberButton>
-        <ActionButton>X</ActionButton>
+        <ActionButton>x</ActionButton>
       </div>
       <div className="row">
-        <NumberButton btnWidth="btn-regular">7</NumberButton>
-        <NumberButton btnWidth="btn-regular">8</NumberButton>
-        <NumberButton btnWidth="btn-regular">9</NumberButton>
-        <ActionButton>X</ActionButton>
+        <NumberButton btnWidth="btn-regular">4</NumberButton>
+        <NumberButton btnWidth="btn-regular">5</NumberButton>
+        <NumberButton btnWidth="btn-regular">6</NumberButton>
+        <ActionButton>-</ActionButton>
       </div>
       <div className="row">
-        <NumberButton btnWidth="btn-regular">7</NumberButton>
-        <NumberButton btnWidth="btn-regular">8</NumberButton>
-        <NumberButton btnWidth="btn-regular">9</NumberButton>
-        <ActionButton>X</ActionButton>
+        <NumberButton btnWidth="btn-regular">1</NumberButton>
+        <NumberButton btnWidth="btn-regular">2</NumberButton>
+        <NumberButton btnWidth="btn-regular">3</NumberButton>
+        <ActionButton>+</ActionButton>
       </div>
       <div className="row">
         <NumberButton btnWidth="btn-wide">0</NumberButton>
