20f601c428d3125410fa63c8b138053ff9fc1d16
Pascale Pierre <pascaleee@gmail.com>
2018-09-23 22:16:26 -0500

    styles more of buttons

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Button.css';
+
+
+const Button = (props) => {
+  return <button className={props.displayStyle}>{props.text}</button>
+}
+
+
+class ActionButton extends Component {
+  render() {
+    return (
+    <div displayStyle="row-left" >
+  
+
+  <Button text="&divide;" displayStyle="action" />
+
+  <Button text="&times;" displayStyle="action" />
+
+  <Button text="-" displayStyle="action" />
+  <Button text="+" displayStyle="action" />
+  <Button text="=" displayStyle="action" />
+      
+      
+          </div>);
+
+  }
+
+  }
+
+  export default ActionButton;
+  
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Display.css';
+
+
+const Button = (props) => {
+  return <button className={props.displayStyle}>{props.text}</button>
+}
+
+
+class CalculatorDisplay extends Component {
+    render() {
+      return (
+      
+        <Button text="0" displayStyle="input" />
+      )}
+  }
+
+  export default CalculatorDisplay;
