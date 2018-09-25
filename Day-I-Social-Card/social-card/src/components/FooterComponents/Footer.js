d96094c6a6a3276bc8b6f52a2ad3495c8ae00ff9
Kelley Henican <kelleyhenican2@gmail.com>
2018-09-25 18:40:07 -0500

    added footer

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+    return (
+        <div>
+        <i class="fa fa-comment" aria-hidden="true"></i>
+        <i class="fa fa-heart" aria-hidden="true">    0</i>
+        </div>
+    )
+}
+
+
+
+
+export default Footer;
