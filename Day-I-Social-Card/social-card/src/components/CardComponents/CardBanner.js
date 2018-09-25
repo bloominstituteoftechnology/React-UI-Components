0c8523e5ef752a4a938bda4ef0bab0383c2e9f4d
Kelley Henican <kelleyhenican2@gmail.com>
2018-09-25 18:37:55 -0500

    finished card banner

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+
+
+const CardBanner = () => {
+    return (
+        <a href="https://www.reactjs.org">
+        <div id="banner"></div>
+        </a>
+    )
+}
+
+
+
+
+export default CardBanner;
