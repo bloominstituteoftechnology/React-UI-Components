4fb71426ba2f6cb3971e91e84e97959000378cc0
Courtney Buratto <you@example.com>
2018-09-26 14:21:45 -0700

    CARDCONTENT.JS complete

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+    return (
+        <div className ="card-content-wrapper">
+            <div className = "card-content-header">Get started with React</div>
+            <div className = "card-content">React makes it painless to create interactive UIs. Design simple views for each state in your application.</div>
+            <div className = "card-content-footer">reactjs.org</div>
+        </div> 
+       
+
+    );
+};
+
+export default CardContent;
