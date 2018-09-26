3f721e44e77e6fd279a01f5a7b186c9bef64a10a
Courtney Buratto <you@example.com>
2018-09-26 14:05:26 -0700

    HEADERTITLE.JS complete

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title-wrapper">
+            <div className="header-name">Lambda School</div>
+            <div className="header-handle">@LambdaSchool</div>
+            <div className="header-date">26 Sept</div> 
+        </div>
+    );
+};
+
+export default HeaderTitle;
