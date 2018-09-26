80dd0da18fb6f55d6a64472afa1a980b1be82527
Kelley Henican <kelleyhenican2@gmail.com>
2018-09-26 18:27:24 -0500

    app added

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './App.css';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
+
+const App = () => {
+  return (
+    <div>
+    <HeaderContainer />
+    <CardContainer />
+    <Footer />
+
+  </div>
+  );
+};
+
+export default App;
