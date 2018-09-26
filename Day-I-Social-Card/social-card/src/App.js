65fc0d4ecc015f58818398ab763ce422d9c11ef9
Jesse <theiseja@gmail.com>
2018-09-26 14:48:38 -0500

    Finished MVP for Social Card. Positioning might be a tad off

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-
+import CardContainer from './components/CardComponents/CardContainer';
 const App = () => {
  return (
    <div className='container'>
     <HeaderContainer />
+    <CardContainer />
    
    </div>
  );
