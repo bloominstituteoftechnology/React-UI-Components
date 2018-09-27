6a1357f13e0bb3baf1f0aa0bfae078add93507e9
Courtney Buratto <you@example.com>
2018-09-26 20:15:45 -0700

    FIX added react link to CardContent.js

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -6,6 +6,7 @@ const CardContent = props => {
         <div className ="card-content-wrapper">
             <div className = "card-content-header">Get started with React</div>
             <div className = "card-content">React makes it painless to create interactive UIs. Design simple views for each state in your application.</div>
+            <a className = "react" href = "https://www.reactjs.org">reactjs.org</a>
         </div> 
        
 
