81efe49c581ee4db60a8731bf8e24296ddc918da
Courtney Buratto <you@example.com>
2018-09-27 19:57:16 -0700

    MVP CalculatorDisplay.js complete

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return (
+    <div className = "display">O</div>
+    );
+};
+
+export default CalculatorDisplay;
