d37a2163daec269fe7207084f57b0ee07329f140
Jesse <theiseja@gmail.com>
2018-09-27 22:00:47 -0500

    Implanted Footer as a stretch goal in the Social Card project.

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer-buttons'>
+        <i className='far fa-comment' />
+        <i className='fas fa-retweet' />
+        <p></p>
+        <i className='far fa-heart' />
+        <p></p>
+        <i className='far fa-envelope' />
+        </div>
+    );
+};
+
+export default Footer;
