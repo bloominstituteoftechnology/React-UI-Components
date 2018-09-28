81e9a97fb2a48cd867cfb909dba3e7cfe582e166
Jesse <theiseja@gmail.com>
2018-09-27 20:02:45 -0500

    Finished calculator display, so MVP is currently done. Not functional yet

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -5,6 +5,7 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 const App = () => {
   return (
     <div className='app'>
+      <div className='blackbox'>0</div>
     <CalculatorDisplay />
     </div>
   );
