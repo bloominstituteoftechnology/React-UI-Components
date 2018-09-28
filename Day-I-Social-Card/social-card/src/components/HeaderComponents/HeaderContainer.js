bcced4c5d3e6f8f44e2a66c3219a315a3c42f7cb
FBT <flaming.bunny.tomb@gmail.com>
2018-09-27 20:14:54 -0700

    Outlined on a whiteboard what I need to do and tree for  files. In VS Code  I made notes framing what I need to do with app.js and header content.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+// import header content, title, img
+
+
+// this holds all the other header bits, HeaderContent, HeaderTitle, Image thumbnail
+// Header Content = 'Let's learn'
+// Header Title = 'Lambda School', date etc
+// Image Thumbnail = ^ logo
+
+// export default HeaderContainer
