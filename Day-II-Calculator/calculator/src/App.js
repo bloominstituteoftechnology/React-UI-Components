ea5f8f51bea7fdf15f225ea00bb1aeb4d1ca2d06
Courtney Buratto <you@example.com>
2018-09-27 20:32:07 -0700

    MVP App.js sketched out, things are rendering :)

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className = "calc-wrapper">
+      <CalculatorDisplay />
+      <div className = "btn-wrapper">
+        <ActionButton />
+        <NumberButton />
+      </div>
+      <div className = "btn-wrapper">
+        <NumberButton />
+        <NumberButton />
+        <NumberButton />
+        <NumberButton />
+      </div>
+      <div className = "btn-wrapper">
+        <NumberButton />
+        <NumberButton />
+        <NumberButton />
+        <NumberButton />
+      </div>
+      <div className = "btn-wrapper">
+        <NumberButton />
+        <NumberButton />
+        <NumberButton />
+        <NumberButton />
+      </div>
+      <div className = "btn-wrapper">
+        <ActionButton />
+        <NumberButton />
+      </div>
     </div>
   );
 };
