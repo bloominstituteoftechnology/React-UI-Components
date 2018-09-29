df6a0bdb17912e5e0e1a7e54c2c84ffc77fb8b69
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-09-29 13:47:27 -0400

    (DAY 1 SOCIAL CARD) : (Card) : created file structure for CardBanner

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = props => {
+    return (
+        <div>
+            <img src="https://ibin.co/3wnC6SgIOJud.png"/>
+        </div>
+    );
+};
+
+export default CardBanner;
