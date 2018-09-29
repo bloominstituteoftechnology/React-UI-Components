13385856d542668776c7dbbc6d517b116b220d7e
Courtney Buratto <you@example.com>
2018-09-29 10:32:35 -0700

    FIX updated NumberButton.js to be able to pass color and size in styles

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return (
+    <button className ={"num-btn ${props.bgColor}"}>{props.text}{props.input}
+    </button>
+    );
+};
+
+export default NumberButton;
