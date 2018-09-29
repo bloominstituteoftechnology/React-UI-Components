05119df81ae3ca90fb2158455b4376f74cdbac53
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-09-29 11:21:34 -0400

    (DAY 1 SOCIAL CARD) : (Header) : created file structure for HeaderContainer.js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = proprs => {
+    return (
+        <div>
+            <ImageThumbnail />
+            <div>
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
+    );
+};
+
+export default HeaderContent;
