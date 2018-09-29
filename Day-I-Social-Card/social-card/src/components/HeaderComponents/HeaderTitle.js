ac00b6a18c922436c5e5b7b9cf2d2beb01753d77
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-09-29 10:58:54 -0400

    (DAY 1 SOCIAL CARD) : (Header) : created initial ImageThumbnail structure

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+cosnt HeaderTitle = props => {
+    return (
+        <div>
+            <div>Lambda School</div>
+            <div>@LambdaSchool</div>
+            <div>&bull;</div>
+            <div>2 sept</div>
+        </div>
+    );
+};
+
+expoert defualt HeaderTitle;
