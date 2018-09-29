ce7d89c0b2c81017c16ce6321cf257b42fcbaa9a
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-09-29 14:27:22 -0400

    (DAY 1 SOCIAL CARD) : (Card) : created file structure for CardContent

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+
+const CardContent = props => {
+    return (
+        <div className="card-content-container">
+            <div className="card-content-title">
+                Get Started
+            </div>
+            <div className="card-content-text">
+                React makes it painles to create interactuve UIs. Design simple views for each state in your application.
+            </div>
+            <div className="card-content-link-text">
+                reactjs.org
+            </div>
+        </div>
+    );
+};
+
+export default CardContent;
