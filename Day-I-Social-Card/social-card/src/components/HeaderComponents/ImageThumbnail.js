e20e77f5375edba9d9601ff853426a2ff5150c60
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-09-29 11:10:15 -0400

    (DAY 1 SOCIAL CARD) : (Header) : created initial file structure for HeaderCOntent

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = props => {
+    return (
+        <div>
+            <img src=""/>
+        </div>
+    );
+};
+
+export default ImageThumbnail;
