5351e3bb17f23713534d78afc716c153f4949fa7
erscriber <erscriber@gmail.com>
2018-09-29 10:48:38 -0400

    Component Skeleton complete

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+	return (
+		<button className={`action-btn`}>{props.input}</button>
+	)
+};
+
+ActionButton.defaultProps = {
+	input: 'Input'
+};
+
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,44 @@
 import React from 'react';
 import './Display.css';
+import NumberButton from '../ButtonComponents/NumberButton';
+import ActionButton from '../ButtonComponents/ActionButton';
+
+const CalculatorDisplay = () => {
+	return (
+		<div className="calculator-container">
+			<div className="output-display">
+				<NumberButton text="0"/>
+			</div>
+			<div className="clear-container">
+				<ActionButton input="clear"/>
+				<ActionButton input="÷"/>
+			</div>
+			<div className="7-container">
+				<NumberButton text="7"/>
+				<NumberButton text="8"/>
+				<NumberButton text="9"/>
+				<ActionButton input="x"/>
+			</div>
+			<div className="4-container">
+				<NumberButton text="4"/>
+				<NumberButton text="5"/>
+				<NumberButton text="6"/>
+				<ActionButton input="-"/>
+			</div>
+			<div className="1-container">
+				<NumberButton text="1"/>
+				<NumberButton text="2"/>
+				<NumberButton text="3"/>
+				<ActionButton input="+"/>
+			</div>
+			<div className="0-container">
+				<NumberButton text="0"/>
+				<ActionButton input="="/>
+			</div>
+
+		</div>
+
+	)
+};
+
+export default CalculatorDisplay;
