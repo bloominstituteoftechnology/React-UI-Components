87404ccc9c239b49acb1fa9eccfbc017efbdb0f5
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-09-29 13:51:59 -0400

    (DAY 1 SOCIAL CARD) : (Card) : created file structure for CardContainer

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -4,7 +4,7 @@ import './Card.css';
 const CardBanner = props => {
     return (
         <div>
-            <img src="https://ibin.co/3wnC6SgIOJud.png"/>
+            <img className="card-banner-img" src="https://ibin.co/3wnC6SgIOJud.png"/>
         </div>
     );
 };
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+const CardContainer = props => {
+    return (
+        <div className="card-container-container">
+            <div className="card-container">
+                <a href="https://www.reactjs.org/">
+                    <CardBanner />
+                    <CardContent />
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default CardContainer;
