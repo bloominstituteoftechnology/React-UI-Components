f943a3c2ed226a66a6729855a46cca8308d976e4
Demetri Bethel <demetribethel@gmail.com>
2018-09-30 19:58:28 -0400

    Done?

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import './Button.css';
+
+
+const NumberButton = props => 
+(
+   
+   
+   
+    <div> <button className={props.buttonStyle}>{props.text}
+    
+    </button>  
+    
+     </div>
+)
+
+
+
+
+
+
+
+
+export default NumberButton; 
