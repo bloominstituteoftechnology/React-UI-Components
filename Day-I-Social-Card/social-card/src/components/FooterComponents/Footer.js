d7307659f15ac6bcd0210cc41e1d5d362198dc4e
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-09-29 20:07:00 -0400

    (STRETCH) : (Footer) : created a Footer.js file structure and attempted to replicate the appearance of the footer

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = props => {
+    return (
+        <div className="footer-container">
+            <img className="comment" src="https://image.ibb.co/kg0h6z/Footer_Img.png"/>
+        </div>
+    );
+};
+
+export default Footer;
