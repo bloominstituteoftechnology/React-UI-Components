5ab712695de0512d5df33cd579d82fc8193a60be
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-10-01 17:00:20 -0600

    card

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 const CardContainer = props => {
     return (
-        <div>
-            CARD CONTAINER
+        <div className="card-wrapper">
+            <CardBanner />
+            <CardContent />
         </div>
     );
 }
