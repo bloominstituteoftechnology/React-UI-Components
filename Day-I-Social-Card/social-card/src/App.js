90f1709079ee50c0591d4196280392c3c04e64ec
Ryan Boris <neodynia@icloud.com>
2018-10-01 12:28:23 -0400

    Update card component

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 
 const App = () => {
   return (
     <div>
+    <div>
       <HeaderContainer />
+    </div>
+    <div>
+      <CardContainer />
+    </div>
     </div>
   );
 };
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+        <div className = "card-banner"><img src="https://ibin.co/3wnC6SgIOJud.png" alt="react logo" /></div>
+        )
+    }
+
+export default CardBanner;
