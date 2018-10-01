2114fcdad7f3a82b70a8e22cae93bb9c336b8d44
FBT <flaming.bunny.tomb@gmail.com>
2018-10-01 14:56:37 -0700

    basic calculator display

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return (
+        <button className ={props.btnStyle}>{props.input}</button>
+    )
+};
+
+ActionButton.defaultProps = {
+    input: 'Input'
+};
+
+
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return (
+        <button className ={props.btnStyle}>{props.text}</button>
+    )
+};
+
+NumberButton.defaultProps = {
+    text: 'Text'
+};
+
+
+export default NumberButton;
