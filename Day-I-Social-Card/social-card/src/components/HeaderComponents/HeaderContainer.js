633e24f469632ec32774da51178a5069ed8f708d
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-01 15:29:48 -0400

    created a header component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = () => {
+    return (
+        <div>
+        <ImageThumbnail />
+        <HeaderTitle />
+        <HeaderContent />
+        </div>
+    );
+}; 
