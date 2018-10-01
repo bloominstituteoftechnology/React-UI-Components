55f8300775d2f995fdbfe87b0218bbea5b0bb028
Ryan Boris <neodynia@icloud.com>
2018-10-01 19:03:57 -0400

    Fix npm package concerning font awesome

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
-import Footer from './components/FooterComponents/Footer'
+import Footer from './components/FooterComponents/Footer';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStroopwafel)
 
 
 const App = () => {
--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Footer.css';
-import '/Users/ryanboris/Documents/Projects/Lambda/week-six/React-UI-Components/Day-I-Social-Card/social-card/node_modules/@fortawesome/fontawesome-free/js/all.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 
 const Footer = () => {
    return(
    <div className="footer">
-        <button className="button1"><i className="fa fa-comment-o"></i></button>
-        <button className="button2"><i className="fa fa-refresh"></i></button>
-        <button className="button3"><i className="fa fa-heart-o"></i></button>
-        <button className="button4"><i className="fa fa-envelope-o"></i></button>
+        <button className="button1"><FontAwesomeIcon icon="comment-o" /></button>
+        <button className="button2"><FontAwesomeIcon icon="refresh" /></button>
+        <button className="button3"><FontAwesomeIcon icon="heart-o" /></button>
+        <button className="button4"><FontAwesomeIcon icon="fa fa-envelope-o" /></button>
     </div>
    )
 }
