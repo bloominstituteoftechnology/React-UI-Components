680ea546bbbfac25cf326859aee3fd9a57a428c5
Cwright0428 <cwright0428@yahoo.com>
2018-10-01 18:28:34 -0500

    css changes

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -7,9 +7,10 @@ const HeaderTitle = () => {
     <div className="header-title-wrapper">
       <div className="header-title">
         Lambda School
-        <span className="date"> july 31</span>
+        <span className="commentor"> @Lambda School</span>
+        <span className="date"> - july 31</span>
+        </div>
         <HeaderContent />
-      </div>
     </div>
   );
 };
