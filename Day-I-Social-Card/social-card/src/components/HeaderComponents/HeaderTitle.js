695d088bcf8c1cb96423be2c3c033beaa580139a
Yanrong Xu <xuyanrong@icloud.com>
2018-10-01 15:38:31 -0400

    complete Header title and imagethumbnail

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return (
+        <div>
+            <h1>Lambda School</h1>
+            <p>@Lambda School</p>
+            <p>1 oct</p>
+        </div>
+    )
+}
+
+export default HeaderTitle;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+const ImageThumbnail = () => {
+    return (
+        <div>
+            <img src= 'https://ibin.co/3whrpKSBbZ81.png' />
+        </div>
+        )
+}
+
+export default ImageThumbnail;
