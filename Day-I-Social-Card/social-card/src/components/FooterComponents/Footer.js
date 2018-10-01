a11c68753bbf2c93c5fdc8d8fe314646c0dfbd8f
kyranmccann <kyran.mccann@gmail.com>
2018-10-01 14:23:42 -0700

    working on click handlers for the icons in the footer

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Footer.css';
 
 
-const Footer = () => {
-  return (
-    <div className='footer-container'>
-      <i className='far fa-comment'></i>
-      <div className='icon-group'>
-        <i className="fas fa-sync-alt"></i> 6
-      </div>
-      <div className='icon-group'>
-        <i className="far fa-heart"></i> 4
-      </div>
-      <i className="far fa-envelope"></i>
-    </div>
-  )
-}
+// const Footer = () => {
+//   return (
+//     <div className='footer-container'>
+//       <i className='far fa-comment'></i>
+//       <div className='icon-group'>
+//         <i className="fas fa-sync-alt"></i> 6
+//       </div>
+//       <div className='icon-group'>
+//         <i className="far fa-heart"></i> 4
+//       </div>
+//       <i className="far fa-envelope"></i>
+//     </div>
+//   )
+// }
 
+class Footer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loves: 0,
+      retweets: 0
+    }
+  }
+
+  loveNumber = () => {
+    this.setState({ loves: this.state.loves + 1})
+  }
+
+  retweetNumber = () => {
+    this.setState( { retweets: this.state.retweets + 1})
+  }
+
+  render() {
+    return (
+      <div className='footer-container'>
+        <i className='far fa-comment'></i>
+        <div className='retweets' onClick={this.retweetNumber}>
+          <i className="fas fa-sync-alt"></i> {this.state.retweets}
+        </div>
+        <div className='loves' onClick={this.loveNumber}>
+          <i className="far fa-heart"></i> {this.state.loves}
+          </div>
+        <i className="far fa-envelope"></i>
+      </div>
+    );
+  }
+}
 export default Footer;
