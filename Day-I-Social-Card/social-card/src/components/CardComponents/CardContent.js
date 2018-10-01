d3df61dd16216ef56b649bdad59abef9c2c7dc67
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-01 15:58:25 -0500

    Card content and banner js

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className="banner-bottom">
+            <p className="banner-content">Get started with React</p>
+            <p className="banner-content">React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <a href="http://reactjs.org" className="banner-content content-link">
+                reactjs.org
+            </a>
+        </div>
+    );
+}
+
+export default CardContent;
