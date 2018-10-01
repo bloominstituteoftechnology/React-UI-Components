9d483b0a3ca15148448cb2bcb4753daab367462e
MEBonham <programmer@mckaybonham.com>
2018-10-01 16:03:34 -0600

    Got header and card, started importing FontAwesome stuff

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from "./CardBanner";
+import CardContent from "./CardContent";
+
+const CardContainer = props => {
+    return (
+        <a href="https://www.reactjs.org">
+            <div className="card">
+                <CardBanner />
+                <CardContent />
+            </div>
+        </a>
+    );
+}
+
+export default CardContainer;
