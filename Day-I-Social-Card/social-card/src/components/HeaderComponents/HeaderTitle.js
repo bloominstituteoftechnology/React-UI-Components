2363c66451dcdf61bf1bcf201a1776099b32eeb6
Andre Myrick <amyrick.820@gmail.com>
2018-10-01 16:08:52 -0400

    completed header component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+    return (
+        <div className="header-title">
+            <p className="twitter-name">Lambda School</p>
+            <p className="twitter-handle">@LambdaSchool • 26 jan</p>
+        </div>
+    );
+}
+
+export default HeaderTitle;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+const ImageThumbnail = props => {
+    return (
+        <img className="twitter-tb" src="https://ibin.co/3whrpKSBbZ81.png" alt="Lambda School logo" />
+    );
+}
+export default ImageThumbnail;
