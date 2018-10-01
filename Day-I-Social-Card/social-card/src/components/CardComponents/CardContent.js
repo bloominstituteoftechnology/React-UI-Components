ab7d8c146789e658dd2837bd02bccfd318d5c190
SavannahGreuel <smallidgesavannah@gmail.com>
2018-10-01 14:54:01 -0600

    added card container components

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className = 'card-content'>
+            <h1>
+                Get Started With React
+            </h1>
+            <p>
+            React makes it painless to create 
+            interactive UIs. Design simple views for each state 
+            in your application. 
+            </p>
+            <p className = 'link-text'>reactjs.org
+            </p>
+        </div>
+    );
+};
+
+export default CardContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from "./ImageThumbnail";
 import HeaderTitle from "./HeaderTitle";
-//import HeaderContent from "./HeaderContent";
-
+import HeaderContent from "./HeaderContent"
 const HeaderContainer = () => {
     return (
         <div className = "header">
         <ImageThumbnail />
         <HeaderTitle />
+        <HeaderContent />
         </div>  
     );
 };
