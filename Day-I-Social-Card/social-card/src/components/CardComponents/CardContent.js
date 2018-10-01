b8bd7944f0e692f2aedb0ee20e774055ea31f4e1
Brad Mortensen <b.mort86@gmail.com>
2018-10-01 13:26:04 -0600

    initial commit, small changes to cardcontent.js

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return <div>
+                <h2>Get started with React</h2>
+                <p>React makes it painless to creae interactive UIs. Design siumple views for each state in your application.</p>
+                <p>reactjs.org</p>
+           </div>
+}
