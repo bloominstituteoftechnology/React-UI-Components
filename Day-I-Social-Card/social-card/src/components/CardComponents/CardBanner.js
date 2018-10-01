f16321554595e6de632fa7e32166f0eab5850e3d
Unknown <zachary.arney@gmail.com>
2018-10-01 18:01:47 -0500

    Set up card components

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = props => {
+  return <img src="https://ibin.co/3wnC6SgIOJud.png" alt="React logo" />;
+};
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+  return (
+    <div className="card-content">
+      <h3>Get started with React</h3>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application.
+      </p>
+      <a href="https://www.reactjs.org">reactjs.org</a>
+    </div>
+  );
+};
+
+export default CardContent;
