7991ef0ae06a4dc84e810a98a77ea9fb58d71231
Angelo Deleon <rudy209@comcast.net>
2018-10-01 15:31:43 -0700

    working on header section

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return(
+        <div className='headerTitle'>
+            <p>Header Title Section</p>
+        </div>
+    );
+}
+
+export default HeaderTitle;
