186f4b56a0e360f09c68a48f23a1f810084ab7d9
Lee Len Formento <leebatef@gmail.com>
2018-10-01 15:42:17 -0400

    CardBanner.js

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardBanner = () => {
+    return (
+        <img class="card-img" src="https://ibin.co/3wnC6SgIOJud.png" alt="React Logo" />
+
+    )
+};
+
+export default CardBanner;
