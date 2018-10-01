4982effd43ea7febbc0c61953cba45e9e0c4a951
Katie Cleary <katiecleary@airmaster.lan>
2018-10-01 17:10:00 -0400

    built basic card components and linked. Next styling

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+    return (
+    <div className = "cardContent">
+    <h1>Get Started with React</h1>
+     <p>React makes it painless to create interactive UIs. Design simple views for each state in your application</p>
+     <h2>reactjs.org</h2>
+    </div>
+ );
+ };
+
+ export default CardContent;
