fc30a7926b2051688e9785ee5d3cad6aa0b1695e
Brandon Vilaychith <blvilaychith@outlook.com>
2018-10-01 17:57:30 -0500

    added footer and icons

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const CardFooter = (props) => {
+    let likes = null;
+    let repost = null;
     return (
-        <div className='footer'>
-            <FontAwesomeIcon icon="faHeart" />
+        <div className='cardFooter'>
+            <FontAwesomeIcon icon="comment" className='icons'/>
+            <FontAwesomeIcon icon="retweet" className='icons'/>
+            <p className='counter'>{repost}</p>
+            <FontAwesomeIcon icon="heart" className='icons'/>
+            <p className='counter'>{likes}</p>
+            <FontAwesomeIcon icon="envelope" className='icons'/>
         </div>
     );
 }
