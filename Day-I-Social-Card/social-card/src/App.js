1e70b4186553d01d4dd29a65dd16ae53771205df
Drewsup123c <drewsup123c@gmail.com>
2018-10-01 14:26:19 -0600

    finished card container react section

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-
+import CardContainer from './components/CardComponents/CardContainer';
 const App = () => {
   return (
-    <HeaderContainer />
+    <div className="Post">
+      <HeaderContainer />
+      <CardContainer />
+    </div>
     /*
     <div>
       <h3>Welcome to React Social Card!</h3>
