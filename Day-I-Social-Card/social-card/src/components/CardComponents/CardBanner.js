279ef80ad1eabae7eff26e3f962668826e072763
Angelo Deleon <rudy209@comcast.net>
2018-10-01 14:29:17 -0700

    started working on card content

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+        <div>
+            <img src='https://ibin.co/3whrpKSBbZ81.png'/>
+        </div>
+    );
+}
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () => {
+    return(
+        <div>
+            <h2>Get Started with React</h2>
+            <p> React makes it painless to create interactice UIs. Design simple views for each state in your application.</p>
+            <p class='reactURL'>reactjs.org</p>
+        </div>
+    );
+}
+
+export default CardContent;
