5d4e2b68d29b3c158843129bf8316ceb644322b1
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-01 16:54:40 -0400

    created CardContent component

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    <div>
+        <p>
+            React makes it painless to create interactive UIs. Design simple views for each state in your application.
+        </p>
+    </div>
+}
