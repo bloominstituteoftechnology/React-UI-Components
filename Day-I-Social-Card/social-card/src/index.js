3cba3af0090c31587aea7fd94aa01bafaaad40a9
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-01 17:44:31 -0400

    [chore] initial file setup

--- Day-I-Social-Card/social-card/src/index.js
+++ Day-I-Social-Card/social-card/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
