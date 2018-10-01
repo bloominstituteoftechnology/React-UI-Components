413037e25a881ac4a429a81d1500d33039c127fa
Gannon <gkleblanc@gmail.com>
2018-10-01 17:26:37 -0400

    finished the components for card contend and banner, now I just need to style it to look more like the design file

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardContent = props => {
+  return (
+    <div>
+      <h2>Get Started with React</h2>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application.
+      </p>
+      <p>reactjs.org</p>
+    </div>
+  );
+};
+
+export default CardContent;
