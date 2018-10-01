951f548783b82c32c271f794f575365300757b1e
Adam <ajmboosh@gmail.com>
2018-10-01 15:00:23 -0500

    got initial form of header section on page

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+import ImageThumbnail from "./ImageThumbnail";
+
+const HeaderContainer = () => {
+  return (
+    <div class="header-container">
+      <ImageThumbnail />
+      <div class="header-text-content">
+        <HeaderTitle />
+
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div>
+      <h1>Lambda Schools</h1>
+      <p>@LambdaSchool</p>
+      <p>26 Jan</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
