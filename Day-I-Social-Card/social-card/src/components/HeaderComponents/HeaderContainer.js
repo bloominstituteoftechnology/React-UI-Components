6c43cb25b36ecd982ac903e7966a6aebe469ab8e
Gannon <gkleblanc@gmail.com>
2018-10-01 17:06:40 -0400

    created the components for the header and successfully imported them though to the app, now going to work on the card content then stylize it a bit after

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+import ImageThumbNail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+
+const HeaderContainer = props => {
+  return (
+    <header>
+      <ImageThumbNail />
+      <div>
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </header>
+  );
+};
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const ImageThumbNail = props => {
+  return <img src=" https://ibin.co/3whrpKSBbZ81.png" alt="Lambda Logo" />;
+};
+
+export default ImageThumbNail;
