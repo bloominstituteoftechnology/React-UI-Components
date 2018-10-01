ff0b97bf43ecc57a5734f62cf2c61277a21a76f5
Elvis Ibarra <ielvisd@gmail.com>
2018-10-01 13:26:00 -0700

    Project Setup and Import/Export is working

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const Header = props => { // props = { type: "primary"}
+
+    return (
+      <div> 
+        <h1>TEST</h1>
+      </div>
+    );
+}
+
+export default Header;
+
+
+
