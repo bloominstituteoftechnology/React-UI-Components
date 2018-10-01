18a7b697b501c67412f4ea37c81f2fb033d09b34
Axolotl <samuelmachat@gmail.com>
2018-10-01 19:46:49 -0400

    add footer with font awesome icons

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -6,11 +6,13 @@ import './App.css'
 // import components
 import HeaderContainer from './components/HeaderComponents/HeaderContainer'
 import CardContainer from './components/CardComponents/CardContainer'
+import Footer from './components/FooterComponents/Footer'
 
 const App = () => (
   <div className="social-card-container">
     <HeaderContainer />
     <CardContainer />
+    <Footer />
   </div>
 )
 
