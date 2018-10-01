129743218d09879e915855d5ec22107a34520ef0
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-01 18:01:06 -0400

    finished styling card

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,7 +4,7 @@ import './Card.css';
 const CardContent = () => {
     return (
         <div className="card-content">
-            <h1 className="card-title">Get started with React</h1>
+            <h1>Get started with React</h1>
             <p>
                 React makes it painless to create interactive UIs. Design simple views for each state in your application.
             </p>
