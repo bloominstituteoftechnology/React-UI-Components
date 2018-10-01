340324467130e4e0c2e06bbe0fa0c8273cad0c88
Stublink <chrisrey95@gmail.com>
2018-10-01 16:57:12 -0400

    Click on container functionality

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,9 +4,12 @@ import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
 const CardContainer = props => {
+    const goToLink = () => {
+        window.location.href = 'https://www.reactjs.org'
+    }
     return (
         <div className='container'>
-            <div className='card-container'>
+            <div className='card-container' onClick={goToLink}>
                 <CardBanner src='https://ibin.co/3wnC6SgIOJud.png'/>
                 <CardContent 
                     title={'Get Started with React'}
