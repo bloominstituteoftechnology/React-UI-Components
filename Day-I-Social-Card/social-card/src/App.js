99b6f5c5231a81ac31e4c9bfe37b5d3d6659089b
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-01 14:38:04 -0700

    Finished MVP

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
+    <div className='container'>
       <HeaderContainer />
+      <CardContainer />
+    </div>
   );
-};
+}
 
 export default App;
 
