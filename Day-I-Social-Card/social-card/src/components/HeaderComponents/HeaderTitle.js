73b11a6166cf3704424f3b15c1a2dff180179fde
Carlos Lantigua <clantigua954@gmail.com>
2018-10-01 17:47:03 -0400

    added momentjs to post date

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './Header.css';
 import thumbnail from "./logo.png";
 import HeaderContent from "./HeaderContent";
+import moment from 'moment';
 
 const ThumbNail = (props) => (
   <img src={thumbnail} alt="LambdaSchool Logo" />
 );
 
+const DateStamp = (props) => {
+  return (
+    <span className="postDate">@LambdaSchool • {moment().calendar()}</span>
+  )
+}
+
 const UserMessage = (props) => (
   <div className="userPost">
     <h4>Lambda School</h4>
-    <span className="postDate">@LambdaSchool - 26 Jan</span>
+    <DateStamp />
     <HeaderContent />
   </div >
 )
