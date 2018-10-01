c7fac48cad6734e6e4c9582dcb56fb4909a9dd2b
Carlos Lantigua <clantigua954@gmail.com>
2018-10-01 19:07:32 -0400

    added alert functionality to buttons

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -6,11 +6,16 @@ import './Footer.css';
 
 
 const FooterContainer = (props) => {
-  const currentIcons = ["far fa-comment", "fas fa-sync-alt", "fas fa-heart", "fas fa-envelope"];
+  const currentIcons = [
+    "far fa-comment",
+    "fas fa-sync-alt",
+    "fas fa-heart",
+    "fas fa-envelope"
+  ];
 
   const iconList = currentIcons.map(items => {
     return (
-      <i className={items}></i>
+      <i className={items} onClick={() => { alert(`you clicked ${items}`) }}></i>
     )
   })
 
