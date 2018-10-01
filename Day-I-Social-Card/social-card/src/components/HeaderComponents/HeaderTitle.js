ddd5d4e28ace93b42b515e0d5fc5adc5fab368d3
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-01 16:42:18 -0400

    created a HeaderTitle component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div>
+            <h1>Lambda School</h1>
+            <h1>@LambdaSchool * 26 jan</h1>
+        </div>
+    );
+}
