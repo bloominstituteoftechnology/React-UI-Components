b44d812bcf7443b7c01a6dbcc3047b85f6d41cef
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-01 15:10:39 -0700

    added in footer

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Card.css";
 import CardBanner from "./CardBanner";
 import CardContent from "./CardContent";
+import Footer from "../FooterComponents/Footer";
 
 const CardContainer = () => {
   return (
@@ -9,6 +10,7 @@ const CardContainer = () => {
       <a href="https://www.reactjs.org" class="fill-div" />
       <CardBanner />
       <CardContent />
+      <Footer />
     </section>
   );
 };
