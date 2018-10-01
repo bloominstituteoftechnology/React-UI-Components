868a3f6941112e146c1d2806abe5e04dd26702ee
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-01 14:01:13 -0700

    Card component styled

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,13 +3,13 @@ import "./Card.css";
 
 const CardContent = () => {
   return (
-    <div>
+    <div className="card-content">
       <h4>Get Started with React</h4>
       <p>
         React makes it painless to create interactive UIs. Design simple views
         for each state in your application
       </p>
-      <a href="https://reactjs.org" className="light-text">
+      <a href="https://www.reactjs.org" className="light-text" target="_blank">
         reactjs.org
       </a>
     </div>
