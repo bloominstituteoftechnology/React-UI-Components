187b5823540a0eadf49024785bcc47d71f45ba4e
bummings <eabummings@gmail.com>
2018-10-01 15:55:28 -0400

    added the HeaderTitle and ImageThumbnail components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+    return (
+        <div>
+            <h1>Lambda School</h1>
+            <h2>@lambdaschool</h2>
+            <h2>Oct 1</h2>
+        </div>
+    );
+}
+
+export default HeaderTitle.js;
