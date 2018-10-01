f36cd4479b2b7a42612ea49102340ee68fd0b7ef
Mindysueasaurus <mymindyyeager@gmail.com>
2018-10-01 17:33:19 -0500

    content finished

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+ const CardBanner = props => {
+   return (
+     <div>
+       <img src="https://ibin.co/3wnC6SgIOJud.png" />
+     </div>
+   )
+ }
+
+ export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+const HeaderTitle = props => {
+   return (
+     <div>
+      <h1>Lambda School</h1>
+      <p>@LambdaSchool · 26 jan</p>
+     </div>
+   )
+}
+export default HeaderTitle;
