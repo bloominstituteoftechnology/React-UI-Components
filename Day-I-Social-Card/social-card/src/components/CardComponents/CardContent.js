fc409c6b65119b6abb523d45d4fa33f2d8f9ceb4
bummings <eabummings@gmail.com>
2018-10-01 18:33:11 -0400

    just about done styling card, need to adjust overall flex properties

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,7 +3,11 @@ import './Card.css';
 
 const CardContent = props => {
   return (
-    <h1>bruh</h1>
+    <div class='cardContentTextBox'>
+      <h3 className = 'cardContentHeadline'>Wow man React</h3>
+      <p className = 'cardContentDescription'>This is faux Twitter, man- this isn't even a real Tweet but hey: your newly-constructed React components sure as hell are.</p>
+      <a class='cardContentLink' href='http://reactjs.org'>reactjs.org</a>
+    </div>
   );
 }
 
