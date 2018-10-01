c2ca79543cbb5370633de20d2331e2c9cd737400
David247365 <ds051128@gmail.com>
2018-10-01 17:11:15 -0500

    created the components for header content

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from "./HeaderContent";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+
+
+const HeaderContainer = props => {
+    return (
+        <div className="header-container">
+            <ImageThumbnail />
+            <div className = "header-content">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
+    );
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+    return (
+      <div className="header-title">
+        <h2>Lambda School</h2>
+        <h3>@LambdaSchool • 26 jan</h3>
+      </div>
+    );
+  };
+
+  export default HeaderTitle;
