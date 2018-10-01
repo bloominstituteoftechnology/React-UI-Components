2c9b8d50be8958820c1ad3e350d3ec43e6773194
Elvis Ibarra <ielvisd@gmail.com>
2018-10-01 16:09:19 -0700

    Finished card container

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import './Card.css';
 
+const CardBanner = props => { // props = { type: "primary"}
+
+    return (
+      <div> 
+        <img class = 'banner-logo' src="https://ibin.co/3wnC6SgIOJud.png" alt="React Logo"/>
+      </div>
+    );
+}
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent'
+
+const CardContainer = props => { // props = { type: "primary"}
+
+    return (
+      <div class = 'card-container'> 
+        <CardBanner/>
+        <div>
+        <CardContent/>
+        </div>
+      </div>
+    );
+}
+
+export default CardContainer;
