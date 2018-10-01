3deca4548db29991980bcbc4c9485e1be46f66db
Lee Len Formento <leebatef@gmail.com>
2018-10-01 14:43:39 -0400

    Just putting headercontainer so i can test w/o getting errors

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import './App.css';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+// import CardContainer from './components/CardComponents/CardContainer';
+// import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
-    <div>
-    <div>
-    <Header  />
-      </div>
-    <div>
-      <Card  />
+    <div class = "container">
+    <HeaderContainer />
+      {/* <CardContainer  /> */}
     </div>
-    <div>
-      <Footer  />
-    </div>
-    </div>
+
   );
 };
 export default App;
