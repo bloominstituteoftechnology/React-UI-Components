929f76bc8b48702e8cedf66a28dc0bcbee10db52
Adam <ajmboosh@gmail.com>
2018-10-01 17:40:10 -0500

    imported font awesome in html, made footer section and marked up basic icons for stretch

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <i class="far fa-comment" />
+      <i class="fas fa-sync" />
+      <i class="far fa-heart" />
+      <i class="far fa-envelope" />
+    </footer>
+  );
+};
+
+export default Footer;
