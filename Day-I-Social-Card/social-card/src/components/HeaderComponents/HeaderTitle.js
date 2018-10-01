2a0a4de7b81208b0edf233f1dc65ae39e59bf856
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-01 15:31:50 -0500

    Header js components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-wrapper">
+            <div>
+                Lambda School
+                <span className="time-stamp">Oct 1st</span>
+                <HeaderContent />
+            </div>
+        </div>
+    );
+}
+
+export default HeaderTitle;
