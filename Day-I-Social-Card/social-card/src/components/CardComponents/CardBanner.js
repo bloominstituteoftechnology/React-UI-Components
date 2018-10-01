f9ff00f7dd29d7c8496ac3f76a2b9df584328870
SeanOne <basement365@gmail.com>
2018-10-01 15:38:34 -0700

    adding content to page | prior to working on styling

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+import reactBanner from '../../img/react-logo-og.png';
+
+const CardBanner = () => {
+    return (
+        <div class='bannerWrapper'>
+            <img class='banner' src={reactBanner} alt='react logo' />
+        </div>
+    )
+};
+
+export default CardBanner;
