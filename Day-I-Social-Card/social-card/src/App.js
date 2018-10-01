a0b2df5a24fa328afa4a37672f005d992f6fbe02
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-01 18:41:39 -0400

    [chore] set up LESS files

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import './App.css';
+import './css/index.css';
 
-const App = () => {
+
+
+const App = props => { 
+
   return (
-    <div>
-      <h3>Welcome to React Social Card!</h3>
-      <p>
-        Begin by exploring the `components` directory. You'll notice we have a
-        few files that we've already included in there to get you started right
-        away building components. You'll need to make sure you include your
-        components that you build in this file to watch your app come to life
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="main-container">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget mi dictum urna venenatis rhoncus et vitae diam. Curabitur ut ex ante. Aenean et hendrerit metus. Morbi ornare ac justo sed euismod. Nunc eget ante eu orci rhoncus facilisis. In pharetra, purus eu faucibus malesuada, nulla sem mattis ligula, non cursus magna lectus quis arcu. Sed at erat et tortor sagittis lacinia. In in pellentesque orci. Pellentesque commodo nisi augue, id imperdiet ex egestas lobortis. Donec ac accumsan eros, ut pharetra nulla. Cras sit amet scelerisque leo. Fusce vitae nunc id ex porta vulputate.
+
+Maecenas sit amet dapibus ex. Maecenas tincidunt a velit ac sollicitudin. Duis auctor felis in erat aliquam tempor. Maecenas tempor gravida dolor quis pellentesque. Fusce lobortis justo in neque dapibus mollis. Nam porta fermentum felis vitae dapibus. Quisque dapibus tempor auctor. Aliquam eu dictum diam, vitae cursus tortor.
     </div>
   );
 };
