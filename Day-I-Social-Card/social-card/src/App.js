45a8929242933a8ad32f6cd5dc44a74fd2ed6434
theryanclausen <theryanclausen@gmail.com>
2018-10-01 13:17:54 -0700

    labyrinth of header imports and exports

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
+
+
+
+
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Social Card!</h3>
+    <div className="container">
+      <HeaderContainer />
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
         few files that we've already included in there to get you started right
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+    return (
+      <div className="header-title">
+        <h2>Lambda School</h2>
+        <h3>@LambdaSchool</h3>
+        <ul>
+          <li>26 jan</li>
+        </ul>
+      </div>
+    );
+  };
+
+  export default HeaderTitle
