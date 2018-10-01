2d36fb4bdffcf0ff1b8eb78a05bdd932ec3b860c
txiong000 <tkx422@gmail.com>
2018-10-01 13:50:52 -0500

    started on header title

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle'
+
+const HeaderContainer = (props) => {
+    return (
+        <div className="header-container">
+            <HeaderTitle />
+        </div>
+    );
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+    return (
+        <header>
+            <h1>Lambda School</h1>
+            <a href="#">@LambdaSchool · 26 jan</a>
+            <p>Let's Learn React by building simple interfaces with components.
+             Don't try to overthink it, just keep it simple and fun.
+             Once you feel comfortable using components you are well on your way to mastering React</p>
+        </header>
+    );
+}
+
+export default HeaderTitle;
