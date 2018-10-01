47291330a2d150a1207340cc24e3d75cf838cd32
Ryan Boris <neodynia@icloud.com>
2018-10-01 19:27:47 -0400

    Added font awesome icons and components

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -4,10 +4,11 @@ import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
 import Footer from './components/FooterComponents/Footer';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+import { faComment, faHeart, faEnvelope, faRecycle } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faStroopwafel)
+library.add(faComment, faHeart, faEnvelope, faRecycle)
+
+
 
 
 const App = () => {
--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,13 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 
+
 const Footer = () => {
    return(
    <div className="footer">
-        <button className="button1"><FontAwesomeIcon icon="comment-o" /></button>
-        <button className="button2"><FontAwesomeIcon icon="refresh" /></button>
-        <button className="button3"><FontAwesomeIcon icon="heart-o" /></button>
-        <button className="button4"><FontAwesomeIcon icon="fa fa-envelope-o" /></button>
+        <FontAwesomeIcon icon="comment" />
+        <FontAwesomeIcon icon="recycle" />
+        <FontAwesomeIcon icon="heart" />
+        <FontAwesomeIcon icon="envelope" />
     </div>
    )
 }
