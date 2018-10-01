7d99d649df16a83a15258c474971881d1cc9d780
kyranmccann <kyran.mccann@gmail.com>
2018-10-01 13:01:10 -0700

    header and card (no date)

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div className='header-title'>
+      <p className='name'>
+        Lambda School </p>
+        <p className='handle'>@LambdaSchool</p>
+    </div>
+  );
+}
+
+export default HeaderTitle; 
