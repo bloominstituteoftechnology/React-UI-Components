84efda1721f72863fa1d00bb9135f3010928a2b3
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-10-01 16:38:55 -0600

    header

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+
+const CardContainer = props => {
+    return (
+        <div>
+            CARD CONTAINER
+        </div>
+    );
+}
+export default CardContainer;
