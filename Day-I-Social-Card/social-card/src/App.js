e596dddf481818c8c47e8f74042f831073b81d3f
Cwright0428 <cwright0428@yahoo.com>
2018-10-01 17:35:20 -0500

    added generic icons

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import CardContainer from './components/CardComponents/CardContainer';
 import Footer from './components/FooterComponents/Footer';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const App = () => {
   return (
--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "./Footer.css";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fab)
 
 const Footer = () => {
   return (
-<i class="far fa-comment"></i>
+    <div className= "icons">
+    <img src="https://cdn2.iconfinder.com/data/icons/asic/50/.svg-7-20.png" alt="comment"/>
+    <img src="https://cdn3.iconfinder.com/data/icons/sympletts-free-sampler/128/refresh-20.png" alt="refresh"/>
+    <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-heart-outline-20.png" alt="likes"/>
+    <img src="https://cdn3.iconfinder.com/data/icons/lexter-flat-outline-apps/45/envelope_outline-20.png" alt="mail"/>
+
+</div>
 
 )};
 
