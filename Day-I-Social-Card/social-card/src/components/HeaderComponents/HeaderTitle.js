128b0ee70ab72e6dc37c68c7ecffeb128e1011d2
Unknown <zachary.arney@gmail.com>
2018-10-01 18:01:25 -0500

    Set up header components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+  return (
+    <div className="title">
+      <h1>Lambda School</h1>
+      <a href="https://twitter.com/lambdaschool?lang=en">@LambdaSchool</a>
+      <div className="date"> · 26 jan</div>
+    </div>
+  );
+};
+
+export default HeaderTitle;
