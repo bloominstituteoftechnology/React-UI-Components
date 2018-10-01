9776b5e4ba4dfe8839917052aad9c53f067f8639
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-01 17:42:19 -0500

    Header title js time stamp updated

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './Header.css';
 import HeaderContent from './HeaderContent';
 
+const getCurrentDate = () => {
+    const date = new Date();
+    return date.toDateString();
+}
+
 const HeaderTitle = () => {
     return (
         <div className="header-wrapper">
             <div className="header-title">
                 Lambda School
-                <span className="time-stamp">@LambdaSchool 1 Oct</span>
+                <span className="time-stamp">@LambdaSchool &middot; {getCurrentDate()}</span>
                 <HeaderContent />
             </div>
         </div>
