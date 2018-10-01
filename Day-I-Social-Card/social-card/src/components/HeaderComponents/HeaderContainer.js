83f982c9d675dc47e5aff02a8f4ae602756de2b4
Brandon Vilaychith <blvilaychith@outlook.com>
2018-10-01 14:01:52 -0500

    added header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import LogoImage from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+const HeaderContainer = (props) => {
+    return (
+        <div>
+            <LogoImage />
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+    );
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const ImageLogo = (props) => {
+    return (
+        <img src={'https://ibin.co/3whrpKSBbZ81.png'} alt='logo'/>
+    );
+}
+
+export default ImageLogo;
