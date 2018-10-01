1196118ecbc6309bbf25477c0a44a836e156a82c
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-01 13:42:42 -0700

    Card component built

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardContent = () => {
+  return (
+    <div>
+      <h4>Get Started with React</h4>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application
+      </p>
+      <a href="https://reactjs.org" className="light-text">
+        reactjs.org
+      </a>
+    </div>
+  );
+};
+
+export default CardContent;
