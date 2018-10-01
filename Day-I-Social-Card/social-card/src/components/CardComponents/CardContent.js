c083c01560f22f0b7f4cdaa6df0a1f85170261d9
Adam <ajmboosh@gmail.com>
2018-10-01 15:13:07 -0500

    got initial state to display for header and card sections

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardContent = () => {
+  return (
+    <div class="card-content">
+      <h1>Get started with React</h1>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application.
+      </p>
+      <p class="source-text">reactjs.org</p>
+    </div>
+  );
+};
+
+export default CardContent;
