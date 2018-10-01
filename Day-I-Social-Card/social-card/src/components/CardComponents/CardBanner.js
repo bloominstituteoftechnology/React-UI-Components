d5805b8d517b71fc77cfd03909f3c2866c1e4a93
Stublink <chrisrey95@gmail.com>
2018-10-01 16:17:13 -0400

    Card Components Boilerplate

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = props => {
+    return (
+        <div>CardBanner</div>
+    );
+}
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+    return (
+        <div>CardContent</div>
+    );
+}
+
+export default CardContent;
