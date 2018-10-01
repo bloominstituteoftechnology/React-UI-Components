83311dc4fee61a45853db9adb0629379f064f6a0
Lee Len Formento <leebatef@gmail.com>
2018-10-01 14:55:53 -0400

    ImageThumbnail,js, added logo

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+const ImageThumbnail = () => {
+    return (
+        <div>
+            <img class='logo' src='https://ibin.co/3whrpKSBbZ81.png' alt='Lambda Logo'/>
+        </div>
+    )
+};
+
+export default ImageThumbnail;
