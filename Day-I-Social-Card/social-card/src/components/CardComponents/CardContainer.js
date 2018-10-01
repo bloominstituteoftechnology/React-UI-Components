b503fb7b476542f9823c4c52af4684d626c0aa53
Andre Myrick <amyrick.820@gmail.com>
2018-10-01 16:51:09 -0400

    completed MVP

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from "./CardBanner";
-// import CardContent from "./CardContent";
+import CardContent from "./CardContent";
 
 const CardContainer = props => {
     return (
-        <div className="card-container">
-            <CardBanner />
-            {/* <CardContent /> */}
-        </div>
+        <a href="https://www.reactjs.org">
+            <div className="card-container">
+                <CardBanner />
+                <CardContent />
+            </div>
+        </a>
     )
 }
 
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+    return (
+        <div className="card-content">
+            <h4>Get started with React</h4> 
+            <p className="card-para">React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <p className="card-url">reactjs.org</p>
+        </div>
+    )
+}
+export default CardContent;
