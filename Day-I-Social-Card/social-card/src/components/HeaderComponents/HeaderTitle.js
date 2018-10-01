f9ca465ab7550f50942955a00d788835109dfcdd
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-01 13:09:27 -0700

    Built out headercomponent

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import './HeaderContent';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return (
+        <div className="headerTitle">
+            <h1>Lambda School</h1>
+            <h2>@LambdaSchool</h2>
+            <h2>• 26 Jan</h2>
+            <HeaderContent />
+        </div>
+    );
+}
+
+export default HeaderTitle;
