1c076a25a762d904af8a72f14d83925bf639705a
Cwright0428 <cwright0428@yahoo.com>
2018-10-01 16:17:12 -0500

    card mostly built attempting to use font awesome for icons

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+<i class="far fa-comment"></i>
+
+)};
+
+export default Footer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
-import './Header.css';
+import HeaderContent from "./HeaderContent";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div className="header-title-wrapper">
+      <div className="header-title">
+        Lambda School
+        <span className="date"> july 31</span>
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderTitle;
