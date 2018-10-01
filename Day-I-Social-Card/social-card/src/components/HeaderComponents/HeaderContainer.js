7ad911cff43bef3baa84e097555e068acc07abf3
Axolotl <samuelmachat@gmail.com>
2018-10-01 15:41:34 -0400

    header components build no styling

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
+
+// import styling
 import './Header.css';
+
+// import components
+import ImageThumbnail from './ImageThumbnail'
+import HeaderTitle from './HeaderTitle'
+import HeaderContent from './HeaderContent'
+
+const HeaderContainer = () => (
+  <div className="header-container">
+    <ImageThumbnail />
+    <div>
+      <HeaderTitle />
+      <HeaderContent />
+    </div>
+  </div>
+)
+
+export default HeaderContainer
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => (
+  <div className="header-title">
+    <strong>Lambda School</strong>
+    @LambdaSchool
+    - Jan 26
+  </div>
+)
+
+export default HeaderTitle
