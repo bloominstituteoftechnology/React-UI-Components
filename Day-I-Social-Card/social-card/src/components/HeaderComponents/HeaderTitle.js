5f4789510efc5858a5c590417c9268b53a420a32
Zatara-Wrought <zatara.forever@gmail.com>
2018-10-01 16:40:48 -0700

    MVP commit

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,10 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => (
+  <div className="header-title">
+    <strong>Lambda School</strong>
+    <span>@LambdaSchool • Jan 26</span>
+  </div>
+);
+export default HeaderTitle;
