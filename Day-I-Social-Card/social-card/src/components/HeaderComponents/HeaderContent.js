7c5a3bace35a0ef8c09b5be1fddb55c87a2a1173
Lee Len Formento <leebatef@gmail.com>
2018-10-01 14:52:41 -0400

    Fixed a minor typo

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -7,7 +7,7 @@ const HeaderContent = () => {
             <p>
                 Let's learn React by building simple interfaces with components. Don't try to
                 overthink it, just keep it simple and have fun. Once you feel comfortable using
-                componentsyou are well on your way to mastering React!
+                components you are well on your way to mastering React!
             </p>
         </div>
     )
