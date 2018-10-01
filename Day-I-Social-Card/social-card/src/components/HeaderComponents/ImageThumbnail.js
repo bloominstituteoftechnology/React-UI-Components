544aad19a0ef4fe7e7c6646194a97214cdd7930d
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-01 16:37:45 -0400

    created an ImageThumbnail component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return (
+        <div>
+            <img className="thumbnail" src="https://ibin.co/3whrpKSBbZ81.png" />
+        </div>
+    );
+}
