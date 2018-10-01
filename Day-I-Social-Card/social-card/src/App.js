3030e167b08e70eed99c64aadf162d34f52c3bc5
Lee Len Formento <leebatef@gmail.com>
2018-10-01 15:44:53 -0400

    Added CardContainer initialization to App.js

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-// import CardContainer from './components/CardComponents/CardContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 // import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div class = "container">
     <HeaderContainer />
-      {/* <CardContainer  /> */}
+      <CardContainer  />
     </div>
 
   );
