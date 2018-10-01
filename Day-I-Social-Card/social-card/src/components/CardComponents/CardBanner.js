768f3d7b40a4c87efb0ac8bc37c3552c7044f55f
Andre Myrick <amyrick.820@gmail.com>
2018-10-01 16:30:54 -0400

    completed CardBanner

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = props => {
+    return (
+        <div className="card-banner" style={{backgroundImage: "url(https://ibin.co/3wnC6SgIOJud.png)"}}>
+        </div>
+    );
+}
+
+export default CardBanner;
