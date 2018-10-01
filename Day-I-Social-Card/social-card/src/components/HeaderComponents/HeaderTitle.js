b5a32bdf7a969702116c8e73914c30b9e697c42c
rytwalker <rytwalker@gmail.com>
2018-10-01 16:12:24 -0400

    Created header components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+  return (
+    <h1 className="HeaderTitle">
+      Lambda School{' '}
+      <span className="HeaderTitle__span">@LambdaSchool * 01 Oct</span>
+    </h1>
+  );
+};
+
+export default HeaderTitle;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = props => {
+  return <img src="https://ibin.co/3whrpKSBbZ81.png" alt="Lambda Logo" />;
+};
+
+export default ImageThumbnail;
