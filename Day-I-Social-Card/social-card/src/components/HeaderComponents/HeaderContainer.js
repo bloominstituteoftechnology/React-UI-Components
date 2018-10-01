558a152cc4e850ec357ec362f7305b898dc95994
SavannahGreuel <smallidgesavannah@gmail.com>
2018-10-01 14:25:10 -0600

    added the headercontainer components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+//import HeaderContent from "./HeaderContent";
+
+const HeaderContainer = () => {
+    return (
+        <div className = "header">
+        <ImageThumbnail />
+        <HeaderTitle />
+        </div>  
+    );
+};
+export default HeaderContainer;
