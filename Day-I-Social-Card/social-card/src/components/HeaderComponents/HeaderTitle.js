cfa0e197a68d0a1094cd7e18b037bba231fd4ea2
Blake <koenneckerb@gmail.com>
2018-10-01 13:13:32 -0700

    Social Card header complete.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div class="header-title">
+            <h1>Lambda School</h1>
+            <p class="at-date">@LambdaSchool</p>
+            <p class="at-date"> • 26 Jan</p>
+        </div>
+    );
+};
+
+export default HeaderTitle;
