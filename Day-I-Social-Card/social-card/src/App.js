f9a5c07e6f3e0b59889a6baf2d095e12f5d7ae48
Peter Pham <phamhere@gmail.com>
2018-10-01 13:23:35 -0600

    initial commit

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Header from './components/HeaderComponents/HeaderContainer';
+import Footer from './components/FooterComponents/Footer';
+import Card from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
