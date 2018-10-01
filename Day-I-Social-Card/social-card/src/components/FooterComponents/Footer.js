f4d7a12bf8109ae912961cd05505317a6065777e
Ryan Boris <neodynia@icloud.com>
2018-10-01 19:55:41 -0400

    Add click event to icons

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -6,12 +6,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const Footer = () => {
+    const eventOne = (e) => {
+        e.target.classList.toggle('red');
+    }
+
+ 
    return(
-   <div className="footer">
-        <FontAwesomeIcon icon="comment" />
-        <FontAwesomeIcon icon="adjust" />
-        <FontAwesomeIcon icon="heart" />
-        <FontAwesomeIcon icon="envelope" />
+    <div className="footer">
+        <FontAwesomeIcon icon="comment" onClick={eventOne}  />
+        <FontAwesomeIcon icon="circle-notch" onClick={eventOne}  />
+        <FontAwesomeIcon icon="heart" onClick={eventOne}  />
+        <FontAwesomeIcon icon="envelope" onClick={eventOne}  />
     </div>
    )
 }
