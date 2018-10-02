f289f47699f9c875fd363a905e0bf566fe7ee83a
Lee Len Formento <leebatef@gmail.com>
2018-10-02 18:16:29 -0400

    Refactored App.js. Also added setstate. needs work

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -5,15 +5,39 @@ import NumberButton from './components/ButtonComponents/NumberButton';
 import Display from './components/DisplayComponents/CalculatorDisplay';
 
 
-const App = () => {
-  return (
-    <div class="container">
-      <Display displayNumb = '0'/>
-      <div class="buttonContainer">
-        <NumberButton />
-        <ActionButton />
+// const App = () => {
+//   return (
+//     <div class="container">
+//       <Display displayNumb = '0'/>
+//       <div class="buttonContainer">
+//         <NumberButton />
+//         <ActionButton />
+//       </div>
+// </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: "2000",
+    }
+  }
+  handleChangeFunction = event => {
+    this.setState({total: event.target.value});
+  };
+
+  render () {
+    return (
+      <div className="container">
+      <Display displayNumb = {this.state.total} />
+      <div className="buttonContainer">
+      <NumberButton />
+      <ActionButton />
       </div>
-</div>
-  );
-};
+      </div>
+    );
+  }
+}
 export default App;
