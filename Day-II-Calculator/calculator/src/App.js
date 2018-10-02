78afcd5b2e3ef400789a9a989d12782bfb74bb41
Blake <koenneckerb@gmail.com>
2018-10-02 13:05:54 -0700

    NumberButton.js complete.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NumberButton from "./components/ButtonComponents/NumberButton";
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="numbers">
+    {numbers.map(number => <NumberButton displaySymbol={number} />)}
     </div>
+    // <div>
+    //   <h3>Welcome to React Calculator</h3>
+    //   <p>
+    //     We have given you a starter project. You'll want to build out your
+    //     components in their respective files, remove this code and replace it
+    //     with the proper components.
+    //   </p>
+    //   <p>
+    //     <strong>
+    //       Don't forget to `default export` your components and import them here
+    //       inside of this file in order to make them work.
+    //     </strong>
+    //   </p>
+    // </div>
   );
 };
-
+/* {students.map(studentFromMap => (
+        <Student studentOnProps={studentFromMap} />
+      ))}; */
 export default App;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+const NumberButton = props => (
+  <div className="number-button"> {props.displaySymbol} </div>
+);
+
+export default NumberButton
