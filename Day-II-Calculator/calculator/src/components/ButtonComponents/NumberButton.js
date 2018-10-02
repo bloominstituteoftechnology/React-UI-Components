628bc174ad38f1e9cfc8cc4283dac93fc4d8d000
CookieMonsta89 <jlstan06@gmail.com>
2018-10-02 16:18:29 -0400

    finished

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,47 @@
 import React from 'react';
 import './Button.css';
+
+
+const WhiteBtn = () => {
+    return ( 
+        
+        <div className='threefourth'>
+            <div className='clear'>
+                <h1>Clear</h1>
+            </div>
+            <div className='seven'>
+                <h1>7</h1>
+            </div>
+            <div className='eight'>
+                <h1>8</h1>
+            </div>
+            <div className='nine'>
+                <h1>9</h1>
+            </div>
+            <div className='four'>
+                <h1>4</h1>
+            </div>
+            <div className='five'>
+                <h1>5</h1>
+            </div>
+            <div className='six'>
+                <h1>6</h1>
+            </div>
+            <div className='one'>
+                <h1>1</h1>
+            </div>
+            <div className='two'>
+                <h1>2</h1>
+            </div>
+            <div className='three'>
+                <h1>3</h1>
+            </div>  
+            <div className='zero'>
+                <h1>0</h1>
+            </div>      
+        </div>
+        
+     );
+}
+ 
+export default WhiteBtn;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Display.css';
+
+const Display = () => {
+    return ( 
+        <div className='display'>
+            <h1>0</h1>
+        </div>
+     );
+}
+ 
+export default Display;
+
+<div className='display'>
+    <h1>0</h1>
+</div>
