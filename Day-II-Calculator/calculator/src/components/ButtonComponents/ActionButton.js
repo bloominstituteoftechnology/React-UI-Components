97388224b9f9f5126863827d274f23d4d86c9eb8
theryanclausen <theryanclausen@gmail.com>
2018-10-02 15:34:31 -0700

    some extra styling

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Button.css';
-
+let funcs = ['÷','x','-','+','=']
 
 const RedButtons = props =>{
-    let funcs = ['÷','x','-','+','=']
+    
     return(
         <div className='red-btns'>
         {funcs.map(func => <div className='btn func' key={func}>{func}</div> )}
