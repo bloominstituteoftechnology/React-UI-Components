7ea480cb4bfa36a7f1da68eed8c3703d975e9f2e
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-02 18:02:39 -0400

    [init] file setup

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+
+const numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "clear"];
+console.log(numArray);
+const calcOperators = ["÷", "x", "-", "+", "="];
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      {numArray.map(num =>{
+        return(
+          <NumberButton text={num}/>
+        );
+      })}
+
+      {calcOperators.map(operator =>{
+        return(
+          <NumberButton text={operator} />
+        );
+      })}
     </div>
   );
 };
