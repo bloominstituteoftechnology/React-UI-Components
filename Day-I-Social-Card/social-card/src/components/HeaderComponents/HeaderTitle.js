c50d0bd20989d6e32f7fea9a5a39cc48191f242c
Cwright0428 <cwright0428@yahoo.com>
2018-10-01 19:03:34 -0500

    added moment js to proj

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeaderContent from "./HeaderContent";
 import "./Header.css";
+import moment from 'moment';
 
 const HeaderTitle = () => {
   return (
@@ -8,7 +9,7 @@ const HeaderTitle = () => {
       <div className="header-title">
         Lambda School
         <span className="commentor"> @Lambda School</span>
-        <span className="date"> - july 31</span>
+        <span className="date"> -  {moment().format("dddd MMM Do YYYY")}</span>
         </div>
         <HeaderContent />
     </div>
