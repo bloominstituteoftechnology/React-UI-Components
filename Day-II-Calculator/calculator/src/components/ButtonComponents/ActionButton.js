eecda4b96c8c6b9e8cb1fe146d7157487b878382
Mindysueasaurus <mymindyyeager@gmail.com>
2018-10-02 16:53:05 -0500

    action button and display components done

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -4,7 +4,7 @@ import './Button.css';
 const ActionButton = props => {
   return (
     <div>
-    
+      <button className={`${props.color}`}>{props.text}</button>
     </div>
   )
 }
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -3,8 +3,8 @@ import './Display.css';
 
 const CalculatorDisplay = props => {
   return (
-    <div>
-    
+    <div className="display">
+      0
     </div>
   )
 }
