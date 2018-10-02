14cbe30e80593d401e2e06b092ee555741edfd94
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-02 18:17:20 -0400

    [chore] added number button component

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import NumberButton from './components/ButtonComponents/NumberButton';
 
-const numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "clear"];
-console.log(numArray);
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, ];
+const actionBtn = [0, "clear"];
 const calcOperators = ["÷", "x", "-", "+", "="];
 
 const App = () => {
   return (
     <div>
+      {/* number butttons */}
       {numArray.map(num =>{
         return(
-          <NumberButton text={num}/>
+          <NumberButton text={num} class={"numberBtn"}/>
         );
       })}
-
+      {/* operator buttons */}
       {calcOperators.map(operator =>{
         return(
-          <NumberButton text={operator} />
+          <NumberButton text={operator} class={"operatorBtn"} />
         );
       })}
     </div>
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -3,8 +3,14 @@ import React from 'react';
 
 
 const NumberButton = props => {
+
+    if(typeof props.text === 'number'){
+        return(
+            <button className={props.class}>{props.text.toString()}</button>
+         );
+    }
     return(
-       <button style={props.buttonStyle}>{props.text}</button>
+       <button className={props.class}>{props.text}</button>
     );
 }
 
