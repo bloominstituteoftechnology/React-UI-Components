eb92dd89a1b24b91a29a2f3e01245f3047a07a23
ssmodish <smodish@hotmail.com>
2018-10-02 17:14:22 -0400

    init commit - basic styling and skeleton components created

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+  return (
+    <div className='action-button'>
+      ACTION
+    </div>
+  );
+}
+
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = props => {
+  return (
+    <div className='display-button'>
+      DISPLAY
+    </div>
+  );
+}
+
+export default CalculatorDisplay;
