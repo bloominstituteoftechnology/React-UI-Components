0c8ad72542b3281737eebf84f7dad17496482a3f
Lee Len Formento <leebatef@gmail.com>
2018-10-02 15:57:21 -0400

    CalcDisplay

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Display.css';
+
+const Display = props => {
+    return (
+        <div className="displayNumb">{props.displayNumb}
+        </div>
+    )
+}
+
+export default Display;
