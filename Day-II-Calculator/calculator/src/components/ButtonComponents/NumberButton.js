d3ccf1331e238cd2fafb4b2f68589d075697f9c6
samsisle <samko9522@gmail.com>
2018-10-02 15:46:47 -0700

    Intial commit

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = () => (
+	<div className="number-buttons">
+		<div className="clear">clear</div>
+		<div className="divide">÷</div>
+		<div className="seven">7</div>
+		<div className="eight">8</div>
+		<div className="nine">9</div>
+		<div className="multiply">×</div>
+		<div className="four">4</div>
+		<div className="five">5</div>
+		<div className="six">6</div>
+		<div className="subtract">−</div>
+		<div className="one">1</div>
+		<div className="two">2</div>
+		<div className="three">3</div>
+		<div className="add">+</div>
+		<div className="zero">0</div>
+		<div className="equal">=</div>
+	</div>
+);
+
+export default NumberButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => (
+    <div className="calculator-display">
+			<div className="display-number">0</div>
+		</div>
+);
+
+export default CalculatorDisplay;
