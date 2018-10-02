1c32a3fc4c014ec4f442ed67d89ddf4c5e5288ff
unknown <brandongrych@gmail.com>
2018-10-02 17:30:41 -0500

    buttons created, calculator centered on screen and numbers positioned

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import './App.css';
+import Button from './components/ButtonComponents/NumberButton'
+
+const buttons = [
+  {
+  zero: '0',  
+  one: '1',
+  two: '2', 
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+  divide: '/',
+  multiply: 'x',
+  subtract: '-',
+  add: '+',
+  equal: '='
+  }
+]
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className = 'calculator'>
+      {buttons.map(button => {
+        return (
+          <Button button={button}/>
+        )
+      })}
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,32 @@
 import React from 'react';
 import './Button.css';
+function Button(props) {
+    const {button} = props;
+    return (
+        <div className='buttons'>
+        <div className="numbers147">
+        <button className = 'numberButton'>{button.one}</button>
+        <button className = 'numberButton'>{button.four}</button>
+        <button className = 'numberButton'>{button.seven}</button>
+        </div>
+        <div className="numbers258">
+        <button className = 'numberButton'>{button.two}</button>
+        <button className = 'numberButton'>{button.five}</button>
+        <button className = 'numberButton'>{button.eight}</button>
+        </div>
+        <div className="numbers369">
+        <button className = 'numberButton'>{button.three}</button>
+        <button className = 'numberButton'>{button.six}</button>
+        <button className = 'numberButton'>{button.nine}</button>
+        </div>
+        <div className="operators">
+        <button className = 'functionButton'>{button.divide}</button>
+        <button className = 'functionButton'>{button.multiply}</button>
+        <button className = 'functionButton'>{button.subtract}</button>
+        <button className = 'functionButton'>{button.add}</button>
+        <button className = 'functionButton'>{button.equal}</button>
+        </div>
+        </div>
+    )
+}
+ export default Button;
