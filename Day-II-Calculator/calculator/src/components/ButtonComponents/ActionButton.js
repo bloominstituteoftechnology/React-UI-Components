b7b3ec51d3a8fac3179304f06057edb8ba1692c5
Mindysueasaurus <mymindyyeager@gmail.com>
2018-10-02 16:18:42 -0500

    files connected

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+  return (
+    <div>
+    <h1>connected button</h1>
+    </div>
+  )
+}
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+import ActionButton from './ActionButton';
+
+const NumberButton = props => {
+  return (
+    <div>
+      <h1>Number Button</h1>
+      <ActionButton />
+    </div>
+  )
+}
+
+export default NumberButton;
