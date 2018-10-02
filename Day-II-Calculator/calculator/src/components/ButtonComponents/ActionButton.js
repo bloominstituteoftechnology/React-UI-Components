fdd1fd911a78cf2db6e2794ec35d9a65df810208
cembreyfarquhar <cembreyfarquhar@gmail.com>
2018-10-02 17:06:10 -0500

    Day 2 commit

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+
+const ActionButton = props => {
+    return (
+        <button onClick={() => props.actionClickHandler(props.text)} className={props.buttonStyle}>{props.text}</button>
+    ) 
+
+ 
+};
+
+
+
+
+export default ActionButton;
