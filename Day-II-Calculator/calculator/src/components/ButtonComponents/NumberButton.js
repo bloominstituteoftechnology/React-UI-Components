905299b0e3ac9e27b8a961046512576bbb5c1847
Drewsup123c <drewsup123c@gmail.com>
2018-10-02 16:09:24 -0600

    Added styling to display section

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Button.css';
 
+// I didn't get what the styling prop was going to be used for so I left it out
 const NumberBtns = props =>{
     return(
         <div className="number-container">
