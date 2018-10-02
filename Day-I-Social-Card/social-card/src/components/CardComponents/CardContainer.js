9ab0810bbb1cca9c90b25a50394d4bf9b3575d08
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-01 21:17:32 -0400

    [chore] card content box

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 
 const CardContainer = () => {
 
     return(
         <div className="card-container">
-          <CardBanner src={"https://ibin.co/3wnC6SgIOJud.png"} />
+            <div className="card-box">
+                <CardBanner src={"https://ibin.co/3wnC6SgIOJud.png"} />
+                <CardContent cardTitle={"Get started with React"} />
+            </div>
         </div>
     );
 }
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
-import './Card.css';
+
+
+
+const CardContent = props => {
+
+    return(
+        <div className="card-content-container">
+            <h2 className="card-title">{props.cardTitle}</h2>
+            <p className="card-txt">{props.cardContent}</p>
+            <a href="" className="card-link">{props.cardLink}</a>
+        </div>
+    );
+}
+
+export default CardContent;
