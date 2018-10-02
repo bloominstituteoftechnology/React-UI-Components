441e775810a83ca4da812a58901ee76556e5071b
Kat Fries <katjohnnson@Kats-MacBook-Air.local>
2018-10-01 23:23:48 -0400

    footer done

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = props => {
+  return (
+    <div class="footerEmojis"> 
+      <h3>&ldquo; &rdquo;    &#9775;    &hearts;    &#9993;</h3>
+    </div>
+  )
+}
+
+export default Footer;
+
+// ✉
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const Thumbnail = props => {
   return (
-      <img src="https://ibin.co/3whrpKSBbZ81.png" alt="Lambda Logo" />
+      <img src="https://yt3.ggpht.com/a-/AN66SAw9C1HPxBru_POv5bcGj8yLp8OSc18VosnELA=s288-mo-c-c0xffffffff-rj-k-no" alt="Lambda Logo" />
   );
 }
 
