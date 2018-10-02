e95feb6a69178684cea50da068947047a1b0f5bd
CookieMonsta89 <jlstan06@gmail.com>
2018-10-02 13:17:30 -0400

    finished

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
 
 const CardBanner = () => {
     return ( 
         <div className='banner'>
-            <img 
-                alt='banner'
-                className='image-banner'
-                src='https://ibin.co/3wnC6SgIOJud.png'
-            />
+            <div className='inner-banner'>
+                <img 
+                    alt='banner'
+                    className='image-banner'
+                    src='https://ibin.co/3wnC6SgIOJud.png'
+                />
+            </div>
+                <CardContent />
         </div>
      );
 }
