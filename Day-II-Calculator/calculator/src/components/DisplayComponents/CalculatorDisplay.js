9e824c374bc53b62d68f14fb2d9b1b86aa49d1b0
Brandon Vilaychith <blvilaychith@outlook.com>
2018-10-02 13:51:57 -0500

    Calculator display added

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 // import './Display.css';
+
+const Display = (props) => {
+    return (
+        <div className='calculatorDisplay'>
+            <p>0</p>
+        </div>
+    );
+} 
+
+export default Display;
