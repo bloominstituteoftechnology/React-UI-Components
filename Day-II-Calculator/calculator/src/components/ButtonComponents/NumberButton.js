369db667cb5253ab5c68bb55b5fe7933f69145ad
MEBonham <programmer@mckaybonham.com>
2018-10-02 16:29:04 -0600

    Got basic calculator layout, not pixel-perfect

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return (
+        <div className={props.buttonStyle}>
+            <p>{props.text}</p>
+        </div>
+    );
+}
+
+export default NumberButton;
