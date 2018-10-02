0eb8942f1dfb0ed414a32b93a5e5239d232ef7d8
Yanrong Xu <xuyanrong@icloud.com>
2018-10-02 16:34:07 -0400

    done with calculator MVP

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = (props) => {
+    return (
+        <button className = {props.displayStyle} > {props.text} </button>
+    )
+}
+
+export default CalculatorDisplay;
