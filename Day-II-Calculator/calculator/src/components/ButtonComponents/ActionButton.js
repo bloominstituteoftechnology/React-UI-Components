e8ffde7ea9e16f53c5753ca027a7af27897a9612
Yusuf Nafey <yusufnafey@yahoo.com>
2018-10-02 15:23:34 -0700

    finished project

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Button.css';
+
+const RedButton = props => <button className={props.buttonStyle}>{props.text}</button>
+
+const ActionButton = props => {
+    return (
+        <div className="right-side">
+            <RedButton buttonStyle="action" text="÷"/>
+            <RedButton buttonStyle="action" text="×"/>
+            <RedButton buttonStyle="action" text="-"/>
+            <RedButton buttonStyle="action" text="+"/>
+            <RedButton buttonStyle="action" text="="/>
+        </div>
+    )
+}
+
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,28 @@
 import React from 'react';
 import './Button.css';
+
+const BasicButton = props => <button className={props.buttonStyle}>{props.text}</button>
+
+const NumberButton = props => {
+    return (
+        <div>
+            <div className="left-side">
+                <BasicButton buttonStyle="clearButton" text="clear" />
+                <div className="small-numbers">
+                    <BasicButton buttonStyle="basicButton" text="1" />
+                    <BasicButton buttonStyle="basicButton" text="2" />
+                    <BasicButton buttonStyle="basicButton" text="3" />
+                    <BasicButton buttonStyle="basicButton" text="4" />
+                    <BasicButton buttonStyle="basicButton" text="5" />
+                    <BasicButton buttonStyle="basicButton" text="6" />
+                    <BasicButton buttonStyle="basicButton" text="7" />
+                    <BasicButton buttonStyle="basicButton" text="8" />
+                    <BasicButton buttonStyle="basicButton" text="9" />
+                </div>
+                <BasicButton buttonStyle="zeroButton" text="0" />
+            </div>
+        </div>
+    )
+}
+
+export default NumberButton;
