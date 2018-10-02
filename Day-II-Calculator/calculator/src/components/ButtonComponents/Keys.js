64ef84d99ac86594d345ab406a7f3041c9354b8a
kyranmccann <kyran.mccann@gmail.com>
2018-10-02 16:56:40 -0700

    updating keys component to map over button array.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/Keys.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/Keys.js
@@ -3,15 +3,98 @@ import './Button.css'
 import ActionButton from './ActionButton';
 import NumberButton from './NumberButton';
 
+const buttons = [
+  {
+    label: '÷',
+    buttonStyle: 'operation-button',
+    value: '/',
+
+  }, {
+    label: '7',
+    buttonStyle: 'number-button',
+    value: '7',
+
+  }, {
+    label: '8',
+    buttonStyle: 'number-button',
+    value: '8',
+
+  }, {
+    label: '9',
+    buttonStyle: 'number-button',
+    value: '9',
+
+  }, {
+    label: 'x',
+    buttonStyle: 'operation-button',
+    value: '*',
+
+  }, {
+    label: '4',
+    buttonStyle: 'number-button',
+    value: '4',
+
+  }, {
+    label: '5',
+    buttonStyle: 'number-button',
+    value: '5',
+
+  }, {
+    label: '6',
+    buttonStyle: 'number-button',
+    value: '6',
+
+  }, {
+    label: '-',
+    buttonStyle: 'operation-button',
+    value: '-',
+
+  }, {
+    label: '1',
+    buttonStyle: 'number-button',
+    value: '1',
+
+  }, {
+    label: '2',
+    buttonStyle: 'number-button',
+    value: '2',
+
+
+  }, {
+    label: '3',
+    buttonStyle: 'number-button',
+    value: '3',
+
+
+  }, {
+    label: '+',
+    buttonStyle: 'operation-button',
+    value: '+',
+
+
+  }, {
+    label: '0',
+    buttonStyle: 'number-button long',
+    value: '0',
+
+
+  }
+]
+
 const Keys = (props) => {
   const handleInput = (e) => {
     props.handleInput(e.target.value);
   };
 
-  return (
-    <div className='keys'>
-          <ActionButton buttonStyle='long' label='clear' onClick={props.handleClear}/>
-          <NumberButton buttonStyle='operation-button' label='&divide;' value='/' onClick={handleInput}/>
+  return (<div className='keys'>
+    <ActionButton buttonStyle='long' label='clear' onClick={props.handleClear}/>
+    {buttons.map(button => {
+      return(
+        <NumberButton button={button} onClick={handleInput}/>
+        )
+      })
+    }
+          {/* <NumberButton buttonStyle='operation-button' label='&divide;' value='/' onClick={handleInput}/>
           <NumberButton buttonStyle='number-button' label='7' value='7' onClick={handleInput}/>
           <NumberButton buttonStyle='number-button' label='8' value='8' onClick={handleInput}/>
           <NumberButton buttonStyle='number-button' label='9' value='9' onClick={handleInput}/>
@@ -24,10 +107,10 @@ const Keys = (props) => {
           <NumberButton buttonStyle='number-button' label='2' value='2' onClick={handleInput}/>
           <NumberButton buttonStyle='number-button' label='3' value='3' onClick={handleInput}/>
           <NumberButton buttonStyle='operation-button' label='+' value='+' onClick={handleInput}/>
-          <NumberButton buttonStyle='number-button long' label='0' value='0' onClick={handleInput}/>
-          <ActionButton buttonStyle='operation-button' label='=' onClick={props.handleCalculate}/>
-    </div>
-  )
+          <NumberButton buttonStyle='number-button long' label='0' value='0' onClick={handleInput}/> */
+    }
+    <ActionButton buttonStyle='operation-button' label='=' onClick={props.handleCalculate}/>
+  </div>)
 }
 
 export default Keys;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './Button.css';
 
 
+// const NumberButton = props => {
+//   return (
+//     <button className={`button ${props.buttonStyle}`} value={props.value} onClick={props.onClick}>{props.label}</button>
+//   )
+// }
+//
+// export default NumberButton;
+
 const NumberButton = props => {
   return (
-    <button className={`button ${props.buttonStyle}`} value={props.value} onClick={props.onClick}>{props.label}</button>
+    <button className={`button ${props.button.buttonStyle}`} value={props.button.value} onClick={props.onClick}>{props.button.label}</button>
   )
 }
 
