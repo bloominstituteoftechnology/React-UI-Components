a6cc017c51b4b8547accb3c83b7d46fab527d82b
Clem <stacy.clem@e-hps.com>
2018-10-02 14:52:06 -0700

    Made the calculator smaller

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberDisplay.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberDisplay.js
@@ -29,47 +29,38 @@ class NumberDisplay extends React.Component {
     }
 
     render() {
-        return (
+        return <div>
+            {/* Initilizing State to 0 on display */}
+            <Display input={this.state.input} />
+
             <div>
-                {/* Initilizing State to 0 on display */ }
-                <Display input={ this.state.input } />
-
-                <div>
-                    <ActionButton buttonStyle={ `action` } handleClick={ () => this.handleClear() } value={ "clear" } text={ "clear"
-                    } />
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ "math" } value={ "/" } text={ <i className="fas fa-divide" />
-                    } />
-                </div>
-
-                <div>
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ `number` } value={ "7" } text={ "7" } />
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ `number` } value={ "8" } text={ "8" } />
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ `number` } value={ "9" } text={ "9" } />
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ "math" } value={ "*" } text={ <i class="fas fa-times" />
-                    } />
-                </div>
-                <div>
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ `number` } value={ "4" } text={ "4" } />
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ `number` } value={ "5" } text={ "5" } />
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ `number` } value={ "6" } text={ "6" } />
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ "math" } value={ "-" } text={ <i class="fas fa-minus" />
-                    } />
-                </div>
-                <div>
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ `number` } value={ "1" } text={ "1" } />
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ `number` } value={ "2" } text={ "2" } />
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ `number` } value={ "3" } text={ "3" } />
-                    <NumberButton handleClick={ this.addToInput } buttonStyle={ "math" } value={ "+" } text={ <i class="fas fa-plus" />
-                    } />
-                </div>
-                <div>
-                    <ActionButton buttonStyle={ "action" } handleClick={ this.addToInput } value={ "0" } text={ "0" } />
-                    <NumberButton handleClick={ () => this.handleEqual() } buttonStyle={ "math" } value={ "=" } text={ <i className="fas fa-equals" />
-                    } />
-                </div>
-
+              <ActionButton buttonStyle={`action`} handleClick={() => this.handleClear()} value={"clear"} text={"clear"} />
+                <NumberButton handleClick={ this.addToInput } buttonStyle={ "math" } value={ "/" } text="&#247;"/>
             </div>
-        )
+
+            <div>
+              <NumberButton handleClick={this.addToInput} buttonStyle={`number`} value={"7"} text={"7"} />
+              <NumberButton handleClick={this.addToInput} buttonStyle={`number`} value={"8"} text={"8"} />
+              <NumberButton handleClick={this.addToInput} buttonStyle={`number`} value={"9"} text={"9"} />
+                <NumberButton handleClick={ this.addToInput } buttonStyle={ "math" } value={ "*" } text="&#215;" />
+            </div>
+            <div>
+              <NumberButton handleClick={this.addToInput} buttonStyle={`number`} value={"4"} text={"4"} />
+              <NumberButton handleClick={this.addToInput} buttonStyle={`number`} value={"5"} text={"5"} />
+              <NumberButton handleClick={this.addToInput} buttonStyle={`number`} value={"6"} text={"6"} />
+                <NumberButton handleClick={ this.addToInput } buttonStyle={ "math" } value={ "-" } text="&#8722;"/>
+            </div>
+            <div>
+              <NumberButton handleClick={this.addToInput} buttonStyle={`number`} value={"1"} text={"1"} />
+              <NumberButton handleClick={this.addToInput} buttonStyle={`number`} value={"2"} text={"2"} />
+              <NumberButton handleClick={this.addToInput} buttonStyle={`number`} value={"3"} text={"3"} />
+                <NumberButton handleClick={ this.addToInput } buttonStyle={ "math" } value={ "+" } text="&#43;" />
+            </div>
+            <div>
+              <ActionButton buttonStyle={"action"} handleClick={this.addToInput} value={"0"} text={"0"} />
+              <NumberButton handleClick={() => this.handleEqual()} buttonStyle={"math"} value={"="} text="&#61;" />
+            </div>
+          </div>;
     }
 }
 
