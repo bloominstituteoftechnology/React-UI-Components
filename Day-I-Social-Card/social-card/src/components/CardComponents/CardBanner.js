6848fa731b6f27e912282f9ec9dce832afb800b9
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-01 21:04:49 -0400

    [chore] Card Banner LESS/Styling

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CardBanner = props => {
     return(
-        <img className="content-banner" src={props.src} alt="React Logo"/>
+        <img className="card-banner" src={props.src} alt="React Logo"/>
     );
 }
 
