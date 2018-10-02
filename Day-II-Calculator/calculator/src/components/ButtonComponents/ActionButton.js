1a9a7c467815cc146bd2e269857fbc2c14bd99b1
Brian G <brian.gregorius@gmail.com>
2018-10-02 18:36:02 -0400

    completed mvp

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+
+const ActionButton = (props) => {
+    return (<div className = 'ActButtons'>
+        <h1> {props.text}</h1>
+        </div>)
+}
+
+export default ActionButton;
