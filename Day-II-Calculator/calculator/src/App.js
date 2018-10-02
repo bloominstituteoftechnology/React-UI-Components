73dfa91f0dfa91ecbe7843f3ecca630fd8bc6a03
Andre Myrick <amyrick.820@gmail.com>
2018-10-02 16:29:55 -0400

    refactored setup

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
 import Display from "./components/DisplayComponents/CalculatorDisplay";
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import NumberButton from "./components/ButtonComponents/NumberButton";
 
 const App = () => {
   return (
-    <div>
-      <Display />
-    </div>
+    <div className="container">
+            <Display />
+            <ActionButton buttonStyle={"action"} text={"clear"} />
+            <NumberButton buttonStyle={"operator"} text={"÷"} />
+            <NumberButton buttonStyle={"number"} text={"7"} />
+            <NumberButton buttonStyle={"number"} text={"8"} />
+            <NumberButton buttonStyle={"number"} text={"9"} />
+            <NumberButton buttonStyle={"operator"} text={"×"} />
+            <NumberButton buttonStyle={"number"} text={"4"} />
+            <NumberButton buttonStyle={"number"} text={"5"} />
+            <NumberButton buttonStyle={"number"} text={"6"} />
+            <NumberButton buttonStyle={"operator"} text={"-"} />
+            <NumberButton buttonStyle={"number"} text={"1"} />
+            <NumberButton buttonStyle={"number"} text={"2"} />
+            <NumberButton buttonStyle={"number"} text={"3"} />
+            <NumberButton buttonStyle={"operator"} text={"+"} />
+            <ActionButton buttonStyle={"action"} text={"0"} />
+            <NumberButton buttonStyle={"operator"} text={"="} />
+        </div>
   );
 };
 
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -5,25 +5,7 @@ import NumberButton from "../ButtonComponents/NumberButton";
 
 const Display = props => {
     return (
-        <div className="container">
-            <div className="display">0</div>
-            <ActionButton buttonStyle={"action"} text={"clear"} />
-            <NumberButton buttonStyle={"operator"} text={"÷"} />
-            <NumberButton buttonStyle={"number"} text={"7"} />
-            <NumberButton buttonStyle={"number"} text={"8"} />
-            <NumberButton buttonStyle={"number"} text={"9"} />
-            <NumberButton buttonStyle={"operator"} text={"×"} />
-            <NumberButton buttonStyle={"number"} text={"4"} />
-            <NumberButton buttonStyle={"number"} text={"5"} />
-            <NumberButton buttonStyle={"number"} text={"6"} />
-            <NumberButton buttonStyle={"operator"} text={"-"} />
-            <NumberButton buttonStyle={"number"} text={"1"} />
-            <NumberButton buttonStyle={"number"} text={"2"} />
-            <NumberButton buttonStyle={"number"} text={"3"} />
-            <NumberButton buttonStyle={"operator"} text={"+"} />
-            <ActionButton buttonStyle={"action"} text={"0"} />
-            <NumberButton buttonStyle={"operator"} text={"="} />
-        </div>
+        <div className="display">0</div>
     );
 }
 
