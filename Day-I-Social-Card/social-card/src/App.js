79cd197eb20e4ff563b058654f67ffd740f0e8d4
frankbenevento <fb.mentoslabs@gmail.com>
2018-10-01 17:56:08 -0700

    added pp footer.

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import FooterContainer from './components/FooterComponents/FooterContainer';
 
 const App = () => {
   return (
     <div>
       <HeaderContainer />
-      <CardContainer  />
+      <CardContainer />
+      <FooterContainer />
     </div>
   );
 };
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Card.css';
 
-
 const CardContent = props =>{
     return(
         <div>
