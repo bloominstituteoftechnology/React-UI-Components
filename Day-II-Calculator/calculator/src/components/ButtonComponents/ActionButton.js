59a078bda5698160aab783cd2c11dd8190edf160
unknown <quan.nguyen199712@gmail.com>
2018-10-02 18:38:45 -0400

    finished with MVP. attempting stretch

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+function ActionButton(prop){
+    return (
+        <div className="action-button">
+            <h1>{prop.buttonOnProps.value}</h1>
+        </div>
+    )
+}
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+function DisplayScreen(){
+    return (
+        <div className="display-screen">
+            <h1>0</h1>
+        </div>
+    )
+}
+export default DisplayScreen;
