fde6dc823c41fa5839e8690665037c04c262d57b
SabrinaGear <sabrinagear.ft@gmail.com>
2018-10-02 18:01:43 -0500

    first components

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return (
+        <div className={props.style}>
+            <h1>{props.text}</h1>
+        </div>
+    )
+}
+
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return (
+        <div className={props.style}>
+            <h1>{props.symbol}</h1>
+        </div>
+    )
+}
+
+export default NumberButton;
