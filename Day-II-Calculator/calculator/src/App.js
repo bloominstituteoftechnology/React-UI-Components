0b6fc6457e9aa35ed557119b279fbf40a69a2c6a
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-02 15:44:37 -0500

    App js updated

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -12,29 +12,29 @@ class App extends React.Component {
         <CalculatorDisplay />
         <ButtonContainer className="clear-button">
           <ActionButton className="three-text" />
-          <NumberButton />
+          <NumberButton bcolor="red-button" text="/"/>
         </ButtonContainer>
         <ButtonContainer>
-          <NumberButton />
-          <NumberButton />
-          <NumberButton />
-          <NumberButton />
+          <NumberButton bcolor="numpad-button" text="7"/>
+          <NumberButton bcolor="numpad-button" text="8"/>
+          <NumberButton bcolor="numpad-button" text="9"/>
+          <NumberButton bcolor="red-button" text="x"/>
         </ButtonContainer>
         <ButtonContainer>
-          <NumberButton />
-          <NumberButton />
-          <NumberButton />
-          <NumberButton />
+          <NumberButton bcolor="numpad-button" text="4"/>
+          <NumberButton bcolor="numpad-button" text="5"/>
+          <NumberButton bcolor="numpad-button" text="6"/>
+          <NumberButton bcolor="red-button" text="-"/>
         </ButtonContainer>
         <ButtonContainer>
-          <NumberButton />
-          <NumberButton />
-          <NumberButton />
-          <NumberButton />
+          <NumberButton bcolor="numpad-button" text="1"/>
+          <NumberButton bcolor="numpad-button" text="2"/>
+          <NumberButton bcolor="numpad-button" text="3"/>
+          <NumberButton bcolor="red-button" text="+"/>
         </ButtonContainer>
         <ButtonContainer>
           <ActionButton className="three-text" />
-          <NumberButton />
+          <NumberButton bcolor="red-button" text="="/>
         </ButtonContainer>
       </div>
     );
