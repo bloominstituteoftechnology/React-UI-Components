d3062cfc6ad967ae716c2152ec2a074bb71896b8
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-10-02 17:02:30 -0600

    calc

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return (
+        <div className="operators">
+            <button className="divide"></button>
+            <button className="divide"></button>
+            <button className="divide"></button>
+            <button className="divide"></button>
+            <button className="divide"></button>   
+        </div>
+    );
+};
+export default ActionButton;
