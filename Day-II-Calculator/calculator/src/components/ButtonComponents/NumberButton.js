0676d390955cf71dac2d0bfef15f342b05bb98a6
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-10-02 18:15:47 -0400

    basic prop

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+function NumberButton(props) { 
+    return(
+        <div className="numberButton">{props.display}</div>
+
+)
+
+}
+
+export default NumberButton
