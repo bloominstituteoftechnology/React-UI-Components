f5453326d526bb3726578ee849623ac4c9ddf0c8
Peter Pham <phamhere@gmail.com>
2018-10-02 16:49:49 -0600

    added functionality to operators

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
     super();
     this.state = {
       total: 0,
-      equation: 0
+      oldNum: 0,
+      operator: ""
     }
   }
   clearTotal = () => {
@@ -45,35 +46,61 @@ class App extends React.Component {
   zeroTotal = () => {
     this.setState({ total: this.state.total * 10 + 0});
   }
+  plus = () => {
+    this.setState({ oldNum: this.state.oldNum + this.state.total, total: 0, operator: '+' })
+  }
+  minus = () => {
+    this.setState({ oldNum: this.state.oldNum === 0 ? this.state.total : this.state.oldNum - this.state.total, total: 0, operator: '-' })
+  }
+  multiply = () => {
+    this.setState({ oldNum: this.state.oldNum === 0 ? this.state.total : this.state.oldNum * this.state.total, total: 0, operator: '*' })
+  }
+  divide = () => {
+    this.setState({ oldNum: this.state.oldNum === 0 ? this.state.total : this.state.oldNum / this.state.total, total: 0, operator: '/' })
+  }
+  equal = () => {
+    if (this.state.operator === "+") {
+      this.setState({ total: this.state.oldNum + this.state.total, oldNum: 0 })
+    }
+    if (this.state.operator === "-") {
+      this.setState({ total: this.state.oldNum - this.state.total, oldNum: 0 })
+    }
+    if (this.state.operator === "*") {
+      this.setState({ total: this.state.oldNum * this.state.total, oldNum: 0 })
+    }
+    if (this.state.operator === "/") {
+      this.setState({ total: (this.state.oldNum / this.state.total).toFixed(1), oldNum: 0 })
+    }
+  }
   render() {
     return (
       <div className="container">
         <CalculatorDisplay total={this.state.total}/>
         <div className="row">
           <ActionButton text="clear" func={this.clearTotal} />
-          <NumberButton text="÷" style="sign" />
+          <NumberButton text="÷" style="sign" func={this.divide} />
         </div>
         <div className="row">
           <NumberButton text="7" style="num" func={this.sevenTotal} />
           <NumberButton text="8" style="num" func={this.eightTotal} />
           <NumberButton text="9" style="num" func={this.nineTotal} />
-          <NumberButton text="×" style="sign" />
+          <NumberButton text="×" style="sign" func={this.multiply} />
         </div>
         <div className="row">
           <NumberButton text="4" style="num" func={this.fourTotal} />
           <NumberButton text="5" style="num" func={this.fiveTotal} />
           <NumberButton text="6" style="num" func={this.sixTotal} />
-          <NumberButton text="-" style="sign" />
+          <NumberButton text="-" style="sign" func={this.minus} />
         </div>
         <div className="row">
           <NumberButton text="1" style="num" func={this.oneTotal} />
           <NumberButton text="2" style="num" func={this.twoTotal} />
           <NumberButton text="3" style="num" func={this.threeTotal} />
-          <NumberButton text="+" style="sign" />
+          <NumberButton text="+" style="sign" func={this.plus} />
         </div>
         <div className="row">
           <ActionButton text="0" func={this.zeroTotal} />
-          <NumberButton text="=" style="sign" />
+          <NumberButton text="=" style="sign" func={this.equal} />
         </div>
       </div>
     );
