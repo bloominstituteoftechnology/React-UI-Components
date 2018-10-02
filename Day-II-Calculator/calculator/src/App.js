10cd64179250ee73921a25eb85b73c9a2f35581a
Justin <jdlow2@gmail.com>
2018-10-02 18:00:04 -0400

    fixed math symbols. mvp complete. branching for stretch.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -4,25 +4,27 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 import NumberButton from "./components/ButtonComponents/NumberButton";
 import ActionButton from "./components/ButtonComponents/ActionButton";
 
-const values = ['/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+']
+const values = ['÷', 7, 8, 9, '×', 4, 5, 6, '−', 1, 2, 3, '+']
 const App = props => {
+  let total = 0;
+
   return (
   <div className='calculator'>
-    <CalculatorDisplay displayText='0' />
+    <CalculatorDisplay displayText={total} />
     <ActionButton buttonText="clear"/>
 
     {values.map(num => {
       let style;
-        if ('=+-x/'.includes(num)) {
+        if ('+×÷−'.includes(num)) {
           style = 'red';
         }
       return (
-        <NumberButton buttonText={num} buttonStyles={style} />
+        <NumberButton key={num} buttonText={num} buttonStyles={style} />
       )
     })}
 
     <ActionButton buttonText="0"/>
-    <NumberButton buttonText={'='} />
+    <NumberButton buttonText={'='} buttonStyles='red' />
 
   </div>
   )
