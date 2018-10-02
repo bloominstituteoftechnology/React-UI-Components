1164901bd195c93d3256447f140f12cb2488bf89
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-01 20:20:10 -0400

    [chore] header styling completed

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -4,7 +4,9 @@ import React from 'react';
 const HeaderContent = () => {
         return(
             <div className="header-content">
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum lacus vel magna egestas, eu lacinia ligula pellentesque. Sed lorem purus, rutrum vel ligula at, congue sollicitudin lacus. Praesent nisi metus, posuere at varius a, vestibulum in justo. Praesent mattis orci lectus, id dapibus diam pellentesque vitae. 
+               <p className="header-content-txt">
+                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum lacus vel magna egestas, eu lacinia ligula pellentesque. Sed lorem purus, rutrum vel ligula at, congue sollicitudin lacus. Praesent nisi metus, posuere at varius a, vestibulum in justo. Praesent mattis orci lectus, id dapibus diam pellentesque vitae. 
+                </p>
             </div>
     );
 }
