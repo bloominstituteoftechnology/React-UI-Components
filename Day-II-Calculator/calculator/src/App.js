29343afc8142b179a4812f8228955fbd2eb68bad
Daniel Ferrer <danfrrr13@gmail.com>
2018-10-02 16:32:12 -0400

    finished the display component. MVP done.

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -2,26 +2,28 @@ import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
     <div className="app">
-      <NumberButton buttonStyle="wide" text="clear" />
-      <ActionButton buttonStyle="red" text="&#247;" />
+      <CalculatorDisplay text="0" />
+      <ActionButton buttonStyle="wide" text="clear" />
+      <NumberButton buttonStyle="red" text="&#247;" />
       <NumberButton buttonStyle="white" text="7" />
       <NumberButton buttonStyle="white" text="8" />
       <NumberButton buttonStyle="white" text="9" />
-      <ActionButton buttonStyle="red" text="&#215;" />
+      <NumberButton buttonStyle="red" text="&#215;" />
       <NumberButton buttonStyle="white" text="4" />
       <NumberButton buttonStyle="white" text="5" />
       <NumberButton buttonStyle="white" text="6" />
-      <ActionButton buttonStyle="red" text="&#8722;" />
+      <NumberButton buttonStyle="red" text="&#8722;" />
       <NumberButton buttonStyle="white" text="1" />
       <NumberButton buttonStyle="white" text="2" />
       <NumberButton buttonStyle="white" text="3" />
-      <ActionButton buttonStyle="red" text="+" />
-      <NumberButton buttonStyle="wide bold" text="0" />
-      <ActionButton buttonStyle="red" text="=" />
+      <NumberButton buttonStyle="red" text="+" />
+      <ActionButton buttonStyle="wide bold" text="0" />
+      <NumberButton buttonStyle="red" text="=" />
     </div>
   );
 };
