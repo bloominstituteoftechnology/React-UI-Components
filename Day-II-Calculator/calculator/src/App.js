d9bdddf1b6fa0915c53c71fa3be2f4097aec7dc5
Angelo Deleon <rudy209@comcast.net>
2018-10-02 15:06:27 -0700

    Finished MVP

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -9,11 +9,45 @@ const App = () => {
   return (
     <div className='calc-container'>
         <CalculatorDisplay />
+        {/* TODO: add 6 rows including number container  */}
         <div className='number-action-container'>
+          {/* row #1  clear/divide */}
           <div className='row'>
               <ActionButton buttonStyle='button clear-btn' text='clear' />
               <NumberButton buttonStyle='button maroon' text='&divide;' />
           </div>
+
+          {/* row #2 7/8/9/x */}
+          <div className='row'>
+            <NumberButton buttonStyle='button' text='7' />
+            <NumberButton buttonStyle='button' text='8' />
+            <NumberButton buttonStyle='button' text='9' />
+            <ActionButton buttonStyle='button maroon' text='&times;' />
+            {/* <ActionButton buttonStyle='button maroon' text='x' /> */}
+          </div>
+
+          {/* row #3 4/5/6/- */}
+          <div className='row'>
+          <NumberButton buttonStyle='button' text='4' />
+            <NumberButton buttonStyle='button' text='5' />
+            <NumberButton buttonStyle='button' text='6' />
+            <ActionButton buttonStyle='button maroon' text='&minus;' />
+          </div>
+
+          {/* row #4 1/2/3/+ */}
+          <div className='row'>
+          <NumberButton buttonStyle='button' text='1' />
+            <NumberButton buttonStyle='button' text='2' />
+            <NumberButton buttonStyle='button' text='3' />
+            <ActionButton buttonStyle='button maroon' text='+' />
+          </div>
+
+          {/* row #5 0/= */}
+          <div className='row'>
+          <NumberButton buttonStyle='button clear-btn' text='0' />
+            <ActionButton buttonStyle='button maroon' text='=' />
+          </div>
+
         </div>
     </div>
   );
