623ed033013588ed967ae2a74f77b6c7cc7a81e6
Brad Mortensen <b.mort86@gmail.com>
2018-10-02 13:26:31 -0600

    init commit

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Button.css';
+
+
+function ActionButton () {
+    return (
+        <div className='actions'>
+            <div className='operators'>÷</div>
+            <div className='operators'>×</div>
+            <div className='operators'>–</div>
+            <div className='operators'>+</div>
+            <div className='operators'>=</div>
+        </div>
+    );
+}
+
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Button.css';
+
+
+function NumberButton () {
+    return (
+        <div className='number-pad'>
+            <div className='number-button'>1</div>
+            <div className='number-button'>2</div>
+            <div className='number-button'>3</div>
+            <div className='number-button'>4</div>
+            <div className='number-button'>5</div>
+            <div className='number-button'>6</div>
+            <div className='number-button'>7</div>
+            <div className='number-button'>8</div>
+            <div className='number-button'>9</div>
+            <div className='number-button' id='zero'>0</div>
+        </div>
+    );
+};
+
+export default NumberButton;
