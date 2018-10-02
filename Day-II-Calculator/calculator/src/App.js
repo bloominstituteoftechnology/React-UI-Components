663c269381b6b85fa9f0a9b3feafdc41a3caa01d
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-02 14:42:16 -0700

    adjusted button widths, corrected number order

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -15,17 +15,17 @@ const App = () => {
       <CalcDisplay />
       <ActionButton buttonStyle="action-btn" text="clear" />
       <NumberButton buttonStyle="operator" text="&divide;" />
-      <NumberButton buttonStyle="num-btn" text="1" />
-      <NumberButton buttonStyle="num-btn" text="2" />
-      <NumberButton buttonStyle="num-btn" text="3" />
+      <NumberButton buttonStyle="num-btn" text="7" />
+      <NumberButton buttonStyle="num-btn" text="8" />
+      <NumberButton buttonStyle="num-btn" text="9" />
       <NumberButton buttonStyle="operator" text="&times;" />
       <NumberButton buttonStyle="num-btn" text="4" />
       <NumberButton buttonStyle="num-btn" text="5" />
       <NumberButton buttonStyle="num-btn" text="6" />
       <NumberButton buttonStyle="operator" text="&minus;" />
-      <NumberButton buttonStyle="num-btn" text="7" />
-      <NumberButton buttonStyle="num-btn" text="8" />
-      <NumberButton buttonStyle="num-btn" text="9" />
+      <NumberButton buttonStyle="num-btn" text="1" />
+      <NumberButton buttonStyle="num-btn" text="2" />
+      <NumberButton buttonStyle="num-btn" text="3" />
       <NumberButton buttonStyle="operator" text="+" />
       <ActionButton buttonStyle="action-btn" text="0" />
       <NumberButton buttonStyle="operator" text="=" />
