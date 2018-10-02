1766fb982092559959460cf1958237bc1c527d95
Jordan Stoddard <jjsincorporated@gmail.com>
2018-10-02 14:44:18 -0700

    [Complete] MVP

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+import NumberButton from './NumberButton'
+
+const ActionButton = props => {
+   
+    return (
+        <div className="actionButtonDiv">
+        <button className={`actionButton ${props.style}`}>{props.text}</button>
+        </div>
+    );
+}
+
+export default ActionButton;
