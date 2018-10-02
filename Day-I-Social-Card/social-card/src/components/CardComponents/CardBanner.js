474bf2ff968f1d80b3deb6f3bd9d89a931a9c1ff
Unknown <zachary.arney@gmail.com>
2018-10-01 22:20:44 -0500

    Made card clickable

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './Card.css';
 
 const CardBanner = props => {
-  return <img src="https://ibin.co/3wnC6SgIOJud.png" alt="React logo" />;
+  const imgStyle = {
+    border: '0',
+  }
+  return (
+  <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    <img src="https://ibin.co/3wnC6SgIOJud.png" alt="React logo" style={imgStyle} />
+  </a>
+  );
 };
 
 export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,12 +4,16 @@ import './Card.css';
 const CardContent = props => {
   return (
     <div className="card-content">
-      <h3>Get started with React</h3>
-      <p>
-        React makes it painless to create interactive UIs. Design simple views
-        for each state in your application.
-      </p>
-      <a href="https://www.reactjs.org">reactjs.org</a>
+      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <h3 className="content title">Get started with React</h3>
+      </a>
+      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <p className="content text">
+          React makes it painless to create interactive UIs. Design simple views
+          for each state in your application.
+        </p>
+      </a>
+      <a href="https://www.reactjs.org" target="_blank" rel="noopener noreferrer">reactjs.org</a>
     </div>
   );
 };
