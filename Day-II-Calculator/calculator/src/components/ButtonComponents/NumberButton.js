3123dfb222b016ce609c52eb7916d8d7582d3e6d
Unknown <zachary.arney@gmail.com>
2018-10-02 17:49:24 -0500

    Created number button component

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton(props) {
+  return <div className="button number">{props.button.text}</div>;
+}
+
+export default NumberButton;
