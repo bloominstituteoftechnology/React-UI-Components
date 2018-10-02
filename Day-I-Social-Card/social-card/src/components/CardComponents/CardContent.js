c3abf16649f958a8f13ef8684322dcb232417476
Edward J <edjeong99@gmail.com>
2018-10-01 23:23:32 -0400

    day2 - init

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 const CardContent = () => {
-    return (
-       <div className="cardContent">
-            <h4>Get started with React</h4>
-            <p>React makes it painless to create interactive UIs.  
-                Design simple views for each state in your application. </p>
-            <p className="source">reactjs.org</p>
-        </div>
-    );
-}
+  return (
+    <div className="cardContent">
+      <h4>Get started with React</h4>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application.{" "}
+      </p>
+      <p className="source">reactjs.org</p>
+    </div>
+  );
+};
 
-export default CardContent 
+export default CardContent;
