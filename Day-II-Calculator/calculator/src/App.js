1e3c349d316f1b145a9bf082a4d13dc4309b85be
Stublink <chrisrey95@gmail.com>
2018-10-02 15:45:23 -0400

    Calculator Skeleton

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className='calculator-container'>
+      <CalculatorDisplay />
+      <div className='calc-row'>
+        <NumberButton />
+        <ActionButton />
+      </div>
+      <div className='calc-row'>
+        <NumberButton />
+        <NumberButton />
+        <NumberButton />
+        <ActionButton />
+      </div>
+      <div className='calc-row'>
+        <NumberButton />
+        <NumberButton />
+        <NumberButton />
+        <ActionButton />
+      </div>
+      <div className='calc-row'>
+        <NumberButton />
+        <NumberButton />
+        <NumberButton />
+        <ActionButton />
+      </div>
+      <div className='calc-row'>
+        <NumberButton />
+        <ActionButton />
+      </div>
+
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = ({value=0}) => {
+    return (
+        <div>
+            {value}
+        </div>
+    );
+}
+
+export default NumberButton;
