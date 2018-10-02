71b0a2cf416350708c971f8079e6a184e5f4c63c
Unknown <zachary.arney@gmail.com>
2018-10-01 22:21:22 -0500

    Removed unnecessary imports

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './Header.css';
-import ImageThumbnail from './ImageThumbnail';
-import HeaderTitle from './HeaderTitle';
 
 const HeaderContent = props => {
   return (
