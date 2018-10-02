5949a4aff43ab44000c518e36c52c359a1ecf46d
David247365 <ds051128@gmail.com>
2018-10-02 02:06:16 -0500

    completed mvp

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 
 const ImageThumbnail = props => {
-    return <img src="https://ibin.co/3whrpKSBbZ81.png" alt="lambda Thumbnail" />;
+    return <img className="logo" src="https://ibin.co/3whrpKSBbZ81.png" alt="lambda Thumbnail" />;
   };
 
   export default ImageThumbnail;
