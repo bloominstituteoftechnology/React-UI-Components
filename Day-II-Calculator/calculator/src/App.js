74969d47e5ef78415c96e7be8d2367cf6c461781
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-10-02 18:48:41 -0400

    number styling

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import NumberButton from "./components/ButtonComponents/NumberButton.js"
 
-const number = 1
+const number = [7, 8, 9, 4, 5, 6, 1, 2, 3]
 
 const App = () => {
   return (
     <div className = "numberContainer">
-      <NumberButton display = {number} />
+      {number.map(number => {
+        return(
+          <NumberButton display = {number} />
+        )
+        })}
 
     </div>
   );
