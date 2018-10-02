2f47b80af626ebae53cc138ea01f56ae301505dc
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-02 12:51:00 -0700

    Calc Display component built and styled

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CalcDisplay from "./components/DisplayComponents/CalculatorDisplay.js";
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
+    <div className="calcContainer">
+      <CalcDisplay />
+      {/* <h3>Welcome to React Calculator</h3>
       <p>
         We have given you a starter project. You'll want to build out your
         components in their respective files, remove this code and replace it
@@ -15,7 +17,7 @@ const App = () => {
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
-      </p>
+      </p> */}
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalcDisplay = () => {
+  return (
+    <div className="calc-display">
+      <span>0</span>
+    </div>
+  );
+};
+
+export default CalcDisplay;
