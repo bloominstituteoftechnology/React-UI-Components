03a1567c467ceb19d0ccac669cb9822a8ddace87
CookieMonsta89 <jlstan06@gmail.com>
2018-10-02 12:00:46 -0400

    initial pr

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return (
+        <div>
+            <img 
+                alt='lambda logo'
+                className='thumb-img'
+                src='https://ibin.co/3whrpKSBbZ81.png'
+                />
+        </div>
+    );
+};
+
+export default ImageThumbnail;
+
