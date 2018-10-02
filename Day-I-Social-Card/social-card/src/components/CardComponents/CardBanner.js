a15ea979fc06e841f44e12096d264fb8c202a5ee
CookieMonsta89 <jlstan06@gmail.com>
2018-10-02 13:07:40 -0400

    some of banner done

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return ( 
+        <div className='banner'>
+            <img 
+                alt='banner'
+                className='image-banner'
+                src='https://ibin.co/3wnC6SgIOJud.png'
+            />
+        </div>
+     );
+}
+ 
+export default CardBanner;
