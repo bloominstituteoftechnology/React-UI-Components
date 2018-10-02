06a35189231752584c4e0ff761d8c3638fde0ce9
Katie Cleary <katiecleary@airmaster.lan>
2018-10-02 15:28:20 -0400

    basic button structure set-up

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Button.css';
+class ActionButton extends React.Component {
+    render() {
+      return (
+        <div className="component-button-panel">
+          <div>
+            <Button name="÷" red />
+            <Button name="X" red />
+            <Button name="-" red />
+            <Button name="+" red />
+            <Button name="=" red />
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  export default ActionButton;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,34 @@
 import React from 'react';
 import './Button.css';
+
+class NumberButton extends React.Component {
+    render() {
+      return (
+        <div className="component-button-panel">
+          <div>
+           <Button name="Clear" wide />
+          </div>
+          <div>
+            <Button name="7" />
+            <Button name="8" />
+            <Button name="9" />
+          </div>
+          <div>
+            <Button name="4"/>
+            <Button name="5"/>
+            <Button name="6"/>
+          </div>
+          <div>
+            <Button name="1"/>
+            <Button name="2"/>
+            <Button name="3"/>
+          </div>
+          <div>
+            <Button name="0"wide />
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  export default NumberButton;
