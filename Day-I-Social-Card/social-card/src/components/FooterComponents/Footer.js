256018706467e6daa772031b479817c99568c141
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-01 18:35:56 -0700

    Like button - heart turns red when clicked

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -18,17 +18,43 @@ import "./Footer.css";
 
 class Footer extends React.Component {
   state = {
-    retweets: 4,
-    likes: 0
+    retweets: 0,
+    likes: 0,
+    isRetweeted: false,
+    isLiked: false,
+    heart: "far fa-heart"
   };
 
   RetweetHandler = () => {
-    this.setState({ retweets: 5 });
+    if (!this.state.isRetweeted) {
+      this.setState({
+        retweets: ++this.state.retweets,
+        isRetweeted: true
+      });
+    } else {
+      this.setState({
+        retweets: --this.state.retweets,
+        isRetweeted: false
+      });
+    }
   };
 
   LikeHandler = () => {
     console.log("Liked!");
-    this.setState({ likes: 1 });
+    console.log(this.state);
+    if (!this.state.isLiked) {
+      this.setState({
+        likes: ++this.state.likes,
+        isLiked: true,
+        heart: "fas fa-heart"
+      });
+    } else {
+      this.setState({
+        likes: --this.state.likes,
+        isLiked: false,
+        heart: "far fa-heart"
+      });
+    }
   };
 
   render() {
@@ -42,7 +68,7 @@ class Footer extends React.Component {
         >
           <span className="counter">{this.state.retweets}</span>
         </i>
-        <i className="far fa-heart" title="Like" onClick={this.LikeHandler}>
+        <i className={this.state.heart} title="Like" onClick={this.LikeHandler}>
           <span className="counter">{this.state.likes}</span>
         </i>
         <i className="far fa-envelope" title="Message" />
