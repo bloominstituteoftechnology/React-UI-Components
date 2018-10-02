ddbad7936c199a7f70d21f9d47eb71f2234d9b59
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-10-02 19:33:11 -0400

    (DAY 2 - CALCULATOR) : (DISPLAY) : created calculator display file structure

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return <div className="calculator-display">
+        0
+    </div>;
+}; 
+
+export default CalculatorDisplay;
