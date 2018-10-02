73453d3ec4297db03f248c28d3a1eb74b5dea912
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-02 12:47:03 -0700

    added total and clear buttons

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+const numberBtn = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+const NumberButton = () => {
+    return (
+        <button>{numberBtn}</button>
+    );
+};
+
+export default NumberButton;
+export default numberBtn;
