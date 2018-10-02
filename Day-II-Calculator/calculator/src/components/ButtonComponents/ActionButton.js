ff6a4a70de7c241358a939c0ba661720b1f7d42e
Justin <jdlow2@gmail.com>
2018-10-02 18:01:05 -0400

    changed 'class' to 'className' in ActionButton.js

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 const ActionButton = props => {
-  return <button class="defaultNumButton actionButton">{props.buttonText}</button>
+  return <button className="defaultNumButton actionButton">{props.buttonText}</button>
 }
 
 export default ActionButton;
