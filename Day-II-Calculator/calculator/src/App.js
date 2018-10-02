ec6dabc36b105d49be25550515f3a428c22cbc43
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-02 17:48:57 -0400

    finished calculator structure

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import ActionButton from './components/ButtonComponents/ActionButton';
-import NumberButton from './components/ButtonComponents/NumberButton';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 
 const App = () => {
   return (
-    <div className="calculator-container">
-      <ActionButton />
-      <NumberButton />
+    <div className="container">
       <CalculatorDisplay />
     </div>
   );
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -5,7 +5,7 @@ import NumberButton from '../ButtonComponents/NumberButton';
 
 const CalculatorDisplay = () =>{
     return(
-        <div className="calculator-container">
+        <div className="calculator">
             <div className="total">
                 <h1>0</h1> 
             </div>
@@ -15,7 +15,26 @@ const CalculatorDisplay = () =>{
                 <ActionButton button-style="operator" text="÷" />
             </div>
 
-            
+            <div className="button-container">
+                <NumberButton button-style="number-button" text="7" />
+                <NumberButton button-style="number-button" text="8" />
+                <NumberButton button-style="number-button" text="9" />
+                <ActionButton button-style="operator" text="x" />
+            </div>
+
+            <div className="button-container">
+                <NumberButton button-style="number-button" text="4" />
+                <NumberButton button-style="number-button" text="5" />
+                <NumberButton button-style="number-button" text="6" />
+                <ActionButton button-style="operator" text="-" />
+            </div>
+
+            <div className="button-container">
+                <NumberButton button-style="number-button" text="1" />
+                <NumberButton button-style="number-button" text="2" />
+                <NumberButton button-style="number-button" text="3" />
+                <ActionButton button-style="operator" text="+" />
+            </div>
 
             <div className="button-container">
                 <NumberButton button-style="zero-button" text="0" />
