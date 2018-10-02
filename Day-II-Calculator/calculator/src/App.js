85dccfa71f5280515f844ef111b32f63e5e819b8
Peter Pham <phamhere@gmail.com>
2018-10-02 09:56:52 -0600

    finished MVP calculator

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="container">
+      <CalculatorDisplay />
+      <div className="row">
+        <ActionButton text="clear" />
+        <NumberButton text="÷" style="sign" />
+      </div>
+      <div className="row">
+        <NumberButton text="7" style="num" />
+        <NumberButton text="8" style="num" />
+        <NumberButton text="9" style="num" />
+        <NumberButton text="×" style="sign" />
+      </div>
+      <div className="row">
+        <NumberButton text="4" style="num" />
+        <NumberButton text="5" style="num" />
+        <NumberButton text="6" style="num" />
+        <NumberButton text="-" style="sign" />
+      </div>
+      <div className="row">
+        <NumberButton text="3" style="num" />
+        <NumberButton text="2" style="num" />
+        <NumberButton text="1" style="num" />
+        <NumberButton text="+" style="sign" />
+      </div>
+      <div className="row">
+        <ActionButton text="0" />
+        <NumberButton text="=" style="sign" />
+      </div>
     </div>
   );
 };
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => <div className="calcDisplay">0</div>;
+
+export default CalculatorDisplay;
