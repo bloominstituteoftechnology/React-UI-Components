a56edf20bdbdec909208a39d50b029b14ac64d7f
Daniel Ferrer <danfrrr13@gmail.com>
2018-10-02 16:18:43 -0400

    created calculator buttons

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="app">
+      <NumberButton buttonStyle="wide" text="clear" />
+      <ActionButton buttonStyle="red" text="&#247;" />
+      <NumberButton buttonStyle="white" text="7" />
+      <NumberButton buttonStyle="white" text="8" />
+      <NumberButton buttonStyle="white" text="9" />
+      <ActionButton buttonStyle="red" text="&#215;" />
+      <NumberButton buttonStyle="white" text="4" />
+      <NumberButton buttonStyle="white" text="5" />
+      <NumberButton buttonStyle="white" text="6" />
+      <ActionButton buttonStyle="red" text="&#8722;" />
+      <NumberButton buttonStyle="white" text="1" />
+      <NumberButton buttonStyle="white" text="2" />
+      <NumberButton buttonStyle="white" text="3" />
+      <ActionButton buttonStyle="red" text="+" />
+      <NumberButton buttonStyle="wide bold" text="0" />
+      <ActionButton buttonStyle="red" text="=" />
     </div>
   );
 };
