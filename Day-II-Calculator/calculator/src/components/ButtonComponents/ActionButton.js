8653e3cd1ed55168b33d4bbcddac9e5d228fc242
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-10-03 16:53:00 -0400

    Final Submission

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+onst ActionButton = (props) => {
+  return (
+    <button className={props.buttonStyle}>
+      {props.ActionButton.text}
+    </button>
+  );
+};
+
+export default ActionButton;
