5292fd278b1f412c2609c51bd45acace9e414feb
lola <lev@lolas-air.lan>
2018-10-03 12:55:26 -0400

    It works!!!!

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -5,46 +5,62 @@ import ActionButton from './components/ButtonComponents/ActionButton';
 import OperationButton from './components/ButtonComponents/OperationButton';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
-const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3];
+const numbers = ['7', '8', '9', '4', '5', '6', '1', '2', '3'];
 const ops = ['/', '*', '-', '+', '='];
 
 class App extends React.Component {
-	state = {
-		total: 4,
-		num1: 0,
-		num2: 0,
-		operator: null
+	constructor() { 
+		super();
+		this.state = {
+			total: '',
+			num1: null,
+			num2: null,
+			operator: null
 	}
-	
+}	
 	clear = () => {
 		this.setState({
-			total: 0
+			total: '0'
 		});
 	}
 
 	selectNum = (num) => {
+		const total = this.state.total;
 		if (this.state.operator) {
 			this.setState({
-				num2: num
+				num2: num,
+				total: total+num
 			})
-		} else {
+		}	else {
 			this.setState({
-				num1: num
+				num1: num,
+				total: total+num,
 			})
 		}
 	}
 
 	selectOp = (op) => {
-		const {num1, num2, operator} = this.state
-		this.setState({
-			operator: op
-		})
-		if (operator === '=') {
+		// const {num1, num2, operator} = this.state;
+		const total = this.state.total;
+		if (!this.state.operator) {
 			this.setState({
-				total: eval(`${num1} ${operator} ${num2}`)
-		});
+				operator: op,
+				total: total+op
+			})
+		} else if (op === '=') {
+			this.setState({
+				total: eval(total)
+			})
+		} else if (this.state.operator, this.state.num1, this.state.num2) {
+			this.setState({
+				num1: total,
+				num2: '',
+				operator: op,
+				total: total+op
+			})
+		}
 	}
-}
+
 
 	render() {
 		return (
@@ -63,7 +79,6 @@ class App extends React.Component {
 			</div>
 		);
 	}
-	
-};
+}
 
 export default App;
