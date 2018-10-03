75389d32f50c8d3832646262d081af0b1c68c03a
SabrinaGear <sabrinagear.ft@gmail.com>
2018-10-02 19:42:50 -0500

    initial display

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return (
+        <div className={props.style}>
+            <h1>{props.symbol}</h1>
+        </div>
+    )
+}
+
+export default CalculatorDisplay;
