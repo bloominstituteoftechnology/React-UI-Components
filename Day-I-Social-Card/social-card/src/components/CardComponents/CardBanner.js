7a2a46f9b6be1bbb57bcb2e85c64deba2a07c607
Sungyun Won <seanwon8@gmail.com>
2018-10-04 13:47:02 -0700

    day 1

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () =>{
+    return(
+        <a href = "https://react.org">
+            <img src="https://ibin.co/3wnC6SgIOJud.png"/>
+        </a>
+    );
+};
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return(
+        <div>
+            Lambda School @LambdaSchool 26 jan
+        </div>
+    );
+};
+
+export default HeaderTitle;
