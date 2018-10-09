5ab4395955da40e308d7aa313fea601d21ec3f05
nickkruger <ngkruger@gmail.com>
2018-10-08 21:37:05 -0600

    built main container

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Social Card!</h3>
+    <div className="container">
+      
+    </div>
+  );
+};
+
+export default App;
+
+
+/* <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
         few files that we've already included in there to get you started right
@@ -16,9 +24,4 @@ const App = () => {
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
-      </p>
-    </div>
-  );
-};
-
-export default App;
+      </p> */
