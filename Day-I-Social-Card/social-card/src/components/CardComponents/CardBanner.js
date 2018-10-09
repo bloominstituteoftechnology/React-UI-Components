cdc03749f0601093acf796c85e7663d2b9bcc67d
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-10-09 17:41:29 -0500

    finished mvp for both projects

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -4,7 +4,7 @@ import './Card.css';
 const CardBanner = () => {
   return (
     <div>
-      <img className = "cardBannerImage"src="https://ibin.co/3wnC6SgIOJud.png" />
+      <img className = "cardBannerImage"src="https://ibin.co/3wnC6SgIOJud.png"alt="React Logo" />
     </div>
   )
 }
