7fecfc9119cbcbba9f351c27e2185fb68407e098
jaklundun <govi.escudero@gmail.com>
2018-10-09 16:15:40 -0700

    Built out CardComponent, HeaderComponent and FooterComponent

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail' ;
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContent = () => {
+    return (
+    <div className="header-content">
+        <ImageThumbnail />
+        <HeaderTitle />
+    </div> 
+    )
+} ;
+
+export default HeaderContent ;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title">
+            <div className="header-title-top">
+                <h3>Lambda School</h3>
+                <h4>@LambdaSchool</h4>
+                <h4>Jan 31</h4>
+            </div> 
+            <br/>
+            <div className="header-title-text">
+                <p>Let's learn React by building simple interfaces with components.  Don't try to overthink it, just keep it simple and have fun.  Once you feel comfortable using components you are well on your way to mastering React!</p>
+            </div>
+        </div>
+    )
+} ;
+
+export default HeaderTitle ;
