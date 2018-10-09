3f36d14ee114faf0eedaafde5e9065791dadb1fa
Jake Thomas <jmaxt12@gmail.com>
2018-10-09 16:13:40 -0600

    Calculator build. Need to style

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
+    <div className = "app">
+      
+      
+    {/*Calculator Display first row for answer*/}
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+        <CalculatorDisplay />
     </div>
-  );
+     {/*Calculator second row : clear and button*/}
+    <div>
+        <NumberButton name="clear" />
+        <ActionButton name="/" />
+    </div>
+   {/*Calculator third row : 7, 8, 9 button*/}
+    <div>
+        <NumberButton  name="7" />
+        <NumberButton  name="8" />
+        <NumberButton  name="9" />
+        <ActionButton  name="x" />
+    </div>
+   {/*Calculator forth row : 4, 5, 6 button*/}
+    <div>
+        <NumberButton  name="4" />
+        <NumberButton  name="5" />
+        <NumberButton  name="6" />
+        <ActionButton  name="-" />
+    </div>
+   {/*Calculator fifth row : 1, 2, 3 button*/}
+    <div>
+        <NumberButton  name="1" />
+        <NumberButton  name="2" />
+        <NumberButton  name="3" />
+        <ActionButton  name="+" />
+    </div>
+   {/*Calculator last row : 0 and = button*/}
+    <div>
+        <ActionButton name="0" />
+        <ActionButton name="=" />
+    </div>
+   </div>
+);
 };
 
 export default App;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Button.css';
+
+
+const ActionButton = props => {
+    return <button className = 'action-button'> {props.name}</button>
+}
+
+export default ActionButton;
