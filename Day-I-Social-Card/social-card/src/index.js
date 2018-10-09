de8a1c5fd9febd7eb7c7c32da2c9968c8405e973
mm2079 <vayne3252@gmail.com>
2018-10-09 16:58:09 -0400

    Starting out

--- Day-I-Social-Card/social-card/src/index.js
+++ Day-I-Social-Card/social-card/src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+// Just started
 ReactDOM.render(<App />, document.getElementById('root'));
