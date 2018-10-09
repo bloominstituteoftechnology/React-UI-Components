753caae4f6a11917376ceb4a75ca09ed3d0ec604
Phil Hart <phil@Phils-iMac.local>
2018-10-08 21:17:14 -0600

    Finished MVP

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
+import CardBanner from './CardBanner';
+
+const CardContainer = () => {
+    return (
+        <div className="CardContainer">
+        <CardContent />
+        <CardBanner />
+        </div>
+    )
+}
+export default CardContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+const Logo = () => {
+    return (
+        <img ClassName ='logo' src= "https://ibin.co/3whrpKSBbZ81.png" />
+    )
+}
+export default Logo;
