62b52fd751827bc8a7fe5a0e5e952dac82bee62b
nickkruger <ngkruger@gmail.com>
2018-10-09 16:28:12 -0600

    Built Header Container Contents

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -3,9 +3,11 @@ import './Card.css';
 
 const CardContainer = () => {
     return (
+      <a href="https://www.reactjs.org">
       <div className="cardContainer">
         
       </div>
+      </a>
     );
   };
   
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,11 +3,11 @@ import './Header.css';
 
 const HeaderTitle = () => {
     return (
-      <div className="headerTitle">
-        <p class="title">Lambda School</p>
-        <p class="at">@LambdaSchool * 26 jan</p>
-      </div>
-    );
+        <div class="headerTitle">
+       <p class="title">Lambda School</p>
+       <p class="at">@LambdaSchool * 26 jan</p>
+       </div>
+      );
   };
 
   export default HeaderTitle;
