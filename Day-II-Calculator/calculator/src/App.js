6f6981a58100ca82a765c9e9775514b2435713ee
Jake Thomas <jmaxt12@gmail.com>
2018-10-10 08:28:17 -0600

    Partially styled

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -10,38 +10,38 @@ const App = () => {
       
       
     {/*Calculator Display first row for answer*/}
-    <div>
+    <div className = 'row'>
         <CalculatorDisplay />
     </div>
      {/*Calculator second row : clear and button*/}
-    <div>
+    <div className = 'row'>
         <NumberButton name="clear" />
         <ActionButton name="/" />
     </div>
    {/*Calculator third row : 7, 8, 9 button*/}
-    <div>
+    <div className = 'row'>
         <NumberButton  name="7" />
         <NumberButton  name="8" />
         <NumberButton  name="9" />
         <ActionButton  name="x" />
     </div>
    {/*Calculator forth row : 4, 5, 6 button*/}
-    <div>
+    <div className = 'row'>
         <NumberButton  name="4" />
         <NumberButton  name="5" />
         <NumberButton  name="6" />
         <ActionButton  name="-" />
     </div>
    {/*Calculator fifth row : 1, 2, 3 button*/}
-    <div>
+    <div className = 'row'>
         <NumberButton  name="1" />
         <NumberButton  name="2" />
         <NumberButton  name="3" />
         <ActionButton  name="+" />
     </div>
    {/*Calculator last row : 0 and = button*/}
-    <div>
-        <ActionButton name="0" />
+    <div className = 'row'>
+        <NumberButton name="0" />
         <ActionButton name="=" />
     </div>
    </div>
