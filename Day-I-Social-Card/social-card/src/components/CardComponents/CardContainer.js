71b2de4bbeb2d0644ed82da75404b5412a236cd6
nickkruger <ngkruger@gmail.com>
2018-10-09 19:45:08 -0600

    Day-II-Built App Box

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Card.css';
 
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
 const CardContainer = () => {
     return (
-      <a href="https://www.reactjs.org">
+      
       <div className="cardContainer">
-        
+      <CardBanner />
+      <CardContent />
       </div>
-      </a>
+      
     );
   };
   
--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <div className="App">
       <h3>Welcome to React Calculator</h3>
       <p>
         We have given you a starter project. You'll want to build out your
