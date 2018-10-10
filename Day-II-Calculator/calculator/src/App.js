3b4b1894e7567ab7d6ed28619df58e80fec9d36c
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-10-10 11:52:11 -0400

    [chore] deleted wrong repo, reuploading

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -3,11 +3,13 @@ import CalculatorLayout from './components/CalculatorLayout/CalculatorLayout';
 
 
 
-const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const actionBtn = [0, "clear"];
-const calcOperators = ["÷", "x", "-", "+", "="];
+
 
 const App = () => {
+  const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const actionBtn = [0, "clear"];
+  const calcOperators = ["÷", "x", "-", "+", "="];
+  
   return (
     <CalculatorLayout nums={numArray} actions={actionBtn} operators={calcOperators} />
   );
