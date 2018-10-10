aa43934e95eb8f1a4611c46084f0cf41af4063c8
nickkruger <ngkruger@gmail.com>
2018-10-09 19:45:08 -0600

    Fixed link issue, MVP done

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -7,7 +7,6 @@ const HeaderContent = () => {
     return (
         <div className="headerContent">
             <HeaderTitle />
-
             <div>
                 <p>Let's Learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel 
                 comfortable using components you are well on your way to mastering React!
