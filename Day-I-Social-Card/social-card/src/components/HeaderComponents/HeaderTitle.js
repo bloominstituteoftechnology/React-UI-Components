94aa78c2250aaa6fed25302e9e145aba3f514dfa
Aiveteran <42783946+Aiveteran@users.noreply.github.com>
2018-10-10 14:06:02 -0700

    first commit today with border around card and header working on importing image

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+    return (
+        <title className="header-title-box">
+            <h3>Lambda School</h3> 
+        </title>
+    );
+};
+
+export default HeaderTitle; 
