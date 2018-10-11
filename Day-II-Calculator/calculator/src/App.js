c6fe0d5ebf2c92736e549948f1773177c245ff66
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-10-11 12:16:02 -0500

    fixed weird width problem

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -6,42 +6,44 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 
 const App = () => {
   return (
-    <div className="MyCalculator">
-      <div className="DisplayBox">
-        <CalculatorDisplay number="0" />
-      </div>
-      <div className="Buttons">
-        <div className="WhiteButtons">
-          <div className="clearBtn">
-            <ActionButton number="clear" />
-          </div>
-          <div className='NumRow'>
-            <NumberButton number="7" />
-            <NumberButton number="8" />
-            <NumberButton number="9" />
-          </div>
-          <div className='NumRow'>
-            <NumberButton number="4" />
-            <NumberButton number="5" />
-            <NumberButton number="6" />
-          </div>
-          <div className='NumRow'>
-            <NumberButton number="1" />
-            <NumberButton number="2" />
-            <NumberButton number="3" />
-          </div>
-          <div className="zeroBtn">
-            <ActionButton number="0" />
-          </div>
+    <div className="container">
+      <div className="myCalculator">
+        <div className="DisplayBox">
+          <CalculatorDisplay number="0" />
         </div>
-        <div className="RedButtons">
-          <NumberButton number="÷" />
-          <NumberButton number="x" />
-          <NumberButton number="―" />
-          <NumberButton number="+" />
-          <NumberButton number="=" />
-        </div>      
-      </div>  
+        <div className="Buttons">
+          <div className="WhiteButtons">
+            <div className="clearBtn">
+              <ActionButton number="clear" />
+            </div>
+            <div className='NumRow'>
+              <NumberButton number="7" />
+              <NumberButton number="8" />
+              <NumberButton number="9" />
+            </div>
+            <div className='NumRow'>
+              <NumberButton number="4" />
+              <NumberButton number="5" />
+              <NumberButton number="6" />
+            </div>
+            <div className='NumRow'>
+              <NumberButton number="1" />
+              <NumberButton number="2" />
+              <NumberButton number="3" />
+            </div>
+            <div className="zeroBtn">
+              <ActionButton number="0" />
+            </div>
+          </div>
+          <div className="RedButtons">
+            <NumberButton number="÷" />
+            <NumberButton number="x" />
+            <NumberButton number="―" />
+            <NumberButton number="+" />
+            <NumberButton number="=" />
+          </div>      
+        </div>  
+      </div>
     </div>  
   );
 };
