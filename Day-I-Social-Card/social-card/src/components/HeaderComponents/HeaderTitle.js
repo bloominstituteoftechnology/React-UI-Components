d669ab155f568a2fd8c994cc3fde9198df43f794
sean-one <basement365@gmail.com>
2018-10-28 11:16:43 -0700

    added moment | date now reflect current date

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
 const HeaderTitle = () => {
     return <div className="profileHeader">
         <p className="name">Lambda School</p>
         <p className="username light">@LambdaSchool</p>
         <p className='light'>&#9679;</p>
-        <p className="date light">26 jan</p>
+        <p className="date light">{moment().format('MMM D')}</p>
       </div>;
 };
           
