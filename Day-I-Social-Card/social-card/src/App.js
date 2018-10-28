38de073a745838deed8b329bd6f303d055334f05
sean-one <basement365@gmail.com>
2018-10-28 13:06:07 -0700

    gave the body a max-width and centered

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className='container'>
         <HeaderContainer />
         <CardContainer />
         <Footer
