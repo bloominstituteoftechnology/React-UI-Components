2d263d8b1b7606c128b0f1a3023a4978c09adcd4
sean-one <basement365@gmail.com>
2018-10-28 12:53:01 -0700

    added like button functionality

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -6,14 +6,37 @@ import Footer from './components/FooterComponents/Footer';
 
 import './App.css';
 
-const App = () => {
-  return (
-    <div>
-      <HeaderContainer />
-      <CardContainer />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shareCount: 6,
+      likeCount: 4,
+      liked: false
+    }
+  }
+
+  likeButton = (prevState) => {
+    this.setState(prevState => ({ 
+      likeCount: prevState.likeCount + 1,
+      liked: true
+     }))
+  }
+
+  render() {
+    return (
+      <div>
+        <HeaderContainer />
+        <CardContainer />
+        <Footer
+          likes={this.state.likeCount}
+          shares={this.state.shareCount}
+          liked={this.state.liked}
+          likeIt={this.likeButton}
+        />
+      </div>
+    );
+  }
 };
 
 export default App;
--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,17 +3,24 @@ import "./Footer.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart, faSyncAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-const comment = <FontAwesomeIcon icon={faComment} flip='horizontal' />;
-const shared = <FontAwesomeIcon icon={faSyncAlt} />;
-const likeHeart = <FontAwesomeIcon icon={faHeart}/>;
-const email = <FontAwesomeIcon icon={faEnvelope} />;
 
-const Footer = () => {
+const Footer = props => {
+    const comment = <FontAwesomeIcon icon={faComment} flip='horizontal' />;
+    const shared = <FontAwesomeIcon icon={faSyncAlt} />;
+    const likeHeart = <FontAwesomeIcon icon={faHeart} color={!props.liked ? 'null' : 'red'} />;
+    const email = <FontAwesomeIcon icon={faEnvelope} />;
+
     return (
         <div className='footerIcons'>
             <p>{comment}</p>
-            <p>{shared} <span className='shareCount'>6</span></p>
-            <p>{likeHeart} <span className='likeCount'>4</span></p>
+            <p>
+                <span className='shareIcon'>{shared}</span>
+                <span className='shareCount'>{props.shares}</span>
+            </p>
+            <p>
+                <span onClick={props.likeIt} className='likeIcon'>{likeHeart}</span> 
+                <span className='likeCount'>{props.likes}</span>
+            </p>
             <p>{email}</p>
         </div>
     );
