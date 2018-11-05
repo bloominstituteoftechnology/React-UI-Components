17f60ac8c37f0a341c01a1cbf4713e5fbe98e3d3
jheinz1999 <jonathan.heinz@aol.com>
2018-11-05 13:44:59 -0800

    added click event

--- Day-I-Social-Card/social-card/src/index.js
+++ Day-I-Social-Card/social-card/src/index.js
@@ -4,3 +4,5 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+document.querySelector('.card-content').addEventListener('click', () => window.location.href = "https://www.reactjs.org");
