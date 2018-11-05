97331e2f525a80bf24d1ff6355054f99f40d441c
wcolts2000 <wcolts2000@gmail.com>
2018-11-05 14:31:22 -0800

    added footer component

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footerContainer">
+      <i class="far fa-comment"></i>
+      <i class="fas fa-sync"></i>
+      <p>6</p>
+      <i class="far fa-heart"></i>
+      <p>4</p>
+      <i class="far fa-envelope"></i>
+    </div>
+  )
+}
+
+export default Footer;
