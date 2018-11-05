2fa005665a0f32092abaa0f867e9f7d98cd19e11
Josh Z <jzieger10@gmail.com>
2018-11-05 17:16:54 -0500

    Finally got the logo to appear and header div to show

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return (
+        <div className="header-container">
+            <ImageThumbnail />
+        </div>
+    )
+}
+
+// const rootElement = document.getElementById('root');
+// ReactDOM.render(<HeaderContainer />, rootElement);
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return <img src="https://ibin.co/3whrpKSBbZ81.png" alt="" className ="logo" />
+}
+
+export default ImageThumbnail;
