3df45178ff07866ad57048a293f77aaee25add30
cbtsao47 <cbtsao47@gmail.com>
2018-11-05 15:16:16 -0600

    initial commit

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
+function HeaderContainer(){
+    return(
+        <React.Fragment>
+            <ImageThumbnail src = 'https://ibin.co/3whrpKSBbZ81.png'/>
+            <HeaderContent title = 'Lambda School'/>
+        </React.Fragment>
+    );
+}
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+
+function HeaderContent(props){
+    return(
+        <div>
+        <HeaderTitle title ='testing'/>
+        </div>
+    );
+}
+export default HeaderContent;
