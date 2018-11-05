1e22ecc29d1644418c1eaf6596ceeae6559f23f6
ToMMaaY <thuynh629@gmail.com>
2018-11-05 15:14:44 -0800

    Completed the mark up for the card container

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+function CardContainer() {
+    return (
+        <div className='card-container'>
+            <CardBanner  source='https://ibin.co/3wnC6SgIOJud.png' alt='ReactJS Banner' />
+            <CardContent 
+                title='Get started with React' 
+                content='React makes it painless to create interactice UIs. Design simple views for each state in your application.' 
+                url='reactjs.org'
+            />
+        </div>
+    )
+}
+
+export default CardContainer;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent(props) {
+    return (
+        <div className='content' >
+            <p className='title'>{props.title}</p>
+            <p>{props.content}</p>
+            <p className='url'>{props.url}</p>
+        </div>
+    )
+}
+
+export default CardContent;
