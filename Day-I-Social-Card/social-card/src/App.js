948c0fa1313350be5bc12e87d671dc0592b132f5
Dennis Orbison <orbison.dennis@gmail.com>
2018-11-05 13:11:41 -0800

    completed MVP

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import Header from './components/HeaderComponents/HeaderContainer.js';
+import CardContainer from './components/CardComponents/CardContainer.js'; 
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Social Card!</h3>
+    <div className={'app'}>
+      <Header 
+      className={'header'}
+      logo={'https://ibin.co/3whrpKSBbZ81.png'}
+      altText={'logo'}
+      title={'Lambda School'}
+      content={`Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!`}
+      identifer={'LambdaSchool'}
+      date={'26 jan'} />
+
+      <CardContainer 
+      image={'https://ibin.co/3wnC6SgIOJud.png'}
+      heading={`Get Started with React`}
+      content={`React makes it painless to create interactive UIs. Design simple views for each state in your applicaiton.`}
+      additional={'reactjs.org'} />
+      {/* <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
         few files that we've already included in there to get you started right
@@ -16,7 +32,7 @@ const App = () => {
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
-      </p>
+      </p> */}
     </div>
   );
 };
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner.js';
+import CardContent from './CardContent.js';
+
+const CardContainer = (props) => {
+  return (
+    <div className={'card-container'}>
+      <CardBanner image={props.image} />
+      <CardContent heading={props.heading} 
+      content={props.content}
+      additional={props.additional} />
+    </div>
+  );
+};
+
+export default CardContainer;
