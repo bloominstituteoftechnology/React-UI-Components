77e79ecfd34540ae77c5eb573ea8815697d19d52
Trai Lynne <trailynne@outlook.com>
2018-11-05 14:34:38 -0700

    Added code to CardBanner Imported and called in CardContaine

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+function(props) {
+    return (
+        <div class='card-banner'>
+
+        </div>
+    );
+}
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Card.css';
 
+import CardBanner from './CardBanner';
+
 function CardContainer(props) {
     return (
         <div class='card-container'>
-
+            <CardBanner />
         </div>
     )
 }
