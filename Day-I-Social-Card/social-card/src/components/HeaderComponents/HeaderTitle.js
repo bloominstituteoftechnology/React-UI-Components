78a4cf294ca1356cc8eceb34588cef02864dc653
wisdomtooth <papaya2002@hotmail.com>
2018-11-05 15:16:16 -0600

    initial commit

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(props){
+    return(
+        <h1 title='header'>{props.title}</h1>
+    );
+}
+export default HeaderTitle;
