f23ae3f6ca22f9632b5711d027c4abbbd26c2864
jheinz1999 <jonathan.heinz@aol.com>
2018-11-05 12:32:15 -0800

    created HeaderContainer

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContainer(props) {
+
+  const textContent = "Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!";
+
+  return (
+
+    <>
+
+      <ImageThumbnail src="https://ibin.co/3whrpKSBbZ81.png" />
+      <HeaderTitle title="Lambda School" handle="@LambdaSchool" timestamp="26 jan" />
+      <HeaderContent content={textContent}/>
+
+    </>
+
+  );
+
+}
