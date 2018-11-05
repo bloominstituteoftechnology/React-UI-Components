f41d9dae4ae0bf33cad163469019de41533d5c28
darenlarson <larson.daren@gmail.com>
2018-11-05 15:57:46 -0600

    Finished structuring the header component.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(props) {
+    return (
+        <div className="header-title">
+            <h1 className="lambdaTitle">Lambda School</h1>
+            <p className="twitterHandle">@LambdaSchool</p>
+            <p className="timestamp">26 jan</p>
+        </div>
+    )
+}
+
+export default HeaderTitle;
