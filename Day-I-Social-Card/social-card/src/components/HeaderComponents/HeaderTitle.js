f074e5f1875ff69f3a3ef6f458532b576656b531
kmarker1101 <kmarker1101@icloud.com>
2018-11-05 14:20:32 -0600

    Header Complete

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+
+        <div className='header'>
+            <h1 className='lambda'>Lambda School </h1>
+            <div className='dateline'> @LambdaSchool • 26 jan</div>
+        </div>)
+};
+
+export default HeaderTitle;
