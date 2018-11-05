35daee5353abd269d41fa1aec9d41019fcf287e2
ToMMaaY <thuynh629@gmail.com>
2018-11-05 14:16:05 -0800

    Completed the mark up structure for the header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(props) {
+    return (
+        <div className='header-bar'>
+            <p class='header-title'>{props.title}</p>
+            <p class='username'>{props.userName}</p>
+            <p class='date-symbol'>.</p>    
+            <p class='header-date'>{props.date}</p>  
+        </div>
+    )
+}
+
+export default HeaderTitle;
