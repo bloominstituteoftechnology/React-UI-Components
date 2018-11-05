50a047038104a4997339421c787efee26c07d6c0
chasefulks <chase.fulks@gmail.com>
2018-11-05 15:13:01 -0600

    header container

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderContainer  = props => {
+    return (
+     <header className = "headerStyle">
+         <ImageThumbnail/>
+         <div className = "headerText">
+         <HeaderTitle></HeaderTitle>
+         </div>
+        <HeaderContent></HeaderContent>
+       
+    </header>
+);
+};
