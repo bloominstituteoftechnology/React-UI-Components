7627187662055613fe093c894c15457bad08259c
Trai Lynne <trailynne@outlook.com>
2018-11-05 14:13:46 -0700

    Added code to HeaderContent Imported and called in HeaderTitle

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.css';
 
+import HeaderContent from './HeaderContent'
+
 function HeaderTitle(props) {
     return (
         <div class='text-content'>
             <h1>Lambda School <span>@LambdaSchool - 05 nov</span></h1>
+
+            <HeaderContent />
         </div>
     );
 }
