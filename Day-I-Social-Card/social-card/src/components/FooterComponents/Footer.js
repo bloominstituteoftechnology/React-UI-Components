0a5816f21563747ac5cb665c3693a604a8ee60ee
Nick O'Ferrall <nickoferrall@gmail.com>
2018-11-05 17:18:40 -0500

    Cleaned code

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,9 +5,9 @@ import './Footer.css';
 const MyFooter = () => {
     return (
         <div className="footer-container">
-            <i class="fa fa-comment-o" aria-hidden="true"></i>
-            <i>H</i>
-            <i class="fa fa-camera-retro"></i> fa-camera-retro
+            {/* <i class="fa fa-comment-o" aria-hidden="true"></i> */}
+            {/* <i>H</i> */}
+            {/* <i class="fa fa-camera-retro"></i> fa-camera-retro */}
         </div>
     );
 }
