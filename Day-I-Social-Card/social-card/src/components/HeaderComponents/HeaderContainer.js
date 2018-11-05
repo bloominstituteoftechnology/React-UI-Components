64418790f3efa33e73b2084595526e616e0f9e45
jheinz1999 <jonathan.heinz@aol.com>
2018-11-05 13:08:40 -0800

    added HeaderContent component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,6 +3,7 @@ import './Header.css';
 
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 
 function HeaderContainer(props) {
 
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContent(props) {
+
+  return <p>{props.textContent}</p>
+
+}
+
+export default HeaderContent;
