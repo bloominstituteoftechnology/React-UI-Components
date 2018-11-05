fec0289ad8cffb15aa715dbbdb4d0241c5143137
dyiar <austin.hl.blake@gmail.com>
2018-11-05 15:07:45 -0700

    Created components for todays project

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+    <div className="headerTitle">
+        <h1>Lamda School</h1> <p>@LamdaSchool · 04 Nov</p>
+        
+    </div>
+
+    )};
+
+export default HeaderTitle
