d0fc4769c91dea12f1764053bfede140304ba588
Trai Lynne <trailynne@outlook.com>
2018-11-05 14:05:16 -0700

    Added code to HeaderTitle Wrapped in div.text-content Imported and called HeaderTitle in HeaderContainer

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Header.css';
 
+import HeaderTitle from './HeaderTitle';
+
 function HeaderContainer(props) {
     return (
-        <header> </header>
+        <header>
+            <HeaderTitle />
+        </header>
     );
 }
 
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(props) {
+    return (
+        <div class='text-content'>
+            <h1>Lambda School <span>@LambdaSchool - 05 nov</span></h1>
+        </div>
+    );
+}
+
+export default HeadeTitle;
