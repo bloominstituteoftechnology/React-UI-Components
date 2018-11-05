b039de8d56ffb18e526e7dde9b7174f40760fab2
wisdomtooth <papaya2002@hotmail.com>
2018-11-05 17:38:04 -0600

    MVP complete

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -3,19 +3,19 @@ import './Card.css';
 import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
-function CardContainer(props){
+const CardContainer = () => {
     return(
         <div className ="container">
-            <div className="card__container">
+            <a href="https://reactjs.org">
+            <div className="card__container"  href="https://reactjs.org">
             <CardBanner src= 'https://ibin.co/3wnC6SgIOJud.png'/>
             <CardContent 
                 title='Get started with React' 
                 text='React makes it painless to create interactive UIs. Design simple views for each state in your application.'
                 handle="reactjs.org"/>
             </div>
+            </a>
         </div>
-
     );
 }
-
 export default CardContainer;
