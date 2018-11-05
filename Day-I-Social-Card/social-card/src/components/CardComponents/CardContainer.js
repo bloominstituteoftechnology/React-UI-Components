0c45adfc6974eb0e027e92b947bfd3cad96d2b3c
jheinz1999 <jonathan.heinz@aol.com>
2018-11-05 13:16:12 -0800

    created CardContainer

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+
+function CardContainer(props) {
+
+  return (
+
+    <div className="card-content">
+
+      
+
+    </div>
+
+  );
+
+}
+
+export default CardContainer;
