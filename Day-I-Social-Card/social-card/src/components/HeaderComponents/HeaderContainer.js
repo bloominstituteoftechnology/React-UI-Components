07ba2e936400d1b8c7c4889d11fb6bc39b0177c3
Thomas-Ketterman <32739188+Thomas-Ketterman@users.noreply.github.com>
2018-11-05 15:08:44 -0500

    Completed Header Components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+function HeaderContainer() {
+  return (
+    <React.Fragment>
+      <ImageThumbnail src="https://ibin.co/3whrpKSBbZ81.png" />
+      <HeaderTitle
+        title="Lambda School"
+        taggedPerson="LambdaSchool"
+        date="26 Jan"
+      />
+      <HeaderContent content="Let's learn React by building  simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to masting React!" />
+    </React.Fragment>
+  );
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+function HeaderTitle(props) {
+  return (
+    <React.Fragment>
+      <h1 className="HeaderTitle_header">{props.title}</h1>
+      <span className="HeaderTitle_supplimental-data">
+        `@
+        {props.taggedPerson} - {props.date}`
+      </span>
+    </React.Fragment>
+  );
+}
+
+export default HeaderTitle;
