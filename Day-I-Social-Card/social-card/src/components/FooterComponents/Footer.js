2852730ae0ba6264664d9bd1d800276a8e3134ae
Addison-Stavlo <AddieStavlo@gmail.com>
2018-11-05 18:39:06 -0500

    footer icon numbers increase on click

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import './Footer.css';
 
+let numComments = 0;
+let numRetweets = 0;
+let numLikes = 0;
+
 function Footer(props) {
     return (
         <footer className='footer-container'>
             <div className='icon-box'>
-                <div className='icon'>
+
+                <div className='icon' onClick={addComment}>
                 <i class="fa fa-comment" aria-hidden="true"></i>
+                <p id='numComments'></p>
                 </div>
 
-                <div className='icon'>
+                <div className='icon' onClick={addRetweet}>
                 <i class="fa fa-retweet" aria-hidden="true"></i>
-                <p>5</p>
+                <p id='numRetweets'></p>
                 </div>
 
-                <div className='icon'>
+                <div className='icon' onClick={addLike}>
                 <i class="fa fa-heart" aria-hidden="true"></i>
-                <p>4</p>
+                <p id='numLikes'></p>
                 </div>
 
                 <div className='icon'>
@@ -26,6 +32,22 @@ function Footer(props) {
             </div>
         </footer>
     );
+}
+
+function addComment() {
+    const commentNumber = document.getElementById('numComments');
+    numComments++;
+    commentNumber.textContent = `${numComments}`;
+}
+function addRetweet() {
+    const retweetNumber = document.getElementById('numRetweets');
+    numRetweets++;
+    retweetNumber.textContent = numRetweets;
+}
+function addLike() {
+    const likesNumber = document.getElementById('numLikes');
+    numLikes++;
+    likesNumber.textContent = numLikes;
 }
 
 export default Footer;
