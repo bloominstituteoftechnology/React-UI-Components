b84b0bca6ee78b828b4243a7cc5c5427e1ee7c35
Civoremo <omerovic_n@hotmail.com>
2018-11-05 17:55:19 -0500

    MVP complete

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(){
+    // const timeSig = new Date.UTC();
+
+    return (
+        <div className='header-title'>
+            <h1>Lambda School</h1>
+            <p>@LambdaSchool · 26 jan</p>
+        </div>        
+    );
+}
+
+export default HeaderTitle;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+function ImageThumbnail(){
+    return (
+        <div className='header-logo'>
+            <img className='logo' src='https://ibin.co/3whrpKSBbZ81.png' alt='logo image'></img>
+        </div>
+    );
+}
+
+export default ImageThumbnail;
