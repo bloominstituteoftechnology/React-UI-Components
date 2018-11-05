59b182d840803c954a097b4099610fe334fb6129
tryingtokeepup <kl.hawaii@gmail.com>
2018-11-05 15:00:56 -0800

    woot woot, im starting to understand how to do this. i wonder how to add click events or whatever...

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+
+function HeaderContent() {
+    return <div className="HeaderContent">
+      <HeaderTitle text="Lambda" />
+      
+      <h2>Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!" </h2>
+    </div>
+  }
+
+
+export default HeaderContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+
+function ImageThumbnail() {
+    
+    return <img class="img-logo" src="https://ibin.co/3whrpKSBbZ81.png" />
+  }
+
+
+
+   export default ImageThumbnail; 
