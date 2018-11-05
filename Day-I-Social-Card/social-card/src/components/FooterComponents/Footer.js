23cb5a7c187d4c1c9687510553da6fed4e7b94a9
jheinz1999 <jonathan.heinz@aol.com>
2018-11-05 14:51:20 -0800

    created Footer component

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer(props) {
+
+  return (
+
+    <div className="footer">
+
+
+
+    </div>
+
+  );
+
+}
+
+export default Footer;
