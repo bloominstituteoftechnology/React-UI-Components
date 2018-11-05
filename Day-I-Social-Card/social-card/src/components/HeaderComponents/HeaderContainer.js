5e6055e886e28725dbcc905d50ea1f126bf0020f
chasefulks <chase.fulks@gmail.com>
2018-11-05 15:20:29 -0600

    headercontent

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Header.css";
-import ImageThumbnail from './ImageThumbnail.js';
-import HeaderTitle from './HeaderTitle.js';
-import HeaderContent from './HeaderContent.js';
+import ImageThumbnail from "./ImageThumbnail.js";
+import HeaderTitle from "./HeaderTitle.js";
+import HeaderContent from "./HeaderContent.js";
 
 const HeaderContainer = props => {
   return (
