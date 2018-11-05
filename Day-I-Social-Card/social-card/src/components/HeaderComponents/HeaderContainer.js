935ca4d6adfd4f6db9310971010629c6c2389736
jheinz1999 <jonathan.heinz@aol.com>
2018-11-05 13:00:00 -0800

    added HeaderTitle component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 
 import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
 
 function HeaderContainer(props) {
 
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(props) {
+
+  return (
+
+    <div className="card-header">
+
+      <h3>{props.title}</h3>
+      <h4>{`${props.handle} · ${props.timestamp}`}</h4>
+
+    </div>
+
+  );
+
+}
+
+export default HeaderTitle;
