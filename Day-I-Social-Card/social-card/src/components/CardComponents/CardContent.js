2f3ccf68c0a61b6ba1918c2b371587a0104170c1
Wesley Meeks <wesmeeks88@gmail.com>
2018-11-05 14:08:20 -0700

    got the card contentcreated, need to work on footer and start styling.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardContent = () => {
+  return (
+    <div class="card-content">
+      <h3>Get Started with React</h3>
+      <p>
+        React makes it painless to create interactive UIs. Designe simple views
+        for each state of your application.
+      </p>
+      <p class="link">reactjs.org</p>
+    </div>
+  );
+};
+
+export default CardContent;
