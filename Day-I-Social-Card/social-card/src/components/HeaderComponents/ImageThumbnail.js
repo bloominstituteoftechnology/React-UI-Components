28973d40614396a2e9c5f42be6860440357019e4
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-05 17:18:09 -0500

    "wip..."

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+function headerImg(props) {
+    return (
+      <div><img src={".jpg"}
+
+        </rdiv>
+    );
+}
+
+export default HeaderImg;
