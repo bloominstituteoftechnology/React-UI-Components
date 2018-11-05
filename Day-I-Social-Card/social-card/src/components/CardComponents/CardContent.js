016be0ab17801482a912e50f496df7a919eb25b5
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-11-05 16:07:22 -0600

    create card component

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+  return (
+    <div className="card--content">
+      <h4 className="card--header"> Get started with React</h4>
+      <p className="card--text">React makes is painless to create interactive UIs. Design simple views for each state in your application.</p>
+      <p className="card--link">reactjs.org</p>
+    </div>
+  );
+}
+
+export default CardContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <p className="header--title">
+      Lambda School <span>@LambdaSchool . 26 jan</span>
+    </p>
+  );
+}
+
+export default HeaderTitle;
