faef635f71ebb3570297115992f2879a91bc48ab
Lidii <lidiiany92@gmail.com>
2018-11-05 17:50:34 -0500

    Created and styled CardContainer component

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 
 const App = () => {
   return (
      <div className="App">
        <HeaderContainer />
+       <CardContainer />
      </div>
   );
 };
 
 export default App;
+
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+function CardBanner(props) {
+    return (
+        <div className="banner-wrapper">
+             <div className="card-banner"></div>
+        </div>
+    );
+}
+
+export default CardBanner;
