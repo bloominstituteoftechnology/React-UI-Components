2829ae5cc4a3eb30d403ba28989bc363cf1f8827
Jarrad Miller <jm020801@gmail.com>
2018-11-05 16:51:35 -0700

    working on sprint

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+
+const MyFooter = () => {
+    return (
+        <div className="footer-container">
+           <i class="fa fa-comment-o" aria-hidden="true"></i>
+           <i class="fa fa-refresh" aria-hidden="true"></i>
+           <i class="fa fa-heart-o" aria-hidden="true"></i>
+           <i class="fa fa-envelope-o" aria-hidden="true"></i>
+        </div>
+    );
+}
+
+export default MyFooter;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const ImgThumb = () => {
+    return <img className="LS-Logo" alt="" src="https://yt3.ggpht.com/a-/AN66SAw9C1HPxBru_POv5bcGj8yLp8OSc18VosnELA=s288-mo-c-c0xffffffff-rj-k-no" />
+}
+
+export default ImgThumb;
