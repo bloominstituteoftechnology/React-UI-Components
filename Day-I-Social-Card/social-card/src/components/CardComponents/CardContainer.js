ddca5ef34116c8c0b3866ad1fae347cb9c2b9c70
Trai Lynne <trailynne@outlook.com>
2018-11-05 14:41:29 -0700

    Added code to CardContent Imported and called in CardContainer

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Card.css';
 
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 function CardContainer(props) {
     return (
         <div class='card-container'>
             <CardBanner />
+
+            <CardContent />
         </div>
     )
 }
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent(props) {
+    return (
+        <div class='card-content' >
+            <h2>Get Started with React</h2>
+
+            <p>
+                React make it painless to create interactive UIs. Design simple views for each state of your application.
+            </p>
+
+            <p><span>reactjs.org</span></p>
+        </div >
+    );
+}
+
+export default CardContent;
