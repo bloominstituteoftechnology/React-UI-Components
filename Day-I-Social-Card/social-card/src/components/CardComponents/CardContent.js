021ff434013fef4b0bbc2d9fb396e48ae83f05ce
cbtsao47 <cbtsao47@gmail.com>
2018-11-05 16:00:26 -0600

    add card component

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -6,8 +6,8 @@ function CardContent (props){
         <div>
             <h2>{props.title}</h2>
             <p>{props.text}</p>
+            <a href='reactjs.org' target='_blank'>{props.handle}</a>
         </div>
     );
 }
-
 export default CardContent;
