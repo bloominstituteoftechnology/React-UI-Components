24351b4f30c9bb14d3ef1261441f66de865a8d69
jheinz1999 <jonathan.heinz@aol.com>
2018-11-05 13:19:24 -0800

    added CardBanner

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Card.css';
 
+import CardBanner from './CardBanner';
+
 function CardContainer(props) {
 
   return (
 
     <div className="card-content">
 
-      
+      <CardBanner src="https://ibin.co/3wnC6SgIOJud.png" />
 
     </div>
 
