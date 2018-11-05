e02ac903170db17e22a4170efd0bdd830990e92c
Aman <amsingh714@gmail.com>
2018-11-05 18:36:20 -0500

    footer complete

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,7 +4,16 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className='FooterContainer'>
-            <i class="fa fa-camera-retro fa-4x"></i>
+            <i class="fa fa-comment-o" aria-hidden="true"></i>
+            <div className="retweet">
+                <i class="fa fa-retweet" aria-hidden="true"></i>
+                <p>6</p>
+            </div>
+            <div className="like">
+                <i class="fa fa-heart-o" aria-hidden="true"></i>
+                <p>4</p>
+            </div>
+            <i class="fa fa-envelope-o" aria-hidden="true"></i>
         </div>
     );
 };
