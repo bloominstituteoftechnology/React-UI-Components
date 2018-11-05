cf9389d9bb7ffffb7ed6f3dbafd2a453eecc7b50
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-11-05 18:13:11 -0500

    Pasted over code from CodeSandbox

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div className="title">
+      <h1>
+        Lambda School <span>&#64;LambdaSchool &middot; 26 jan </span>
+      </h1>
+    </div>
+  );
+};
+
+export default HeaderTitle;
