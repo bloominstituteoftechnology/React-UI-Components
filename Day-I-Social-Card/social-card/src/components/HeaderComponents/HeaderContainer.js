cf7b0547a6d1e0c2e139180fdb778ddd3b843260
ChyChy <c.otikor@gmail.com>
2018-11-05 14:21:56 -0700

    Working on header component

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+function HeaderContainer() {
+    return (
+        <header>
+            <ImageThumbnail source="https://ibin.co/3whrpKSBbZ81.png" />
+            <div>
+                <HeaderTitle user='Lambda School' handle='@LambdaSchool' time='26 jan'/>
+                <HeaderContent />
+            </div>
+        </header>
+    );    
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(props) {
+  return (
+    <React.Fragment>
+      <h1 className="header">{props.user}</h1>
+      <h2 className="username">{props.handle + ' \u00B7 ' + props.time}</h2>
+    </React.Fragment>
+  );
+}
+
+export default HeaderTitle;
