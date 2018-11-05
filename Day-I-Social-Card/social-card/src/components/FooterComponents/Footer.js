6a3b70e04046cd6902529364a4886674651ad4e4
Josh Z <jzieger10@gmail.com>
2018-11-05 18:52:14 -0500

    Completed some stretch goals, added some hover coloring and <a> tags

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,10 +4,10 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="footer-container">
-            <i class="far fa-comment icon"></i>
+            <a href='#'><i class="far fa-comment icon"></i></a>
             <a href='#'><i class="fas fa-sync-alt icon"></i>6</a>
             <a href='#'><i class="far fa-heart icon"></i>4</a>
-            <i class="far fa-envelope icon"></i>
+            <a href='#'><i class="far fa-envelope icon"></i></a>
         </div>
     )
 }
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Header.css';
 
 const ImageThumbnail = () => {
-    return <img src="https://ibin.co/3whrpKSBbZ81.png" alt="" className ="logo" />
+    return (
+        <a href="#">
+            <img src="https://ibin.co/3whrpKSBbZ81.png" alt="" className ="logo" />
+        </a>
+    )
 }
 
 export default ImageThumbnail;
