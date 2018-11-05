59aa88de55fa06bc887aa71ac782543de23d38f7
brooks <bpoltl1@gmail.com>
2018-11-05 15:54:24 -0600

    header completed

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+import ImgThumbnail from "./ImageThumbnail";
+import HeaderContent from "./HeaderContent";
+import HeaderTitle from "./HeaderTitle";
+
+const HeaderContainer = () =>{
+    return(
+        <div className = "container">
+            <div className = 'content'>
+                <ImgThumbnail />
+                <div className = "text-content">
+                    <HeaderTitle />
+                    <HeaderContent />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default HeaderContainer;
