a49b9290f413cd9e6f1efb2b4ef16754a3f22477
Trai Lynne <trailynne@outlook.com>
2018-11-05 14:20:55 -0700

    Added code to ImageThumbnail Imported and called in HeaderContainer

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
 
+import ImageThumbnail from './ImageThumbnail'
 import HeaderTitle from './HeaderTitle';
 
 function HeaderContainer(props) {
     return (
         <header>
+            <ImageThumbnail />
             <HeaderTitle />
         </header>
     );
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+function ImageThumbnail(props) {
+    return (
+        <img src='https://ibin.co/3whrpKSBbZ81.png' alt='Thumbnail' />
+    );
+}
+
+export default ImageThumbnail;
