402b8db19b7ea7b7a708228be976b21df4faad66
steelworx <eileencuevas62@gmail.com>
2018-11-05 17:21:32 -0500

    completed styling for header

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,19 +3,21 @@ import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
 import HeaderContent from './HeaderContent';
-import image from './img/lambda-logo.png'
+import image from './img/lambda.jpg'
 
 function HeaderContainer() {
     return (
-        <React.Fragment>
+        <div className="header">
             <ImageThumbnail src={image} alt='Lambda Logo' />
-            <HeaderTitle 
-                name='Lambda School' 
-                username='@LambdaSchool' 
-                date='26 jan' 
-            />
-            <HeaderContent content = "Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!" />
-        </React.Fragment>
+            <div className="header-text">
+                <HeaderTitle 
+                    name='Lambda School' 
+                    username='@LambdaSchool' 
+                    date='26 jan' 
+                />
+                <HeaderContent content = "Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!" />
+            </div>
+        </div>
     );
 }
 
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,12 +3,12 @@ import './Header.css';
 
 function HeaderTitle(props) {
     return (
-        <React.Fragment>
+        <div className="header-title">
             <h2 className="header-name">{props.name}</h2>
             <p className="header-username">{props.username}</p>
             <p>&#9679;</p>
             <p className="header-date">{props.date}</p>
-        </React.Fragment>
+        </div>
     );
 }
 
