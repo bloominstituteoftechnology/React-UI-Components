bcc01331c35a35f5fd5bcfb7359ffc7fa82c3f4c
wisdomtooth <papaya2002@hotmail.com>
2018-11-05 16:00:26 -0600

    add card component

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -7,8 +7,10 @@ function CardContainer(props){
     return(
         <div>
             <CardBanner src= 'https://ibin.co/3wnC6SgIOJud.png'/>
-            <CardContent title='Get started with React' text='React makes it painless to create interactive UIs. Design simple views for each state in your application.'/>
-            <h3>reactjs.org</h3>
+            <CardContent 
+                title='Get started with React' 
+                text='React makes it painless to create interactive UIs. Design simple views for each state in your application.'
+                handle="reactjs.org"/>
         </div>
 
     );
