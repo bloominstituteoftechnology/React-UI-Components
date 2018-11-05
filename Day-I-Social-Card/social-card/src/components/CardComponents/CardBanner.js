805e61d88038c342b607079dfa9a9b8aff1d05a7
ChyChy <c.otikor@gmail.com>
2018-11-05 15:55:37 -0700

    Finished MVP moving on to stretch

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+function CardBanner(props) {
+  return (
+    <React.Fragment>
+      <img src={props.source} alt="React Symbol" />
+    </React.Fragment>
+  );
+}
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+function CardContainer() {
+  return <a href="https://www.reactjs.org" target="_blank" rel="noopener"> 
+        <main>
+          <section>
+            <CardBanner source="https://ibin.co/3wnC6SgIOJud.png" />
+            <div>
+              <CardContent />
+            </div>
+          </section>
+        </main>
+      </a>;
+}
+
+
+export default CardContainer;
