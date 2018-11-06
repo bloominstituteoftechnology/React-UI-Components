bebca22f472c20816c2c8ba5a2561b182d9419bf
Civoremo <omerovic_n@hotmail.com>
2018-11-06 17:26:48 -0500

    calculator displays properly

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton(props){
+
+    return (
+        <React.Fragment>
+            <button className={props.buttonStyle}>{props.text}</button>
+        </React.Fragment>
+    );
+}
+
+export default NumberButton;
