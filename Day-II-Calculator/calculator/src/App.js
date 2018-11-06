d0927f17641bc5b2e9d42fdf043772268957b92f
Trai Lynne <trailynne@outlook.com>
2018-11-06 15:23:51 -0700

    Added code for CalculatorDisplay Added height and width Currently rendering with black background with big white text Imported and called in App.js Add classes .display and .value

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 import './App.css';
 
 // Components
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 import ButtonContainer from './components/ButtonComponents/ButtonContainer'
 
 const App = () => {
   return (
     <Fragment>
+      <CalculatorDisplay value='0' />
       <ButtonContainer />
     </Fragment>
   );
