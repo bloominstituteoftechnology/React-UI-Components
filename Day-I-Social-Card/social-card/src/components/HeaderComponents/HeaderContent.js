fc7fd88f6e748195992c53a14403524cf096436e
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-05 23:55:10 -0500

    "css errors now"

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -4,13 +4,13 @@ import HeaderTitle from './HeaderTitle.js';
 
 function HeaderContent(props) {
     return (
-      <react.Fragment >
+      <React.Fragment >
 
             <HeaderTitle />
 
             <h4 className="header Para" > Let 's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun.Once you feel confortable using components you are well o your way to mastering React! </h4>
 
-        </react.Fragment>
+        </React.Fragment>
     );
 }
 
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -4,11 +4,11 @@ import './Header.css';
 
 function HeaderTitle(props) {
     return (
-        <react.Fragment >
+        <React.Fragment >
             <h1 className="header title" > Lambda School </h1>  <h5 className = "header titleb">@LambdaSchool * 26 jan </h5>
 
 
-        </react.Fragment>
+        </React.Fragment>
     );
 }
 
