270686065cee4a7bf0e0cef093a269b2d438d5f5
Wesley Meeks <wesmeeks88@gmail.com>
2018-11-06 10:35:35 -0700

    got display component created also created layout for calculator in app.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./Button.css";
 
-import React from "react";
-import "./Button.css";
-
 const ActionButton = props => {
   return (
     <button className={`action-button ${props.buttonStyle}`}>
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalculatorDisplay = props => {
+  return (
+    <div className={`calculator-display ${props.type}`}> {props.text}</div>
+  );
+};
+
+export default CalculatorDisplay;
