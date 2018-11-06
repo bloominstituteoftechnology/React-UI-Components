628717325b6523acf74a4345d731f38f86469801
Lidii <lidiiany92@gmail.com>
2018-11-06 17:38:38 -0500

    Fished styling of display component

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import ButtonsContainer from './components/ButtonComponents/ButtonsContainer';
 import OperatorsContainer from './components/ButtonComponents/OperatorsContainer';
+import  CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
-    <div class="calc-wrapper">
-      <ButtonsContainer />
-      <OperatorsContainer />
-    </div>
+    <React.Fragment>
+      <CalculatorDisplay />
+      <div class="calc-wrapper">
+        <ButtonsContainer />
+        <OperatorsContainer />
+      </div>
+    </React.Fragment>
+
   );
 };
 
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Display.css';
+
+class CalculatorDisplay extends Component {
+
+    state = {
+        initialValue: 0
+    }
+
+
+
+    render() {
+        return (
+            <div className="calculator-display"><span>{this.state.initialValue}</span></div>
+          );
+    }
+
+  }
+
+
+  
+  export default CalculatorDisplay;
