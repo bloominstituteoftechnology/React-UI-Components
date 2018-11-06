386dc062614812736d951c9898a94b70aa8115ba
David Situ <davidsitu626@gmail.com>
2018-11-05 16:06:58 -0800

    Setting up HTML, CSS pending

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+
+function HeaderContainer(){
+  return (
+    <React.Fragment>
+      <HeaderTitle title= 'Lambda School' />
+      <HeaderTitle tags='@LambdaSchool 	• 26 jan' />
+      <HeaderContent description='Lets learn React by building simple interfaces with components. Dont try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!' />
+    </React.Fragment>
+  );
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+
+function HeaderTitle(props){
+  return(
+    <React.Fragment>
+      <h1 className='header'>{props.title}</h1>
+      <p className='tags'>{props.tags}</p>
+    </React.Fragment>
+  );
+}
+
+export default HeaderTitle
