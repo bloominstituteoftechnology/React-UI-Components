49eeed20f5f47910dc6ee774bc044ce5896fd3b9
wcolts2000 <wcolts2000@gmail.com>
2018-11-05 17:26:52 -0800

    evening checkpoint before going far into adding footer states

--- Day-I-Social-Card/social-card/src/components/CommentComponents/Comment.js
+++ Day-I-Social-Card/social-card/src/components/CommentComponents/Comment.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './Comment.css';
 
 const Comment = (props) => {
-  const commentBubble = document.querySelector('.fa-comment');
-  console.log(commentBubble);
   
   return (
     <div className="commentBlock">
--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,9 +1,34 @@
+// import React, { Component } from 'react';
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => {
+const Footer = (props) => {
+//   state = {
+//     comment: "",
+//     retweet: 6,
+//     like: 4,
+//     email: "",
+//   }
+
+  // class Footer extends Component {
+  //   state = {
+  //     comment: "",
+  //     retweet: 6,
+  //     like: 4,
+  //     email: "",
+  //   }
+  
+
+  // handleClickEvent = event => {
+  //   prompt("Add comment...");
+  //   this.setState({
+  //     comment: event.target.value
+  //   })
+  // }
+
   return (
     <div className="footerContainer">
+      {/* <i className="far fa-comment" onClick={handleClickEvent()} value={this.state.comment} ></i> */}
       <i className="far fa-comment"></i>
       <i className="fas fa-sync"></i>
       <p>6</p>
@@ -14,4 +39,19 @@ const Footer = () => {
   )
 }
 
+  // render() {
+  //  return (
+  //     <div className="footerContainer">
+  //       <i className="far fa-comment" onClick={handleClickEvent()} value={this.state.comment} ></i>
+  //       <i className="fas fa-sync"></i>
+  //       <p>6</p>
+  //       <i className="far fa-heart"></i>
+  //       <p>4</p>
+  //       <i className="far fa-envelope"></i>
+  //     </div>
+  //   )
+
+
+  // }
+// }
 export default Footer;
