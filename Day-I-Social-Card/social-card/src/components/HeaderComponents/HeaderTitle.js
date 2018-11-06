3d923b35aac4f8ad52c5032b193e24a49f64807d
ToMMaaY <thuynh629@gmail.com>
2018-11-05 16:26:00 -0800

    Completed the footer

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -4,10 +4,10 @@ import './Header.css';
 function HeaderTitle(props) {
     return (
         <div className='header-bar'>
-            <p class='header-title'>{props.title}</p>
-            <p class='username'>{props.userName}</p>
-            <p class='date-symbol'>.</p>    
-            <p class='header-date'>{props.date}</p>  
+            <p className='header-title'>{props.title}</p>
+            <p className='username'>{props.userName}</p>
+            <p className='date-symbol'>.</p>    
+            <p className='header-date'>{props.date}</p>  
         </div>
     )
 }
