1c1fe55c39d82107bc3dc8a4c8a4f6f4658ac909
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-11-06 16:05:01 -0600

    day II. Lookin better

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Display.css';
 
 function CalculatorDisplay(props){
-   return <div><h3>{props.currentValue}</h3></div>
+   return <div className="display"><h3>{props.currentValue}</h3></div>
 }
 
 export default CalculatorDisplay;
