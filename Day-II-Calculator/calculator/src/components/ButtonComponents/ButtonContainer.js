8ca6fca6b79b0afb674d5449a9aa398203b1ffbd
Trai Lynne <trailynne@outlook.com>
2018-11-06 14:57:42 -0700

    Added code for ButtonContainer Added height and width to container Currently rendering with red background Imported and called in App.js

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+const ButtonContainer = (props) => {
+    return (
+        <div className='button-container'>
+        </div>
+    )
+}
+
+export default ButtonContainer;
