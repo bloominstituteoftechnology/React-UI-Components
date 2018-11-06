92a95d09c19567b5772f7ffe203471af976fa6d5
Wesley Meeks <wesmeeks88@gmail.com>
2018-11-06 10:29:09 -0700

    Initial commit(day 2) created components for calculator buttons.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+import React from "react";
+import "./Button.css";
+
+const ActionButton = props => {
+  return (
+    <button className={`action-button ${props.buttonStyle}`}>
+      {props.text}
+    </button>
+  );
+};
+
+export default ActionButton;
