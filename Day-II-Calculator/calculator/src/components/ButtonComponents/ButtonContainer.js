d748064c52469994b3a56dc8bca0f9c61ab5e810
Trai Lynne <trailynne@outlook.com>
2018-11-06 15:44:22 -0700

    Added CSS reset to index.css file Imported and called in ButtonContainer Currently has a dark gray with white border Flexed containing presentational div

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Button.css';
 
+// Components
+import ClearButton from './ClearButton';
+
 const ButtonContainer = (props) => {
     return (
         <div className='button-container'>
+            <ClearButton text='AC' />
         </div>
     )
 }
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ClearButton = (props) => {
+    return (
+        <div className='clear'>
+            <p>{props.text}</p>
+        </div>
+    );
+}
+
+export default ClearButton;
