86b15ab1602a2c2fc5dda44b302b9106ac7c9b1f
chasefulks <chase.fulks@gmail.com>
2018-11-06 15:31:45 -0600

    display

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+class Display extends React.Component {
+  render() {
+    return (
+      <div className="displayContainer">
+        <div className="displayValue">0</div>
+      </div>
+    );
+  }
+}
+export default Display;
