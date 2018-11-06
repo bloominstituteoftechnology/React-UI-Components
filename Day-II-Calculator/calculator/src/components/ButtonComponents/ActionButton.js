d01409665148e22fbe9f8ef51aa4a38369075cc1
chasefulks <chase.fulks@gmail.com>
2018-11-06 15:27:36 -0600

    Actionbutton

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+import Button from "./Button.js";
+
+class ActionButton extends React.Component {
+  render() {
+    return (
+      <div class="buttonPanelAction">
+        <Button name="÷" red />
+        <Button name="x" red />
+        <Button name="-" red />
+        <Button name="+" red />
+        <Button name="=" red />
+      </div>
+    );
+  }
+}
+
+export default ActionButton;
