b613683450258edaf2674d4e1c143f0d38cebf0d
Lidii <lidiiany92@gmail.com>
2018-11-06 16:59:56 -0500

    Styled the buttons and started to style the operator buttons

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+function ActionButton(props) {
+    return (
+        <div className="action-button-component">
+            <button className={props.classNames}>{props.text}</button>
+        </div>
+    );
+}
+
+export default ActionButton;
