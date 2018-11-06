69bbd48825470ad7f5a0a46e033c45b10c41d57d
cbtsao47 <cbtsao47@gmail.com>
2018-11-06 16:13:26 -0600

    add layout for operators

--- Day-II-Calculator/calculator/src/components/ButtonComponents/OperatorButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/OperatorButton.js
@@ -5,11 +5,11 @@ import './Button.css';
 function OperatorButton (){
     return(
         <div className="calculator__buttons--operator">
-            <div>+</div>
-            <div>-</div>
-            <div>X</div>
-            <div>d</div>
-            <div>=</div>
+            <div className="calculator__button--operator">+</div>
+            <div className="calculator__button--operator">-</div>
+            <div className="calculator__button--operator">×</div>
+            <div className="calculator__button--operator">÷</div>
+            <div className="calculator__button--operator">=</div>
         </div>
     );
 }
