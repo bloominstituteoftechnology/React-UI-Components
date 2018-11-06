1fc9347a2c282fc30ec4ed106ff3f2d44b301626
Holden B <holden.bucher.92@gmail.com>
2018-11-06 16:19:43 -0600

    other changes I made but forgot to save apparently?

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Display.css';
+// import ButtonPad from '../ButtonComponents/NumberPad';
+
+function Display(props) {
+    return (
+        <div className='display'>
+            <p>0</p>
+        </div>
+    )
+}
+
+
+
+export default Display;
