59367008e77d4781b70d0f4beabbff3eb2496fff
Josh Z <jzieger10@gmail.com>
2018-11-06 16:10:24 -0500

    Calculator structure in place with some styling, working on functionality

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import './App.css';
+import './components/ButtonComponents/NumberButton'
+import NumberButton from './components/ButtonComponents/NumberButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
+    <div className="app">
+      <div className="calc">
+        <CalculatorDisplay num="0"/>
+      
+        <div className="button-container">
+          <div className="row">
+            <NumberButton text="&#247;" buttonStyle="operation"/>
+            <NumberButton text="clear" buttonStyle="number wide-button clear"/>
+          </div>
+          <div className="row">
+            <NumberButton text="&times;" buttonStyle="operation"/>
+            <NumberButton text="9" buttonStyle="number"/>
+            <NumberButton text="8" buttonStyle="number"/>
+            <NumberButton text="7" buttonStyle="number"/>
+          </div>
+          <div className="row">
+            <NumberButton text="-" buttonStyle="operation"/>
+            <NumberButton text="6" buttonStyle="number"/>
+            <NumberButton text="5" buttonStyle="number"/>
+            <NumberButton text="4" buttonStyle="number"/>
+          </div>
+          <div className="row">
+            <NumberButton text="+" buttonStyle="operation"/>
+            <NumberButton text="3" buttonStyle="number"/>
+            <NumberButton text="2" buttonStyle="number"/>
+            <NumberButton text="1" buttonStyle="number"/>
+          </div>
+          <div className="row">
+            <NumberButton text="=" buttonStyle="operation"/>
+            <NumberButton text="0" buttonStyle="number wide-button"/>
+            
+          </div>
+        </div>
+      </div>
+    </div>  
   );
 };
 
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Button.css';
+
+// let buttonNumber = () => {
+//     for
+// }
+
+let NumberButton = (props) => {
+    return (
+        <div className={props.buttonStyle}>
+            {props.text}
+        </div>   
+    )
+    
+}
+
+export default NumberButton;
