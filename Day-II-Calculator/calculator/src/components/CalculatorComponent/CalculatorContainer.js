1322c1d292a4af44d708aebb6d62ce72fed62657
wcolts2000 <wcolts2000@gmail.com>
2018-11-06 15:01:30 -0800

    added state to display, savepoint before digging into changing it

--- Day-II-Calculator/calculator/src/components/CalculatorComponent/CalculatorContainer.js
+++ Day-II-Calculator/calculator/src/components/CalculatorComponent/CalculatorContainer.js
@@ -1,41 +1,85 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './CalculatorContainer.css';
 import Display from '../DisplayComponents/CalculatorDisplay';
 import ButtonContainer from "../ButtonComponents/ButtonContainer";
 import NumberButton from '../ButtonComponents/NumberButton';
 import ActionButton from '../ButtonComponents/ActionButton';
 
-// const numbers = ["0","1",'2','3','4','5','6','7','8','9', 'clear'];
-// const actions = ['=','+','-','*','/'];
+// const Calculator = props => {
+//   return (
+//     <div className="calculator">
+//       <Display />
+//       <ButtonContainer containerStyle="btnContainer">
+//         <ButtonContainer containerStyle="numberSide">
+//           <ActionButton buttonStyle="wide numberBtn narrow" text="clear"/>
+//           <NumberButton buttonStyle="numberBtn" text="7"/>
+//           <NumberButton buttonStyle="numberBtn" text="8"/>
+//           <NumberButton buttonStyle="numberBtn" text="9"/>
+//           <NumberButton buttonStyle="numberBtn" text="4"/>
+//           <NumberButton buttonStyle="numberBtn" text="5"/>
+//           <NumberButton buttonStyle="numberBtn" text="6"/>
+//           <NumberButton buttonStyle="numberBtn" text="1"/>
+//           <NumberButton buttonStyle="numberBtn" text="2"/>
+//           <NumberButton buttonStyle="numberBtn" text="3"/>
+//           <ActionButton buttonStyle="wide numberBtn" text="0"/>
+//         </ButtonContainer>
+//         <ButtonContainer containerStyle="operationsSide">
+//           <NumberButton buttonStyle="numberBtn bgRed" text="/"/>
+//           <NumberButton buttonStyle="numberBtn bgRed" text="*"/>
+//           <NumberButton buttonStyle="numberBtn bgRed" text="-"/>
+//           <NumberButton buttonStyle="numberBtn bgRed" text="+"/>
+//           <NumberButton buttonStyle="numberBtn bgRed" text="="/>
+//         </ButtonContainer>
+//       </ButtonContainer>
+//     </div>
+//   )
+// }
 
-const Calculator = props => {
-  return (
-    <div className="calculator">
-      <Display />
-      <ButtonContainer containerStyle="btnContainer">
-        <ButtonContainer containerStyle="numberSide">
-          <ActionButton buttonStyle="wide numberBtn narrow" text="clear"/>
-          <NumberButton buttonStyle="numberBtn" text="7"/>
-          <NumberButton buttonStyle="numberBtn" text="8"/>
-          <NumberButton buttonStyle="numberBtn" text="9"/>
-          <NumberButton buttonStyle="numberBtn" text="4"/>
-          <NumberButton buttonStyle="numberBtn" text="5"/>
-          <NumberButton buttonStyle="numberBtn" text="6"/>
-          <NumberButton buttonStyle="numberBtn" text="1"/>
-          <NumberButton buttonStyle="numberBtn" text="2"/>
-          <NumberButton buttonStyle="numberBtn" text="3"/>
-          <ActionButton buttonStyle="wide numberBtn" text="0"/>
+class Calculator extends Component {
+  state = {
+    calcDisplay: '0',
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      calcDisplay: event.target.value
+    })
+  }
+
+  render() {
+    // if(this.state.calcDisplay !== '') {
+    //   this.state.calcDisplay = 
+    // }
+
+
+    return (
+      <div className="calculator">
+        <Display handleInputChange={this.handleInputChange} calcDisplay={this.state.calcDisplay}/>
+        <ButtonContainer containerStyle="btnContainer">
+          <ButtonContainer containerStyle="numberSide">
+            <ActionButton buttonStyle="wide numberBtn narrow" text="clear"/>
+            <NumberButton buttonStyle="numberBtn" text="7"/>
+            <NumberButton buttonStyle="numberBtn" text="8"/>
+            <NumberButton buttonStyle="numberBtn" text="9"/>
+            <NumberButton buttonStyle="numberBtn" text="4"/>
+            <NumberButton buttonStyle="numberBtn" text="5"/>
+            <NumberButton buttonStyle="numberBtn" text="6"/>
+            <NumberButton buttonStyle="numberBtn" text="1"/>
+            <NumberButton buttonStyle="numberBtn" text="2"/>
+            <NumberButton buttonStyle="numberBtn" text="3"/>
+            <ActionButton buttonStyle="wide numberBtn" text="0"/>
+          </ButtonContainer>
+          <ButtonContainer containerStyle="operationsSide">
+            <NumberButton buttonStyle="numberBtn bgRed" text="/"/>
+            <NumberButton buttonStyle="numberBtn bgRed" text="*"/>
+            <NumberButton buttonStyle="numberBtn bgRed" text="-"/>
+            <NumberButton buttonStyle="numberBtn bgRed" text="+"/>
+            <NumberButton buttonStyle="numberBtn bgRed" text="="/>
+          </ButtonContainer>
         </ButtonContainer>
-        <ButtonContainer containerStyle="operationsSide">
-          <NumberButton buttonStyle="numberBtn bgRed" text="/"/>
-          <NumberButton buttonStyle="numberBtn bgRed" text="*"/>
-          <NumberButton buttonStyle="numberBtn bgRed" text="-"/>
-          <NumberButton buttonStyle="numberBtn bgRed" text="+"/>
-          <NumberButton buttonStyle="numberBtn bgRed" text="="/>
-        </ButtonContainer>
-      </ButtonContainer>
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
 export default Calculator;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Display.css';
 
 const Display = (props) => {
-  return <div className="display" text={props.display}>0</div>
+  return <div className="display" text={props.display} value="props.state" onChange={props.handleInputChange}>{props.calcDisplay}</div>
 }
 
 export default Display;
