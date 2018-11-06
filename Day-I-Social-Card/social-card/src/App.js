cc4c9b8cc665c43547c0e39c5a5b16c08ff9e473
Lidii <lidiiany92@gmail.com>
2018-11-05 20:38:09 -0500

    Added Functionality to the footer buttons

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
-
+import FooterContainer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
      <div className="App">
        <HeaderContainer />
        <CardContainer />
+       <FooterContainer />
      </div>
   );
 };
