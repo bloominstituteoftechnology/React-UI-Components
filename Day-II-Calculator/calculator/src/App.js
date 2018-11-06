7296d465b03a4eb7b99a1e1edf105055cc46ccdd
steelworx <eileencuevas62@gmail.com>
2018-11-06 16:49:23 -0500

    added numbers, operators and styling to calculator app

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -6,10 +6,31 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 
 const App = () => {
   return (
-    <div>
+    <div className='calculator'>
       <CalculatorDisplay text='0' />
-      <NumberButton className='number' text='2' />
-      <ActionButton text='clear' />
+      <div className="calculator-functions">
+        <div className="numbers">
+          <ActionButton text='clear' />
+          <NumberButton buttonStyle='number' text='1' />
+          <NumberButton buttonStyle='number' text='2' />
+          <NumberButton buttonStyle='number' text='3' />
+          <NumberButton buttonStyle='number' text='4' />
+          <NumberButton buttonStyle='number' text='5' />
+          <NumberButton buttonStyle='number' text='6' />
+          <NumberButton buttonStyle='number' text='7' />
+          <NumberButton buttonStyle='number' text='8' />
+          <NumberButton buttonStyle='number' text='9' />
+          <ActionButton text='0' />
+        </div>
+
+        <div className='operators'>
+        <NumberButton buttonStyle='arithmetic' text='&divide;' />
+        <NumberButton buttonStyle='arithmetic' text='&times;' />
+        <NumberButton buttonStyle='arithmetic' text='&minus;' />
+        <NumberButton buttonStyle='arithmetic' text='&#x2b;' />
+        <NumberButton buttonStyle='arithmetic' text='&#x3d;' />
+        </div>
+      </div> 
     </div>
   );
 };
