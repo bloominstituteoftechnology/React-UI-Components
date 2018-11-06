b6c39e94797d04e07355e6ce7603b92b3fd60c64
steelworx <eileencuevas62@gmail.com>
2018-11-06 17:24:47 -0500

    arranged the numbers in Digits.js so that they were the right order

--- Day-II-Calculator/calculator/src/components/ButtonComponents/Digits.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/Digits.js
@@ -3,17 +3,17 @@ import NumberButton from './NumberButton';
 
 function Digits() {
     return(
-        <Fragment>
-            <NumberButton buttonStyle='number' text='1' />
-            <NumberButton buttonStyle='number' text='2' />
-            <NumberButton buttonStyle='number' text='3' />
-            <NumberButton buttonStyle='number' text='4' />
-            <NumberButton buttonStyle='number' text='5' />
-            <NumberButton buttonStyle='number' text='6' />
+        <div className='digits'>
             <NumberButton buttonStyle='number' text='7' />
             <NumberButton buttonStyle='number' text='8' />
             <NumberButton buttonStyle='number' text='9' />
-        </Fragment>
+            <NumberButton buttonStyle='number' text='4' />
+            <NumberButton buttonStyle='number' text='5' />
+            <NumberButton buttonStyle='number' text='6' />
+            <NumberButton buttonStyle='number' text='1' />
+            <NumberButton buttonStyle='number' text='2' />
+            <NumberButton buttonStyle='number' text='3' />
+        </div>
     );
 }
 
