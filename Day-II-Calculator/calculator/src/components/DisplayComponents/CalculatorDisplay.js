4f93265fc28e127df18f3f4adac89145affd5fc1
steelworx <eileencuevas62@gmail.com>
2018-11-06 16:04:00 -0500

    created foundation for calculator buttons/display

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './Display.css';
+
+
+function CalculatorDisplay (props) {
+    return(
+        <Fragment>
+            <p className='display'>{props.text}</p>
+        </Fragment>
+    );
+}
+
+export default CalculatorDisplay;
+
