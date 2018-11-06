35d8f9adcb71eef0263fbd357563c3bbe67f7072
jheinz1999 <jonathan.heinz@aol.com>
2018-11-06 13:35:40 -0800

    finished styling

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.sass';
+
+const ActionButton = ({action}) => {
+
+  return <button className="action">{action}</button>;
+
+}
+
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/ButtonContainer.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/ButtonContainer.js
@@ -2,22 +2,37 @@ import React from 'react';
 import './Display.sass';
 
 import NumberButton from '../ButtonComponents/NumberButton';
+import ActionButton from '../ButtonComponents/ActionButton';
 
 const ButtonContainer = () => {
 
-  const numBtns = [];
+  let numBtns = [7, 8, 9, 4, 5, 6, 1, 2, 3];
 
-  for (let i = 1; i <= 9; i++) {
-
-    numBtns.push(<NumberButton id={i} />);
-
-  }
+  numBtns = numBtns.map(btn => <NumberButton id={btn} />);
 
   return (
 
-    <div className="button-container">
+    <div className="buttons">
 
-      {numBtns}
+      <div className="nums-container">
+
+        <ActionButton action="clear" />
+
+        {numBtns}
+
+        <ActionButton action="0" />
+
+      </div>
+
+      <div className="action-container">
+
+        <ActionButton action="÷" />
+        <ActionButton action="x" />
+        <ActionButton action="-" />
+        <ActionButton action="+" />
+        <ActionButton action="=" />
+
+      </div>
 
     </div>
 
