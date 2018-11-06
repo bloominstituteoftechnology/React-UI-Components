e536ab0de24cb6f26524ca397c23d0752bd94150
Jarrad Miller <jm020801@gmail.com>
2018-11-06 16:46:11 -0700

    calculator

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return <button className = "actionButtonStyles"> {props.name}</button>;
+}
+
+export default ActionButton;
