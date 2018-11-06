a2ba6a97bdfc18fac7a9505964df8d4623a5530b
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-05 23:34:40 -0500

    "wip"

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import HeaderTitle from 'HeaderTitle.js';
+import HeaderTitle from './HeaderTitle.js';
 
 function headerContent(props) {
     return (
