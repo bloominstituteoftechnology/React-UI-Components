94f214af9eaf74cfdd0f0661b8f33c564c1a9838
tryingtokeepup <kl.hawaii@gmail.com>
2018-11-06 17:04:01 -0800

    today was omgggg hellish but i got MVP

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,15 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './Button.css';
+
+
+function ActionButton (props) {
+    return (
+        <Fragment>
+            <button className="actionButton">{props.text}</button>
+    
+        </Fragment>
+    )
+}
+
+
+export default ActionButton;
