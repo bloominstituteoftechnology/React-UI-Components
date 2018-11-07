8a7c5d0aa16f6cb5b4d01bab7d55aecb7ba71a6d
Zayna C <zaynacrick@gmail.com>
2018-11-06 19:15:48 -0500

    1st push

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+
+function NumberButton(props) {
+    return (
+        <div className="buttons">
+        <button>{props.btn.text}</button>
+        </div>
+        // {props.buttonStyle}
+        
+    )
+}
+
+export default NumberButton;
