75bed66195e83e2b74efa1d56040e26d24e86f34
Dennis Orbison <orbison.dennis@gmail.com>
2018-11-06 20:35:17 -0800

    calculator works

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -9,24 +9,55 @@ class App extends React.Component {
     super();
     this.state = {
       total: '0',
+      operator: '',
+      value1: '',
+      value2: '',
     }
 
   }
 
-  handleButton = (event) => {
-    console.log(event.target.textContent)
-    
-    if(event.target.textContent === '=') {
-      this.setState({total: 5})
-    } else {
-      this.setState({total: `${this.state.total} ${event.target.textContent}`})
+  calculate = (event) => {
+    let value;
+    switch(this.state.operator) {
+      case '+':  // if (x === 'value1')
+        value = (Number(this.state.value1) + Number(this.state.total));
+        break;
+      case '-':  // if (x === 'value1')
+        value = (Number(this.state.value1) - Number(this.state.total));
+        break;
+      case '*':  // if (x === 'value1')
+        value = (Number(this.state.value1) * Number(this.state.total));
+        break;
+      case '/':  // if (x === 'value1')
+        value = (Number(this.state.value1) / Number(this.state.total));
+        break;
+      default:
+        console.log('here')
+        break;
     }
+    this.setState({operator: ''});
+    this.setState({total: `${value}`});
+  }
+
+
+  operator = (event) => {
+    this.setState({operator: event.target.textContent});
+
+    if('+-*/'.includes(this.state.total) !== true) this.setState({value1: this.state.total});
+    this.setState({total: event.target.textContent});
+  }
+
+
+  handleButton = (event) => {
+    if(('+-*/'.includes(this.state.total) && '+-*/'.includes(event.target.textContent) !== true) || this.state.total === '0') {
+      this.setState({total: event.target.textContent});
+    } else if(event.target.textContent === '=') this.calculate(event);
+    else if('+-*/'.includes(event.target.textContent)) this.operator(event);
+    else if('0123456789'.includes(event.target.textContent)) this.setState({total: `${this.state.total}${event.target.textContent}`});
 
     if(event.target.textContent === 'clear') {
-      this.setState({total: 0})
+      this.setState({total: '0'})
     }
-    
-
   }
 
   render() {
@@ -36,11 +67,13 @@ class App extends React.Component {
         <CalculatorDisplay className={'display'} data={this.state.total}/>
         
         <div className={'button-components'}>
+
           {[0,7,8,9,4,5,6,1,2,3,'clear'].map( (num) => <NumberButton className={'number-key'} number={num} handlerButton={this.handleButton} /> )}
+
         </div>
   
         <div className={'action-components'}>
-          {['+', '*', '/', '-', '='].map( (action, index) => <ActionButton className={'action-button'} keys={index} action={action} handlerButton={this.handleButton} /> )}
+          {[<span>&#43;</span>, '*', '/', '-', '='].map( (action, index) => <ActionButton className={'action-button'} keys={index} action={action} handlerButton={this.handleButton} /> )}
         </div>
         
       </div>
