4b31b92245d1e020e223f13c3c883cdaee2c04d1
Trai Lynne <trailynne@outlook.com>
2018-11-07 15:33:33 -0700

    Added code to TodoList Functional component Currently renders empty unordered list

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -24,6 +24,39 @@ const operators = [
     }
 ]
 
+const numbers = [
+    {
+        value: 1
+    },
+    {
+        value: 2
+    },
+    {
+        value: 3
+    },
+    {
+        value: 4
+    },
+    {
+        value: 5
+    },
+    {
+        value: 6
+    },
+    {
+        value: 7
+    },
+    {
+        value: 8
+    },
+    {
+        value: 9
+    },
+    {
+        value: 0
+    }
+]
+
 
 const ButtonContainer = props => {
     return (
