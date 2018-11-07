6a6ac94ecfbc2285328074673480bca976b9f939
chasefulks <chase.fulks@gmail.com>
2018-11-07 10:22:11 -0600

    looks right no work

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+function ActionButton(props) {
+  return (
+    <div
+      className={props.className}
+      key={props.keys}
+      onClick={props.handlerButton}
+    >
+      {props.action}
+    </div>
+  );
+}
+export default ActionButton;
