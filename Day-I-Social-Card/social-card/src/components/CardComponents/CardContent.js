b72e068a623ec23bdf49cbbbf4bf33986ba9b334
chrisgins <christiansgina@gmail.com>
2018-11-12 16:59:53 -0700

    social card components

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+  return (
+    <div className="card-content">
+      <h2>Get started with React</h2>
+      <p>
+        React makes it painless to creat interactive UIs. Design simple views
+        for each state in your application.
+      </p>
+      <h3>reactjs.org</h3>
+    </div>
+  );
+};
+
+export default CardContent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = props => {
+    return <img src="https://files.slack.com/files-tmb/T4JUEB3ME-FD45H9GJU-3b84d015de/lambdalogo_360.jpg" alt="lambda Thumbnail" className='header-img'/>;
+  };
+
+  
+  export default ImageThumbnail;
