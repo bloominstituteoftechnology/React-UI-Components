891a98801b2058300b10e840ff7d299a5b587764
Joseph Eastman <josephryan96@gmail.com>
2018-11-13 12:41:25 -0800

    Day-I Social Card MVP

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardBanner = () => {
+  return (
+    <div className='banner-container'>
+      <a href="https://reactjs.org/">
+        <img src="https://ibin.co/3wnC6SgIOJud.png" alt="banner-img" />
+      </a>
+    </div>
+  );
+};
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const ImageThumbnail = () => {
+  return (
+    <div className='thumbnail'>
+      <img src="https://ibin.co/3whrpKSBbZ81.png" alt="logo" className = 'lambda-img'/>
+    </div>
+  );
+};
+
+export default ImageThumbnail;
