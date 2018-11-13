4f9001ac295c7b18e6b43d31cf19921d283db678
chrisgins <christiansgina@gmail.com>
2018-11-12 17:57:50 -0700

    add header thumbnail image

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 
 const ImageThumbnail = props => {
-    return <img src="https://files.slack.com/files-tmb/T4JUEB3ME-FD45H9GJU-3b84d015de/lambdalogo_360.jpg" alt="lambda Thumbnail" className='header-img'/>;
+    return <img src="%HeaderComponents%/ImageThumbnail.png" alt="lambda Thumbnail" className='header-img'/>;
   };
 
   
