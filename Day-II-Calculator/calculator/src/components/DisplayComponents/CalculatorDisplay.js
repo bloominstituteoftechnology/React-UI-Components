9c292b731690873f94d9018efad029ca596144fc
Joseph Eastman <josephryan96@gmail.com>
2018-11-13 14:17:44 -0800

    Day II Calculator JavaScript

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalculatorDisplay = props => {
+  return <p className="calculator-display">props.text</p>;
+};
+
+export default CalculatorDisplay;
