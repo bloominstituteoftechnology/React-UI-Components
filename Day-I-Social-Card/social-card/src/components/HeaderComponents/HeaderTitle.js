728e3313b238c9da27d460bfef515c16b76e1464
1@$@1github <asaofdiamonds@gmail.com>
2018-11-13 10:44:13 -0700

    HeaderTitle.js completed

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from "./HeaderContent";
+import moment from 'moment';
+const time = moment();
+
+const HeaderTitle = () => {
+    return (
+        <React.Fragment>
+            <div className='header-title'>
+                Lambda School
+                <span className='time-stamp'>
+                {time.format('MMM Do ')}
+                </span>
+                <HeaderContent />
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default HeaderTitle;
+
