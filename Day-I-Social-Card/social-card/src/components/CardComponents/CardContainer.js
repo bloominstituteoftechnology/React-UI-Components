938468d880435e5f513e5e787ce6e5307d2e21bc
1@$@1github <asaofdiamonds@gmail.com>
2018-11-13 11:11:40 -0700

    CardContainer.js completed

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -3,3 +3,13 @@ import './Card.css';
 import CardContent from "./CardContent";
 import CardBanner from "./CardBanner";
 
+const CardContainer = () => {
+    return (
+        <div className='card-container'>
+        <CardBanner />
+        <CardContent />
+        </div>
+    );
+}
+
+export default CardContainer;
