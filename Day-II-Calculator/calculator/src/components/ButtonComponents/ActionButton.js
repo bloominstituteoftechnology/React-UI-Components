8c9df431471128dd996337bfd2f12fca198399c8
chrisgins <christiansgina@gmail.com>
2018-11-12 18:35:00 -0700

    calculator - sorry for the lack of commits.

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Button.css';
+
+
+const RedButtons = props =>{
+    
+    return(
+    
+        <div className='btn func' onClick={() => props.clickHandler(props.func)}>
+            {props.func}
+          </div>
+      
+    )
+}
+
+export default RedButtons;
+
