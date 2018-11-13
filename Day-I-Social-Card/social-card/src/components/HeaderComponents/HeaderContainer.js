5cd02d2cb06b31415080730b69113140f534dcf0
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-13 14:15:09 -0500

    header title and header content have been added and rendedered.Still missing are header image and header styling.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+
+
+function HeaderContainer() {
+  return (
+        <div>
+    <React.Fragment>
+      <HeaderTitle title="Lambda School" />
+      <HeaderContent text="Let's learn React by building simple interfaces with components. Don't try to over think it, just keep it simple and have fun. Once you feel comfortable using components, you are well on your way to mastering React!" />
+    </React.Fragment>
+        </div>
+  );
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+
+// const HeaderTitle = () => {
+//     return <h1>Lambda School</h1>;
+// };
+
+function HeaderTitle(props) {
+    return (
+      <React.Fragment>
+        <h1 className="header">{props.title}</h1>
+      </React.Fragment>
+    );
+  }
+
+
+export default HeaderTitle;
+// export default App;
