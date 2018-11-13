daee9a18ab99487c838b9c9316254bbeebae3572
Joseph Eastman <josephryan96@gmail.com>
2018-11-13 15:47:03 -0800

    Day I Footer WIP + JS Fix

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,7 +3,7 @@ import "./Card.css";
 
 const CardContent = () => {
   return (
-    <div class='card-content'>
+    <div className='card-content'>
       <h2>Get Started with React</h2>
       <p>
         React makes it painless to create interactive UIs. Design simple views
