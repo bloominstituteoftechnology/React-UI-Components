642808c3dd013dee4a953e4ee46b2f641fff744a
Nolan Allen <nolan.r.allen@gmail.com>
2018-11-14 15:37:55 -0600

    initial commit

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return (
+        <button className={`button-container ${props.bgColor}`}>
+            {props.text}
+        </button>
+    );
+};
+
+export default NumberButton;
