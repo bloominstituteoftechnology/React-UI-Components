529b0b22063b0108a3fbcd2757660d336d05af11
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-13 23:20:38 -0500

    calculator added Button Container

--- Day-II-Calculator/calculator/src/index.js
+++ Day-II-Calculator/calculator/src/index.js
@@ -4,3 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Remember to capitalize DOM on ReactDOM
+//Remember the comma after the JSX elem and before document.getElementbyID
