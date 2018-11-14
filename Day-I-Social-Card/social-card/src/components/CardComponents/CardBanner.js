1c322ac47b8955ff1f8d40008ae7a2811e5da4ae
Nolan Allen <nolan.r.allen@gmail.com>
2018-11-14 11:57:50 -0600

    second commit

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+        <a className="banner-container-wrapper" href="https://reactjs.org">
+            <img
+                alt="React logo"
+                className="banner-img"
+                scr="https://ibin.co/3wnC6SgIOJud.png"
+            />
+        </a>
+    );
+};
+
+export default CardBanner;
