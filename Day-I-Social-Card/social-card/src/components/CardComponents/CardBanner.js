fe87b38a666b1676d6d24ecb37251fe2d9626cf0
CatsAreMLG <joshuadgon@gmail.com>
2018-11-28 15:29:16 -0600

    Added card container

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,7 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+const CardBanner = () => {
+  return <div className="card__banner" />;
+};
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+const CardContent = () => {
+  return (
+    <div className="card__content">
+      <h1 className="card__title">Get started with React</h1>
+      <p className="card__description">
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application
+      </p>
+      <p className="card__site">
+        <a className="card__site__link" href="https://reactjs.org/">
+          reactjs.org
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default CardContent;
