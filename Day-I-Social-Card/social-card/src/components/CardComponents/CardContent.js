cca266f2a01f278ed9e88ae7ae9bae3d87d0b038
brettmadrid <brettmadrid9@gmail.com>
2018-11-28 11:33:31 -0800

    Added header and central content data

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardContent = props => {
+  return (
+    <div className="card-content">
+      <h4>Get started with React</h4>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application.
+      </p>
+      <a className="react-link" href="reactjs.org">reactjs.org</a>
+    </div>
+  );
+};
+
+export default CardContent;
