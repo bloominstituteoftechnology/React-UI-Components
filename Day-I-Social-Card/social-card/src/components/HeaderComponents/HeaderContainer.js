6e83a99179e2973fa89c1e81df5f4db165517c33
Taylor Belk <taylorbelk@Taylors-MacBook-Air.local>
2018-11-28 17:08:43 -0600

    created components for header and managed to get them all to appear in the React application.

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = props => {
+    return <div class="header-wrapper">
+        <ImageThumbnail />
+        <HeaderTitle />
+        <HeaderContent />
+    </div>
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle= props => {
+    return (
+        <div class = "title">
+            <h3>Lambda School</h3>
+            <p>@LambdaSchool * 26 Jan</p>
+        </div>
+    )
+}
+
+export default HeaderTitle;
