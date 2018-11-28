2509d009dc1038c1207f857e7304b58501ed5aa6
Bill Geist <wcgeist@gmail.com>
2018-11-28 00:15:26 -0500

    setting up headercontainer js

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+
+
+
+
+const HeaderContainer = () => {
+    return (
+        <div className = "header-container">
+            <ImageThumbnail />
+            <HeaderTitle />        
+        </div>
+    )
+  
+}    
+
+export default HeaderContainer;
