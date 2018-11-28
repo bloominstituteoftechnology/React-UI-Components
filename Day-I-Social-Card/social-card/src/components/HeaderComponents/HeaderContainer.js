0ae4accc16e52291b80ad4a423f6ee3de707f3b2
unknown <bj2336@email.vccs.edu>
2018-11-28 09:15:47 -0800

    HeaderContent Started

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail.js';
+import HeaderContent from './HeaderContent.js';
 
 
 const HeaderContainer = props => {
   return (
     <div>
       <ImageThumbnail />
+      <HeaderContent />
     </div>
   );
 }
