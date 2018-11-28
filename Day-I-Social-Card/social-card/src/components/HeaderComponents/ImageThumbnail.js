1e164c572eadcb3541796052f29ea3ed6c4787d1
Bill Geist <wcgeist@gmail.com>
2018-11-28 00:20:57 -0500

    added thumbnail

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+
+    return (
+        <div className = "thumbnail-container">
+        <img alt = "image" className = "thumbnail-img" src = "https://ibin.co/3whrpKSBbZ81.png"/>
+        
+        </div>
+    )
+
+}
+
+export default ImageThumbnail;
