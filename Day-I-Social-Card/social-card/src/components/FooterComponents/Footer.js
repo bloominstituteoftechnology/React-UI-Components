59cc9b1521c9fa474354035a0ab66ec6894579bb
brettmadrid <brettmadrid9@gmail.com>
2018-11-28 13:39:26 -0800

    adding chat popup logic

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,12 +4,41 @@ import "./Footer.css";
 const FooterContent = props => {
     return (
         <div className="footer-container">
-        <i className="material-icons">chat_bubble_outline</i>
-            <i className="material-icons">autorenew</i>
-            <i className="material-icons">favorite_border</i>
-            <i className="material-icons">mail_outline</i>
+        <i className="material-icons chat-bubble">chat_bubble_outline</i>
+            <i className="material-icons retweet">autorenew</i>
+            <i className="material-icons like">favorite_border</i>
+            <i className="material-icons mail">mail_outline</i>
         </div>
     );
 }
+
+const chatBubble = document.querySelector('.chat-bubble');
+const reTweet = document.querySelector('.retweet');
+const Like = document.querySelector('.like');
+const Mail = document.querySelector('.mail');
+
+<div class="chat-popup" id="myForm">
+  <form action="/action_page.php" class="form-container">
+    <h1>Chat</h1>
+
+    <label for="msg"><b>Message</b></label>
+    <textarea placeholder="Type message.." name="msg" required></textarea>
+
+    <button type="submit" class="btn">Send</button>
+    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+  </form>
+</div>
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+chatBubble.addEventListener('click', {
+    openForm();
+});
 
 export default FooterContent;
