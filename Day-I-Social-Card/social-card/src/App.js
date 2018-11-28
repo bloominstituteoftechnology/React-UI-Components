b262ff825b1b78a8ed4b36ef17148c83cc3c932f
Phil Smith <pbsmithtbn@gmail.com>
2018-11-27 23:22:19 -0600

    Switching machines

--- Day-I-Social-Card/social-card/src/App.js
+++ Day-I-Social-Card/social-card/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import HeaderContent from './components/HeaderComponents/HeaderContent';
+import HeaderTitle from './components/HeaderComponents/HeaderTitle';
+import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
 
 const App = () => {
   return (
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = props => {
+  return (
+    <div className="header-title-container">
+      <h1 className="header-title">Lambda School</h1>
+      <p className="header-@">@LambdaSchool</p>
+      <p className="header-time">26 jan</p>
+    </div>
+  )
+}
+
+export default HeaderTitle;
