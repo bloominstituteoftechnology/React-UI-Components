98d57726be211811e1880879066c8c29435b643f
unknown <bj2336@email.vccs.edu>
2018-11-28 09:23:13 -0800

    HeaderContent and HeaderTitle Components Completed

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+  return (
+    <div>
+    <h2>{props.name}</h2>
+    <h3>@{props.name}</h3>
+    <date>{props.date}</date>
+    </div>
+  );
+}
+
+export default HeaderTitle;
