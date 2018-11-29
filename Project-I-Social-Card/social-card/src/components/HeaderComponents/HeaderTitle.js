35aef292350a4affc4537b270018e39bc18336b3
Jason Cook <the.root.user@gmail.com>
2018-11-28 19:48:57 -0500

    Wrote HeaderContainer component/css

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div className="headerTitle">
+      <p className="pName">Lambda School</p>
+      <p className="pHandle">@LambdaSchool</p>
+      <p className="pTimeStamp">‣ Jan 26</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+  return (
+    <div className='lambdaLogo'></div>
+  );
+};
+
+
+export default ImageThumbnail; 
