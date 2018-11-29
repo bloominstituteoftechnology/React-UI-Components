274f3e20cd9fdd07195ed38fd3ab47e888eaa741
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-11-28 21:33:14 -0700

    completed stretch goal of adding footer

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,7 +3,12 @@ import './Footer.css';
 
 const Footer = (props) => {
   return (
-
+    <div className="footer">
+      <div className="footer-icon"><img src="https://i.imgur.com/bDi6wmh.png" /></div>
+      <div className="footer-icon"><img src="https://i.imgur.com/qNI4CTC.png" /><span>6</span></div>
+      <div className="footer-icon"><img src="https://i.imgur.com/CWkoyIk.png" /><span>4</span></div>
+      <div className="footer-icon"><img src="https://i.imgur.com/g45H4kZ.png" /></div>
+    </div>
   )
 }
 
