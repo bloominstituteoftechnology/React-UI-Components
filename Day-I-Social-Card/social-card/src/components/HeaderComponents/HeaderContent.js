a98f6cb5c72f4f462150e1c7d9a0191f65e47290
Bill Geist <wcgeist@gmail.com>
2018-11-28 21:31:46 -0500

    added header title and added header components to app

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Header.css';
 
 const HeaderContent = () => {
-    <p className="header-content">
-        Let's learn React by building simple interfaces with components.
-        Don't try to over think it, just keep it simple and have fun.
-        Once you feel comfortable using components you are well on your way to mastering React!
+
+    return (
+        <p className="header-content">
+            Let's learn React by building simple interfaces with components.
+            Don't try to over think it, just keep it simple and have fun.
+            Once you feel comfortable using components you are well on your way to mastering React!
     </p>
+    )
+
 
 
 }
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,23 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+
+    return (
+        <h1 className="header-title">
+            Lambda School 
+            <span className="header-username">
+                @LambdaSchool 28 nov
+            </span>
+
+        </h1>
+
+
+
+
+    )
+
+}
+
+
+export default HeaderTitle;
