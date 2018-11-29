1fd561cf041d8b58cf1ac55b9aec40dadc642ddc
Andrew Yurlov <yurlovandrew@gmail.com>
2018-11-28 19:07:21 -0800

    added onclick functionality

--- Day-I-Social-Card/social-card/src/index.js
+++ Day-I-Social-Card/social-card/src/index.js
@@ -4,3 +4,10 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const cardClick = () => {
+  window.location.href = "http://www.google.com";
+};
+
+const cardContainer = document.querySelector('.cardContainer');
+cardContainer.addEventListener('click', cardClick);
