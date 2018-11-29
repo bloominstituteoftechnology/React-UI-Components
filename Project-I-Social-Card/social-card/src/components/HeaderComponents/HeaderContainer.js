8dd073d1886be191ec9d4075fc59a9306f48af4e
iradsanchez <ira.d.sanchez@gmail.com>
2018-11-28 19:25:41 -0700

    barebones header added

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ImageThumbnail from './ImageThumbnail';
-// import HeaderTitle from './HeaderTitle';
+import HeaderTitle from './HeaderTitle';
 // import HeaderContent from './HeaderContent';
 import './Header.css';
 
 const HeaderContainer = () => {
-    return (
-      <ImageThumbnail />
-      // <HeaderTitle />
-      // <HeaderContent />
+    return ( 
+        <div className = "header-container" >
+        <ImageThumbnail / >
+        <HeaderTitle />
+        {/*<HeaderContent /> */}
+        </div>
     );
-  };
+};
 
-  export default HeaderContainer;
+export default HeaderContainer;
