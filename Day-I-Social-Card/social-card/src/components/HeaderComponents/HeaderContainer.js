23312935749dbc0a99ce9385a155b6ba37243fab
Andrew Yurlov <yurlovandrew@gmail.com>
2018-11-28 17:43:02 -0800

    created header content layout

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderImage from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = (props) => (
+  <div className="headerContainer">
+    <HeaderImage />
+    <HeaderTitle />
+    <HeaderContent />
+  </div>
+);
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = (props) => (
+  <div className="headerTitle">
+    <h3>Lambda School</h3>
+    <p>@LambdaSchool - 26 Jan</p>
+  </div>
+);
+
+export default HeaderTitle;
