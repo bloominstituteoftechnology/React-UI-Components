9a4c9f61f35e80aaf86cd13429d107f9f17c25b4
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-28 20:28:52 -0500

    ADD HEADER CONTAINER AND COMPONENT

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderComponent = () => {
+    return (
+        <div>
+        <h1>Hello</h1>
+        <HeaderContent />
+        </div>
+    )
+}
+
+export default HeaderComponent;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = () => {
+    return (
+        <div> This HTML file is a template.
+        If you open it directly in the browser, you will see an empty page.</div>
+    )
+}
+
+export default HeaderContent;
