3292d296665e1d19f3f308f7e1767f0bd56f55cf
Andrew <andrewryanogle@gmail.com>
2018-11-28 22:16:18 -0800

    trying to get the imagethumbnail to appear

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import imageThumbnail from './ImageThumbnail.js';
+
+const HeaderContainer = () => {
+    return (
+      <div className = 'header'>
+       <imageThumbnail />
+      </div>
+    );
+  };
+
+  export default HeaderContainer;
