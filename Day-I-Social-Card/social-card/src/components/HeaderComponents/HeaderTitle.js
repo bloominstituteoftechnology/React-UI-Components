7850dfaf749a3e2256f517ccfa50434cd2c7b031
Leila <nb.leila10@gmail.com>
2018-11-28 20:56:03 -0600

    Add time-stamp in header to be today's date

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Header.css';
 
+var moment = require('moment');
+
 const headerTitle = (props) => {
   return (
-    <h4>Lambda School <span className="date">@LambdaSchool . 28 Nov </span></h4>
+    <h4>Lambda School <span className="date">@LambdaSchool . {moment().format("MMM Do")} </span></h4>
   );
 };
 
