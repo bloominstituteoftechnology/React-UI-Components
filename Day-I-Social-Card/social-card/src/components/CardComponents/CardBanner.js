c7fa0f385054a2cb4bedef2fe7cde1fd39b6c657
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-28 21:37:04 -0500

    ADD CARD BANNER CONTENT

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+        <div>
+            <h1>Get started with React</h1>
+            <p>React makes it painless to create interactive UIs.</p><p>Design simple views for each state in your application</p>
+            <br></br>
+            <span>reactj.irg</span>
+        </div>
+    )
+}
+
+export default CardBanner;
