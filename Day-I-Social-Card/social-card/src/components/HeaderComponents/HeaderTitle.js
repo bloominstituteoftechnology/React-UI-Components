ac0c56749418dbd985d6fa3f9249a254b92fd58a
Paul DeYonghe <p.deyonghe@gmail.com>
2018-11-28 20:44:58 -0600

    mvp satisfied

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+	return (
+		<div className="headerInfo">
+			<h1 className="headerItem">Lambda School</h1>
+			<p className="headerItem">@LambdaSchool</p>
+			<p className="headerItem">- Jan 26</p>
+		</div>
+	)
+};
+
+export default HeaderTitle;
