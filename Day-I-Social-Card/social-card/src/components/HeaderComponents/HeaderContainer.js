e2aa1f2b290460540330643ea4a262bf37003690
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-11-28 18:36:09 -0700

    added to HeaderTitle and HeaderContent components

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle'
+
+const HeaderContainer = (props) => {
+  return (
+  <div className="header-container">
+  <p>Testing!!!! </p>
+    <HeaderTitle />
+  </div>
+  )
+}
+
+export default HeaderContainer;
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = (props) => {
+  return (
+    <div className="header-title">
+      <div className="header-title-name">Lambda School</div>
+      <div className="header-title-handle">@LambdaSchool</div>
+      <div className="header-title-date">&middot; 26 jan</div>
+    </div>
+  )
+}
+  
+export default HeaderTitle;
