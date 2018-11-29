f9b7687328e93f564efa7723f80be7f720f05da6
brettmadrid <brettmadrid9@gmail.com>
2018-11-28 17:11:08 -0800

    Completed MVP and added footer icons

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = props => <img src="https://cdn-images-1.medium.com/max/1920/1*ivjksIhvAs7TUXbQCxAU0A.jpeg" width="600" height="297" alt="React Symbol"></img>;
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => <h1 className="header-title">Lambda School <span className="header-p">@LambdaSchool - 27 Nov</span></h1>;
+
+export default HeaderTitle;
