41bdc5fb58750d5e9f18786327c92aa90e17bc53
VonKleistman <VonKleistman@gmail.com>
2018-11-29 01:23:48 -0600

    Fixed minor spelling error

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -8,7 +8,7 @@ const headerContent = (props) => {
     <div className='headerContent'>
       <ImageThumbnail/>
       <div className='textContent'>
-        <div className='userName'><h3 className='screenName'>Lambda School</h3><h4 className='atNameAndDate'> @Lambda School • 26 jan</h4></div>
+        <div className='userName'><h3 className='screenName'>Lambda School</h3><h4 className='atNameAndDate'> @LambdaSchool • 26 jan</h4></div>
         <div className='headerParagraph'><p>Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p></div>
       </div>
     </div>
