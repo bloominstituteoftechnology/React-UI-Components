68c80198271c62082ca71a22fb0d24e13d1e792a
CatsAreMLG <joshuadgon@gmail.com>
2018-11-28 20:48:48 -0600

    Added card click functionality

--- Day-I-Social-Card/social-card/src/index.js
+++ Day-I-Social-Card/social-card/src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+document.querySelector(".card").addEventListener("click", e => {
+  window.location.href = "https://www.reactjs.org";
+});
