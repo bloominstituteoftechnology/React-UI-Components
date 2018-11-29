438b14dfbc9aabb3969c7a0fed786f5bfb8f2255
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-28 23:43:38 -0500

    ADD REACT HREF FIX

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -7,7 +7,7 @@ import CardContent from './CardContent';
 const CardContainer = () => {
     return (
         <div className="card-container">
-            <a href={'/https://www.reactjs.org'}>
+            <a href={'https://www.reactjs.org'}>
             <CardBanner />
             <CardContent />
             </a>
