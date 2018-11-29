257db69324720893ee7486e6cc324cf6c4dd2b83
David Reichardt <davereichardt@gmail.com>
2018-11-28 23:02:40 -0500

    added HeaderComponents, updated App.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent'
+
+const HeaderContainer = () => {
+    return (
+        <div class="headerContainer">
+            <ImageThumbnail />
+            <div class="text">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
+    )
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div class="headerTitle">
+            <h1>Lambda School</h1>
+            <p class="gray">@LambdaSchool</p>
+            <p class="gray"> • 26 Jan</p>
+        </div>
+    )
+};
+
+export default HeaderTitle;
