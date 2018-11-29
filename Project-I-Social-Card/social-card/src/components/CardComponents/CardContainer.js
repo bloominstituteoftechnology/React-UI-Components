88f653012bcb671e270bf67de3a6f3fd48864e21
Jonathan Dorety <kyullog@gmail.com>
2018-11-28 22:39:48 -0500

    Basic card component finished.
    Still need to make whole card clickable to go to reactjs.org.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const CardContainer = props => {
+  return (<div class="card-container">
+    <CardBanner />
+    <CardContent />
+  </div>)
+}
+
+export default CardContainer;
