f25a1666cbee6567f1b48e310854f76bf30303a9
Bao Pham <ryanvuepham@gmail.com>
2018-11-28 23:02:29 -0600

    Still working on MVP

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return (
+      <div className="header-container">
+        <ImageThumbnail />
+        <div className="header-content">
+          <HeaderTitle />
+          <HeaderContent />
+        </div>
+      </div>
+    )
+  }
+  
+  export default HeaderContainer
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+import React from 'react';
+import './Header.css';
+
+const HeaderContent = () => {
+  return (
+    <div>
+      <p>Let's Learn React by building simple interfaces with components. Dont try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
+    </div>
+  )
+}
+
+export default HeaderContent;
