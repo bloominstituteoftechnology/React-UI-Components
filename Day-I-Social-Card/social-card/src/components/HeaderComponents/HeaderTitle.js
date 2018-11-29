f78031254ca18977ce07bc066b7babb35532192a
jrizza88 <jetorres88@gmail.com>
2018-11-28 20:24:15 -0500

    completed react header css styling

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -4,13 +4,13 @@ import HeaderContent from './HeaderContent';
 
 const HeaderTitle = () => {
    return <div className ="header-title">
-        <div className="title">
-            <h1>Lambda School</h1>
-            <h2 className="title-htwo">@LambdaSchool</h2>
-            <h2 className="title-htwo">26 jan</h2>
-        </div>
-        <HeaderContent />
-    </div>
+            <div className="title">
+                <h1>Lambda School</h1>
+                <h2 className="title-htwo">@LambdaSchool</h2>
+                <h2 className="title-htwo">• 26 jan</h2>
+            </div>
+            <HeaderContent />
+         </div>
 };
 
 export default HeaderTitle;
