da14e27a278346cbae713c253aec4de63e1936a2
Bill Geist <wcgeist@gmail.com>
2018-11-28 22:55:44 -0500

    starting footere

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const FooterContainer = () => {
+    return (
+        <div className="footer-container">
+        
+        </div>
+    )
+}
+
+export default FooterContainer;
