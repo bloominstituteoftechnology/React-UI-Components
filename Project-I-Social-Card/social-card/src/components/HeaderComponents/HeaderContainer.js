4ddf150333804c598f2c3205c21e1b60ecf8a49c
Jonathan Dorety <kyullog@gmail.com>
2018-11-28 22:04:56 -0500

    Complete header component
    Header isn't pixel perfect, but looks good enough.  Will tweak later if 
    time is available.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = props => {
+  return (<header>
+    <ImageThumbnail/>
+    <div class="header-info">
+      <HeaderTitle/>
+      <HeaderContent/>
+    </div>
+  </header>)
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+  return (
+    <div>
+    <h1 class="display-name">Lambda School</h1>
+    <h2 class="user-name">@LambdaSchool</h2>
+    <time dateTime="2018-1-26">26 jan</time>
+    </div>
+  )
+}
+
+export default HeaderTitle;
