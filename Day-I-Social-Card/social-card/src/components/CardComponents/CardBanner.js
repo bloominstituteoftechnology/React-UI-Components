5e3d64e681d1989cd1e6ffa6b26106a4336327be
Bill Geist <wcgeist@gmail.com>
2018-11-28 22:52:07 -0500

    card formatting. small right margin that i can't get rid of

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -4,7 +4,7 @@ import './Card.css';
 
 const CardBanner = () => {
 return (
-    <img className = "card-banner" src="https://cdn-images-1.medium.com/max/1000/1*y6C4nSvy2Woe0m7bWEn4BA.png" alt="banner" width="600px"></img>
+    <img className = "card-banner" src="https://cdn-images-1.medium.com/max/1000/1*y6C4nSvy2Woe0m7bWEn4BA.png" alt="banner" />
 )
 }
 
