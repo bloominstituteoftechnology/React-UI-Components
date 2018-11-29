3080278c44239133c42d7d98389e473d449b566a
Ashley Owens <ash4741@gmail.com>
2018-11-29 02:06:29 -0500

    Add Card Components and Finish up all Header Components

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
-    <div className="el-header-container">
-      <HeaderContainer />
+    <div className="content-wrapper">
+      <section className="el-header-container">
+        <HeaderContainer />
+      </section>
+      <section className="el-card-container">
+        <CardContainer />
+      </section>
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -4,7 +4,6 @@ import './Header.css';
 const ImageThumbnail = props => {
     return (
         <img alt="Lambda Logo" src ="https://cdn-images-1.medium.com/fit/c/120/120/1*iTABE417EkZDwRv9Uj91Qg.png"/>
-        // <img src="http://Project-I-Social-Card/social-card/public/favicon.ico"/>
     );
 };
 
