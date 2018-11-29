0f1eed5ef2d3e37d30c0c72995848966901d5ace
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-11-28 20:55:48 -0700

    completed styles for social card

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,7 +4,7 @@ import './Card.css';
 const CardContent = (props) => {
   return (
     <div className='card-content'>
-      <h2>Get started with React</h2>
+      <h4>Get started with React</h4>
       <p>React makes it painless to create interactive UIs. Design simple views for each state in your application</p>
       <a href='https://reactjs.org'>reactjs.org</a>
     </div>
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -7,7 +7,8 @@ const HeaderContent = (props) => {
       <div className="header-title-top">
         <div className="header-title-name">Lambda School</div>
         <div className="header-title-handle">@LambdaSchool</div>
-        <div className="header-title-date">&middot; 26 jan</div>
+        <div className="header-title-separator">&middot;</div>
+        <div className="header-title-date">26 jan</div>
       </div>
       <div className="header-title-desc">
         <p>Let's learn React by building simple interfaces with components. Don't try to overthing it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
