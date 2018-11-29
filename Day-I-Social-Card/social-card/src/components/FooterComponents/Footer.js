30467edb883262e5b7129e2fc3f773bb0e24a93f
dickwinters <coding1965@gmail.com>
2018-11-28 20:34:39 -0600

    Action buttons creation

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <img src="./public/favicon.ico" alt="" />
+    </footer>
+  );
+};
+
+export default Footer;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,40 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButtons = () => {
+  return (
+    <div className="actionButtons">
+      <ClearButton />
+      <DivideButton />
+      <MultiplyButton />
+      <SubtractButton />
+      <AddButton />
+      <EqualsButton />
+    </div>
+  );
+};
+const ClearButton = () => {
+  return <div className="clear">Clear</div>;
+};
+
+const DivideButton = () => {
+  return <div classname="divide">/</div>;
+};
+
+const MultiplyButton = () => {
+  return <div className="multiply">X</div>;
+};
+
+const SubtractButton = () => {
+  return <div className="subtract">-</div>;
+};
+
+const AddButton = () => {
+  return <div className="add">+</div>;
+};
+
+const EqualsButton = () => {
+  return <div className="equals">=</div>;
+};
+
+export default ActionButtons;
