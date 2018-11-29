398d56b7f3b9e612f412ba0d10f6ecdb8e92a18e
CatsAreMLG <joshuadgon@gmail.com>
2018-11-28 20:58:28 -0600

    Added time stamp functionality

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Header.css";
-
+const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let date = new Date();
 const HeaderTitle = () => {
   return (
     <div className="header__name">
       <h1 className="header__title">Lambda School</h1>
       <p className="header__info">
-        @LambdaSchool * <span>Nov 28</span>
+        @LambdaSchool *{" "}
+        <span>
+          {date.getDay()} {monthNames[date.getMonth()]}
+        </span>
       </p>
     </div>
   );
