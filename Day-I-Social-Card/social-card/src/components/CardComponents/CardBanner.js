3e14e753a9a67c332a6bd9e40fab297e6a9938ee
mustafa inaya <moose.inaya@icloud.com>
2018-11-28 19:32:36 -0800

    FINISH, Finished MVP.

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Card.css';
 const CardBanner = () => {
 	return (
-		// <div className="img">
-		<img className="img" alt="whatever" src="./Day-I-social-card/React.png" />
-		// {/* </div> */}
+		<div className="img">
+			<img className="img" alt="whatever" src="https://reactjs.org/logo-og.png" />
+		</div>
 	);
 };
 export default CardBanner;
---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// import './Header.css';
+const Footer = () => {
+	return <div className="footer" />;
+};
+
+export default Footer;
