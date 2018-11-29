207720593218f4c2ebc313c1cb3a99a25e623c39
VonKleistman <VonKleistman@gmail.com>
2018-11-29 01:22:38 -0600

    Completed card content MVP

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+import Jumbo from './img/ReactLogo.png';
+
+const CardBanner = (props) => {
+  return (
+  <img src={Jumbo} className='cardBanner' alt='' />);
+}
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = (props) => {
+  return (
+    <div className='cardContent'>
+      <h3 className='cardContentHeader'>Get started with React</h3>
+      <p className='cardContentParagraph'>React makes it painless to make interactive UIs. Design simple views for each state in your application.</p>
+      <a href='https://reactjs.org/' className='srcLink'>reactjs.org</a>
+    </div>
+  )
+}
+
+export default CardContent;
