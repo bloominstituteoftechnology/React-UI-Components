395c5f33b5e0771f4091c8936d4a0d36474e3ae9
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-11-30 12:44:26 -0700

    cleaned up spacing in js and css files for Calculator project

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -12,37 +12,34 @@ const App = () => {
           <CalculatorDisplay numDisplay="0"/>
         </div>
         <div className="keypad">
-        <div className="left-side">
-          <div className="btn-clear">
-            <ActionButton buttonStyle="btn-rectangle btn-white btn-clear" text="clear" />
+          <div className="left-side">
+            <div className="btn-clear">
+              <ActionButton buttonStyle="btn-rectangle btn-white btn-clear" text="clear" />
+            </div>
+            <div className="btn-nums">
+              <NumberButton buttonStyle="btn-square btn-white btn-number" text="7" />
+              <NumberButton buttonStyle="btn-square btn-white btn-number" text="8" />
+              <NumberButton buttonStyle="btn-square btn-white btn-number" text="9" />
+              <NumberButton buttonStyle="btn-square btn-white btn-number" text="4" />
+              <NumberButton buttonStyle="btn-square btn-white btn-number" text="5" />
+              <NumberButton buttonStyle="btn-square btn-white btn-number" text="6" />
+              <NumberButton buttonStyle="btn-square btn-white btn-number" text="1" />
+              <NumberButton buttonStyle="btn-square btn-white btn-number" text="2" />
+              <NumberButton buttonStyle="btn-square btn-white btn-number" text="3" />
+            </div>
+            <div className="btn-zero">
+              <ActionButton buttonStyle="btn-rectangle btn-number" text="0" />
+            </div>
           </div>
-          <div className="btn-nums">
-            <NumberButton buttonStyle="btn-square btn-white btn-number" text="7" />
-            <NumberButton buttonStyle="btn-square btn-white btn-number" text="8" />
-            <NumberButton buttonStyle="btn-square btn-white btn-number" text="9" />
-            <NumberButton buttonStyle="btn-square btn-white btn-number" text="4" />
-            <NumberButton buttonStyle="btn-square btn-white btn-number" text="5" />
-            <NumberButton buttonStyle="btn-square btn-white btn-number" text="6" />
-            <NumberButton buttonStyle="btn-square btn-white btn-number" text="1" />
-            <NumberButton buttonStyle="btn-square btn-white btn-number" text="2" />
-            <NumberButton buttonStyle="btn-square btn-white btn-number" text="3" />
-          </div>
-          <div className="btn-zero">
-            <ActionButton buttonStyle="btn-rectangle btn-number" text="0" />
+          <div className="right-side">
+            <NumberButton buttonStyle="btn-square btn-red btn-symbol" text="&divide;" />
+            <NumberButton buttonStyle="btn-square btn-red btn-symbol" text="&times;" />
+            <NumberButton buttonStyle="btn-square btn-red btn-symbol" text="-" />
+            <NumberButton buttonStyle="btn-square btn-red btn-symbol" text="+" />
+            <NumberButton buttonStyle="btn-square btn-red btn-symbol" text="=" />
           </div>
         </div>
-        <div className="right-side">
-          <NumberButton buttonStyle="btn-square btn-red btn-symbol" text="&divide;" />
-          <NumberButton buttonStyle="btn-square btn-red btn-symbol" text="&times;" />
-          <NumberButton buttonStyle="btn-square btn-red btn-symbol" text="-" />
-          <NumberButton buttonStyle="btn-square btn-red btn-symbol" text="+" />
-          <NumberButton buttonStyle="btn-square btn-red btn-symbol" text="=" />
-        </div>
-        </div>
-        
       </div>
-      
-        
     </div>
   );
 };
