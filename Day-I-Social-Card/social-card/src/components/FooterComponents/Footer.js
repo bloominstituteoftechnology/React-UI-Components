4ad0fbeb218c26ad0ab5b31a5becd7dd0273f14f
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2018-11-29 22:34:20 -0600

    Removed boders for testing, still needs styling and Card Container click

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,8 +1,8 @@
-import React from 'react';
+/*import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
   return (<h1>Footer Test</h1>);
 };
 
-export default Footer;
+export default Footer;*/
