37f6babe6e47a7d956d83926df8313f2a39d0bb2
Andrew <andrewryanogle@gmail.com>
2018-11-29 16:46:02 -0800

    finished building the header react componets

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail.js';
+import HeaderTitle from './HeaderTitle.js';
 
 const HeaderContainer = () => {
     return (
       <div className = 'header'>
        <ImageThumbnail />
+       <HeaderTitle />
       </div>
     );
   };
--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <React.Fragment>
+            <h1>Lambda School</h1>
+            <p>@LambdaSchool</p>
+            <ul>
+                <li>26 jan</li>
+            </ul>
+        </React.Fragment>
+    );
+};
+
+export default HeaderTitle;
