7ff70ded844e87deb5934b0f4be922602f7424b0
Jason Cook <the.root.user@gmail.com>
2018-12-01 13:38:36 -0500

    Fix typo
    Somehow my component was still rendering even though I was only
    importing 'Display' instead of 'CalculatorDisplay'

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Display from './components/DisplayComponents/CalculatorDisplay';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 
 const App = () => {
   return (
     <div className = 'container'>
-    <Display />
+    <CalculatorDisplay />
     </div>
   );
 };
