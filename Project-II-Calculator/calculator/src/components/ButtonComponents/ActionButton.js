79a14a9e901362d443ccca355403de8206d2d554
Ashley Owens <ash4741@gmail.com>
2018-12-01 02:40:41 -0500

    Complete Calculator Project

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+  return (
+    <button className={`num-btn ${props.buttonStyle}`}>{props.type}</button>
+  );
+};
+
+
+export default ActionButton;
