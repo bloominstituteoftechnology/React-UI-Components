936677d3e7ae25ccbf5e917f273b5731fb7f882c
Noah <noah.stoeckel@gmail.com>
2018-12-01 14:04:33 -0500

    Add Number Button Component
    - Add number button component
    - Layout calculator

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.css';
 import  CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div className="calculator-container" >
       <CalculatorDisplay />
       <ActionButton text="clear" buttonStyle="btn-action"/>
+      <NumberButton text="&divide;" buttonStyle="btn-operation"/>
+      <NumberButton text="7" buttonStyle="btn-number"/>
+      <NumberButton text="8" buttonStyle="btn-number"/>
+      <NumberButton text="9" buttonStyle="btn-number"/>
+      <NumberButton text="&times;" buttonStyle="btn-operation"/>
+      <NumberButton text="4" buttonStyle="btn-number"/>
+      <NumberButton text="5" buttonStyle="btn-number"/>
+      <NumberButton text="6" buttonStyle="btn-number"/>
+      <NumberButton text="-" buttonStyle="btn-operation"/>
+      <NumberButton text="1" buttonStyle="btn-number"/>
+      <NumberButton text="2" buttonStyle="btn-number"/>
+      <NumberButton text="3" buttonStyle="btn-number"/>
+      <NumberButton text="+" buttonStyle="btn-operation"/>
+      <ActionButton text="0" buttonStyle="btn-action-number"/>
+      <NumberButton text="=" buttonStyle="btn-operation"/>
     </div>
   );
 };
