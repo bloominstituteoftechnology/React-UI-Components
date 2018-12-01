6123c50f2809e060ae3bea6cd02177d02126a031
Bill Geist <wcgeist@gmail.com>
2018-12-01 06:50:07 -0500

    added action button

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
-
+import ActionButton from './components/ButtonComponents/ActionButton';
+// import DisplayButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div className="calculator-container">
-      {/* <div className="calculator-display-block"> */}
-        <CalculatorDisplay />
-      {/* </div> */}
-    </div>
-  );
-};
+      <CalculatorDisplay />
 
-export default App;
+      <div className="row" id="row-one">
+        <ActionButton name="clear" />
+        {/* <NumberButton name="&#038;" extraStyle="redButton"> */}
+      </div>
+
+
+
+      </div>
+      );
+    };
+    
+    export default App;
