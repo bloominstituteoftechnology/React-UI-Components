580e253d40293c5a50457a58263c1261a68e300b
CatsAreMLG <joshuadgon@gmail.com>
2018-12-01 00:54:26 -0600

    Added Calculator display and container for calc

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,6 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalculatorDisplay = _ => <div className="display">0</div>;
+
+export default CalculatorDisplay;
