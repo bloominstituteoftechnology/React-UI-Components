c051c1ff8392a489d8036c13f62dadf87fcfd5f0
Noah <noah.stoeckel@gmail.com>
2018-12-01 13:06:12 -0500

    Set Up Environment
    -Set up environment
    -Add CalculatorDisplay component

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const  CalculatorDisplay = props=>{
+    return(
+        <div className="display-container">
+            <p className="display-text">0</p>
+        </div>
+    )
+}
+
+export default CalculatorDisplay;
