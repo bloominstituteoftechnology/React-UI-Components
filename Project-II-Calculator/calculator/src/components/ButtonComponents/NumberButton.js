cda5a4dcf567b0470e7f97d89746456a747a546a
Daniel Lim <danheelim@gmail.com>
2018-12-01 11:10:38 -0800

    added the numbers and colors

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+
+const NumberButton = props => {
+	return (
+		<button className={`btn-styles ${props.extraStyles}`}>{props.text}</button>
+	)
+};
+
+export default NumberButton;
