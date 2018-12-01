2c66a76be7d58a81b4470a6ac5999bee671ab6b8
Noah <noah.stoeckel@gmail.com>
2018-12-01 13:45:26 -0500

    Add ActionButton Component

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import  CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
     <div className="calculator-container" >
       <CalculatorDisplay />
+      <ActionButton text="clear" buttonStyle="btn-action"/>
     </div>
   );
 };
