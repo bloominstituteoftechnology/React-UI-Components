69df1a50e1fbd777a24b79d6666a3be0a6cc8272
Bill Geist <wcgeist@gmail.com>
2018-12-01 06:05:22 -0500

    added calculator display

--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => <input type ="text" id="display" placeholder= {props.placeholder} />
+
+CalculatorDisplay.defaultProps = {
+    placeholder: 0
+}
+
+
+export default CalculatorDisplay;
