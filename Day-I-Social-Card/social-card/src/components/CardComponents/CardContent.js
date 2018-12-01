3330359320dca7f49e09a38b094fddeedb478837
Andrew <andrewryanogle@gmail.com>
2018-12-01 11:54:56 -0800

    finished with day one

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className="cardContent">
+            <h1 className="ccHeader">Get started with React</h1>
+            <p className="ccParagraph">React makes it painless to create interactive UIs.
+             Design simple views for each state in your application</p>
+             <a href = "www.reactjs.org">reactjs.org</a>
+        </div>
+    );
+}
+
+export default CardContent;
