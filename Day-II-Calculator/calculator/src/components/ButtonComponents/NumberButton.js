403a6d6db98ff4ae116ac54d23f5a215c83333ef
Andrew Yurlov <yurlovandrew@gmail.com>
2018-12-01 10:05:34 -0800

    created num button component and css

--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const NumButton = props => {
+  return (
+    <button className={`btn-styles ${props.spec}`}>{props.text}</button>
+  )
+};
+
+export default NumButton;
