1f0f90c3336bfb7793ac53b9918837a47cb28b76
Andrew Yurlov <yurlovandrew@gmail.com>
2018-12-01 10:05:53 -0800

    finished calculator

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import './App.css';
+import Display from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="wrap">
+      <Display />
+      <div className="row1">
+        <ActionButton text="clear" />
+        <NumButton text="/" spec="red" />
+      </div>
+      <div className="row2">
+        <NumButton text="7" />
+        <NumButton text="8" />
+        <NumButton text="9" />
+        <NumButton text="X" spec="red" />
+      </div>
+      <div className="row3">
+        <NumButton text="4" />
+        <NumButton text="5" />
+        <NumButton text="6" />
+        <NumButton text="--" spec="red" />
+      </div>
+      <div className="row4">
+        <NumButton text="1" />
+        <NumButton text="2" />
+        <NumButton text="3" />
+        <NumButton text="+" spec="red" />
+      </div>
+      <div className="row5">
+        <ActionButton text="0" />
+        <NumButton text="=" spec="red" />
+      </div>
     </div>
   );
 };
