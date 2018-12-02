2bd3561b2ca8c22f00b9fe5207e81154c552ed16
James Wigdor <jpwigdor@gmail.com>
2018-12-02 16:32:05 -0500

    Add basic CSS and design
    Now i'm creating the actual button layout

--- Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (props) => {
+  return(
+      <button className={`btn-default ${props.buttonStyle}`}>
+          <p>{props.text}</p>
+      </button>
+  );
+};
+
+export default ActionButton;
--- Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Day-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = (props) => {
+  return (
+    <button className={`btn-default ${props.buttonStyle}`}>
+      <p>{props.text}</p>
+    </button>
+  );
+};
+
+
+export default NumberButton;
