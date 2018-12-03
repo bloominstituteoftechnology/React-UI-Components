8aaf2ca0ab41663889b31567515c464c45bb79b4
Lndubose <lndubose13@gmail.com>
2018-12-02 21:55:35 -0600

    Add functions and display state
    --Did the functions in a different application and converting it over to github

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -3,13 +3,86 @@ import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import Buttons from './components/ButtonComponents/Buttons';
 
-const App = () => {
-  return (
-    <div className="calculator">
-      <CalculatorDisplay text="0" />
-      <Buttons />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = {
+    display: '0',
+    equation: {
+      numbers: [],
+      sign: [],
+    },
+  };
+
+  numberClick = event => {
+    // console.log(event.target.innerText);
+    if (this.state.display === '0') {
+      this.setState({ display: event.target.innerText });
+      return;
+    }
+
+    this.setState({ display: this.state.display + event.target.innerText });
+  };
+
+  operator = event => {
+    this.setState({
+      ...this.state,
+      display: '0',
+      equation: {
+        numbers: [...this.state.equation.numbers, this.state.display],
+        sign: [...this.state.equation.sign, event.target.innerText],
+      },
+    });
+  };
+
+  equal = () => {
+    let numbers = this.state.equation.numbers.map(number => Number(number));
+    numbers = [...numbers, Number(this.state.display)];
+    const signs = this.state.equation.sign;
+    let total = numbers[0];
+    for (let i = 1; i < numbers.length + 1; i++) {
+      if (signs[0] === '+') {
+        total += numbers[i];
+        signs.shift();
+      } else if (signs[0] === '-') {
+        total -= numbers[i];
+        signs.shift();
+      } else if (signs[0] === 'X') {
+        total *= numbers[i];
+        signs.shift();
+      } else if (signs[0] === '/') {
+        total /= numbers[i];
+        signs.shift();
+      }
+    }
+
+    total = total.toString();
+    this.setState({
+      ...this.state,
+      display: total,
+      equation: {
+        numbers: [],
+        sign: [],
+      },
+    });
+  };
+
+  clear = () => {
+    this.setState({
+      display: '0',
+      equation: {
+        numbers: [],
+        sign: [],
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className="calculator">
+        <CalculatorDisplay display={this.state.display} />
+        <Buttons />
+      </div>
+    );
+  }
+}
 
 export default App;
--- Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Day-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../../index.css';
 
-
-const CalculatorDisplay = (props) => {
-  return(
+const CalculatorDisplay = props => {
+  return (
     <div>
-      <h1 className={`display ${props.buttonStyle}`} >{props.text}</h1>
+      <h1 className={`display ${props.buttonStyle}`}>{props.display}</h1>
     </div>
-  ); 
-}
+  );
+};
 
 export default CalculatorDisplay;
