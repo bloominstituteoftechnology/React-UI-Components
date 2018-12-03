b879f00f15bb3b11a4fa2f20f636a3cb386a9de0
karlie0 <kamabrapa88@gmail.com>
2018-12-03 17:07:07 -0500

    Finished project I Social Card

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 import './Card.css';
+
+const CardContainer = () => {
+    return (
+        <a href="https://www.reactjs.org" className="link">
+            <div className="cardContainer">
+                <CardBanner />
+                <CardContent />
+            </div>
+        </a>  
+    )
+}
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,22 @@
 import React from 'react';
+import HeaderTitle from './HeaderTitle'
+import ImageThumbnail from './ImageThumbnail'
+import HeaderContent from './HeaderContent'
 import './Header.css';
+
+//Create a <HeaderContainer /> container component that will hold your header components.
+
+const HeaderContainer = props => {
+    return (
+        <div className="header">
+            <ImageThumbnail />
+            <div className="headerText">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+            
+        </div>
+    )   
+}
+
+export default HeaderContainer;
