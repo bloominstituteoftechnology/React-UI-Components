412dde55ef242af83acb7442e01167bab12c11c9
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2018-12-04 19:15:19 -0800

    working on css and finished header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+
+
+const HeaderTitle = () => (
+  <div class="header-content">
+    <div class="header-title">
+      <h2>
+        Lambda School <span>@LambdaSchool * 26 Jan</span>
+      </h2>
+    </div>
+  </div>
+);
+
+export default HeaderTitle;
