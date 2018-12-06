c308921674ff3f2b12a2824e83a3f8bf19cbb4c5
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2018-12-05 19:37:01 -0800

    finished header and card components and added to apps. js

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css'; 
-
+import CardContainer from './components/CardComponents/CardContainer';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 
 const App = () => (
   <div class = 'container-app'>
     <HeaderContainer />
-   
+    <CardContainer />
   </div>
 );
 
