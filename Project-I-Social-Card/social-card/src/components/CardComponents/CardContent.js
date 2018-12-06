37982729d24f1241aed2960d994b0147794e4129
ltann <leeroyleeroy18@gmail.com>
2018-12-05 22:55:17 -0800

    finished MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className="card_content">
+            <h1>Get started with React</h1>
+            <p>React makes it painless to create UIs. Design simple views for each state in your application.</p>
+            <p class="grey">reactjs.org</p>
+        </div>
+    );
+};
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header_title">
+            <p><span class="bold">Lambda School</span> @Lambda School . Jan 26</p>
+        </div>
+    );
+  };
+
+  export default HeaderTitle;
