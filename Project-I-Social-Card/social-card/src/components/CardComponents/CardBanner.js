d5c7337d997571eb8212089dfa8d34d3abf009c5
Tevvin <Tevvin7395@icloud.com>
2018-12-06 00:26:49 -0500

    Finished components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+        <div className='middle-jumbo'>
+
+        </div>
+    )
+}
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+
+// Name The Component
+const CardContent = () => {
+    return (
+        <div>
+            <h2>get started with React</h2>
+            <p>
+                React makes it painless to create interactive UIs. Design simple views for <br />
+                each state in your application.
+            </p>
+        </div>
+    )
+}
+
+// export The Default
+export default CardContent;
