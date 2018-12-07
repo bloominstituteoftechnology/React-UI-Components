508537b474cc6e6c0465ca80bb560ea899160123
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2018-12-07 12:19:41 -0800

    display button component done

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = props => (
+    <div>
+      <h1 className={props.displayStyle}>{props.text}</h1>
+    </div>
+  );
+  
+  export default CalculatorDisplay;
