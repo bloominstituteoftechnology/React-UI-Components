d9fcad0c8ce157ae9f5b4567fbdc85b77470df51
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2018-12-07 16:27:49 -0800

    finished apps js and css

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import NumberButton from "./components/ButtonComponents/NumberButton";
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+const App = () => (
+  <div class="calculator">
+    <div class="display">
+      <CalculatorDisplay text="0" displayStyle="style" />
     </div>
-  );
-};
+    <div class="board">
+      <div class="numbers">
+        <ActionButton text="clear" buttonStyle="long" />
+        <NumberButton text="9" buttonStyle="num" />
+        <NumberButton text="8" buttonStyle="num" />
+        <NumberButton text="7" buttonStyle="num" />
+        <NumberButton text="6" buttonStyle="num" />
+        <NumberButton text="5" buttonStyle="num" />
+        <NumberButton text="4" buttonStyle="num" />
+        <NumberButton text="3" buttonStyle="num" />
+        <NumberButton text="2" buttonStyle="num" />
+        <NumberButton text="1" buttonStyle="num" />
+        <ActionButton text="0" buttonStyle="long" />
+      </div>
+      <div class="operators">
+        <NumberButton text="+" buttonStyle="operator" />
+        <NumberButton text="-" buttonStyle="operator" />
+        <NumberButton text="×" buttonStyle="operator" />
+        <NumberButton text="÷" buttonStyle="operator" />
+        <NumberButton text="=" buttonStyle="operator" />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
