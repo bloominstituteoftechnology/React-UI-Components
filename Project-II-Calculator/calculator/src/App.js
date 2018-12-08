e097531588777c2944c7f979d053708e483bd86d
Andrew <acarmstrong98@gmail.com>
2018-12-08 14:16:19 -0500

    finished app.js

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -19,6 +19,44 @@ const App = () => {
           inside of this file in order to make them work.
         </strong>
       </p>
+
+      <div className="calculator-container">
+        <CalculatorDisplay />
+
+
+        <div className= "row-one">
+          <ActionButton name = "clear" />
+          <NumberButton name = "&#247;" redButton = "red-button" />
+        </div>
+
+        <div className="row-two">
+          <NumberButton name = "7"/>
+          <NumberButton name = "8"/>
+          <NumberButton name = "9"/>
+          <NumberButton name = "x" redButton = "red-button" />
+        </div>
+
+        <div className="row-three">
+          <NumberButton name = "4"/>
+          <NumberButton name = "5"/>
+          <NumberButton name = "6"/>
+          <NumberButton name = "-" redButton = "red-button" />
+        </div>
+
+        <div className="row-four">
+          <NumberButton name = "1"/>
+          <NumberButton name = "2"/>
+          <NumberButton name = "3"/>
+          <NumberButton name = "+" redButton = "red-button" />
+        </div>
+
+        <div className= "row-five">
+          <ActionButton name = "0" />
+          <NumberButton name = "=" redButton = "red-button" />
+        </div>
+
+
+      </div>
     </div>
   );
 };
