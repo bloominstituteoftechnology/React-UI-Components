20044fda625f383b05d33c09ba500ac416f570ec
derekjones-io <42226382+derekjones-io@users.noreply.github.com>
2018-12-08 10:46:27 -0500

    Finish MVP for Social Card project

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+const HeaderContainer = () => {
+  return (
+    <div id="header-container">
+      <ImageThumbnail />
+      <div class="header-body">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+  return <img src="https://ibin.co/3whrpKSBbZ81.png" alt="lambda thumbnail" />;
+};
+export default ImageThumbnail;
