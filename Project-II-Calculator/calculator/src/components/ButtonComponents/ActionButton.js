f0a9a09fd5aa8600e1fc24544ea71507c4ed59dd
Aquariax <43307745+Aquariax@users.noreply.github.com>
2018-12-08 15:56:25 -0500

    added items

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+
+    return (
+        <button className="btn three-quarter-button">{props.name}</button>
+    )
+
+}
+
+ActionButton.defaultProps = {
+    name: "default"
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Display.css';
+const CalculatorDisplay = props => <input type="text" id="calculator-display" placeholder={props.placeholder} />
+ CalculatorDisplay.defaultProps = {
+    placeholder: 0
+}
+ export default CalculatorDisplay; 
