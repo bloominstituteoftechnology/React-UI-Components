7f628e5c4a563f337306242b64fc7ff4a745d149
Tevvin <Tevvin7395@icloud.com>
2018-12-08 20:07:37 -0500

    Finished Project II - React Calculator

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Display.css';
+
+// Name CalculatorDisplay
+const CalculatorDisplay = props => {
+    return (
+        <div>
+            <h1>{props.text}</h1>
+        </div>
+    )
+}
+
+// Export the deafult
+export default CalculatorDisplay;
