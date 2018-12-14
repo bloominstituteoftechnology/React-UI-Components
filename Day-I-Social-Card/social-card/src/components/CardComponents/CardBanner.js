f726aa9fbf40c30a9238ea85a95addf823c16021
J A <jarata@gmail.com>
2018-12-14 08:58:34 -0800

    card built

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+  return (
+    <div>
+      <img
+        src="https://ibin.co/3wnC6SgIOJud.png"
+        alt="cardBanner" />
+    </div>
+  )
+}
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+const CardContainer = () => {
+  return (
+    <div>
+      <CardBanner />
+      <CardContent />
+    </div>
+  )
+}
+
+export default CardContainer;
