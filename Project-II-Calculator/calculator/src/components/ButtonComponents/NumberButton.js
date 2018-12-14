4a005d570559e5527da7256df4afc8b5f0e1896a
Pwolfe39 <peterwolfe35@gmail.com>
2018-12-13 22:32:31 -0800

    Added the calculator mock up

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,32 @@
 import React from 'react';
 import './Button.css';
+
+
+const NumberButton = () => {
+    return (
+        <div className="">
+          
+          <div className="calc-clear-btn calc-clear-text">clear</div>
+          <div className="numbers">
+            <div className="top-numbers">
+                <div className="button-border-top">7</div>
+                <div className="button-border-top">8</div>
+                <div className="button-border-top">9</div>
+            </div>
+            <div className="top-numbers">
+                <div className="button-border-mid">4</div>
+                <div className="button-border-mid">5</div>
+                <div className="button-border-mid">6</div>
+            </div>
+            <div className="top-numbers">
+                <div className="button-border-lower">1</div>
+                <div className="button-border-lower">2</div>
+                <div className="button-border-lower">3</div>
+            </div> 
+          </div>
+          <div className="bottom-number-zero">0</div>
+        </div>
+    );
+}; 
+
+export default NumberButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = () => {
+    return (
+        <div className="calculator-container">
+          <p className="display-number">0</p>
+        </div>
+    );
+};
+
+export default CalculatorDisplay;
