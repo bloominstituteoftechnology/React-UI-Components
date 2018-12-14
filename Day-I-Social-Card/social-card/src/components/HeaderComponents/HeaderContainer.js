ff32d63bd21c274f2ccc3c4d947a87d0f44bb701
J A <jarata@gmail.com>
2018-12-14 09:05:46 -0800

    display

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,13 +3,15 @@ import './Header.css';
 
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
-import HeaderContent from '/HeaderContent';
+import HeaderContent from './HeaderContent';
 
 const HeaderContainer = () => {
   return (
-    <ImageThumbnail />
-    <HeaderTitle />
-    <HeaderContent />
+    <div>
+      <ImageThumbnail />
+      <HeaderTitle />
+      <HeaderContent />
+    </div>
   )
 }
 
