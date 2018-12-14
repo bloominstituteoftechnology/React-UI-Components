f26e4c32dabf519e30c3ffe76331f82c75672626
J A <jarata@gmail.com>
2018-12-14 11:41:08 -0800

    button display

--- Day-II-Calculator/calculator/src/App.js
+++ Day-II-Calculator/calculator/src/App.js
@@ -5,12 +5,41 @@ import ActionButton from './components/ButtonComponents/ActionButton'
 import NumberButton from './components/ButtonComponents/NumberButton'
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 
-const App = () => {
-  return (
-    <CalculatorDisplay />
-  )
+class App extends React.Component {
+  render () {
+    return(
+      <div className="calculator">
+        <CalculatorDisplay />
+        <div>
+          <ActionButton text="clear" />
+          <NumberButton text="/" />
+        </div>
+        <div>
+          <NumberButton text="7" />
+          <NumberButton text="8" />
+          <NumberButton text="9" />
+          <NumberButton text="*" />
+        </div>
+        <div>
+          <NumberButton text="4" />
+          <NumberButton text="5" />
+          <NumberButton text="6" />
+          <NumberButton text="-" />
+        </div>
+        <div>
+          <NumberButton text="1" />
+          <NumberButton text="2" />
+          <NumberButton text="3" />
+          <NumberButton text="+" />
+        </div>
+        <div>
+          <ActionButton text="0" />
+          <NumberButton text="=" />
+        </div>
+      </div>
+    )
+  }
 }
-
 // const App = () => {
 //   return (
 //     <div>
