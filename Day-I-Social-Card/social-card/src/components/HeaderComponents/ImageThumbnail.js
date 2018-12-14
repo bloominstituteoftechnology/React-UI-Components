5a7b2336262b1769940bc7f1104b488802a1046a
J A <jarata@gmail.com>
2018-12-14 08:52:41 -0800

    header built

--- Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Day-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+  return (
+    <div>
+      <img
+        src="https://ibin.co/3whrpKSBbZ81.png"
+        alt="imageThumbnail" />
+    </div>
+  )
+}
+
+export default ImageThumbnail;
