c2002e41f977a07665062b038eb5c1a633120e89
J A <jarata@gmail.com>
2018-12-14 09:01:22 -0800

    footer layout

---
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <img />
+      <img />
+      <img />
+      <img />
+    </div>
+  )
+}
+export default Footer;
