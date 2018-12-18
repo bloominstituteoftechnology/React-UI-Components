bdf7fa2dbd8e0089b97d1b63679d789cd6cf3b85
Nathan Thomas <nwthomas@me.com>
2018-12-18 14:17:24 -0800

    Refactor App.js to class component

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -6,14 +6,16 @@ import "./App.css";
 
 const numbers = [9, 8, 7, 6, 5, 4, 3, 2, 1];
 
-const App = _ => {
-  return (
-    <div className="container">
-      <CalculatorDisplay />
-      <ButtonContainer numbers={numbers} />
-      <OperatorButtons />
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <CalculatorDisplay />
+        <ButtonContainer numbers={numbers} />
+        <OperatorButtons />
+      </div>
+    );
+  }
+}
 
 export default App;
