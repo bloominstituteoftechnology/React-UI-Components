9ecb773477d7be7d021c2789f916a0a017c738a8
BrannanC <brannanconrad@gmail.com>
2018-12-18 17:14:53 -0600

    Project I Header and Card components made

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return <div><h1>Card Title</h1><p>Card Paragraph</p></div>
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = () => {
+    return <p>Header Content</p>
+}
+
+export default HeaderContent;
