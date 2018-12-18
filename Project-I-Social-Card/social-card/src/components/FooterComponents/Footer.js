0530edaccfaf29a4006e442c282c509a1f380c84
cecil john tantay <cjbtantay@gmail.com>
2018-12-18 00:40:56 -0800

    added footer functionality

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -6,25 +6,49 @@ import email from "./envelope-regular.svg";
 import sync from "./sync-solid.svg";
 
 
-const Footer = () => {
-  return (
-    <div className="footer">
-      <div className="comment">
-        <img src={commentIcon} alt="" />
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shareCount: 0,
+      likeCount: 0
+    }
+
+  }
+
+  addShare = ()=> {
+    this.setState(prevState => {
+      return {shareCount: prevState.shareCount + 1}
+   })
+  }
+
+  addlike = ()=> {
+    this.setState(prevState => {
+      return {likeCount: prevState.likeCount + 1}
+   })
+  }
+
+  render() {
+    return (
+      <div className="footer">
+        <div className="comment">
+          <img src={commentIcon} alt="" />
+        </div>
+        <div className="share" onClick={this.addShare}>
+          <img src={sync} alt="" />
+          <p className="share-num" >{this.state.shareCount}</p>
+        </div>
+        <div className="like" onClick={this.addlike}>
+          <img src={heartIcon} alt="" />
+          <p className="like-num">{this.state.likeCount}</p>
+        </div>
+        <div className="email">
+          <img src={email} alt="" />
+        </div>
       </div>
-      <div className="share">
-        <img src={sync} alt="" />
-        <p className="share-num">6</p>
-      </div>
-      <div className="like">
-        <img src={heartIcon} alt="" />
-        <p className="like-num">4</p>
-      </div>
-      <div className="email">
-        <img src={email} alt="" />
-      </div>
-    </div>
-  );
+    )
+  } 
+  
 };
 
 export default Footer;
