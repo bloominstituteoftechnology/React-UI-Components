709c61d08d2d8c27c2c87fd14edcc279bd953d54
Olivia Osborn <olioz95@gmail.com>
2018-12-18 12:45:41 -0500

    filled in header title

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(props) {
+    return (
+        <React.Fragment>
+            <h2 className="header-title">{props.title}</h2>
+            <p className="handle">{props.handle}</p>
+            <p className="date">{props.date}</p>
+        </React.Fragment>
+    );
+}
