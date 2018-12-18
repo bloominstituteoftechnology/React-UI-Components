0b8cc8701fa18ea89012945f48e5e227a0c93afd
Nathan Thomas <nwthomas@me.com>
2018-12-18 12:52:40 -0800

    Refactor NumberButton component to accept props and class

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,11 +3,13 @@ import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay"
 import ButtonContainer from "./components/ButtonComponents/ButtonContainer";
 import "./App.css";
 
-const App = () => {
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+const App = _ => {
   return (
     <div className="container">
       <CalculatorDisplay />
-      <ButtonContainer />
+      <ButtonContainer numbers={numbers} />
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -1,31 +1,13 @@
 import React from "react";
+import NumberButton from "./NumberButton";
 import "./Button.css";
 
-const ButtonContainer = () => {
+const ButtonContainer = props => {
   return (
     <div className="btn__container">
-      <div className="number__btns__container">
-        <div className="btn clear">clear</div>
-        <div className="number__btns">
-          <div className="btn one">1</div>
-          <div className="btn two">2</div>
-          <div className="btn three">3</div>
-          <div className="btn four">4</div>
-          <div className="btn five">5</div>
-          <div className="btn six">6</div>
-          <div className="btn seven">7</div>
-          <div className="btn eight">8</div>
-          <div className="btn nine">9</div>
-        </div>
-        <div className="btn zero">0</div>
-      </div>
-      <div className="action__btns__container">
-        <div className="btn divide">÷</div>
-        <div className="btn multiply">x</div>
-        <div className="btn subtract">-</div>
-        <div className="btn add">+</div>
-        <div className="btn equal">=</div>
-      </div>
+      {props.numbers.map(number => {
+        return <NumberButton className={"btn"} text={number} />;
+      })}
     </div>
   );
 };
