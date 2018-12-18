d565ec96b02c6599c69ccc661f7eb74c9dd45c21
Olivia Osborn <olioz95@gmail.com>
2018-12-18 13:58:48 -0500

    got all elements for card component to appear

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from "./CardBanner";
+import CardContent from "./CardContent";
 
 function CardContainer() {
     return (
         <React.Fragment>
             <CardBanner/>
+            <CardContent title="Get started with React"/>
+            <CardContent text="React makes it painless to create interactive UIs. Design simple views for each state in your application."/>
+            <CardContent linkName="reactjs.org"/>
         </React.Fragment>
     )
 }
+
 
 export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent(props) {
+    return (
+        <React.Fragment>
+            <h2>{props.title}</h2>
+            <p>{props.text}</p>
+            <p className="gray-text">{props.linkName}</p>
+
+        </React.Fragment>
+    )
+}
+
+export default CardContent;
