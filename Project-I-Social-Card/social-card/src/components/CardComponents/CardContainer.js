2692f27b13563868940364ac3f1e4e4070b2f9cc
Moonkaman <nickbatmanglidj@gmail.com>
2018-12-18 20:00:30 -0700

    React structure laid out

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Card.css';
+import HeaderContainer from '../HeaderComponents/HeaderContainer';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const CardContainer = _ => {
+  return (
+    <div className='card-container'>
+      <HeaderContainer />
+      <CardBanner imgSrc='https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png' />
+      <CardContent bannerTitle='Get started with React' 
+      bannerContent='React makes it painless to create interacive UIs. Design simple views for each state in your application.'  
+      bannerLink='reactjs.org' 
+      bannerLinkHref='https://reactjs.org/' />
+    </div>
+  )
+}
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+  return (
+  <div>
+    <h3>{props.bannerTitle}</h3>
+    <p>{props.bannerContent}</p>
+    <a href={props.bannerLinkHref}>{props.bannerLink}</a>
+  </div>
+  )
+}
+
+export default CardContent;
