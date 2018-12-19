194a63aaa04fb94557a8a6b1e2c52c1c1e168910
Jaouad Sarouti <jadsarouti@outlook.com>
2018-12-19 02:46:49 -0500

    Header complete|needs some smalls 'fixes' size to match the design file

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from "./HeaderContent";
+
+const HeaderContainer =()=>{
+    return (
+        <div>
+            <HeaderContent/>
+        </div>
+    )
+}
+
+
+export default HeaderContainer;
