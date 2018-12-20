0b41f5f54d17414abcf8dafbb101f90da51d99d0
Daniel Weinman <namniewd@gmail.com>
2018-12-20 15:44:32 -0500

    completed project 1 social card

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent.js';
+
+const HeaderTitle = props => {
+    return (
+        <div className="header-content">
+            <h2>Lambda School</h2>
+            <h3>@LambdaSchool</h3>
+            <h3>&middot;</h3>
+            <h3>20 dec</h3>
+            <HeaderContent />
+        </div>
+    );
+};
+
+export default HeaderTitle;
