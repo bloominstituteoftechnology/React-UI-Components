01ab59fa96bd99aa97a3707d9e4fcd7038ef691c
Daniel Weinman <namniewd@gmail.com>
2018-12-22 18:44:26 -0500

    added calculator display

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const Display = props => {
+    return (
+        <div className="calc-display">
+            <h1>0</h1>
+        </div>
+    )
+}
+
+export default Display;
