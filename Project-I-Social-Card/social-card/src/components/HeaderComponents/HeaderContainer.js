829f95d31a9d4539984660d6f4e0c5d9201a0ee1
cacheUp <bradb26@gmail.com>
2018-12-26 22:58:11 +0300

    bradley ball

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import HeaderContent from "./HeaderContent";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+
+const HeaderContainer = props => {
+  return (
+    <div className="header">
+      <ImageThumbnail />
+      <div className="headerContent">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderContainer;
