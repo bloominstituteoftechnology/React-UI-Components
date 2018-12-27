902391c59286e8ed3148092895c9d6d487e28336
Aman <amsingh714@gmail.com>
2018-12-27 14:21:27 -0500

    fixed previous incorrect logic with liking and retweeting

--- Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Day-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,21 +1,55 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => {
+class Footer extends React.Component {
+
+    state = {
+        likes: 3,
+        retweets: 4,
+        liked: false,
+        retweeted: false
+    }
+
+    handleAddLike = () => {
+        let numOfLikes = this.state.likes;
+
+        this.state.liked === false ? numOfLikes += 1 : numOfLikes -= 1;
+
+        this.setState({
+            likes: numOfLikes,
+            liked: !this.state.liked
+        })
+    }
+
+    handleAddRetweet = () => {
+        let numOfRetweets = this.state.retweets;
+
+        this.state.retweeted === false ? numOfRetweets += 1 : numOfRetweets -= 1;
+
+        this.setState({
+            retweets: numOfRetweets,
+            retweeted: !this.state.retweeted
+        })
+
+    }
+    
+
+    render() {
     return (
         <div className='FooterContainer'>
             <i className="fa fa-comment-o" aria-hidden="true"></i>
             <div className="retweet">
-                <i className="fa fa-retweet" aria-hidden="true"></i>
-                <p className='retweetNumber'>0</p>
+                <i className={`fa fa-retweet ${this.state.retweeted}`} aria-hidden="true" onClick={this.handleAddRetweet}></i>
+                <p className='retweetNumber'>{this.state.retweets}</p>
             </div>
             <div className="like">
-                <i className="fa fa-heart-o" aria-hidden="true"></i>
-                <p className='likeNumber'>0</p>
+                <i className={`fas fa-heart ${this.state.liked}`} aria-hidden="true" onClick={this.handleAddLike}></i>
+                <p className="likeNumber">{this.state.likes}</p>
             </div>
             <i className="fa fa-envelope-o" aria-hidden="true"></i>
         </div>
     );
+    }
 };
 
 export default Footer;
