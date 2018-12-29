44cf4b31ab0bfa9a3d98f468310b886def72e032
Angel Torres <alas4224@gmail.com>
2018-12-29 17:20:04 -0600

    finished header components still working on card components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+import Banner from './CardBanner';
+
+const CardContainer = () => {
+    return (
+        <Banner />
+    )
+};
+
+export default CardContainer;
