cfa136645454ad2e0590a4a3aad3733b9c571435
Michael Landers <landers.mike@gmail.com>
2019-01-05 18:58:18 -0800

    Button styling

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Button from './Button'
 import './Button.css';
 
-const numbers = [1,2,3,4,5,6,7,8,9,0]
+const numbers = [1,2,3,4,5,6,7,8,9]
 const numberArr = numbers.map(x => <Button item = {x}/>)
 
 function NumberButton(){
     return(
         <div className="numbers">
             {numberArr}
-        </div>
-       
+            <Button item = '0'/>
+        </div>       
     );
 }
 
