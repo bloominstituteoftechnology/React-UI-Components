748e6bfbd28d911fadf5750e735474da0909f70d
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-07 17:33:15 -0500

    Built and imported Footer

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer.js';
 
 const App = () => {
   return (
     <div className='app'>
       <HeaderContainer />
       <CardContainer />
+      <Footer />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,5 +2,25 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-    
+    return (
+        <div className="footer">
+            <div className='social-action'>
+                <i class="far fa-comment"></i>
+                <span className='social-number'></span>
+            </div>
+            <div className='social-action'>
+                <i class="fas fa-retweet"></i>
+                <span className='social-number'>6</span>
+            </div>
+            <div className='social-action'>
+                <i class="far fa-heart"></i>
+                <span className='social-number'>4</span>
+            </div>
+            <div className='social-action'>
+                <i class="far fa-envelope"></i>
+            </div>
+        </div>
+    );
 }
+
+export default Footer;
