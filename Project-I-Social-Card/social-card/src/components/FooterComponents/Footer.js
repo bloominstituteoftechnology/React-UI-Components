e857641c3a3807abea4b68913358471d4bf7a2fe
Matt Basile <matthewbasile2@gmail.com>
2019-01-07 18:21:35 -0500

    Added footer component to design

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -6,7 +6,13 @@ import './Footer.css';
 
 const Footer = () =>{
     return(
-        <div className="footer">
+        <div className="footer-container flex">
+        <div className="footer flex">
+            <i class="far fa-comment"></i>
+            <i class="fas fa-sync"> 6</i>
+            <i class="far fa-heart"> 4</i>
+            <i class="far fa-envelope"></i>
+        </div>
         </div>
     )
 }
