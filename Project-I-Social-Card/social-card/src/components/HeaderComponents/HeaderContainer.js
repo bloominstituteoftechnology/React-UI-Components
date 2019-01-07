b0197095fffd717873e3c114012d45a9c2e313e2
spinon187 <spinon187@gmail.com>
2019-01-07 17:34:55 -0600

    React I unfinished, trying to commit yet yet again

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
-import './Header.css';
+import  './Header.css';
+import {ImageThumbnail} from './ImageThumbnail';
+import {HeaderTitle} from './HeaderTitle';
+import {HeaderContent} from './HeaderContent';
+
+export const HeaderContainer = () => <header>
+    <ImageThumbnail />
+    <div className = 'headerText'>
+        <HeaderTitle />
+        <HeaderContent />
+    </div>
+</header>;
