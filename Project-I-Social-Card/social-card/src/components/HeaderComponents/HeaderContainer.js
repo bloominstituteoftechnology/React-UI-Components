d3fbc3479e8304934b8283aa3d45989bf6ffea73
rmejia-tajc <rmejia.tajc@gmail.com>
2019-01-07 15:03:16 -0700

    populate the HeaderComponents .js files

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+
+
+
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => (
+    <div>
+        <ImageThumbnail />
+        <HeaderContent />
+    </div>
+)
+
+
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => <h2>Lambda School</h2>;
+
+export default HeaderTitle;
