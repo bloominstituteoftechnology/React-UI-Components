3e3ea1315cc17b500d851ab80d64e099e315d7d5
EJaona <ejaona@gmail.com>
2019-01-07 16:27:35 -0600

    Completed mvp and stretch

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <div className="footer">
+    <i class="far fa-comment fa-lg" />
+    <i class="fas fa-sync-alt fa-lg">6</i>
+    <i class="far fa-heart fa-lg">4</i>
+    <i class="far fa-envelope fa-lg" />
+  </div>
+);
+
+export default Footer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+import "./Header.css";
+
+const HeaderContainer = () => (
+  <div className="header-container">
+    <ImageThumbnail />
+    <div className="title-content">
+      <HeaderTitle />
+      <HeaderContent />
+    </div>
+  </div>
+);
+
+export default HeaderContainer;
