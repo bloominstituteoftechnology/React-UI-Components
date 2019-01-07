42dc2f55e6dec6275d5d41a5314fd2446576531b
ills5771 <ilyayelizarov1@gmail.com>
2019-01-07 15:26:57 -0800

    styled header and card components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -6,6 +6,7 @@ export const ImageThumbnail = () => {
     <img
       src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"
       alt="lambdalogo"
+      className="lambda-logo"
     />
   );
 };
