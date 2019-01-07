301e9f2eee3707d795c538276d3af3f567c53c5f
Mike Landers <landers.mike@gmail.com>
2019-01-07 14:27:56 -0800

    Attempt at increasing like value when clicked. value does up up in console just not in the DOM

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './Footer.css';
+let value = 5
+
+function heartValue(){
+    value ++;
+    console.log(value)
+    console.log('The link was clicked.');
+}
 
 function Footer (){
     return(
         <div className = 'footerIcons'>
             <i className="far fa-comment"></i>
             <i className="fas fa-sync"> 6</i>
-            <i className="far fa-heart"> 4</i>
+            <i className="far fa-heart" onClick={heartValue}> {value}</i>
             <i className="far fa-envelope"></i>
         </div>
     )
 }
+
+
+
+
+
 
 export default Footer
