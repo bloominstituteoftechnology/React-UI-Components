248dd40b5c0b99bdf6f73eb867aa79845c0b4685
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-01-07 16:10:53 -0600

    working on header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return (
+        <div>
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />
+
+        </div>
+    );
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <React.Fragment>
+            <h2>Lambda School <span>@LambdaSchool</span> - 7 Jan</h2>
+        </React.Fragment>
+    );
+}
+
+export default HeaderTitle;
