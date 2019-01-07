703cbe3c084cfdf632debf9f88f3b86967f6a341
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-07 15:30:45 -0700

    Setup fontawesome and Icons needed

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,6 +4,7 @@ import HeaderContainer from '../HeaderComponents/HeaderContainer';
 import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 import lambdaImg from '../../imgs/lambda-logo.jpg';
+import Footer from '../FooterComponents/Footer';
 
 const CardContainer = _ => {
   return (
@@ -17,6 +18,7 @@ const CardContainer = _ => {
           bannerContent='React makes it painless to create interacive UIs. Design simple views for each state in your application.'  
           bannerLink='reactjs.org' 
           bannerLinkHref='https://reactjs.org/' />
+          <Footer />
         </div>
       </a>
     </div>
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../../../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import './Footer.css'
+
+
+const Footer = _ => (
+  <div className="footer">
+    <i class="far fa-comment"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="far fa-heart"></i>
+    <i class="far fa-envelope"></i>
+  </div>
+)
+
+export default Footer;
