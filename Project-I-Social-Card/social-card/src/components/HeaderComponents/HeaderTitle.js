b51fa9e316608d55729ccfb6fd45a55c50e44877
Scott Vojik <scott.vojik@gmail.com>
2019-01-07 12:56:51 -0800

    completed header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+  return (
+    <div className="titleContainer">
+      <h1>Lambda School</h1>
+      <p>@Lambda School - 2 Jan</p>
+    </div>
+    
+  )
+}
+
+export default HeaderTitle
