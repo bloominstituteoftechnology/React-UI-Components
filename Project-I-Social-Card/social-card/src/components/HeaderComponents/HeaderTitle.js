e1ed06d143cf5ce2039436057d2501734006d729
bhumip214 <bhumip214@gmail.com>
2019-01-07 12:42:15 -0800

    Created HeaderTitle component and imported and exported all header components into HeaderContainer.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import Timestamp from "react-timestamp";
+
+export function HeaderTitle() {
+  return (
+    <div>
+      <h1> Lambda School </h1>
+      <h3> @LambdaSchool </h3>
+      <Timestamp> 'time' </Timestamp>
+    </div>
+  );
+}
