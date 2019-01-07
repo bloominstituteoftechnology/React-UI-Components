b847472c666731ebe9a57604db1049cb798e23b9
ills5771 <ilyayelizarov1@gmail.com>
2019-01-07 14:05:40 -0800

    Added header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import { ImageThumbnail } from "./ImageThumbnail";
+import { HeaderTitle } from "./HeaderTitle";
+import { HeaderContent } from "./HeaderContent";
+
+export const HeaderContainer = () => {
+  return (
+    <div className="header-container">
+      <ImageThumbnail />
+      <HeaderTitle />
+      <HeaderContent />
+    </div>
+  );
+};
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,10 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+export const HeaderTitle = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="header-title">Lambda School @LambdaSchool - 26 Jan</div>
+    </div>
+  );
+};
