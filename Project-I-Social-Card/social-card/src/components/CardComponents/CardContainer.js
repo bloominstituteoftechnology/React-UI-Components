da57c6eb22e54b07fd7e5a6d04e30f8d14d89f35
samjeffcoat <samjeffcoat@gmail.com>
2019-01-07 15:12:50 -0600

    Set up the project and linked containers to App.js

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,3 @@
 import React from 'react';
 import './Card.css';
+export const CardContainer = () => <h1>Is this working?</h1>
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,4 @@
 import React from 'react';
 import './Header.css';
+
+export const HeaderContainer = () => <h1>Is this working?</h1>
