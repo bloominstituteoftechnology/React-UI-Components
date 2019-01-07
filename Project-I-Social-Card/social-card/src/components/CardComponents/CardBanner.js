a6854d71123355142a451d36c132d899e2678c1e
Samsoucy <newf930@outlook.com>
2019-01-07 17:15:49 -0500

    added card components. Sub heading and par with link to reactjs

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner= ()=>{
+    return(
+        <React.Fragment>
+            <img src ="ttps://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" alt="React Logo"/>
+        </React.Fragment>
+    );
+}
+
+export default CardBanner;
