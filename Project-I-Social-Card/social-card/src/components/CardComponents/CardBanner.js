41ebc5037e952cc00355914a904f2ce8eb18f0aa
spinon187 <spinon187@gmail.com>
2019-01-07 17:54:16 -0600

    React UI Components, no CSS

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import './Card.css';
 
-export const CardBanner = () => <img src=
+export const CardBanner = () => <img src = './reactbackground.png' alt = 'React Logo' />;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Card.css';
+import {CardBanner} from './CardBanner';
+import {CardContent} from './CardContent';
+
+export const CardContainer = () => <div>
+        <CardBanner />
+        <CardContent />
+    </div>;
