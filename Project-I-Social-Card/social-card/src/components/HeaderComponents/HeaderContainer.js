e7778f8e01725bae4273e2817b8d7da8dd12bedd
bhumip214 <bhumip214@gmail.com>
2019-01-07 12:21:20 -0800

    Created ImageThumbnail component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import { ImageThumbnail } from "./ImageThumbnail";
+
+function HeaderContainer() {
+  return (
+    <div className="Header-container">
+      <ImageThumbnail />
+    </div>
+  );
+}
+
+export default HeaderContainer;
