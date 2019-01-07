badd648c607d21599a225f8f7ef04651373f047b
MichaelNey <ney.quentin.michael@gmail.com>
2019-01-07 13:13:11 -0800

    Created SocialCard component that implements all of the individual Social Card components into one.

---
+++ Project-I-Social-Card/social-card/src/components/SocialCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import HeaderContainer from './HeaderComponents/HeaderContainer';
+import CardContainer from './CardComponents/CardContainer';
+
+const SocialCard = () => (
+    <div className="SocialCard">
+        <HeaderContainer />
+        <CardContainer />
+    </div>
+);
+
+export default SocialCard;
