30d9ae988439d58f8a0b797f6e2118b069c57404
MichaelNey <ney.quentin.michael@gmail.com>
2019-01-07 12:37:58 -0800

    Project setup, and header components created.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContainer = () => (
+    <div className="HeaderContainer">
+
+    </div>
+)
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => (
+    <div className="HeaderTitle">
+        <strong>Lambda School</strong><span>@LambdaSchool</span><span>26 jan</span>
+    </div>
+)
+
+export default HeaderTitle;
