978ba3dac2f4beea0713adcfa51b5300f144479e
Nathan Thomas <nwthomas@me.com>
2019-01-07 15:15:01 -0800

    Make share and like footer components increment and decrement

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -4,14 +4,67 @@ import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
 import CardContainer from "./components/CardComponents/CardContainer";
 import Footer from "./components/FooterComponents/Footer";
 
-const App = () => {
-  return (
-    <div className="social__card__wrapper">
-      <HeaderContainer />
-      <CardContainer />
-      <Footer />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shareClicked: false,
+      likeClicked: false,
+      shareCount: 6,
+      likeCount: 4
+    };
+  }
+
+  setLocalStorage = () => {
+    // localStorage
+  };
+
+  shareClicked = event => {
+    if (this.state.shareClicked === false) {
+      const tempNum = this.state.shareCount + 1;
+      this.setState({
+        shareClicked: true,
+        shareCount: tempNum
+      });
+    } else {
+      const tempNum = this.state.shareCount - 1;
+      this.setState({
+        shareClicked: false,
+        shareCount: tempNum
+      });
+    }
+  };
+
+  likeClicked = event => {
+    if (this.state.likeClicked === false) {
+      const tempNum = this.state.likeCount + 1;
+      this.setState({
+        likeClicked: true,
+        likeCount: tempNum
+      });
+    } else {
+      const tempNum = this.state.likeCount - 1;
+      this.setState({
+        likeClicked: false,
+        likeCount: tempNum
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="social__card__wrapper">
+        <HeaderContainer />
+        <CardContainer />
+        <Footer
+          shareCount={this.state.shareCount}
+          likeCount={this.state.likeCount}
+          shareClicked={this.shareClicked}
+          likeClicked={this.likeClicked}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,20 +5,24 @@ import heartIcon from "../../images/heart-icon.svg";
 import envelopeIcon from "../../images/envelope-icon.svg";
 import "./Footer.css";
 
-const Footer = () => {
+const Footer = props => {
   return (
     <div className="footer__container">
       <div className="comment__icon icon">
         <img src={commentIcon} alt="Comment icon" />
       </div>
-      <div className="share__icon icon">
+      <div
+        onClick={props.shareClicked}
+        name="share"
+        className="share__icon icon"
+      >
         <img src={shareIcon} alt="Share icon" />
       </div>
-      <p className="share__count">6</p>
-      <div className="heart__icon icon">
+      <p className="share__count">{props.shareCount}</p>
+      <div onClick={props.likeClicked} name="like" className="heart__icon icon">
         <img src={heartIcon} alt="Heart icon" />
       </div>
-      <p className="like__count">4</p>
+      <p className="like__count">{props.likeCount}</p>
       <div className="envelope__icon icon">
         <img src={envelopeIcon} alt="Envelope icon" />
       </div>
