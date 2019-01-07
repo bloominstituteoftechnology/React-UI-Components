96bb166b8876dee68be8f372f2da4a9b6f48dadf
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-07 16:16:52 -0600

    completed the MVP and created the card and header components with CSS

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -6,8 +6,10 @@ import CardContent from './CardContent';
 const CardContainer = () => {
     return (
         <a href="https://www.reactjs.org" target="blank">
-            <CardBanner/>
-            <CardContent/>
+            <div className ="card-container">
+                <CardBanner/>
+                <CardContent/>
+            </div>
         </a>
     );
 };
