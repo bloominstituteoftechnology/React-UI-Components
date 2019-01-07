c9ce66a952a42ccf281ae079b96a820f1893e570
matt0418 <mfeldma7@gmail.com>
2019-01-07 18:38:48 -0500

    Completed the mvp for the project for today

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () => {
+    return (
+        <div id = "bottom-text">
+            <h5>Get Started with React</h5>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <h6>reactjs.org</h6>
+        </div>
+    )
+}
+
+export default CardContent
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className = "head-info">
+            <h4>Lambda School</h4>
+            <p>@LambdaSchool  •  7 Jan 2019</p>
+        </div>
+    )
+}
+
+
+
+export default HeaderTitle;
