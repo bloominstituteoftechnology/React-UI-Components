7ef45901be53292a6ab046a7127c3c2941f3afc1
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-07 15:59:26 -0500

    Created and connected ImageThumbnail to HeaderContainer to App

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return (
+        <div className="header-container">
+            <ImageThumbnail />
+        </div>
+    )
+}
+
+export default HeaderContainer;
