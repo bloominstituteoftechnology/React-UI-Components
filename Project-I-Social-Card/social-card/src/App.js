c24c8201fa3e47a4009ebf8b89376619b6d59f6c
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-01-07 15:16:32 -0600

    Working on HeaderComponents

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Social Card!!!</h3>
+      {/* <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
         few files that we've already included in there to get you started right
@@ -16,7 +16,7 @@ const App = () => {
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
-      </p>
+      </p> */}
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+      <div>
+        <h1>Lambda School @lambdaschool 26 jan</h1>
+      </div>
+    );
+  };
+  
+  export default HeaderTitle;
+
