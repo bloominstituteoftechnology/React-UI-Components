dc3ebb5b8d39e4e2ccddd62347bdf1b3991a531d
MichaelNey <ney.quentin.michael@gmail.com>
2019-01-07 14:48:26 -0800

    SocialCard component made into a Class to hold the repost/like counts as state, and Footer component made into a functional component that gets the repost and like counts as props, as well as the repost and like methods as props to be used for onClick event listeners.

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,44 +1,21 @@
 import React, { Component } from 'react';
 import './Footer.css';
 
-class Footer extends Component {
-    constructor() {
-        super();
-        this.state = {
-            retweets: 0,
-            likes: 0
-        }
-    }
-
-    render() {
-        return(
-            <div className="Footer">
-                <div className="social-button hover-blue">
-                    <i className="far fa-comment"></i>
-                </div>
-                <div className="social-button hover-green" onClick={(e) => {
-                    let newCount = this.state.retweets + 1;
-                    this.setState({
-                        retweets: newCount
-                    });
-                }}>
-                    <i className="fas fa-sync"></i>  {this.state.retweets}
-                </div>
-                <div className="social-button hover-pink" onClick={(e) => {
-                    let newCount = this.state.likes + 1;
-                    this.setState({
-                        likes: newCount
-                    });
-                }}>
-                    <i className="far fa-heart"></i>  {this.state.likes}
-                </div>
-                <div className="social-button hover-orange">
-                    <i className="far fa-envelope"></i>
-                </div>
-            </div>
-        )
-    }
-    
-}
+const Footer = (props) => (
+    <div className="Footer">
+        <div className="social-button hover-blue">
+            <i className="far fa-comment"></i>
+        </div>
+        <div className="social-button hover-green" onClick={props.retweet}>
+            <i className="fas fa-sync"></i>  {props.retweets}
+        </div>
+        <div className="social-button hover-pink" onClick={props.like}>
+            <i className="far fa-heart"></i>  {props.likes}
+        </div>
+        <div className="social-button hover-orange">
+            <i className="far fa-envelope"></i>
+        </div>
+    </div>
+)
 
 export default Footer;
--- Project-I-Social-Card/social-card/src/components/SocialCard.js
+++ Project-I-Social-Card/social-card/src/components/SocialCard.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import HeaderContainer from './HeaderComponents/HeaderContainer';
 import CardContainer from './CardComponents/CardContainer';
 import Footer from './FooterComponents/Footer';
-const SocialCard = () => (
-    <div className="SocialCard">
-        <HeaderContainer />
-        <CardContainer />
-        <Footer />
-    </div>
-);
+
+class SocialCard extends Component {
+    constructor() {
+        super();
+
+        this.retweet = this.retweet.bind(this);
+        this.like = this.like.bind(this);
+
+        this.state = {
+            retweets: 0,
+            likes: 0
+        }
+    }
+
+    retweet() {
+        this.setState({
+            retweets: this.state.retweets + 1
+        });
+    }
+
+    like() {
+        this.setState({
+            likes: this.state.likes + 1
+        });
+    }
+    
+    render() {
+        return(
+            <div className="SocialCard">
+                <HeaderContainer />
+                <CardContainer />
+                <Footer retweet={this.retweet} 
+                        like={this.like} 
+                        retweets={this.state.retweets} 
+                        likes={this.state.likes} />
+            </div>
+        )
+    }
+
+}
 
 export default SocialCard;
