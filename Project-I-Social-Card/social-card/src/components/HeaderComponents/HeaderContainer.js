413d25297354e35f4c4c36a5f93f36cb552ce538
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-07 14:39:42 -0600

    Init commit. started working on completing the header component for the social card

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const Headercontainer = () => {
+    return (
+        <h1>Header content goes here</h1>
+    );
+};
+
+export default Headercontainer;
