ce9a9455217665bfbe4168044c1003e2cdf7b6f3
ills5771 <ilyayelizarov1@gmail.com>
2019-01-07 14:31:03 -0800

    Added Card components

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 
 import { HeaderContainer } from "./components/HeaderComponents/HeaderContainer";
+import { CardContainer } from "./components/CardComponents/CardContainer";
 import { HeaderContent } from "./components/HeaderComponents/HeaderContent";
 import { ImageThumbnail } from "./components/HeaderComponents/ImageThumbnail";
 
 const App = () => {
   return (
-    <div>
+    <div className="main-container">
       <HeaderContainer />
+      <CardContainer />
     </div>
     //  <div>
     //   <h3>Welcome to React Social Card!</h3>
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+export const CardContent = () => {
+  return (
+    <div className="banner-text">
+      <h1 className="banner-heading">Get Started with React</h1>
+      <p className="text-section">
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application
+      </p>
+      <p className="react-url"> reactjs.org</p>
+    </div>
+  );
+};
