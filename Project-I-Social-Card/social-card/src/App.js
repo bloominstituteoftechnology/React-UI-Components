422b49e4a20eb5bb378602401234489c96c07df9
leiannel <leilouis112@gmail.com>
2019-01-07 16:48:43 -0500

    completed styling and components from the footer

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 import './App.css';
+
 
 const App = () => {
   return (
     <section className='cardContainer'>
       <HeaderContainer />
       <CardContainer />
+      <Footer />
     </section>
   )
 };
