02dfc16fee9e21653de9bfdcd7e992bf523f63cc
Mike Landers <landers.mike@gmail.com>
2019-01-07 14:44:39 -0800

    Attempted state on footer.js

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import './Footer.css';
-let value = 5
+// let value = 5
+
+
+class Footer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {value: 5};
+    }
+    render(){
+        return(
+            <div className = 'footerIcons'>
+            <i className="far fa-comment"></i>
+            <i className="fas fa-sync"> 6</i>
+            <i className="far fa-heart" onClick={heartValue}> {this.state.value}</i>
+            <i className="far fa-envelope"></i>
+        </div>
+        )
+    }
+}
 
 function heartValue(){
-    value ++;
-    console.log(value)
+    // this.setState({value = )
+    // console.log(value)
     console.log('The link was clicked.');
 }
 
-function Footer (){
-    return(
-        <div className = 'footerIcons'>
-            <i className="far fa-comment"></i>
-            <i className="fas fa-sync"> 6</i>
-            <i className="far fa-heart" onClick={heartValue}> {value}</i>
-            <i className="far fa-envelope"></i>
-        </div>
-    )
-}
+
+// function Footer (){
+//     return(
+//         <div className = 'footerIcons'>
+//             <i className="far fa-comment"></i>
+//             <i className="fas fa-sync"> 6</i>
+//             <i className="far fa-heart" onClick={heartValue}> {value}</i>
+//             <i className="far fa-envelope"></i>
+//         </div>
+//     )
+// }
 
 
 
