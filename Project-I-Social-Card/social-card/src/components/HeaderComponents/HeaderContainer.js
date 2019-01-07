09b68f47d876941f41a1a46d103b5f7b20f00b83
Imon Ovbude <imonovbude@gmail.com>
2019-01-07 13:51:21 -0800

    imported image thumbnail into HeaderContainer and imported HeaderContainer into App.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+
+import ImageThumbnail from './ImageThumbnail'
+import HeaderContent from './HeaderContent'
+import HeaderTitle from './HeaderTitle'
+
+function HeaderContainer() {
+    return (
+        <div className='HeaderContainer'>
+            <ImageThumbnail />
+
+
+        </div>
+    )
+}
+
+export default HeaderContainer;
