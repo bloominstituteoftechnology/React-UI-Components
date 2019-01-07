e830ee8fdf31ba5096fe0f7f3bc1ca1d71580fa6
croy <roy.christie95@gmail.com>
2019-01-07 16:06:36 -0600

    Styling main card components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const CardContainer = () => {
+    return (
+        <div className="card-container">
+        <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+            <div className="card-img">
+            <CardBanner/>
+            </div>
+            <div className="card-text">
+            <CardContent/>
+            </div>
+        </a>  
+        </div>
+    );
+}
+
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className="card-content">
+        <p className="card-content-title">Get started with React</p>
+        <p className="card-content-paragraph">React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+        <p className="card-content-link">reactjs.org</p>
+        </div>
+    );
+}
+
+export default CardContent;
