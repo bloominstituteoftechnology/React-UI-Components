b5cc280b18963cb977997b84ecf697e6ce45fb2a
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-07 18:46:11 -0500

    Implemented click event handlers to update state of Footer icon count

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -9,10 +9,28 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      comments: null,
-      retweets: null,
-      likes: null
+      comments: 0,
+      retweets: 0,
+      likes: 0
     };
+  }
+
+  increaseLikes = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    })
+  }
+
+  increaseComments = () => {
+    this.setState({
+      comments: this.state.comments + 1
+    })
+  }
+
+  increaseRetweets = () => {
+    this.setState({
+      retweets: this.state.retweets + 1
+    })
   }
 
   render() {
@@ -22,8 +40,11 @@ class App extends React.Component {
         <CardContainer />
         <Footer 
           comments={this.state.comments}
+          increaseComments={this.increaseComments}
           retweets={this.state.retweets}
+          increaseRetweets={this.increaseRetweets}
           likes={this.state.likes}
+          increaseLikes={this.increaseLikes}
         />
       </div>
     );
--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,15 +4,15 @@ import './Footer.css';
 const Footer = props => {
     return (
         <div className="footer">
-            <div className='social-action'>
+            <div className='social-action' onClick={props.increaseComments}>
                 <i class="far fa-comment"></i>
                 <span className='social-number'>{props.comments}</span>
             </div>
-            <div className='social-action'>
+            <div className='social-action' onClick={props.increaseRetweets}>
                 <i class="fas fa-retweet"></i>
                 <span className='social-number'>{props.retweets}</span>
             </div>
-            <div className='social-action'>
+            <div className='social-action' onClick={props.increaseLikes}>
                 <i class="far fa-heart"></i>
                 <span className='social-number'>{props.likes}</span>
             </div>
