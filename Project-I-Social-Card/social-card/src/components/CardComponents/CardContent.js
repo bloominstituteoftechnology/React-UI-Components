6c4e395be6c6ca8dc41b3990d89b2527a9693b9d
Nate Boyette <nateboyette@icloud.com>
2019-01-07 15:23:58 -0800

    Styled the card components.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 const CardContent = props => {
   return (
-    <div>
-      <h3>{props.post.postTitle}</h3>
-      <p>{props.post.postContent}</p>
-      <p>{props.post.postLink}</p>
+    <div className="card-content-container">
+      <div className="card-content">
+        <h4 className="card-content-item">{props.post.postTitle}</h4>
+        <p className="card-content-item">{props.post.postContent}</p>
+        <p className="card-content-item grey">{props.post.postLink}</p>
+      </div>
+      
     </div>
   );
-}
-
+};
 
 export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -4,10 +4,10 @@ import "./Header.css";
 const HeaderTitle = props => {
   return (
     <div className="header-title">
-      <h3>{props.profileName}</h3>
-      <p className="header-item">{props.profileHandle}</p>
-      <span className="header-item">&#8226;</span>
-      <p className="header-item">{props.date}</p>
+      <h3 className="header-item">{props.profileName}</h3>
+      <p className="header-item grey">{props.profileHandle}</p>
+      <span className="header-item grey">&#8226;</span>
+      <p className="header-item grey">{props.date}</p>
     </div>
   );
 };
