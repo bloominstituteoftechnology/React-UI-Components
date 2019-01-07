e1d39db955a7279898e2a305e20b19c985fa81b2
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-07 16:59:09 -0600

    Added the footer compontent with non-functioning icons

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Headercontainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div className = "App">
       <Headercontainer/>
       <CardContainer/>
+      <Footer/>
     </div>
   );
 };
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <i className ="far fa-comment"></i>
+            <i className ="fas fa-sync-alt">   0</i>
+            <i className ="far fa-heart">   0</i>
+            <i className ="far fa-envelope"></i>
+        </div>
+    );
+};
+
+export default Footer;
