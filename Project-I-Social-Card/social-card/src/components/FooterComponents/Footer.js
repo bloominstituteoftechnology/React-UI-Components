4f58ad12dead4c29f0a9f0a03a84233bb251bf6c
Mike Landers <landers.mike@gmail.com>
2019-01-07 15:22:52 -0800

    setup state for footer share and favorite icons

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -6,25 +6,33 @@ import './Footer.css';
 class Footer extends React.Component {
     constructor(props){
         super(props);
-        this.state = {value: 5};
+        this.state = {share: 0, fav: 0};
     }
+
+    shareValue = () => {
+        const sValue = this.state.share +1
+        this.setState({share: sValue})
+        console.log(this.state.share)
+    }
+
+    favValue = () => {
+        const fValue = this.state.fav +1
+        this.setState({fav: fValue})
+        console.log(this.state.fav)
+    }
+    
     render(){
         return(
             <div className = 'footerIcons'>
             <i className="far fa-comment"></i>
-            <i className="fas fa-sync"> 6</i>
-            <i className="far fa-heart" onClick={heartValue}> {this.state.value}</i>
+            <i className="fas fa-sync" onClick={this.shareValue}> {this.state.share}</i>
+            <i className="far fa-heart" onClick={this.favValue}> {this.state.fav}</i>
             <i className="far fa-envelope"></i>
         </div>
         )
     }
 }
 
-function heartValue(){
-    // this.setState({value = )
-    // console.log(value)
-    console.log('The link was clicked.');
-}
 
 
 // function Footer (){
