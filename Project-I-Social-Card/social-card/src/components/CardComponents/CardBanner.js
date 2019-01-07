f827b9f6377feffe2a9b476becd050682d83ef76
christiansallen <christiansallen@gmail.com>
2019-01-07 15:36:30 -0700

    Completed the MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const Banner = () => {
+  return (
+    <img
+      src=" https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
+      className="card-banner"
+      alt="post-banner"
+    />
+  );
+};
+
+export default Banner;
