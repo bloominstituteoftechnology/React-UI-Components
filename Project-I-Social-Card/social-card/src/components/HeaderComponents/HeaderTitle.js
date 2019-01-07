682d5021d99f6167fcd2467b7c145ac84088549c
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-07 16:06:46 -0500

    Composed HeaderTitle and HeaderContent in HeaderContainer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className='header-content'>
+            <span className='header-user'>Lambda School</span>
+            <span className='header-social'>@LambdaSchool</span>
+            <span className="header-date">26 Jan</span>
+        </div>
+    );
+}
+
+export default HeaderTitle;
