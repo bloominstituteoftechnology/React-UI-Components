37903b865753d17bb6e7ead25b378f2b50725556
RachelKolk <rach.m.obrien@gmail.com>
2019-01-07 18:42:05 -0500

    added icons to footer element

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -4,7 +4,7 @@ import './Header.css';
 const HeaderTitle = () => {
     return (
         <div className="title">
-            <p className="username">Lambda School <span className="handle-date">  @LambdaSchool - 17 Dec</span></p>
+            <p className="username">Lambda School <span className="handle-date">  @LambdaSchool &bull; 17 Dec</span></p>
         </div>
     );
 }    
