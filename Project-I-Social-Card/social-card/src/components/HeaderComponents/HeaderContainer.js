010d41c03dc5f500b8d5b650d7f24084fba0af7f
siratl <elishubax@gmail.com>
2019-01-07 15:41:57 -0800

    created header tile and header content components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
 
 
 const HeaderContainer = () => {
     return (
         <div className="header-container">
             <ImageThumbnail/>
-            </>
+            <HeaderTitle/>
         </div>
     )
 }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title-container">
+            <div className="header-title" >
+                Lambda School
+                <span className="time-stamp">{time.format('MMM Do ')}</span>
+                <HeaderContent/>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderTitle;
