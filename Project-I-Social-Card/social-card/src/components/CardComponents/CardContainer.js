b119bfd824ab6705bcfedc84cdbf8861cd0c4eed
Lenna <lenna@lennamau.com>
2019-01-07 15:02:34 -0700

    Social Card - Lenna

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+
+import CardContent from './CardContent';
+import CardBanner from './CardBanner';
+
+
+
+const CardContainer = () => {
+    return (
+        <div className = "banner-wrapper">
+            <CardBanner />
+            <CardContent />
+        </div>
+    );
+}
+                            
+export default CardContainer;
