2074b38572fcc32707be07368844c3abf22dd2f7
nickkruger <ngkruger@gmail.com>
2019-01-07 13:44:59 -0700

    setup project

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
 
+
+import CardContainer from './components/CardComponents/CardContainer'
+
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Social Card!</h3>
+      <CardContainer />
+    </div>
+  );
+};
+
+export default App;
+
+
+{/* <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
         few files that we've already included in there to get you started right
@@ -16,9 +27,4 @@ const App = () => {
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
-      </p>
-    </div>
-  );
-};
-
-export default App;
+      </p> */}
