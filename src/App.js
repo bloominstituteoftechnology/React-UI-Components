16b9c0ade4ae549ec58ca735dc0b2eca7bbf31d8
umbur <work1415@gmail.com>
2019-01-07 15:45:02 -0700

    message

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import HeaderContainer from  './components/HeaderComponents/HeaderContainer.js';
+import CardContainer from  './components/CardComponents/CardContainer.js';
+
+const App = () => {
+  return (
+    <div className="app">
+     <HeaderContainer />
+     <CardContainer />
+
+    </div>
+  );
+};
+
+export default App;
