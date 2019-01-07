2393578bb9f0d4fd9c1f6a513e3599d9a1b06751
EJaona <ejaona@gmail.com>
2019-01-07 16:41:20 -0600

    added slight animation to footer icon

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,10 +3,20 @@ import "./Footer.css";
 
 const Footer = () => (
   <div className="footer">
-    <i class="far fa-comment fa-lg" />
-    <i class="fas fa-sync-alt fa-lg">6</i>
-    <i class="far fa-heart fa-lg">4</i>
-    <i class="far fa-envelope fa-lg" />
+    <div className="icon comment">
+      <i class="far fa-comment fa-lg" />
+    </div>
+    <div className="icon sync">
+      <i class="fas fa-sync-alt fa-spin fa-lg" />
+      <span> 6</span>
+    </div>
+    <div className="icon heart">
+      <i class="far fa-heart fa-lg" />
+      <span> 4</span>
+    </div>
+    <div className="icon mail">
+      <i class="far fa-envelope fa-lg" />
+    </div>
   </div>
 );
 
