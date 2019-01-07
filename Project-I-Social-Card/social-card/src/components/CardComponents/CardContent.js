7e07035874190b688c84c16ad7dc56f8675d197e
josharmantrout91 <josh.armantrout@gmail.com>
2019-01-07 18:24:38 -0500

    added content to Header Components and Card Components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className= "card-content">
+        <h4>Get started with React</h4>
+        <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+        <a href="https://reactjs.org/">reactjs.org</a>
+        </div>
+    )
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-// import lambdaLogo from 'lambda-logo.jpg';
+
 
 const ImageThumbnail = () => {
     return (
