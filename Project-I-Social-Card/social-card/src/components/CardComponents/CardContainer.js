7e8f8834a257a4a2c4f79f56836cb37e5d8d250d
KROHNICK <business.kanekperez@gmail.com>
2019-01-07 13:34:30 -0800

    Created card component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardContainer = () => (
+  <div
+    className="CardContainer"
+    onClick={e => {
+      e.stopPropagation();
+      window.open("https://www.reactjs.org");
+    }}
+  />
+);
+
+export default CardContainer;
