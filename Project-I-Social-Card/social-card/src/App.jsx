34446213eebcebd0f1ba0faf3a38f1dcf1abf252
Kingaa6 <Kingaa6@gmail.com>
2019-01-07 12:33:04 -0800

    Initial commit, dependencies confirmed working

---
+++ Project-I-Social-Card/social-card/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import HeaderContainer from './components/HeaderComponents/HeaderContainer'
+import CardContainer from './components/CardComponents/CardContainer'
+import Footer from './components/FooterComponents/Footer'
+
+const App = () => {
+  return (
+    <div>
+      Test
+      {/* <HeaderContainer />
+      <CardContainer />
+      <Footer /> */}
+    </div>
+  );
+};
+
+export default App;
