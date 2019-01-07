6726c21f0e153026c1106f82a85ea4e2a5b50e44
croy <roy.christie95@gmail.com>
2019-01-07 15:23:45 -0600

    Header elements are in order

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -4,3 +4,25 @@ import './Header.css';
 import Header from "./HeaderContent";
 import HeaderTitle from "./HeaderTitle";
 import ImageThumbnail from "./ImageThumbnail";
+
+const HeaderContainer = () => {
+    return (
+        <div className="header-cont">
+            <div className="header-img">
+            <ImageThumbnail/>
+            </div>
+            <div className="header-text">
+                <div className="header-title">
+                <HeaderTitle/>
+                </div>
+
+                <div className="header-paragraph">
+                <Header/>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default HeaderContainer;
