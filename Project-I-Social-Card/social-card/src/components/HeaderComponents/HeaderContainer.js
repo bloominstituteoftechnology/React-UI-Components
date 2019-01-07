0931a14a2a1a46582e4bdbdf32b92dcddb9dbf44
siratl <elishubax@gmail.com>
2019-01-07 15:29:12 -0800

    created image thumbnail component using source url

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
 
 
 const HeaderContainer = () => {
     return (
         <div className="header-container">
-        
+            <ImageThumbnail/>
+            </>
         </div>
     )
 }
