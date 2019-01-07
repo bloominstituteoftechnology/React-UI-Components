b6474ce3c9d51e9cb8fc5ca9d8f7fed481e5024e
Kingaa6 <Kingaa6@gmail.com>
2019-01-07 13:53:04 -0800

    Pixel Perfect stretch goal completed

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.jsx
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 const HeaderTitle = () => {
   return (
     <div className="header-title">
-      <h2 className="title">Lambda School <span>@LambdaSchool• 26 jan</span></h2>
+      <h2 className="title">Lambda School <span>@LambdaSchool • 26 jan</span></h2>
     </div>
   )
 }
