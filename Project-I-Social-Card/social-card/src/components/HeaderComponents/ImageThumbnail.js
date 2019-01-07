f52261685fac233319b21696c8f2e63f89e26cd8
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-01-07 17:47:52 -0600

    working on images

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
-import './images/lambda_logo.jpg';
+import './lambdacrest.png';
 
 const ImageThumbnail = () => {
     return (
         <div>
-            <img className="logo" src={logo} alt="Lambda School Logo" />
+            <img className="logo" 
+            src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" 
+            alt="Lambda School Logo" />
         </div>
     )
 }
