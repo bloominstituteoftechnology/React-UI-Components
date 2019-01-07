4080d77058363232b00b227d68f38e50af2cf7de
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-07 15:45:07 -0600

    Completed the CSS for the header title

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
-import HeaderContent from './HeaderContent';
 
 const Headercontainer = () => {
     return (
         <div className="header">
             <ImageThumbnail/>
             <HeaderTitle/>
-            <HeaderContent/>
         </div>
     );
 };
