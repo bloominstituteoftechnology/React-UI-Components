be602081a54070c80fee4075705ceeeb0c42b565
Sheila <s.hopper01110@gmail.com>
2019-01-07 17:43:07 -0500

    React UI Comp - Social Card SF

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+import  ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return (
+    <div className="container-heading">
+        <ImageThumbnail />
+    </div> 
+    )
+
+}
+
+export default HeaderContainer;
