ff9eeaa57f372e0341f85c5b7398ea45dd9b5b2e
Matt Basile <matthewbasile2@gmail.com>
2019-01-07 15:17:47 -0500

    Added boiler plate React content for header and card compoent containers.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+const Card = () =>{
+    return <div>Card Content</div>
+}
+
+export default Card
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+const Header = () =>{
+    return <h1>Test</h1>
+}
+
+
+export default Header
