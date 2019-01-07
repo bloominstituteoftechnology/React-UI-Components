a90124da9b1a9bc6e65dc5763764809da05e0b34
Kingaa6 <Kingaa6@gmail.com>
2019-01-07 14:42:15 -0800

    Updated footer icons and styling

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.jsx
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Footer.css';
 
+import comment from './comment.png';
+import circle from './circle.png';
+import heart from './heart.png';
+import mail from './mail.png';
+
+// prettier-ignore
 const Footer = (props) => {
   return (
     <div className="footer-container">
-      <i className="far fa-comment"></i>
-      <i className="fas fa-retweet icon"><span className="number">6</span></i>
-      <i className="far fa-heart icon"><span className="number">4</span></i>
-      <i className="far fa-envelope icon"></i>
+      <img src={comment} alt=""/>
+      <img src={circle} alt=""/><span className="number">6</span>
+      <img src={heart} alt=""/><span className="number">4</span>
+      <img src={mail} alt=""/>
     </div>
   )
 }
