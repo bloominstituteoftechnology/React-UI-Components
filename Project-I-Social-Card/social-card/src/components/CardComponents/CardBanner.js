f3d1038ba6712fcc298733227393bf440483536c
McNultyPT <mcnultypt@gmail.com>
2019-01-07 13:54:49 -0800

    initial push

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+import banner from './public/favicon.ico';
+
+function CardBanner() {
+    return <img className='banner' src={banner}
+}
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+export default CardContainer;
