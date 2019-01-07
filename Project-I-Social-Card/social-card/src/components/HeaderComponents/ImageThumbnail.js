592f578ca54b074f88a0fc1796c90717eafe5ede
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-01-07 17:34:01 -0500

    Social Card

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Header.css';
+
+
+const ImageThumbnail = () => <img src="https://files.slack.com/files-pri/T4JUEB3ME-FF8CH4NTC/lambda-logo.jpg"></img>
+
+export default  ImageThumbnail;
