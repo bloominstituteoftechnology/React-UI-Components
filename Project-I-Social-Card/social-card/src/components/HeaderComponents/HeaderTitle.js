e1e85c67496a7485f42a40e16030c1f47c068f9d
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-07 17:52:28 -0500

    Implemented moment.js in Header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Header.css';
 
+const moment = require('moment');
+
 const HeaderTitle = () => {
     return (
         <div className='header-title'>
             <p className='header-user'>Lambda School 
                 <span className='header-social'>@LambdaSchool</span>
-                <span className="header-date">26 Jan</span>
+                <span className="header-date">{moment().format("D MMM")}</span>
+                
             </p>
             
         </div>
