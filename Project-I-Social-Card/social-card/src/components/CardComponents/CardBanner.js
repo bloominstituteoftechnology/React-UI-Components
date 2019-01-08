2f8dec2528f3308292bb068d02264a91bbb534f7
matt0418 <mfeldma7@gmail.com>
2019-01-08 11:07:56 -0500

    Small cahnges

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const CardBanner = () => {
     return (
-        <div>
+        <div className = 'testing'>
             <img src = "https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" alt=''/>
         </div>
     )
