479f53075009c29f38bf1aa865cf471545a40644
bhumip214 <bhumip214@gmail.com>
2019-01-07 20:51:05 -0800

    Added footer icons and updated footer.css

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -7,7 +7,9 @@ let timeStamp = Moment().format("D MMM");
 export function HeaderTitle() {
   return (
     <div className="heading">
-      <h1> Lambda School </h1>
+      <h1>
+        <strong>Lambda School </strong>
+      </h1>
       <h3> @LambdaSchool </h3>
       <h3 className="dot">.</h3>
       <h3>{timeStamp}</h3>
