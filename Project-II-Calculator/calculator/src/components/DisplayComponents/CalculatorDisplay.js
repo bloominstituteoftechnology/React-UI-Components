f9b6b0d1b6b90c2972e6b6f9148af771b279a0b7
siratl <elishubax@gmail.com>
2019-01-08 15:13:40 -0800

    completed MVP requirements

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return (
+        <div className="calculatorDisplay">0</div>
+    );
+};
+
+export default CalculatorDisplay
