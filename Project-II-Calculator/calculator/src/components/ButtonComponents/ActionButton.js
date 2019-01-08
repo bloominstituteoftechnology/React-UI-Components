28d0821fb216dc1867bd3bfa9811e8f13856ddff
leiannel <leilouis112@gmail.com>
2019-01-08 16:59:58 -0500

    completed MVP markup

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = () => {
+    const action = ['÷', 'x', '-', '+', '=']
+    return (
+        <div className='actionContainer'>
+            {
+                action.map(e => {
+                    return <p className='calculatorBtn calculatorBtn--action'>{e}</p>
+                })
+            }
+        </div>
+    )
+};
+
+export default ActionButton;
---
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorNumber.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CalculatorNumber = (props) => <p className='calculatorDisplay--number' >{props.value}</p>
+
+export default CalculatorNumber;
