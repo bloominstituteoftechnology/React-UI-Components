7e8704a89cc61cee556927af777e0e4b4d619ca1
Samsoucy <newf930@outlook.com>
2019-01-07 19:34:21 -0500

    made changes to header and positioning

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const HeaderContent =() =>{
     return(
-        <p classname="headerPar">Let's learn about React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun.
+        <p className="headerPar">Let's learn about React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun.
            Once you feel comfortable using components you are well on your way to mastering React!</p>
     );
 }
