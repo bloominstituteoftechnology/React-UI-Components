b374e18902d29224ebe2b6118ac1882de7416844
Kingaa6 <Kingaa6@gmail.com>
2019-01-08 12:18:53 -0800

    Changed App component to a class, created NumberButton component with styles

--- Project-II-Calculator/calculator/src/App.jsx
+++ Project-II-Calculator/calculator/src/App.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import './App.css';
 
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
-const App = () => {
-  return (
-    <div className="container">
-      <CalculatorDisplay display="0" />
-    </div>
-  );
-};
+const buttons = ['clear', '9', '8', '7', '6', '5', '4', '3', '2', '1'];
+
+const operators = ['÷', 'x', '-', '+', '='];
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        <CalculatorDisplay display="0" />
+        <NumberButton text="9" />
+        <NumberButton text="9" />
+        <NumberButton text="9" />
+        <NumberButton text="9" />
+      </div>
+    );
+  }
+}
 
 export default App;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.jsx
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.jsx
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+  return (
+    <p
+      className={`button number-button ${props.fontWeight} ${
+        props.backgroundColor
+      } ${props.fontWeight}`}
+    >
+      {props.text}
+    </p>
+  );
+};
+
+export default NumberButton;
