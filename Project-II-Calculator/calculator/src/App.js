716985455564706765085935335724394dcd3b41
samjeffcoat <samjeffcoat@gmail.com>
2019-01-08 15:12:22 -0600

    added number and action buttons to App.js

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -19,17 +19,15 @@ const App = () => {
         <NumberButton text="3" type="small" />
         <NumberButton text="0" type="large" />
       </div>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
+      <div className= "action">
+        <ActionButton text= "÷" type = "small"/>
+        <ActionButton text="*" type="small" />
+        <ActionButton text="-" type="small" />
+        <ActionButton text="+" type="small" />
+        <ActionButton text="=" type="small" />
+        </div>
+
+      </div>
   );
 };
 
