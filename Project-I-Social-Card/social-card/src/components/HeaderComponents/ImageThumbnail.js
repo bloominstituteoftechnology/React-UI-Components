d547ccdf80d3403f69a5984d2e5c760fe4f7a026
ajfrierson <alvinfrierson@fuller.edu>
2019-01-07 16:30:44 -0800

    completed image thumbnail.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = (props) => {
+    return (
+      <div>
+          <img src="https://ibin.co/3whrpKSBbZ81.png" alt="Lambda logo"></img>
+      </div>
+    )
+  }
+
+export default ImageThumbnail;
