bb435bc86030c39ebb83e174f87a15b4902958b0
JamesB <jblazer1@yahoo.com>
2019-01-08 07:23:50 -0800

    Add main image and container content

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,4 +1,27 @@
 import React from "react";
 import "./Header.css";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
 
-import ImageThumbnail from "./components/HeaderComponents/ImageThumbnail";
+// import ImageThumbnail from "./components/HeaderComponents/ImageThumbnail";
+
+const HeaderContainer = () => {
+  return (
+    <div class="container">
+      <div>
+        <ImageThumbnail />
+      </div>
+
+      <div class="header-title">
+        <HeaderTitle />
+      </div>
+
+      <div class="content">
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div>
+      <h1>Lambda School</h1>
+      <p>@LambdaSchool 26 jan</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
