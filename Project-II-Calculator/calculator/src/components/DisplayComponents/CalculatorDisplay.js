7301b3060b3b8cf902e302ececbd9e0da4f333e9
samjeffcoat <samjeffcoat@gmail.com>
2019-01-08 15:49:27 -0600

    Calculator Display built

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './Display.css';
 
+function CalculatorDisplay(props) {
+    return (
+        <div className= "display">
+            <DynamicButton text = " " type =  "disLg"/>
+        </div>
+    );
+};
 
+function DynamicButton(props) {
+    return (
+        <button className = {`dynamic-button ${props.type}`}>{props.text}</button>
+    )
+}
 
-expo
+export default CalculatorDisplay;
