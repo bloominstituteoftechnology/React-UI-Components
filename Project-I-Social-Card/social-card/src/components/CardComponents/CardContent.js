2163c370d5ecdc41fbb20a4ead7ae943156d2aa0
leiannel <leilouis112@gmail.com>
2019-01-08 16:53:14 -0500

    completed components and styling for buttons and displays

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -6,7 +6,7 @@ const CardContent = () => {
         <div className='mainContent--container'>
             <h2>Get started with React</h2>
             <p className='mainContent--text'>React makes it painless to create interactive UIs. Design Simple views for each state in you application</p>
-            <p className='mainContent--web'><a href='reactjs.org'>reactjs.org</a></p>
+            <p className='mainContent--web'><a href='https://reactjs.org' target='_blank'>reactjs.org</a></p>
         </div>
     )
 }
