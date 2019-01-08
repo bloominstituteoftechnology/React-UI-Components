8fb50309738b64596e41f4c40a59f8e2a020502c
cori-peck <cori.peck@ymail.com>
2019-01-08 16:09:09 -0600

    completed mvp requirements

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -11,20 +11,25 @@ const App = () => {
   return (
     <div>
       <NumberButton customClass="black-btn" number="0" />
-      <NumberButton customClass="white-btn" number="clear" />
+      <br />
+      <NumberButton customClass="clear-btn" number="clear" />
       <ActionButton customClass="action-btn" action="÷" />
+      <br />
       <NumberButton customClass="number-btn" number="7" />
       <NumberButton customClass="number-btn" number="8" />
       <NumberButton customClass="number-btn" number="9" />
       <ActionButton customClass="action-btn" action="x" />
+      <br />
       <NumberButton customClass="number-btn" number="4" />
       <NumberButton customClass="number-btn" number="5" />
       <NumberButton customClass="number-btn" number="6" />
       <ActionButton customClass="action-btn" action="-" />
+      <br />
       <NumberButton customClass="number-btn" number="1" />
       <NumberButton customClass="number-btn" number="2" />
       <NumberButton customClass="number-btn" number="3" />
       <ActionButton customClass="action-btn" action="+" />
+      <br />
       <NumberButton customClass="white-btn" number="0" />
       <ActionButton customClass="action-btn" action="=" />
 
