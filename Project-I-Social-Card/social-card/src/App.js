e4dae9f91d6baa8f6ce016d991095a4b9cb3f0ae
Jamie Hall <hall.jamie@gmail.com>
2019-01-07 19:16:02 -0500

    Tweaked footer.js to use named export

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { HeaderContainer } from "./components/HeaderComponents/HeaderContainer";
 import { CardContainer } from "./components/CardComponents/CardContainer";
-import  Footer  from "./components/FooterComponents/Footer";
+import { Footer }  from "./components/FooterComponents/Footer";
 
 
 
--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,30 +2,28 @@ import React from 'react';
 import './Footer.css';
 
 
-const Footer = () =>{
-  return (
+export const Footer = () =>
+ 
     <div className = "footer-container">
       <div className = "footer">
-      <span className="far fa-comment"></span>
+      <span className="far fa-comment fa-rotate-270"></span>
       </div>
 
       <div className = "footer">
-      <span className="fas fa-retweet"></span>6
+      <span className="fas fa-retweet"></span><em>6</em>
       </div>
 
       <div className = "footer">
-      <span className="far fa-heart"></span>4
+      <span className="far fa-heart"></span><em>4</em>
       </div>
 
       <div className = "footer">
       <span className="far fa-envelope"></span>
       </div>
 
-    </div>
-
-  );
-}
+    </div>;
 
 
 
-export default Footer;
+
+
