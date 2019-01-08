bcc75431b182432764ff564c255e8caf73891400
bhumip214 <bhumip214@gmail.com>
2019-01-08 13:10:06 -0800

    Created and exported  NumberButton and ActionButton components

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+function NumberButton(props) {
+  return <button className={props.buttonStyle}>{props.text}</button>;
+}
+
+export default NumberButton;
