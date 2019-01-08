12d373692605eff1eb96b3b05e9d187936ad4121
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-08 15:23:45 -0500

    Create and export NumberButton

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+const operators = ["÷", "×", "-", "+", "="];
+
+const NumberButton = () => {
+    return <div className='button-style'>Text</div>;
+}
+
+export default NumberButton;
