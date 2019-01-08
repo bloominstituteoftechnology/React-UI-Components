094e9f0292086e7aceb08d934c6ca4054a047b47
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-01-08 15:15:25 -0600

    components written out. on to styling

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import OperationButton from './components/ButtonComponents/OperationButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div className = 'container'>
-      
+      <CalculatorDisplay />
+      <ActionButton text = 'clear' />
+      <OperationButton text = '÷' />
+      <NumberButton text = '7' />
+      <NumberButton text = '8' />
+      <NumberButton text = '9' />
+      <OperationButton text = 'X' />
+      <NumberButton text = '4' />
+      <NumberButton text = '5' />
+      <NumberButton text = '6' />
+      <OperationButton text = '-' />
+      <NumberButton text = '1' />
+      <NumberButton text = '2' />
+      <NumberButton text = '3' />
+      <OperationButton text = '+' />
+      <ActionButton text = '0' />
+      <OperationButton text = '=' />
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return (
+        <React.Fragment>
+            <div className = 'display'>0</div>
+        </React.Fragment>
+    );
+}
+
+export default CalculatorDisplay;
