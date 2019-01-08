481e39229cc5753566fcbf16ba9a9440fd92009b
Lenna <lenna@lennamau.com>
2019-01-07 21:04:54 -0700

    footer

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,11 +5,13 @@ const Footer = () => {
     return (
 
         <div className= "footer">
-        <div className = "footer-img">
+        <div className = "footer-img" id= "comment-img">
             <i class="far fa-comment"></i>
             </div>
             <div className = "footer-img">
+            
             <i class="fas fa-retweet"></i>
+            
             </div>   
             <div className = "footer-img">
                 <i class="far fa-heart"></i>
@@ -24,3 +26,6 @@ const Footer = () => {
 }
 
 export default Footer;
+
+
+<img src="https://img.icons8.com/ios/50/000000/topic.png"></img>
