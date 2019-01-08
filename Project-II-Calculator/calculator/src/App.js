7e51b03b864026537b1fcdc1d9defb437e2a46c2
Nathan Thomas <nwthomas@me.com>
2019-01-08 12:29:25 -0800

    Outline calculator container and display components

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { CalculatorDisplay } from "./components/DisplayComponents";
 import "./App.css";
 
 class App extends Component {
@@ -12,7 +13,9 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <div />
+        <div className="calculator__container">
+          <CalculatorDisplay />
+        </div>
       </div>
     );
   }
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalculatorDisplay = props => {
+  return (
+    <div className="calculator__display">
+      <p className="display__text">{props.total}</p>
+    </div>
+  );
+};
+
+export default CalculatorDisplay;
