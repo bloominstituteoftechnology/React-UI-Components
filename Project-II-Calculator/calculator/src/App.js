c751df3f7d1e1d03b2b4ffb5f2138b7f4fd0e4be
umbur <work1415@gmail.com>
2019-01-08 14:01:03 -0700

    initial commit for Calculator project

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <div> 
       <h3>Welcome to React Calculator</h3>
       <p>
         We have given you a starter project. You'll want to build out your
