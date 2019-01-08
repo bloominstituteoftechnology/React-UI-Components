cabacd0e9eb61b02e84a0230c2e248c2308516c3
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-08 16:46:39 -0500

    Stretch: Change App to class component

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -9,29 +9,43 @@ import ActionButton from './components/ButtonComponents/ActionButton';
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const operators = ["÷", "×", "-", "+", "="];
 
-const App = () => {
-  return (
-    <div className='calculator-container'>
-      <CalculatorDisplay result='0' />
-      <div className='buttons-container'>
-        <div className='numbers-container'>
-          <ActionButton text='clear' />
-          {numbers.map(number => { return <NumberButton 
-            text={number} 
-            className='btn btn-number'
-            key={number} /> })}
-          <ActionButton text='0' />
-        </div>
-        <div className='symbols-container'>
-          {operators.map(operator => { return <NumberButton 
-            text={operator} 
-            className='btn btn-operator'
-            key={operator} /> })}
-        </div>
-      </div>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: 0
+    }
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className='calculator-container'>
+        <CalculatorDisplay result={this.state.result} />
+        <div className='buttons-container'>
+          <div className='numbers-container'>
+            <ActionButton text='clear' />
+            {numbers.map(number => {
+              return <NumberButton
+                text={number}
+                className='btn btn-number'
+                key={number} />
+            })}
+            <ActionButton text='0' />
+          </div>
+          <div className='symbols-container'>
+            {operators.map(operator => {
+              return <NumberButton
+                text={operator}
+                className='btn btn-operator'
+                key={operator} />
+            })}
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+
 };
 
 export default App;
