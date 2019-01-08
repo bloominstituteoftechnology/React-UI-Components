43ec4f1aa3da2b4a555c2d27ccb27070b79377f8
ills5771 <ilyayelizarov1@gmail.com>
2019-01-08 13:58:25 -0800

    Added RowContainer and ActionButton components

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./App.css";
 
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import RowContainer from "./components/ButtonComponents/RowContainer";
+import ActionButton from "./components/ButtonComponents/ActionButton";
 
 const App = () => {
   return (
     <div className="container-wrapper">
       <CalculatorDisplay />
+      <RowContainer className="row-container">
+        <ActionButton className="action-button" text="clear" />
+      </RowContainer>
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+const ActionButton = props => {
+  return (
+    <button className={`action-button ${props.className}`}>
+      {" "}
+      {props.text}
+    </button>
+  );
+};
+
+export default ActionButton;
