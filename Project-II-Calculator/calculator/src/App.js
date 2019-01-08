0377cda3858ddb74649938b303c4ccaee282fa53
samjeffcoat <samjeffcoat@gmail.com>
2019-01-08 15:08:11 -0600

    getting started by importing files and creating number buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
+    <div className = "top">
+      <CalculatorDisplay/>
+      <div className= "numbers">
+        <NumberButton text= "clear" type = "large"/>
+        <NumberButton text="7" type="small" />
+        <NumberButton text="8" type="small" />
+        <NumberButton text="9" type="small" />
+        <NumberButton text="4" type="small" />
+        <NumberButton text="5" type="small" />
+        <NumberButton text="6" type="small" />
+        <NumberButton text="1" type="small" />
+        <NumberButton text="2" type="small" />
+        <NumberButton text="3" type="small" />
+        <NumberButton text="0" type="large" />
+      </div>
         We have given you a starter project. You'll want to build out your
         components in their respective files, remove this code and replace it
         with the proper components.
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Display.css';
+
+
+
+export default CalculatorDisplay;
