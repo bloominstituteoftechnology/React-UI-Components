86e5489bac681f835fb182c3ac455627aa807c68
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-01-08 10:53:52 -0600

    finished stretch

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Footer.css';
+// import CardContent from './CardContent';
+// import CardBanner from './CardBanner';
+
+
+const Footer = () => {
+    return (
+        <div className="footerCard">
+        <div>
+        <i class="far fa-comment"></i>
+        </div>
+        <div>
+        <i class="fas fa-retweet"></i>
+        </div>
+        <div>
+        <i class="fas fa-heart"></i>
+        </div>
+        <div>
+        <i class="far fa-envelope"></i>
+        </div>
+
+
+        </div>
+
+    )
+}
+
+export default Footer;
+
