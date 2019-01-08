30084ff655a6cc398135e49d31111e8dab8287fe
joepound <14357797+joepound@users.noreply.github.com>
2019-01-08 13:14:13 -0700

    Added display area

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
+
 import './Display.css';
+
+const CalculatorDisplay = props => {
+  return (
+    <div className="calculator__display">
+      
+    </div>
+  );
+};
+
+export default CalculatorDisplay;
