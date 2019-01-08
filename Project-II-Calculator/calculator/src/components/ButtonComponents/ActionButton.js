adf8690985f7b917f7223464536cc506fe708ff2
JamesB <jblazer1@yahoo.com>
2019-01-08 15:31:36 -0800

    Basic structure of calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,18 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+// ÷
+
+const ActionButtons = () => {
+  return (
+    <div className="action-butts">
+      <h1>÷</h1>
+      <h1>X</h1>
+      <h1>-</h1>
+      <h1>+</h1>
+      <h1>=</h1>
+    </div>
+  );
+};
+
+export default ActionButtons;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,22 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+const NumberButton = () => {
+  return (
+    <div className="number-butts">
+      <h1 className="clear-butt">clear</h1>
+      <h1>7</h1>
+      <h1>8</h1>
+      <h1>9</h1>
+      <h1>4</h1>
+      <h1>5</h1>
+      <h1>6</h1>
+      <h1>1</h1>
+      <h1>2</h1>
+      <h1>3</h1>
+      <h1>0</h1>
+    </div>
+  );
+};
+
+export default NumberButton;
