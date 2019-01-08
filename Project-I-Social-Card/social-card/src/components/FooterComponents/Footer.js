0800e22f8fe9984460192e167cb79745203567e6
cecil john tantay <cjbtantay@gmail.com>
2019-01-07 22:12:04 -0800

    finished footer and close to pixel perfection

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import envelope from './envelope-light.svg';
+import comment from './comment-light.svg';
+import heart from './heart-light.svg';
+import sync from './sync-light.svg';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      <h1>this is the footer</h1>
+    <div className='footer-container'>
+      <img className='footer-img' src={comment} alt='' />
+      <div className='num sync-num'>
+        <img className='footer-img' src={sync} alt='' />
+        <p>6</p>
+      </div>
+      <div className='num heart-num'>
+        <img className='footer-img' src={heart} alt='' />
+        <p>4</p>
+      </div>
+
+      <img className='footer-img' src={envelope} alt='' />
     </div>
   );
 };
