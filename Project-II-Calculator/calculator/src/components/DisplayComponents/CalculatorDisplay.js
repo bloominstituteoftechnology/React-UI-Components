3990dd6d34152ef5157c43d545c94772e75f252b
Angel Torres <alas4224@gmail.com>
2019-01-08 14:22:57 -0600

    finished display component

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = function() {
+    return (
+        <div className="display">
+            <h1 className="displayDigits">0</h1>
+        </div>
+    )
+};
+
+export default CalculatorDisplay;
