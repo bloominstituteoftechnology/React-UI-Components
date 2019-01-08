ed6d35707b2458c2884f693d71dfaf60e0169778
Samsoucy <newf930@outlook.com>
2019-01-08 17:24:51 -0500

    added calculator display component

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const Display = props => {
+    return (
+        <h1 className ={props.display.buttonStyle}>{props.display.text}</h1>
+    );
+}
+
+export default Display;
