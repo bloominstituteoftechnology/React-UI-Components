cf6ce7d6594c5067f4cce8cf9dd4d810207c5215
ills5771 <ilyayelizarov1@gmail.com>
2019-01-08 15:18:27 -0800

    Added number components to the rest of the project

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,13 +4,37 @@ import "./App.css";
 import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
 import RowContainer from "./components/ButtonComponents/RowContainer";
 import ActionButton from "./components/ButtonComponents/ActionButton";
+import NumberButton from "./components/ButtonComponents/NumberButton";
 
 const App = () => {
   return (
     <div className="container-wrapper">
       <CalculatorDisplay />
-      <RowContainer className="row-container">
+      <RowContainer>
         <ActionButton className="action-button" text="clear" />
+        <NumberButton background="functional-btn" text="÷" />
+      </RowContainer>
+      <RowContainer>
+        <NumberButton text="7" />
+        <NumberButton text="8" />
+        <NumberButton text="9" />
+        <NumberButton text="x" />
+      </RowContainer>
+      <RowContainer>
+        <NumberButton text="4" />
+        <NumberButton text="5" />
+        <NumberButton text="6" />
+        <NumberButton text="-" />
+      </RowContainer>
+      <RowContainer>
+        <NumberButton text="1" />
+        <NumberButton text="2" />
+        <NumberButton text="3" />
+        <NumberButton text="+" />
+      </RowContainer>
+      <RowContainer>
+        <ActionButton className="action-button" text="0" />
+        <NumberButton background="functional-btn" text="=" />
       </RowContainer>
     </div>
   );
