f828183621522e384bdcb7e63f73f3b017b399bb
ills5771 <ilyayelizarov1@gmail.com>
2019-01-08 13:06:05 -0800

    Added CalculatorDisplay component

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+
 const App = () => {
-  return <div className="container-wrapper" />;
+  return (
+    <div className="container-wrapper">
+      <CalculatorDisplay />
+    </div>
+  );
 };
 
 export default App;
