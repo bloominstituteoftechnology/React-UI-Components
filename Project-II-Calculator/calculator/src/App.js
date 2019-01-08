269f76b5e05a781e1097516d7d03fc9aa667a1da
cori-peck <cori.peck@ymail.com>
2019-01-08 15:23:51 -0600

    Have all buttons displaying, need to apply styles

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+
+NumberButton.defaultProps = {
+  number: `1`
+}
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      <NumberButton customClass="black-btn" number="0" />
+      <NumberButton customClass="white-btn" number="clear" />
+      <ActionButton customClass="action-btn" action="÷" />
+      <NumberButton customClass="number-btn" number="7" />
+      <NumberButton customClass="number-btn" number="8" />
+      <NumberButton customClass="number-btn" number="9" />
+      <ActionButton customClass="action-btn" action="x" />
+      <NumberButton customClass="number-btn" number="4" />
+      <NumberButton customClass="number-btn" number="5" />
+      <NumberButton customClass="number-btn" number="6" />
+      <ActionButton customClass="action-btn" action="-" />
+      <NumberButton customClass="number-btn" number="1" />
+      <NumberButton customClass="number-btn" number="2" />
+      <NumberButton customClass="number-btn" number="3" />
+      <ActionButton customClass="action-btn" action="+" />
+      <NumberButton customClass="white-btn" number="0" />
+      <ActionButton customClass="action-btn" action="=" />
+
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return <button className={props.customClass}>{props.number}</button>
+}
+
+export default NumberButton;
