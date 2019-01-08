64f1240ba51c94729f2fdbb4bab21f8a2fee684a
cacheUp <bradb26@gmail.com>
2019-01-09 02:03:21 +0300

    link fix

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,10 +3,12 @@ import "./Card.css";
 
 const CardBanner = props => {
   return (
-    <img
-      src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
-      alt="eskietit"
-    />
+    <a href="https://www.reactjs.org">
+      <img
+        src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
+        alt="eskietit"
+      />
+    </a>
   );
 };
 
