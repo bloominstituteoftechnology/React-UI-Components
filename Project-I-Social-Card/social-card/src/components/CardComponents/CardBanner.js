6d47ae080346d70a6093da12c008b0a67475591c
cecil john tantay <cjbtantay@gmail.com>
2019-01-07 20:25:13 -0800

    styled banner

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,12 +2,7 @@ import React from 'react';
 import './Card.css';
 
 const CardBanner = () => {
-  return (
-    <img
-      src='https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png'
-      alt=''
-    />
-  );
+  return <div className='banner-img' />;
 };
 
 export default CardBanner;
