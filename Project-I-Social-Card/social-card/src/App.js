2770084090000e983f6b82882b8eaab3defb5955
Sheila <s.hopper01110@gmail.com>
2019-01-07 23:49:59 -0500

    worked on footer

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer.js';
 
 const App = () => {
   return (
     <div className="whole-container">
       <HeaderContainer />
       <CardContainer />
-     
-      
+      <Footer />
       
     </div>
   );
