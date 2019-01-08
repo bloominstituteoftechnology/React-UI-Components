e00647371692c69f72e6e955790ecf125f02a1c4
cacheUp <bradb26@gmail.com>
2019-01-08 21:56:02 +0300

    button config

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -45,6 +45,35 @@ class App extends Component {
         break;
     }
   };
+  render() {
+    return (
+      <div className="App">
+        <Display data={this.state.operations} />
+        <Buttons>
+          <Button onClick={this.handleClick} label="C" value="clear" />
+          <Button onClick={this.handleClick} label="7" value="7" />
+          <Button onClick={this.handleClick} label="4" value="4" />
+          <Button onClick={this.handleClick} label="1" value="1" />
+          <Button onClick={this.handleClick} label="0" value="0" />
+
+          <Button onClick={this.handleClick} label="/" value="/" />
+          <Button onClick={this.handleClick} label="8" value="8" />
+          <Button onClick={this.handleClick} label="5" value="5" />
+          <Button onClick={this.handleClick} label="2" value="2" />
+          <Button onClick={this.handleClick} label="." value="." />
+
+          <Button onClick={this.handleClick} label="x" value="*" />
+          <Button onClick={this.handleClick} label="9" value="9" />
+          <Button onClick={this.handleClick} label="6" value="6" />
+          <Button onClick={this.handleClick} label="3" value="3" />
+
+          <Button onClick={this.handleClick} label="-" value="-" />
+          <Button onClick={this.handleClick} label="+" value="+" />
+          <Button onClick={this.handleClick} label="=" value="equal" />
+        </Buttons>
+      </div>
+    );
+  }
 }
 
 export default App;
