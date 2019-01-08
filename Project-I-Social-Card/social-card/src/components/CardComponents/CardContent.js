6dc51b772a77f0120d8f4ece6a50f90ba6149d61
cecil john tantay <cjbtantay@gmail.com>
2019-01-07 20:41:11 -0800

    styled cardcontents

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,13 +3,15 @@ import './Card.css';
 
 const CardContent = () => {
   return (
-    <div>
-      <h1>Get started with React</h1>
-      <p>
-        React makes it painless to create interactive UIs. Design simple views
-        for each state in your application
-      </p>
-      <span>reactjs.org</span>
+    <div className='card-content'>
+      <div className='card-content-container'>
+        <h1>Get started with React</h1>
+        <p>
+          React makes it painless to create interactive UIs. Design simple views
+          for each state in your application
+        </p>
+        <span>reactjs.org</span>
+      </div>
     </div>
   );
 };
