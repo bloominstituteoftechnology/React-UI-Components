b6b1e85920541559f7c731248a233e44c24b3077
ajfrierson <alvinfrierson@fuller.edu>
2019-01-07 16:03:22 -0800

    completed CardBanner.js

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = (props) => {
+    return (
+      <div>
+          <img src="https://ibin.co/3wnC6SgIOJud.png" alt="React logo"></img>
+      </div>
+    )
+  }
+
+  export default CardBanner;
