0846ace15a0def1652e50c724edeed61fa38d4b9
BrannanC <brannanconrad@gmail.com>
2019-01-08 15:35:42 -0600

    Display finished, working on making functional

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,76 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      didEqual: false,
+      displayBox: '0'
+    }
+  }
+
+  displayNum = (num) => {
+    if(this.state.didEqual){
+      this.setState({
+        displayBox: '',
+        didEqual: false
+      })
+    }
+    this.setState({
+      displayBox: this.displayBox += num
+    }) 
+    if(num === "clear"){      
+      this.setState({
+        displayBox: '',
+      })
+    }
+  }
+
+  equals = () => {
+    if(this.state.displayBox.slice(-2) === "/0"){
+      this.setState({
+        displayBox: 'stop that',
+    })}
+    this.setState({
+      displayBox: eval(this.state.displayBox),
+      didEqual: true
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <CalculatorDisplay displayBox={this.state.displayBox}/>
+        <div className="buttons">
+          <div className="NumberButtons">
+            < NumberButton name="clear" symbol="clear" buttonStyle="number-long" />
+            < NumberButton name="seven" symbol="7" buttonStyle="number-short" />
+            < NumberButton name="eight" symbol="8" buttonStyle="number-short" />
+            < NumberButton name="nine" symbol="9" buttonStyle="number-short" />
+            < NumberButton name="four" symbol="4" buttonStyle="number-short" />
+            < NumberButton name="five" symbol="5" buttonStyle="number-short" />
+            < NumberButton name="six" symbol="6" buttonStyle="number-short" />
+            < NumberButton name="one" symbol="1" buttonStyle="number-short" />
+            < NumberButton name="two" symbol="2" buttonStyle="number-short" />
+            < NumberButton name="three" symbol="3" buttonStyle="number-short" />
+            < NumberButton name="zero" symbol="0" buttonStyle="number-long" />
+          </div>
+          <div className="ActionButtons">
+            < ActionButton symbol="÷" name="divide" />
+            < ActionButton symbol="X" name="multiply" />
+            < ActionButton symbol="-" name="minus" />
+            < ActionButton symbol="+" name="plus" />
+            < ActionButton symbol="=" name="equals" />
+          </div>        
+        </div>
+      </div>
+    );
+  }
+
 };
 
 export default App;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (props) => {
+    return <p className={`${props.name} ${props.buttonStyle}`}>{props.symbol}</p>
+}
+
+export default ActionButton;
