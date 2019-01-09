a9c58dd4be4669784e62a795cdd868b531ed77c1
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-01-08 18:45:43 -0600

    trying something different

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
-import ActionButton from './components/ButtonComponents/ActionButton';
-import OperationButton from './components/ButtonComponents/OperationButton';
-import NumberButton from './components/ButtonComponents/NumberButton';
+import ButtonContainer from './components/ButtonComponents/ButtonContainer';
 
 const App = () => {
   return (
     <div className = 'container'>
       <CalculatorDisplay />
-      <ActionButton buttonStyle = 'white-btn action-btn' value = 'clear' />
-      <OperationButton buttonStyle = 'red-btn operator-btn' value = '&divide;' />
-      <NumberButton buttonStyle = 'white-btn number-btn' value = '7' />
-      <NumberButton buttonStyle = 'white-btn number-btn' value = '8' />
-      <NumberButton buttonStyle = 'white-btn number-btn' value = '9' />
-      <OperationButton buttonStyle = 'red-btn operator-btn' value = '&times;' />
-      <NumberButton buttonStyle = 'white-btn number-btn' value = '4' />
-      <NumberButton buttonStyle = 'white-btn number-btn' value = '5' />
-      <NumberButton buttonStyle = 'white-btn number-btn' value = '6' />
-      <OperationButton buttonStyle = 'red-btn operator-btn' value = '-' />
-      <NumberButton buttonStyle = 'white-btn number-btn' value = '1' />
-      <NumberButton buttonStyle = 'white-btn number-btn' value = '2' />
-      <NumberButton buttonStyle = 'white-btn number-btn' value = '3' />
-      <OperationButton buttonStyle = 'red-btn operator-btn' value = '+' />
-      <ActionButton buttonStyle = 'white-btn action-btn' value = '0' />
-      <OperationButton buttonStyle = 'red-btn operator-btn' value = '=' />
+      <ButtonContainer />
     </div>
   );
 };
