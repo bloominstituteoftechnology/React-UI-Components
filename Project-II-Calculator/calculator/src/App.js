fa7d3fbce55278206ef0053bdbae5ef5d9940ae5
Nathan Thomas <nwthomas@me.com>
2019-01-08 19:47:22 -0800

    Create ability to use numbers, operators, and clear functionality

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { CalculatorDisplay } from "./components/DisplayComponents";
 import { ButtonsContainer } from "./components/ButtonsComponent";
 import "./App.css";
-// import * as math from "mathjs";
 
 const operators = ["÷", "×", "-", "+", "="];
 const numbers = ["clear", 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
@@ -11,24 +10,86 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      total: 0,
-      operation: ""
+      usedAlready: false,
+      total: [],
+      operator: "",
+      content: []
     };
   }
 
+  componentDidMount() {
+    this.setState({
+      total: [0]
+    });
+  }
+
+  clearInput = () => {
+    const newTotal = [0]; // Used an extra variable here as [0] turned into 0 without an array otherwise
+    this.setState({
+      firstNum: [],
+      total: newTotal,
+      operator: "",
+      usedAlready: false
+    });
+  };
+
   clicked = event => {
     if (event.target.name === "clear") {
+      // Clear input case
+      this.clearInput();
+    } else if (
+      event.target.name === "÷" ||
+      event.target.name === "×" ||
+      event.target.name === "-" ||
+      event.target.name === "+"
+    ) {
+      // Operator input (excluding "=") case
+      const currentDisplay = Number(this.state.total.join(""));
+      const newTotal = [0];
       this.setState({
-        total: 0,
-        operation: ""
-      });
-    } else if (!Number(event.target.name)) {
-      console.log("It's working!!!");
-    } else {
-      const newTotal = Number(event.target.name) + 5;
-      this.setState({
+        firstNum: currentDisplay,
+        operator: event.target.name,
         total: newTotal
       });
+    } else if (event.target.name === "=") {
+      // Equals input case
+      if (this.state.total) {
+        const firstNum = Number(this.state.firstNum);
+        const secondNum = Number(this.state.total.join(""));
+        let newTotal;
+        if (this.state.operator === "÷") {
+          newTotal = [firstNum / secondNum];
+        } else if (this.state.operator === "×") {
+          newTotal = [firstNum * secondNum];
+        } else if (this.state.operator === "-") {
+          newTotal = [firstNum - secondNum];
+        } else if (this.state.operator === "+") {
+          newTotal = [firstNum + secondNum];
+        } else {
+          alert("Something went wrong.");
+        }
+        this.setState({
+          total: newTotal,
+          operator: "",
+          firstNum: [],
+          usedAlready: true
+        });
+      } else {
+        return;
+      }
+    } else {
+      // Number input case
+      if (this.state.total.length === 1 && this.state.total[0] === 0) {
+        const newTotal = Number(event.target.name);
+        this.setState({
+          total: [newTotal]
+        });
+      } else {
+        const newTotal = [...this.state.total, Number(event.target.name)];
+        this.setState({
+          total: newTotal
+        });
+      }
     }
   };
 
@@ -36,7 +97,7 @@ class App extends Component {
     return (
       <div className="container">
         <div className="calculator__container">
-          <CalculatorDisplay total={this.state.total} />
+          <CalculatorDisplay content={this.state.total} />
           <ButtonsContainer
             operators={operators}
             numbers={numbers}
