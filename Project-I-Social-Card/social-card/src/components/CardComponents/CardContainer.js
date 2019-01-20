9c20f74093b95d6061bc9029a4c51e4555abc4ed
austie702 <austie702@gmail.com>
2019-01-19 21:54:48 -0600

    Add content without styling to Social Card && Scratch start for calculator

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const CardContainer = props => {
+  return(
+    <div className='content'>
+      <CardBanner />
+      <CardContent />
+    </div>
+  )
+}
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Header.css';
+
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const Header = props => {
+  return (
+    <div className='header flexy'>
+      <div>
+        <ImageThumbnail />
+      </div>
+      <div>
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  )
+}
+
+export default Header;
