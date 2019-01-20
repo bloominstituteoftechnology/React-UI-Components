508db30214548fa75fbe267f70df8e45cc23b071
austie702 <austie702@gmail.com>
2019-01-19 21:56:35 -0600

    Add content without styling to Social Card && Scratch start for calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+  return(
+    <button className='square-butt brown-butt'>
+      {/*  */}
+    </button>
+  )
+}
+
+export default ActionButton;
