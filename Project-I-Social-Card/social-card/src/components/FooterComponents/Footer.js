68419b0f46b1ee06c5dc9895fa43f962a7e21abb
Jaouad Sarouti <jadsarouti@outlook.com>
2019-02-11 17:15:27 -0500

    adding interaction to footer

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,14 +1,32 @@
 import React from "react";
+import ReactDOM from "react-dom"
 
-const Footer = ()=>{
+class Footer extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            count : 0
+        }
+    }
+
+    addHandler =()=>{
+        this.setState ((prevState)=>{
+            return {
+                count : prevState.count +1
+            }
+        })
+    }
+
+    render(){
     return (
         <div style={{display:"flex",width:"50%",justifyContent:"space-between",marginLeft:"10%",marginTop:"4%"}}>
-            <div><i class="comment outline icon"></i></div>
-            <div><i class="sync alternate icon"></i></div>
-            <div><i class="heart outline icon"></i></div>
-            <div><i class="envelope outline icon"></i></div>    
+            <div onClick={this.addHandler}>{this.state.count}<i class="comment outline icon"></i></div>
+            <div><i class="sync alternate icon"></i>0</div>
+            <div><i class="heart outline icon"></i>0</div>
+            <div><i class="envelope outline icon"></i>0</div>    
         </div>
     )
+    }
 }
 
 export default Footer;
