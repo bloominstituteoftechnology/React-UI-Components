378892ad1902ea15231a0dd25d52a3660189ee2f
Corey Gumbs <cgumbs@Coreys-MBP-3.home>
2019-02-11 17:47:58 -0500

    [chore] completed header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -3,8 +3,11 @@ import React from 'react';
 
 const HeaderContent = () => {
     return(
-        <div className="header-content">
-
+        <div className="header-description-container">
+            <p className="header-description-txt">
+                Let's learn React by building simple interfaces with components. Don't try to over think it, just keep it simple and have fun. Once you feel comfortable using 
+                components you are well on your way to mastering React!
+            </p>
         </div>
     );
 } 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import HeaderTitleText from './HeaderTitleText';
+import HeaderTag from './HeaderTag';
 
 const HeaderTitle = () => {
     return(
-        <div className="header-title">
-        Header title
+        <div className="header-title-container">
+            <HeaderTitleText />
+            <HeaderTag/>
         </div>
     );
 }
