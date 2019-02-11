c1d10a14dab26a77275895e3ca1e57631c2a8d34
Corey Gumbs <cgumbs@Coreys-MBP-3.home>
2019-02-11 18:17:18 -0500

    [chore] completed CardComponents

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBannerDescription.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBannerDescription.js
@@ -3,9 +3,15 @@ import React from 'react';
 const CardBannerDescription = () => {
     return(
         <div className="card-txt-container">
-            <p className="card-header"></p>
-            <p className="card-txt"></p>
-            <p className="card-link"></p>
+            <h4 className="card-header">
+                Get started with React
+            </h4>
+            <p className="card-txt">
+                React makes it painless to create interactive UIs. Design simple views for each state in your application.
+            </p>
+            <p className="card-link">
+                <a href="https://reactjs.org/" className="card-link">reactjs.org</a>
+            </p>
         </div>
     );
 }
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBannerImage.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBannerImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CardBannerImage =  () => {
     return(
-        <img src="" alt="" className="card-img"/>
+        <img src="https://ibin.co/3wnC6SgIOJud.png" alt="React Logo Banner" className="card-img"/>
     )
 }
 
