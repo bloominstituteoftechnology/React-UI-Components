0addd75c4d120d6d2445e53f33dd9d3b228cc7e2
James Basile <jbasile14@gmail.com>
2019-02-11 18:04:20 -0500

    initial commit, project one complete

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,23 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return (
+        <div className="header-container">
+            
+            <ImageThumbnail />
+            <HeaderTitle />
+
+        
+        </div>
+    );
+};
+
+
+
+
+
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Header.css';
+
+
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title">
+
+            <h1 className="big-lambda">Lambda School</h1>
+            <p className="small-lambda">@LambdaSchool - 26 jan</p>
+            <p className="text-content">Lets learn React by building simple interface components. Try not to overthink it, just keep it simple and have fun. Once you feel
+             comfortable using components you are well on your way to mastering React!</p>
+
+        </div>
+    );
+};
+
+
+
+
+export default HeaderTitle;
