e0f8cf41c77cf97e8a17b78ef40bac8b227efd84
Jaouad Sarouti <jadsarouti@outlook.com>
2019-02-11 18:04:10 -0500

    changeColor footer simple interaction

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom"
 class Footer extends React.Component{
     constructor(){
         super()
+        this.addHandler = this.addHandler.bind(this)
+        this.addHandlerHeart = this.addHandlerHeart.bind(this)
+        this.changeColorHandler = this.changeColorHandler.bind(this)
         this.state = {
             count : 0,
             countHeart : 0,
-            comment : []
+            color : ""
         }
     }
 
@@ -27,11 +30,19 @@ class Footer extends React.Component{
         })
     }
 
+
+    changeColorHandler=()=>{
+      this.setState({
+          color : "red"
+      })
+    }
+
+
     render(){
     return (
         <div style={{display:"flex",width:"50%",justifyContent:"space-between",marginLeft:"10%",marginTop:"4%"}}>
-            <div ><i class="comment outline icon"></i></div>
-            <div><i class="sync alternate icon"></i>0</div>
+            <div><i class="comment outline icon"></i></div>
+            <div onMouseLeave={this.changeColorHandler} style={{color:this.state.color}}><i class="sync alternate icon"></i>0</div>
             <div onClick={this.addHandlerHeart}>{this.state.countHeart}<i class="heart outline icon"></i></div>
             <div onClick={this.addHandler}>{this.state.count}<i class="envelope outline icon"></i></div>    
         </div>
