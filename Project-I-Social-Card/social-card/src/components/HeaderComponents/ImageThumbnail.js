fe1ae0fd6737f796dd4fb0dfdd264e4132091d59
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-02-11 17:16:18 -0500

    created CardContainer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const = () => { return (
+    <div className="imgThumb">
+        <img alt="img" className="imgThumb" src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png "/>
+    </div>);};
+export default ImageThumbnail;
