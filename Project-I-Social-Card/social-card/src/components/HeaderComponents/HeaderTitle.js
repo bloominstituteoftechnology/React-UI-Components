501e060d736ab085ef9828f08d97caa64b9cdd8a
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2019-02-11 08:30:56 +0000

    Add <HeaderTitle />

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <p>{props.handle}</p>
+            <p>{props.date}</p>
+        </div>
+    )
+}
+
+export default HeaderTitle
