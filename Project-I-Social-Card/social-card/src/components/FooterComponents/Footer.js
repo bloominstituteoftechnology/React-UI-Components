747c1c1eca21b5904f938cdf46fe94e75c064326
Jaouad Sarouti <jadsarouti@outlook.com>
2019-02-11 17:21:20 -0500

    switching footer to a class compo

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,7 +5,9 @@ class Footer extends React.Component{
     constructor(){
         super()
         this.state = {
-            count : 0
+            count : 0,
+            countHeart : 0,
+            comment : []
         }
     }
 
@@ -17,13 +19,21 @@ class Footer extends React.Component{
         })
     }
 
+    addHandlerHeart =()=>{
+        this.setState ((prevS)=>{
+            return {
+                countHeart :prevS.countHeart+1
+            }
+        })
+    }
+
     render(){
     return (
         <div style={{display:"flex",width:"50%",justifyContent:"space-between",marginLeft:"10%",marginTop:"4%"}}>
-            <div onClick={this.addHandler}>{this.state.count}<i class="comment outline icon"></i></div>
+            <div ><i class="comment outline icon"></i></div>
             <div><i class="sync alternate icon"></i>0</div>
-            <div><i class="heart outline icon"></i>0</div>
-            <div><i class="envelope outline icon"></i>0</div>    
+            <div onClick={this.addHandlerHeart}>{this.state.countHeart}<i class="heart outline icon"></i></div>
+            <div onClick={this.addHandler}>{this.state.count}<i class="envelope outline icon"></i></div>    
         </div>
     )
     }
