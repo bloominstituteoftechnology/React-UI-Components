55c110afee5ab4da1292c1440a18c97152e271ae
Corey Gumbs <cgumbs@Coreys-MBP-3.home>
2019-02-11 18:11:51 -0500

    [chore] added CardBannerImage, CardBannerDescription components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import CardBannerImage from './CardBannerImage';
+import CardBannerDescription from './CardBannerDescription';
 
 
 const CardBanner = () => {
     return(
         <div className="card-banner-container">
-            card   bannercontainer
+            <div className="card-banner-img">
+                <CardBannerImage />
+            </div>
+            <div className="card-banner-description">
+                <CardBannerDescription />
+            </div>
         </div>
     );
 }
---
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBannerDescription.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CardBannerDescription = () => {
+    return(
+        <div className="card-txt-container">
+            <p className="card-header"></p>
+            <p className="card-txt"></p>
+            <p className="card-link"></p>
+        </div>
+    );
+}
+
+export default CardBannerDescription;
