9df694c6ca2f54673e7580880415cbe0759dd0c3
erin-koen <erin.koen@gmail.com>
2019-02-11 15:16:14 -0500

    header react scaffolded

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle () {
+    return (
+        <div className = "header-title"> 
+            <h2 className ="display-name">Lambda School</h2>
+            <p className = "link-text">@LambdaSchool - 26 Jan</p>  
+        </div>
+    );
+};
+
+export default HeaderTitle;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return (
+        <img className="header-image" href ="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"></img>
+    );
+};
+
+export default ImageThumbnail;
