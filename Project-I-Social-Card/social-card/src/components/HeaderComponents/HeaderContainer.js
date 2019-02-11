43558e61d1766c2559d51a36c215b91a1cf90042
nevets615 <surj316@yahoo.com>
2019-02-11 15:48:03 -0500

    initial pull request

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import HeaderContent from './components/HeaderContent';
+import HeaderTitle from './components/HeaderTitle';
+import imageThumnail from '/components/imageThumnail';
 import './Header.css';
+
+
+function App() {
+    return (
+      <div className="App">
+        <HeaderContent />
+        <HeaderTitle />
+        <imageThumnail />
+      </div>
+    );
+  }
+  
+  const rootElement = document.getElementById('root');
+  ReactDOM.render(<App />, rootElement);
