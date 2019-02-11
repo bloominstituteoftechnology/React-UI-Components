198fcc1d354d1af080f49724b1007627a00f13c1
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-02-11 17:41:28 -0500

    made card content and links

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Card.css';
 
 const = () => { return (
-    <div className="banner-lord">
+    <div className="banner-lord" href="https://react.org">
         <img alt="banner" className="banner-lord" src=" https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"/>
     </div>);};
-export default ImageThumbnail;
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent =()=>{return(
+<div className="card-content">
+<h1 className="card-content-h1">Get started with React</h1>
+<p className="card-content-p">React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+<a className="card-content-a" href="http://reactjs.org"> reactjs.org</a>
+</div>
+);
+};
+export default CardContent;
