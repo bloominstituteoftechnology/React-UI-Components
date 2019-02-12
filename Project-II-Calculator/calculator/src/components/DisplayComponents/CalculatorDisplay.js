69674feaf3105b7e44e3ee813f264a9c025e0ff0
Corey Gumbs <cgumbs@Coreys-MBP-3.home>
2019-02-12 16:20:44 -0500

    [init] initial file setup

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = () => {
+    return(
+        <div className="calc-display">
+       test
+        </div>
+    );
+}
+
+export default CalculatorDisplay;
