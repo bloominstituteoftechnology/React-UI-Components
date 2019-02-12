1e987ba56dad0c085854bf0405bcf267131b020f
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-02-12 15:47:17 -0500

    reset for async.

--- Day-II-Calculator/calculator/src/index.js
+++ Day-II-Calculator/calculator/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import DynamicButtonGray from './components/ButtonComponents/NumberButtonGray';
-import DynamicButtonRed from './components/ButtonComponents/NumberButtonRed';
-import ButtonWide from './components/ButtonComponents/WideGray';
+
 import App from './App';
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// <DynamicButtonGray buttonText='8'/>
-// <DynamicButtonGray buttonText='9'/>
-// <DynamicButtonRed buttonText='X'/>
+ReactDOM.render( < App / > , document.getElementById('root'));
