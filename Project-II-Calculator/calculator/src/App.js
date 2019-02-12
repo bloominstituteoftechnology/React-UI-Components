ae08396ac000442546b4183dfaed79fd7e0be4c8
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-02-12 16:33:21 -0500

    building calculator

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton.js';
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
+      <h3>React Calculator</h3>
+    
       <p>
         <strong>
           Don't forget to `default export` your components and import them here
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return( 
+    <div className="num-b">
+    {props.text}
+    </div>
+
+                            );
+                            }
+export default NumberButton;
