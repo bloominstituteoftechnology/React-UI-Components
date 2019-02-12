fc9be17460436e2be40df1cc135abd63ce74a94d
erin-koen <erin.koen@gmail.com>
2019-02-11 19:05:13 -0500

    moment added for dynamic dates

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Header.css';
+import Moment from 'moment';
 
 function HeaderTitle () {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    const month = months[Moment().month()];
+    const day = Moment().day();
+    const year = Moment().year();
     return (
+
         <div className = "header-title"> 
             <h3 className ="display-name">Lambda School</h3>
             <p className ="link-text">@LambdaSchool</p>  
-            <p className ="link-text">Feb 11, 2019</p>
+            <p className ="link-text">{month} {day}, {year}</p>
         </div>
     );
 };
