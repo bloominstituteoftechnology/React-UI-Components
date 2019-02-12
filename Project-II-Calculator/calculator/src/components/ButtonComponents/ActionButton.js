12490460770d234b531464d8c03e2e7f5fb7051a
UnknownMonk <smodcartoon@gmail.com>
2019-02-12 16:20:10 -0500

    Added buttons action buttons and css for project

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+  return (
+    <button onClick={props.onClick} className={props.buttonStyle}>
+      {props.text}
+    </button>
+  );
+};
+
+export default ActionButton;
