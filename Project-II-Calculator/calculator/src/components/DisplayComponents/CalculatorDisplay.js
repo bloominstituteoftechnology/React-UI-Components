afe289b66dbbcb9f86e25ccbce2e2474bba56adf
Corey Gumbs <cgumbs@Coreys-MBP-3.home>
2019-02-12 19:05:42 -0500

    [chore] completed Components and Styling

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -3,13 +3,7 @@ import React from 'react';
 
 const CalculatorDisplay = props => {
     return(
-        <div className="calc-display">
-            <strong>
-                <h1 className="numDisplay">
-                    {props.numDisplay}
-                </h1>
-            </strong>
-        </div>
+        <h1 className="numDisplay">{props.numDisplay}</h1>
     );
 }
 
