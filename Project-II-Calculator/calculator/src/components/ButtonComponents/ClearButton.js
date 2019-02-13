6ebfaecdc77ad0c2a0b792dc40628730d6f14ef3
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-02-12 19:10:53 -0500

    added css styles

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Button.css';
+
+const ClearButton = props => {
+    return (<div className={`${props.buttonStyles}`}>
+            {props.text}</div>
+                                                );
+                                                }
+export default ClearButton;
