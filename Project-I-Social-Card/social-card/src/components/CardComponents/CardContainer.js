d01806aa68df9ab7a0a3524fe8bcb558d9b30b94
Corey Gumbs <cgumbs@Coreys-MBP-3.home>
2019-02-12 21:57:01 -0500

    [chore] completed & fixed Card Component architecture and styling

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import CardBanner from './CardBanner';
 import CardContent from './CardContent';
 
 
 const CardContainer = () => {
     return(
+        <a href="https://www.reactjs.org" className="card-link" target="_blank">
         <div className="card-container">
+            <CardBanner />
             <CardContent />
         </div>
+        </a>
     );
 }
 
