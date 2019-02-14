278ba9bff83febcccd879c88aff3bab9ce146aa9
novinary <novina.dev@gmail.com>
2019-02-14 15:26:57 +0000

    Initial setup

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 
 const App = () => {
   return (
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderContainer = () => {
+    return(
+        <div>NovinaTest</div>
+    );
+}
+
+export default HeaderContainer; 
