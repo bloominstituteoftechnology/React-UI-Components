89bae29256445db5000fbc9eb1183738a9687469
cori-peck <cori.peck@ymail.com>
2019-02-15 15:18:37 -0600

    Finished footer styles, added moment.js. Project Complete MVP & stretch

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
+import Moment from 'moment';
 
 function HeaderTitle() {
+    const dateStamp = Moment().format('Do MMM')
     return (
         <div classname="title">
             <h4 className="hdText">Lambda School</h4>
             <p className="handle">@LambdaSchool</p>
-            <p className="date"> - 14 feb</p>
+            <p className="date">{dateStamp}</p>
         </div>
     )
 }
