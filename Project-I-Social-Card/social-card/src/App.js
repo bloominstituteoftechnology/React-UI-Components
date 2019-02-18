70cfdc93d945f6ea6c04ba95196697d785c92b7a
jkaddu <jkaddu@yahoo.com>
2019-02-18 13:01:01 -0800

    pull

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
+import FooterContainer from './components/FooterComponents/Footer';
+
 const App = () => {
   return (
     <div>
