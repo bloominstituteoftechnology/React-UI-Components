1ae5cf193c06c27a3fd27351d63e4dcc21f0d83c
paintedlbird7 <rosaperezlarios@gmail.com>
2019-02-18 16:05:37 -0800

    ImageThumbnail & HeaderTitle done

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return <h1>Lambda School</h1>;
+    //  <h2>@LambdaSchool</h2>;
+  };
+  
+  export default HeaderTitle;
