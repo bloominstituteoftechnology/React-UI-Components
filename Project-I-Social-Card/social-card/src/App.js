89ec4e4b86127bfe16dc34be3c323b4ea4f59ac3
Marc Thiongo <marcthuo@gmail.com>
2019-02-21 02:43:35 -0500

    app.css edit

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -4,7 +4,7 @@ import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
 import CardContainer from "./components/CardComponents/CardContainer";
 
 const App = () => (
-    <div>
+    <div class="App">
       <HeaderContainer />
       <CardContainer />
     </div>  
