2307d7085363c2b71dfb7a3e2bb121ea9aa9ff5b
Rane Wallin <rane@ranewallin.com>
2019-02-27 06:42:13 -0700

    Scaffold HeaderContainer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,12 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderContainer = () => {
+  return (
+    <div>
+      <h1>This is a container</h1>
+    </div>
+  );
+};
+
+export default HeaderContainer;
