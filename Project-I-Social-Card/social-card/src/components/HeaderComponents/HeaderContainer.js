54583d7a6a9fc5ab61f70ec1b2122dfe660b151d
Dexter Lewis <dexter.lewis@gmail.com>
2019-02-27 18:43:36 -0500

    create initial card and loaded image

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderContent from './HeaderContent'
+import HeaderTitle from './HeaderTitle'
+import ImageThumbnail from './ImageThumbnail'
+
+function HeaderContainer() {
+    return (
+        <div>
+            <ImageThumbnail />
+        </div>
+    )
+}
+
+export default HeaderContainer
