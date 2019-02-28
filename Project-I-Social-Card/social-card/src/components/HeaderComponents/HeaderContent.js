f85e9c4986772f23a23b602f3058427082ab8f18
David <digital0david@gmail.com>
2019-02-27 20:31:34 -0500

    header 1

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContent () {
+    return (
+        <p>Let's learn react by building simple interface with components. Don't try to overthink interface, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React.</p>
+    )
+}
+
+export default HeaderContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div class="Title">
+            <h1>Lambda</h1>
+            <h2>@LambdaSchool &middot; 27 feb</h2>  
+        </div>
+    );
+}
+
+export default HeaderTitle;
