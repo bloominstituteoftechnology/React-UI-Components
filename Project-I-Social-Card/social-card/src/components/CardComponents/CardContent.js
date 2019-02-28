ed79c00966cf2497415684878c2ed67fdeb7d167
JustinAndrade <j.andrade.john@gmail.com>
2019-02-27 19:25:16 -0500

    base app created

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <React.Fragment>
+            <h2>Get started with React</h2>
+            <p>React Makes it painless to create interactive UIs.  Design simple views for each state in your application.</p>
+            <a href="reactjs.org">reactjs.org</a>
+        </React.Fragment>
+    );
+}
+
+export default CardContent
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return (
+      <div className ="headerContainer">
+      <div className ="imageThumbnail">
+        <ImageThumbnail />
+        </div>
+        <div className ="headerContent">
+        <HeaderTitle />
+        <HeaderContent />
+        </div>
+      </div>
+    );
+  };
+  
+  export default HeaderContainer;
