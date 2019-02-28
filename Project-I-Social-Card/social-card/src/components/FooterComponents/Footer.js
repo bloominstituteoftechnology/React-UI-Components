e6f0759b85b178370264961b14223e022cefd8f8
Dexter Lewis <dexter.lewis@gmail.com>
2019-02-27 21:04:54 -0500

    Add footer

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Footer.css"
+function Footer(){
+    return (
+        <footer>
+            <i className="far fa-comment"></i>
+            <i className="fas fa-sync"> 6</i>
+            <i className="far fa-heart"> 4</i>
+            <i className="far fa-envelope"></i>
+        </footer>
+    )
+}
+
+export default Footer
