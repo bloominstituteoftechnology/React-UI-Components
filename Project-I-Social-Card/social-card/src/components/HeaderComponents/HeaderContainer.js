72be3a985a17c2ebaad1308b3c8553af82c42e37
shayaanakbar <shayaanakbar1106@gmail.com>
2019-02-27 20:59:28 -0600

    Initial

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+import ImageThumbNail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+
+
+function HeaderContainer() {
+    return (
+        <div className="HeadCon">
+            <ImageThumbNail />
+            <HeaderTitle />
+        </div>
+    );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div className="head">
+            <div className="head-title">
+                <h2>Lambda School</h2>
+                <a>@LambdaSchool</a>
+                <p>27 Feb</p>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderTitle;
