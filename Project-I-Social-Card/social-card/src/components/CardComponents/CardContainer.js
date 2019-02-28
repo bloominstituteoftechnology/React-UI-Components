c31a6159c39f18e27e8267d9fd911ef421ab7bac
mmorpchicago <mmohamed.rp@gmail.com>
2019-02-27 20:05:06 -0600

    Completed MVP without footer

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+function CardContainer() {
+    return (
+        <div>
+            <CardBanner imgSrc="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"/>
+            <CardContent />
+        </div>
+    );
+}
+
+export default CardContainer;
