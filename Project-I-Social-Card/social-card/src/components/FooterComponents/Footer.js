33b21eb0d47bfcb357bc8653a3372765d2a5152c
Jason Cook <the.root.user@gmail.com>
2019-02-28 08:00:26 -0500

    Try to enable onClick icons

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './Footer.css';
 import {
 FaComment, FaSync, FaHeart, FaEnvelope
-} from 'react-icons/fa-outline';
+} from 'react-icons/fa';
 
 const Footer = () => {
     return (
-    <React.Fragment>
-        <FaComment/> 
-        <FaSync/> 
-        <FaHeart/> 
-        <FaEnvelope/> 
-    </React.Fragment>
+    <div className="footer">
+        <FaComment size="1.8em" onClick=""/> 
+        <FaSync size="1.8em" onClick=""/> 
+        <FaHeart size="1.8em" onClick=""/> 
+        <FaEnvelope size="1.8em" onClick=""/> 
+    </div>
     );
 } 
 
