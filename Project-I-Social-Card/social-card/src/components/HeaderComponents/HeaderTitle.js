88b1a9158c19bd21813e9bcaa2503dadda4e9e5c
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-02-27 18:38:53 -0800

    Deleted hardcoded time-stamp and added current date formated with moment.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
+
+const date = new Date();
+const format = 'D MMM';
+let time = moment(date).format(format);
+let currentDate = time.toLowerCase();
 
 function HeaderTitle (props) {
     return (
@@ -7,7 +13,7 @@ function HeaderTitle (props) {
             <div className="header-title">
                 <h4>{props.title}</h4>
                 <p className='small-text'>@{props.title}</p>
-                <p className='small-text'>{props.timestamp}</p>
+                <p className='small-text'>{currentDate}</p>
             </div>
         </div>  
     );
