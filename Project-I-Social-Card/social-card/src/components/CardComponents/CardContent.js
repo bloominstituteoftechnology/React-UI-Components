cf3111accea254d4fd6657766e43b05dc62b835e
Scott Grobe <gitgrobe@gmail.com>
2019-02-27 21:51:28 -0500

    open link in new tab

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -6,7 +6,7 @@ function CardContent () {
         <div className="card-content">
             <h1>Getting Started with React</h1>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-            <a href="https://reactjs.org/">reactjs.org</a>
+            <a href="https://reactjs.org/" target="_blank">reactjs.org</a>
         </div>
     );
 }
