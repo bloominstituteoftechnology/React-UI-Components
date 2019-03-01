65681f836e4ec9d861e629c7bf16212e18ebbb9b
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-03-01 15:42:50 -0800

    Finished Button component, added focus styling for buttons

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+function ActionButton (props) {
+    return (
+        <button className={props.btnClassName}>{props.content}</button>
+    );
+} 
+
+
+export default ActionButton;
