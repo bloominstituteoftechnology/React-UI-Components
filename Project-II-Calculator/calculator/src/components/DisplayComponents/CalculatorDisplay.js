d9f4ec7a6ab1b30cd2ddb6a895f6be1e73a03920
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-03-01 12:00:37 -0800

    Started working on the Calculator Project. Build Calculator display component

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay (props) {
+    return (
+        <div>
+            <h1 className='calculator-display'>0</h1>
+        </div>
+    );
+} 
+
+
+export default CalculatorDisplay;
