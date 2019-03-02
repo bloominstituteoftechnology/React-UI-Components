ae695d816bafc469a08215f232064833b770cdf3
jacquetta <jacquettavb@hotmail.com>
2019-03-02 14:56:14 -0600

    updated MVP

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -12,33 +12,33 @@ const Calculator = () => {
 
         <div className="top row">
           <ActionButton className="med" text="clear" />
-          <ActionButton className="operator" text="&#247;" />
+          <NumberButton className="operator" text="&#247;" />
         </div>
 
         <div className="topNums row">
           <NumberButton className="sm" text="7" />
           <NumberButton className="sm" text="8" />
           <NumberButton className="sm" text="9" />
-          <ActionButton className="operator" text="&#215;" />
+          <NumberButton className="operator" text="&#215;" />
         </div>
 
         <div className="middleNums row">
           <NumberButton className="sm" text="4" />
           <NumberButton className="sm" text="5" />
           <NumberButton className="sm" text="6" />
-          <ActionButton className="operator" text="&#8722;" />
+          <NumberButton className="operator" text="&#8722;" />
         </div>
 
         <div className="bottomNums row">
           <NumberButton className="sm" text="1" />
           <NumberButton className="sm" text="2" />
           <NumberButton className="sm" text="3" />
-          <ActionButton className="operator" text="&#43;" />
+          <NumberButton className="operator" text="&#43;" />
         </div>
 
         <div className="bottom row">
           <ActionButton className='med' text='0' />
-          <ActionButton className='operator' text='&#61;' />
+          <NumberButton className='operator' text='&#61;' />
         </div>
       </div>
     );
