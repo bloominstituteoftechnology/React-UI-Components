53f31775c2c97ea0cdac0ccefced3975a7c578a2
JustinAndrade <j.andrade.john@gmail.com>
2019-03-02 13:16:13 -0500

    Basic functionality added

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => <button className="action">{props.action}</button>;
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => <button className="number">{props.number}</button>
+
+export default NumberButton;
