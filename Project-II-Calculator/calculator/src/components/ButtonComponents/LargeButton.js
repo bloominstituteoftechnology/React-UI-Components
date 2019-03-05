93621c6edc317692d0f64bb2eed5202dfb460c48
MrT3313 <rturgeon13@gmail.com>
2019-03-04 19:06:50 -0600

    last inline style removed

--- Project-II-Calculator/calculator/src/components/ButtonComponents/LargeButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/LargeButton.js
@@ -4,14 +4,8 @@ import './Button.css';
 const LargeButton = props => {
     const { displayProps } = props;
 
-
-    const buttonStyle = {
-        borderRight: '1px solid black'
-    };
-
-
     return (
-        <div style={buttonStyle} className='largeButton'>
+        <div className='largeButton'>
             {displayProps.textContent}
         </div>    
     )
