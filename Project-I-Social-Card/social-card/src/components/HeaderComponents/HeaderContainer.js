c861e50a58273a32fda070d4db77636a7129c975
Tara Sherman <taradelin@gmail.com>
2019-03-13 19:40:48 -0400

    header content, title, image, container

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
+
+// import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+// import ImageThumbnail from './ImageThumbnail';
+
 import './Header.css';
+
+const HeaderContainer = props => {
+    return (
+        <div className = "header-container">
+            <HeaderContent />        
+        </div>
+    );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+    return (
+        <div className = "header-container">
+            <h1 className = "header-title">
+                Lambda School
+            </h1>
+            <div className = "handle">
+                @LambdaSchool
+            </div>
+            <div className = "timestamp">26 jan
+            </div>
+        </div>
+    );
+};
+
+export default HeaderTitle;
