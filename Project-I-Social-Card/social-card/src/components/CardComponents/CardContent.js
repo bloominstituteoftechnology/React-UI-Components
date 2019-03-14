32d8eaee0c858b4650be34b7be0681d289f4aaf8
peter-wolfe <peterwolfe35@gmail.com>
2019-03-13 18:37:42 -0800

    Finished MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className="CardContentText">
+            <h2>Get Started with React</h2>
+            <p>React makes it painless to make interactive UI's. Design simple views for each state in your application.</p>
+            <a href="https://www.reactjs.org">reactjs.org</a>
+            
+        </div>
+    );
+}
+
+export default CardContent;
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="FooterContainer">
+            <div className="FooterIcons">
+                <i className="fa fa-comment-o IconSize"></i>
+                <i className="fa fa-refresh IconSize"></i>
+                <i className="fa fa-heart-o IconSize"></i>
+                <i className="fa fa-envelope IconSize"></i>
+            </div>
+        </div>
+    );
+};
+
+export default Footer; 
