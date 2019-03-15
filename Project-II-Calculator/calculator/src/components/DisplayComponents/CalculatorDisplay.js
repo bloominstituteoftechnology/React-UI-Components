85714487e9850d6524eef7415df2e2df4841712e
Mariana <mhuh0176@gmail.com>
2019-03-15 13:57:01 -0700

    added some added components imports and exports and styling

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => (
+  <div className='display'>{props.text}
+  </div>
+);
+
+export default CalculatorDisplay;
