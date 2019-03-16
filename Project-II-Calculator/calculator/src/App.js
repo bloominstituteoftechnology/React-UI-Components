a85ddf0d7129742a067c9fd9df7ac6b33ecdae72
Mariana <mhuh0176@gmail.com>
2019-03-15 17:06:17 -0700

    minor changes to styling

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -15,29 +15,29 @@ const App = () => {
 
       <div className='row'>
         <ActionButton buttonStyle='clear' text='clear' />
-        <NumberButton buttonStyle='operation' text='÷' />
+        <ActionButton buttonStyle='action' text='÷' />
       </div>
       <div className='row'>
         <NumberButton buttonStyle='number' text='7' />
         <NumberButton buttonStyle='number' text='8' />
         <NumberButton buttonStyle='number' text='9' />
-        <NumberButton buttonStyle='operation' text='×' />
+        <ActionButton buttonStyle='action' text='×' />
       </div>
       <div className='row'>
         <NumberButton buttonStyle='number' text='4' />
         <NumberButton buttonStyle='number' text='5' />
         <NumberButton buttonStyle='number' text='6' />
-        <NumberButton buttonStyle='operation' text='－' />
+        <ActionButton buttonStyle='action' text='－' />
       </div>
       <div className='row'>
         <NumberButton buttonStyle='number' text='1' />
         <NumberButton buttonStyle='number' text='2' />
         <NumberButton buttonStyle='number' text='3' />
-        <NumberButton buttonStyle='operation' text='﹢' />
+        <ActionButton buttonStyle='action' text='﹢' />
       </div>
       <div className='row'>
         <NumberButton buttonStyle='zero' text='0' />
-        <NumberButton buttonStyle='operation' text='﹦' />
+        <ActionButton buttonStyle='action' text='﹦' />
       </div>
     </div>
   );
