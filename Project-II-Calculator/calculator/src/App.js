bef43c25d070c1763f6eb2395ac6501c0b7cfb89
Divya Nair <divnairs@gmail.com>
2019-03-18 15:43:51 -0700

    add action button component

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,13 +2,19 @@ import React from 'react'
 import './App.css'
 
 import NumberButton from './components/ButtonComponents/NumberButton'
+import ActionButton from './components/ButtonComponents/ActionButton'
 
 const App = () => {
   const text = [1, 2, 3, 4, 5, 6, 7, 8, 9, '=', '+', '-', '*', '/']
+  const action = [0, 'clear']
+
   return (
     <div className="app">
       {text.map(text => (
         <NumberButton text={text} />
+      ))}
+      {action.map(action => (
+        <ActionButton action={action} />
       ))}
     </div>
   )
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return <button>{props.action}</button>
+  }
+  
+  export default ActionButton
