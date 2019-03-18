73858cfedf2afca56576b267f651d81c71a84f93
Henry Blevins <heblevi@gmail.com>
2019-03-18 18:13:17 -0400

    Actions work

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -10,16 +10,46 @@ class App extends React.Component {
                        stack: false,
                        action: false };
         this.handleNumber = this.handleNumber.bind(this);
+        this.handleAction = this.handleAction.bind(this);
+        this.actions = [{action: (a, b) => a / b, text: "÷"},
+                        {action: (a, b) => a * b, text: "×"},
+                        {action: (a, b) => a - b, text: "−"},
+                        {action: (a, b) => a + b, text: "+"},
+                        {action: true, text: "="}];
     }
     handleNumber(num) {
-        this.setState({ total: (this.state.stack === false ? num : (10 * this.state.total) + num),
-                        stack: (this.state.stack === false ? this.state.total : this.state.stack) });
+        this.setState({ total: (this.state.stack ? num : (10 * this.state.total) + num),
+                        stack: (this.state.stack ? this.state.total : this.state.stack) });
+    }
+    handleAction(action) {
+        if (action === true) { // '=' triggers stacked action
+            this.setState({total: this.state.action ? this.state.action(this.state.stack, this.state.total) : this.state.total,
+                           action: false });
+        } else if (action === false) { // 'clear' clears total and stacks
+            this.setState({ total: 0,
+                            stack: false,
+                            action: false });
+        } else if (action) { // otherwise, stack action and total
+            this.setState({ action: action,
+                            stack: this.state.total });
+        }
     }
     render() {
         return <div className="calculator">
                  <Display total={this.state.total} />
-                 {[...Array(9)].map((_, idx) => <NumberButton key={idx+1} number={idx+1} handleNumber={this.handleNumber}/>)}
+                 <ActionButton action={false}
+                               text={'clear'}
+                               handleAction={this.handleAction}/>
+                 {[...Array(9)].map((_, idx) =>
+                                    <NumberButton key={idx+1}
+                                                  number={idx+1}
+                                                  handleNumber={this.handleNumber}/>)}
                  <NumberButton key={0} number={0} handleNumber={this.handleNumber}/>
+                 {this.actions.map(({action, text}, idx) =>
+                                   <ActionButton key={idx}
+                                                 action={action}
+                                                 text={text}
+                                                 handleAction={this.handleAction}/>)}
                </div>;
     }
 }
