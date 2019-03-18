9fbe74cae1360822196141b6037de1ce6210975f
Henry Blevins <heblevi@gmail.com>
2019-03-18 14:39:58 -0400

    Sample post content

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import './App.css';
 
+const post = {
+    header: {name: "Lambda School",
+             username: "@LambdaSchool",
+             img: "https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png",
+             date: "26 jan",
+             content: "Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!"},
+    card: {img: "https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png",
+           title: "Get started with React",
+           content: "React makes it painless to create interactive UIs. Design simple views for each state in your application",
+           url: "https://www.reactjs.org"},
+    shares: 6,
+    likes: 4,
+};
+
+class Post extends React.Component {
+    render() {
+        return <div><h2>{post.header.name}</h2><p>{post.header.content}</p></div>;
+    }
+}
+
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Social Card!</h3>
-      <p>
-        Begin by exploring the `components` directory. You'll notice we have a
-        few files that we've already included in there to get you started right
-        away building components. You'll need to make sure you include your
-        components that you build in this file to watch your app come to life
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
+      <Post />
   );
 };
 
