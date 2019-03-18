bfe31b98b1cecb642a977a2bf6736eb7f37fcd02
Lambda_School_Loaner_93 <lambda_school_loaner_93@Lambda-School-Loaner-93s-MacBook-Pro.local>
2019-03-18 16:41:55 -0400

    1st save

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import HeaderContainer from "./components/HeaderComponents/HeaderContainer"
 
 const App = () => {
   return (
     <div>
+      <HeaderContainer/>
       <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
@@ -14,7 +16,7 @@ const App = () => {
       <p>
         <strong>
           Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
+          inside of this file in order to make them wo
         </strong>
       </p>
     </div>
