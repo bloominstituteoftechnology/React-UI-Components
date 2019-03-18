fe8f27cc84637888191bfa272af8c754e7ac12d9
Divya Nair <divnairs@gmail.com>
2019-03-18 15:40:23 -0700

    add number button component

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import NumberButton from './components/ButtonComponents/NumberButton'
 
 const App = () => {
+  const text = [1, 2, 3, 4, 5, 6, 7, 8, 9, '=', '+', '-', '*', '/']
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="app">
+      {text.map(text => (
+        <NumberButton text={text} />
+      ))}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Button.css';
+import React from 'react'
+import './Button.css'
+
+const NumberButton = props => {
+  return <button>{props.text}</button>
+}
+
+export default NumberButton
