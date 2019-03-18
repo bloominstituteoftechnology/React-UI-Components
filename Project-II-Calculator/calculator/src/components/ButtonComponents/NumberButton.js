761b44e9acb433d88ea81512afa92e1c6e9c20b0
peter-wolfe <peterwolfe35@gmail.com>
2019-03-17 19:25:10 -0700

    Added Number Buttons. MVP complete. Still need to make pixel perfect

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -5,21 +5,24 @@ import './Button.css';
 const NumberButtons = () => {
     return (
         <div>
-            <div className="action-clear-button">Clear</div>
-            <div className="number-container-flex">
-                <div className="number-container">7</div>
-                <div className="number-container">8</div>
-                <div className="number-container">9</div>
-            </div>
-            <div className="number-container-flex">
-                <div className="number-container">6</div>
-                <div className="number-container">5</div>
-                <div className="number-container">4</div>
-            </div>
-            <div className="number-container-flex">
-                <div className="number-container">3</div>
-                <div className="number-container">2</div>
-                <div className="number-container">1</div>
+            <div className="button-position">
+                <div className="action-clear-button">Clear</div>
+                <div className="number-container-flex">
+                    <div className="number-container">7</div>
+                    <div className="number-container">8</div>
+                    <div className="number-container">9</div>
+                </div>
+                <div className="number-container-flex">
+                    <div className="number-container">4</div>
+                    <div className="number-container">5</div>
+                    <div className="number-container">6</div>
+                </div>
+                <div className="number-container-flex">
+                    <div className="number-container">1</div>
+                    <div className="number-container">2</div>
+                    <div className="number-container">3</div>
+                </div>
+                <div className="action-clear-button number-container-zero">0</div>
             </div>
         </div>
     );
