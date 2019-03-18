78536ddb0acf3f50bc6c1b447400aaa03738aa0f
cammac <camm@rocktechcomputer.com>
2019-03-18 13:57:44 -0700

    added header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div id='title'>
+      <h2>Lambda School</h2>
+      <span>@LambdaSchool</span>
+      <span id="date"> 18 Mar</span>
+
+    </div>
+  );
+}
+
+export default HeaderTitle;
