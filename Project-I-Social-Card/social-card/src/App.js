1e8fc47e7c8c598a1f72560e9d1197085791c80d
cammac <camm@rocktechcomputer.com>
2019-03-18 14:09:08 -0700

    added card components

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 
 const App = () => {
   return (
     <div id='root'>
       <HeaderContainer />
-
+      <CardContainer />
     </div>
   );
 };
