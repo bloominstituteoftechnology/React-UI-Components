ad281478ddcfc7171b7bdf9c7a3dd765aefde991
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-03-18 15:48:37 -0400

    Ethan Baugher

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+function ImageThumbnail(){
+    return (
+        <div className="image-thumbnail">
+            <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="Lambda Logo"/>
+        </div>
+    );
+}
+
+export default ImageThumbnail;
