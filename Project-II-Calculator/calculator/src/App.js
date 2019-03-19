4848212e0aa9987d9beb2690ef6bc8c13510c3fc
Divya Nair <divnairs@gmail.com>
2019-03-19 10:40:53 -0700

    import arithmetic fonts

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -7,20 +7,20 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 
 const App = () => {
   const button = [
-    ['/', 'symbol'],
+    [<i class="fas fa-divide" />, 'symbol'],
     [7, 'number'],
     [8, 'number'],
     [9, 'number'],
-    ['*', 'symbol'],
+    [<i class="fas fa-times" />, 'symbol'],
     [4, 'number'],
     [5, 'number'],
     [6, 'number'],
-    ['-', 'symbol'],
+    [<i class="fas fa-minus" />, 'symbol'],
     [1, 'number'],
     [2, 'number'],
     [3, 'number'],
-    ['+', 'symbol'],
-    ['=', 'symbol']
+    [<i class="fas fa-plus" />, 'symbol'],
+    [<i class="fas fa-equals" />, 'symbol']
   ]
 
   return (
