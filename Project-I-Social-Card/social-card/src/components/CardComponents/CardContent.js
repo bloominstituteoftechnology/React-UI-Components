46226c3b6f71f6acadd8df02083cd7c5f5f36c29
deanovision <rodeanfraser@gmail.com>
2019-03-19 10:06:31 -0400

    finished mvp and added footer for stretch goal

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+const CardContent = () => {
+  return (
+    <p>
+      <strong>
+        Don't forget to `default export` your components and import them here
+        inside of this file in order to make them work.
+      </strong>
+    </p>
+  );
+};
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+const HeaderContent = () => {
+  return (
+    <p>
+      Begin by exploring the `components` directory. You'll notice we have a few
+      files that we've already included in there to get you started right away
+      building components. You'll need to make sure you include your components
+      that you build in this file to watch your app come to life
+    </p>
+  );
+};
+export default HeaderContent;
