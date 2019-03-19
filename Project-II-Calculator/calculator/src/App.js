4f3828a10c5ea3294831a910f0c8421bd48dc637
ntsum <natsukajiwaram@gmail.com>
2019-03-19 16:13:49 -0400

    holy crap that styling was not bad at all

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -9,32 +9,29 @@ const App = () => {
   return (
     <div className='container'>
       <CalculatorDisplay buttonStyle='disply' text='0' />
-      <div className='one'>
+      
         <ActionButton buttonStyle='clear' text='clear' />
         <NumberButton buttonStyle='sign' text='÷' />
-      </div>
-      <div className='two'>
+      
         <NumberButton buttonStyle='number' text='7' />
         <NumberButton buttonStyle='number' text='8' />
         <NumberButton buttonStyle='number' text='9' />
         <NumberButton buttonStyle='sign' text='×' />
-      </div>
-      <div className='three'>
+     
         <NumberButton buttonStyle='number' text='4' />
         <NumberButton buttonStyle='number' text='5' />
         <NumberButton buttonStyle='number' text='6' />
         <NumberButton buttonStyle='sign' text='−' />
-      </div>
-      <div className='four'>
+      
         <NumberButton buttonStyle='number' text='1' />
         <NumberButton buttonStyle='number' text='2' />
         <NumberButton buttonStyle='number' text='3' />
         <NumberButton buttonStyle='sign' text='+' />
-      </div>
-      <div className='five'>
+      
+      
         <NumberButton buttonStyle='zero' text='0' />
         <NumberButton buttonStyle='sign' text='=' />
-      </div>
+      
     </div>
   );
 };
