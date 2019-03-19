2a4e92c7c25900ea39f1ae6b9cbcacd44206834d
deanovision <rodeanfraser@gmail.com>
2019-03-19 16:11:39 -0400

    initial commit started working on NumberButton component

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,53 @@
 import React from 'react';
 import './Button.css';
+const numbersObj = [
+  {display: "0",
+   style: "action-button"
+  },
+  {display: "1",
+    style: "number-button"
+  },
+  {display: "2",
+    style: "number-button"
+  },
+  {display: "3",
+    style: "number-button"
+  },
+  {display: "4",
+    style: "number-button"
+  },
+  {display: "5",
+    style: "number-button"
+  },
+  {display: "6",
+    style: "number-button"
+  },
+  {display: "7",
+    style: "number-button"
+  },
+  {display: "8",
+    style: "number-button"
+  },
+  {display: "9",
+    style: "number-button"
+  },
+  {display: "Clear",
+    style: "action-button"
+  }
+]
+const NumberButon = ()=> {
+  return(
+    <div>
+      
+    </div>
+  );
+}
+function numbers() {
+  return (
+    <div className="number-button">
+    {numbersObj.map(function(numbers){
+      return <NumberButton numberProps={numbers}/>
+    });}
+    </div>
+  );
+}
