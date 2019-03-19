bebc7e4a3e988f773f94812deba7702bbf128335
ntsum <natsukajiwaram@gmail.com>
2019-03-19 16:05:29 -0400

    mvp complete, working on styling

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className='container'>
+      <CalculatorDisplay buttonStyle='disply' text='0' />
+      <div className='one'>
+        <ActionButton buttonStyle='clear' text='clear' />
+        <NumberButton buttonStyle='sign' text='÷' />
+      </div>
+      <div className='two'>
+        <NumberButton buttonStyle='number' text='7' />
+        <NumberButton buttonStyle='number' text='8' />
+        <NumberButton buttonStyle='number' text='9' />
+        <NumberButton buttonStyle='sign' text='×' />
+      </div>
+      <div className='three'>
+        <NumberButton buttonStyle='number' text='4' />
+        <NumberButton buttonStyle='number' text='5' />
+        <NumberButton buttonStyle='number' text='6' />
+        <NumberButton buttonStyle='sign' text='−' />
+      </div>
+      <div className='four'>
+        <NumberButton buttonStyle='number' text='1' />
+        <NumberButton buttonStyle='number' text='2' />
+        <NumberButton buttonStyle='number' text='3' />
+        <NumberButton buttonStyle='sign' text='+' />
+      </div>
+      <div className='five'>
+        <NumberButton buttonStyle='zero' text='0' />
+        <NumberButton buttonStyle='sign' text='=' />
+      </div>
     </div>
   );
 };
