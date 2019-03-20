cf7f02ccefb4a8d1e56a3681255b0bd9f1f25bbb
deanovision <rodeanfraser@gmail.com>
2019-03-19 22:18:47 -0400

    finished mvp

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,53 +1,10 @@
-import React from 'react';
-import './Button.css';
-const numbersObj = [
-  {display: "0",
-   style: "action-button"
-  },
-  {display: "1",
-    style: "number-button"
-  },
-  {display: "2",
-    style: "number-button"
-  },
-  {display: "3",
-    style: "number-button"
-  },
-  {display: "4",
-    style: "number-button"
-  },
-  {display: "5",
-    style: "number-button"
-  },
-  {display: "6",
-    style: "number-button"
-  },
-  {display: "7",
-    style: "number-button"
-  },
-  {display: "8",
-    style: "number-button"
-  },
-  {display: "9",
-    style: "number-button"
-  },
-  {display: "Clear",
-    style: "action-button"
-  }
-]
-const NumberButon = ()=> {
-  return(
-    <div>
-      
-    </div>
-  );
-}
-function numbers() {
+import React from "react";
+import "./Button.css";
+const NumberButton = props => {
   return (
-    <div className="number-button">
-    {numbersObj.map(function(numbers){
-      return <NumberButton numberProps={numbers}/>
-    });}
+    <div className={props.numberProps.style}>
+      <h3>{props.numberProps.display}</h3>
     </div>
   );
-}
+};
+export default NumberButton;
