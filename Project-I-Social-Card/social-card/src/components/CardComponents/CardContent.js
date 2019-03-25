5b18d84abb4aa0177cd14ed9d3ac7f935ef7f04b
DwayneDevelops <mrsuccessfuldev@gmail.com>
2019-03-25 17:55:34 -0500

    mvp met

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -6,7 +6,7 @@ const CardContent = () => {
         <div>
             <h1>Get Started with React</h1>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in you application.</p>
-            <a href="https://www.reactjs.org" target='_blank' />
+            <a href="https://www.reactjs.org" target='_blank'>reactjs.org</a>
         </div>
     );
 };
