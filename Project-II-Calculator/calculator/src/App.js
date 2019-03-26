08f7f601aad38013ad2495a2cc85819ed38bb6c1
MAllen07 <mrsmallen07@gmail.com>
2019-03-26 16:52:41 -0400

    intiated project and created calculator display folder

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import ActionButton from "../src/components/ButtonComponents/ActionButton";
+// import NumberButton from "../src/components/ButtonComponents/NumberButton
 
 const App = () => {
   return (
     <div>
       <h3>Welcome to React Calculator</h3>
+      <ActionButton />
+      
       <p>
         We have given you a starter project. You'll want to build out your
         components in their respective files, remove this code and replace it
