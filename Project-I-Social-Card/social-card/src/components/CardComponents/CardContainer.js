8b0c6472c649a6431280426dbf21854435fb18ef
MAllen07 <mrsmallen07@gmail.com>
2019-03-26 11:23:41 -0400

    finished the header and card components having issues with the picture

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Card.css';
+import CardContent from "./CardContent";
+import CardBanner from "./CardBanner";
+
+const CardContainer = () => {
+    return (
+      <div className="banner-wrapper">
+        
+        <CardBanner />
+        <CardContent />
+      </div>
+    );
+  };
+
+
+
+
+export default CardContainer;
