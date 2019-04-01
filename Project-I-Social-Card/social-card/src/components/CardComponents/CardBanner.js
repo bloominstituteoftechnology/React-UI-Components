d540a76423c331a3b5350e27540fc70bb92404a2
Aquariax <roberthrrs8@gmail.com>
2019-04-01 18:39:41 -0400

    finished social card project

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+const CardBanner = () => {
+    return (
+      <div>
+        <a href="https://reactjs.org">
+          <img
+            className="banner-logo"
+            href="https://reactjs.org"
+            src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
+            alt="React Logo"
+          />
+        </a>
+      </div>
+    );
+  };
+  
+   export default CardBanner;
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,8 @@
+import {react} from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return
+  };
+  
+   export default Footer; 
