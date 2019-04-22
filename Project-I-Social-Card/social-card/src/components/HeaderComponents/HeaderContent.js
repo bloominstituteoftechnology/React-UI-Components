aa4e78f040be0cfa6220739994690e35920ebf7d
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 13:28:17 -0700

    add HeaderContent component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+
+export default function HeaderContent(props) {
+    return (
+        <div>
+            <img 
+                src={props.img.src || "../../../../public/favicon.ico"}
+                alt={props.img.alt || "React Icon"}
+             />
+            <h1>{props.title}</h1>
+            <p>{props.body}</p>
+            {
+                (props.link) 
+                    ? <a href="https://reactjs.org/">reactjs.org</a>
+                    : null
+            }
+        </div>
+    )
+}
