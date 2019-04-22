a1b78f953eba19d55eb729f5ac7a2335ed00c926
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-04-22 15:14:02 -0700

    first commit got the header and images

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Header.css';
+// import'./HeaderContent.js';
+// import './ImageThumbnail.js'
+// import HeaderContainer from './HeaderContent.js';
+import ImageThumbnail from './ImageThumbnail.js';
+import HeaderTitle from './HeaderTitle.js';
+import HeaderContent from './HeaderContent.js';
+
+
+
+function HeaderContainer() {
+return (
+    <div className=""HeaderContainer>
+    <ImageThumbnail />
+    <HeaderTitle />
+    <HeaderContent />
+    </div>
+)
+}
+
+export default HeaderContainer
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+
+
+function HeaderContent() {
+    return (
+    <div><p>Lets learn React </p></div>
+    )
+};
+
+export default HeaderContent;
