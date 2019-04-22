7ca7c80e12f33d10a33bda902ed9b6c59aab2411
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 13:36:03 -0700

    refactor HeaderContent component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -4,17 +4,7 @@ import './Header.css';
 export default function HeaderContent(props) {
     return (
         <div>
-            <img 
-                src={props.img.src || "../../../../public/favicon.ico"}
-                alt={props.img.alt || "React Icon"}
-             />
-            <h1>{props.title}</h1>
-            <p>{props.body}</p>
-            {
-                (props.link) 
-                    ? <a href="https://reactjs.org/">reactjs.org</a>
-                    : null
-            }
+            <p>{props.content}</p>
         </div>
     )
 }
