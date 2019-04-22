248a916bc4e6b65507320ba18f13999d491b1b11
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 14:16:22 -0700

    render HeaderContainer and CardContainer in App.js

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer'
+import CardContainer from './components/CardComponents/CardContainer'
 
 const App = () => {
   return (
-      <HeaderContainer />
+      <div>
+        <HeaderContainer />
+        <CardContainer />
+      </div>
   );
 };
 
