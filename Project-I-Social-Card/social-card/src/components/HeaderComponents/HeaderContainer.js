63d1be7205ccca13a8299d8a44e9af5e1e0bc512
PetejStone <petejstoney@gmail.com>
2019-04-22 13:30:29 -0700

    initial commit and components created

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return (
+        <div className='header-container'>
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />
+        </div>
+        
+    );
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return (
+        <div className="banner">
+            <h1>Lambda School</h1>
+            <p>@LambdaSchool</p>
+            <p>26 jan</p>
+        </div>
+    );
+}
+
+
+export default HeaderTitle;
+
