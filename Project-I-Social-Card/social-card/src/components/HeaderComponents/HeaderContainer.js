061c2b8e8d51ef6ef8f4a3e0fd47a77004257057
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 13:32:40 -0700

    add HeaderContainer component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import './Header.css';
+import React from 'react'
+import './Header.css'
+import Thumbnail from './ImageThumbnail'
+import HeaderTitle from './HeaderTitle'
+import HeaderContent from './HeaderContent'
+
+export default function HeaderContainer(props) {
+    return (
+        <header>
+            <div>
+                <Thumbnail />
+                <HeaderTitle />
+            </div>
+
+            <div>
+                <HeaderContent />
+            </div>
+        </header>
+    )
+}
