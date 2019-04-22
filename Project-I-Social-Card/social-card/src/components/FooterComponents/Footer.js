8e0371d95a961e498368461548210a3da38b2224
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 14:34:42 -0700

    add Footer component markup and styles

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer(props) {
+    return (
+        <footer className="footer">
+            <span>Comments</span>
+            <span>Shares</span>
+            <span>Likes</span>
+            <span>Messages</span>
+        </footer>
+    )
+}
