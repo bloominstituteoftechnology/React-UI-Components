3bf30131aaa391d32e7589d17874935dab2405d0
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 16:07:23 -0700

    refactor CalculatorDisplay component

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -5,34 +5,34 @@ import NumberBtn from'../ButtonComponents/NumberButton'
 
 export default function CalculatorDisplay(props) {
     return (
-        <article className="calculatorDisplay">
-            <div className="row1" id="display"> 0</div>
+        <article className="calculator-display">
+            <div className="row" id="display"> 0</div>
 
-            <div className="row2">
+            <div className="row">
                 <ActionBtn content="clear" large />
                 <ActionBtn content="/" />
             </div>
 
-            <div className="row3">
+            <div className="row">
                 <NumberBtn content="7" />
                 <NumberBtn content="8" />
                 <NumberBtn content="9" />
                 <ActionBtn content="x" />
             </div>
-            <div className="row4">
+            <div className="row">
                 <NumberBtn content="4" />
                 <NumberBtn content="5" />
                 <NumberBtn content="6" />
                 <ActionBtn content="-" />
             </div>
-            <div className="row5">
+            <div className="row">
                 <NumberBtn content="1" />
                 <NumberBtn content="2" />
                 <NumberBtn content="3" />
                 <ActionBtn content="+" />
             </div>
-            <div className="row6">
-                <NumberBtn content="0" />
+            <div className="row">
+                <NumberBtn content="0" large />
                 <ActionBtn content="=" />
             </div>
         </article>
