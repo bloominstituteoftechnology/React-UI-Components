c977d3f6a0ab589a1d828c254cabb17275edc065
PetejStone <petejstoney@gmail.com>
2019-04-22 13:38:54 -0700

    created footer component

--- Project-I-Social-Card/social-card/src/components/FooterComponents/FooterIcons.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterIcons.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => {
+const FooterIcons = () => {
     return (
-        <div className="footer">
+        <div className='icons'>
             
         </div>
     );
-
 }
