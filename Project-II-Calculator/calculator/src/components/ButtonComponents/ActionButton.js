c4eb0c714841f4249fc84aa0a2ec697753496949
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 14:44:08 -0700

    add jsx for button components

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Button.css';
+
+export default function ActionButton({action}) {
+    return <div className="btn">{action}</div>
+}
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Button.css';
+
+export default function NumberButton({num}) {
+    return <div className="btn">{num}</div>
+}
