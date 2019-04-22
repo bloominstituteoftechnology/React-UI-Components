cfe1daed04a045971755c221ba4182d6203fe022
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 13:38:54 -0700

    fix: remove React Fragment in HeaderTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,10 +3,10 @@ import './Header.css';
 
 export default function HeaderTitle(props) {
     return (
-        <>
-        <h1>{props.title || "Lambda School"}</h1>
-        <span>{props.subtitle || "@Lambda Shchool"}</span> 
-        <span>{props.date || "@Jan 26"}</span>
-        </>
+        <div>
+            <h1>{props.title || "Lambda School"}</h1>
+            <span>{props.subtitle || "@Lambda Shchool"}</span> 
+            <span>{props.date || "@Jan 26"}</span>
+        </div>
     )
 }
