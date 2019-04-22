4543ff9f63fb6f4281e61cfa7f6001f62cb6f480
Jonathan Taylor <jonyonson@icloud.com>
2019-04-22 17:04:40 -0500

    style tweaks

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div className="App__container">
       <HeaderContainer />
       <CardContainer />
+      <Footer />
     </div>
   );
 };
