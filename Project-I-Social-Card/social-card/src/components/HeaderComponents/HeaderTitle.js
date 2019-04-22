9737d231a85de74fa599a0c3e23d40eae0003f55
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 13:19:21 -0700

    add ImageThumbnail and HeaderTitle components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+export default function HeaderTitle(props) {
+    return (
+        <>
+        <h1>{props.title || "Lambda School"}</h1>
+        <span>{props.subtitle || "@Lambda Shchool"}</span> 
+        <span>{props.date || "@Jan 26"}</span>
+        </>
+    )
+}
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import './Header.css';
+
+export default function Thumbnail({src, alt}) {
+    return <img src={src} alt={alt} />
+}
