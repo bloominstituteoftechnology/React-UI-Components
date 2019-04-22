f7b3c8b76f336e9ccc98951df32117c61c37de98
Moses Samuel <mosamuel98@gmail.com>
2019-04-22 13:43:55 -0700

    remove unecessary markup

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -6,7 +6,7 @@ export default function HeaderTitle(props) {
         <div>
             <h1>{props.title || "Lambda School"}</h1>
             <span>{props.subtitle || "@Lambda Shchool"}</span> 
-            <span>{props.date || "@Jan 26"}</span>
+            <span>{props.date || "Jan 26"}</span>
         </div>
     )
 }
