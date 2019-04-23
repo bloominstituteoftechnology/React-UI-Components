7a4587f5a46e1431346701878f372822ad5ca813
Manju KC <Manju KC>
2019-04-22 21:17:45 -0400

    fixed the img

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Footer.css';
 
 
-function Footer () {
+function Footer() {
     return (
-    <div class="footer">
-       <i class="far fa-comment icon"></i> 
-        <i class="fas fa-sync-alt icon"></i> <span>6</span>
-       <i class="far fa-heart icon"></i> <span>4</span>
-       <i class="far fa-envelope icon"></i>
-    </div>
+        <div class="footer">
+            <i class="far fa-comment icon"></i>
+            <i class="fas fa-sync-alt icon"></i> <span>6</span>
+            <i class="far fa-heart icon"></i> <span>4</span>
+            <i class="far fa-envelope icon"></i>
+        </div>
     )
 }
 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Header.css';
 
-function ImageThumbnail (){
-    return(
-        <img className = 'logo' src="https://pbs.twimg.com/profile_images/973277209644249089/0Te2jtBH_400x400.jpg" alt='logo'/>
+function ImageThumbnail() {
+    return (
+        <img className='logo' src="https://pbs.twimg.com/profile_images/973277209644249089/0Te2jtBH_400x400.jpg" alt='logo' />
     )
 }
 
