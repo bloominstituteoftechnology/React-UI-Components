efd0d2cac1f20b9f63a3a27dc64a40c9fbabab4e
PetejStone <petejstoney@gmail.com>
2019-04-23 16:29:10 -0700

    added input

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -5,19 +5,23 @@ import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
 import ActionsDisplay from './components/DisplayComponents/ActionsDisplay';
 import NumbersDisplay from './components/DisplayComponents/NumbersDisplay';
+import InputDisplay from './components/DisplayComponents/InputDisplay';
 
 
 
 const App = (props) => {
   return (
     <div className='calculator'>
-      <div className='numbers'>
-        <NumbersDisplay />
-      </div>
+      <InputDisplay />
+      <div className="calc-content">
+          <div className='numbers'>
+            <NumbersDisplay />
+          </div>
 
-      <div className='actions'>
-        <ActionsDisplay />
-      </div>
+          <div className='actions'>
+            <ActionsDisplay />
+          </div>
+        </div>
     </div>
     
   );
