597008a3eccb1502587345699f7a613315d4f601
Manya0 <manya01@protonmail.com>
2019-04-22 20:10:30 -0400

    third

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
-import './Header.css';
+import './Header.scss';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+
+function HeaderContainer (){
+    return(
+        <div className='HeaderContainer'>
+            <HeaderTitle />
+            <HeaderContent />  
+        </div>
+    )
+}
+
+
+export default HeaderContainer; 
