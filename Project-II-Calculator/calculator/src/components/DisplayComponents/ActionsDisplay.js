c05c4d143b3031f13a2502519121e66201071093
PetejStone <petejstoney@gmail.com>
2019-04-23 16:08:06 -0700

    swapped actions button text

--- Project-II-Calculator/calculator/src/components/DisplayComponents/ActionsDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/ActionsDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Display.scss';
 import ActionButton from '../ButtonComponents/ActionButton';
 
-const Actions = ['/','-', '+', '=', 'X'];
+const Actions = ['/','X', '-', '+', '='];
 
 const ActionsDisplay = (props) => {
   return (
