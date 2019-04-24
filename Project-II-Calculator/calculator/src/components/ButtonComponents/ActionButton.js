fc0cc16b9f9e0d44892f29f5392d74483443cc98
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-04-23 18:58:11 -0700

    final mvp done

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+
+function ActionButton(props) {
+ return (
+     <div className="Button">
+         <button className={props.buttonStyle}>{props.text}</button>
+     </div>
+ )
+}
+
+export default ActionButton
