aa29a38ec145d149461556aecbeaa1a4930e5094
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-04-29 16:52:10 -0700

    add

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner'
+import CardContent from './CardContent'
+
+const CardContainer = () => {
+  return (
+    <a href="https://www.reactjs.org">
+      <div className="CardContainer">
+        <CardBanner />
+        <CardContent />
+      </div>
+    </a>
+  )
+}
+
+export default CardContainer
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import './Header.css';
+//import moment from 'moment';
+
+const HeaderTitle = () => {
+    //Javascript Date
+    //const date = new Date()
+    //const options = { month: "short" }
+    //const short = new Intl.DateTimeFormat("en-US", options).format(date);
+    //const today = `${date.getDate()} ${short}`
+
+   // MomentJS Date
+ // const momentDate = moment().format("D MMM")
+
+  return (
+    <div className="HeaderTitle">
+      <p className="displayName bold">Lambda School</p>
+      <p className="accountName gray">@LambdaSchool</p>
+      <p className="bullet gray">• 26 jan</p>
+    </div>
+  )
+}
+
+export default HeaderTitle
