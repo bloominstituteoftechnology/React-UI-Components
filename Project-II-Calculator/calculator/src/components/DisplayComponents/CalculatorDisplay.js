86f13a5b4258e2841b92a24a8b52694019ff1259
Vanessa McLendon <vanessamclendon@Vanessas-MacBook-Pro-2.local>
2019-05-01 13:33:10 -0400

    commit project

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay(props) {
+  return (
+    <div className={props.className}>{props.data}</div>
+  );
+};
+
+export default CalculatorDisplay;
