d104d9afd69d9015c77ae5dbdfbbd3566036ce84
Vanessa McLendon <vanessamclendon@Vanessas-MacBook-Pro-2.local>
2019-05-01 14:37:58 -0400

    changed name

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -12,9 +12,9 @@ const App = () => {
       className={'header'}
       logo={'https://ibin.co/3whrpKSBbZ81.png'}
       altText={'logo'}
-      title={'Lambda School'}
+      title={'Cool School'}
       content={`Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!`}
-      identifer={'LambdaSchool'}
+      identifer={'CoolSchool'}
       date={'25 Mar'} />
 
       <CardContainer 
