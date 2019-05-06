c68800898f36ba24f88ab041651b135d9f2438d8
angel prats <angelpratz@gmail.com>
2019-05-06 15:41:03 -0400

    social card initial commit

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = () => {
+  return (
+    <div>
+      <HeaderTitle />
+
+    </div>
+  );
+};
+
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const Header = () => {
+  return (
+    <h3>Lambda School </h3>
+  );
+}
+
+
+export default Header;
