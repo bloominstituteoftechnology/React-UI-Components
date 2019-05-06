48e9359f10a245c855dc869a6016fef0a5ca631b
bwpeck <benjaminwade483@yahoo.com>
2019-05-06 17:23:53 -0400

    love react

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,19 @@
 import React from 'react';
+import HeaderContent from './HeaderContent';
 import './Header.css';
+import moment from 'moment';
+
+const time = moment();
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title-wrapper">
+        <div className="header-title">
+        Lambda School
+        <span className="time-stamp">{time.format('MMM Do ')}</span>
+        </div>
+        </div>
+    )
+}
+
+export default HeaderTitle;
