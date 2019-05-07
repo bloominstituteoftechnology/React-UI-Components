8f997128e6f2dc4a280e3f18514a3a22745faa0b
angel prats <angelpratz@gmail.com>
2019-05-07 16:09:35 -0400

    added dispcalculator display

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NumberButton from './NumberButton';
+import ActionButton from './ActionButton';
+
+const ButtonContainer = props => {
+  return (
+    <div>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default ButtonContainer;
