d0cb09be1212d9dfe5bc2194b9a720a7a9581957
Cory Hedeen <hedeencory@gmail.com>
2019-05-08 15:34:55 -0400

    Finished styling calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Button.css';
+import './NumberButton.js';
+
+
+
+const ActionButton = props => {
+  return (
+          <div>
+            <button className={`action ${props.className}`}>
+              <h1>{props.text}</h1>
+            </button>
+          </div>
+        );
+
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+
+const NumberButton = props => {
+  return (
+    <div >
+    <button className={`buttons ${props.className}`}>
+      <h1>{props.number}</h1>
+    </button>
+    </div>
+  );
+}
+
+export default NumberButton;
