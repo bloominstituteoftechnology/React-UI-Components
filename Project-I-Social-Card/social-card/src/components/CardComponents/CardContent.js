9ec94a68153cb5755167b510eea00a36a5362f1f
Kay Jay <kharris2k16@gmail.com>
2019-05-08 19:12:43 -0700

    Social Card components in place to be styled

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+  return (
+    <div>
+      <h2>Get started with React</h2>
+      <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+      <p>reactjs.org</p>
+    </div>
+  )
+}
+
+export default CardContent
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div>
+      <h1>Lambda School</h1>
+      <p>@LambdaSchool 26 Jan</p>
+    </div>
+  )
+}
+
+export default HeaderTitle
