f43957e67e4f537d70fb50c3399c405ad0fa0921
RyanSWest <west56309@gmail.com>
2019-05-09 22:59:37 -0500

    ryan-west 1st commit

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContainer = () => {
+	return (
+		<div>
+			<HeaderContainer />
+			<CardBanner />
+			<CardContent />
+		</div>
+	);
+};
+
+export default CardContainer;
