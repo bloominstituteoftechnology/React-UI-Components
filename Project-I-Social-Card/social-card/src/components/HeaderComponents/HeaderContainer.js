d7d2f99115cffc921e4d8f6161370f002a08c465
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-22 23:57:41 -0700

    inserted headercontainer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContainer (props) {
+    return (
+        <div>
+            <h3>If you take your love away from me...Ill go crazy! Ill go insane!!! Forever! (Stay!) Nah baby nah baby nah baby..</h3>
+        </div>
+    )
+}
