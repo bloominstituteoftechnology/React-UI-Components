10b6cd7ff2eb35fa2d9fea541f20dfd8a69bc0e1
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-23 00:48:08 -0700

    export imagethumbnails

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -2,5 +2,7 @@ import React from 'react';
 import './Header.css';
 
 function ImageThumbnail(prop) {
-    return <img src='dinner-egg.jpg'
+    return <img src={dinner-egg.jpg} alt='Dinner On Bottom' />;
 }
+
+export default ImageThumbnail;
