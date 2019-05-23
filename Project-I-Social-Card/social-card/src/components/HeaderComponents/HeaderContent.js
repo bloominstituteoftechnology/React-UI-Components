52186003f5281b31c49cbd340dad26e127d9845d
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-22 23:54:01 -0700

    export HeaderContent

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -6,5 +6,7 @@ function HeaderContent(props) {
         <div>
             <h2>I know that I try to make a lot of people smile and laugh but I actually love to learn and love to flow with the computer. If you want a little context as to exactly whats going on and how im able to this...come closer to the screen...closer...(whispers)M M M M M Mayback Music</h2>
         </div>
-    )
+    );
 }
+
+export default HeaderContent;
