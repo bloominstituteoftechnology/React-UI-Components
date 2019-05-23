5cfbd740f83e58b725d6550b3ad44682b6d4b972
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-23 00:24:19 -0700

    remove filler and put in my words

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import HeaderTitle from './components/HeaderComponents/HeaderTitle';
 import './App.css';
 import HeaderContent from './components/HeaderComponents/HeaderContent';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Social Card!</h3>
+      <h3>Welcome!</h3>
       <HeaderTitle title='This is' />
       <HeaderTitle title='Just Spoken Codes' />
       <p>
@@ -15,8 +16,7 @@ const App = () => {
 
       <p>
         <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
+          <HeaderContainer />
         </strong>
       </p>
     </div>
