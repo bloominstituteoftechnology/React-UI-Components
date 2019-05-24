d3ef2d87d9542faaaa595f836cb3205ba7a0b944
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-24 02:19:34 -0700

    removed all the other tags that i didnt need

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -5,21 +5,16 @@ import HeaderContent from './components/HeaderComponents/HeaderContent';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
 import CardBanner from './components/CardComponents/CardBanner';
+import Button from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
     <div>
       <ImageThumbnail />
       <HeaderTitle title='This is Just Spoken Codes' />
-      <p>
-        <HeaderContent />
-      </p>
-     <CardBanner />
-      <p>
-        <strong>
-      
-        </strong>
-      </p>
+      <HeaderContent />
+      <CardBanner />
+      <Button />
     </div>
   );
 };
