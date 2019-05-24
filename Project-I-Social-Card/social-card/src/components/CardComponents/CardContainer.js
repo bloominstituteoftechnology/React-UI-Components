9147fc67b2e8a476d2ed03988706417ff8e1c191
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-24 02:23:17 -0700

    created a link

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const Button= props => {
     return (
-        <button className="click-button" id='click'>
+        <button onClick='https://www.reactjs.org' className="click-button" id='click'>
             Do It!
         </button>
     );
