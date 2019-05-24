4d2d566d6a57658bb127abf5e6532a4f7c07d493
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-24 00:26:25 -0700

    sizing for everything

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 function HeaderContainer (props) {
     return (
-        <div>
+        <div className='header-container'>
             <h3>If you take your love away from me...Ill go crazy! Ill go insane!!! Forever! (Stay!) Nah baby nah baby nah baby..</h3>
         </div>
     );
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Header.css';
-import './dinner-egg.jpg';
+import LambdaLogo from './lambdacrest.png';
 
 function ImageThumbnail(prop) {
-    return <img  alt='Dinner On Bottom' />;
+    return <img className='lambda' src= {LambdaLogo} alt='Dinner On Bottom' />;
 }
 
 export default ImageThumbnail;
