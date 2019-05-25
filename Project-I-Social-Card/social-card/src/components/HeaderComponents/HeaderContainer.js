4c538d8703bb6a0e1fc33f1663a8c31187dc7ef0
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-05-25 13:16:34 -0600

    finished header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent'
+
+function HeaderContainer () {
+    return(
+        <div className='header-container'>
+            <ImageThumbnail />
+            <div className='header-info'>
+                <HeaderTitle />
+                <div className='header-content'>
+                    <HeaderContent />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div className='header-boxes'>
+            <h4 className='headertitle'>Lambda School</h4>
+            <p className='header-tag'>@LambdaSchool - 26 Jan</p>
+        </div>
+    )
+}
+
+
+export default HeaderTitle;
