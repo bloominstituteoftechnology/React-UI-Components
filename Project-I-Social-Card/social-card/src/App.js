97056853beac42a3e74f9cb1d4473be05c51e815
ckopecky <kopecky12112@gmail.com>
2019-05-26 00:34:06 -0700

    add RetrieveDate function

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -5,10 +5,56 @@ import lambdaLogo from './assets/lambdalogo.jpg';
 import CardContainer from './components/CardComponents/CardContainer';
 import Footer from './components/FooterComponents/Footer';
 const App = () => {
-  console.log(lambdaLogo);
+  const retrieveDate = () => {
+    let date = new Date();
+    let d = date.getDate();
+    let m = date.getMonth();
+    switch(m) {
+      case 0:
+        m ='Jan';
+        break;
+      case 1:
+        m = 'Feb';
+        break;
+      case 2:
+        m = 'Mar';
+        break;
+      case 3:
+        m = 'Apr';
+        break;
+      case 4:
+        m = 'May';
+        break
+      case 5:
+        m = 'Jun';
+        break;
+      case 6:
+        m = 'Jul';
+        break;
+      case 7:
+        m = 'Aug';
+        break;
+      case 8:
+        m = 'Sept';
+        break;
+      case 9: 
+        m = 'Oct';
+        break;
+      case 10:
+        m = 'Nov';
+        break;
+      case 11:
+        m = 'Dec';
+        break;
+      default:
+        return m;
+    }
+    return `${m} ${d}`
+
+  }
   return (
     <div>
-      <HeaderContainer lambdaLogo={lambdaLogo} altText="lambda-logo"/>
+      <HeaderContainer lambdaLogo={lambdaLogo} altText="lambda-logo" retrieveDate={retrieveDate}/>
       <CardContainer  />
       <Footer />
     </div>
