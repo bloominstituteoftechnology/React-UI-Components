57318da473e8eff097a3600c6b2b09f73c820ab4
Nabeelah <nabeelahyousuph@gmail.com>
2019-05-27 12:56:10 +0100

    Feat: Import card banner and card content into card container

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent'
 
 function CardContainer () {
     return (
-        
+        <div>
+            <CardBanner />
+            <CardContent />
+        </div>
     )
-
 }
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+
+
+function CardContent({ title, text }) {
+    return (
+        <div>
+            <h3>{ title }</h3>
+            <p>{ text }</p>
+        </div>
+    )
+}
