3c3def7981c8fac56a9646a056e645c3a56ad634
Nabeelah <nabeelahyousuph@gmail.com>
2019-05-27 12:45:40 +0100

    Import header container component into app.js file

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Header.css';
-import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 import ImageThumbnail from './ImageThumbnail';
 
-function HeaderContainer () {
+function HeaderContainer() {
     return (
-        <ImageThumbnail />
-        <HeaderTitle />
+        <div>
+            <ImageThumbnail />
+            <HeaderContent />
+        </div>
     )
 }
+
+export default HeaderContainer;
