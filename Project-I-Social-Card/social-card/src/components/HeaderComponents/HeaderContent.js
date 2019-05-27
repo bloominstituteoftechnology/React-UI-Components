d9e9f3f544259e7b4471b9332b334fbf5c030da4
Nabeelah <nabeelahyousuph@gmail.com>
2019-05-27 12:23:41 +0100

    Feat: Create header content component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContent ({ text }) {
+    return (
+        <div>
+            <p>{text}</p>
+        </div>
+    )
+}
+
+export default HeaderContent;
