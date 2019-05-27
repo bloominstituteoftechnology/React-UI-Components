15ffa7c8328cba56c1be8d5c0d235aeb53eac99b
Nabeelah <nabeelahyousuph@gmail.com>
2019-05-27 12:47:55 +0100

    Feat: Create card banner component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const url = 'https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png'
+
+function CardBanner () {
+    return (
+        <img src = {url} alt='banner'/>
+    )
+}
+
+export default CardBanner;
