a1b46cab90ac63fe2ea8129e0e00d0ab86df027c
Ty Lippe <tylippe94@gmail.com>
2019-05-28 18:48:09 -0400

    layout complete

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButtons = () => {
+    return (
+        <div className="action">
+            <button>&divide;</button>
+            <button>&times;</button>
+            <button>-</button>
+            <button>+</button>
+            <button>=</button>
+        </div>
+    );
+}
+
+export default ActionButtons;
