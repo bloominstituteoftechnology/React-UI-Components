651497d4b360bdcbe4dfd99cfd1f0e3b01376240
Jengopockets <jengopockets@gmail.com>
2019-05-28 17:15:41 -0400

    Actionbutton.js

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return (
+        <button className={`three-quater-button`}>
+            {props.text}
+        </button>
+    );
+};
+
+export default ActionButton;
