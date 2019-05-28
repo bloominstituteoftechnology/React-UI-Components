e72f3484e477ff417b7835a9da6d5c6d2f5a33aa
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-05-27 19:26:45 -0700

    Image Thumbnail Start

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+  return <div className="image-thumbnail">
+          <img src="lambdacrest.png"></img>
+  </div>
+}
+export default ImageThumbnail;
