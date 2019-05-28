176fae707a63f6784fb9c1062c51197390f43878
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-05-27 19:43:50 -0700

    card content updated

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return <div className="card-content">
+            <p>Get Started with React</p>
+            <p>React Makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <a href="#">reactjs.org</a>
+    </div>
+}
+
+export default CardContent;
