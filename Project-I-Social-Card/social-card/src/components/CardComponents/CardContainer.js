2cd3d5ffb0cc7505caa7fb66d837a3741991240a
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-05-27 19:50:51 -0700

    header styles started

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Card.css';
-
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 const CardContainer = () => {
   return <div className="card-container">
