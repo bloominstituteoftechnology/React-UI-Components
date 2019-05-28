9560291b5071c07afe967b3805072a04ec376fbc
Jengo Pockets <jengopockets@gmail.com>
2019-05-28 14:39:50 -0400

    Header Finished

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+const ImageThumbnail = () => {
+    return (
+        <div className="thumb">
+            <img alt="lambda logo" className="thumbimg" src="https://ibin.co/3whrpKSBbZ81.png"/>
+        </div>
+    );
+;}
+
+export default ImageThumbnail;
