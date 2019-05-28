7332cb121335aab28212a7e9a609cff12955bd52
rushi444 <rushi44@gmail.com>
2019-05-28 08:59:30 -0700

    part 1 done

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+
+import ImageThumbnail from './ImageThumbnail'; 
+import HeaderTitle from './HeaderTitle' ; 
+import HeaderContent from './HeaderContent'; 
+
+const HeaderContainer = () => {
+    return (
+        <header>
+    <ImageThumbnail />
+    <div>
+    <HeaderTitle />
+    <HeaderContent />
+    </div>
+    </header>
+    )
+}; 
+
+export default HeaderContainer; 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return <h1>Lambda School <span>@LambdaSchool 26 jan</span></h1>
+}; 
+
+export default HeaderTitle; 
