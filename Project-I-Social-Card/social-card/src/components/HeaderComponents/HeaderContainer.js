40094847924d2fee219270eba12a53866b4147af
teej387 <teej387@yahoo.com>
2019-05-29 00:19:36 -0500

    Moved HeaderContent into HeaderTitle.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
-import HeaderContent from './HeaderContent';
 
 function HeaderContainer() {
   return (
     <div className="HeaderContainer">
       <ImageThumbnail />
       <HeaderTitle />
-      <HeaderContent />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
 
 function HeaderTitle() {
   return (
     <div className="HeaderTitle">
       <h3>Lambda School</h3>
       <p className="lambdaHandle">@LambdaSchool &#8729; 26 jan</p>
+      <HeaderContent />
     </div>
   );
 }
