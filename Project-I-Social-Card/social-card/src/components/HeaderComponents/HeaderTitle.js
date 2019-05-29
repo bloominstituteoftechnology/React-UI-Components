bd1d25ab45742d3331ddb906696e34526f617deb
anthonyvigz <anthonyvigz@gmail.com>
2019-05-29 19:05:37 -0400

    added all main header container components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 function HeaderTitle() {
     return <div className="headertitle">
-                <h3>Lambda School></h3>
+                <h3>Lambda School</h3>
                 <h4>@LambdaSchool</h4>
                 <h5>- 29 May</h5>
             </div>
