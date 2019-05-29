3d50cd0ec8f6654f416e22dd99155ebaf22e0617
anthonyvigz <anthonyvigz@gmail.com>
2019-05-29 19:52:34 -0400

    created and added the text content to the card container

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Card.css';
 import Banner from './CardBanner';
+import Content from './CardContent';
 
 function CardContainer() {
     return <div className="mainCard">
               <Banner />
+              <Content />
            </div>
 }
 
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+    return <div></div>
+}
+
+export default CardContent;
