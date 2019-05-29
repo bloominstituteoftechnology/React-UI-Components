8efcd2518c23d9f219d43f83bd912a4abcf3a2ad
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-29 10:57:33 -0700

    name the paragraph for cal

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <p>
+      <p class= 'cal-container'>
         <strong>
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
