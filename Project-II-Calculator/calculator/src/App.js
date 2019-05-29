25840ba5ed6570e48f23ac3fdda0f3286e8b4ef9
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-05-29 13:13:27 -0700

    Made my 0

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
     <div>
       <p class= 'cal-container'>
+      <ActionButton />
         <strong>
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
