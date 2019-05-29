2465a660d3b2600452643b0838a55b0de0639716
Lucas Larrubia <Lucas@Marcias-iMac.local>
2019-05-29 19:42:32 -0400

    initial commit

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+function Lam() {
+    return (
+        <div className="Lam">
+            
+
+        
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Lam />, rootElement);
