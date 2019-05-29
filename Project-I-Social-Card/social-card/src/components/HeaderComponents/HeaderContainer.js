410dd53301f9afef49b416ed3d796a56a006cef6
anthonyvigz <anthonyvigz@gmail.com>
2019-05-29 18:44:17 -0400

    installed modules and started container

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContainer() {
+    return <div className="header"> 
+                <ImageThumbnail />
+                <div className="headermain">
+                    <HeaderTitle />
+                    <HeaderContent />
+                </div>
+            </div>
+}
+
+export default HeaderContainer;
