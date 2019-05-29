e2e463b2801f89e3fdd43b4d3ed485bf854a2143
kmcknight1 <katiemcknight1991@gmail.com>
2019-05-29 08:28:39 -0500

    figuring out props

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+// import ActionButton from './components/ButtonComponents/ActionButton';
+// import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      <NumberButton buttonStyle="long-button" text="clear"/>
+      <NumberButton buttonStyle="red-button" text="/"/><br />
+
+      <NumberButton buttonStyle="regular-button" text="7"/>
+      <NumberButton buttonStyle="regular-button" text="8"/>
+      <NumberButton buttonStyle="regular-button" text="9"/>
+      <NumberButton buttonStyle="red-button" text="X"/><br />
+
+      <NumberButton buttonStyle="regular-button" text="4"/>
+      <NumberButton buttonStyle="regular-button" text="5"/>
+      <NumberButton buttonStyle="regular-button" text="6"/>
+      <NumberButton buttonStyle="red-button" text="-"/><br />
+
+      <NumberButton buttonStyle="regular-button" text="1"/>
+      <NumberButton buttonStyle="regular-button" text="2"/>
+      <NumberButton buttonStyle="regular-button" text="3"/>
+      <NumberButton buttonStyle="red-button" text="+"/><br />
+
+      <NumberButton buttonStyle="long-button" text="0"/>
+      <NumberButton buttonStyle="regular-button" text="="/>
+
+
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = (props) => {
+  return (
+    <div>
+      <button className={props.buttonStyle}>{props.text}</button>
+      {/* <button className={props.buttonStyle}>{props.text}</button> */}
+    </div>
+  );
+};
+
+export default NumberButton;
