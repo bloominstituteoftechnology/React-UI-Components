8243fa58c8e3871cae76e71493dff5c6f84e001b
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-05-28 19:40:47 -0700

    Calculator structure laid out. Button styling complete

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton'
+import ActionButton from './components/ButtonComponents/ActionButton'
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator">
+      <div className="row">
+        <ActionButton text="clear" buttonStyle="buttonStyle largeButton clearButton" />
+        <ActionButton text="÷" buttonStyle="buttonStyle actionButton" />
+      </div>
+      <div className="row">
+        <NumberButton text="7" buttonStyle="buttonStyle numberButton" />
+        <NumberButton text="8" buttonStyle="buttonStyle numberButton" />
+        <NumberButton text="9" buttonStyle="buttonStyle numberButton" />
+        <ActionButton text="×" buttonStyle="buttonStyle actionButton" />
+      </div>
+      <div className="row">
+        <NumberButton text="4" buttonStyle="buttonStyle numberButton" />
+        <NumberButton text="5" buttonStyle="buttonStyle numberButton" />
+        <NumberButton text="6" buttonStyle="buttonStyle numberButton" />
+        <ActionButton text="-" buttonStyle="buttonStyle actionButton" />
+      </div>
+      <div className="row">
+        <NumberButton text="1" buttonStyle="buttonStyle numberButton" />
+        <NumberButton text="2" buttonStyle="buttonStyle numberButton" />
+        <NumberButton text="3" buttonStyle="buttonStyle numberButton" />
+        <ActionButton text="+" buttonStyle="buttonStyle actionButton" />
+      </div>
+      <div className="row">
+        <NumberButton text="0" buttonStyle="buttonStyle largeButton zeroButton" />
+        <ActionButton text="=" buttonStyle="buttonStyle actionButton" />
+      </div>
     </div>
   );
 };
