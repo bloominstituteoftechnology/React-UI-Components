e05ca5f2b99a522dd60ba23e8b4658e9cf5b0bdf
anthonyvigz <anthonyvigz@gmail.com>
2019-05-29 19:50:37 -0400

    set up the banner image and imported it to the card container

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Card.css';
+import Banner from './CardBanner';
 
 function CardContainer() {
     return <div className="mainCard">
-
+              <Banner />
            </div>
 }
 
