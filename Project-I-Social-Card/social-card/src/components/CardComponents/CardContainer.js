6437d444a4c20ec0f4bc93617aa1e8df04f05807
Ronald Cho <ronaldcho@gmail.com>
2019-05-29 19:07:34 -0700

    CardContainer.js

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
+import CardBanner from './CardBanner';
+
+funciton CardContainer(){
+    return (
+        <div className="banner-wrapper">
+        <CardBanner />
+        </div>
+    );
+};
+
+export default CardContainer;
