c92aa2d2ae2783a95cc76db70ed67161c9505c23
Ronald Cho <ronaldcho@gmail.com>
2019-05-29 20:31:52 -0700

    updated App.js

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HeaderContainer from './componets/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
     <div className="card-container">
+      <HeaderContainer />
       <CardContainer />
       <h3>Welcome to React Social Card!</h3>
       <p>
