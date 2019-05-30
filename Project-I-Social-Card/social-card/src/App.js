a11120ee2d408f2bcfbc83e2591f66215ae35573
teej387 <teej387@yahoo.com>
2019-05-29 23:46:13 -0500

    Started footer component - local images not uploading

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer.js';
 import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div>
       <HeaderContainer />
       <CardContainer />
+      <Footer />
     </div>
   );
 };
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footerContainer'>
+      <img className='comment' src= 'icons/speech-bubble.png' alt= 'comment bubble' />
+      <img className= 'refresh' src='icons/refresh-icon.png' alt='reload icon' />
+      
+    </div>
+  )
+}
+
+export default Footer;
