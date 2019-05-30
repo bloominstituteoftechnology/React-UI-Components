bb80f03eae90f3a67c57d719ee2461d24727c608
anthonyvigz <anthonyvigz@gmail.com>
2019-05-29 20:49:16 -0400

    imported footer component

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/HeaderComponents/HeaderContainer';
 import Card from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div className="mainApp">
       <Header />
       <a href="https://www.reactjs.org"><Card /></a>
+      <Footer />
     </div>
   );
 };
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return <div className="allIcons">
+
+            </div>
+}
+
+export default Footer;
