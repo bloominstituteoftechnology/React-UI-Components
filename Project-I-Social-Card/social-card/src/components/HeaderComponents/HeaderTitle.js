386633470ed4b09f4397dcd57c526666d5421614
Ronald Cho <ronaldcho@gmail.com>
2019-05-29 20:29:15 -0700

    HeaderTitle.js added

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div className="header-title-wrapper">
+            Lambda School
+            <HeaderContent />
+        </div>
+    );
+};
+
+export default HeaderTitle;
