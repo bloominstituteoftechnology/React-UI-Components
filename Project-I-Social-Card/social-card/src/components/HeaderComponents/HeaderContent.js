631cb1186fc382a79c0a6cb392430ba62bfd96bf
jtwray <tuckerwray84@gmail.com>
2019-05-30 15:55:25 -0400

    Corrects typos

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -9,7 +9,7 @@ function HeaderContent() {
       <p>
         Let's learn React by building simple interfaces with components. Don't
         try to overthink it, just keep it simple and have fun. Once you feel
-        comfortable using componenets you are will on your way to mastering
+        comfortable using components you are well on your way to mastering
         React.
       </p>
     </div>
