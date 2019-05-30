c7e7e02d19a98f20819bb6a28844022e3f772e73
Junior Dugue <jrdugue@gmail.com>
2019-05-30 00:08:43 -0400

    added font awesome icons and completed footer component

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,7 +5,10 @@ import "./Footer.scss";
 export default function Footer() {
   return (
     <div className="footer">
-      
+      <i class="far fa-comment"></i>
+      <i class="fas fa-sync-alt"><span>6</span></i>    
+      <i class="far fa-heart"><span>4</span></i>
+      <i class="far fa-envelope"></i>
     </div>
   )
 }
