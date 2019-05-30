a0c5a52745bb08d49921c953079a208b13bdb8c0
Angela Williams <angel_legato@icloud.com>
2019-05-29 20:34:01 -0400

    added all components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
+
+function CardBanner (){
+    return (
+<a className = 'cardBanner' href= 'https://www.reactjs.org'>
+<img className = 'reactImage' src ="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"></img>
+<CardContent />
+</a>
+    )
+}
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+
+function HeaderContainer (){
+   return(
+<header className ='header'>
+<ImageThumbnail />
+<HeaderContent />
+</header>
+   );
+}
+
+export default HeaderContainer;
