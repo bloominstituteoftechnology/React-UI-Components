645bb466391f542f7f73cf49451b47dbd11c5ac6
Ronald Cho <ronaldcho@gmail.com>
2019-05-29 20:25:02 -0700

    HeaderContainer.js added

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderCotainer() {
+    return(
+        <div className="container-header">
+            <ImageThumbnail/>
+            <HeaderYTitle />
+        </div>
+    );
+};
+
+export default HeaderCotainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+function ImageThumbnail() {
+    return (
+        <div>
+            <img 
+                alt="lambda logo"
+                className="thumb-img"
+                src="https://ibin.co/3whrpKSBbZ81.png"
+            />
+        </div>
+    );
+};
+
+export default ImageThumbnail;
