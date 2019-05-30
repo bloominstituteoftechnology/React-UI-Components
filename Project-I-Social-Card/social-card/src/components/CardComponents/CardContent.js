c7eb7dbad3c9404a62cd3be6f5b635a20df01058
jtwray <tuckerwray84@gmail.com>
2019-05-30 18:10:50 -0400

    Adds image and styling to card component/ adds footer /Encapsulates cardcomponent in anchor tag linking to reactjs.org

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+function CardContent() {
+  return (
+    <div>
+      <h3>Get started with React</h3>
+      <p>
+        React makes it painlesss to create interactive UIs. Design simple views
+        for each state in your application.
+        <br></br><a className="reactjslink" href="reactjs.org">reactjs.org</a>
+        </p>
+    </div>
+  );
+  
+}
+export default CardContent;
