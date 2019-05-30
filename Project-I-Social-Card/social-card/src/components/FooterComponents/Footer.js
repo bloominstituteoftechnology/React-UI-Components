c649035482db5259b1f9554dc53761b4f2b53251
anthonyvigz <anthonyvigz@gmail.com>
2019-05-29 20:59:12 -0400

    added icons to flexbox

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,7 +3,18 @@ import './Footer.css';
 
 function Footer() {
     return <div className="allIcons">
-
+                <div className="comment">
+                    <img src="https://image.flaticon.com/icons/svg/25/25663.svg" alt="cmnt" />
+                </div>
+                <div className="retweet">
+                    <img src="https://image.flaticon.com/icons/svg/25/25328.svg" alt="rt" />
+                </div>
+                <div className="like">
+                    <img src="https://image.flaticon.com/icons/svg/60/60993.svg" alt="like" />
+                </div>
+                <div className="share">
+                    <img src="https://image.flaticon.com/icons/svg/12/12194.svg" alt="share" />   
+                </div>
             </div>
 }
 
