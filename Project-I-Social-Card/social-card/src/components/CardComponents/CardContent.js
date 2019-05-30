836f05769458ee9e3d06d6f728662e7bea1c7602
teej387 <teej387@yahoo.com>
2019-05-29 21:59:17 -0500

    Added styling for HeaderContainer and CardContainer

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -6,7 +6,7 @@ function CardContent() {
     <div className='cardContent'>
       <h3>Get started with React</h3>
       <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-      <p>reactjs.org</p>
+      <p className='reactURL'>reactjs.org</p>
     </div>
   )
 }
