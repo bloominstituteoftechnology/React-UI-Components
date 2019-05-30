5799ef01d7b2d379d5e3d58ae0b6c6a24b76459a
Jordan Lanier <jordan.6195@yahoo.com>
2019-05-29 21:13:41 -0400

    some changes

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail.js';
+const HeaderContainer = () => {
+	return <ImageThumbnail />;
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+	return <h4>Lambda School @LambdaSchool 26 jan </h4>;
+};
+
+export default HeaderTitle;
