e5860936d4ddc67b601b69e2d9f57eee058865e3
Junior Dugue <jrdugue@gmail.com>
2019-05-29 20:39:59 -0400

    finished the hdeader title and header content component contents, working on card container now

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.scss';
 import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
 
 export default function HeaderContainer() {
   return (
     <div className="headerContainer">
       <ImageThumbnail />
+      <HeaderTitle />
     </div>
   )
 }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.scss';
+import HeaderContent from './HeaderContent';
 
 export default function HeaderTitle() {
   return (
-    <div>
-      
+    <div className="headerTitle">
+      <p>Lambda School <span className="grey">@LambdaSchool • 26 jan</span></p>
+      <HeaderContent/>
     </div>
   )
 }
