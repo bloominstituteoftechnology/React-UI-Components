a2fa1f0a18f3606f36760d127d90b5d8b49e1908
teej387 <teej387@yahoo.com>
2019-05-30 21:12:25 -0500

    Project-I icon images imported and work.

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Footer.css';
+import commentIcon from './icons/speech-bubble.png';
+import refresh from './icons/refresh-icon.png';
 
 function Footer() {
   return (
     <div className='footerContainer'>
-      <img className='comment' src= './icons/speech-bubble.png' alt= 'comment-bubble' />
-      <img className= 'refresh' src='./icons/refresh-icon.png' alt='reload icon' />
+      <img className='comment' src= {commentIcon} alt= 'comment-bubble' />
+      <img className= 'refresh' src= {refresh} alt= 'reload-icon'/>
       
     </div>
   )
