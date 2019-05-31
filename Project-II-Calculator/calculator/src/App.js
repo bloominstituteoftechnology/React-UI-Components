1177ed3ed0ee819bbf7f16a1eb20c6c159ffe553
teej387 <teej387@yahoo.com>
2019-05-30 22:56:42 -0500

    Imported button and display components into App

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
     <div className='App'>
-      
+      <NumberButton />
+      <ActionButton />
+      <CalculatorDisplay />
     </div>
   );
 };
