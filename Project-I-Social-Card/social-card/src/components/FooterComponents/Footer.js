99a964ea497558f23726eae92a13aa69c8b84b32
teej387 <teej387@yahoo.com>
2019-05-30 20:52:51 -0500

    img src issue for icons in local folder

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css';
 function Footer() {
   return (
     <div className='footerContainer'>
-      <img className='comment' src= 'icons/speech-bubble.png' alt= 'comment bubble' />
-      <img className= 'refresh' src='icons/refresh-icon.png' alt='reload icon' />
+      <img className='comment' src= './icons/speech-bubble.png' alt= 'comment-bubble' />
+      <img className= 'refresh' src='./icons/refresh-icon.png' alt='reload icon' />
       
     </div>
   )
