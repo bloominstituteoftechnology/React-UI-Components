7b028c4714109bcbb42c198e30f184132b5be632
Ronald Cho <ronaldcho@gmail.com>
2019-05-31 17:59:23 -0700

    updated social card

--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './_App';
 // import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 // import CardContainer from './components/CardComponents/CardContainer';
 
