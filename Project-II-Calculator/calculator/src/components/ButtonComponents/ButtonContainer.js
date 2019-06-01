925627f6a8c8a74069a1968fca39e9abd1371a96
teej387 <teej387@yahoo.com>
2019-06-01 01:58:12 -0500

    Used .map to create number buttons with unique classNames

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -5,13 +5,13 @@ import ActionButton from './ActionButton';
 
 let nums = [7, 8, 9, 4, 5, 6, 1, 2, 3];
 
-function ButtonContainer(props) {
+function ButtonContainer() {
   return (
     <div className='buttonContainer'>
       <ActionButton buttonStyle='clear' text='CLEAR' />
       {
         nums.map((num => {
-          return <NumberButton buttonStyle='number' text={num} />
+          return <NumberButton buttonStyle={`number ${num}`} text={num} />
         }))
       }
       <ActionButton buttonStyle='zero' text='0' />
