5c8e6d1deec764c455b08da0301573b51fd2ebdb
Lucas Larrubia <Lucas@Marcias-iMac.local>
2019-05-31 21:46:14 -0400

    added components now to style

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    return (
+    <div className="App">
+      <div className="display">0</div>
+      <div className="longButtonRow">
+        <NumberButton class="threeRow" text="clear" />
+        <ActionButton class="operator" text="÷" />
+      </div>
+      <div className="number-button">
+        <NumberButton class="number" text="7" />
+        <NumberButton class="number" text="8" />
+        <NumberButton class="number" text="9" />
+        <ActionButton class="operator" text="X" />
+      </div>
+      <div className="number-button">
+        <NumberButton class="number" text="4" />
+        <NumberButton class="number" text="5" />
+        <NumberButton class="number" text="6" />
+        <ActionButton class="operator" text="-" />
+        </div>
+      <div className="number-button">
+        <NumberButton class="number" text="1" />
+        <NumberButton class="number" text="2" />
+        <NumberButton class="number" text="3" />
+        <ActionButton class="operator" text="+" />
+      </div>
+      <div class="longButtonRow">
+        <NumberButton class="threeRow" text="0"/>
+        <ActionButton class="operator" text="=" />
+      </div>
     </div>
-  );
-};
+    )
+}
 
 export default App;
