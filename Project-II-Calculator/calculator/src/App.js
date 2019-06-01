ee5b641ee0d4cfcb87d10be0137c6481d8c44a24
jtwray <tuckerwray84@gmail.com>
2019-05-31 20:15:50 -0400

    Creates Component CalculatorDisplay adding colors, styles and alignment to inner text content and general shape/size

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
+import ActionButton from "./components/ButtonComponents/ActionButton"
+import NumberButton from "./components/ButtonComponents/NumberButton"
+import Display from "./components/DisplayComponents/CalculatorDisplay"
+
 
 const App = () => {
   return (
     <div>
       <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
+      <ActionButton/>
+      <NumberButton/>
+      <Display/>
       <p>
         <strong>
           Don't forget to `default export` your components and import them here
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+let answer = 0;
+
+function CalculatorDisplay(){
+    return(
+        <div className="calcdisplay">{answer}
+
+        </div>
+    );
+}
+
+export default CalculatorDisplay;
