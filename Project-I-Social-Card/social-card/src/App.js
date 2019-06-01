20995a1e6d30505f9ae20d9c94dd72b376caf311
cladams <cladams0203@gmail.com>
2019-06-01 18:40:00 -0400

    finished content and styling

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/HeaderComponents/HeaderContainer';
-
+import CardContainer from './components/CardComponents/CardContainer'
 const App = () => {
   return (
     <div className='container'>
-    
       <Header />
+      <CardContainer />
     </div>
   );
 };
