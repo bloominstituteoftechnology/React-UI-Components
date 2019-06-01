a6b3cc6e4bfda04dffc03cab968fcf7e47be4ac8
teej387 <teej387@yahoo.com>
2019-05-31 23:16:07 -0500

    Added ButtonContainer component

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ButtonContainer(props) {
+  return (
+    <div className='buttonContainer'>
+      {props.numbers};
+    </div>
+  );
+};
+
+export default ButtonContainer;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Display.css';
 
 function CalculatorDisplay() {
-  return <button className='calculatorDisplay'></button>
+  return <div className='calculatorDisplay'>0</div>
 }
 
 export default CalculatorDisplay;
