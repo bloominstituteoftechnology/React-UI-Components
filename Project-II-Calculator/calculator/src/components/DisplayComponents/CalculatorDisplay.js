a277fa7965de08219f77aaa7139c672d944abf72
anthonyvigz <anthonyvigz@gmail.com>
2019-06-02 12:35:35 -0400

    set up main calc box and started display component

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+let result = 0;
+
+function Display() {
+    return <div className="display">
+                <h1>{result}</h1>;
+            </div>
+}
+
+export default Display;
