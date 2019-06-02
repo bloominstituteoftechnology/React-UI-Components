9d6e0895044409d78bc5746b209a702a7cd910da
Soraia Mustafa <soraiamustafa@Soraias-iMac.local>
2019-06-02 09:44:31 -0400

    completed Header components, Card components and Footer components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,28 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import moment from 'moment';
+
+// Create a <HeaderTitle /> component that displays the Lambda School header text, @LambdaSchool handle and timestamp.
+
+
+
+function HeaderTitle(){
+
+    let date = moment().format("MMM Do");
+    
+    
+        return (
+
+            <div className="title">
+                <p className="headerLogo">
+                    <h1 className="lamHeader">Lambda School</h1>
+                    <h3 className="lambdaSchool">@LambdaSchool &middot; {date} </h3> 
+                </p>
+                    <HeaderContent />
+            </div>
+            
+        );
+    }
+
+export default HeaderTitle;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+// Create an <ImageThumbnail /> component using this image url 
+// https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png as it's img src.
+
+function ImageThumbnail(){
+    return (
+        <div className="logoImage">
+            <img className="image" src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="lambda logo"/>
+        </div>
+    );
+}
+
+export default ImageThumbnail;
