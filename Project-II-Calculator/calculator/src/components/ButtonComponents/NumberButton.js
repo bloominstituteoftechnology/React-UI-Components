f880e23022d7dd755a2d1d58810c35d6b8bc8f38
Angela Williams <angel_legato@icloud.com>
2019-06-02 20:31:18 -0400

    Everything except for styling complete

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton (props){
+    return(
+        <div className={props.class}>{props.text}</div>
+    );
+}
+
+export default NumberButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,34 @@
 import React from 'react';
 import './Display.css';
+import NumberButton from '../ButtonComponents/NumberButton';
+import ActionButton from '../ButtonComponents/ActionButton';
+import { PadNumbers } from "../data/Calc";
+import { PadAction } from "../data/Calc";
+
+
+function CalculatorDisplay(){
+    return(
+        <div className="calc">
+            <div className="calcScreen">0</div>
+            <div className="numPad">
+                <div className="wideButton">clear</div>
+                    {
+                        PadNumbers.map( num => {
+                        return <NumberButton class="numButton" text={num.text}/>
+                        })
+                    }
+                <div className="wideButton">0</div>
+            </div>
+
+             <div className="actionPad">
+                {
+                    PadAction.map( action => {
+                        return <ActionButton class="actionButton" text={action.text}/>
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+ export default CalculatorDisplay;
