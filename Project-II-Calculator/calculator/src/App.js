ae5a476ed11a910677ae84ab0cf786a799e7017c
Ronald Cho <ronaldcho@gmail.com>
2019-06-04 20:44:45 -0700

    index.css App.js App.css

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
+
 const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <CalculatorDisplay />
+            <NumberButton />
+            <ActionButton />
+        </div>
+    );
 };
 
 export default App;
