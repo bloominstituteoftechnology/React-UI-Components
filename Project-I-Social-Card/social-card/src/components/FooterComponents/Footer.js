83db1f65bb0ec3a3f1e3d84fc8c44d335ae8d6fe
josepaniagua <jpanjr@gmail.com>
2019-06-13 03:18:21 -0700

    added border all around

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
+
     return (
         <div className='footerCont'>
-            <img className='iconImg' src="https://img.icons8.com/metro/26/000000/topic.png" />
+            <img className='iconImg' src="https://img.icons8.com/metro/26/000000/topic.png" alt='comment' />
             <div className='flexIcons'>
-                <img className='iconImg' src="https://img.icons8.com/ios/26/000000/connection-sync.png" />
+                <img className='iconImg' src="https://img.icons8.com/ios/26/000000/connection-sync.png" alt='cycle' />
                 <p>6</p>
             </div>
             <div className='flexIcons'>
-                <img className='iconImg' src="https://img.icons8.com/ios/50/000000/hearts.png" />
+                <img className='iconImg' src="https://img.icons8.com/ios/50/000000/hearts.png" alt='heart' />
                 <p>4</p>
             </div>
-            <img className='iconImg' src="https://img.icons8.com/ios/50/000000/new-post.png" />
+            <img className='iconImg' src="https://img.icons8.com/ios/50/000000/new-post.png" alt='mail' />
         </div>
     );
 };
