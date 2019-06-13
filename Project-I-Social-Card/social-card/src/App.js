686e834bc262f6ba767ded6a5a2ec0bcd5f77093
josepaniagua <jpanjr@gmail.com>
2019-06-13 01:22:52 -0700

    created footercont

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
 import CardContainer from "./components/CardComponents/CardContainer";
+import Footer from "./components/FooterComponents/Footer";
 
 const App = () => {
   return (
     <div>
         <HeaderContainer/>
         <CardContainer/>
+        <Footer/>
       {/*<h3>Welcome to React Social Card!</h3>*/}
       {/*<p>*/}
       {/*  Begin by exploring the `components` directory. You'll notice we have a*/}
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footerCont'>
+            <h3>Place Holder</h3>
+        </div>
+    );
+};
+
+export default Footer
