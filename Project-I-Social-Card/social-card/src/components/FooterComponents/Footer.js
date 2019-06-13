7a134cd843c0c221569010fc4f831c829bfcdae8
josepaniagua <jpanjr@gmail.com>
2019-06-13 02:35:41 -0700

    added icons to footer

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -4,7 +4,16 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className='footerCont'>
-            <h3>Place Holder</h3>
+            <img className='iconImg' src="https://img.icons8.com/metro/26/000000/topic.png" />
+            <div className='flexIcons'>
+                <img className='iconImg' src="https://img.icons8.com/ios/26/000000/connection-sync.png" />
+                <p>6</p>
+            </div>
+            <div className='flexIcons'>
+                <img className='iconImg' src="https://img.icons8.com/ios/50/000000/hearts.png" />
+                <p>4</p>
+            </div>
+            <img className='iconImg' src="https://img.icons8.com/ios/50/000000/new-post.png" />
         </div>
     );
 };
