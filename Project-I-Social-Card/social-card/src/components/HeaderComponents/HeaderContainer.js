66c5d5169f33464c6930150785f507dde00bc157
josepaniagua <jpanjr@gmail.com>
2019-06-12 18:07:04 -0700

    Headercontent

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail.js';
+import HeaderTitle from './HeaderTitle.js';
+
+const HeaderContainer = () => {
+    return (
+        <div>
+            <ImageThumbnail/>
+            <div>
+                <HeaderTitle/>
+            </div>
+        </div>
+    );
+};
+
+export default HeaderContainer
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default HeaderTitle
