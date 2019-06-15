c5372c9eb4605b019d449a4bfb1e8ce795491af1
josepaniagua <jpanjr@gmail.com>
2019-06-15 00:15:33 -0700

    intital commit for calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = (props) => {
+    return <button props.buttonStyle>{props.text}</button>;
+};
+
+export default ActionButton
